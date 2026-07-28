45f105b91b2d54ab417d8838767193cb368e121f
dginovker <danielginovker@gmail.com>
2026-07-27 20:55:16 -0700

    match scanPercentToReal as C++ (call scanReal * 0.01f)

--- Code/GameEngine/Source/Common/INI/ini.cpp
+++ Code/GameEngine/Source/Common/INI/ini.cpp
@@ -1630,10 +1630,7 @@ void INI::initFromINIMulti( void *what, const MultiIniFieldParse& parseTableList
 //-------------------------------------------------------------------------------------------------
 /*static*/ Real INI::scanPercentToReal(const char* token)
 {
-	Real value;
-	if (sscanf( token, "%f", &value ) != 1)
-		throw INI_INVALID_DATA;
-	return value / 100.0f;
+	return scanReal(token) * 0.01f;
 }
 
 //-------------------------------------------------------------------------------------------------
