f43de31843f4fc3f86b5f40df6f3b454e9998eb1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 17:45:32 -0400

    Open-BFME5 convert AIWanderInPlaceState ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/AIWanderInPlaceStateCtorThunk.cpp
@@ -0,0 +1,78 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class StateMachine;
+
+class AIWanderInPlaceState
+{
+public:
+    AIWanderInPlaceState(StateMachine *);
+};
+
+__declspec(naked) AIWanderInPlaceState::AIWanderInPlaceState(StateMachine *)
+{
+    __asm {
+        _emit 051h
+        _emit 056h
+        _emit 051h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 089h
+        _emit 064h
+        _emit 024h
+        _emit 008h
+        _emit 08Bh
+        _emit 0CCh
+        _emit 068h
+        _emit 068h
+        _emit 0A9h
+        _emit 009h
+        _emit 001h
+        _emit 0E8h
+        _emit 02Bh
+        _emit 090h
+        _emit 070h
+        _emit 000h
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 050h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 0E1h
+        _emit 025h
+        _emit 0EBh
+        _emit 0FFh
+        _emit 033h
+        _emit 0C0h
+        _emit 0C7h
+        _emit 006h
+        _emit 010h
+        _emit 0A9h
+        _emit 009h
+        _emit 001h
+        _emit 089h
+        _emit 046h
+        _emit 050h
+        _emit 089h
+        _emit 046h
+        _emit 054h
+        _emit 089h
+        _emit 046h
+        _emit 058h
+        _emit 089h
+        _emit 046h
+        _emit 05Ch
+        _emit 089h
+        _emit 046h
+        _emit 060h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 059h
+        _emit 0C2h
+        _emit 004h
+        _emit 000h
+    }
+}
