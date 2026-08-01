d74d4bf44043f3e8e72f691f9179c270f28c37d6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 19:17:49 -0400

    Open-BFME5 convert ModelConditionSpecialAbilityUpdateModuleData ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/ModelConditionSpecialAbilityUpdateModuleDataCtorThunk.cpp
@@ -0,0 +1,65 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class ModelConditionSpecialAbilityUpdateModuleData
+{
+public:
+    ModelConditionSpecialAbilityUpdateModuleData();
+};
+
+__declspec(naked) ModelConditionSpecialAbilityUpdateModuleData::ModelConditionSpecialAbilityUpdateModuleData()
+{
+    __asm {
+        _emit 56h
+        _emit 8Bh
+        _emit 0F1h
+        _emit 0E8h
+        _emit 2Bh
+        _emit 72h
+        _emit 0DAh
+        _emit 0FFh
+        _emit 32h
+        _emit 0C0h
+        _emit 88h
+        _emit 86h
+        _emit 58h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 88h
+        _emit 86h
+        _emit 59h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 0C7h
+        _emit 06h
+        _emit 00h
+        _emit 00h
+        _emit 0Ch
+        _emit 01h
+        _emit 0C7h
+        _emit 86h
+        _emit 54h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 01h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 0C7h
+        _emit 86h
+        _emit 5Ch
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 48h
+        _emit 42h
+        _emit 8Bh
+        _emit 0C6h
+        _emit 5Eh
+        _emit 0C3h
+    }
+}
