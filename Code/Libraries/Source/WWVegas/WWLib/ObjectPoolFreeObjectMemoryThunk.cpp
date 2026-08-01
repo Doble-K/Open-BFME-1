b45e3c5ae9589dccf19f7a84d1b9b0d365671363
dginovker <danielginovker@gmail.com>
2026-08-01 07:32:28 -0700

    Luna Open-BFME4 convert ObjectPool allocate memory thunk

--- Code/Libraries/Source/WWVegas/WWLib/ObjectPoolFreeObjectMemoryThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ObjectPoolFreeObjectMemoryThunk.cpp
@@ -22,8 +22,21 @@ template<class Type, int BlockSize>
 class ObjectPoolClass
 {
 public:
+	Type * Allocate_Object_Memory();
 	void Free_Object_Memory(Type *);
 };
+
+template<class Type, int BlockSize>
+__declspec(naked) Type * ObjectPoolClass<Type, BlockSize>::Allocate_Object_Memory()
+{
+	__asm {
+		_emit 0E9h
+		_emit 068h
+		_emit 0EDh
+		_emit 06Ch
+		_emit 000h
+	}
+}
 
 template<class Type, int BlockSize>
 __declspec(naked) void ObjectPoolClass<Type, BlockSize>::Free_Object_Memory(Type *)
@@ -41,3 +54,4 @@ template __declspec(naked) void ObjectPoolClass<AABTreeLinkClass, 256>::Free_Obj
 template __declspec(naked) void ObjectPoolClass<AABTreeNodeClass, 256>::Free_Object_Memory(AABTreeNodeClass *);
 template __declspec(naked) void ObjectPoolClass<CameraShakeSystemClass::CameraShakerClass, 256>::Free_Object_Memory(CameraShakeSystemClass::CameraShakerClass *);
 template __declspec(naked) void ObjectPoolClass<GenericSLNode, 256>::Free_Object_Memory(GenericSLNode *);
+template __declspec(naked) CameraShakeSystemClass::CameraShakerClass * ObjectPoolClass<CameraShakeSystemClass::CameraShakerClass, 256>::Allocate_Object_Memory();
