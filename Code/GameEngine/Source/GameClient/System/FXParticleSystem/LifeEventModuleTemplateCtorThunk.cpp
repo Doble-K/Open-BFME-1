e978b1c6c20fc6b3c4504ae63887d17fac0f7ae7
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 14:20:28 -0400

    Open-BFME5 convert LifeEventModuleTemplate constructor

---
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/LifeEventModuleTemplateCtorThunk.cpp
@@ -0,0 +1,130 @@
+// cl: /DNDEBUG /MD /EHsc
+
+namespace FXParticleSystem
+{
+class LifeEventModuleTemplate
+{
+public:
+    LifeEventModuleTemplate();
+};
+
+__declspec(naked) LifeEventModuleTemplate::LifeEventModuleTemplate()
+{
+    __asm {
+        _emit 06Ah
+        _emit 0FFh
+        _emit 068h
+        _emit 0D8h
+        _emit 0ADh
+        _emit 003h
+        _emit 001h
+        _emit 064h
+        _emit 0A1h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 050h
+        _emit 064h
+        _emit 089h
+        _emit 025h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 051h
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 0C7h
+        _emit 046h
+        _emit 004h
+        _emit 05Ch
+        _emit 037h
+        _emit 007h
+        _emit 001h
+        _emit 0B0h
+        _emit 001h
+        _emit 088h
+        _emit 046h
+        _emit 008h
+        _emit 088h
+        _emit 046h
+        _emit 009h
+        _emit 057h
+        _emit 089h
+        _emit 074h
+        _emit 024h
+        _emit 008h
+        _emit 0C7h
+        _emit 006h
+        _emit 048h
+        _emit 038h
+        _emit 007h
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 004h
+        _emit 044h
+        _emit 038h
+        _emit 007h
+        _emit 001h
+        _emit 08Dh
+        _emit 07Eh
+        _emit 00Ch
+        _emit 08Bh
+        _emit 0CFh
+        _emit 0C7h
+        _emit 044h
+        _emit 024h
+        _emit 014h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 0E8h
+        _emit 0DDh
+        _emit 01Ah
+        _emit 0A3h
+        _emit 0FFh
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 00Ch
+        _emit 0C7h
+        _emit 007h
+        _emit 0A4h
+        _emit 012h
+        _emit 011h
+        _emit 001h
+        _emit 0C7h
+        _emit 006h
+        _emit 090h
+        _emit 012h
+        _emit 011h
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 004h
+        _emit 08Ch
+        _emit 012h
+        _emit 011h
+        _emit 001h
+        _emit 05Fh
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 064h
+        _emit 089h
+        _emit 00Dh
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 083h
+        _emit 0C4h
+        _emit 010h
+        _emit 0C3h
+    }
+}
+}
