158065e0f25302256108d07dfc2abe7adb701c78
dginovker <danielginovker@gmail.com>
2026-08-01 06:16:11 -0700

    Luna Open-BFME5 convert Bridge module constructor thunk

---
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/BridgeBehaviorModuleDataCtorThunk.cpp
@@ -0,0 +1,18 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class BridgeBehaviorModuleData
+{
+public:
+    BridgeBehaviorModuleData();
+};
+
+__declspec(naked) BridgeBehaviorModuleData::BridgeBehaviorModuleData()
+{
+    __asm {
+        _emit 0E9h
+        _emit 0A8h
+        _emit 05Bh
+        _emit 01Dh
+        _emit 000h
+    }
+}
