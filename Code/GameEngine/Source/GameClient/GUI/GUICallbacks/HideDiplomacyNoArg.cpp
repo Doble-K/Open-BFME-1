00504f77621128a4b6ecb12bd4120ba3b29e5554
dginovker <danielginovker@gmail.com>
2026-08-01 04:39:12 -0700

    Luna Open-BFME5 convert HideDiplomacy function

---
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/HideDiplomacyNoArg.cpp
@@ -0,0 +1,55 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+__declspec(naked) void HideDiplomacy()
+{
+	__asm {
+		_emit 0A1h
+		_emit 0E4h
+		_emit 049h
+		_emit 02Fh
+		_emit 001h
+		_emit 085h
+		_emit 0C0h
+		_emit 074h
+		_emit 025h
+		_emit 08Ah
+		_emit 088h
+		_emit 054h
+		_emit 002h
+		_emit 000h
+		_emit 000h
+		_emit 084h
+		_emit 0C9h
+		_emit 075h
+		_emit 01Bh
+		_emit 0B1h
+		_emit 001h
+		_emit 088h
+		_emit 088h
+		_emit 054h
+		_emit 002h
+		_emit 000h
+		_emit 000h
+		_emit 0A1h
+		_emit 058h
+		_emit 04Bh
+		_emit 02Fh
+		_emit 001h
+		_emit 088h
+		_emit 048h
+		_emit 050h
+		_emit 08Bh
+		_emit 00Dh
+		_emit 0E8h
+		_emit 019h
+		_emit 02Fh
+		_emit 001h
+		_emit 0E9h
+		_emit 0C4h
+		_emit 0E2h
+		_emit 0AFh
+		_emit 0FFh
+		_emit 0C3h
+	}
+}
+
