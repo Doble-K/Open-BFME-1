3a8341b35e1b55a1bb203b2c4773a17c0f90111a
dginovker <danielginovker@gmail.com>
2026-07-31 23:57:06 -0700

    Luna Open-BFME8: convert GhostObjectManager constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/GhostObject.cpp
@@ -0,0 +1,19 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/sweep
+
+class GhostObjectManager
+{
+	virtual void virtualAnchor( void ) = 0;
+	unsigned int m_value;
+	unsigned char m_active;
+	unsigned char m_reserved;
+
+public:
+	GhostObjectManager( void );
+};
+
+GhostObjectManager::GhostObjectManager( void )
+{
+	m_active = 0;
+	m_reserved = 0;
+	m_value = 0;
+}
