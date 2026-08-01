b11cfab651b6044f498434a0c4acd1e80d8e483d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 15:49:50 -0400

    Open-BFME5 convert TextureClass release alias

--- Code/Libraries/Source/WWVegas/WW3D2/TextureBaseReleaseRefThunk.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/TextureBaseReleaseRefThunk.cpp
@@ -47,3 +47,51 @@ __declspec(naked) void TextureBaseClass::Release_Ref()
         _emit 0C3h
     }
 }
+
+class TextureClass
+{
+public:
+    void Release_Ref();
+};
+
+__declspec(naked) void TextureClass::Release_Ref()
+{
+    __asm {
+        _emit 0F7h
+        _emit 041h
+        _emit 004h
+        _emit 0FFh
+        _emit 0FFh
+        _emit 000h
+        _emit 000h
+        _emit 074h
+        _emit 01Ah
+        _emit 066h
+        _emit 0FFh
+        _emit 049h
+        _emit 004h
+        _emit 08Bh
+        _emit 041h
+        _emit 004h
+        _emit 0A9h
+        _emit 0FFh
+        _emit 0FFh
+        _emit 000h
+        _emit 000h
+        _emit 075h
+        _emit 00Ch
+        _emit 0A9h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 001h
+        _emit 074h
+        _emit 005h
+        _emit 08Bh
+        _emit 001h
+        _emit 0FFh
+        _emit 060h
+        _emit 020h
+        _emit 0C3h
+    }
+}
