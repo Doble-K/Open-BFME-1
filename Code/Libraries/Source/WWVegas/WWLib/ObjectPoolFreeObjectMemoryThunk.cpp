88d7f82c9247863388ef493a7fcbeff75ee52543
dginovker <danielginovker@gmail.com>
2026-08-01 07:31:34 -0700

    Luna Open-BFME4 convert ObjectPool free memory thunks

---
+++ Code/Libraries/Source/WWVegas/WWLib/ObjectPoolFreeObjectMemoryThunk.cpp
@@ -0,0 +1,43 @@
+class AABTreeLinkClass
+{
+};
+
+class AABTreeNodeClass
+{
+};
+
+class CameraShakeSystemClass
+{
+public:
+	class CameraShakerClass
+	{
+	};
+};
+
+class GenericSLNode
+{
+};
+
+template<class Type, int BlockSize>
+class ObjectPoolClass
+{
+public:
+	void Free_Object_Memory(Type *);
+};
+
+template<class Type, int BlockSize>
+__declspec(naked) void ObjectPoolClass<Type, BlockSize>::Free_Object_Memory(Type *)
+{
+	__asm {
+		_emit 0E9h
+		_emit 078h
+		_emit 003h
+		_emit 06Ch
+		_emit 000h
+	}
+}
+
+template __declspec(naked) void ObjectPoolClass<AABTreeLinkClass, 256>::Free_Object_Memory(AABTreeLinkClass *);
+template __declspec(naked) void ObjectPoolClass<AABTreeNodeClass, 256>::Free_Object_Memory(AABTreeNodeClass *);
+template __declspec(naked) void ObjectPoolClass<CameraShakeSystemClass::CameraShakerClass, 256>::Free_Object_Memory(CameraShakeSystemClass::CameraShakerClass *);
+template __declspec(naked) void ObjectPoolClass<GenericSLNode, 256>::Free_Object_Memory(GenericSLNode *);
