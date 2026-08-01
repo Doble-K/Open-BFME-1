3cf0320fbd4314b61aadee3a5175242956650e3f
dginovker <danielginovker@gmail.com>
2026-08-01 07:06:42 -0700

    Luna Open-BFME5 convert Bridge height thunk

---
+++ Code/GameEngine/Source/Common/RTS/BridgeGetBridgeHeightThunk.cpp
@@ -0,0 +1,20 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+struct Coord3D;
+
+class Bridge
+{
+public:
+    float getBridgeHeight(const Coord3D *, Coord3D *);
+};
+
+__declspec(naked) float Bridge::getBridgeHeight(const Coord3D *, Coord3D *)
+{
+    __asm {
+        _emit 0E9h
+        _emit 019h
+        _emit 0BDh
+        _emit 019h
+        _emit 000h
+    }
+}
