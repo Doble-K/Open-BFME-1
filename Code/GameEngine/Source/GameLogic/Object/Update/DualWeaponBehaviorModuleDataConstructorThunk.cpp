0717d1dd8bb7a6d5e3bc38cc31c1e4dc652c74d3
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 12:59:10 -0400

    Open-BFME5 convert DualWeapon module data constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/DualWeaponBehaviorModuleDataConstructorThunk.cpp
@@ -0,0 +1,41 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+#include "PreRTS.h"
+
+class DualWeaponBehaviorModuleData
+{
+public:
+    DualWeaponBehaviorModuleData();
+};
+
+__declspec(naked) DualWeaponBehaviorModuleData::DualWeaponBehaviorModuleData()
+{
+    __asm {
+        _emit 08Bh
+        _emit 0C1h
+        _emit 033h
+        _emit 0C9h
+        _emit 0C7h
+        _emit 000h
+        _emit 068h
+        _emit 034h
+        _emit 00Ah
+        _emit 001h
+        _emit 089h
+        _emit 048h
+        _emit 008h
+        _emit 088h
+        _emit 048h
+        _emit 00Ch
+        _emit 089h
+        _emit 048h
+        _emit 010h
+        _emit 088h
+        _emit 048h
+        _emit 014h
+        _emit 088h
+        _emit 048h
+        _emit 015h
+        _emit 0C3h
+    }
+}
