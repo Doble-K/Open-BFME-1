2871126640565be96fa8d9a2024026dd500c644e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 19:20:45 -0400

    Open-BFME5 convert RunOffMapBehaviorModuleData ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/RunOffMapBehaviorModuleDataCtorThunk.cpp
@@ -0,0 +1,109 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class RunOffMapBehaviorModuleData
+{
+public:
+    RunOffMapBehaviorModuleData();
+};
+
+__declspec(naked) RunOffMapBehaviorModuleData::RunOffMapBehaviorModuleData()
+{
+    __asm {
+        _emit 6Ah
+        _emit 0FFh
+        _emit 68h
+        _emit 33h
+        _emit 19h
+        _emit 00h
+        _emit 01h
+        _emit 64h
+        _emit 0A1h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 50h
+        _emit 64h
+        _emit 89h
+        _emit 25h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 51h
+        _emit 56h
+        _emit 8Bh
+        _emit 0F1h
+        _emit 89h
+        _emit 74h
+        _emit 24h
+        _emit 04h
+        _emit 33h
+        _emit 0C0h
+        _emit 8Dh
+        _emit 4Eh
+        _emit 14h
+        _emit 0C7h
+        _emit 06h
+        _emit 0F0h
+        _emit 0E9h
+        _emit 08h
+        _emit 01h
+        _emit 89h
+        _emit 44h
+        _emit 24h
+        _emit 10h
+        _emit 89h
+        _emit 01h
+        _emit 0BAh
+        _emit 00h
+        _emit 00h
+        _emit 20h
+        _emit 41h
+        _emit 68h
+        _emit 50h
+        _emit 6Eh
+        _emit 33h
+        _emit 01h
+        _emit 0C6h
+        _emit 44h
+        _emit 24h
+        _emit 14h
+        _emit 01h
+        _emit 88h
+        _emit 46h
+        _emit 18h
+        _emit 89h
+        _emit 56h
+        _emit 08h
+        _emit 89h
+        _emit 56h
+        _emit 0Ch
+        _emit 88h
+        _emit 46h
+        _emit 10h
+        _emit 0E8h
+        _emit 0E2h
+        _emit 14h
+        _emit 76h
+        _emit 00h
+        _emit 8Bh
+        _emit 4Ch
+        _emit 24h
+        _emit 08h
+        _emit 8Bh
+        _emit 0C6h
+        _emit 5Eh
+        _emit 64h
+        _emit 89h
+        _emit 0Dh
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 83h
+        _emit 0C4h
+        _emit 10h
+        _emit 0C3h
+    }
+}
