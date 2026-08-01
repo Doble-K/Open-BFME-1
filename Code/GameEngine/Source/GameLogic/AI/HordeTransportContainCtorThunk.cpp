cc797a306fa1527fcd767a0b94a83808cf3ec97c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 18:26:19 -0400

    Open-BFME5 convert HordeTransportContain ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/HordeTransportContainCtorThunk.cpp
@@ -0,0 +1,131 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class HordeTransportContain
+{
+public:
+    HordeTransportContain(Thing *, const ModuleData *);
+};
+
+__declspec(naked) HordeTransportContain::HordeTransportContain(Thing *, const ModuleData *)
+{
+    __asm {
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 008h
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 008h
+        _emit 050h
+        _emit 051h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 09Dh
+        _emit 06Ch
+        _emit 0DBh
+        _emit 0FFh
+        _emit 0C7h
+        _emit 006h
+        _emit 088h
+        _emit 006h
+        _emit 00Bh
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 00Ch
+        _emit 0C0h
+        _emit 005h
+        _emit 00Bh
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 010h
+        _emit 0B0h
+        _emit 005h
+        _emit 00Bh
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 020h
+        _emit 008h
+        _emit 004h
+        _emit 00Bh
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 024h
+        _emit 0ECh
+        _emit 003h
+        _emit 00Bh
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 028h
+        _emit 0E8h
+        _emit 003h
+        _emit 00Bh
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 02Ch
+        _emit 0D8h
+        _emit 003h
+        _emit 00Bh
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 030h
+        _emit 09Ch
+        _emit 003h
+        _emit 00Bh
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 034h
+        _emit 08Ch
+        _emit 003h
+        _emit 00Bh
+        _emit 001h
+        _emit 0C7h
+        _emit 086h
+        _emit 0D4h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 088h
+        _emit 003h
+        _emit 00Bh
+        _emit 001h
+        _emit 0C6h
+        _emit 086h
+        _emit 0E8h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 0C7h
+        _emit 086h
+        _emit 0E4h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 018h
+        _emit 0FCh
+        _emit 0FFh
+        _emit 0FFh
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C2h
+        _emit 008h
+        _emit 000h
+    }
+}
