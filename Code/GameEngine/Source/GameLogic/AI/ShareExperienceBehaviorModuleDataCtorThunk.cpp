b255d72f9450b9b8409c61196e05410855f4f378
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 19:21:44 -0400

    Open-BFME5 convert ShareExperienceBehaviorModuleData ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/ShareExperienceBehaviorModuleDataCtorThunk.cpp
@@ -0,0 +1,93 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class ShareExperienceBehaviorModuleData
+{
+public:
+    ShareExperienceBehaviorModuleData();
+};
+
+__declspec(naked) ShareExperienceBehaviorModuleData::ShareExperienceBehaviorModuleData()
+{
+    __asm {
+        _emit 6Ah
+        _emit 0FFh
+        _emit 68h
+        _emit 58h
+        _emit 0BDh
+        _emit 00h
+        _emit 01h
+        _emit 64h
+        _emit 0A1h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 50h
+        _emit 64h
+        _emit 89h
+        _emit 25h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 51h
+        _emit 56h
+        _emit 8Bh
+        _emit 0F1h
+        _emit 89h
+        _emit 74h
+        _emit 24h
+        _emit 04h
+        _emit 33h
+        _emit 0C0h
+        _emit 8Dh
+        _emit 4Eh
+        _emit 14h
+        _emit 89h
+        _emit 44h
+        _emit 24h
+        _emit 10h
+        _emit 0C7h
+        _emit 06h
+        _emit 0C8h
+        _emit 5Fh
+        _emit 0Ah
+        _emit 01h
+        _emit 89h
+        _emit 46h
+        _emit 08h
+        _emit 89h
+        _emit 46h
+        _emit 0Ch
+        _emit 0C7h
+        _emit 46h
+        _emit 10h
+        _emit 00h
+        _emit 00h
+        _emit 80h
+        _emit 3Fh
+        _emit 0E8h
+        _emit 8Ch
+        _emit 1Dh
+        _emit 0E3h
+        _emit 0FFh
+        _emit 8Bh
+        _emit 4Ch
+        _emit 24h
+        _emit 08h
+        _emit 8Bh
+        _emit 0C6h
+        _emit 5Eh
+        _emit 64h
+        _emit 89h
+        _emit 0Dh
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 83h
+        _emit 0C4h
+        _emit 10h
+        _emit 0C3h
+    }
+}
