fc9ffa04282c753601425075c457bf8f0bfce54f
dginovker <danielginovker@gmail.com>
2026-08-01 08:26:07 -0700

    Luna Open-BFME4 convert ICoord2D vector resize thunk

--- Code/Libraries/Source/WWVegas/WWLib/VectorReserveHorzLineThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/VectorReserveHorzLineThunk.cpp
@@ -2,6 +2,10 @@ struct HorzLine
 {
 };
 
+struct ICoord2D
+{
+};
+
 namespace _STL
 {
 	template <class Type>
@@ -14,6 +18,7 @@ namespace _STL
 	{
 	public:
 		void reserve(unsigned int);
+		void resize(unsigned int);
 	};
 
 	template <class Type, class Allocator>
@@ -29,4 +34,18 @@ namespace _STL
 	}
 
 	template __declspec(naked) void vector<HorzLine, allocator<HorzLine> >::reserve(unsigned int);
+
+	template <class Type, class Allocator>
+	__declspec(naked) void vector<Type, Allocator>::resize(unsigned int)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 080h
+			_emit 007h
+			_emit 045h
+			_emit 000h
+		}
+	}
+
+	template __declspec(naked) void vector<ICoord2D, allocator<ICoord2D> >::resize(unsigned int);
 }
