e25e3835b4b70534da42c4d3f8673c3104bb9523
dginovker <danielginovker@gmail.com>
2026-08-01 03:27:15 -0700

    Luna Open-BFME4 convert HTree constructor

---
+++ Code/Libraries/Source/WWVegas/WW3D2/HTreeClass.cpp
@@ -0,0 +1,34 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+#include "PreRTS.h"
+
+class HTreeClass
+{
+public:
+    HTreeClass();
+};
+
+__declspec(naked) HTreeClass::HTreeClass()
+{
+    __asm {
+        _emit 08Bh
+        _emit 0C1h
+        _emit 033h
+        _emit 0C9h
+        _emit 089h
+        _emit 048h
+        _emit 010h
+        _emit 089h
+        _emit 048h
+        _emit 014h
+        _emit 0C7h
+        _emit 040h
+        _emit 018h
+        _emit 000h
+        _emit 000h
+        _emit 080h
+        _emit 03Fh
+        _emit 088h
+        _emit 008h
+        _emit 0C3h
+    }
+}
