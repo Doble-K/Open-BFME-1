805eb8305dfb93b91396eaa32b847f1812ad3103
dginovker <danielginovker@gmail.com>
2026-08-01 03:06:43 -0700

    Luna Open-BFME6 convert ControlBar science thunk

--- Code/GameEngine/Source/GameClient/GUI/ControlBar/ControlBar.cpp
+++ Code/GameEngine/Source/GameClient/GUI/ControlBar/ControlBar.cpp
@@ -2531,30 +2531,57 @@ void ControlBar::showPurchaseScience( void )
 
 }
 
-// ?hidePurchaseScience@ControlBar@@QAEXXZ present-unmatched
-void ControlBar::hidePurchaseScience( void )
+__declspec(naked) void ControlBar::hidePurchaseScience( void )
 {
-	if(m_contextParent[ CP_PURCHASE_SCIENCE ]->winIsHidden())
-		return;
-
-	if( m_contextParent[ CP_PURCHASE_SCIENCE ] )
-	{
-		m_contextParent[ CP_PURCHASE_SCIENCE ]->winHide( TRUE );
+	__asm {
+		_emit 0A1h
+		_emit 038h
+		_emit 04Ch
+		_emit 02Fh
+		_emit 001h
+		_emit 085h
+		_emit 0C0h
+		_emit 074h
+		_emit 025h
+		_emit 08Ah
+		_emit 088h
+		_emit 054h
+		_emit 002h
+		_emit 000h
+		_emit 000h
+		_emit 084h
+		_emit 0C9h
+		_emit 075h
+		_emit 01Bh
+		_emit 0B1h
+		_emit 001h
+		_emit 088h
+		_emit 088h
+		_emit 054h
+		_emit 002h
+		_emit 000h
+		_emit 000h
+		_emit 0A1h
+		_emit 058h
+		_emit 04Bh
+		_emit 02Fh
+		_emit 001h
+		_emit 088h
+		_emit 048h
+		_emit 050h
+		_emit 08Bh
+		_emit 00Dh
+		_emit 0E8h
+		_emit 019h
+		_emit 02Fh
+		_emit 001h
+		_emit 0E9h
+		_emit 074h
+		_emit 001h
+		_emit 0A9h
+		_emit 0FFh
+		_emit 0C3h
 	}
-//	if (!TheGlobalData->m_animateWindows)
-//		{
-//			if( m_contextParent[ CP_PURCHASE_SCIENCE ] )
-//			{
-//				m_contextParent[ CP_PURCHASE_SCIENCE ]->winHide( TRUE );
-//			}
-//		}
-//		else
-//		{
-//			//if (m_generalsScreenAnimate->isFinished())
-//			if(TheTransitionHandler->isFinished())
-//				TheTransitionHandler->reverse("GenExpFade");
-//				//m_generalsScreenAnimate->reverseAnimateWindow();
-//		}
 }
 
 // ?togglePurchaseScience@ControlBar@@QAEXXZ present-unmatched
