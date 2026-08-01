da9d6068968a0064bf586f609d4db3d5abd9e8d3
dginovker <danielginovker@gmail.com>
2026-08-01 04:21:30 -0700

    Luna Open-BFME5 convert HeightDieUpdate constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/HeightDieUpdateConstructor.cpp
@@ -0,0 +1,119 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class Thing
+{
+};
+
+class ModuleData
+{
+};
+
+class HeightDieUpdate
+{
+public:
+	HeightDieUpdate( Thing *, const ModuleData * );
+};
+
+__declspec(naked) HeightDieUpdate::HeightDieUpdate( Thing *, const ModuleData * )
+{
+	__asm {
+		_emit 08Bh
+		_emit 044h
+		_emit 024h
+		_emit 008h
+		_emit 056h
+		_emit 08Bh
+		_emit 0F1h
+		_emit 08Bh
+		_emit 04Ch
+		_emit 024h
+		_emit 008h
+		_emit 050h
+		_emit 051h
+		_emit 08Bh
+		_emit 0CEh
+		_emit 0E8h
+		_emit 080h
+		_emit 01Bh
+		_emit 0D8h
+		_emit 0FFh
+		_emit 0C7h
+		_emit 046h
+		_emit 00Ch
+		_emit 0D0h
+		_emit 0C9h
+		_emit 009h
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 010h
+		_emit 0A0h
+		_emit 0CBh
+		_emit 009h
+		_emit 001h
+		_emit 033h
+		_emit 0C0h
+		_emit 089h
+		_emit 046h
+		_emit 014h
+		_emit 088h
+		_emit 046h
+		_emit 020h
+		_emit 088h
+		_emit 046h
+		_emit 021h
+		_emit 0B8h
+		_emit 000h
+		_emit 000h
+		_emit 080h
+		_emit 0BFh
+		_emit 083h
+		_emit 0C9h
+		_emit 0FFh
+		_emit 089h
+		_emit 046h
+		_emit 024h
+		_emit 089h
+		_emit 046h
+		_emit 028h
+		_emit 089h
+		_emit 046h
+		_emit 02Ch
+		_emit 089h
+		_emit 04Eh
+		_emit 018h
+		_emit 089h
+		_emit 04Eh
+		_emit 01Ch
+		_emit 0C7h
+		_emit 006h
+		_emit 0DCh
+		_emit 0F6h
+		_emit 00Bh
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 00Ch
+		_emit 018h
+		_emit 0F6h
+		_emit 00Bh
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 010h
+		_emit 008h
+		_emit 0F6h
+		_emit 00Bh
+		_emit 001h
+		_emit 089h
+		_emit 04Eh
+		_emit 030h
+		_emit 08Bh
+		_emit 0C6h
+		_emit 05Eh
+		_emit 0C2h
+		_emit 008h
+		_emit 000h
+	}
+}
+
