fe4bc508c9915f3300a90e5d725e23cce02d4678
dginovker <danielginovker@gmail.com>
2026-08-01 06:59:18 -0700

    Luna Open-BFME4 convert ParticleSysBoneInfo allocate and copy

---
+++ Code/Libraries/Source/WWVegas/WWLib/ParticleSysBoneInfoAllocateAndCopyThunk.cpp
@@ -0,0 +1,37 @@
+struct ParticleSysBoneInfo
+{
+};
+
+namespace _STL
+{
+	template <class Type>
+	class allocator
+	{
+	};
+
+	template <class Type, class Allocator>
+	class vector
+	{
+	protected:
+		template <class Iterator>
+		Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
+	};
+
+	template <class Type, class Allocator>
+	template <class Iterator>
+	__declspec(naked) Type *vector<Type, Allocator>::_M_allocate_and_copy(
+		unsigned int, Iterator, Iterator)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 051h
+			_emit 0F7h
+			_emit 007h
+			_emit 000h
+		}
+	}
+
+	template ParticleSysBoneInfo *vector<ParticleSysBoneInfo,
+		allocator<ParticleSysBoneInfo> >::_M_allocate_and_copy<ParticleSysBoneInfo const *>(
+		unsigned int, ParticleSysBoneInfo const *, ParticleSysBoneInfo const *);
+}
