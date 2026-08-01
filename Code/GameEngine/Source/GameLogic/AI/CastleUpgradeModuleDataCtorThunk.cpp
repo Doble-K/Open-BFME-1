d6d135fa7cfd2ec715db84c4c2d9e000523c4ee1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 18:50:25 -0400

    Open-BFME5 convert CastleUpgradeModuleData ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/CastleUpgradeModuleDataCtorThunk.cpp
@@ -0,0 +1,103 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class CastleUpgradeModuleData
+{
+public:
+    CastleUpgradeModuleData();
+};
+
+__declspec(naked) CastleUpgradeModuleData::CastleUpgradeModuleData()
+{
+    __asm {
+        _emit 08Bh
+        _emit 0C1h
+        _emit 033h
+        _emit 0C9h
+        _emit 08Bh
+        _emit 0D0h
+        _emit 089h
+        _emit 00Ah
+        _emit 089h
+        _emit 04Ah
+        _emit 004h
+        _emit 089h
+        _emit 04Ah
+        _emit 008h
+        _emit 089h
+        _emit 04Ah
+        _emit 00Ch
+        _emit 089h
+        _emit 04Ah
+        _emit 010h
+        _emit 089h
+        _emit 04Ah
+        _emit 014h
+        _emit 08Dh
+        _emit 050h
+        _emit 018h
+        _emit 089h
+        _emit 00Ah
+        _emit 089h
+        _emit 04Ah
+        _emit 004h
+        _emit 089h
+        _emit 04Ah
+        _emit 008h
+        _emit 089h
+        _emit 04Ah
+        _emit 00Ch
+        _emit 089h
+        _emit 04Ah
+        _emit 010h
+        _emit 089h
+        _emit 04Ah
+        _emit 014h
+        _emit 089h
+        _emit 048h
+        _emit 030h
+        _emit 089h
+        _emit 048h
+        _emit 034h
+        _emit 089h
+        _emit 048h
+        _emit 038h
+        _emit 089h
+        _emit 048h
+        _emit 03Ch
+        _emit 089h
+        _emit 048h
+        _emit 040h
+        _emit 089h
+        _emit 048h
+        _emit 044h
+        _emit 089h
+        _emit 048h
+        _emit 048h
+        _emit 089h
+        _emit 048h
+        _emit 04Ch
+        _emit 089h
+        _emit 048h
+        _emit 050h
+        _emit 089h
+        _emit 048h
+        _emit 054h
+        _emit 088h
+        _emit 048h
+        _emit 058h
+        _emit 0C7h
+        _emit 040h
+        _emit 05Ch
+        _emit 0FFh
+        _emit 0FFh
+        _emit 0FFh
+        _emit 0FFh
+        _emit 089h
+        _emit 048h
+        _emit 060h
+        _emit 089h
+        _emit 048h
+        _emit 064h
+        _emit 0C3h
+    }
+}
