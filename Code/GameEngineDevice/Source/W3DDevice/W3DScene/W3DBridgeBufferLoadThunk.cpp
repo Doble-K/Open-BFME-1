52a2dad95fd718063aeacc38fd3a59484d27a35d
dginovker <danielginovker@gmail.com>
2026-08-01 08:23:02 -0700

    Luna Open-BFME4 convert W3D bridge buffer thunk

---
+++ Code/GameEngineDevice/Source/W3DDevice/W3DScene/W3DBridgeBufferLoadThunk.cpp
@@ -0,0 +1,25 @@
+class RenderObjClass
+{
+};
+
+template <class T>
+class RefMultiListIterator
+{
+};
+
+class W3DBridgeBuffer
+{
+protected:
+	void loadBridgesInVertexAndIndexBuffers(RefMultiListIterator<RenderObjClass> *);
+};
+
+__declspec(naked) void W3DBridgeBuffer::loadBridgesInVertexAndIndexBuffers(RefMultiListIterator<RenderObjClass> *)
+{
+	__asm {
+		_emit 0E9h
+		_emit 060h
+		_emit 037h
+		_emit 06Ah
+		_emit 000h
+	}
+}
