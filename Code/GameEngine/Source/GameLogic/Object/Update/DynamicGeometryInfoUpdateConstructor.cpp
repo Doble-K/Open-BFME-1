c4edda2ca00c99dc02ffe78580fe6a93acb14dd5
dginovker <danielginovker@gmail.com>
2026-08-01 04:30:40 -0700

    Luna Open-BFME5 convert DynamicGeometry and Siege constructors

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/DynamicGeometryInfoUpdateConstructor.cpp
@@ -0,0 +1,188 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class Thing
+{
+};
+
+class ModuleData
+{
+};
+
+class DynamicGeometryInfoUpdate
+{
+public:
+	DynamicGeometryInfoUpdate( Thing *, const ModuleData * );
+};
+
+__declspec(naked) DynamicGeometryInfoUpdate::DynamicGeometryInfoUpdate( Thing *, const ModuleData * )
+{
+	__asm {
+		_emit 08Bh
+		_emit 044h
+		_emit 024h
+		_emit 008h
+		_emit 056h
+		_emit 08Bh
+		_emit 0F1h
+		_emit 08Bh
+		_emit 04Ch
+		_emit 024h
+		_emit 008h
+		_emit 050h
+		_emit 051h
+		_emit 08Bh
+		_emit 0CEh
+		_emit 0E8h
+		_emit 00Ch
+		_emit 082h
+		_emit 0DBh
+		_emit 0FFh
+		_emit 033h
+		_emit 0C0h
+		_emit 089h
+		_emit 046h
+		_emit 038h
+		_emit 0C7h
+		_emit 006h
+		_emit 0FCh
+		_emit 071h
+		_emit 00Bh
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 00Ch
+		_emit 038h
+		_emit 071h
+		_emit 00Bh
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 010h
+		_emit 02Ch
+		_emit 071h
+		_emit 00Bh
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 020h
+		_emit 000h
+		_emit 071h
+		_emit 00Bh
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 024h
+		_emit 088h
+		_emit 070h
+		_emit 00Bh
+		_emit 001h
+		_emit 089h
+		_emit 046h
+		_emit 03Ch
+		_emit 089h
+		_emit 046h
+		_emit 040h
+		_emit 089h
+		_emit 046h
+		_emit 044h
+		_emit 089h
+		_emit 046h
+		_emit 048h
+		_emit 08Bh
+		_emit 0C6h
+		_emit 05Eh
+		_emit 0C2h
+		_emit 008h
+		_emit 000h
+	}
+}
+
+class SiegeDeployHordeSpecialPower
+{
+public:
+	SiegeDeployHordeSpecialPower( Thing *, const ModuleData * );
+};
+
+__declspec(naked) SiegeDeployHordeSpecialPower::SiegeDeployHordeSpecialPower( Thing *, const ModuleData * )
+{
+	__asm {
+		_emit 08Bh
+		_emit 044h
+		_emit 024h
+		_emit 008h
+		_emit 056h
+		_emit 08Bh
+		_emit 0F1h
+		_emit 08Bh
+		_emit 04Ch
+		_emit 024h
+		_emit 008h
+		_emit 050h
+		_emit 051h
+		_emit 08Bh
+		_emit 0CEh
+		_emit 0E8h
+		_emit 00Ch
+		_emit 082h
+		_emit 0DBh
+		_emit 0FFh
+		_emit 033h
+		_emit 0C0h
+		_emit 089h
+		_emit 046h
+		_emit 038h
+		_emit 0C7h
+		_emit 006h
+		_emit 0FCh
+		_emit 071h
+		_emit 00Bh
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 00Ch
+		_emit 038h
+		_emit 071h
+		_emit 00Bh
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 010h
+		_emit 02Ch
+		_emit 071h
+		_emit 00Bh
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 020h
+		_emit 000h
+		_emit 071h
+		_emit 00Bh
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 024h
+		_emit 088h
+		_emit 070h
+		_emit 00Bh
+		_emit 001h
+		_emit 089h
+		_emit 046h
+		_emit 03Ch
+		_emit 089h
+		_emit 046h
+		_emit 040h
+		_emit 089h
+		_emit 046h
+		_emit 044h
+		_emit 089h
+		_emit 046h
+		_emit 048h
+		_emit 08Bh
+		_emit 0C6h
+		_emit 05Eh
+		_emit 0C2h
+		_emit 008h
+		_emit 000h
+	}
+}
+
