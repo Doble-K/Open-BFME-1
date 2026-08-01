66509499b24780f3723721ff2e561319e2e5fd83
dginovker <danielginovker@gmail.com>
2026-08-01 08:35:29 -0700

    Luna Open-BFME4 convert integer pair tree erase thunk

--- Code/Libraries/Source/WWVegas/WWLib/RbTreeIntPairCopyThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/RbTreeIntPairCopyThunk.cpp
@@ -29,6 +29,7 @@ namespace _STL
 	class _Rb_tree
 	{
 		_Rb_tree_node<Value> *_M_copy(_Rb_tree_node<Value> *, _Rb_tree_node<Value> *);
+		void _M_erase(_Rb_tree_node<Value> *);
 	};
 
 	template <class Key, class Value, class KeyOfValue, class Compare, class Allocator>
@@ -45,4 +46,18 @@ namespace _STL
 
 	typedef pair<int const, unsigned int> IntPair;
 	template __declspec(naked) _Rb_tree_node<IntPair> *_Rb_tree<int, IntPair, _Select1st<IntPair>, less<int>, allocator<IntPair> >::_M_copy(_Rb_tree_node<IntPair> *, _Rb_tree_node<IntPair> *);
+
+	template <class Key, class Value, class KeyOfValue, class Compare, class Allocator>
+	__declspec(naked) void _Rb_tree<Key, Value, KeyOfValue, Compare, Allocator>::_M_erase(_Rb_tree_node<Value> *)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 02Dh
+			_emit 01Fh
+			_emit 006h
+			_emit 000h
+		}
+	}
+
+	template __declspec(naked) void _Rb_tree<int, IntPair, _Select1st<IntPair>, less<int>, allocator<IntPair> >::_M_erase(_Rb_tree_node<IntPair> *);
 }
