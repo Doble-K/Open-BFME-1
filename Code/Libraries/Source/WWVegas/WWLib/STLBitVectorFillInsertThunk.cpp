344d66de577e9e1f61f56adf99bfcfa2648fd7d4
dginovker <danielginovker@gmail.com>
2026-08-01 08:40:59 -0700

    Luna Open-BFME5 convert STL bit vector initialization thunk

--- Code/Libraries/Source/WWVegas/WWLib/STLBitVectorFillInsertThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/STLBitVectorFillInsertThunk.cpp
@@ -17,6 +17,9 @@ class allocator
 template<class T, class Alloc>
 class vector
 {
+protected:
+	void _M_initialize(unsigned);
+
 public:
 	void _M_fill_insert(_Bit_iter<_Bit_reference, _Bit_reference *> , unsigned, bool);
 };
@@ -36,3 +39,20 @@ __declspec(naked) void vector<T, Alloc>::_M_fill_insert(_Bit_iter<_Bit_reference
 
 typedef _STL::vector<bool, _STL::allocator<bool> > BitVector;
 template __declspec(naked) void BitVector::_M_fill_insert(_STL::_Bit_iter<_STL::_Bit_reference, _STL::_Bit_reference *> , unsigned, bool);
+
+namespace _STL
+{
+template<class T, class Alloc>
+__declspec(naked) void vector<T, Alloc>::_M_initialize(unsigned)
+{
+	__asm {
+		_emit 0E9h
+		_emit 01Ah
+		_emit 0BAh
+		_emit 04Eh
+		_emit 000h
+	}
+}
+}
+
+template __declspec(naked) void BitVector::_M_initialize(unsigned);
