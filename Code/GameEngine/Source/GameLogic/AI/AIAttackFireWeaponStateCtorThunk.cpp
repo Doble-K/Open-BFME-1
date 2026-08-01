90461dd26c68d32e2a8460d044a7037c706590cb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 17:28:49 -0400

    Open-BFME5 convert AIAttackFireWeaponState ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/AIAttackFireWeaponStateCtorThunk.cpp
@@ -0,0 +1,73 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class StateMachine;
+class NotifyWeaponFiredInterface;
+
+class AIAttackFireWeaponState
+{
+public:
+    AIAttackFireWeaponState(StateMachine *, NotifyWeaponFiredInterface *);
+};
+
+__declspec(naked) AIAttackFireWeaponState::AIAttackFireWeaponState(StateMachine *, NotifyWeaponFiredInterface *)
+{
+    __asm {
+        _emit 051h
+        _emit 056h
+        _emit 051h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 089h
+        _emit 064h
+        _emit 024h
+        _emit 008h
+        _emit 08Bh
+        _emit 0CCh
+        _emit 068h
+        _emit 014h
+        _emit 07Eh
+        _emit 009h
+        _emit 001h
+        _emit 0E8h
+        _emit 0EBh
+        _emit 078h
+        _emit 071h
+        _emit 000h
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 050h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 0D1h
+        _emit 022h
+        _emit 0E9h
+        _emit 0FFh
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 010h
+        _emit 0C7h
+        _emit 006h
+        _emit 0C0h
+        _emit 07Dh
+        _emit 009h
+        _emit 001h
+        _emit 089h
+        _emit 04Eh
+        _emit 024h
+        _emit 0C6h
+        _emit 046h
+        _emit 028h
+        _emit 000h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 059h
+        _emit 0C2h
+        _emit 008h
+        _emit 000h
+    }
+}
