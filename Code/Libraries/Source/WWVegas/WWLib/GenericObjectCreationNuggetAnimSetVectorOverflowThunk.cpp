78d7a33770d1bf3cf97e8dcb92b20585c936a235
dginovker <danielginovker@gmail.com>
2026-08-01 06:23:07 -0700

    Luna Open-BFME4 convert AnimSet vector overflow

---
+++ Code/Libraries/Source/WWVegas/WWLib/GenericObjectCreationNuggetAnimSetVectorOverflowThunk.cpp
@@ -0,0 +1,37 @@
+struct GenericObjectCreationNugget
+{
+	struct AnimSet
+	{
+	};
+};
+
+namespace _STL
+{
+	struct __false_type
+	{
+	};
+
+	template <class Type>
+	class allocator
+	{
+	};
+
+	template <class Type, class Allocator>
+	class vector
+	{
+	protected:
+		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+	};
+
+	__declspec(naked) void vector<GenericObjectCreationNugget::AnimSet, allocator<GenericObjectCreationNugget::AnimSet> >::_M_insert_overflow(
+		GenericObjectCreationNugget::AnimSet *, const GenericObjectCreationNugget::AnimSet &, const __false_type &, unsigned int, bool)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 06Ch
+			_emit 081h
+			_emit 074h
+			_emit 000h
+		}
+	}
+}
