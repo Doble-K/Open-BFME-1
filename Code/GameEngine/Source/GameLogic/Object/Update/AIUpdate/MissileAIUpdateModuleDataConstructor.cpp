123af9489adc1542be32593c3581993ae5f4291b
dginovker <danielginovker@gmail.com>
2026-08-01 04:19:10 -0700

    Luna Open-BFME5 convert MissileAIUpdateModuleData constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate/MissileAIUpdateModuleDataConstructor.cpp
@@ -0,0 +1,59 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class MissileAIUpdateModuleData
+{
+public:
+	MissileAIUpdateModuleData();
+};
+
+__declspec(naked) MissileAIUpdateModuleData::MissileAIUpdateModuleData()
+{
+	__asm {
+		_emit 056h
+		_emit 08Bh
+		_emit 0F1h
+		_emit 0E8h
+		_emit 06Eh
+		_emit 08Eh
+		_emit 0E2h
+		_emit 0FFh
+		_emit 033h
+		_emit 0C0h
+		_emit 089h
+		_emit 046h
+		_emit 064h
+		_emit 089h
+		_emit 046h
+		_emit 068h
+		_emit 088h
+		_emit 046h
+		_emit 06Ch
+		_emit 089h
+		_emit 046h
+		_emit 07Ch
+		_emit 089h
+		_emit 046h
+		_emit 074h
+		_emit 089h
+		_emit 046h
+		_emit 078h
+		_emit 0C7h
+		_emit 006h
+		_emit 098h
+		_emit 085h
+		_emit 00Ah
+		_emit 001h
+		_emit 0C7h
+		_emit 046h
+		_emit 070h
+		_emit 0FFh
+		_emit 0FFh
+		_emit 0FFh
+		_emit 0FFh
+		_emit 08Bh
+		_emit 0C6h
+		_emit 05Eh
+		_emit 0C3h
+	}
+}
+
