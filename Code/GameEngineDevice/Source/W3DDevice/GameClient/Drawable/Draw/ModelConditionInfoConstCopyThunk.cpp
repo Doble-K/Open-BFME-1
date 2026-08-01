a1f61bb365d03e11cae7ed40a18d534700a936ac
dginovker <danielginovker@gmail.com>
2026-08-01 05:32:13 -0700

    Luna Open-BFME5 convert ModelConditionInfo const copy thunk

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Drawable/Draw/ModelConditionInfoConstCopyThunk.cpp
@@ -0,0 +1,26 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class ModelConditionInfo
+{
+public:
+	struct HideShowSubObjInfo;
+};
+
+namespace _STL
+{
+	struct random_access_iterator_tag;
+
+	template <class In, class Out, class Distance>
+	__declspec(naked) Out __copy(In, In, Out, const random_access_iterator_tag &, Distance *)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 02Eh
+			_emit 057h
+			_emit 037h
+			_emit 000h
+		}
+	}
+
+	template ModelConditionInfo::HideShowSubObjInfo *__copy<const ModelConditionInfo::HideShowSubObjInfo *, ModelConditionInfo::HideShowSubObjInfo *, int>(const ModelConditionInfo::HideShowSubObjInfo *, const ModelConditionInfo::HideShowSubObjInfo *, ModelConditionInfo::HideShowSubObjInfo *, const random_access_iterator_tag &, int *);
+}
