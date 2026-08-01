5e50590764d366aa34d2fdaa64f78e525c8ea3bd
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 18:08:13 -0400

    Open-BFME5 convert SiegeAIUpdate ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/SiegeAIUpdateCtorThunk.cpp
@@ -0,0 +1,76 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class SiegeAIUpdate
+{
+public:
+    SiegeAIUpdate(Thing *, const ModuleData *);
+};
+
+__declspec(naked) SiegeAIUpdate::SiegeAIUpdate(Thing *, const ModuleData *)
+{
+    __asm {
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 008h
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 008h
+        _emit 050h
+        _emit 051h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 05Fh
+        _emit 03Fh
+        _emit 0D6h
+        _emit 0FFh
+        _emit 0C7h
+        _emit 006h
+        _emit 090h
+        _emit 08Bh
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 00Ch
+        _emit 0C8h
+        _emit 08Ah
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 010h
+        _emit 0B8h
+        _emit 08Ah
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 020h
+        _emit 0B4h
+        _emit 08Ah
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 024h
+        _emit 098h
+        _emit 08Ah
+        _emit 00Ch
+        _emit 001h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C2h
+        _emit 008h
+        _emit 000h
+    }
+}
