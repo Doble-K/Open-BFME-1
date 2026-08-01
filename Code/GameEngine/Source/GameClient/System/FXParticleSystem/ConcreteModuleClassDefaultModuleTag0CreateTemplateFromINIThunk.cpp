d6efd6881c05d4ce416bfe0e216d69c31a9c6fcd
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 17:13:33 -0400

    Open-BFME5 convert default module tag 00 createTemplate INI

---
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/ConcreteModuleClassDefaultModuleTag0CreateTemplateFromINIThunk.cpp
@@ -0,0 +1,162 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class INI;
+
+namespace FXParticleSystem
+{
+
+template<int Category>
+class DefaultModuleTag
+{
+};
+
+template<int Category>
+class DefaultModuleTemplate
+{
+};
+
+template<class Tag>
+class ConcreteModuleClass
+{
+public:
+    virtual DefaultModuleTemplate<1> *createTemplate(INI *) const;
+};
+
+template<class Tag>
+__declspec(naked) DefaultModuleTemplate<1> *ConcreteModuleClass<Tag>::createTemplate(INI *) const
+{
+    __asm {
+        _emit 06Ah
+        _emit 0FFh
+        _emit 068h
+        _emit 0B3h
+        _emit 0B6h
+        _emit 003h
+        _emit 001h
+        _emit 064h
+        _emit 0A1h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 050h
+        _emit 064h
+        _emit 089h
+        _emit 025h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 051h
+        _emit 056h
+        _emit 068h
+        _emit 08Ch
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 0E8h
+        _emit 00Fh
+        _emit 0DEh
+        _emit 029h
+        _emit 000h
+        _emit 08Bh
+        _emit 0F0h
+        _emit 083h
+        _emit 0C4h
+        _emit 004h
+        _emit 089h
+        _emit 074h
+        _emit 024h
+        _emit 004h
+        _emit 085h
+        _emit 0F6h
+        _emit 0C7h
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 074h
+        _emit 01Dh
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 058h
+        _emit 02Eh
+        _emit 0A3h
+        _emit 0FFh
+        _emit 0C7h
+        _emit 006h
+        _emit 06Ch
+        _emit 00Fh
+        _emit 011h
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 004h
+        _emit 068h
+        _emit 00Fh
+        _emit 011h
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 008h
+        _emit 054h
+        _emit 00Fh
+        _emit 011h
+        _emit 001h
+        _emit 0EBh
+        _emit 002h
+        _emit 033h
+        _emit 0F6h
+        _emit 089h
+        _emit 074h
+        _emit 024h
+        _emit 004h
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 018h
+        _emit 050h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0C7h
+        _emit 044h
+        _emit 024h
+        _emit 014h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 0E8h
+        _emit 020h
+        _emit 029h
+        _emit 0A6h
+        _emit 0FFh
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 008h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 064h
+        _emit 089h
+        _emit 00Dh
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 083h
+        _emit 0C4h
+        _emit 010h
+        _emit 0C2h
+        _emit 004h
+        _emit 000h
+    }
+}
+
+template class ConcreteModuleClass<DefaultModuleTag<1> >;
+}
