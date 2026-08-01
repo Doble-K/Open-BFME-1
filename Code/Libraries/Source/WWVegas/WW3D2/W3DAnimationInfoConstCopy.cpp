8f666286785db59eadb6d5b459f8f6d73d77c7b4
dginovker <danielginovker@gmail.com>
2026-08-01 05:34:13 -0700

    Luna Open-BFME5 convert W3DAnimationInfo const copy thunk

---
+++ Code/Libraries/Source/WWVegas/WW3D2/W3DAnimationInfoConstCopy.cpp
@@ -0,0 +1,22 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class W3DAnimationInfo;
+
+namespace _STL
+{
+	struct random_access_iterator_tag;
+
+	template <class In, class Out, class Distance>
+	__declspec(naked) Out __copy(In, In, Out, const random_access_iterator_tag &, Distance *)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 00Ch
+			_emit 020h
+			_emit 037h
+			_emit 000h
+		}
+	}
+
+	template W3DAnimationInfo *__copy<const W3DAnimationInfo *, W3DAnimationInfo *, int>(const W3DAnimationInfo *, const W3DAnimationInfo *, W3DAnimationInfo *, const random_access_iterator_tag &, int *);
+}
