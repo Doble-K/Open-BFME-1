de34d4df3fa1b6901649b7ab23da1740689fc4fd
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 19:30:30 -0400

    Open-BFME5 convert ProductionSpeedBonusModuleData ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/ProductionSpeedBonusModuleDataCtorThunk.cpp
@@ -0,0 +1,120 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class ProductionSpeedBonusModuleData
+{
+public:
+    ProductionSpeedBonusModuleData();
+};
+
+__declspec(naked) ProductionSpeedBonusModuleData::ProductionSpeedBonusModuleData()
+{
+    __asm {
+        _emit 6Ah
+        _emit 0FFh
+        _emit 68h
+        _emit 0E6h
+        _emit 0F9h
+        _emit 00h
+        _emit 01h
+        _emit 64h
+        _emit 0A1h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 50h
+        _emit 64h
+        _emit 89h
+        _emit 25h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 51h
+        _emit 56h
+        _emit 8Bh
+        _emit 0F1h
+        _emit 89h
+        _emit 74h
+        _emit 24h
+        _emit 04h
+        _emit 0E8h
+        _emit 92h
+        _emit 15h
+        _emit 0DBh
+        _emit 0FFh
+        _emit 33h
+        _emit 0C0h
+        _emit 8Dh
+        _emit 8Eh
+        _emit 18h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 0C7h
+        _emit 06h
+        _emit 30h
+        _emit 69h
+        _emit 0Bh
+        _emit 01h
+        _emit 89h
+        _emit 01h
+        _emit 89h
+        _emit 41h
+        _emit 04h
+        _emit 89h
+        _emit 44h
+        _emit 24h
+        _emit 10h
+        _emit 89h
+        _emit 41h
+        _emit 08h
+        _emit 89h
+        _emit 86h
+        _emit 10h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 89h
+        _emit 86h
+        _emit 14h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 8Bh
+        _emit 41h
+        _emit 04h
+        _emit 8Bh
+        _emit 11h
+        _emit 50h
+        _emit 52h
+        _emit 0C6h
+        _emit 44h
+        _emit 24h
+        _emit 18h
+        _emit 01h
+        _emit 0E8h
+        _emit 2Eh
+        _emit 05h
+        _emit 0DCh
+        _emit 0FFh
+        _emit 8Bh
+        _emit 4Ch
+        _emit 24h
+        _emit 08h
+        _emit 8Bh
+        _emit 0C6h
+        _emit 5Eh
+        _emit 64h
+        _emit 89h
+        _emit 0Dh
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 83h
+        _emit 0C4h
+        _emit 10h
+        _emit 0C3h
+    }
+}
