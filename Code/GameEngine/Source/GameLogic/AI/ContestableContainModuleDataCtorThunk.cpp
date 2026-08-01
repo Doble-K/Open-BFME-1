71c384f50577f5cff7a1541919870b2fe87b4f07
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 18:20:00 -0400

    Open-BFME5 convert ContestableContainModuleData ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/ContestableContainModuleDataCtorThunk.cpp
@@ -0,0 +1,99 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class ContestableContainModuleData
+{
+public:
+    ContestableContainModuleData();
+};
+
+__declspec(naked) ContestableContainModuleData::ContestableContainModuleData()
+{
+    __asm {
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 0E8h
+        _emit 00Dh
+        _emit 0AAh
+        _emit 0DCh
+        _emit 0FFh
+        _emit 033h
+        _emit 0C0h
+        _emit 089h
+        _emit 086h
+        _emit 07Ch
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 080h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 0C7h
+        _emit 006h
+        _emit 060h
+        _emit 0FEh
+        _emit 00Ah
+        _emit 001h
+        _emit 089h
+        _emit 086h
+        _emit 084h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 088h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 08Ch
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 09Ch
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 0A0h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 0A4h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 090h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 094h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 098h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C3h
+    }
+}
