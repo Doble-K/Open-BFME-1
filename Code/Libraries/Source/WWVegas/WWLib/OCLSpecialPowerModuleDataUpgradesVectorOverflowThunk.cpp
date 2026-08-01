58439dece851f0d87c15a2be29268d448a1d462b
dginovker <danielginovker@gmail.com>
2026-08-01 06:39:22 -0700

    Luna Open-BFME4 convert OCL Upgrades vector overflow

---
+++ Code/Libraries/Source/WWVegas/WWLib/OCLSpecialPowerModuleDataUpgradesVectorOverflowThunk.cpp
@@ -0,0 +1,37 @@
+struct OCLSpecialPowerModuleData
+{
+	struct Upgrades
+	{
+	};
+};
+
+namespace _STL
+{
+	struct __false_type
+	{
+	};
+
+	template <class Type>
+	class allocator
+	{
+	};
+
+	template <class Type, class Allocator>
+	class vector
+	{
+	protected:
+		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+	};
+
+	__declspec(naked) void vector<OCLSpecialPowerModuleData::Upgrades, allocator<OCLSpecialPowerModuleData::Upgrades> >::_M_insert_overflow(
+		OCLSpecialPowerModuleData::Upgrades *, const OCLSpecialPowerModuleData::Upgrades &, const __false_type &, unsigned int, bool)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 09Ch
+			_emit 09Eh
+			_emit 022h
+			_emit 000h
+		}
+	}
+}
