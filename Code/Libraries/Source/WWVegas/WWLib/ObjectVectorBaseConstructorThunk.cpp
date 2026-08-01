637df4aea8931003fbdd7dbe836aa223429b8e01
dginovker <danielginovker@gmail.com>
2026-08-01 06:50:29 -0700

    Luna Open-BFME4 convert Object vector base constructor

---
+++ Code/Libraries/Source/WWVegas/WWLib/ObjectVectorBaseConstructorThunk.cpp
@@ -0,0 +1,30 @@
+class Object
+{
+};
+
+namespace _STL
+{
+	template <class Type>
+	class allocator
+	{
+	};
+
+	template <class Type, class Allocator>
+	class _Vector_base
+	{
+	public:
+		_Vector_base(unsigned int, const Allocator &);
+	};
+
+	__declspec(naked) _Vector_base<Object *, allocator<Object *> >::_Vector_base(
+		unsigned int, const allocator<Object *> &)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 0D4h
+			_emit 033h
+			_emit 013h
+			_emit 000h
+		}
+	}
+}
