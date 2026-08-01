6ad9e221e6725a597c656ab2b18c773b1222b9b7
dginovker <danielginovker@gmail.com>
2026-08-01 04:27:26 -0700

    Luna Open-BFME5 convert TensileFormationUpdateModuleData constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/TensileFormationUpdateModuleDataConstructor.cpp
@@ -0,0 +1,43 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class TensileFormationUpdateModuleData
+{
+public:
+	TensileFormationUpdateModuleData();
+};
+
+__declspec(naked) TensileFormationUpdateModuleData::TensileFormationUpdateModuleData()
+{
+	__asm {
+		_emit 056h
+		_emit 08Bh
+		_emit 0F1h
+		_emit 0E8h
+		_emit 083h
+		_emit 024h
+		_emit 0D9h
+		_emit 0FFh
+		_emit 033h
+		_emit 0C0h
+		_emit 089h
+		_emit 046h
+		_emit 064h
+		_emit 089h
+		_emit 046h
+		_emit 068h
+		_emit 089h
+		_emit 046h
+		_emit 06Ch
+		_emit 0C7h
+		_emit 006h
+		_emit 000h
+		_emit 068h
+		_emit 00Ch
+		_emit 001h
+		_emit 08Bh
+		_emit 0C6h
+		_emit 05Eh
+		_emit 0C3h
+	}
+}
+
