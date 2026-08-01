cdaacdca569d9fbf691823f2733e3fea0eb517f3
dginovker <danielginovker@gmail.com>
2026-08-01 06:07:41 -0700

    Luna Open-BFME5 convert GameResultsQueue constructor thunk

---
+++ Code/GameEngine/Source/GameNetwork/GameSpy/Thread/GameResultsQueueCtorThunk.cpp
@@ -0,0 +1,18 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class GameResultsQueue
+{
+public:
+    GameResultsQueue();
+};
+
+__declspec(naked) GameResultsQueue::GameResultsQueue()
+{
+    __asm {
+        _emit 0E9h
+        _emit 07Eh
+        _emit 005h
+        _emit 063h
+        _emit 000h
+    }
+}
