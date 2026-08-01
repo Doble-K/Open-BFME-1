85675d3698bf52d9a0da62be8503c4310297149c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 12:50:40 -0400

    Open-BFME5 convert Cftp constructor thunk

---
+++ Code/GameEngine/Source/GameNetwork/CftpConstructorThunk.cpp
@@ -0,0 +1,33 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+#include "PreRTS.h"
+
+class Cftp
+{
+public:
+    Cftp();
+};
+
+__declspec(naked) Cftp::Cftp()
+{
+    __asm {
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 0C7h
+        _emit 006h
+        _emit 0CCh
+        _emit 02Eh
+        _emit 013h
+        _emit 001h
+        _emit 0E8h
+        _emit 0E2h
+        _emit 0F8h
+        _emit 0FFh
+        _emit 0FFh
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C3h
+    }
+}
