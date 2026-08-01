c8fa80f52faf63fedad4cfe15c36860318c72b38
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 17:46:31 -0400

    Open-BFME5 convert AIWanderState ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/AIWanderStateCtorThunk.cpp
@@ -0,0 +1,97 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class StateMachine;
+
+class AIWanderState
+{
+public:
+    AIWanderState(StateMachine *);
+};
+
+__declspec(naked) AIWanderState::AIWanderState(StateMachine *)
+{
+    __asm {
+        _emit 051h
+        _emit 056h
+        _emit 051h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 089h
+        _emit 064h
+        _emit 024h
+        _emit 008h
+        _emit 08Bh
+        _emit 0CCh
+        _emit 068h
+        _emit 008h
+        _emit 0A8h
+        _emit 009h
+        _emit 001h
+        _emit 0E8h
+        _emit 0ABh
+        _emit 090h
+        _emit 070h
+        _emit 000h
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 050h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 061h
+        _emit 026h
+        _emit 0EBh
+        _emit 0FFh
+        _emit 033h
+        _emit 0C0h
+        _emit 089h
+        _emit 046h
+        _emit 050h
+        _emit 089h
+        _emit 046h
+        _emit 054h
+        _emit 089h
+        _emit 046h
+        _emit 058h
+        _emit 089h
+        _emit 046h
+        _emit 05Ch
+        _emit 089h
+        _emit 046h
+        _emit 060h
+        _emit 089h
+        _emit 046h
+        _emit 064h
+        _emit 088h
+        _emit 046h
+        _emit 068h
+        _emit 088h
+        _emit 046h
+        _emit 069h
+        _emit 089h
+        _emit 046h
+        _emit 06Ch
+        _emit 089h
+        _emit 046h
+        _emit 070h
+        _emit 0C6h
+        _emit 046h
+        _emit 06Ah
+        _emit 001h
+        _emit 0C7h
+        _emit 006h
+        _emit 0A8h
+        _emit 0A8h
+        _emit 009h
+        _emit 001h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 059h
+        _emit 0C2h
+        _emit 004h
+        _emit 000h
+    }
+}
