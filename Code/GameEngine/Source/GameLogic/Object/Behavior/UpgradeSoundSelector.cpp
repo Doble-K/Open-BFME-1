c97f20bdca9975188af082cf64e97f200ffa551c
dginovker <danielginovker@gmail.com>
2026-08-01 02:58:57 -0700

    Luna Open-BFME4 convert upgrade sound selector ctor

---
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/UpgradeSoundSelector.cpp
@@ -0,0 +1,34 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+#include "PreRTS.h"
+
+class UpgradeSoundSelectorClientBehaviorModuleData
+{
+public:
+    UpgradeSoundSelectorClientBehaviorModuleData();
+};
+
+__declspec(naked) UpgradeSoundSelectorClientBehaviorModuleData::UpgradeSoundSelectorClientBehaviorModuleData()
+{
+    __asm {
+        _emit 08Bh
+        _emit 0C1h
+        _emit 033h
+        _emit 0C9h
+        _emit 0C7h
+        _emit 000h
+        _emit 038h
+        _emit 05Ah
+        _emit 011h
+        _emit 001h
+        _emit 089h
+        _emit 048h
+        _emit 008h
+        _emit 089h
+        _emit 048h
+        _emit 00Ch
+        _emit 089h
+        _emit 048h
+        _emit 010h
+        _emit 0C3h
+    }
+}
