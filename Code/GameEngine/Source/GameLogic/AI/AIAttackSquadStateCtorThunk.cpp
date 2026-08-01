b7b9d82cb43cf5510a3d5f4e2070ed55d310f0a1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 17:27:32 -0400

    Open-BFME5 convert AIAttackSquadState ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/AIAttackSquadStateCtorThunk.cpp
@@ -0,0 +1,70 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class StateMachine;
+class AttackExitConditionsInterface;
+
+class AIAttackSquadState
+{
+public:
+    AIAttackSquadState(StateMachine *, AttackExitConditionsInterface *);
+};
+
+__declspec(naked) AIAttackSquadState::AIAttackSquadState(StateMachine *, AttackExitConditionsInterface *)
+{
+    __asm {
+        _emit 051h
+        _emit 056h
+        _emit 051h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 089h
+        _emit 064h
+        _emit 024h
+        _emit 008h
+        _emit 08Bh
+        _emit 0CCh
+        _emit 068h
+        _emit 084h
+        _emit 07Eh
+        _emit 009h
+        _emit 001h
+        _emit 0E8h
+        _emit 07Bh
+        _emit 078h
+        _emit 071h
+        _emit 000h
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 050h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 061h
+        _emit 022h
+        _emit 0E9h
+        _emit 0FFh
+        _emit 033h
+        _emit 0C0h
+        _emit 089h
+        _emit 046h
+        _emit 024h
+        _emit 088h
+        _emit 046h
+        _emit 028h
+        _emit 0C7h
+        _emit 006h
+        _emit 030h
+        _emit 07Eh
+        _emit 009h
+        _emit 001h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 059h
+        _emit 0C2h
+        _emit 008h
+        _emit 000h
+    }
+}
