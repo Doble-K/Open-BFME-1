3fce4c3d31161098f0c13a710e0a58e9f8d6cdad
dginovker <danielginovker@gmail.com>
2026-08-01 08:02:45 -0700

    Luna Open-BFME5 convert transition object parser thunk

---
+++ Code/GameEngine/Source/Common/RTS/TransitionDamageFXModuleDataParseObjectCreationListThunk.cpp
@@ -0,0 +1,20 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /Code/GameEngine/Include /Code/GameEngine/Include/Precompiled /Code/Libraries/Source/WWVegas/WWLib
+
+class INI;
+
+class TransitionDamageFXModuleData
+{
+public:
+    __declspec(dllexport) static void parseObjectCreationList(INI *, void *, void *, const void *);
+};
+
+__declspec(naked) void TransitionDamageFXModuleData::parseObjectCreationList(INI *, void *, void *, const void *)
+{
+    __asm {
+        _emit 0E9h
+        _emit 070h
+        _emit 0DDh
+        _emit 024h
+        _emit 000h
+    }
+}
