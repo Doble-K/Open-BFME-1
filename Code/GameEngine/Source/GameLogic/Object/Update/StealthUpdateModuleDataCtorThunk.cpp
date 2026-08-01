33f502a7fe1f1b25e77df345cb713ca1efd98cc0
dginovker <danielginovker@gmail.com>
2026-08-01 06:17:09 -0700

    Luna Open-BFME5 convert Stealth module constructor thunk

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/StealthUpdateModuleDataCtorThunk.cpp
@@ -0,0 +1,18 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class StealthUpdateModuleData
+{
+public:
+    StealthUpdateModuleData();
+};
+
+__declspec(naked) StealthUpdateModuleData::StealthUpdateModuleData()
+{
+    __asm {
+        _emit 0E9h
+        _emit 078h
+        _emit 0F2h
+        _emit 00Dh
+        _emit 000h
+    }
+}
