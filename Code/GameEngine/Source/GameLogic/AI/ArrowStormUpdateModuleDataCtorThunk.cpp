d874b337f51535b34eb93ccdcfcd6fc98ddd4a7d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 18:29:43 -0400

    Open-BFME5 convert ArrowStormUpdateModuleData ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/ArrowStormUpdateModuleDataCtorThunk.cpp
@@ -0,0 +1,74 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class ArrowStormUpdateModuleData
+{
+public:
+    ArrowStormUpdateModuleData();
+};
+
+__declspec(naked) ArrowStormUpdateModuleData::ArrowStormUpdateModuleData()
+{
+    __asm {
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 0E8h
+        _emit 02Bh
+        _emit 07Fh
+        _emit 0DEh
+        _emit 0FFh
+        _emit 033h
+        _emit 0C9h
+        _emit 0B8h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 0C7h
+        _emit 006h
+        _emit 068h
+        _emit 036h
+        _emit 00Bh
+        _emit 001h
+        _emit 089h
+        _emit 08Eh
+        _emit 054h
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 05Ch
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 060h
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 064h
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 08Eh
+        _emit 058h
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 088h
+        _emit 08Eh
+        _emit 068h
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C3h
+    }
+}
