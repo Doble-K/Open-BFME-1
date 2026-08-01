33c4764d2750e0e88c53f1a9b483fab075e04e64
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 15:23:56 -0400

    Open-BFME5 convert GameSpy locale setter

--- Code/GameEngine/Source/Common/UserPreferences.cpp
+++ Code/GameEngine/Source/Common/UserPreferences.cpp
@@ -903,9 +903,46 @@ __declspec(naked) Int GameSpyMiscPreferences::getLocale( void )
 	}
 }
 
-void GameSpyMiscPreferences::setLocale( Int val )
+__declspec(naked) void GameSpyMiscPreferences::setLocale( Int )
 {
-	setInt("Locale", val);
+	__asm {
+		_emit 08Bh
+		_emit 044h
+		_emit 024h
+		_emit 004h
+		_emit 056h
+		_emit 050h
+		_emit 051h
+		_emit 08Bh
+		_emit 0F1h
+		_emit 089h
+		_emit 064h
+		_emit 024h
+		_emit 010h
+		_emit 08Bh
+		_emit 0CCh
+		_emit 068h
+		_emit 06Ch
+		_emit 00Fh
+		_emit 008h
+		_emit 001h
+		_emit 0E8h
+		_emit 0D7h
+		_emit 0F1h
+		_emit 07Dh
+		_emit 000h
+		_emit 08Bh
+		_emit 0CEh
+		_emit 0E8h
+		_emit 04Dh
+		_emit 001h
+		_emit 0F6h
+		_emit 0FFh
+		_emit 05Eh
+		_emit 0C2h
+		_emit 004h
+		_emit 000h
+	}
 }
 
 AsciiString GameSpyMiscPreferences::getCachedStats( void )
