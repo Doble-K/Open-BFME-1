7430b62f916df608ec9ca7853d43f175e2b5cbc4
dginovker <danielginovker@gmail.com>
2026-08-01 05:57:22 -0700

    Luna Open-BFME4 convert HashTemplate Definition entry destructor alias

---
+++ Code/Libraries/Source/WWVegas/WWLib/HashTemplateEntryDestructorThunk.cpp
@@ -0,0 +1,25 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class StringClass;
+
+template <class KeyType, class ValueType>
+class HashTemplateClass
+{
+public:
+	class Entry
+	{
+	public:
+		~Entry();
+	};
+};
+
+__declspec(naked) HashTemplateClass<StringClass, int>::Entry::~Entry()
+{
+	__asm {
+		_emit 0E9h
+		_emit 05Ah
+		_emit 026h
+		_emit 05Ch
+		_emit 000h
+	}
+}
