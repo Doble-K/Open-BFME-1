619655e1f55e36168ca7af20d626d2ebeb91285d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 17:25:11 -0400

    Open-BFME5 convert AIEnterState ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/AIEnterStateCtorThunk.cpp
@@ -0,0 +1,68 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class StateMachine;
+
+class AIEnterState
+{
+public:
+    AIEnterState(StateMachine *);
+};
+
+__declspec(naked) AIEnterState::AIEnterState(StateMachine *)
+{
+    __asm {
+        _emit 051h
+        _emit 056h
+        _emit 051h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 089h
+        _emit 064h
+        _emit 024h
+        _emit 008h
+        _emit 08Bh
+        _emit 0CCh
+        _emit 068h
+        _emit 028h
+        _emit 0ACh
+        _emit 009h
+        _emit 001h
+        _emit 0E8h
+        _emit 0BBh
+        _emit 08Bh
+        _emit 070h
+        _emit 000h
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 050h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 071h
+        _emit 021h
+        _emit 0EBh
+        _emit 0FFh
+        _emit 0C7h
+        _emit 006h
+        _emit 0D0h
+        _emit 0ABh
+        _emit 009h
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 050h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 059h
+        _emit 0C2h
+        _emit 004h
+        _emit 000h
+    }
+}
