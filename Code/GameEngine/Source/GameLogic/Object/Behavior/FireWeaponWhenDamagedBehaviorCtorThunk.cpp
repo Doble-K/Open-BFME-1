61d874bb99b5d71570d88810c8156682ba5d6f0c
dginovker <danielginovker@gmail.com>
2026-08-01 06:28:47 -0700

    Luna Open-BFME5 convert FireWeapon damaged constructor thunk

---
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/FireWeaponWhenDamagedBehaviorCtorThunk.cpp
@@ -0,0 +1,21 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class Thing;
+class ModuleData;
+
+class FireWeaponWhenDamagedBehavior
+{
+public:
+    FireWeaponWhenDamagedBehavior(Thing *, const ModuleData *);
+};
+
+__declspec(naked) FireWeaponWhenDamagedBehavior::FireWeaponWhenDamagedBehavior(Thing *, const ModuleData *)
+{
+    __asm {
+        _emit 0E9h
+        _emit 052h
+        _emit 012h
+        _emit 01Dh
+        _emit 000h
+    }
+}
