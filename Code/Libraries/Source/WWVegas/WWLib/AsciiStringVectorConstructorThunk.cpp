68770d6e30558deb72178c6a518b6b86b5bc5a4a
dginovker <danielginovker@gmail.com>
2026-08-01 06:53:03 -0700

    Luna Open-BFME4 convert AsciiString vector constructor

---
+++ Code/Libraries/Source/WWVegas/WWLib/AsciiStringVectorConstructorThunk.cpp
@@ -0,0 +1,30 @@
+class AsciiString
+{
+};
+
+namespace _STL
+{
+	template <class Type>
+	class allocator
+	{
+	};
+
+	template <class Type, class Allocator>
+	class vector
+	{
+	public:
+		vector(const Allocator &);
+	};
+
+	__declspec(naked) vector<AsciiString, allocator<AsciiString> >::vector(
+		const allocator<AsciiString> &)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 02Eh
+			_emit 0B7h
+			_emit 04Bh
+			_emit 000h
+		}
+	}
+}
