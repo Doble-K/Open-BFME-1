bc4b655b428f7887e5bb7adfc9a116f2254d23ac
dginovker <danielginovker@gmail.com>
2026-08-01 05:50:16 -0700

    Luna Open-BFME4 convert WindowVideo destructor thunk

---
+++ Code/GameEngine/Source/GameClient/WindowVideoDestructorThunk.cpp
@@ -0,0 +1,18 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class WindowVideo
+{
+public:
+	~WindowVideo();
+};
+
+__declspec(naked) WindowVideo::~WindowVideo()
+{
+	__asm {
+		_emit 0E9h
+		_emit 0B4h
+		_emit 02Dh
+		_emit 047h
+		_emit 000h
+	}
+}
