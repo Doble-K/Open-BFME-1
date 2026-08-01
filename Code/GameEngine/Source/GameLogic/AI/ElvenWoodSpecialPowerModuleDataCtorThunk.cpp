b00afed9ac762386a95177e5292c2c17a9388866
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 19:09:37 -0400

    Open-BFME5 convert ElvenWoodSpecialPowerModuleData ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/ElvenWoodSpecialPowerModuleDataCtorThunk.cpp
@@ -0,0 +1,85 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class ElvenWoodSpecialPowerModuleData
+{
+public:
+    ElvenWoodSpecialPowerModuleData();
+};
+
+__declspec(naked) ElvenWoodSpecialPowerModuleData::ElvenWoodSpecialPowerModuleData()
+{
+    __asm {
+        _emit 56h
+        _emit 8Bh
+        _emit 0F1h
+        _emit 0E8h
+        _emit 0FCh
+        _emit 91h
+        _emit 0DBh
+        _emit 0FFh
+        _emit 33h
+        _emit 0C0h
+        _emit 0C7h
+        _emit 06h
+        _emit 38h
+        _emit 4Ch
+        _emit 0Bh
+        _emit 01h
+        _emit 89h
+        _emit 86h
+        _emit 10h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 89h
+        _emit 86h
+        _emit 14h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 89h
+        _emit 86h
+        _emit 18h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 89h
+        _emit 86h
+        _emit 1Ch
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 89h
+        _emit 86h
+        _emit 20h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 89h
+        _emit 86h
+        _emit 28h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 89h
+        _emit 86h
+        _emit 2Ch
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 0C7h
+        _emit 86h
+        _emit 24h
+        _emit 02h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 00h
+        _emit 20h
+        _emit 41h
+        _emit 8Bh
+        _emit 0C6h
+        _emit 5Eh
+        _emit 0C3h
+    }
+}
