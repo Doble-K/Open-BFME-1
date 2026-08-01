c8b568b2052b2faeac70ed81279447f583800f13
dginovker <danielginovker@gmail.com>
2026-08-01 06:52:16 -0700

    Luna Open-BFME4 convert Locomotor vector base constructor

---
+++ Code/Libraries/Source/WWVegas/WWLib/LocomotorTemplateVectorBaseConstructorThunk.cpp
@@ -0,0 +1,30 @@
+class LocomotorTemplate
+{
+};
+
+namespace _STL
+{
+	template <class Type>
+	class allocator
+	{
+	};
+
+	template <class Type, class Allocator>
+	class _Vector_base
+	{
+	public:
+		_Vector_base(unsigned int, const Allocator &);
+	};
+
+	__declspec(naked) _Vector_base<LocomotorTemplate const *, allocator<LocomotorTemplate const *> >::_Vector_base(
+		unsigned int, const allocator<LocomotorTemplate const *> &)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 062h
+			_emit 0BCh
+			_emit 01Bh
+			_emit 000h
+		}
+	}
+}
