169c4e492992a8a3ea49acbc0560b599de945994
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 17:33:35 -0400

    Open-BFME5 convert AIGuardRetaliateState ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/AIGuardRetaliateStateCtorThunk.cpp
@@ -0,0 +1,68 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class StateMachine;
+
+class AIGuardRetaliateState
+{
+public:
+    AIGuardRetaliateState(StateMachine *);
+};
+
+__declspec(naked) AIGuardRetaliateState::AIGuardRetaliateState(StateMachine *)
+{
+    __asm {
+        _emit 051h
+        _emit 056h
+        _emit 051h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 089h
+        _emit 064h
+        _emit 024h
+        _emit 008h
+        _emit 08Bh
+        _emit 0CCh
+        _emit 068h
+        _emit 09Ch
+        _emit 082h
+        _emit 009h
+        _emit 001h
+        _emit 0E8h
+        _emit 05Bh
+        _emit 074h
+        _emit 071h
+        _emit 000h
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 050h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 041h
+        _emit 01Eh
+        _emit 0E9h
+        _emit 0FFh
+        _emit 0C7h
+        _emit 006h
+        _emit 048h
+        _emit 082h
+        _emit 009h
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 024h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 059h
+        _emit 0C2h
+        _emit 004h
+        _emit 000h
+    }
+}
