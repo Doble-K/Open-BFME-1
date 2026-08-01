817ee9de86cd25194992c0c0820d06af60655210
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 16:01:02 -0400

    Open-BFME5 convert category template destructor 02

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/CategoryModuleTemplateBaseDestructorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/CategoryModuleTemplateBaseDestructorThunk.cpp
@@ -52,4 +52,50 @@ __declspec(naked) CategoryModuleTemplateBase<1>::~CategoryModuleTemplateBase()
         _emit 0C3h
     }
 }
+
+template<>
+class __declspec(novtable) CategoryModuleTemplateBase<3>
+{
+public:
+    virtual ~CategoryModuleTemplateBase();
+};
+
+__declspec(naked) CategoryModuleTemplateBase<3>::~CategoryModuleTemplateBase()
+{
+    __asm {
+        _emit 085h
+        _emit 0C9h
+        _emit 074h
+        _emit 00Eh
+        _emit 0C7h
+        _emit 041h
+        _emit 004h
+        _emit 0E4h
+        _emit 0F9h
+        _emit 010h
+        _emit 001h
+        _emit 0C7h
+        _emit 001h
+        _emit 058h
+        _emit 037h
+        _emit 007h
+        _emit 001h
+        _emit 0C3h
+        _emit 033h
+        _emit 0C0h
+        _emit 0C7h
+        _emit 000h
+        _emit 0E4h
+        _emit 0F9h
+        _emit 010h
+        _emit 001h
+        _emit 0C7h
+        _emit 001h
+        _emit 058h
+        _emit 037h
+        _emit 007h
+        _emit 001h
+        _emit 0C3h
+    }
+}
 }
