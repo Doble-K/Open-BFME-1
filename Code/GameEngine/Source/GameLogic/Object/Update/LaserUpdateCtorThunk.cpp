bd66aaa349845d952e6aaef6f4e9040c67c80144
dginovker <danielginovker@gmail.com>
2026-08-01 06:26:54 -0700

    Luna Open-BFME5 convert LaserUpdate constructor thunk

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/LaserUpdateCtorThunk.cpp
@@ -0,0 +1,21 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class Thing;
+class ModuleData;
+
+class LaserUpdate
+{
+public:
+    LaserUpdate(Thing *, const ModuleData *);
+};
+
+__declspec(naked) LaserUpdate::LaserUpdate(Thing *, const ModuleData *)
+{
+    __asm {
+        _emit 0E9h
+        _emit 0CAh
+        _emit 0B2h
+        _emit 027h
+        _emit 000h
+    }
+}
