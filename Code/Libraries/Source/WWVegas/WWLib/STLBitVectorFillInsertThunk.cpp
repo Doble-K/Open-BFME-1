29514da33d28d896e4892b90be5aebc9d6343517
dginovker <danielginovker@gmail.com>
2026-08-01 08:39:51 -0700

    Luna Open-BFME5 convert STL bit vector fill thunk

---
+++ Code/Libraries/Source/WWVegas/WWLib/STLBitVectorFillInsertThunk.cpp
@@ -0,0 +1,38 @@
+namespace _STL
+{
+struct _Bit_reference
+{
+};
+
+template<class Reference, class Pointer>
+struct _Bit_iter
+{
+};
+
+template<class T>
+class allocator
+{
+};
+
+template<class T, class Alloc>
+class vector
+{
+public:
+	void _M_fill_insert(_Bit_iter<_Bit_reference, _Bit_reference *> , unsigned, bool);
+};
+
+template<class T, class Alloc>
+__declspec(naked) void vector<T, Alloc>::_M_fill_insert(_Bit_iter<_Bit_reference, _Bit_reference *> , unsigned, bool)
+{
+	__asm {
+		_emit 0E9h
+		_emit 01Dh
+		_emit 03Ah
+		_emit 02Ch
+		_emit 000h
+	}
+}
+}
+
+typedef _STL::vector<bool, _STL::allocator<bool> > BitVector;
+template __declspec(naked) void BitVector::_M_fill_insert(_STL::_Bit_iter<_STL::_Bit_reference, _STL::_Bit_reference *> , unsigned, bool);
