776b38a23dd6371c5086100b41f8cbb4b6b7f57a
dginovker <danielginovker@gmail.com>
2026-08-01 08:24:58 -0700

    Luna Open-BFME4 convert HorzLine vector reserve thunk

---
+++ Code/Libraries/Source/WWVegas/WWLib/VectorReserveHorzLineThunk.cpp
@@ -0,0 +1,32 @@
+struct HorzLine
+{
+};
+
+namespace _STL
+{
+	template <class Type>
+	class allocator
+	{
+	};
+
+	template <class Type, class Allocator>
+	class vector
+	{
+	public:
+		void reserve(unsigned int);
+	};
+
+	template <class Type, class Allocator>
+	__declspec(naked) void vector<Type, Allocator>::reserve(unsigned int)
+	{
+		__asm {
+			_emit 0E9h
+			_emit 006h
+			_emit 0B1h
+			_emit 005h
+			_emit 000h
+		}
+	}
+
+	template __declspec(naked) void vector<HorzLine, allocator<HorzLine> >::reserve(unsigned int);
+}
