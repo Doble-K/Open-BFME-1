6373332ba09a12b81e15690612f40f5a209d9066
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 17:23:57 -0400

    Open-BFME5 convert ContinueState ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/ContinueStateCtorThunk.cpp
@@ -0,0 +1,61 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class StateMachine;
+
+class ContinueState
+{
+public:
+    ContinueState(StateMachine *);
+};
+
+__declspec(naked) ContinueState::ContinueState(StateMachine *)
+{
+    __asm {
+        _emit 051h
+        _emit 056h
+        _emit 051h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 089h
+        _emit 064h
+        _emit 024h
+        _emit 008h
+        _emit 08Bh
+        _emit 0CCh
+        _emit 068h
+        _emit 00Ch
+        _emit 07Ah
+        _emit 009h
+        _emit 001h
+        _emit 0E8h
+        _emit 00Bh
+        _emit 07Dh
+        _emit 071h
+        _emit 000h
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 050h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 0F1h
+        _emit 026h
+        _emit 0E9h
+        _emit 0FFh
+        _emit 0C7h
+        _emit 006h
+        _emit 0B8h
+        _emit 079h
+        _emit 009h
+        _emit 001h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 059h
+        _emit 0C2h
+        _emit 004h
+        _emit 000h
+    }
+}
