4039f004d9e2df0fe088e6eabaa36e041378b340
dginovker <danielginovker@gmail.com>
2026-08-01 03:19:22 -0700

    Luna Open-BFME4 convert porcupine formation ctor

---
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/PorcupineFormationBodyModuleModuleData.cpp
@@ -0,0 +1,40 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+#include "PreRTS.h"
+
+class PorcupineFormationBodyModuleModuleData
+{
+public:
+    PorcupineFormationBodyModuleModuleData();
+};
+
+__declspec(naked) PorcupineFormationBodyModuleModuleData::PorcupineFormationBodyModuleModuleData()
+{
+    __asm {
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 0E8h
+        _emit 0A0h
+        _emit 023h
+        _emit 0E2h
+        _emit 0FFh
+        _emit 033h
+        _emit 0C0h
+        _emit 089h
+        _emit 046h
+        _emit 05Ch
+        _emit 089h
+        _emit 046h
+        _emit 060h
+        _emit 0C7h
+        _emit 006h
+        _emit 010h
+        _emit 090h
+        _emit 00Ah
+        _emit 001h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C3h
+    }
+}
