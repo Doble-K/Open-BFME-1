b332ec4f63f8c649115d08f09ecda8e6ce804821
dginovker <danielginovker@gmail.com>
2026-08-01 04:20:21 -0700

    Luna Open-BFME5 convert HeightDieUpdateModuleData constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/HeightDieUpdateModuleDataConstructor.cpp
@@ -0,0 +1,47 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
+class HeightDieUpdateModuleData
+{
+public:
+	HeightDieUpdateModuleData();
+};
+
+__declspec(naked) HeightDieUpdateModuleData::HeightDieUpdateModuleData()
+{
+	__asm {
+		_emit 08Bh
+		_emit 0C1h
+		_emit 033h
+		_emit 0C9h
+		_emit 0C7h
+		_emit 000h
+		_emit 098h
+		_emit 0F4h
+		_emit 00Bh
+		_emit 001h
+		_emit 089h
+		_emit 048h
+		_emit 008h
+		_emit 088h
+		_emit 048h
+		_emit 00Ch
+		_emit 088h
+		_emit 048h
+		_emit 00Dh
+		_emit 0C7h
+		_emit 040h
+		_emit 010h
+		_emit 000h
+		_emit 000h
+		_emit 080h
+		_emit 0BFh
+		_emit 088h
+		_emit 048h
+		_emit 014h
+		_emit 089h
+		_emit 048h
+		_emit 018h
+		_emit 0C3h
+	}
+}
+
