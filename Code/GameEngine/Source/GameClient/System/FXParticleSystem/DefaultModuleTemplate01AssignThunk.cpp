23b8b974ffc9448705fd899628b0e3eccb5a3193
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 16:56:48 -0400

    Open-BFME5 convert default template assign 01

---
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/DefaultModuleTemplate01AssignThunk.cpp
@@ -0,0 +1,66 @@
+// cl: /DNDEBUG /MD /EHsc
+
+namespace FXParticleSystem
+{
+template<int Category>
+class DefaultModuleTemplate
+{
+public:
+    DefaultModuleTemplate<Category> &operator=(const DefaultModuleTemplate<Category> &);
+};
+
+template<int Category>
+__declspec(naked) DefaultModuleTemplate<Category> &DefaultModuleTemplate<Category>::operator=(const DefaultModuleTemplate<Category> &)
+{
+    __asm {
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 004h
+        _emit 085h
+        _emit 0C0h
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 074h
+        _emit 012h
+        _emit 083h
+        _emit 0C0h
+        _emit 008h
+        _emit 050h
+        _emit 08Dh
+        _emit 04Eh
+        _emit 008h
+        _emit 0E8h
+        _emit 0A2h
+        _emit 0A5h
+        _emit 0A2h
+        _emit 0FFh
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C2h
+        _emit 004h
+        _emit 000h
+        _emit 033h
+        _emit 0C0h
+        _emit 050h
+        _emit 08Dh
+        _emit 04Eh
+        _emit 008h
+        _emit 0E8h
+        _emit 091h
+        _emit 0A5h
+        _emit 0A2h
+        _emit 0FFh
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C2h
+        _emit 004h
+        _emit 000h
+    }
+}
+
+template class DefaultModuleTemplate<2>;
+}
