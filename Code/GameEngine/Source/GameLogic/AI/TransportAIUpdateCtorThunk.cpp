0ebb1143643511f1a407fa84ce5ec80c97d8aa18
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 18:03:43 -0400

    Open-BFME5 convert TransportAIUpdate ctor

---
+++ Code/GameEngine/Source/GameLogic/AI/TransportAIUpdateCtorThunk.cpp
@@ -0,0 +1,76 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class TransportAIUpdate
+{
+public:
+    TransportAIUpdate(Thing *, const ModuleData *);
+};
+
+__declspec(naked) TransportAIUpdate::TransportAIUpdate(Thing *, const ModuleData *)
+{
+    __asm {
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 008h
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 008h
+        _emit 050h
+        _emit 051h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 06Fh
+        _emit 022h
+        _emit 0D6h
+        _emit 0FFh
+        _emit 0C7h
+        _emit 006h
+        _emit 0A0h
+        _emit 096h
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 00Ch
+        _emit 0D8h
+        _emit 095h
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 010h
+        _emit 0CCh
+        _emit 095h
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 020h
+        _emit 0C8h
+        _emit 095h
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 024h
+        _emit 0ACh
+        _emit 095h
+        _emit 00Ch
+        _emit 001h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C2h
+        _emit 008h
+        _emit 000h
+    }
+}
