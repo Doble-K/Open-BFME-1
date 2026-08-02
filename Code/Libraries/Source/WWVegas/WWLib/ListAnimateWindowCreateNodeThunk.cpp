b5169a5ee5396b530a0b6ed3b67a3878e18f19ad
dginovker <danielginovker@gmail.com>
2026-08-02 01:37:55 -0700

    Open-BFME5 convert list AnimateWindow create_node thunk

--- Code/Libraries/Source/WWVegas/WWLib/ListAnimateWindowCreateNodeThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ListAnimateWindowCreateNodeThunk.cpp
@@ -4,34 +4,34 @@ class AnimateWindow
 
 namespace _STL
 {
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type>
-	struct _List_node
-	{
-	};
+template <class Type>
+struct _List_node
+{
+};
 
-	template <class Type, class Allocator>
-	class list
-	{
-	protected:
-		_List_node<Type> *_M_create_node(Type const &);
-	};
+template <class Type, class Allocator>
+class list
+{
+protected:
+	_List_node<Type> *_M_create_node(Type const &);
+};
 
-	template <class Type, class Allocator>
-	__declspec(naked) _List_node<Type> *list<Type, Allocator>::_M_create_node(Type const &)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0A5h
-			_emit 0ADh
-			_emit 043h
-			_emit 000h
-		}
-	}
+class AnimateWindowListCreateNodeShim
+{
+public:
+	_List_node<AnimateWindow *> *create_node(AnimateWindow *const &x);
+};
 
-	template __declspec(naked) _List_node<AnimateWindow *> *list<AnimateWindow *, allocator<AnimateWindow *> >::_M_create_node(AnimateWindow * const &);
+template <class Type, class Allocator>
+_List_node<Type> *list<Type, Allocator>::_M_create_node(Type const &x)
+{
+	return (_List_node<Type> *)((AnimateWindowListCreateNodeShim *)this)->create_node((AnimateWindow *const &)x);
+}
+
+template _List_node<AnimateWindow *> *list<AnimateWindow *, allocator<AnimateWindow *> >::_M_create_node(AnimateWindow *const &);
 }
