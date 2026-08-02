19a05faaabe56011681fd9d37afafc82a91659ca
dginovker <danielginovker@gmail.com>
2026-08-01 17:21:14 -0700

    Luna Open-BFME5.0 convert PlayerHeal special power

---
+++ Code/GameEngine/Source/GameLogic/Object/SpecialPower/PlayerHealSpecialPower.cpp
@@ -0,0 +1,44 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class BehaviorModule
+{
+public:
+	virtual void behaviorModuleAnchor();
+
+private:
+	unsigned char m_data[8];
+};
+
+class SpecialPowerModuleInterface
+{
+public:
+	virtual void specialPowerModuleInterfaceAnchor();
+};
+
+class ModuleInterface
+{
+public:
+	virtual void moduleInterfaceAnchor();
+};
+
+class SpecialPowerModule : public BehaviorModule,
+	public SpecialPowerModuleInterface,
+	public ModuleInterface
+{
+public:
+	SpecialPowerModule( Thing *thing, const ModuleData *moduleData );
+};
+
+class PlayerHealSpecialPower : public SpecialPowerModule
+{
+public:
+	PlayerHealSpecialPower( Thing *thing, const ModuleData *moduleData );
+};
+
+PlayerHealSpecialPower::PlayerHealSpecialPower( Thing *thing, const ModuleData *moduleData )
+	: SpecialPowerModule( thing, moduleData )
+{
+}
