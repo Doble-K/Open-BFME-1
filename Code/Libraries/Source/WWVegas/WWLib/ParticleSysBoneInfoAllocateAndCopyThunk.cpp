23aa29175a86d9cd836d67889a48ccc4891945bf
dginovker <danielginovker@gmail.com>
2026-08-02 01:09:45 -0700

    Open-BFME5 convert STL vector ParticleSysBoneInfo allocate/clear thunks

--- Code/Libraries/Source/WWVegas/WWLib/ParticleSysBoneInfoAllocateAndCopyThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ParticleSysBoneInfoAllocateAndCopyThunk.cpp
@@ -4,46 +4,40 @@ struct ParticleSysBoneInfo
 
 namespace _STL
 {
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		template <class Iterator>
-		Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
-		void _M_clear();
-	};
-
-	template <class Type, class Allocator>
+template <class Type, class Allocator>
+class vector
+{
+protected:
 	template <class Iterator>
-	__declspec(naked) Type *vector<Type, Allocator>::_M_allocate_and_copy(
-		unsigned int, Iterator, Iterator)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 051h
-			_emit 0F7h
-			_emit 007h
-			_emit 000h
-		}
-	}
+	Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
+	void _M_clear();
+};
 
-	template ParticleSysBoneInfo *vector<ParticleSysBoneInfo,
-		allocator<ParticleSysBoneInfo> >::_M_allocate_and_copy<ParticleSysBoneInfo const *>(
-		unsigned int, ParticleSysBoneInfo const *, ParticleSysBoneInfo const *);
+class ParticleSysBoneInfoAllocateAndCopyShim
+{
+public:
+	ParticleSysBoneInfo *allocate_and_copy(unsigned int n, const ParticleSysBoneInfo *first, const ParticleSysBoneInfo *last);
+	void clear();
+};
 
-	__declspec(naked) void vector<ParticleSysBoneInfo, allocator<ParticleSysBoneInfo> >::_M_clear()
-	{
-		__asm {
-			_emit 0E9h
-			_emit 02Dh
-			_emit 0F1h
-			_emit 009h
-			_emit 000h
-		}
-	}
+template <class Type, class Allocator>
+template <class Iterator>
+Type *vector<Type, Allocator>::_M_allocate_and_copy(unsigned int n, Iterator first, Iterator last)
+{
+	return (Type *)((ParticleSysBoneInfoAllocateAndCopyShim *)this)->allocate_and_copy(
+		n, (const ParticleSysBoneInfo *)first, (const ParticleSysBoneInfo *)last);
+}
+
+template ParticleSysBoneInfo *vector<ParticleSysBoneInfo, allocator<ParticleSysBoneInfo> >::_M_allocate_and_copy<ParticleSysBoneInfo const *>(
+	unsigned int, ParticleSysBoneInfo const *, ParticleSysBoneInfo const *);
+
+void vector<ParticleSysBoneInfo, allocator<ParticleSysBoneInfo> >::_M_clear()
+{
+	((ParticleSysBoneInfoAllocateAndCopyShim *)this)->clear();
+}
 }
