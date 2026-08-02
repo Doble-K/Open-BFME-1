f3c0a25c803a8b647744ebb60c60775439137606
dginovker <danielginovker@gmail.com>
2026-08-02 00:17:11 -0700

    Open-BFME5 convert TeamsInfoRec addTeam thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local shim pinned to the real body.

--- Code/Libraries/Source/WWVegas/WWLib/TeamsInfoRecAddTeamThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/TeamsInfoRecAddTeamThunk.cpp
@@ -3,16 +3,16 @@ class Dict;
 class TeamsInfoRec
 {
 public:
-	void addTeam(const Dict *);
+    void addTeam(const Dict *);
 };
 
-__declspec(naked) void TeamsInfoRec::addTeam(const Dict *)
+class TeamsInfoRecAddTeamShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 063h
-		_emit 016h
-		_emit 005h
-		_emit 000h
-	}
+public:
+    void add(const Dict *dict);
+};
+
+void TeamsInfoRec::addTeam(const Dict *dict)
+{
+    ((TeamsInfoRecAddTeamShim *)this)->add(dict);
 }
