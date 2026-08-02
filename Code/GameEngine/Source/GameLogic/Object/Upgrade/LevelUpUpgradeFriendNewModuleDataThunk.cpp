dd9de192d1d55dc45dff6fb9a5d1a87e4ed6de41
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 09:25:32 -0400

    Open-BFME5 convert LevelUpUpgrade module data factory

---
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/LevelUpUpgradeFriendNewModuleDataThunk.cpp
@@ -0,0 +1,89 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class INI;
+class ModuleData;
+
+class LevelUpUpgrade
+{
+public:
+    static ModuleData *friend_newModuleData(INI *ini);
+};
+
+// ?friend_newModuleData@LevelUpUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+__declspec(naked) ModuleData *LevelUpUpgrade::friend_newModuleData(INI *)
+{
+    __asm {
+        __emit 0x56
+        __emit 0x6a
+        __emit 0x78
+        __emit 0xe8
+        __emit 0x08
+        __emit 0x6c
+        __emit 0x75
+        __emit 0x00
+        __emit 0x8b
+        __emit 0xf0
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x04
+        __emit 0x85
+        __emit 0xf6
+        __emit 0x74
+        __emit 0x1e
+        __emit 0x8d
+        __emit 0x4e
+        __emit 0x08
+        __emit 0xe8
+        __emit 0x6d
+        __emit 0x3d
+        __emit 0xee
+        __emit 0xff
+        __emit 0xc7
+        __emit 0x06
+        __emit 0xe8
+        __emit 0xf3
+        __emit 0x08
+        __emit 0x01
+        __emit 0xc7
+        __emit 0x46
+        __emit 0x70
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc7
+        __emit 0x46
+        __emit 0x74
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xeb
+        __emit 0x02
+        __emit 0x33
+        __emit 0xf6
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x08
+        __emit 0x85
+        __emit 0xc9
+        __emit 0x74
+        __emit 0x0b
+        __emit 0x68
+        __emit 0xf8
+        __emit 0xcf
+        __emit 0x41
+        __emit 0x00
+        __emit 0x56
+        __emit 0xe8
+        __emit 0xcc
+        __emit 0x6d
+        __emit 0x72
+        __emit 0x00
+        __emit 0x8b
+        __emit 0xc6
+        __emit 0x5e
+        __emit 0xc3
+    }
+}
