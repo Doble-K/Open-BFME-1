b0fc79022df22f752c4fa074d1d4231784a0e7b3
dginovker <danielginovker@gmail.com>
2026-08-02 00:22:59 -0700

    Open-BFME5 convert GetGameLogicRandomValueReal thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local static shim pinned to the real body.

--- Code/GameEngine/Source/GameLogic/ScriptEngine/GetGameLogicRandomValueReal.cpp
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/GetGameLogicRandomValueReal.cpp
@@ -1,12 +1,14 @@
 // cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
 
-__declspec(naked) float GetGameLogicRandomValueReal(float, float, char *, int)
+float GetGameLogicRandomValueReal(float, float, char *, int);
+
+class GetGameLogicRandomValueRealShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 026h
-		_emit 0A1h
-		_emit 006h
-		_emit 000h
-	}
+public:
+    static float run(float lo, float hi, char *file, int line);
+};
+
+float GetGameLogicRandomValueReal(float lo, float hi, char *file, int line)
+{
+    return GetGameLogicRandomValueRealShim::run(lo, hi, file, line);
 }
