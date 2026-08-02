4a2607427dbaad4e482a3e3c74a905d87f3e1e81
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 19:34:29 -0400

    Open-BFME5 convert FireWeaponWhenDead module data constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/FireWeaponWhenDeadBehaviorModuleDataCtorThunk.cpp
@@ -0,0 +1,125 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class FireWeaponWhenDeadBehaviorModuleData
+{
+public:
+    FireWeaponWhenDeadBehaviorModuleData();
+};
+
+__declspec(naked) FireWeaponWhenDeadBehaviorModuleData::FireWeaponWhenDeadBehaviorModuleData()
+{
+    __asm {
+        __emit 0x6a;
+        __emit 0xff;
+        __emit 0x68;
+        __emit 0xb3;
+        __emit 0x20;
+        __emit 0x00;
+        __emit 0x01;
+        __emit 0x64;
+        __emit 0xa1;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x50;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x25;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x51;
+        __emit 0x53;
+        __emit 0x56;
+        __emit 0x8b;
+        __emit 0xf1;
+        __emit 0x89;
+        __emit 0x74;
+        __emit 0x24;
+        __emit 0x08;
+        __emit 0x33;
+        __emit 0xdb;
+        __emit 0x8d;
+        __emit 0x4e;
+        __emit 0x08;
+        __emit 0x89;
+        __emit 0x5c;
+        __emit 0x24;
+        __emit 0x14;
+        __emit 0xc7;
+        __emit 0x06;
+        __emit 0xd0;
+        __emit 0xf0;
+        __emit 0x08;
+        __emit 0x01;
+        __emit 0xe8;
+        __emit 0xb4;
+        __emit 0x4e;
+        __emit 0xee;
+        __emit 0xff;
+        __emit 0x8d;
+        __emit 0x8e;
+        __emit 0x84;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0xc6;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x14;
+        __emit 0x01;
+        __emit 0xe8;
+        __emit 0xe2;
+        __emit 0xcf;
+        __emit 0xed;
+        __emit 0xff;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x0c;
+        __emit 0x88;
+        __emit 0x5e;
+        __emit 0x70;
+        __emit 0x88;
+        __emit 0x5e;
+        __emit 0x71;
+        __emit 0x89;
+        __emit 0x5e;
+        __emit 0x74;
+        __emit 0x89;
+        __emit 0x9e;
+        __emit 0xb0;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x89;
+        __emit 0x5e;
+        __emit 0x78;
+        __emit 0x89;
+        __emit 0x5e;
+        __emit 0x7c;
+        __emit 0x89;
+        __emit 0x9e;
+        __emit 0x80;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x8b;
+        __emit 0xc6;
+        __emit 0x5e;
+        __emit 0x5b;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x0d;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x83;
+        __emit 0xc4;
+        __emit 0x10;
+        __emit 0xc3;
+    }
+}
