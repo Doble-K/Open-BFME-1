26c48c5db0d84b4674ba8db7acb685f0eb650df2
dginovker <danielginovker@gmail.com>
2026-08-02 01:33:24 -0700

    Open-BFME5 convert FX module parseParticleSystem thunks

--- Code/GameEngine/Source/Common/RTS/FXModuleDataParseParticleSystemThunks.cpp
+++ Code/GameEngine/Source/Common/RTS/FXModuleDataParseParticleSystemThunks.cpp
@@ -3,33 +3,33 @@ class INI;
 class BoneFXUpdateModuleData
 {
 public:
-	__declspec(dllexport) static void parseParticleSystem(INI *, void *, void *, const void *);
+	static void parseParticleSystem(INI *, void *, void *, const void *);
 };
-
-__declspec(naked) void BoneFXUpdateModuleData::parseParticleSystem(INI *, void *, void *, const void *)
-{
-	__asm {
-		_emit 0E9h
-		_emit 037h
-		_emit 014h
-		_emit 027h
-		_emit 000h
-	}
-}
 
 class TransitionDamageFXModuleData
 {
 public:
-	__declspec(dllexport) static void parseParticleSystem(INI *, void *, void *, const void *);
+	static void parseParticleSystem(INI *, void *, void *, const void *);
 };
 
-__declspec(naked) void TransitionDamageFXModuleData::parseParticleSystem(INI *, void *, void *, const void *)
+class BoneFXParseParticleSystemShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 0C8h
-		_emit 013h
-		_emit 023h
-		_emit 000h
-	}
+public:
+	static void parseParticleSystem(INI *, void *, void *, const void *);
+};
+
+class TransitionDamageFXParseParticleSystemShim
+{
+public:
+	static void parseParticleSystem(INI *, void *, void *, const void *);
+};
+
+void BoneFXUpdateModuleData::parseParticleSystem(INI *a0, void *a1, void *a2, const void *a3)
+{
+	BoneFXParseParticleSystemShim::parseParticleSystem(a0, a1, a2, a3);
+}
+
+void TransitionDamageFXModuleData::parseParticleSystem(INI *a0, void *a1, void *a2, const void *a3)
+{
+	TransitionDamageFXParseParticleSystemShim::parseParticleSystem(a0, a1, a2, a3);
 }
