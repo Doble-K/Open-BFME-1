c69b389de5c180dc2acbe053bff5089fb1ac7613
dginovker <danielginovker@gmail.com>
2026-08-02 01:23:24 -0700

    Open-BFME5 convert STL hashtable clear GameWindowVideo dual thunk

--- Code/GameEngine/Source/GameClient/WindowVideoManagerGameWindowHashtableClearThunk.cpp
+++ Code/GameEngine/Source/GameClient/WindowVideoManagerGameWindowHashtableClearThunk.cpp
@@ -7,37 +7,50 @@ class WindowVideo
 class WindowVideoManager
 {
 public:
-    struct hashConstGameWindowPtr
-    {
-    };
+	struct hashConstGameWindowPtr
+	{
+	};
 };
 
 namespace _STL
 {
-template<class First, class Second> struct pair { };
-template<class T> struct _Select1st { };
-template<class T> struct equal_to { };
-template<class T> class allocator { };
-
-template<class Value, class Key, class Hash, class Select, class Equal, class Alloc>
-class hashtable
+template <class First, class Second>
+struct pair
 {
-    void clear();
+};
+template <class T>
+struct _Select1st
+{
+};
+template <class T>
+struct equal_to
+{
+};
+template <class T>
+class allocator
+{
 };
 
-template<class Value, class Key, class Hash, class Select, class Equal, class Alloc>
-__declspec(naked) void hashtable<Value, Key, Hash, Select, Equal, Alloc>::clear()
+template <class Value, class Key, class Hash, class Select, class Equal, class Alloc>
+class hashtable
 {
-    __asm {
-        _emit 0E9h
-        _emit 02Eh
-        _emit 02Ch
-        _emit 045h
-        _emit 000h
-    }
+private:
+	void clear();
+};
+
+class GameWindowHashtableClearShim
+{
+public:
+	void clear();
+};
+
+template <class Value, class Key, class Hash, class Select, class Equal, class Alloc>
+void hashtable<Value, Key, Hash, Select, Equal, Alloc>::clear()
+{
+	((GameWindowHashtableClearShim *)this)->clear();
 }
 }
 
 typedef _STL::pair<const GameWindow *, WindowVideo *> GameWindowVideoPair;
 typedef _STL::hashtable<GameWindowVideoPair, const GameWindow *, WindowVideoManager::hashConstGameWindowPtr, _STL::_Select1st<GameWindowVideoPair>, _STL::equal_to<const GameWindow *>, _STL::allocator<GameWindowVideoPair> > GameWindowVideoHashtable;
-template __declspec(naked) void GameWindowVideoHashtable::clear();
+template void GameWindowVideoHashtable::clear();
