1d8ed220ab6bb1f0183f3fe3a3e7a236e6ec94c2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 04:20:11 -0400

    Open-BFME5 convert AssistedTargeting isFreeToAssist

--- Code/GameEngine/Source/Common/promoted__isFreeToAssist_AssistedTargetingUpdate_QBE_NXZ_0027FCE0.cpp
+++ Code/GameEngine/Source/Common/promoted__isFreeToAssist_AssistedTargetingUpdate_QBE_NXZ_0027FCE0.cpp
@@ -2,68 +2,32 @@
 // Grok promote from masm_dumps — retail 0x0027FCE0 size 56
 // was: Code/masm_dumps/_cache__isFreeToAssist_AssistedTargetingUpdate__QBE_NXZ_27FCE0.asm
 
+class AssistedTargetingObjectShim {
+public:
+	bool check();
+	void *find(int flags);
+};
+
+class AssistedTargetingResultShim {
+public:
+	int check();
+};
+
 class AssistedTargetingUpdate { public: bool isFreeToAssist(void) const; };
 
 // ?isFreeToAssist@AssistedTargetingUpdate@@QBE_NXZ
-__declspec(naked) bool AssistedTargetingUpdate::isFreeToAssist(void) const
+bool AssistedTargetingUpdate::isFreeToAssist(void) const
 {
-__asm {
-		_emit 056h
-		_emit 08Bh
-		_emit 071h
-		_emit 008h
-		_emit 08Bh
-		_emit 0CEh
-		_emit 0E8h
-		_emit 0ECh
-		_emit 022h
-		_emit 0D8h
-		_emit 0FFh
-		_emit 084h
-		_emit 0C0h
-		_emit 074h
-		_emit 025h
-		_emit 06Ah
-		_emit 000h
-		_emit 08Bh
-		_emit 0CEh
-		_emit 0E8h
-		_emit 049h
-		_emit 09Fh
-		_emit 0D8h
-		_emit 0FFh
-		_emit 085h
-		_emit 0C0h
-		_emit 074h
-		_emit 018h
-		_emit 06Ah
-		_emit 000h
-		_emit 08Bh
-		_emit 0CEh
-		_emit 0E8h
-		_emit 03Ch
-		_emit 09Fh
-		_emit 0D8h
-		_emit 0FFh
-		_emit 08Bh
-		_emit 0C8h
-		_emit 0E8h
-		_emit 080h
-		_emit 09Ah
-		_emit 0D8h
-		_emit 0FFh
-		_emit 085h
-		_emit 0C0h
-		_emit 075h
-		_emit 004h
-		_emit 0B0h
-		_emit 001h
-		_emit 05Eh
-		_emit 0C3h
-		_emit 032h
-		_emit 0C0h
-		_emit 05Eh
-		_emit 0C3h
+	AssistedTargetingObjectShim *object =
+		(AssistedTargetingObjectShim *)*(const void **)((const unsigned char *)this + 8);
+	if (object->check()) {
+		if (object->find(0)) {
+			AssistedTargetingResultShim *result =
+				(AssistedTargetingResultShim *)object->find(0);
+			if (!result->check())
+				return true;
+		}
 	}
+	return false;
 }
 
