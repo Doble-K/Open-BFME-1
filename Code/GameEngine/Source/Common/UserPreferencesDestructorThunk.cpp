7ed5c4e5f05dad0413ce27bf9c5c72c8355f827d
dginovker <danielginovker@gmail.com>
2026-08-02 01:44:15 -0700

    Open-BFME5 convert UserPreferences dtor thunk

--- Code/GameEngine/Source/Common/UserPreferencesDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/UserPreferencesDestructorThunk.cpp
@@ -1,16 +1,18 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+
 class __declspec(novtable) UserPreferences
 {
 public:
 	virtual ~UserPreferences();
 };
 
-__declspec(naked) UserPreferences::~UserPreferences()
+class UserPreferencesDestructorShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 0D0h
-		_emit 0BCh
-		_emit 006h
-		_emit 000h
-	}
+public:
+	void destroy();
+};
+
+UserPreferences::~UserPreferences()
+{
+	((UserPreferencesDestructorShim *)this)->destroy();
 }
