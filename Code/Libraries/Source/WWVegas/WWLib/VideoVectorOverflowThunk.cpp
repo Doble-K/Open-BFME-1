1e41f4bd973dc7349f2d8646853ab88c71ec9da7
dginovker <danielginovker@gmail.com>
2026-08-02 01:14:09 -0700

    Open-BFME5 convert STL _M_insert_overflow Video thunk

--- Code/Libraries/Source/WWVegas/WWLib/VideoVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/VideoVectorOverflowThunk.cpp
@@ -4,31 +4,31 @@ struct Video
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<Video, allocator<Video> >::_M_insert_overflow(
-		Video *, const Video &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 008h
-			_emit 04Eh
-			_emit 06Ch
-			_emit 000h
-		}
-	}
+class VideoInsertOverflowShim
+{
+public:
+	void insert_overflow(Video * pos, const Video & x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<Video, allocator<Video> >::_M_insert_overflow(
+	Video * pos, const Video & x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((VideoInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
