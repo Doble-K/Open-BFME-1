55ee006f9146a73adee5ad0bcd4834ff2c9ee142
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 13:42:00 -0400

    Open-BFME5 convert DozerActionMoveToActionPosState constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/AIUpdate/DozerActionMoveToActionPosStateCtorThunk.cpp
@@ -0,0 +1,72 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class StateMachine;
+enum DozerTask
+{
+    DOZER_TASK_DUMMY
+};
+
+class DozerActionMoveToActionPosState
+{
+public:
+    DozerActionMoveToActionPosState(StateMachine *, DozerTask);
+};
+
+__declspec(naked) DozerActionMoveToActionPosState::DozerActionMoveToActionPosState(StateMachine *, DozerTask)
+{
+    __asm {
+        __emit 0x51;
+        __emit 0x56;
+        __emit 0x51;
+        __emit 0x8b;
+        __emit 0xf1;
+        __emit 0x89;
+        __emit 0x64;
+        __emit 0x24;
+        __emit 0x08;
+        __emit 0x8b;
+        __emit 0xcc;
+        __emit 0x68;
+        __emit 0xf4;
+        __emit 0x6d;
+        __emit 0x0c;
+        __emit 0x01;
+        __emit 0xe8;
+        __emit 0x3b;
+        __emit 0x18;
+        __emit 0x5d;
+        __emit 0x00;
+        __emit 0x8b;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x10;
+        __emit 0x50;
+        __emit 0x8b;
+        __emit 0xce;
+        __emit 0xe8;
+        __emit 0x21;
+        __emit 0xc2;
+        __emit 0xd4;
+        __emit 0xff;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x10;
+        __emit 0xc7;
+        __emit 0x06;
+        __emit 0xa0;
+        __emit 0x6d;
+        __emit 0x0c;
+        __emit 0x01;
+        __emit 0x89;
+        __emit 0x4e;
+        __emit 0x24;
+        __emit 0x8b;
+        __emit 0xc6;
+        __emit 0x5e;
+        __emit 0x59;
+        __emit 0xc2;
+        __emit 0x08;
+        __emit 0x00;
+    }
+}
