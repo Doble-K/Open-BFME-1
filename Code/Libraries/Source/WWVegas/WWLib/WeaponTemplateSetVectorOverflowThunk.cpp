af419c64dbefedc0f6bc5f364235ebfb870a1196
dginovker <danielginovker@gmail.com>
2026-08-02 01:18:54 -0700

    Open-BFME5 convert STL _M_insert_overflow WeaponTemplateSet thunk

--- Code/Libraries/Source/WWVegas/WWLib/WeaponTemplateSetVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/WeaponTemplateSetVectorOverflowThunk.cpp
@@ -4,31 +4,31 @@ class WeaponTemplateSet
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<WeaponTemplateSet, allocator<WeaponTemplateSet> >::_M_insert_overflow(
-		WeaponTemplateSet *, const WeaponTemplateSet &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 02Ch
-			_emit 017h
-			_emit 02Dh
-			_emit 000h
-		}
-	}
+class WeaponTemplateSetInsertOverflowShim
+{
+public:
+	void insert_overflow(WeaponTemplateSet *pos, const WeaponTemplateSet &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<WeaponTemplateSet, allocator<WeaponTemplateSet> >::_M_insert_overflow(
+	WeaponTemplateSet *pos, const WeaponTemplateSet &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((WeaponTemplateSetInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
