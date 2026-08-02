0f81e83b39316579c4cdefd423525a1e57982b4d
dginovker <danielginovker@gmail.com>
2026-08-02 01:51:17 -0700

    Open-BFME5 convert AudioEvent hashtable bkt_num_key thunk

--- Code/Libraries/Source/WWVegas/WWLib/STLAsciiAudioHashtableBucketThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/STLAsciiAudioHashtableBucketThunk.cpp
@@ -7,31 +7,49 @@ struct AudioEventInfo
 
 namespace _STL
 {
-template<class First, class Second> struct pair { };
-template<class T> struct _Select1st { };
-template<class T> class allocator { };
-template<class Value, class Key, class Hash, class Select, class Equal, class Alloc>
+template <class First, class Second>
+struct pair
+{
+};
+template <class T>
+struct _Select1st
+{
+};
+template <class T>
+class allocator
+{
+};
+template <class Value, class Key, class Hash, class Select, class Equal, class Alloc>
 class hashtable
 {
-    unsigned int _M_bkt_num_key(Key const &, unsigned int) const;
+	unsigned int _M_bkt_num_key(Key const &, unsigned int) const;
 };
-template<class Value, class Key, class Hash, class Select, class Equal, class Alloc>
-__declspec(naked) unsigned int hashtable<Value, Key, Hash, Select, Equal, Alloc>::_M_bkt_num_key(Key const &, unsigned int) const
+
+class AudioEventHashtableBktNumKeyShim
 {
-    __asm {
-        _emit 0E9h
-        _emit 074h
-        _emit 082h
-        _emit 00Ah
-        _emit 000h
-    }
+public:
+	unsigned int bkt_num_key(AsciiString const &key, unsigned int n) const;
+};
+
+template <class Value, class Key, class Hash, class Select, class Equal, class Alloc>
+unsigned int hashtable<Value, Key, Hash, Select, Equal, Alloc>::_M_bkt_num_key(Key const &key, unsigned int n) const
+{
+	return ((AudioEventHashtableBktNumKeyShim const *)this)->bkt_num_key((AsciiString const &)key, n);
 }
 }
+
 namespace rts
 {
-template<class T> struct hash { };
-template<class T> struct equal_to { };
+template <class T>
+struct hash
+{
+};
+template <class T>
+struct equal_to
+{
+};
 }
+
 typedef _STL::pair<const AsciiString, AudioEventInfo *> AudioEventPair;
 typedef _STL::hashtable<AudioEventPair, AsciiString, rts::hash<AsciiString>, _STL::_Select1st<AudioEventPair>, rts::equal_to<AsciiString>, _STL::allocator<AudioEventPair> > AudioEventHashtable;
 template unsigned int AudioEventHashtable::_M_bkt_num_key(AsciiString const &, unsigned int) const;
