c18a0575e2204a1d0ab7e537a696e38aeb0af33c
dginovker <danielginovker@gmail.com>
2026-08-02 01:16:42 -0700

    Open-BFME5 convert STL vector ModelConditionInfo_WeaponBarrelInfo overflow thunk

--- Code/Libraries/Source/WWVegas/WWLib/ModelConditionInfoWeaponBarrelInfoVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ModelConditionInfoWeaponBarrelInfoVectorOverflowThunk.cpp
@@ -7,31 +7,31 @@ struct ModelConditionInfo
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<ModelConditionInfo::WeaponBarrelInfo, allocator<ModelConditionInfo::WeaponBarrelInfo> >::_M_insert_overflow(
-		ModelConditionInfo::WeaponBarrelInfo *, const ModelConditionInfo::WeaponBarrelInfo &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0C9h
-			_emit 0FBh
-			_emit 072h
-			_emit 000h
-		}
-	}
+class ModelConditionInfoWeaponBarrelInfoInsertOverflowShim
+{
+public:
+	void insert_overflow(ModelConditionInfo::WeaponBarrelInfo *pos, const ModelConditionInfo::WeaponBarrelInfo &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<ModelConditionInfo::WeaponBarrelInfo, allocator<ModelConditionInfo::WeaponBarrelInfo> >::_M_insert_overflow(
+	ModelConditionInfo::WeaponBarrelInfo *pos, const ModelConditionInfo::WeaponBarrelInfo &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((ModelConditionInfoWeaponBarrelInfoInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
