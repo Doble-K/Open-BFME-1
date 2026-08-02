ed199fd173939300f370628cb1413a9770b648c1
dginovker <danielginovker@gmail.com>
2026-08-02 01:10:31 -0700

    Open-BFME5 convert STL vector ProductionPrerequisite allocate/clear thunks

--- Code/Libraries/Source/WWVegas/WWLib/ProductionPrerequisiteAllocateAndCopyThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ProductionPrerequisiteAllocateAndCopyThunk.cpp
@@ -4,46 +4,40 @@ class ProductionPrerequisite
 
 namespace _STL
 {
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		template <class Iterator>
-		Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
-		void _M_clear();
-	};
-
-	template <class Type, class Allocator>
+template <class Type, class Allocator>
+class vector
+{
+protected:
 	template <class Iterator>
-	__declspec(naked) Type *vector<Type, Allocator>::_M_allocate_and_copy(
-		unsigned int, Iterator, Iterator)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0FAh
-			_emit 01Ah
-			_emit 03Ah
-			_emit 000h
-		}
-	}
+	Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
+	void _M_clear();
+};
 
-	template ProductionPrerequisite *vector<ProductionPrerequisite,
-		allocator<ProductionPrerequisite> >::_M_allocate_and_copy<ProductionPrerequisite const *>(
-		unsigned int, ProductionPrerequisite const *, ProductionPrerequisite const *);
+class ProductionPrerequisiteAllocateAndCopyShim
+{
+public:
+	ProductionPrerequisite *allocate_and_copy(unsigned int n, const ProductionPrerequisite *first, const ProductionPrerequisite *last);
+	void clear();
+};
 
-	__declspec(naked) void vector<ProductionPrerequisite, allocator<ProductionPrerequisite> >::_M_clear()
-	{
-		__asm {
-			_emit 0E9h
-			_emit 079h
-			_emit 0FFh
-			_emit 038h
-			_emit 000h
-		}
-	}
+template <class Type, class Allocator>
+template <class Iterator>
+Type *vector<Type, Allocator>::_M_allocate_and_copy(unsigned int n, Iterator first, Iterator last)
+{
+	return (Type *)((ProductionPrerequisiteAllocateAndCopyShim *)this)->allocate_and_copy(
+		n, (const ProductionPrerequisite *)first, (const ProductionPrerequisite *)last);
+}
+
+template ProductionPrerequisite *vector<ProductionPrerequisite, allocator<ProductionPrerequisite> >::_M_allocate_and_copy<ProductionPrerequisite const *>(
+	unsigned int, ProductionPrerequisite const *, ProductionPrerequisite const *);
+
+void vector<ProductionPrerequisite, allocator<ProductionPrerequisite> >::_M_clear()
+{
+	((ProductionPrerequisiteAllocateAndCopyShim *)this)->clear();
+}
 }
