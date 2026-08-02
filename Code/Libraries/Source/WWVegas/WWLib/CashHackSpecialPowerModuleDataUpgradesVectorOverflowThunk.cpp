5164eae247c7936979593e5ea4893e04d51e2505
dginovker <danielginovker@gmail.com>
2026-08-02 01:18:00 -0700

    Open-BFME5 convert STL vector CashHackSpecialPowerModuleData_Upgrades overflow thunk

--- Code/Libraries/Source/WWVegas/WWLib/CashHackSpecialPowerModuleDataUpgradesVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/CashHackSpecialPowerModuleDataUpgradesVectorOverflowThunk.cpp
@@ -7,31 +7,31 @@ struct CashHackSpecialPowerModuleData
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<CashHackSpecialPowerModuleData::Upgrades, allocator<CashHackSpecialPowerModuleData::Upgrades> >::_M_insert_overflow(
-		CashHackSpecialPowerModuleData::Upgrades *, const CashHackSpecialPowerModuleData::Upgrades &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0DAh
-			_emit 09Ch
-			_emit 024h
-			_emit 000h
-		}
-	}
+class CashHackSpecialPowerModuleDataUpgradesInsertOverflowShim
+{
+public:
+	void insert_overflow(CashHackSpecialPowerModuleData::Upgrades *pos, const CashHackSpecialPowerModuleData::Upgrades &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<CashHackSpecialPowerModuleData::Upgrades, allocator<CashHackSpecialPowerModuleData::Upgrades> >::_M_insert_overflow(
+	CashHackSpecialPowerModuleData::Upgrades *pos, const CashHackSpecialPowerModuleData::Upgrades &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((CashHackSpecialPowerModuleDataUpgradesInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
