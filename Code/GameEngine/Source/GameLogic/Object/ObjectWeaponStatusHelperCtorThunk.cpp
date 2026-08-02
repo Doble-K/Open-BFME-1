602116f414bc4c1bdeb4dbb77c709a915c39d898
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 08:50:54 -0400

    Open-BFME5 convert object weapon status helper

---
+++ Code/GameEngine/Source/GameLogic/Object/ObjectWeaponStatusHelperCtorThunk.cpp
@@ -0,0 +1,124 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class ObjectWeaponStatusHelper
+{
+public:
+    ObjectWeaponStatusHelper(Thing *thing, const ModuleData *moduleData);
+};
+
+// ??0ObjectWeaponStatusHelper@@QAE@PAVThing@@PBVModuleData@@@Z
+__declspec(naked) ObjectWeaponStatusHelper::ObjectWeaponStatusHelper(Thing *thing, const ModuleData *moduleData)
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0xb8
+        __emit 0x8d
+        __emit 0x00
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x51
+        __emit 0x8b
+        __emit 0x44
+        __emit 0x24
+        __emit 0x18
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xf1
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x18
+        __emit 0x50
+        __emit 0x51
+        __emit 0x8b
+        __emit 0xce
+        __emit 0x89
+        __emit 0x74
+        __emit 0x24
+        __emit 0x0c
+        __emit 0xe8
+        __emit 0x7a
+        __emit 0x08
+        __emit 0xe6
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x56
+        __emit 0x08
+        __emit 0x6a
+        __emit 0x01
+        __emit 0x52
+        __emit 0x8b
+        __emit 0xce
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x18
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc7
+        __emit 0x06
+        __emit 0xd4
+        __emit 0xe8
+        __emit 0x09
+        __emit 0x01
+        __emit 0xc7
+        __emit 0x46
+        __emit 0x0c
+        __emit 0x10
+        __emit 0xe8
+        __emit 0x09
+        __emit 0x01
+        __emit 0xc7
+        __emit 0x46
+        __emit 0x10
+        __emit 0x04
+        __emit 0xe8
+        __emit 0x09
+        __emit 0x01
+        __emit 0xe8
+        __emit 0x43
+        __emit 0x44
+        __emit 0xe5
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x08
+        __emit 0x8b
+        __emit 0xc6
+        __emit 0x5e
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc2
+        __emit 0x08
+        __emit 0x00
+    }
+}
