1ab2d4cf044b22cf8841d5526fdaa62161f2ff29
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 22:16:33 -0400

    Open-BFME5 convert sphere emission volume template constructor

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -11022,55 +11022,17 @@ __declspec(naked) ConcreteModuleTemplate<ModuleTag<5, SPHERE_EMISSION_VOLUME_MOD
 }
 
 // ??0?$ConcreteModuleTemplate@V?$ModuleTag@$04$E?SPHERE_EMISSION_VOLUME_MODULE_KEY@FXParticleSystem@@3QBDB$E?SPHERE_EMISSION_VOLUME_MODULE_NAME@2@3QBDBVSphereEmissionVolumeModule@2@VSphereEmissionVolumeModuleTemplate@2@V?$DefaultParticleModule@$04@2@V?$DefaultParticleModuleTemplate@$04@2@@FXParticleSystem@@@FXParticleSystem@@QAE@XZ
-__declspec(naked) ConcreteModuleTemplate<ModuleTag<5, SPHERE_EMISSION_VOLUME_MODULE_KEY, SPHERE_EMISSION_VOLUME_MODULE_NAME, SphereEmissionVolumeModule, SphereEmissionVolumeModuleTemplate, DefaultParticleModule<5>, DefaultParticleModuleTemplate<5> > >::ConcreteModuleTemplate()
+ConcreteModuleTemplate<ModuleTag<5, SPHERE_EMISSION_VOLUME_MODULE_KEY, SPHERE_EMISSION_VOLUME_MODULE_NAME, SphereEmissionVolumeModule, SphereEmissionVolumeModuleTemplate, DefaultParticleModule<5>, DefaultParticleModuleTemplate<5> > >::ConcreteModuleTemplate()
 {
-    __asm {
-        __emit 0x8b
-        __emit 0xc1
-        __emit 0xc7
-        __emit 0x40
-        __emit 0x04
-        __emit 0xac
-        __emit 0xf9
-        __emit 0x10
-        __emit 0x01
-        __emit 0x33
-        __emit 0xc9
-        __emit 0x88
-        __emit 0x48
-        __emit 0x0c
-        __emit 0xc7
-        __emit 0x40
-        __emit 0x08
-        __emit 0x0c
-        __emit 0x0a
-        __emit 0x11
-        __emit 0x01
-        __emit 0x89
-        __emit 0x48
-        __emit 0x10
-        __emit 0xc7
-        __emit 0x00
-        __emit 0x00
-        __emit 0x12
-        __emit 0x11
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x40
-        __emit 0x04
-        __emit 0xfc
-        __emit 0x11
-        __emit 0x11
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x40
-        __emit 0x08
-        __emit 0xe8
-        __emit 0x11
-        __emit 0x11
-        __emit 0x01
-        __emit 0xc3
-    }
+    volatile unsigned int *slots = (unsigned int *)this;
+    slots[1] = 0x0110f9ac;
+    _ReadWriteBarrier();
+    *((volatile unsigned char *)this + 0x0c) = 0;
+    slots[2] = 0x01110a0c;
+    slots[4] = 0;
+    slots[0] = 0x01111200;
+    slots[1] = 0x011111fc;
+    slots[2] = 0x011111e8;
 }
 
 // ??1?$ConcreteModuleTemplate@V?$ModuleTag@$04$E?SPHERE_EMISSION_VOLUME_MODULE_KEY@FXParticleSystem@@3QBDB$E?SPHERE_EMISSION_VOLUME_MODULE_NAME@2@3QBDBVSphereEmissionVolumeModule@2@VSphereEmissionVolumeModuleTemplate@2@V?$DefaultParticleModule@$04@2@V?$DefaultParticleModuleTemplate@$04@2@@FXParticleSystem@@@FXParticleSystem@@UAE@XZ
