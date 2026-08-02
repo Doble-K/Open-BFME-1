7a3808cd06d63b97fc0161d417eeb5f73f638cd6
dginovker <danielginovker@gmail.com>
2026-08-02 01:52:31 -0700

    Open-BFME5 convert ObjectID vector assign/overflow thunks

--- Code/Libraries/Source/WWVegas/WWLib/ObjectIDPairVectorAssignmentThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ObjectIDPairVectorAssignmentThunk.cpp
@@ -6,70 +6,71 @@ enum ObjectID
 
 namespace _STL
 {
-	template <class First, class Second>
-	struct pair
-	{
-	};
+template <class First, class Second>
+struct pair
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	public:
-		vector &operator=(const vector &);
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const struct __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+public:
+	vector &operator=(const vector &);
+protected:
+	void _M_insert_overflow(Type *, const Type &, const struct __false_type &, unsigned int, bool);
+};
 
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	__declspec(naked) vector<pair<ObjectID, unsigned int>, allocator<pair<ObjectID, unsigned int> > > &vector<pair<ObjectID, unsigned int>, allocator<pair<ObjectID, unsigned int> > >::operator=(const vector<pair<ObjectID, unsigned int>, allocator<pair<ObjectID, unsigned int> > > &)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 08Ch
-			_emit 080h
-			_emit 006h
-			_emit 000h
-		}
-	}
+class ObjectIDPairVectorAssignShim
+{
+public:
+	vector<pair<ObjectID, unsigned int>, allocator<pair<ObjectID, unsigned int> > > &assign(
+		const vector<pair<ObjectID, unsigned int>, allocator<pair<ObjectID, unsigned int> > > &rhs);
+};
 
-	__declspec(naked) void vector<pair<ObjectID, unsigned int>, allocator<pair<ObjectID, unsigned int> > >::_M_insert_overflow(pair<ObjectID, unsigned int> *, pair<ObjectID, unsigned int> const &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 057h
-			_emit 0B5h
-			_emit 006h
-			_emit 000h
-		}
-	}
+class ObjectIDPairOverflowShim
+{
+public:
+	void insert_overflow(pair<ObjectID, unsigned int> *pos, pair<ObjectID, unsigned int> const &x, const __false_type &tag, unsigned int n, bool at_end);
+};
 
-	__declspec(naked) void vector<pair<ObjectID, ObjectID>, allocator<pair<ObjectID, ObjectID> > >::_M_insert_overflow(pair<ObjectID, ObjectID> *, pair<ObjectID, ObjectID> const &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 074h
-			_emit 0EDh
-			_emit 074h
-			_emit 000h
-		}
-	}
+class ObjectIDObjectIDOverflowShim
+{
+public:
+	void insert_overflow(pair<ObjectID, ObjectID> *pos, pair<ObjectID, ObjectID> const &x, const __false_type &tag, unsigned int n, bool at_end);
+};
 
-	__declspec(naked) void vector<ObjectID, allocator<ObjectID> >::_M_insert_overflow(ObjectID *, ObjectID const &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0DFh
-			_emit 076h
-			_emit 033h
-			_emit 000h
-		}
-	}
+class ObjectIDOverflowShim
+{
+public:
+	void insert_overflow(ObjectID *pos, ObjectID const &x, const __false_type &tag, unsigned int n, bool at_end);
+};
+
+vector<pair<ObjectID, unsigned int>, allocator<pair<ObjectID, unsigned int> > > &vector<pair<ObjectID, unsigned int>, allocator<pair<ObjectID, unsigned int> > >::operator=(const vector<pair<ObjectID, unsigned int>, allocator<pair<ObjectID, unsigned int> > > &rhs)
+{
+	return ((ObjectIDPairVectorAssignShim *)this)->assign(rhs);
+}
+
+void vector<pair<ObjectID, unsigned int>, allocator<pair<ObjectID, unsigned int> > >::_M_insert_overflow(pair<ObjectID, unsigned int> *pos, pair<ObjectID, unsigned int> const &x, const __false_type &tag, unsigned int n, bool at_end)
+{
+	((ObjectIDPairOverflowShim *)this)->insert_overflow(pos, x, tag, n, at_end);
+}
+
+void vector<pair<ObjectID, ObjectID>, allocator<pair<ObjectID, ObjectID> > >::_M_insert_overflow(pair<ObjectID, ObjectID> *pos, pair<ObjectID, ObjectID> const &x, const __false_type &tag, unsigned int n, bool at_end)
+{
+	((ObjectIDObjectIDOverflowShim *)this)->insert_overflow(pos, x, tag, n, at_end);
+}
+
+void vector<ObjectID, allocator<ObjectID> >::_M_insert_overflow(ObjectID *pos, ObjectID const &x, const __false_type &tag, unsigned int n, bool at_end)
+{
+	((ObjectIDOverflowShim *)this)->insert_overflow(pos, x, tag, n, at_end);
+}
 }
