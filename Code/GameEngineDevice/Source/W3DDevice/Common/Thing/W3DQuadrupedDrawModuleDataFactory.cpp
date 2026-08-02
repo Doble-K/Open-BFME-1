0cc07c22a9843099e60c8c021d6d5bc5f409a1b8
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 12:38:41 -0400

    Open-BFME5 convert W3DQuadrupedDraw module data factory

---
+++ Code/GameEngineDevice/Source/W3DDevice/Common/Thing/W3DQuadrupedDrawModuleDataFactory.cpp
@@ -0,0 +1,127 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class INI;
+class ModuleData;
+
+class W3DQuadrupedDraw
+{
+public:
+    static ModuleData *friend_newModuleData(INI *);
+};
+
+// ?friend_newModuleData@W3DQuadrupedDraw@@SAPAVModuleData@@PAVINI@@@Z
+__declspec(naked) ModuleData *W3DQuadrupedDraw::friend_newModuleData(INI *)
+{
+    __asm {
+        __emit 0x6a;
+        __emit 0xff;
+        __emit 0x68;
+        __emit 0x5b;
+        __emit 0x9b;
+        __emit 0x04;
+        __emit 0x01;
+        __emit 0x64;
+        __emit 0xa1;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x50;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x25;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x51;
+        __emit 0x56;
+        __emit 0x68;
+        __emit 0x6c;
+        __emit 0x01;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0xe8;
+        __emit 0x5f;
+        __emit 0x20;
+        __emit 0x1c;
+        __emit 0x00;
+        __emit 0x83;
+        __emit 0xc4;
+        __emit 0x04;
+        __emit 0x89;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x04;
+        __emit 0x85;
+        __emit 0xc0;
+        __emit 0xc7;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x10;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x74;
+        __emit 0x0b;
+        __emit 0x8b;
+        __emit 0xc8;
+        __emit 0xe8;
+        __emit 0x5a;
+        __emit 0x0a;
+        __emit 0x97;
+        __emit 0xff;
+        __emit 0x8b;
+        __emit 0xf0;
+        __emit 0xeb;
+        __emit 0x02;
+        __emit 0x33;
+        __emit 0xf6;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x18;
+        __emit 0x85;
+        __emit 0xc9;
+        __emit 0xc7;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x10;
+        __emit 0xff;
+        __emit 0xff;
+        __emit 0xff;
+        __emit 0xff;
+        __emit 0x74;
+        __emit 0x0b;
+        __emit 0x68;
+        __emit 0xa9;
+        __emit 0xe2;
+        __emit 0x41;
+        __emit 0x00;
+        __emit 0x56;
+        __emit 0xe8;
+        __emit 0x24;
+        __emit 0x22;
+        __emit 0x19;
+        __emit 0x00;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x08;
+        __emit 0x8b;
+        __emit 0xc6;
+        __emit 0x5e;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x0d;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x83;
+        __emit 0xc4;
+        __emit 0x10;
+        __emit 0xc3;
+    }
+}
