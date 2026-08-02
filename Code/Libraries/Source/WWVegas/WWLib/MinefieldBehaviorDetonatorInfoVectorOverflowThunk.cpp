0c09dff1f1bee75a34c0adf51a159cbca982151f
dginovker <danielginovker@gmail.com>
2026-08-02 01:17:26 -0700

    Open-BFME5 convert STL vector MinefieldBehavior_DetonatorInfo overflow thunk

--- Code/Libraries/Source/WWVegas/WWLib/MinefieldBehaviorDetonatorInfoVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/MinefieldBehaviorDetonatorInfoVectorOverflowThunk.cpp
@@ -7,31 +7,31 @@ struct MinefieldBehavior
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<MinefieldBehavior::DetonatorInfo, allocator<MinefieldBehavior::DetonatorInfo> >::_M_insert_overflow(
-		MinefieldBehavior::DetonatorInfo *, const MinefieldBehavior::DetonatorInfo &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 098h
-			_emit 0F4h
-			_emit 074h
-			_emit 000h
-		}
-	}
+class MinefieldBehaviorDetonatorInfoInsertOverflowShim
+{
+public:
+	void insert_overflow(MinefieldBehavior::DetonatorInfo *pos, const MinefieldBehavior::DetonatorInfo &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<MinefieldBehavior::DetonatorInfo, allocator<MinefieldBehavior::DetonatorInfo> >::_M_insert_overflow(
+	MinefieldBehavior::DetonatorInfo *pos, const MinefieldBehavior::DetonatorInfo &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((MinefieldBehaviorDetonatorInfoInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
