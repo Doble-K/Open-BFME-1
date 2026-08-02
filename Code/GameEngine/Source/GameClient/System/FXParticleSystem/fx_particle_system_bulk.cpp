b60afef6248622fc19a87648f71629aaa5d5e953
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 22:57:03 -0400

    Open-BFME5 convert StreakDraw writeINI

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -30537,45 +30537,10 @@ __declspec(naked) void SphericalEmissionVelocityModuleTemplate::writeINI(File &f
 }
 
 // ?writeINI@StreakDrawModuleTemplate@FXParticleSystem@@UBEXAAVFile@@I@Z
-__declspec(naked) void StreakDrawModuleTemplate::writeINI(File &file, unsigned int flags) const
+void StreakDrawModuleTemplate::writeINI(File &file, unsigned int flags) const
 {
-    __asm {
-        __emit 0x56
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x08
-        __emit 0x8d
-        __emit 0x44
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x50
-        __emit 0x56
-        __emit 0x51
-        __emit 0xe8
-        __emit 0xf8
-        __emit 0xda
-        __emit 0xa4
-        __emit 0xff
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x18
-        __emit 0x51
-        __emit 0x56
-        __emit 0xe8
-        __emit 0xb0
-        __emit 0xdb
-        __emit 0xa4
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x14
-        __emit 0x5e
-        __emit 0xc2
-        __emit 0x08
-        __emit 0x00
-    }
+    writeDrawTemplateBase(this, file, &flags);
+    writeDrawInfo(file, &flags);
 }
 
 // ?writeINI@TerrainCollisionModuleTemplate@FXParticleSystem@@UBEXAAVFile@@I@Z
