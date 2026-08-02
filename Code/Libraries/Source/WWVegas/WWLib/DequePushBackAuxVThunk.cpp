ba6930fde1de01465fa18a6329e74a6e903e2b0b
dginovker <danielginovker@gmail.com>
2026-08-02 01:26:05 -0700

    Open-BFME5 convert STL deque PushBackAuxV thunk

--- Code/Libraries/Source/WWVegas/WWLib/DequePushBackAuxVThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/DequePushBackAuxVThunk.cpp
@@ -1,42 +1,39 @@
 class MapObject
 {
 };
-
 class PartitionCell
 {
 };
-
 class SightingInfo
 {
 };
 
 namespace _STL
 {
-	template<class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template<class Type, class Allocator>
-	class deque
-	{
-	protected:
-		void _M_push_back_aux_v(Type const &);
-	};
+template <class Type, class Allocator>
+class deque
+{
+protected:
+	void _M_push_back_aux_v(Type const &);
+};
 
-	template<class Type, class Allocator>
-	__declspec(naked) void deque<Type, Allocator>::_M_push_back_aux_v(Type const &)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 089h
-			_emit 02Ah
-			_emit 033h
-			_emit 000h
-		}
-	}
+class DequePushBackAuxVThunkShim
+{
+public:
+	void push_back_aux(void const *x);
+};
 
-	template __declspec(naked) void deque<MapObject *, allocator<MapObject *> >::_M_push_back_aux_v(MapObject * const &);
-	template __declspec(naked) void deque<PartitionCell *, allocator<PartitionCell *> >::_M_push_back_aux_v(PartitionCell * const &);
-	template __declspec(naked) void deque<SightingInfo *, allocator<SightingInfo *> >::_M_push_back_aux_v(SightingInfo * const &);
+template <class Type, class Allocator>
+void deque<Type, Allocator>::_M_push_back_aux_v(Type const &x)
+{
+	((DequePushBackAuxVThunkShim *)this)->push_back_aux((void const *)&x);
+}
+template void deque<MapObject *, allocator<MapObject *> >::_M_push_back_aux_v(MapObject * const &);
+template void deque<PartitionCell *, allocator<PartitionCell *> >::_M_push_back_aux_v(PartitionCell * const &);
+template void deque<SightingInfo *, allocator<SightingInfo *> >::_M_push_back_aux_v(SightingInfo * const &);
 }
