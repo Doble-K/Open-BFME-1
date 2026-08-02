1728bb7de214de9d5a6db42cb318991a3bb142fa
dginovker <danielginovker@gmail.com>
2026-08-02 00:21:17 -0700

    Open-BFME5 convert PopulateLobbyPlayerListbox thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local static shim pinned to the real body.

--- Code/GameEngine/Source/GameClient/GUI/GUICallbacks/PopulateLobbyPlayerListbox.cpp
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/PopulateLobbyPlayerListbox.cpp
@@ -1,12 +1,14 @@
 // cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
 
-__declspec(naked) void PopulateLobbyPlayerListbox()
+void PopulateLobbyPlayerListbox();
+
+class PopulateLobbyPlayerListboxShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 048h
-		_emit 044h
-		_emit 04Fh
-		_emit 000h
-	}
+public:
+    static void run();
+};
+
+void PopulateLobbyPlayerListbox()
+{
+    PopulateLobbyPlayerListboxShim::run();
 }
