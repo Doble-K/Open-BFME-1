46247eadd814af0d3fe99444d1d7fa8eae81de4d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 05:10:07 -0400

    Open-BFME5 convert CustomMatchPreferences allowsObservers

--- Code/GameEngine/Source/Common/promoted__allowsObservers_CustomMatchPreferences_QAE_NXZ_000AC510.cpp
+++ Code/GameEngine/Source/Common/promoted__allowsObservers_CustomMatchPreferences_QAE_NXZ_000AC510.cpp
@@ -2,107 +2,43 @@
 // Grok promote from masm_dumps — retail 0x000AC510 size 95
 // was: Code/masm_dumps/CustomMatchPreferences_allowsObservers.asm
 
+struct CustomAsciiStringShim {
+	void *data;
+	void init(const char *);
+	void destroy(void);
+};
+
+struct CustomMapNodeShim {
+	unsigned char padding[0x14];
+	void *value;
+};
+
+struct CustomPreferenceMapShim {
+	CustomMapNodeShim *sentinel;
+	CustomMapNodeShim *find(CustomAsciiStringShim *);
+};
+
 class CustomMatchPreferences { public: bool allowsObservers(void); };
 
+typedef int (__cdecl *CustomStringCompare)(const char *, const char *);
+
 // ?allowsObservers@CustomMatchPreferences@@QAE_NXZ
-__declspec(naked) bool CustomMatchPreferences::allowsObservers(void)
+bool CustomMatchPreferences::allowsObservers(void)
 {
-__asm {
-		_emit 051h
-		_emit 056h
-		_emit 057h
-		_emit 08Bh
-		_emit 0F1h
-		_emit 068h
-		_emit 0E8h
-		_emit 011h
-		_emit 008h
-		_emit 001h
-		_emit 08Dh
-		_emit 04Ch
-		_emit 024h
-		_emit 00Ch
-		_emit 0E8h
-		_emit 09Dh
-		_emit 0C6h
-		_emit 07Dh
-		_emit 000h
-		_emit 08Dh
-		_emit 044h
-		_emit 024h
-		_emit 008h
-		_emit 083h
-		_emit 0C6h
-		_emit 004h
-		_emit 050h
-		_emit 08Bh
-		_emit 0CEh
-		_emit 0E8h
-		_emit 07Ah
-		_emit 0E9h
-		_emit 0F5h
-		_emit 0FFh
-		_emit 08Dh
-		_emit 04Ch
-		_emit 024h
-		_emit 008h
-		_emit 08Bh
-		_emit 0F8h
-		_emit 0E8h
-		_emit 003h
-		_emit 0B4h
-		_emit 07Dh
-		_emit 000h
-		_emit 03Bh
-		_emit 03Eh
-		_emit 075h
-		_emit 006h
-		_emit 05Fh
-		_emit 0B0h
-		_emit 001h
-		_emit 05Eh
-		_emit 059h
-		_emit 0C3h
-		_emit 08Bh
-		_emit 07Fh
-		_emit 014h
-		_emit 085h
-		_emit 0FFh
-		_emit 08Dh
-		_emit 047h
-		_emit 008h
-		_emit 075h
-		_emit 005h
-		_emit 0B8h
-		_emit 08Bh
-		_emit 038h
-		_emit 007h
-		_emit 001h
-		_emit 068h
-		_emit 0C0h
-		_emit 00Fh
-		_emit 008h
-		_emit 001h
-		_emit 050h
-		_emit 0FFh
-		_emit 015h
-		_emit 03Ch
-		_emit 093h
-		_emit 035h
-		_emit 001h
-		_emit 083h
-		_emit 0C4h
-		_emit 008h
-		_emit 0F7h
-		_emit 0D8h
-		_emit 01Ah
-		_emit 0C0h
-		_emit 05Fh
-		_emit 0FEh
-		_emit 0C0h
-		_emit 05Eh
-		_emit 059h
-		_emit 0C3h
-	}
+	unsigned char keyStorage[sizeof(CustomAsciiStringShim)];
+	CustomAsciiStringShim *key = (CustomAsciiStringShim *)keyStorage;
+	key->init((const char *)0x010811E8);
+	CustomPreferenceMapShim *map = (CustomPreferenceMapShim *)this;
+	map = (CustomPreferenceMapShim *)((unsigned char *)map + 4);
+	CustomMapNodeShim *node = map->find(key);
+	key->destroy();
+	if (node == map->sentinel)
+		return true;
+	node = *(CustomMapNodeShim **)((unsigned char *)node + 0x14);
+	const char *text = node ? (const char *)((unsigned char *)node + 8)
+		: (const char *)0x0107388B;
+	unsigned char result = (unsigned char)(((CustomStringCompare)*(void **)0x0135933C)(
+		text, (const char *)0x01080FC0) == 0);
+	return result;
 }
 
