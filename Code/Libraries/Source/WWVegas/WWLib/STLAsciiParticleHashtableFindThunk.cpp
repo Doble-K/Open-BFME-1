c564019afd2bf6affb83d6602c04868b73f1f8c0
dginovker <danielginovker@gmail.com>
2026-08-02 01:42:03 -0700

    Open-BFME5 convert ParticleSystem hashtable find thunk

--- Code/Libraries/Source/WWVegas/WWLib/STLAsciiParticleHashtableFindThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/STLAsciiParticleHashtableFindThunk.cpp
@@ -7,35 +7,54 @@ class ParticleSystemTemplate
 
 namespace _STL
 {
-template<class First, class Second> struct pair { };
-template<class Value> struct _Hashtable_node { };
-template<class T> struct _Select1st { };
-template<class T> class allocator { };
-
-template<class Value, class Key, class Hash, class Select, class Equal, class Alloc>
-class hashtable
+template <class First, class Second>
+struct pair
 {
-    template<class SearchKey> _Hashtable_node<Value> *_M_find(SearchKey const &) const;
+};
+template <class Value>
+struct _Hashtable_node
+{
+};
+template <class T>
+struct _Select1st
+{
+};
+template <class T>
+class allocator
+{
 };
 
-template<class Value, class Key, class Hash, class Select, class Equal, class Alloc>
-template<class SearchKey>
-__declspec(naked) _Hashtable_node<Value> *hashtable<Value, Key, Hash, Select, Equal, Alloc>::_M_find(SearchKey const &) const
+template <class Value, class Key, class Hash, class Select, class Equal, class Alloc>
+class hashtable
 {
-    __asm {
-        _emit 0E9h
-        _emit 074h
-        _emit 07Fh
-        _emit 05Bh
-        _emit 000h
-    }
+	template <class SearchKey>
+	_Hashtable_node<Value> *_M_find(SearchKey const &) const;
+};
+
+class ParticleSystemHashtableFindShim
+{
+public:
+	_Hashtable_node<pair<const AsciiString, ParticleSystemTemplate *> > *find(AsciiString const &k) const;
+};
+
+template <class Value, class Key, class Hash, class Select, class Equal, class Alloc>
+template <class SearchKey>
+_Hashtable_node<Value> *hashtable<Value, Key, Hash, Select, Equal, Alloc>::_M_find(SearchKey const &k) const
+{
+	return (_Hashtable_node<Value> *)((ParticleSystemHashtableFindShim const *)this)->find((AsciiString const &)k);
 }
 }
 
 namespace rts
 {
-template<class T> struct hash { };
-template<class T> struct equal_to { };
+template <class T>
+struct hash
+{
+};
+template <class T>
+struct equal_to
+{
+};
 }
 
 typedef _STL::pair<const AsciiString, ParticleSystemTemplate *> ParticleSystemPair;
