0abc720c0d1420f77edfb34c108aec8a7a2ba82e
dginovker <danielginovker@gmail.com>
2026-08-02 01:25:21 -0700

    Open-BFME5 convert STL deque pop_front PSRequest/PeerRequest thunks

--- Code/Libraries/Source/WWVegas/WWLib/DequePopFrontThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/DequePopFrontThunk.cpp
@@ -8,30 +8,30 @@ class PeerRequest
 
 namespace _STL
 {
-	template<class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template<class Type, class Allocator>
-	class deque
-	{
-	public:
-		void pop_front();
-	};
+template <class Type, class Allocator>
+class deque
+{
+public:
+	void pop_front();
+};
 
-	template<class Type, class Allocator>
-	__declspec(naked) void deque<Type, Allocator>::pop_front()
-	{
-		__asm {
-			_emit 0E9h
-			_emit 06Fh
-			_emit 0AFh
-			_emit 061h
-			_emit 000h
-		}
-	}
+class DequePopFrontShim
+{
+public:
+	void pop_front();
+};
 
-	template __declspec(naked) void deque<PSRequest, allocator<PSRequest> >::pop_front();
-	template __declspec(naked) void deque<PeerRequest, allocator<PeerRequest> >::pop_front();
+template <class Type, class Allocator>
+void deque<Type, Allocator>::pop_front()
+{
+	((DequePopFrontShim *)this)->pop_front();
+}
+
+template void deque<PSRequest, allocator<PSRequest> >::pop_front();
+template void deque<PeerRequest, allocator<PeerRequest> >::pop_front();
 }
