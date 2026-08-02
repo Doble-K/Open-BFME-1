67e30684329a782a6873ffacb0d8d37fb9b2fe69
dginovker <danielginovker@gmail.com>
2026-08-02 01:17:04 -0700

    Open-BFME5 convert STL vector DeliverPayloadNugget_Payload overflow thunk

--- Code/Libraries/Source/WWVegas/WWLib/DeliverPayloadNuggetPayloadVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/DeliverPayloadNuggetPayloadVectorOverflowThunk.cpp
@@ -7,31 +7,31 @@ struct DeliverPayloadNugget
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<DeliverPayloadNugget::Payload, allocator<DeliverPayloadNugget::Payload> >::_M_insert_overflow(
-		DeliverPayloadNugget::Payload *, const DeliverPayloadNugget::Payload &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 028h
-			_emit 0CAh
-			_emit 076h
-			_emit 000h
-		}
-	}
+class DeliverPayloadNuggetPayloadInsertOverflowShim
+{
+public:
+	void insert_overflow(DeliverPayloadNugget::Payload *pos, const DeliverPayloadNugget::Payload &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<DeliverPayloadNugget::Payload, allocator<DeliverPayloadNugget::Payload> >::_M_insert_overflow(
+	DeliverPayloadNugget::Payload *pos, const DeliverPayloadNugget::Payload &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((DeliverPayloadNuggetPayloadInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
