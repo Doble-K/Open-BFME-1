7a800cc308ba2e1b36c51ef6c96e912d29fba192
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 20:59:31 -0400

    Open-BFME5 convert FXListDie ctor

---
+++ Code/GameEngine/Source/GameLogic/Object/Die/FXListDieCtorThunk.cpp
@@ -0,0 +1,74 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class FXListDie
+{
+public:
+    FXListDie(Thing *, const ModuleData *);
+};
+
+__declspec(naked) FXListDie::FXListDie(Thing *, const ModuleData *)
+{
+    __asm _emit(0x8B);
+    __asm _emit(0x44);
+    __asm _emit(0x24);
+    __asm _emit(0x08);
+    __asm _emit(0x56);
+    __asm _emit(0x8B);
+    __asm _emit(0xF1);
+    __asm _emit(0x8B);
+    __asm _emit(0x4C);
+    __asm _emit(0x24);
+    __asm _emit(0x08);
+    __asm _emit(0x50);
+    __asm _emit(0x51);
+    __asm _emit(0x8B);
+    __asm _emit(0xCE);
+    __asm _emit(0xE8);
+    __asm _emit(0x70);
+    __asm _emit(0x1D);
+    __asm _emit(0xDC);
+    __asm _emit(0xFF);
+    __asm _emit(0xC7);
+    __asm _emit(0x46);
+    __asm _emit(0x0C);
+    __asm _emit(0xD0);
+    __asm _emit(0xC9);
+    __asm _emit(0x09);
+    __asm _emit(0x01);
+    __asm _emit(0xC7);
+    __asm _emit(0x46);
+    __asm _emit(0x10);
+    __asm _emit(0x7C);
+    __asm _emit(0x25);
+    __asm _emit(0x0A);
+    __asm _emit(0x01);
+    __asm _emit(0xC7);
+    __asm _emit(0x06);
+    __asm _emit(0xDC);
+    __asm _emit(0x2F);
+    __asm _emit(0x0B);
+    __asm _emit(0x01);
+    __asm _emit(0xC7);
+    __asm _emit(0x46);
+    __asm _emit(0x0C);
+    __asm _emit(0x18);
+    __asm _emit(0x2F);
+    __asm _emit(0x0B);
+    __asm _emit(0x01);
+    __asm _emit(0xC7);
+    __asm _emit(0x46);
+    __asm _emit(0x10);
+    __asm _emit(0x10);
+    __asm _emit(0x2F);
+    __asm _emit(0x0B);
+    __asm _emit(0x01);
+    __asm _emit(0x8B);
+    __asm _emit(0xC6);
+    __asm _emit(0x5E);
+    __asm _emit(0xC2);
+    __asm _emit(0x08);
+    __asm _emit(0x00);
+}
