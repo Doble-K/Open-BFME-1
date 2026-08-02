0d1e77bf476108fe7eaadd4e59c68bce437bc3f8
dginovker <danielginovker@gmail.com>
2026-08-02 01:29:16 -0700

    Open-BFME5 convert Terrain/Water tracks shutdown thunks

--- Code/Libraries/Source/WWVegas/WWLib/TracksRenderSystemShutdownThunks.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/TracksRenderSystemShutdownThunks.cpp
@@ -10,24 +10,24 @@ public:
 	void shutdown();
 };
 
-__declspec(naked) void TerrainTracksRenderObjClassSystem::shutdown()
+class TerrainTracksShutdownShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 004h
-		_emit 027h
-		_emit 070h
-		_emit 000h
-	}
+public:
+	void shutdown();
+};
+
+class WaterTracksShutdownShim
+{
+public:
+	void shutdown();
+};
+
+void TerrainTracksRenderObjClassSystem::shutdown()
+{
+	((TerrainTracksShutdownShim *)this)->shutdown();
 }
 
-__declspec(naked) void WaterTracksRenderSystem::shutdown()
+void WaterTracksRenderSystem::shutdown()
 {
-	__asm {
-		_emit 0E9h
-		_emit 090h
-		_emit 0ABh
-		_emit 077h
-		_emit 000h
-	}
+	((WaterTracksShutdownShim *)this)->shutdown();
 }
