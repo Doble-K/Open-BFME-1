3925a4dd6b72c7502c0209cf6ee8f589b910200d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 04:36:34 -0400

    Open-BFME5 convert PoisonedBehavior update

--- Code/GameEngine/Source/Common/promoted_update_PoisonedBehavior_UAE_AW4UpdateSleepTime_XZ_00202BD0.cpp
+++ Code/GameEngine/Source/Common/promoted_update_PoisonedBehavior_UAE_AW4UpdateSleepTime_XZ_00202BD0.cpp
@@ -3,60 +3,22 @@
 // was: Code/masm_dumps/update_PoisonedBehavior_00202BD0_packet301.asm
 
 enum UpdateSleepTime { UPDATE_SLEEP_NONE=0 };
+class PoisonedBehaviorOwnerShim {
+public:
+	UpdateSleepTime helper(int, void *, int, int);
+};
+
 class PoisonedBehavior { public: virtual UpdateSleepTime update(void); };
 
 // ?update@PoisonedBehavior@@UAE?AW4UpdateSleepTime@@XZ
-__declspec(naked) UpdateSleepTime PoisonedBehavior::update(void)
+UpdateSleepTime PoisonedBehavior::update(void)
 {
-__asm {
-		_emit 08Bh
-		_emit 041h
-		_emit 018h
-		_emit 083h
-		_emit 0C1h
-		_emit 0F0h
-		_emit 085h
-		_emit 0C0h
-		_emit 074h
-		_emit 020h
-		_emit 08Bh
-		_emit 015h
-		_emit 098h
-		_emit 008h
-		_emit 02Fh
-		_emit 001h
-		_emit 03Bh
-		_emit 042h
-		_emit 03Ch
-		_emit 074h
-		_emit 015h
-		_emit 068h
-		_emit 0FFh
-		_emit 0FFh
-		_emit 0FFh
-		_emit 03Fh
-		_emit 068h
-		_emit 0FFh
-		_emit 0FFh
-		_emit 0FFh
-		_emit 03Fh
-		_emit 050h
-		_emit 08Bh
-		_emit 041h
-		_emit 024h
-		_emit 050h
-		_emit 0E8h
-		_emit 0AEh
-		_emit 0D6h
-		_emit 0E1h
-		_emit 0FFh
-		_emit 0C3h
-		_emit 0B8h
-		_emit 0FFh
-		_emit 0FFh
-		_emit 0FFh
-		_emit 03Fh
-		_emit 0C3h
-	}
+	void *object = *(void **)((unsigned char *)this + 0x18);
+	PoisonedBehaviorOwnerShim *owner = (PoisonedBehaviorOwnerShim *)this;
+	owner = (PoisonedBehaviorOwnerShim *)((unsigned char *)owner - 0x10);
+	if (object && object != *(void **)((unsigned char *)(*(void **)0x012F0898) + 0x3C))
+		return owner->helper(*(int *)((unsigned char *)owner + 0x24), object,
+			0x3FFFFFFF, 0x3FFFFFFF);
+	return (UpdateSleepTime)0x3FFFFFFF;
 }
 
