6e75b624dddcec73f1481512fe6398eaa70295b5
dginovker <danielginovker@gmail.com>
2026-08-02 01:20:41 -0700

    Open-BFME5 convert STL _M_insert_overflow ICoord2D/Coord3D pair thunk

--- Code/Libraries/Source/WWVegas/WWLib/Coord2DCoord3DPairVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/Coord2DCoord3DPairVectorOverflowThunk.cpp
@@ -8,36 +8,36 @@ struct Coord3D
 
 namespace _STL
 {
-	template <class First, class Second>
-	struct pair
-	{
-	};
+template <class First, class Second>
+struct pair
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<pair<ICoord2D, Coord3D>, allocator<pair<ICoord2D, Coord3D> > >::_M_insert_overflow(
-		pair<ICoord2D, Coord3D> *, const pair<ICoord2D, Coord3D> &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 019h
-			_emit 027h
-			_emit 02Eh
-			_emit 000h
-		}
-	}
+class Coord2DCoord3DPairInsertOverflowShim
+{
+public:
+	void insert_overflow(pair<ICoord2D, Coord3D> *pos, const pair<ICoord2D, Coord3D> &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<pair<ICoord2D, Coord3D>, allocator<pair<ICoord2D, Coord3D> > >::_M_insert_overflow(
+	pair<ICoord2D, Coord3D> *pos, const pair<ICoord2D, Coord3D> &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((Coord2DCoord3DPairInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
