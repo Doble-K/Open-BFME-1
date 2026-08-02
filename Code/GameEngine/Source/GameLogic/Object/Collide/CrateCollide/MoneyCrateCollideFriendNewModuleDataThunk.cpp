2774d541a1e3111ffe698cb317c1b38ed4e17f1d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 11:12:41 -0400

    Open-BFME5 convert MoneyCrateCollide module data factory

---
+++ Code/GameEngine/Source/GameLogic/Object/Collide/CrateCollide/MoneyCrateCollideFriendNewModuleDataThunk.cpp
@@ -0,0 +1,137 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class INI;
+class ModuleData;
+
+class MoneyCrateCollide
+{
+public:
+    static ModuleData *friend_newModuleData(INI *);
+};
+
+// ?friend_newModuleData@MoneyCrateCollide@@SAPAVModuleData@@PAVINI@@@Z
+__declspec(naked) ModuleData *MoneyCrateCollide::friend_newModuleData(INI *)
+{
+    __asm {
+        _emit 06Ah
+        _emit 0FFh
+        _emit 068h
+        _emit 01Bh
+        _emit 01Bh
+        _emit 000h
+        _emit 001h
+        _emit 064h
+        _emit 0A1h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 050h
+        _emit 064h
+        _emit 089h
+        _emit 025h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 051h
+        _emit 056h
+        _emit 06Ah
+        _emit 058h
+        _emit 0E8h
+        _emit 0F2h
+        _emit 0AEh
+        _emit 075h
+        _emit 000h
+        _emit 08Bh
+        _emit 0F0h
+        _emit 083h
+        _emit 0C4h
+        _emit 004h
+        _emit 089h
+        _emit 074h
+        _emit 024h
+        _emit 004h
+        _emit 085h
+        _emit 0F6h
+        _emit 0C7h
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 074h
+        _emit 016h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 070h
+        _emit 0D1h
+        _emit 0F1h
+        _emit 0FFh
+        _emit 0C7h
+        _emit 006h
+        _emit 0A0h
+        _emit 0EAh
+        _emit 008h
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 054h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 0EBh
+        _emit 002h
+        _emit 033h
+        _emit 0F6h
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 018h
+        _emit 085h
+        _emit 0C9h
+        _emit 0C7h
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 0FFh
+        _emit 0FFh
+        _emit 0FFh
+        _emit 0FFh
+        _emit 074h
+        _emit 00Bh
+        _emit 068h
+        _emit 0EEh
+        _emit 0BAh
+        _emit 042h
+        _emit 000h
+        _emit 056h
+        _emit 0E8h
+        _emit 0AAh
+        _emit 0B0h
+        _emit 072h
+        _emit 000h
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 008h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 064h
+        _emit 089h
+        _emit 00Dh
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 083h
+        _emit 0C4h
+        _emit 010h
+        _emit 0C3h
+    }
+}
