4e66075028ab92c5d6ce81fc43d37c82f89bfed9
dginovker <danielginovker@gmail.com>
2026-08-02 00:51:43 -0700

    Open-BFME5 convert STL _Construct pair ICoord2D Coord3D thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local static shim pinned to the real body.

--- Code/GameEngine/Source/GameClient/Coord3DPairConstruct.cpp
+++ Code/GameEngine/Source/GameClient/Coord3DPairConstruct.cpp
@@ -1,28 +1,31 @@
 // cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
 
-struct ICoord2D;
-struct Coord3D;
+struct ICoord2D { int x, y; };
+struct Coord3D { float x, y, z; };
 
 namespace _STL
 {
-	template <class T, class U>
-	struct pair
-	{
-		T first;
-		U second;
-	};
+template <class T, class U>
+struct pair
+{
+    T first;
+    U second;
+};
 
-	template <class T, class U>
-	__declspec(naked) void _Construct(T *, const U &)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 048h
-			_emit 091h
-			_emit 011h
-			_emit 000h
-		}
-	}
+template <class T, class U>
+void _Construct(T *, const U &);
 
-	template void _Construct<pair<ICoord2D, Coord3D>, pair<ICoord2D, Coord3D> >(pair<ICoord2D, Coord3D> *, const pair<ICoord2D, Coord3D> &);
+class Coord3DPairConstructShim
+{
+public:
+    static void construct(pair<ICoord2D, Coord3D> *p, const pair<ICoord2D, Coord3D> &v);
+};
+
+template <class T, class U>
+void _Construct(T *p, const U &v)
+{
+    Coord3DPairConstructShim::construct((pair<ICoord2D, Coord3D> *)p, *(const pair<ICoord2D, Coord3D> *)&v);
+}
+
+template void _Construct<pair<ICoord2D, Coord3D>, pair<ICoord2D, Coord3D> >(pair<ICoord2D, Coord3D> *, const pair<ICoord2D, Coord3D> &);
 }
