824602222279ee5c5a4e73459b4cb458a7aa2f6b
dginovker <danielginovker@gmail.com>
2026-08-02 01:12:37 -0700

    Open-BFME5 convert STL _M_insert_overflow AsciiString thunk

--- Code/Libraries/Source/WWVegas/WWLib/AsciiStringVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/AsciiStringVectorOverflowThunk.cpp
@@ -4,31 +4,31 @@ class AsciiString
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<AsciiString, allocator<AsciiString> >::_M_insert_overflow(
-		AsciiString *, const AsciiString &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0ABh
-			_emit 069h
-			_emit 074h
-			_emit 000h
-		}
-	}
+class AsciiStringInsertOverflowShim
+{
+public:
+	void insert_overflow(AsciiString *pos, const AsciiString &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<AsciiString, allocator<AsciiString> >::_M_insert_overflow(
+	AsciiString *pos, const AsciiString &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((AsciiStringInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
