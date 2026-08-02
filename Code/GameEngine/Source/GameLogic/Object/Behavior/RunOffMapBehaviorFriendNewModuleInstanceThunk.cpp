35ec12d60a29fbea28584348b5c3e51cdb6cf9d5
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 17:13:17 -0400

    Open-BFME5 convert RunOffMapBehavior friend_newModuleInstance

---
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/RunOffMapBehaviorFriendNewModuleInstanceThunk.cpp
@@ -0,0 +1,114 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Module;
+class Thing;
+class ModuleData;
+
+class RunOffMapBehavior
+{
+public:
+    static Module *friend_newModuleInstance(Thing *, const ModuleData *);
+};
+
+// ?friend_newModuleInstance@RunOffMapBehavior@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
+__declspec(naked) Module *RunOffMapBehavior::friend_newModuleInstance(Thing *, const ModuleData *)
+{
+    __asm {
+        __emit 0x6a;
+        __emit 0xff;
+        __emit 0x68;
+        __emit 0x4b;
+        __emit 0xe6;
+        __emit 0xff;
+        __emit 0x00;
+        __emit 0x64;
+        __emit 0xa1;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x50;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x25;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x51;
+        __emit 0x6a;
+        __emit 0x14;
+        __emit 0xe8;
+        __emit 0x33;
+        __emit 0xc6;
+        __emit 0x76;
+        __emit 0x00;
+        __emit 0x83;
+        __emit 0xc4;
+        __emit 0x04;
+        __emit 0x89;
+        __emit 0x04;
+        __emit 0x24;
+        __emit 0x85;
+        __emit 0xc0;
+        __emit 0xc7;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x0c;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x74;
+        __emit 0x20;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x18;
+        __emit 0x8b;
+        __emit 0x54;
+        __emit 0x24;
+        __emit 0x14;
+        __emit 0x51;
+        __emit 0x52;
+        __emit 0x8b;
+        __emit 0xc8;
+        __emit 0xe8;
+        __emit 0x34;
+        __emit 0x74;
+        __emit 0xf1;
+        __emit 0xff;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x04;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x0d;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x83;
+        __emit 0xc4;
+        __emit 0x10;
+        __emit 0xc3;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x04;
+        __emit 0x33;
+        __emit 0xc0;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x0d;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x83;
+        __emit 0xc4;
+        __emit 0x10;
+        __emit 0xc3;
+    }
+}
