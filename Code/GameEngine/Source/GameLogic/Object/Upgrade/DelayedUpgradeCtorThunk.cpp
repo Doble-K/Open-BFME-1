e44b74706605e4ac00c086630ddf2babac127789
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 20:40:52 -0400

    Open-BFME5 convert DelayedUpgrade ctor

---
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/DelayedUpgradeCtorThunk.cpp
@@ -0,0 +1,70 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class DelayedUpgrade
+{
+public:
+    DelayedUpgrade(Thing *, const ModuleData *);
+};
+
+__declspec(naked) DelayedUpgrade::DelayedUpgrade(Thing *, const ModuleData *)
+{
+    __asm {
+        _emit 08Bh
+        _emit 044h
+        _emit 024h
+        _emit 008h
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 008h
+        _emit 050h
+        _emit 051h
+        _emit 08Bh
+        _emit 0CEh
+        _emit 0E8h
+        _emit 0E1h
+        _emit 075h
+        _emit 0D3h
+        _emit 0FFh
+        _emit 0C7h
+        _emit 006h
+        _emit 014h
+        _emit 0C8h
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 00Ch
+        _emit 050h
+        _emit 0C7h
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 010h
+        _emit 000h
+        _emit 0C7h
+        _emit 00Ch
+        _emit 001h
+        _emit 0C7h
+        _emit 046h
+        _emit 018h
+        _emit 0ECh
+        _emit 0C6h
+        _emit 00Ch
+        _emit 001h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C2h
+        _emit 008h
+        _emit 000h
+    }
+}
+
