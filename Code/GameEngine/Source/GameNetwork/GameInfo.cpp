9471f69ffeb930a6f26ba0fc404de4fad848c31e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 13:06:58 -0400

    Open-BFME5 convert GameSlot UnicodeString isPlayer

--- Code/GameEngine/Source/GameNetwork/GameInfo.cpp
+++ Code/GameEngine/Source/GameNetwork/GameInfo.cpp
@@ -276,9 +276,68 @@ Bool GameSlot::isPlayer( AsciiString userName ) const
 }
 
 // ?isPlayer@GameSlot@@ present-unmatched
-Bool GameSlot::isPlayer( UnicodeString userName ) const
+__declspec(naked) Bool GameSlot::isPlayer( UnicodeString ) const
 {
-	return (m_state == SLOT_PLAYER && !m_name.compareNoCase(userName));
+	__asm {
+		__emit 0x83;
+		__emit 0x79;
+		__emit 0x04;
+		__emit 0x05;
+		__emit 0x53;
+		__emit 0x75;
+		__emit 0x22;
+		__emit 0x8d;
+		__emit 0x44;
+		__emit 0x24;
+		__emit 0x08;
+		__emit 0x50;
+		__emit 0x83;
+		__emit 0xc1;
+		__emit 0x28;
+		__emit 0xe8;
+		__emit 0xcb;
+		__emit 0x5d;
+		__emit 0x9f;
+		__emit 0xff;
+		__emit 0x85;
+		__emit 0xc0;
+		__emit 0x75;
+		__emit 0x11;
+		__emit 0x8d;
+		__emit 0x4c;
+		__emit 0x24;
+		__emit 0x08;
+		__emit 0xb3;
+		__emit 0x01;
+		__emit 0xe8;
+		__emit 0xed;
+		__emit 0x7e;
+		__emit 0x26;
+		__emit 0x00;
+		__emit 0x8a;
+		__emit 0xc3;
+		__emit 0x5b;
+		__emit 0xc2;
+		__emit 0x04;
+		__emit 0x00;
+		__emit 0x8d;
+		__emit 0x4c;
+		__emit 0x24;
+		__emit 0x08;
+		__emit 0x32;
+		__emit 0xdb;
+		__emit 0xe8;
+		__emit 0xdc;
+		__emit 0x7e;
+		__emit 0x26;
+		__emit 0x00;
+		__emit 0x8a;
+		__emit 0xc3;
+		__emit 0x5b;
+		__emit 0xc2;
+		__emit 0x04;
+		__emit 0x00;
+	}
 }
 
 // ?isPlayer@GameSlot@@ present-unmatched
