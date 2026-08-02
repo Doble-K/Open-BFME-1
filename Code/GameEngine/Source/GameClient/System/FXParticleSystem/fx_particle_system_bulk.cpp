b2d341016461d05a500fcb4bf6ad970b802f21b7
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 23:17:39 -0400

    Open-BFME5 convert DefaultAlphaModuleInfo DoXfer

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -18054,83 +18054,15 @@ void ButterflyDrawModuleInfo::DoXfer(Xfer &xfer)
 }
 
 // ?DoXfer@DefaultAlphaModuleInfo@FXParticleSystem@@UAEXAAVXfer@@@Z
-__declspec(naked) void DefaultAlphaModuleInfo::DoXfer(Xfer &xfer)
+void DefaultAlphaModuleInfo::DoXfer(Xfer &xfer)
 {
-    __asm {
-        __emit 0x51
-        __emit 0x53
-        __emit 0x56
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x10
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xf9
-        __emit 0xb0
-        __emit 0x01
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x88
-        __emit 0x44
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x88
-        __emit 0x44
-        __emit 0x24
-        __emit 0x0d
-        __emit 0x8b
-        __emit 0x06
-        __emit 0x51
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x50
-        __emit 0x28
-        __emit 0x83
-        __emit 0xc7
-        __emit 0x10
-        __emit 0xbb
-        __emit 0x08
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x57
-        __emit 0xf4
-        __emit 0x52
-        __emit 0x56
-        __emit 0xe8
-        __emit 0xc0
-        __emit 0x08
-        __emit 0xa5
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x06
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x08
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x50
-        __emit 0x74
-        __emit 0x83
-        __emit 0xc7
-        __emit 0x10
-        __emit 0x4b
-        __emit 0x75
-        __emit 0xe5
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5b
-        __emit 0x59
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
+    Xfer::Version v;
+    v.data[0] = 1;
+    v.data[1] = 1;
+    xfer == v;
+    for (int i = 0; i < 8; ++i) {
+        xferRandomVariable(xfer, m_alphaKey[i].var);
+        xfer == *(unsigned int *)&m_alphaKey[i].frame;
     }
 }
 
