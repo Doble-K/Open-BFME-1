2eefdc23938823c0eb96ad68bc9fd4d66df82ec4
dginovker <danielginovker@gmail.com>
2026-08-02 01:53:44 -0700

    Open-BFME5 convert Relationship hashtable _M_insert thunk

--- Code/Libraries/Source/WWVegas/WWLib/STLRelationshipHashtableInsertThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/STLRelationshipHashtableInsertThunk.cpp
@@ -4,51 +4,51 @@ enum Relationship
 
 namespace _STL
 {
-template<class First, class Second>
+template <class First, class Second>
 struct pair
 {
 };
 
-template<class T>
+template <class T>
 struct hash
 {
 };
 
-template<class T>
+template <class T>
 struct _Select1st
 {
 };
 
-template<class T>
+template <class T>
 struct equal_to
 {
 };
 
-template<class T>
+template <class T>
 class allocator
 {
 };
 
-template<class Value, class Key, class Hash, class Extract, class Equal, class Alloc>
+template <class Value, class Key, class Hash, class Extract, class Equal, class Alloc>
 class hashtable
 {
 public:
 	Value &_M_insert(const Value &);
 };
 
-template<class Value, class Key, class Hash, class Extract, class Equal, class Alloc>
-__declspec(naked) Value &hashtable<Value, Key, Hash, Extract, Equal, Alloc>::_M_insert(const Value &)
+class RelationshipHashtableInsertShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 053h
-		_emit 05Dh
-		_emit 078h
-		_emit 000h
-	}
+public:
+	pair<const int, Relationship> &insert(const pair<const int, Relationship> &v);
+};
+
+template <class Value, class Key, class Hash, class Extract, class Equal, class Alloc>
+Value &hashtable<Value, Key, Hash, Extract, Equal, Alloc>::_M_insert(const Value &v)
+{
+	return (Value &)((RelationshipHashtableInsertShim *)this)->insert((const pair<const int, Relationship> &)v);
 }
 }
 
 typedef _STL::pair<const int, Relationship> RelationshipPair;
 typedef _STL::hashtable<RelationshipPair, int, _STL::hash<int>, _STL::_Select1st<RelationshipPair>, _STL::equal_to<int>, _STL::allocator<RelationshipPair> > RelationshipHashtable;
-template __declspec(naked) RelationshipPair &RelationshipHashtable::_M_insert(const RelationshipPair &);
+template RelationshipPair &RelationshipHashtable::_M_insert(const RelationshipPair &);
