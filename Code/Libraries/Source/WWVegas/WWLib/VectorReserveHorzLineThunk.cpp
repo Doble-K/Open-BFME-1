299b0586265b45af627ad5e27cf9b451dadf2cea
dginovker <danielginovker@gmail.com>
2026-08-02 01:51:30 -0700

    Open-BFME5 convert STL vector reserve/resize thunks

--- Code/Libraries/Source/WWVegas/WWLib/VectorReserveHorzLineThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/VectorReserveHorzLineThunk.cpp
@@ -8,44 +8,44 @@ struct ICoord2D
 
 namespace _STL
 {
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	public:
-		void reserve(unsigned int);
-		void resize(unsigned int);
-	};
+template <class Type, class Allocator>
+class vector
+{
+public:
+	void reserve(unsigned int);
+	void resize(unsigned int);
+};
 
-	template <class Type, class Allocator>
-	__declspec(naked) void vector<Type, Allocator>::reserve(unsigned int)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 006h
-			_emit 0B1h
-			_emit 005h
-			_emit 000h
-		}
-	}
+class HorzLineVectorReserveShim
+{
+public:
+	void reserve(unsigned int n);
+};
 
-	template __declspec(naked) void vector<HorzLine, allocator<HorzLine> >::reserve(unsigned int);
+class ICoord2DVectorResizeShim
+{
+public:
+	void resize(unsigned int n);
+};
 
-	template <class Type, class Allocator>
-	__declspec(naked) void vector<Type, Allocator>::resize(unsigned int)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 080h
-			_emit 007h
-			_emit 045h
-			_emit 000h
-		}
-	}
+template <class Type, class Allocator>
+void vector<Type, Allocator>::reserve(unsigned int n)
+{
+	((HorzLineVectorReserveShim *)this)->reserve(n);
+}
 
-	template __declspec(naked) void vector<ICoord2D, allocator<ICoord2D> >::resize(unsigned int);
+template void vector<HorzLine, allocator<HorzLine> >::reserve(unsigned int);
+
+template <class Type, class Allocator>
+void vector<Type, Allocator>::resize(unsigned int n)
+{
+	((ICoord2DVectorResizeShim *)this)->resize(n);
+}
+
+template void vector<ICoord2D, allocator<ICoord2D> >::resize(unsigned int);
 }
