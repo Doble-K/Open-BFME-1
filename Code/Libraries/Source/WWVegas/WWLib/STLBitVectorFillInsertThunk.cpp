170d1cddd37cee29249d0822b030854c48861d01
dginovker <danielginovker@gmail.com>
2026-08-02 01:34:37 -0700

    Open-BFME5 convert STL bit vector fill/insert/init thunks

--- Code/Libraries/Source/WWVegas/WWLib/STLBitVectorFillInsertThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/STLBitVectorFillInsertThunk.cpp
@@ -4,73 +4,64 @@ struct _Bit_reference
 {
 };
 
-template<class Reference, class Pointer>
+template <class Reference, class Pointer>
 struct _Bit_iter
 {
 };
 
-template<class T>
+template <class T>
 class allocator
 {
 };
 
-template<class T, class Alloc>
+template <class T, class Alloc>
 class vector
 {
 protected:
 	void _M_initialize(unsigned);
-	void _M_insert_aux(_Bit_iter<_Bit_reference, _Bit_reference *> , bool);
-
+	void _M_insert_aux(_Bit_iter<_Bit_reference, _Bit_reference *>, bool);
 public:
-	void _M_fill_insert(_Bit_iter<_Bit_reference, _Bit_reference *> , unsigned, bool);
+	void _M_fill_insert(_Bit_iter<_Bit_reference, _Bit_reference *>, unsigned, bool);
 };
 
-template<class T, class Alloc>
-__declspec(naked) void vector<T, Alloc>::_M_fill_insert(_Bit_iter<_Bit_reference, _Bit_reference *> , unsigned, bool)
+class BitVectorFillInsertShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 01Dh
-		_emit 03Ah
-		_emit 02Ch
-		_emit 000h
-	}
+public:
+	void fill_insert(_Bit_iter<_Bit_reference, _Bit_reference *> pos, unsigned n, bool x);
+};
+
+class BitVectorInitializeShim
+{
+public:
+	void initialize(unsigned n);
+};
+
+class BitVectorInsertAuxShim
+{
+public:
+	void insert_aux(_Bit_iter<_Bit_reference, _Bit_reference *> pos, bool x);
+};
+
+template <class T, class Alloc>
+void vector<T, Alloc>::_M_fill_insert(_Bit_iter<_Bit_reference, _Bit_reference *> pos, unsigned n, bool x)
+{
+	((BitVectorFillInsertShim *)this)->fill_insert(pos, n, x);
+}
+
+template <class T, class Alloc>
+void vector<T, Alloc>::_M_initialize(unsigned n)
+{
+	((BitVectorInitializeShim *)this)->initialize(n);
+}
+
+template <class T, class Alloc>
+void vector<T, Alloc>::_M_insert_aux(_Bit_iter<_Bit_reference, _Bit_reference *> pos, bool x)
+{
+	((BitVectorInsertAuxShim *)this)->insert_aux(pos, x);
 }
 }
 
 typedef _STL::vector<bool, _STL::allocator<bool> > BitVector;
-template __declspec(naked) void BitVector::_M_fill_insert(_STL::_Bit_iter<_STL::_Bit_reference, _STL::_Bit_reference *> , unsigned, bool);
-
-namespace _STL
-{
-template<class T, class Alloc>
-__declspec(naked) void vector<T, Alloc>::_M_initialize(unsigned)
-{
-	__asm {
-		_emit 0E9h
-		_emit 01Ah
-		_emit 0BAh
-		_emit 04Eh
-		_emit 000h
-	}
-}
-}
-
-template __declspec(naked) void BitVector::_M_initialize(unsigned);
-
-namespace _STL
-{
-template<class T, class Alloc>
-__declspec(naked) void vector<T, Alloc>::_M_insert_aux(_Bit_iter<_Bit_reference, _Bit_reference *> , bool)
-{
-	__asm {
-		_emit 0E9h
-		_emit 0D0h
-		_emit 0F3h
-		_emit 029h
-		_emit 000h
-	}
-}
-}
-
-template __declspec(naked) void BitVector::_M_insert_aux(_STL::_Bit_iter<_STL::_Bit_reference, _STL::_Bit_reference *> , bool);
+template void BitVector::_M_fill_insert(_STL::_Bit_iter<_STL::_Bit_reference, _STL::_Bit_reference *>, unsigned, bool);
+template void BitVector::_M_initialize(unsigned);
+template void BitVector::_M_insert_aux(_STL::_Bit_iter<_STL::_Bit_reference, _STL::_Bit_reference *>, bool);
