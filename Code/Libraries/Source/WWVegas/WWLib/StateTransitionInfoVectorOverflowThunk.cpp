f1e8111dafb5356fb9c5dab2729d3ff27a17278d
dginovker <danielginovker@gmail.com>
2026-08-02 01:18:11 -0700

    Open-BFME5 convert STL vector State_TransitionInfo overflow thunk

--- Code/Libraries/Source/WWVegas/WWLib/StateTransitionInfoVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/StateTransitionInfoVectorOverflowThunk.cpp
@@ -7,31 +7,31 @@ struct State
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<State::TransitionInfo, allocator<State::TransitionInfo> >::_M_insert_overflow(
-		State::TransitionInfo *, const State::TransitionInfo &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 043h
-			_emit 021h
-			_emit 020h
-			_emit 000h
-		}
-	}
+class StateTransitionInfoInsertOverflowShim
+{
+public:
+	void insert_overflow(State::TransitionInfo *pos, const State::TransitionInfo &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<State::TransitionInfo, allocator<State::TransitionInfo> >::_M_insert_overflow(
+	State::TransitionInfo *pos, const State::TransitionInfo &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((StateTransitionInfoInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
