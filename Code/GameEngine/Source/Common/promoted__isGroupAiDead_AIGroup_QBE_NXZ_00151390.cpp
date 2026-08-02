e4907847a47edac2c268df7ab331cb0ff5bc3d35
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 04:25:19 -0400

    Open-BFME5 convert AIGroup isGroupAiDead

--- Code/GameEngine/Source/Common/promoted__isGroupAiDead_AIGroup_QBE_NXZ_00151390.cpp
+++ Code/GameEngine/Source/Common/promoted__isGroupAiDead_AIGroup_QBE_NXZ_00151390.cpp
@@ -2,62 +2,37 @@
 // Grok promote from masm_dumps — retail 0x00151390 size 50
 // was: Code/masm_dumps/_cache__isGroupAiDead_AIGroup__QBE_NXZ_151390.asm
 
+struct AIGroupNode {
+	AIGroupNode *next;
+	void *unused;
+	void *object;
+};
+
 class AIGroup { public: bool isGroupAiDead(void) const; };
 
 // ?isGroupAiDead@AIGroup@@QBE_NXZ
-__declspec(naked) bool AIGroup::isGroupAiDead(void) const
+bool AIGroup::isGroupAiDead(void) const
 {
-__asm {
-		_emit 056h
-		_emit 08Bh
-		_emit 071h
-		_emit 004h
-		_emit 08Bh
-		_emit 00Eh
-		_emit 03Bh
-		_emit 0CEh
-		_emit 0B0h
-		_emit 001h
-		_emit 074h
-		_emit 024h
-		_emit 08Dh
-		_emit 064h
-		_emit 024h
-		_emit 000h
-		_emit 08Bh
-		_emit 051h
-		_emit 008h
-		_emit 085h
-		_emit 0D2h
-		_emit 074h
-		_emit 013h
-		_emit 084h
-		_emit 0C0h
-		_emit 074h
-		_emit 00Dh
-		_emit 0F6h
-		_emit 082h
-		_emit 044h
-		_emit 003h
-		_emit 000h
-		_emit 000h
-		_emit 001h
-		_emit 074h
-		_emit 004h
-		_emit 0B0h
-		_emit 001h
-		_emit 0EBh
-		_emit 002h
-		_emit 032h
-		_emit 0C0h
-		_emit 08Bh
-		_emit 009h
-		_emit 03Bh
-		_emit 0CEh
-		_emit 075h
-		_emit 0E0h
-		_emit 05Eh
-		_emit 0C3h
+	AIGroupNode *sentinel = *(AIGroupNode **)((const unsigned char *)this + 4);
+	AIGroupNode *node = sentinel->next;
+	bool dead = true;
+	while (node != sentinel) {
+		void *object = node->object;
+		if (!object)
+			goto next;
+		if (!dead)
+			goto set_false;
+		if (*(const unsigned char *)((const unsigned char *)object + 0x344) & 1)
+			goto set_true;
+		goto set_false;
+	set_true:
+		dead = true;
+		goto next;
+	set_false:
+		dead = false;
+	next:
+		node = node->next;
 	}
+	return dead;
 }
 
