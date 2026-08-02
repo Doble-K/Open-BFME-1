a765f60359f302e8435fc43b1b3836e807594e77
dginovker <danielginovker@gmail.com>
2026-08-01 17:29:58 -0700

    Luna Open-BFME9: convert LightningEmission destructor

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -20349,60 +20349,14 @@ LightningDrawModuleTemplate::~LightningDrawModuleTemplate()
 }
 
 // ??1LightningEmissionModuleTemplate@FXParticleSystem@@UAE@XZ
-__declspec(naked) LightningEmissionModuleTemplate::~LightningEmissionModuleTemplate()
+LightningEmissionModuleTemplate::~LightningEmissionModuleTemplate()
 {
-    __asm {
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x05
-        __emit 0x8d
-        __emit 0x41
-        __emit 0x08
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xc0
-        __emit 0x85
-        __emit 0xc9
-        __emit 0xc7
-        __emit 0x00
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x74
-        __emit 0x0e
-        __emit 0xc7
-        __emit 0x41
-        __emit 0x04
-        __emit 0xac
-        __emit 0xf9
-        __emit 0x10
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x01
-        __emit 0x58
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0xc3
-        __emit 0x33
-        __emit 0xc0
-        __emit 0xc7
-        __emit 0x00
-        __emit 0xac
-        __emit 0xf9
-        __emit 0x10
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x01
-        __emit 0x58
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0xc3
-    }
+    unsigned char *info = this ? (unsigned char *)this + 8 : 0;
+    *(volatile unsigned int *)info = 0x01073744;
+
+    unsigned char *base = this ? (unsigned char *)this + 4 : 0;
+    *(volatile unsigned int *)base = 0x0110f9ac;
+    *(volatile unsigned int *)this = 0x01073758;
 }
 
 // ??1LineEmissionVolumeModuleTemplate@FXParticleSystem@@UAE@XZ
