57de86ce5d74c36ad01fd7afbec4120005f6e5a0
dginovker <danielginovker@gmail.com>
2026-08-02 00:16:24 -0700

    Open-BFME5 convert TeamFactory clear thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local shim pinned to the real body.

--- Code/Libraries/Source/WWVegas/WWLib/TeamFactoryClearThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/TeamFactoryClearThunk.cpp
@@ -1,16 +1,16 @@
 class TeamFactory
 {
 public:
-	void clear();
+    void clear();
 };
 
-__declspec(naked) void TeamFactory::clear()
+class TeamFactoryClearShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 0F4h
-		_emit 0E7h
-		_emit 00Ch
-		_emit 000h
-	}
+public:
+    void run();
+};
+
+void TeamFactory::clear()
+{
+    ((TeamFactoryClearShim *)this)->run();
 }
