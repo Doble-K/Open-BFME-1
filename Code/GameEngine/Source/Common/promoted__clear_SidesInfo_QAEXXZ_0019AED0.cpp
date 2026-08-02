5a5d76ae6e3cad011c91be11b8f9218ccc415bdd
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 04:46:14 -0400

    Open-BFME5 convert SidesInfo clear

--- Code/GameEngine/Source/Common/promoted__clear_SidesInfo_QAEXXZ_0019AED0.cpp
+++ Code/GameEngine/Source/Common/promoted__clear_SidesInfo_QAEXXZ_0019AED0.cpp
@@ -2,78 +2,38 @@
 // Grok promote from masm_dumps — retail 0x0019AED0 size 66
 // was: Code/masm_dumps/SidesList_SidesInfo_clear.asm
 
+class SidesInfoNodeShim {
+public:
+	virtual void clear(int) = 0;
+};
+
+class SidesInfoSegmentShim {
+public:
+	void reset(void);
+	void finish(void *, void *);
+};
+
 class SidesInfo { public: void clear(void); };
 
 // ?clear@SidesInfo@@QAEXXZ
-__declspec(naked) void SidesInfo::clear(void)
+void SidesInfo::clear(void)
 {
-__asm {
-		_emit 056h
-		_emit 08Bh
-		_emit 0F1h
-		_emit 08Bh
-		_emit 00Eh
-		_emit 085h
-		_emit 0C9h
-		_emit 074h
-		_emit 006h
-		_emit 08Bh
-		_emit 001h
-		_emit 06Ah
-		_emit 001h
-		_emit 0FFh
-		_emit 010h
-		_emit 08Dh
-		_emit 04Eh
-		_emit 004h
-		_emit 0C7h
-		_emit 006h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 0E8h
-		_emit 059h
-		_emit 090h
-		_emit 0E9h
-		_emit 0FFh
-		_emit 08Bh
-		_emit 04Eh
-		_emit 008h
-		_emit 085h
-		_emit 0C9h
-		_emit 074h
-		_emit 006h
-		_emit 08Bh
-		_emit 011h
-		_emit 06Ah
-		_emit 001h
-		_emit 0FFh
-		_emit 012h
-		_emit 08Dh
-		_emit 04Eh
-		_emit 00Ch
-		_emit 0C7h
-		_emit 046h
-		_emit 008h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 08Bh
-		_emit 041h
-		_emit 004h
-		_emit 08Bh
-		_emit 011h
-		_emit 050h
-		_emit 052h
-		_emit 0E8h
-		_emit 007h
-		_emit 09Dh
-		_emit 0E8h
-		_emit 0FFh
-		_emit 05Eh
-		_emit 0C3h
-	}
+	SidesInfoNodeShim *first =
+		*(SidesInfoNodeShim **)((unsigned char *)this + 0x00);
+	if (first)
+		first->clear(1);
+	SidesInfoSegmentShim *firstSegment =
+		(SidesInfoSegmentShim *)((unsigned char *)this + 0x04);
+	*(SidesInfoNodeShim **)((unsigned char *)this + 0x00) = 0;
+	firstSegment->reset();
+	SidesInfoNodeShim *second =
+		*(SidesInfoNodeShim **)((unsigned char *)this + 0x08);
+	if (second)
+		second->clear(1);
+	SidesInfoSegmentShim *secondSegment =
+		(SidesInfoSegmentShim *)((unsigned char *)this + 0x0C);
+	*(SidesInfoNodeShim **)((unsigned char *)this + 0x08) = 0;
+	secondSegment->finish(*(void **)((unsigned char *)secondSegment + 0x00),
+		*(void **)((unsigned char *)secondSegment + 0x04));
 }
 
