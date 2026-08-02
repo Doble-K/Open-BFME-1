13ea2fd445b625b1a85f3f5e894926d1711e26f9
dginovker <danielginovker@gmail.com>
2026-08-02 00:44:55 -0700

    Open-BFME5 convert BoneFXUpdateModuleData parseObjectCreationList thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local static shim pinned to the real body.

--- Code/GameEngine/Source/Common/RTS/BoneFXUpdateModuleDataParseObjectCreationListThunk.cpp
+++ Code/GameEngine/Source/Common/RTS/BoneFXUpdateModuleDataParseObjectCreationListThunk.cpp
@@ -1,20 +1,20 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /Code/GameEngine/Include /Code/GameEngine/Include/Precompiled /Code/Libraries/Source/WWVegas/WWLib
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
 
 class INI;
 
 class BoneFXUpdateModuleData
 {
 public:
-    __declspec(dllexport) static void parseObjectCreationList(INI *, void *, void *, const void *);
+    static void parseObjectCreationList(INI *, void *, void *, const void *);
 };
 
-__declspec(naked) void BoneFXUpdateModuleData::parseObjectCreationList(INI *, void *, void *, const void *)
+class BoneFXUpdateModuleDataParseObjectCreationListShim
 {
-    __asm {
-        _emit 0E9h
-        _emit 0D6h
-        _emit 046h
-        _emit 028h
-        _emit 000h
-    }
+public:
+    static void parse(INI *ini, void *store, void *userData, const void *desc);
+};
+
+void BoneFXUpdateModuleData::parseObjectCreationList(INI *ini, void *store, void *userData, const void *desc)
+{
+    BoneFXUpdateModuleDataParseObjectCreationListShim::parse(ini, store, userData, desc);
 }
