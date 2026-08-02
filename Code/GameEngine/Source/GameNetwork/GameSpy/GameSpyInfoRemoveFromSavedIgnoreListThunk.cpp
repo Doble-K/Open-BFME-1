017f5a3f7196e144167ce0160a191eb7921502af
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 08:40:10 -0400

    Open-BFME5 convert saved ignore removal

---
+++ Code/GameEngine/Source/GameNetwork/GameSpy/GameSpyInfoRemoveFromSavedIgnoreListThunk.cpp
@@ -0,0 +1,132 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class GameSpyInfo
+{
+public:
+    virtual void removeFromSavedIgnoreList(int profileID);
+};
+
+// ?removeFromSavedIgnoreList@GameSpyInfo@@UAEXH@Z
+__declspec(naked) void GameSpyInfo::removeFromSavedIgnoreList(int profileID)
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0x98
+        __emit 0x0c
+        __emit 0x04
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xec
+        __emit 0x14
+        __emit 0x8d
+        __emit 0x44
+        __emit 0x24
+        __emit 0x24
+        __emit 0x50
+        __emit 0x81
+        __emit 0xc1
+        __emit 0xd4
+        __emit 0x06
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x65
+        __emit 0xfb
+        __emit 0x9c
+        __emit 0xff
+        __emit 0x8d
+        __emit 0x0c
+        __emit 0x24
+        __emit 0xe8
+        __emit 0x29
+        __emit 0x8c
+        __emit 0x9f
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x24
+        __emit 0x6a
+        __emit 0x00
+        __emit 0x51
+        __emit 0x68
+        __emit 0x50
+        __emit 0x6e
+        __emit 0x33
+        __emit 0x01
+        __emit 0x8d
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x0c
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x28
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x0c
+        __emit 0x10
+        __emit 0x9e
+        __emit 0xff
+        __emit 0x8d
+        __emit 0x0c
+        __emit 0x24
+        __emit 0xe8
+        __emit 0x10
+        __emit 0xad
+        __emit 0x9f
+        __emit 0xff
+        __emit 0x8d
+        __emit 0x0c
+        __emit 0x24
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x1c
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xe8
+        __emit 0x8e
+        __emit 0xe2
+        __emit 0x9f
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x14
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x20
+        __emit 0xc2
+        __emit 0x04
+        __emit 0x00
+    }
+}
