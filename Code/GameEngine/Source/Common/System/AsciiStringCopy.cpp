2bcd1e29b03c0dfe710c3dedc2ef15c230e800b3
dginovker <danielginovker@gmail.com>
2026-08-02 00:55:32 -0700

    Open-BFME5 convert STL __copy AsciiString thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local static shim pinned to the real body.

--- Code/GameEngine/Source/Common/System/AsciiStringCopy.cpp
+++ Code/GameEngine/Source/Common/System/AsciiStringCopy.cpp
@@ -4,19 +4,22 @@ class AsciiString;
 
 namespace _STL
 {
-	struct random_access_iterator_tag;
+struct random_access_iterator_tag {};
 
-	template <class In, class Out, class Distance>
-	__declspec(naked) Out __copy(In, In, Out, const random_access_iterator_tag &, Distance *)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0C5h
-			_emit 073h
-			_emit 06Bh
-			_emit 000h
-		}
-	}
+template <class In, class Out, class Distance>
+Out __copy(In, In, Out, const random_access_iterator_tag &, Distance *);
 
-	template AsciiString *__copy<AsciiString *, AsciiString *, int>(AsciiString *, AsciiString *, AsciiString *, const random_access_iterator_tag &, int *);
+class AsciiStringCopyShim
+{
+public:
+    static AsciiString *copy(AsciiString *first, AsciiString *last, AsciiString *result, const random_access_iterator_tag &, int *n);
+};
+
+template <class In, class Out, class Distance>
+Out __copy(In first, In last, Out result, const random_access_iterator_tag &tag, Distance *n)
+{
+    return (Out)AsciiStringCopyShim::copy((AsciiString *)first, (AsciiString *)last, (AsciiString *)result, tag, (int *)n);
+}
+
+template AsciiString *__copy<AsciiString *, AsciiString *, int>(AsciiString *, AsciiString *, AsciiString *, const random_access_iterator_tag &, int *);
 }
