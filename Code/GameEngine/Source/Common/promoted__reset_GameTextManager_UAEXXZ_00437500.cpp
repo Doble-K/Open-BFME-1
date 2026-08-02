e9d9fe5c1469a8bd59450ceb152966cfe89a6b52
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 04:54:26 -0400

    Open-BFME5 convert GameTextManager reset

--- Code/GameEngine/Source/Common/promoted__reset_GameTextManager_UAEXXZ_00437500.cpp
+++ Code/GameEngine/Source/Common/promoted__reset_GameTextManager_UAEXXZ_00437500.cpp
@@ -4,95 +4,23 @@
 
 class GameTextManager { public: virtual void reset(void); };
 
+extern "C" void __stdcall gameTextPrepare(void *, int, void *, int);
+extern "C" void __cdecl gameTextRelease(void *);
+
 // ?reset@GameTextManager@@UAEXXZ
-__declspec(naked) void GameTextManager::reset(void)
+void GameTextManager::reset(void)
 {
-__asm {
-		_emit 056h
-		_emit 08Bh
-		_emit 0F1h
-		_emit 08Bh
-		_emit 086h
-		_emit 028h
-		_emit 078h
-		_emit 000h
-		_emit 000h
-		_emit 085h
-		_emit 0C0h
-		_emit 074h
-		_emit 029h
-		_emit 08Bh
-		_emit 048h
-		_emit 0FCh
-		_emit 057h
-		_emit 08Dh
-		_emit 078h
-		_emit 0FCh
-		_emit 068h
-		_emit 00Eh
-		_emit 051h
-		_emit 041h
-		_emit 000h
-		_emit 051h
-		_emit 06Ah
-		_emit 008h
-		_emit 050h
-		_emit 0E8h
-		_emit 054h
-		_emit 0F8h
-		_emit 05Bh
-		_emit 000h
-		_emit 057h
-		_emit 0E8h
-		_emit 0C8h
-		_emit 0A9h
-		_emit 044h
-		_emit 000h
-		_emit 083h
-		_emit 0C4h
-		_emit 004h
-		_emit 0C7h
-		_emit 086h
-		_emit 028h
-		_emit 078h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 05Fh
-		_emit 08Bh
-		_emit 086h
-		_emit 02Ch
-		_emit 078h
-		_emit 000h
-		_emit 000h
-		_emit 085h
-		_emit 0C0h
-		_emit 074h
-		_emit 013h
-		_emit 050h
-		_emit 0E8h
-		_emit 0AAh
-		_emit 0A9h
-		_emit 044h
-		_emit 000h
-		_emit 083h
-		_emit 0C4h
-		_emit 004h
-		_emit 0C7h
-		_emit 086h
-		_emit 02Ch
-		_emit 078h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 05Eh
-		_emit 0C3h
+	void *first = *(void **)((unsigned char *)this + 0x7828);
+	if (first) {
+		void *array = (unsigned char *)first - 4;
+		gameTextPrepare(first, 8, *(void **)array, 0x0041510E);
+		gameTextRelease(array);
+		*(void **)((unsigned char *)this + 0x7828) = 0;
+	}
+	void *second = *(void **)((unsigned char *)this + 0x782C);
+	if (second) {
+		gameTextRelease(second);
+		*(void **)((unsigned char *)this + 0x782C) = 0;
 	}
 }
 
