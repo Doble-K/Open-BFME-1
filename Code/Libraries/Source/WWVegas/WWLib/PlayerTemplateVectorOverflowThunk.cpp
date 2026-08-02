3fe312c8c669d6380afe369b33a67fe0fb88e40f
dginovker <danielginovker@gmail.com>
2026-08-02 01:14:29 -0700

    Open-BFME5 convert STL _M_insert_overflow PlayerTemplate thunk

--- Code/Libraries/Source/WWVegas/WWLib/PlayerTemplateVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/PlayerTemplateVectorOverflowThunk.cpp
@@ -4,31 +4,31 @@ class PlayerTemplate
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<PlayerTemplate, allocator<PlayerTemplate> >::_M_insert_overflow(
-		PlayerTemplate *, const PlayerTemplate &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 05Dh
-			_emit 03Ah
-			_emit 00Bh
-			_emit 000h
-		}
-	}
+class PlayerTemplateInsertOverflowShim
+{
+public:
+	void insert_overflow(PlayerTemplate * pos, const PlayerTemplate & x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<PlayerTemplate, allocator<PlayerTemplate> >::_M_insert_overflow(
+	PlayerTemplate * pos, const PlayerTemplate & x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((PlayerTemplateInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
