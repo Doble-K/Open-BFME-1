62aec246bbb57dda56ee924e9995c4a43a1ca2bd
dginovker <danielginovker@gmail.com>
2026-08-02 00:23:47 -0700

    Open-BFME5 convert GameFileClass Set_Name thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local shim pinned to the real body.

--- Code/Libraries/Source/WWVegas/WWLib/GameFileSetNameThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/GameFileSetNameThunk.cpp
@@ -1,16 +1,16 @@
 class GameFileClass
 {
 public:
-	virtual char const *Set_Name(char const *);
+    virtual const char *Set_Name(const char *);
 };
 
-__declspec(naked) char const *GameFileClass::Set_Name(char const *)
+class GameFileSetNameShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 0A6h
-		_emit 0F4h
-		_emit 06Eh
-		_emit 000h
-	}
+public:
+    const char *set(const char *name);
+};
+
+const char *GameFileClass::Set_Name(const char *name)
+{
+    return ((GameFileSetNameShim *)this)->set(name);
 }
