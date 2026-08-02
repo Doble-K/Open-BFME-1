30b842fa618ec106a0f9982d78b71f9dbb3b1a9e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 08:32:19 -0400

    Open-BFME5 convert W3DStatusCircle ctor

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DStatusCircleCtorThunk.cpp
@@ -0,0 +1,104 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class W3DStatusCircle
+{
+public:
+    W3DStatusCircle();
+};
+
+// ??0W3DStatusCircle@@QAE@XZ
+__declspec(naked) W3DStatusCircle::W3DStatusCircle()
+{
+    __asm {
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xf1
+        __emit 0xe8
+        __emit 0xa8
+        __emit 0xa4
+        __emit 0x1f
+        __emit 0x00
+        __emit 0x33
+        __emit 0xc0
+        __emit 0xb9
+        __emit 0x00
+        __emit 0x00
+        __emit 0x80
+        __emit 0xbf
+        __emit 0x89
+        __emit 0x86
+        __emit 0xc8
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x89
+        __emit 0x86
+        __emit 0xcc
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x89
+        __emit 0x86
+        __emit 0xd8
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc7
+        __emit 0x06
+        __emit 0x18
+        __emit 0x10
+        __emit 0x12
+        __emit 0x01
+        __emit 0xc7
+        __emit 0x46
+        __emit 0x08
+        __emit 0x10
+        __emit 0x10
+        __emit 0x12
+        __emit 0x01
+        __emit 0x89
+        __emit 0x8e
+        __emit 0xd0
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x89
+        __emit 0x8e
+        __emit 0xd4
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc7
+        __emit 0x86
+        __emit 0xdc
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x1b
+        __emit 0x44
+        __emit 0x10
+        __emit 0x00
+        __emit 0x89
+        __emit 0x86
+        __emit 0xe0
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x89
+        __emit 0x86
+        __emit 0xe4
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x89
+        __emit 0x86
+        __emit 0xe8
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x8b
+        __emit 0xc6
+        __emit 0x5e
+        __emit 0xc3
+    }
+}
