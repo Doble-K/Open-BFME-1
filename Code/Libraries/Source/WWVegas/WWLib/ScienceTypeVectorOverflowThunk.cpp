5824bcedc73c783e176aed92e37706ea157be9cf
dginovker <danielginovker@gmail.com>
2026-08-02 01:20:22 -0700

    Open-BFME5 convert STL _M_insert_overflow ScienceType thunk

--- Code/Libraries/Source/WWVegas/WWLib/ScienceTypeVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ScienceTypeVectorOverflowThunk.cpp
@@ -2,34 +2,36 @@
 
 enum ScienceType
 {
+	SCIENCE_INVALID = -1
 };
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<ScienceType, allocator<ScienceType> >::_M_insert_overflow(ScienceType *, ScienceType const &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 073h
-			_emit 090h
-			_emit 009h
-			_emit 000h
-		}
-	}
+class ScienceTypeInsertOverflowShim
+{
+public:
+	void insert_overflow(ScienceType *pos, const ScienceType &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<ScienceType, allocator<ScienceType> >::_M_insert_overflow(
+	ScienceType *pos, const ScienceType &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((ScienceTypeInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
