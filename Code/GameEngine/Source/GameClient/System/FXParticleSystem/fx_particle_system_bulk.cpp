1183106aac6c5e9392b11234303745856ef2f1ac
dginovker <danielginovker@gmail.com>
2026-08-01 18:05:41 -0700

    Luna Open-BFME8 convert default module one destructor to C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -9417,60 +9417,14 @@ __declspec(naked) ConcreteModuleTemplate<DefaultModuleTag<1> >::ConcreteModuleTe
 }
 
 // ??1?$ConcreteModuleTemplate@V?$DefaultModuleTag@$00@FXParticleSystem@@@FXParticleSystem@@UAE@XZ
-__declspec(naked) ConcreteModuleTemplate<DefaultModuleTag<1> >::~ConcreteModuleTemplate()
+ConcreteModuleTemplate<DefaultModuleTag<1> >::~ConcreteModuleTemplate()
 {
-    __asm {
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x05
-        __emit 0x8d
-        __emit 0x41
-        __emit 0x08
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xc0
-        __emit 0x85
-        __emit 0xc9
-        __emit 0xc7
-        __emit 0x00
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x74
-        __emit 0x0e
-        __emit 0xc7
-        __emit 0x41
-        __emit 0x04
-        __emit 0x78
-        __emit 0xf9
-        __emit 0x10
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x01
-        __emit 0x58
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0xc3
-        __emit 0x33
-        __emit 0xc0
-        __emit 0xc7
-        __emit 0x00
-        __emit 0x78
-        __emit 0xf9
-        __emit 0x10
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x01
-        __emit 0x58
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0xc3
-    }
+    unsigned char *info = this ? (unsigned char *)this + 8 : 0;
+    *(volatile unsigned int *)info = 0x01073744;
+
+    unsigned char *base = this ? (unsigned char *)this + 4 : 0;
+    *(volatile unsigned int *)base = 0x0110f978;
+    *(volatile unsigned int *)this = 0x01073758;
 }
 
 // ??4?$ConcreteModuleTemplate@V?$DefaultModuleTag@$00@FXParticleSystem@@@FXParticleSystem@@QAEAAV01@ABV01@@Z
