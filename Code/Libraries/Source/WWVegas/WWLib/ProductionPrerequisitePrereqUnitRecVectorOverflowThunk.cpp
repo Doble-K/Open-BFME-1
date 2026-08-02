59f5ed50a9cfd80a1d7b8768fef681d5c7fd8d84
dginovker <danielginovker@gmail.com>
2026-08-02 01:16:53 -0700

    Open-BFME5 convert STL vector ProductionPrerequisite_PrereqUnitRec overflow thunk

--- Code/Libraries/Source/WWVegas/WWLib/ProductionPrerequisitePrereqUnitRecVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ProductionPrerequisitePrereqUnitRecVectorOverflowThunk.cpp
@@ -7,31 +7,31 @@ struct ProductionPrerequisite
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<ProductionPrerequisite::PrereqUnitRec, allocator<ProductionPrerequisite::PrereqUnitRec> >::_M_insert_overflow(
-		ProductionPrerequisite::PrereqUnitRec *, const ProductionPrerequisite::PrereqUnitRec &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0D4h
-			_emit 053h
-			_emit 057h
-			_emit 000h
-		}
-	}
+class ProductionPrerequisitePrereqUnitRecInsertOverflowShim
+{
+public:
+	void insert_overflow(ProductionPrerequisite::PrereqUnitRec *pos, const ProductionPrerequisite::PrereqUnitRec &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<ProductionPrerequisite::PrereqUnitRec, allocator<ProductionPrerequisite::PrereqUnitRec> >::_M_insert_overflow(
+	ProductionPrerequisite::PrereqUnitRec *pos, const ProductionPrerequisite::PrereqUnitRec &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((ProductionPrerequisitePrereqUnitRecInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
