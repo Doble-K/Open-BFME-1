06671605f6fd7f87b35bb61dd7bf679699b18141
dginovker <danielginovker@gmail.com>
2026-08-02 01:48:52 -0700

    Open-BFME5 convert STL _Rb_tree destructor thunks

--- Code/Libraries/Source/WWVegas/WWLib/ThingTemplateRbTreeDestructorThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ThingTemplateRbTreeDestructorThunk.cpp
@@ -10,63 +10,63 @@ struct UnicodeStringLessThan
 
 namespace _STL
 {
-	template <class First, class Second>
-	struct pair
-	{
-	};
+template <class First, class Second>
+struct pair
+{
+};
 
-	template <class Pair>
-	struct _Select1st
-	{
-	};
+template <class Pair>
+struct _Select1st
+{
+};
 
-	template <class Type>
-	struct less
-	{
-	};
+template <class Type>
+struct less
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Key, class Value, class Select, class Compare, class Allocator>
-	class _Rb_tree
-	{
-	public:
-		~_Rb_tree();
-	};
+template <class Key, class Value, class Select, class Compare, class Allocator>
+class _Rb_tree
+{
+public:
+	~_Rb_tree();
+};
 
-	__declspec(naked) _Rb_tree<ThingTemplate const *, pair<ThingTemplate const * const, int>, _Select1st<pair<ThingTemplate const * const, int> >, less<ThingTemplate const *>, allocator<pair<ThingTemplate const * const, int> > >::~_Rb_tree()
-	{
-		__asm {
-			_emit 0E9h
-			_emit 01Eh
-			_emit 0C0h
-			_emit 00Ah
-			_emit 000h
-		}
-	}
+class ThingTemplateRbTreeDestructorShim
+{
+public:
+	void destroy();
+};
 
-	__declspec(naked) _Rb_tree<AsciiString, pair<AsciiString const, bool>, _Select1st<pair<AsciiString const, bool> >, less<AsciiString>, allocator<pair<AsciiString const, bool> > >::~_Rb_tree()
-	{
-		__asm {
-			_emit 0E9h
-			_emit 074h
-			_emit 03Fh
-			_emit 006h
-			_emit 000h
-		}
-	}
+class AsciiStringRbTreeDestructorShim
+{
+public:
+	void destroy();
+};
 
-	__declspec(naked) _Rb_tree<UnicodeString, pair<UnicodeString const, bool>, _Select1st<pair<UnicodeString const, bool> >, UnicodeStringLessThan, allocator<pair<UnicodeString const, bool> > >::~_Rb_tree()
-	{
-		__asm {
-			_emit 0E9h
-			_emit 09Fh
-			_emit 020h
-			_emit 041h
-			_emit 000h
-		}
-	}
+class UnicodeStringRbTreeDestructorShim
+{
+public:
+	void destroy();
+};
+
+_Rb_tree<ThingTemplate const *, pair<ThingTemplate const *const, int>, _Select1st<pair<ThingTemplate const *const, int> >, less<ThingTemplate const *>, allocator<pair<ThingTemplate const *const, int> > >::~_Rb_tree()
+{
+	((ThingTemplateRbTreeDestructorShim *)this)->destroy();
+}
+
+_Rb_tree<AsciiString, pair<AsciiString const, bool>, _Select1st<pair<AsciiString const, bool> >, less<AsciiString>, allocator<pair<AsciiString const, bool> > >::~_Rb_tree()
+{
+	((AsciiStringRbTreeDestructorShim *)this)->destroy();
+}
+
+_Rb_tree<UnicodeString, pair<UnicodeString const, bool>, _Select1st<pair<UnicodeString const, bool> >, UnicodeStringLessThan, allocator<pair<UnicodeString const, bool> > >::~_Rb_tree()
+{
+	((UnicodeStringRbTreeDestructorShim *)this)->destroy();
+}
 }
