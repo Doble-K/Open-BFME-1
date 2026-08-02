49e441dbbcc3ecad32fbcc3d49dbebcd13e1e922
dginovker <danielginovker@gmail.com>
2026-08-02 01:26:17 -0700

    Open-BFME5 convert STL deque PushBackAuxValueThunks.cpp thunk

--- Code/Libraries/Source/WWVegas/WWLib/DequePushBackAuxValueThunks.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/DequePushBackAuxValueThunks.cpp
@@ -2,44 +2,30 @@ class GameResultsRequest
 {
 };
 
-class GameResultsResponse
-{
-};
-
-class PingRequest
-{
-};
-
-class PingResponse
-{
-};
-
 namespace _STL
 {
-	template<class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template<class Type, class Allocator>
-	class deque
-	{
-	protected:
-		void _M_push_back_aux_v(Type const &);
-	};
+template <class Type, class Allocator>
+class deque
+{
+protected:
+	void _M_push_back_aux_v(Type const &);
+};
 
-	template<class Type, class Allocator>
-	__declspec(naked) void deque<Type, Allocator>::_M_push_back_aux_v(Type const &)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 05Dh
-			_emit 0FFh
-			_emit 061h
-			_emit 000h
-		}
-	}
+class DequePushBackAuxValueThunksShim
+{
+public:
+	void push_back_aux(void const *x);
+};
 
-	template __declspec(naked) void deque<GameResultsRequest, allocator<GameResultsRequest> >::_M_push_back_aux_v(const GameResultsRequest &);
-
+template <class Type, class Allocator>
+void deque<Type, Allocator>::_M_push_back_aux_v(Type const &x)
+{
+	((DequePushBackAuxValueThunksShim *)this)->push_back_aux((void const *)&x);
+}
+template void deque<GameResultsRequest, allocator<GameResultsRequest> >::_M_push_back_aux_v(GameResultsRequest const &);
 }
