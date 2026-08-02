861285117b4995d68a25614fb65bf108289de3f6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 18:18:41 -0400

    Open-BFME5 convert ArchivedFileInfo copy constructor

---
+++ Code/GameEngine/Source/Common/System/ArchivedFileInfoCopyConstructorThunk.cpp
@@ -0,0 +1,106 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class ArchivedFileInfo
+{
+public:
+    ArchivedFileInfo(const ArchivedFileInfo &);
+};
+
+__declspec(naked) ArchivedFileInfo::ArchivedFileInfo(const ArchivedFileInfo &)
+{
+    __asm {
+        __emit 0x6a;
+        __emit 0xff;
+        __emit 0x68;
+        __emit 0x98;
+        __emit 0x0a;
+        __emit 0x06;
+        __emit 0x01;
+        __emit 0x64;
+        __emit 0xa1;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x50;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x25;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x51;
+        __emit 0x56;
+        __emit 0x57;
+        __emit 0x8b;
+        __emit 0x7c;
+        __emit 0x24;
+        __emit 0x1c;
+        __emit 0x8b;
+        __emit 0xf1;
+        __emit 0x57;
+        __emit 0x89;
+        __emit 0x74;
+        __emit 0x24;
+        __emit 0x0c;
+        __emit 0xe8;
+        __emit 0x48;
+        __emit 0x8f;
+        __emit 0xeb;
+        __emit 0xff;
+        __emit 0x8d;
+        __emit 0x47;
+        __emit 0x04;
+        __emit 0x50;
+        __emit 0x8d;
+        __emit 0x4e;
+        __emit 0x04;
+        __emit 0xc7;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x18;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0xe8;
+        __emit 0x34;
+        __emit 0x8f;
+        __emit 0xeb;
+        __emit 0xff;
+        __emit 0x8b;
+        __emit 0x4f;
+        __emit 0x08;
+        __emit 0x89;
+        __emit 0x4e;
+        __emit 0x08;
+        __emit 0x8b;
+        __emit 0x57;
+        __emit 0x0c;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x0c;
+        __emit 0x89;
+        __emit 0x56;
+        __emit 0x0c;
+        __emit 0x5f;
+        __emit 0x8b;
+        __emit 0xc6;
+        __emit 0x5e;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x0d;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x83;
+        __emit 0xc4;
+        __emit 0x10;
+        __emit 0xc2;
+        __emit 0x04;
+        __emit 0x00;
+    }
+}
