fb7a37bfeb7a36ccb25809d8010d955d0e00969e
dginovker <danielginovker@gmail.com>
2026-08-01 17:37:23 -0700

    Luna Open-BFME5.0 convert AttributeModifier upgrade

---
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/AttributeModifierUpgrade.cpp
@@ -0,0 +1,55 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class BehaviorModule
+{
+public:
+	virtual void behaviorModuleAnchor();
+
+private:
+	unsigned char m_data[8];
+};
+
+class BehaviorModuleInterface
+{
+public:
+	virtual void behaviorModuleInterfaceAnchor();
+};
+
+class UpgradeMux
+{
+public:
+	virtual void upgradeMuxAnchor();
+
+private:
+	bool m_upgradeExecuted;
+};
+
+class ModuleInterface
+{
+public:
+	virtual void moduleInterfaceAnchor();
+};
+
+class UpgradeModule : public BehaviorModule,
+	public BehaviorModuleInterface,
+	public UpgradeMux,
+	public ModuleInterface
+{
+public:
+	UpgradeModule( Thing *thing, const ModuleData *moduleData );
+};
+
+class AttributeModifierUpgrade : public UpgradeModule
+{
+public:
+	AttributeModifierUpgrade( Thing *thing, const ModuleData *moduleData );
+};
+
+AttributeModifierUpgrade::AttributeModifierUpgrade(
+	Thing *thing, const ModuleData *moduleData )
+	: UpgradeModule( thing, moduleData )
+{
+}
