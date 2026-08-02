96f9f150848db09a8b0493ed56d637bf650de700
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 09:34:34 -0400

    Open-BFME5 convert AODHordeContain module data factory

---
+++ Code/GameEngine/Source/GameLogic/AI/AODHordeContainFriendNewModuleDataThunk.cpp
@@ -0,0 +1,133 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class INI;
+class ModuleData;
+
+class AODHordeContain
+{
+public:
+    static ModuleData *friend_newModuleData(INI *ini);
+};
+
+// ?friend_newModuleData@AODHordeContain@@SAPAVModuleData@@PAVINI@@@Z
+__declspec(naked) ModuleData *AODHordeContain::friend_newModuleData(INI *)
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0x0b
+        __emit 0x13
+        __emit 0x00
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x51
+        __emit 0x56
+        __emit 0x68
+        __emit 0xf4
+        __emit 0x02
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x3f
+        __emit 0xef
+        __emit 0x75
+        __emit 0x00
+        __emit 0x8b
+        __emit 0xf0
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x04
+        __emit 0x89
+        __emit 0x74
+        __emit 0x24
+        __emit 0x04
+        __emit 0x85
+        __emit 0xf6
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x74
+        __emit 0x0f
+        __emit 0x8b
+        __emit 0xce
+        __emit 0xe8
+        __emit 0x6a
+        __emit 0xbd
+        __emit 0xee
+        __emit 0xff
+        __emit 0xc7
+        __emit 0x06
+        __emit 0x60
+        __emit 0x99
+        __emit 0x08
+        __emit 0x01
+        __emit 0xeb
+        __emit 0x02
+        __emit 0x33
+        __emit 0xf6
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x18
+        __emit 0x85
+        __emit 0xc9
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0x74
+        __emit 0x0b
+        __emit 0x68
+        __emit 0xf6
+        __emit 0x34
+        __emit 0x43
+        __emit 0x00
+        __emit 0x56
+        __emit 0xe8
+        __emit 0xfe
+        __emit 0xf0
+        __emit 0x72
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x08
+        __emit 0x8b
+        __emit 0xc6
+        __emit 0x5e
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc3
+    }
+}
