50163104c5c8ae409a1524d897b9a96a939686ac
dginovker <danielginovker@gmail.com>
2026-08-01 19:12:13 -0700

    Luna Open-BFME5.0 convert default draw module template copy constructor

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -46,6 +46,11 @@ public:
     void construct();
 };
 
+class DefaultModuleTemplate6CopyCtorShim {
+public:
+    void construct(const void *source);
+};
+
 class DefaultModuleTemplate7CtorShim {
 public:
     void construct();
@@ -9212,49 +9217,12 @@ __declspec(naked) DefaultModule<3> *ConcreteModuleTemplate<DefaultModuleTag<3> >
 }
 
 // ??0?$ConcreteModuleTemplate@V?$DefaultModuleTag@$05@FXParticleSystem@@@FXParticleSystem@@QAE@ABV01@@Z
-__declspec(naked) ConcreteModuleTemplate<DefaultModuleTag<6> >::ConcreteModuleTemplate(const ConcreteModuleTemplate<DefaultModuleTag<6> > &that)
+ConcreteModuleTemplate<DefaultModuleTag<6> >::ConcreteModuleTemplate(const ConcreteModuleTemplate<DefaultModuleTag<6> > &that)
 {
-    __asm {
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x04
-        __emit 0x56
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0xe8
-        __emit 0xed
-        __emit 0x72
-        __emit 0xa6
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x0c
-        __emit 0x0e
-        __emit 0x11
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x04
-        __emit 0x08
-        __emit 0x0e
-        __emit 0x11
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x08
-        __emit 0xf4
-        __emit 0x0d
-        __emit 0x11
-        __emit 0x01
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-    }
+    ((DefaultModuleTemplate6CopyCtorShim *)this)->construct(&that);
+    *(volatile unsigned int *)this = 0x01110e0c;
+    *(volatile unsigned int *)((unsigned char *)this + 4) = 0x01110e08;
+    *(volatile unsigned int *)((unsigned char *)this + 8) = 0x01110df4;
 }
 
 // ??0?$ConcreteModuleTemplate@V?$DefaultModuleTag@$05@FXParticleSystem@@@FXParticleSystem@@QAE@XZ
