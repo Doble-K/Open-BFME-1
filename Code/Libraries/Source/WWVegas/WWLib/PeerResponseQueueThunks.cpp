b46b69d651be49767a94d0f0ade454e82f8f0f1f
dginovker <danielginovker@gmail.com>
2026-08-02 01:28:29 -0700

    Open-BFME5 convert PeerResponse deque/queue pop/push thunks

--- Code/Libraries/Source/WWVegas/WWLib/PeerResponseQueueThunks.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/PeerResponseQueueThunks.cpp
@@ -4,49 +4,49 @@ class PeerResponse
 
 namespace _STL
 {
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class deque
-	{
-	public:
-		void pop_front();
-	};
+template <class Type, class Allocator>
+class deque
+{
+public:
+	void pop_front();
+};
 
-	template <class Type, class Allocator>
-	__declspec(naked) void deque<Type, Allocator>::pop_front()
-	{
-		__asm {
-			_emit 0E9h
-			_emit 097h
-			_emit 0E3h
-			_emit 061h
-			_emit 000h
-		}
-	}
+template <class Type, class Container>
+class queue
+{
+public:
+	void push(Type const &);
+};
 
-	template <class Type, class Container>
-	class queue
-	{
-	public:
-		void push(Type const &);
-	};
+class PeerResponseDequePopFrontShim
+{
+public:
+	void pop_front();
+};
 
-	template <class Type, class Container>
-	__declspec(naked) void queue<Type, Container>::push(Type const &)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 024h
-			_emit 0FDh
-			_emit 060h
-			_emit 000h
-		}
-	}
+class PeerResponseQueuePushShim
+{
+public:
+	void push(void const *x);
+};
 
-	template __declspec(naked) void deque<PeerResponse, allocator<PeerResponse> >::pop_front();
-	template __declspec(naked) void queue<PeerResponse, deque<PeerResponse, allocator<PeerResponse> > >::push(PeerResponse const &);
+template <class Type, class Allocator>
+void deque<Type, Allocator>::pop_front()
+{
+	((PeerResponseDequePopFrontShim *)this)->pop_front();
+}
+
+template <class Type, class Container>
+void queue<Type, Container>::push(Type const &x)
+{
+	((PeerResponseQueuePushShim *)this)->push((void const *)&x);
+}
+
+template void deque<PeerResponse, allocator<PeerResponse> >::pop_front();
+template void queue<PeerResponse, deque<PeerResponse, allocator<PeerResponse> > >::push(PeerResponse const &);
 }
