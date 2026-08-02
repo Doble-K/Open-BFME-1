ebae2248fb2c99615c2343562a8ceede7c435cca
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 19:44:29 -0400

    Open-BFME5 convert DynamicShroud module data constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/DynamicShroudClearingRangeUpdateModuleDataCtorThunk.cpp
@@ -0,0 +1,106 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class DynamicShroudClearingRangeUpdateModuleData
+{
+public:
+    DynamicShroudClearingRangeUpdateModuleData();
+};
+
+__declspec(naked) DynamicShroudClearingRangeUpdateModuleData::DynamicShroudClearingRangeUpdateModuleData()
+{
+    __asm {
+        __emit 0x6a;
+        __emit 0xff;
+        __emit 0x68;
+        __emit 0xe8;
+        __emit 0x16;
+        __emit 0x01;
+        __emit 0x01;
+        __emit 0x64;
+        __emit 0xa1;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x50;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x25;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x51;
+        __emit 0x53;
+        __emit 0x56;
+        __emit 0x8b;
+        __emit 0xf1;
+        __emit 0x89;
+        __emit 0x74;
+        __emit 0x24;
+        __emit 0x08;
+        __emit 0x33;
+        __emit 0xdb;
+        __emit 0x8d;
+        __emit 0x4e;
+        __emit 0x28;
+        __emit 0x89;
+        __emit 0x5c;
+        __emit 0x24;
+        __emit 0x14;
+        __emit 0xc7;
+        __emit 0x06;
+        __emit 0x00;
+        __emit 0xe1;
+        __emit 0x0b;
+        __emit 0x01;
+        __emit 0xe8;
+        __emit 0x4f;
+        __emit 0xb0;
+        __emit 0xd9;
+        __emit 0xff;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x0c;
+        __emit 0x89;
+        __emit 0x5e;
+        __emit 0x08;
+        __emit 0x89;
+        __emit 0x5e;
+        __emit 0x0c;
+        __emit 0x89;
+        __emit 0x5e;
+        __emit 0x10;
+        __emit 0x89;
+        __emit 0x5e;
+        __emit 0x14;
+        __emit 0x89;
+        __emit 0x5e;
+        __emit 0x18;
+        __emit 0x89;
+        __emit 0x5e;
+        __emit 0x1c;
+        __emit 0x89;
+        __emit 0x5e;
+        __emit 0x20;
+        __emit 0x88;
+        __emit 0x5e;
+        __emit 0x24;
+        __emit 0x8b;
+        __emit 0xc6;
+        __emit 0x5e;
+        __emit 0x5b;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x0d;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x83;
+        __emit 0xc4;
+        __emit 0x10;
+        __emit 0xc3;
+    }
+}
