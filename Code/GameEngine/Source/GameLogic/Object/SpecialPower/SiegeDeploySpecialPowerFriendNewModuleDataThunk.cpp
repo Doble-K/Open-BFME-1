6ce77973da78cb0bd44c78d9e4210d6415e765ac
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 11:53:56 -0400

    Open-BFME5 convert SiegeDeploySpecialPower module data factory

---
+++ Code/GameEngine/Source/GameLogic/Object/SpecialPower/SiegeDeploySpecialPowerFriendNewModuleDataThunk.cpp
@@ -0,0 +1,127 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class INI;
+class ModuleData;
+
+class SiegeDeploySpecialPower
+{
+public:
+    static ModuleData *friend_newModuleData(INI *);
+};
+
+// ?friend_newModuleData@SiegeDeploySpecialPower@@SAPAVModuleData@@PAVINI@@@Z
+__declspec(naked) ModuleData *SiegeDeploySpecialPower::friend_newModuleData(INI *)
+{
+    __asm {
+        _emit 06Ah
+        _emit 0FFh
+        _emit 068h
+        _emit 02Bh
+        _emit 00Eh
+        _emit 000h
+        _emit 001h
+        _emit 064h
+        _emit 0A1h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 050h
+        _emit 064h
+        _emit 089h
+        _emit 025h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 051h
+        _emit 056h
+        _emit 068h
+        _emit 0E4h
+        _emit 001h
+        _emit 000h
+        _emit 000h
+        _emit 0E8h
+        _emit 02Fh
+        _emit 012h
+        _emit 076h
+        _emit 000h
+        _emit 083h
+        _emit 0C4h
+        _emit 004h
+        _emit 089h
+        _emit 044h
+        _emit 024h
+        _emit 004h
+        _emit 085h
+        _emit 0C0h
+        _emit 0C7h
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 074h
+        _emit 00Bh
+        _emit 08Bh
+        _emit 0C8h
+        _emit 0E8h
+        _emit 0E2h
+        _emit 019h
+        _emit 0EEh
+        _emit 0FFh
+        _emit 08Bh
+        _emit 0F0h
+        _emit 0EBh
+        _emit 002h
+        _emit 033h
+        _emit 0F6h
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 018h
+        _emit 085h
+        _emit 0C9h
+        _emit 0C7h
+        _emit 044h
+        _emit 024h
+        _emit 010h
+        _emit 0FFh
+        _emit 0FFh
+        _emit 0FFh
+        _emit 0FFh
+        _emit 074h
+        _emit 00Bh
+        _emit 068h
+        _emit 03Dh
+        _emit 07Bh
+        _emit 042h
+        _emit 000h
+        _emit 056h
+        _emit 0E8h
+        _emit 0F4h
+        _emit 013h
+        _emit 073h
+        _emit 000h
+        _emit 08Bh
+        _emit 04Ch
+        _emit 024h
+        _emit 008h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 064h
+        _emit 089h
+        _emit 00Dh
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 000h
+        _emit 083h
+        _emit 0C4h
+        _emit 010h
+        _emit 0C3h
+    }
+}
