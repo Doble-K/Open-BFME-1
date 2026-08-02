c7321993746625e8d161b924ad50ce80715bcb57
dginovker <danielginovker@gmail.com>
2026-08-01 17:21:31 -0700

    Luna Open-BFME4 convert ManTheWalls special power

---
+++ Code/GameEngine/Source/GameLogic/Object/SpecialPower/ManTheWallsSpecialPower.cpp
@@ -0,0 +1,44 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class BehaviorModule
+{
+public:
+	virtual void behaviorModuleAnchor();
+
+private:
+	unsigned char m_data[8];
+};
+
+class SpecialPowerModuleInterface
+{
+public:
+	virtual void specialPowerModuleInterfaceAnchor();
+};
+
+class ModuleInterface
+{
+public:
+	virtual void moduleInterfaceAnchor();
+};
+
+class SpecialPowerModule : public BehaviorModule,
+	public SpecialPowerModuleInterface,
+	public ModuleInterface
+{
+public:
+	SpecialPowerModule( Thing *thing, const ModuleData *moduleData );
+};
+
+class ManTheWallsSpecialPower : public SpecialPowerModule
+{
+public:
+	ManTheWallsSpecialPower( Thing *thing, const ModuleData *moduleData );
+};
+
+ManTheWallsSpecialPower::ManTheWallsSpecialPower( Thing *thing, const ModuleData *moduleData )
+	: SpecialPowerModule( thing, moduleData )
+{
+}
