77c338e2e6b03cc220727bb7632d17c5af6b5eb0
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 02:24:28 -0400

    Open-BFME5 convert RenderObjectDrawModuleInfo DoXfer

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -17405,224 +17405,27 @@ void QuadDrawModuleInfo::DoXfer(Xfer &xfer)
 }
 
 // ?DoXfer@RenderObjectDrawModuleInfo@FXParticleSystem@@UAEXAAVXfer@@@Z
-__declspec(naked) void RenderObjectDrawModuleInfo::DoXfer(Xfer &xfer)
+void RenderObjectDrawModuleInfo::DoXfer(Xfer &xfer)
 {
-    __asm {
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xf9
-        __emit 0xb0
-        __emit 0x01
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x88
-        __emit 0x44
-        __emit 0x24
-        __emit 0x08
-        __emit 0x88
-        __emit 0x44
-        __emit 0x24
-        __emit 0x09
-        __emit 0x8b
-        __emit 0x06
-        __emit 0x51
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x50
-        __emit 0x28
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x0c
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x92
-        __emit 0x8c
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x10
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x52
-        __emit 0x68
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x14
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x52
-        __emit 0x78
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x18
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x52
-        __emit 0x6c
-        __emit 0x8d
-        __emit 0x4f
-        __emit 0x1c
-        __emit 0x51
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x47
-        __emit 0xf0
-        __emit 0xa4
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x08
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x20
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x52
-        __emit 0x68
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x24
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x52
-        __emit 0x78
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x28
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x52
-        __emit 0x6c
-        __emit 0x8d
-        __emit 0x4f
-        __emit 0x2c
-        __emit 0x51
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x19
-        __emit 0xf0
-        __emit 0xa4
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x08
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x30
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x52
-        __emit 0x68
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x34
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x52
-        __emit 0x78
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x38
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x52
-        __emit 0x6c
-        __emit 0x8d
-        __emit 0x4f
-        __emit 0x3c
-        __emit 0x51
-        __emit 0x56
-        __emit 0xe8
-        __emit 0xeb
-        __emit 0xef
-        __emit 0xa4
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x08
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x04
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x92
-        __emit 0x8c
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x16
-        __emit 0x83
-        __emit 0xc7
-        __emit 0x08
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xff
-        __emit 0x52
-        __emit 0x6c
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x59
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-    }
+    Xfer::Version v;
+    v.data[0] = 1;
+    v.data[1] = 1;
+    xfer == v;
+    xfer == *(bool *)((char *)this + 0x0c);
+    xfer == *(AsciiString *)((char *)this + 0x10);
+    xfer == *(int *)((char *)this + 0x14);
+    xfer == *(float *)((char *)this + 0x18);
+    xferRandomVariable(xfer, *(GameClientRandomVariable *)((char *)this + 0x1c));
+    xfer == *(AsciiString *)((char *)this + 0x20);
+    xfer == *(int *)((char *)this + 0x24);
+    xfer == *(float *)((char *)this + 0x28);
+    xferRandomVariable(xfer, *(GameClientRandomVariable *)((char *)this + 0x2c));
+    xfer == *(AsciiString *)((char *)this + 0x30);
+    xfer == *(int *)((char *)this + 0x34);
+    xfer == *(float *)((char *)this + 0x38);
+    xferRandomVariable(xfer, *(GameClientRandomVariable *)((char *)this + 0x3c));
+    xfer == *(bool *)((char *)this + 0x04);
+    xfer == *(float *)((char *)this + 0x08);
 }
 
 // ?DoXfer@RenderObjectUpdateModuleInfo@FXParticleSystem@@UAEXAAVXfer@@@Z
