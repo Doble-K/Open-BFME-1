685786a55197e6a5371d07d7e8a746d627c49cd8
dginovker <danielginovker@gmail.com>
2026-08-02 01:38:08 -0700

    Open-BFME5 convert bitset set and BitFlags testSetAndClear thunks

--- Code/Libraries/Source/WWVegas/WWLib/BitsetSetBitFlagsTestThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/BitsetSetBitFlagsTestThunk.cpp
@@ -1,44 +1,45 @@
 namespace _STL
 {
-	template<int Bits>
-	class bitset
-	{
-	public:
-		bitset<Bits> & set(unsigned int, int);
-	};
+template <int Bits>
+class bitset
+{
+public:
+	bitset<Bits> &set(unsigned int, int);
+};
 
-	template<int Bits>
-	__declspec(naked) bitset<Bits> & bitset<Bits>::set(unsigned int, int)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 03Eh
-			_emit 0FDh
-			_emit 00Ch
-			_emit 000h
-		}
-	}
+class Bitset67SetShim
+{
+public:
+	bitset<67> &set(unsigned int pos, int val);
+};
 
-	template __declspec(naked) bitset<67> & bitset<67>::set(unsigned int, int);
+template <int Bits>
+bitset<Bits> &bitset<Bits>::set(unsigned int pos, int val)
+{
+	return ((Bitset67SetShim *)this)->set(pos, val);
 }
 
-template<int Bits>
+template bitset<67> &bitset<67>::set(unsigned int, int);
+}
+
+template <int Bits>
 class BitFlags
 {
 public:
 	bool testSetAndClear(const BitFlags<Bits> &, const BitFlags<Bits> &) const;
 };
 
-template<int Bits>
-__declspec(naked) bool BitFlags<Bits>::testSetAndClear(const BitFlags<Bits> &, const BitFlags<Bits> &) const
+class BitFlags116TestSetAndClearShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 03Eh
-		_emit 0FDh
-		_emit 00Ch
-		_emit 000h
-	}
+public:
+	bool testSetAndClear(const BitFlags<116> &a, const BitFlags<116> &b) const;
+};
+
+template <int Bits>
+bool BitFlags<Bits>::testSetAndClear(const BitFlags<Bits> &a, const BitFlags<Bits> &b) const
+{
+	return ((BitFlags116TestSetAndClearShim const *)this)->testSetAndClear(
+		(const BitFlags<116> &)a, (const BitFlags<116> &)b);
 }
 
-template __declspec(naked) bool BitFlags<116>::testSetAndClear(const BitFlags<116> &, const BitFlags<116> &) const;
+template bool BitFlags<116>::testSetAndClear(const BitFlags<116> &, const BitFlags<116> &) const;
