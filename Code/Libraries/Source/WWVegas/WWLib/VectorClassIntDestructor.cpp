045c9852fdf5aadc8823eee4eb98542565d2271a
dginovker <danielginovker@gmail.com>
2026-08-01 17:47:47 -0700

    Luna Open-BFME4 convert VectorClass int destructor

---
+++ Code/Libraries/Source/WWVegas/WWLib/VectorClassIntDestructor.cpp
@@ -0,0 +1,30 @@
+// cl: /DNDEBUG /MD /EHsc
+
+template <class T>
+class VectorClass
+{
+public:
+	virtual ~VectorClass();
+
+private:
+	T *m_vector;
+	int m_vectorMax;
+	bool m_isValid;
+	bool m_isAllocated;
+	bool m_padding[2];
+};
+
+template <class T>
+VectorClass<T>::~VectorClass()
+{
+	if (m_vector != 0 && m_isAllocated)
+	{
+		delete[] m_vector;
+		m_vector = 0;
+	}
+
+	m_isAllocated = false;
+	m_vectorMax = 0;
+}
+
+template class VectorClass<int>;
