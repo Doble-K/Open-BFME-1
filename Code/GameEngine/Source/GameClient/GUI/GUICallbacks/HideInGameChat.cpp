df1866c5ada19f3928c7917f32210edf267fad19
dginovker <danielginovker@gmail.com>
2026-08-02 00:20:51 -0700

    Open-BFME5 convert HideInGameChat thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local static shim pinned to the real body.

--- Code/GameEngine/Source/GameClient/GUI/GUICallbacks/HideInGameChat.cpp
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/HideInGameChat.cpp
@@ -1,12 +1,14 @@
 // cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
 
-__declspec(naked) void HideInGameChat()
+void HideInGameChat();
+
+class HideInGameChatShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 0E8h
-		_emit 090h
-		_emit 04Fh
-		_emit 000h
-	}
+public:
+    static void run();
+};
+
+void HideInGameChat()
+{
+    HideInGameChatShim::run();
 }
