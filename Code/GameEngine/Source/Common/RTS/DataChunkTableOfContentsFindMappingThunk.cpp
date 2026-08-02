48b79082fd0977f0aa309ed56480fcb018b386dc
dginovker <danielginovker@gmail.com>
2026-08-02 00:34:26 -0700

    Open-BFME5 convert DataChunkTableOfContents findMapping thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local shim pinned to the real body.

--- Code/GameEngine/Source/Common/RTS/DataChunkTableOfContentsFindMappingThunk.cpp
+++ Code/GameEngine/Source/Common/RTS/DataChunkTableOfContentsFindMappingThunk.cpp
@@ -8,13 +8,13 @@ class DataChunkTableOfContents
     Mapping *findMapping(const AsciiString &);
 };
 
-__declspec(naked) Mapping *DataChunkTableOfContents::findMapping(const AsciiString &)
+class DataChunkTableOfContentsFindMappingShim
 {
-    __asm {
-        _emit 0E9h
-        _emit 021h
-        _emit 0E4h
-        _emit 00Eh
-        _emit 000h
-    }
+public:
+    Mapping *find(const AsciiString &name);
+};
+
+Mapping *DataChunkTableOfContents::findMapping(const AsciiString &name)
+{
+    return ((DataChunkTableOfContentsFindMappingShim *)this)->find(name);
 }
