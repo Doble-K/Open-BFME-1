21992c9d9c074224fc04457edbfaca43bebe82e2
dginovker <danielginovker@gmail.com>
2026-08-02 01:25:54 -0700

    Open-BFME5 convert STL deque PingResponse thunk

--- Code/Libraries/Source/WWVegas/WWLib/DequePingResponseThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/DequePingResponseThunk.cpp
@@ -4,23 +4,28 @@ class PingResponse
 
 namespace _STL
 {
-	template <class Type> class allocator { };
-	template <class Type, class Allocator>
-	class deque
-	{
-	protected:
-		void _M_push_back_aux_v(Type const &);
-	};
-	template <class Type, class Allocator>
-	__declspec(naked) void deque<Type, Allocator>::_M_push_back_aux_v(Type const &)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 02Bh
-			_emit 042h
-			_emit 062h
-			_emit 000h
-		}
-	}
-	template __declspec(naked) void deque<PingResponse, allocator<PingResponse> >::_M_push_back_aux_v(PingResponse const &);
+template <class Type>
+class allocator
+{
+};
+
+template <class Type, class Allocator>
+class deque
+{
+protected:
+	void _M_push_back_aux_v(Type const &);
+};
+
+class DequePingResponseThunkShim
+{
+public:
+	void push_back_aux(void const *x);
+};
+
+template <class Type, class Allocator>
+void deque<Type, Allocator>::_M_push_back_aux_v(Type const &x)
+{
+	((DequePingResponseThunkShim *)this)->push_back_aux((void const *)&x);
+}
+template void deque<PingResponse, allocator<PingResponse> >::_M_push_back_aux_v(PingResponse const &);
 }
