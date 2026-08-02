9cb6296ae1ec448e8943e1d91136a136cef9c985
dginovker <danielginovker@gmail.com>
2026-08-02 01:09:16 -0700

    Open-BFME5 convert STL _M_allocate_and_copy Object* thunk

--- Code/Libraries/Source/WWVegas/WWLib/ObjectPointerVectorAllocateAndCopyThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ObjectPointerVectorAllocateAndCopyThunk.cpp
@@ -4,33 +4,33 @@ class Object
 
 namespace _STL
 {
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		template <class Iterator>
-		Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
-	};
-
-	template <class Type, class Allocator>
+template <class Type, class Allocator>
+class vector
+{
+protected:
 	template <class Iterator>
-	__declspec(naked) Type *vector<Type, Allocator>::_M_allocate_and_copy(
-		unsigned int, Iterator, Iterator)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0C0h
-			_emit 0ACh
-			_emit 045h
-			_emit 000h
-		}
-	}
+	Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
+};
 
-	template Object **vector<Object *, allocator<Object *> >::_M_allocate_and_copy<Object * const *>(
-		unsigned int, Object * const *, Object * const *);
+class ObjectPointerAllocateAndCopyShim
+{
+public:
+	Object **allocate_and_copy(unsigned int n, Object *const *first, Object *const *last);
+};
+
+template <class Type, class Allocator>
+template <class Iterator>
+Type *vector<Type, Allocator>::_M_allocate_and_copy(unsigned int n, Iterator first, Iterator last)
+{
+	return (Type *)((ObjectPointerAllocateAndCopyShim *)this)->allocate_and_copy(
+		n, (Object *const *)first, (Object *const *)last);
+}
+
+template Object **vector<Object *, allocator<Object *> >::_M_allocate_and_copy<Object *const *>(
+	unsigned int, Object *const *, Object *const *);
 }
