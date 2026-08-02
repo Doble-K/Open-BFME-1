c53511c49fe7c5083f6271f90869a98be6e28956
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-01 20:36:31 -0400

    Open-BFME5 convert WeaponFireSpecialAbilityUpdateModuleData ctor

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/WeaponFireSpecialAbilityUpdateModuleDataCtorThunk.cpp
@@ -0,0 +1,64 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class WeaponFireSpecialAbilityUpdateModuleData
+{
+public:
+    WeaponFireSpecialAbilityUpdateModuleData();
+};
+
+__declspec(naked) WeaponFireSpecialAbilityUpdateModuleData::WeaponFireSpecialAbilityUpdateModuleData()
+{
+    __asm {
+        _emit 056h
+        _emit 08Bh
+        _emit 0F1h
+        _emit 0E8h
+        _emit 0CBh
+        _emit 01Eh
+        _emit 0DDh
+        _emit 0FFh
+        _emit 033h
+        _emit 0C0h
+        _emit 0C7h
+        _emit 006h
+        _emit 098h
+        _emit 08Dh
+        _emit 00Bh
+        _emit 001h
+        _emit 089h
+        _emit 086h
+        _emit 054h
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 058h
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 088h
+        _emit 086h
+        _emit 05Ch
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 089h
+        _emit 086h
+        _emit 060h
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 088h
+        _emit 086h
+        _emit 064h
+        _emit 002h
+        _emit 000h
+        _emit 000h
+        _emit 08Bh
+        _emit 0C6h
+        _emit 05Eh
+        _emit 0C3h
+    }
+}
+
