5b2ef5ea9efc0ee7fd7904e8bd151cf253f935bd
dginovker <danielginovker@gmail.com>
2026-08-02 01:41:07 -0700

    Open-BFME5 convert STL AsciiString tree lower/upper_bound thunks

--- Code/Libraries/Source/WWVegas/WWLib/STLAsciiStringTreeBoundsThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/STLAsciiStringTreeBoundsThunk.cpp
@@ -4,47 +4,58 @@ class AsciiString
 
 namespace _STL
 {
-template<class T> struct _Identity { };
-template<class T> struct less { };
-template<class T> class allocator { };
-template<class Value> struct _Rb_tree_node { };
+template <class T>
+struct _Identity
+{
+};
 
-template<class Key, class Value, class Select, class Less, class Alloc>
+template <class T>
+struct less
+{
+};
+
+template <class T>
+class allocator
+{
+};
+
+template <class Value>
+struct _Rb_tree_node
+{
+};
+
+template <class Key, class Value, class Select, class Less, class Alloc>
 class _Rb_tree
 {
 	_Rb_tree_node<Value> *_M_lower_bound(const Key &) const;
 	_Rb_tree_node<Value> *_M_upper_bound(const Key &) const;
 };
 
-template<class Key, class Value, class Select, class Less, class Alloc>
-__declspec(naked) _Rb_tree_node<Value> *_Rb_tree<Key, Value, Select, Less, Alloc>::_M_lower_bound(const Key &) const
+class AsciiStringTreeLowerBoundShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 0F5h
-		_emit 0BBh
-		_emit 062h
-		_emit 000h
-	}
+public:
+	_Rb_tree_node<AsciiString> *lower_bound(const AsciiString &k) const;
+};
+
+class AsciiStringTreeUpperBoundShim
+{
+public:
+	_Rb_tree_node<AsciiString> *upper_bound(const AsciiString &k) const;
+};
+
+template <class Key, class Value, class Select, class Less, class Alloc>
+_Rb_tree_node<Value> *_Rb_tree<Key, Value, Select, Less, Alloc>::_M_lower_bound(const Key &k) const
+{
+	return (_Rb_tree_node<Value> *)((AsciiStringTreeLowerBoundShim const *)this)->lower_bound((const AsciiString &)k);
+}
+
+template <class Key, class Value, class Select, class Less, class Alloc>
+_Rb_tree_node<Value> *_Rb_tree<Key, Value, Select, Less, Alloc>::_M_upper_bound(const Key &k) const
+{
+	return (_Rb_tree_node<Value> *)((AsciiStringTreeUpperBoundShim const *)this)->upper_bound((const AsciiString &)k);
 }
 }
 
 typedef _STL::_Rb_tree<AsciiString, AsciiString, _STL::_Identity<AsciiString>, _STL::less<AsciiString>, _STL::allocator<AsciiString> > AsciiTree;
-template __declspec(naked) _STL::_Rb_tree_node<AsciiString> *AsciiTree::_M_lower_bound(const AsciiString &) const;
-
-namespace _STL
-{
-template<class Key, class Value, class Select, class Less, class Alloc>
-__declspec(naked) _Rb_tree_node<Value> *_Rb_tree<Key, Value, Select, Less, Alloc>::_M_upper_bound(const Key &) const
-{
-	__asm {
-		_emit 0E9h
-		_emit 0EDh
-		_emit 079h
-		_emit 006h
-		_emit 000h
-	}
-}
-}
-
-template __declspec(naked) _STL::_Rb_tree_node<AsciiString> *AsciiTree::_M_upper_bound(const AsciiString &) const;
+template _STL::_Rb_tree_node<AsciiString> *AsciiTree::_M_lower_bound(const AsciiString &) const;
+template _STL::_Rb_tree_node<AsciiString> *AsciiTree::_M_upper_bound(const AsciiString &) const;
