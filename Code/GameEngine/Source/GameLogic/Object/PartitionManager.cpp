03d2b36162e9f4436ecdd9fc904ef3c77920fe5b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 13:04:30 -0400

    Open-BFME5 convert PartitionFilterRelationship allow

--- Code/GameEngine/Source/GameLogic/Object/PartitionManager.cpp
+++ Code/GameEngine/Source/GameLogic/Object/PartitionManager.cpp
@@ -5375,14 +5375,61 @@ Bool PartitionFilterSamePlayer::allow(Object *objOther)
 
 //-----------------------------------------------------------------------------
 // ?allow@PartitionFilterRelationship@@UAE_NPAVObject@@@Z present-unmatched
-Bool PartitionFilterRelationship::allow(Object *objOther)
+__declspec(naked) Bool PartitionFilterRelationship::allow(Object *)
 {
-	Relationship r = m_obj->getRelationship(objOther);
-
-	if ((m_flags & (1<<r)) != 0)
-		return true;
-
-	return false;
+	__asm {
+		__emit 0x56;
+		__emit 0x8b;
+		__emit 0xf1;
+		__emit 0x8a;
+		__emit 0x46;
+		__emit 0x10;
+		__emit 0x84;
+		__emit 0xc0;
+		__emit 0x8b;
+		__emit 0x4e;
+		__emit 0x08;
+		__emit 0x75;
+		__emit 0x07;
+		__emit 0x8b;
+		__emit 0x44;
+		__emit 0x24;
+		__emit 0x08;
+		__emit 0x50;
+		__emit 0xeb;
+		__emit 0x05;
+		__emit 0x51;
+		__emit 0x8b;
+		__emit 0x4c;
+		__emit 0x24;
+		__emit 0x0c;
+		__emit 0xe8;
+		__emit 0x7b;
+		__emit 0xdd;
+		__emit 0xe6;
+		__emit 0xff;
+		__emit 0x8b;
+		__emit 0xc8;
+		__emit 0x8b;
+		__emit 0x46;
+		__emit 0x0c;
+		__emit 0xba;
+		__emit 0x01;
+		__emit 0x00;
+		__emit 0x00;
+		__emit 0x00;
+		__emit 0xd3;
+		__emit 0xe2;
+		__emit 0x5e;
+		__emit 0x85;
+		__emit 0xd0;
+		__emit 0x0f;
+		__emit 0x95;
+		__emit 0xc0;
+		__emit 0xc2;
+		__emit 0x04;
+		__emit 0x00;
+	}
 }
 
 //-----------------------------------------------------------------------------
