46a7ca93f00cf8d3945cd470c237ee393404d2ca
dginovker <danielginovker@gmail.com>
2026-08-01 17:51:36 -0700

    Luna Open-BFME5.0 convert CreateObjectDie constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Die/CreateObjectDieConstructor.cpp
@@ -0,0 +1,48 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class ObjectModule
+{
+public:
+	virtual void objectModuleAnchor();
+	ObjectModule( Thing *thing, const ModuleData *moduleData );
+
+private:
+	unsigned char m_data[8];
+};
+
+class BehaviorModuleInterface
+{
+public:
+	virtual void behaviorModuleInterfaceAnchor();
+};
+
+class DieModuleInterface
+{
+public:
+	virtual void dieModuleInterfaceAnchor();
+};
+
+class DieModule : public ObjectModule,
+	public BehaviorModuleInterface,
+	public DieModuleInterface
+{
+public:
+	DieModule( Thing *thing, const ModuleData *moduleData )
+		: ObjectModule( thing, moduleData )
+	{
+	}
+};
+
+class CreateObjectDie : public DieModule
+{
+public:
+	CreateObjectDie( Thing *thing, const ModuleData *moduleData );
+};
+
+CreateObjectDie::CreateObjectDie( Thing *thing, const ModuleData *moduleData )
+	: DieModule( thing, moduleData )
+{
+}
