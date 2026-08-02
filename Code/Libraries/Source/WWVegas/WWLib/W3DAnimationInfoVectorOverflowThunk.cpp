6bfced8bf6482c22d92e99ab78d869cf35307050
dginovker <danielginovker@gmail.com>
2026-08-02 01:14:38 -0700

    Open-BFME5 convert STL _M_insert_overflow W3DAnimationInfo thunk

--- Code/Libraries/Source/WWVegas/WWLib/W3DAnimationInfoVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/W3DAnimationInfoVectorOverflowThunk.cpp
@@ -4,31 +4,31 @@ class W3DAnimationInfo
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+};
 
-	__declspec(naked) void vector<W3DAnimationInfo, allocator<W3DAnimationInfo> >::_M_insert_overflow(
-		W3DAnimationInfo *, const W3DAnimationInfo &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 007h
-			_emit 010h
-			_emit 075h
-			_emit 000h
-		}
-	}
+class W3DAnimationInfoInsertOverflowShim
+{
+public:
+	void insert_overflow(W3DAnimationInfo * pos, const W3DAnimationInfo & x, const __false_type &tag, unsigned int fill_len, bool at_end);
+};
+
+void vector<W3DAnimationInfo, allocator<W3DAnimationInfo> >::_M_insert_overflow(
+	W3DAnimationInfo * pos, const W3DAnimationInfo & x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((W3DAnimationInfoInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
 }
