b745c7870e6cb8a2ba819707713233c8693ad960
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 00:58:49 -0400

    Open-BFME5 convert TerrainCollisionModuleTemplate assignment

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -17384,100 +17384,24 @@ TerrainCollisionModuleInfo &TerrainCollisionModuleInfo::operator=(const TerrainC
 }
 
 // ??4TerrainCollisionModuleTemplate@FXParticleSystem@@QAEAAV01@ABV01@@Z
-__declspec(naked) TerrainCollisionModuleTemplate &TerrainCollisionModuleTemplate::operator=(const TerrainCollisionModuleTemplate &that)
+TerrainCollisionModuleTemplate &TerrainCollisionModuleTemplate::operator=(const TerrainCollisionModuleTemplate &that)
 {
-    __asm {
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x56
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x74
-        __emit 0x05
-        __emit 0x8d
-        __emit 0x48
-        __emit 0x08
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xc9
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x66
-        __emit 0x8b
-        __emit 0x09
-        __emit 0x66
-        __emit 0x89
-        __emit 0x4e
-        __emit 0x08
-        __emit 0x74
-        __emit 0x05
-        __emit 0x8d
-        __emit 0x78
-        __emit 0x0c
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xff
-        __emit 0x8d
-        __emit 0x57
-        __emit 0x04
-        __emit 0x52
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x10
-        __emit 0xe8
-        __emit 0xbf
-        __emit 0xe3
-        __emit 0x2a
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x08
-        __emit 0x8b
-        __emit 0x10
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x14
-        __emit 0x89
-        __emit 0x11
-        __emit 0x8b
-        __emit 0x50
-        __emit 0x04
-        __emit 0x89
-        __emit 0x51
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x40
-        __emit 0x08
-        __emit 0x89
-        __emit 0x41
-        __emit 0x08
-        __emit 0x8a
-        __emit 0x4f
-        __emit 0x14
-        __emit 0x88
-        __emit 0x4e
-        __emit 0x20
-        __emit 0x8b
-        __emit 0x57
-        __emit 0x18
-        __emit 0x5f
-        __emit 0x89
-        __emit 0x56
-        __emit 0x24
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-    }
+    const void *src = &that;
+    unsigned char *destination = (unsigned char *)this;
+    const unsigned char *base = src ? (const unsigned char *)src + 8 : 0;
+    *(unsigned short *)(destination + 8) = *(const unsigned short *)base;
+
+    const unsigned char *info = src ? (const unsigned char *)src + 0x0c : 0;
+    ((LifeEventAsciiStringAssignShim *)(destination + 0x10))->assign(info + 4);
+    struct ThreeDwords {
+        unsigned int value0;
+        unsigned int value1;
+        unsigned int value2;
+    };
+    *(ThreeDwords *)(destination + 0x14) = *(const ThreeDwords *)(info + 8);
+    *(unsigned char *)(destination + 0x20) = *(const unsigned char *)(info + 0x14);
+    *(unsigned int *)(destination + 0x24) = *(const unsigned int *)(info + 0x18);
+    return *this;
 }
 
 // ??4WindModuleInfo@FXParticleSystem@@QAEAAV01@ABV01@@Z
