c1d44e72a2535016dd3d27dfa020dfd5e875e9e2
dginovker <danielginovker@gmail.com>
2026-08-02 01:08:37 -0700

    Open-BFME5 convert STL _M_allocate_and_copy HideShowSubObjInfo thunk

--- Code/Libraries/Source/WWVegas/WWLib/ModelConditionInfoHideShowSubObjInfoAllocateAndCopyThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ModelConditionInfoHideShowSubObjInfoAllocateAndCopyThunk.cpp
@@ -7,37 +7,39 @@ struct ModelConditionInfo
 
 namespace _STL
 {
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		template <class Iterator>
-		Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
-	};
-
-	template <class Type, class Allocator>
+template <class Type, class Allocator>
+class vector
+{
+protected:
 	template <class Iterator>
-	__declspec(naked) Type *vector<Type, Allocator>::_M_allocate_and_copy(
-		unsigned int, Iterator, Iterator)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 012h
-			_emit 03Fh
-			_emit 038h
-			_emit 000h
-		}
-	}
+	Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
+};
 
-	template ModelConditionInfo::HideShowSubObjInfo *vector<
-		ModelConditionInfo::HideShowSubObjInfo,
-		allocator<ModelConditionInfo::HideShowSubObjInfo>
-	>::_M_allocate_and_copy<ModelConditionInfo::HideShowSubObjInfo const *>(
-		unsigned int, ModelConditionInfo::HideShowSubObjInfo const *,
-		ModelConditionInfo::HideShowSubObjInfo const *);
+class HideShowSubObjInfoAllocateAndCopyShim
+{
+public:
+	ModelConditionInfo::HideShowSubObjInfo *allocate_and_copy(unsigned int n, const ModelConditionInfo::HideShowSubObjInfo *first, const ModelConditionInfo::HideShowSubObjInfo *last);
+};
+
+template <class Type, class Allocator>
+template <class Iterator>
+Type *vector<Type, Allocator>::_M_allocate_and_copy(unsigned int n, Iterator first, Iterator last)
+{
+	return (Type *)((HideShowSubObjInfoAllocateAndCopyShim *)this)->allocate_and_copy(
+		n,
+		(const ModelConditionInfo::HideShowSubObjInfo *)first,
+		(const ModelConditionInfo::HideShowSubObjInfo *)last);
+}
+
+template ModelConditionInfo::HideShowSubObjInfo *vector<
+	ModelConditionInfo::HideShowSubObjInfo,
+	allocator<ModelConditionInfo::HideShowSubObjInfo>
+>::_M_allocate_and_copy<ModelConditionInfo::HideShowSubObjInfo const *>(
+	unsigned int, ModelConditionInfo::HideShowSubObjInfo const *,
+	ModelConditionInfo::HideShowSubObjInfo const *);
 }
