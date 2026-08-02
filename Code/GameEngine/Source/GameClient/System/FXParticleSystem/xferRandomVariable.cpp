8191e3d3709397cc028d018ac4200eeefb5a2c5a
dginovker <danielginovker@gmail.com>
2026-08-02 00:45:46 -0700

    Open-BFME5 convert xferRandomVariable thunk
    Replace the 5-byte E9 incremental-link naked dump with clean C++ that
    forwards through a TU-local static shim pinned to the real body.

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/xferRandomVariable.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/xferRandomVariable.cpp
@@ -3,13 +3,15 @@
 class Xfer;
 class GameClientRandomVariable;
 
-__declspec(naked) void xferRandomVariable(Xfer &, GameClientRandomVariable &)
+void xferRandomVariable(Xfer &, GameClientRandomVariable &);
+
+class XferRandomVariableShim
 {
-	__asm {
-		_emit 0E9h
-		_emit 099h
-		_emit 0D6h
-		_emit 00Ch
-		_emit 000h
-	}
+public:
+    static void run(Xfer &xfer, GameClientRandomVariable &var);
+};
+
+void xferRandomVariable(Xfer &xfer, GameClientRandomVariable &var)
+{
+    XferRandomVariableShim::run(xfer, var);
 }
