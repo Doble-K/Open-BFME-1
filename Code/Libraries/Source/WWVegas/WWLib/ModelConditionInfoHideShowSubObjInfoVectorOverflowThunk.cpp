754fbad935c1922ac0247381d189ddec4e386df0
dginovker <danielginovker@gmail.com>
2026-08-02 01:16:31 -0700

    Open-BFME5 convert STL vector ModelConditionInfo_HideShowSubObjInfo overflow thunk

--- Code/Libraries/Source/WWVegas/WWLib/ModelConditionInfoHideShowSubObjInfoVectorOverflowThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/ModelConditionInfoHideShowSubObjInfoVectorOverflowThunk.cpp
@@ -7,43 +7,38 @@ struct ModelConditionInfo
 
 namespace _STL
 {
-	struct __false_type
-	{
-	};
+struct __false_type
+{
+};
 
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
-		void _M_clear();
-	};
+template <class Type, class Allocator>
+class vector
+{
+protected:
+	void _M_insert_overflow(Type *, const Type &, const __false_type &, unsigned int, bool);
+	void _M_clear();
+};
 
-	__declspec(naked) void vector<ModelConditionInfo::HideShowSubObjInfo, allocator<ModelConditionInfo::HideShowSubObjInfo> >::_M_insert_overflow(
-		ModelConditionInfo::HideShowSubObjInfo *, const ModelConditionInfo::HideShowSubObjInfo &, const __false_type &, unsigned int, bool)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0DBh
-			_emit 0EBh
-			_emit 067h
-			_emit 000h
-		}
-	}
+class ModelConditionInfoHideShowSubObjInfoInsertOverflowShim
+{
+public:
+	void insert_overflow(ModelConditionInfo::HideShowSubObjInfo *pos, const ModelConditionInfo::HideShowSubObjInfo &x, const __false_type &tag, unsigned int fill_len, bool at_end);
+	void clear();
+};
 
-	__declspec(naked) void vector<ModelConditionInfo::HideShowSubObjInfo, allocator<ModelConditionInfo::HideShowSubObjInfo> >::_M_clear()
-	{
-		__asm {
-			_emit 0E9h
-			_emit 0D3h
-			_emit 0A8h
-			_emit 039h
-			_emit 000h
-		}
-	}
+void vector<ModelConditionInfo::HideShowSubObjInfo, allocator<ModelConditionInfo::HideShowSubObjInfo> >::_M_insert_overflow(
+	ModelConditionInfo::HideShowSubObjInfo *pos, const ModelConditionInfo::HideShowSubObjInfo &x, const __false_type &tag, unsigned int fill_len, bool at_end)
+{
+	((ModelConditionInfoHideShowSubObjInfoInsertOverflowShim *)this)->insert_overflow(pos, x, tag, fill_len, at_end);
+}
+
+void vector<ModelConditionInfo::HideShowSubObjInfo, allocator<ModelConditionInfo::HideShowSubObjInfo> >::_M_clear()
+{
+	((ModelConditionInfoHideShowSubObjInfoInsertOverflowShim *)this)->clear();
+}
 }
