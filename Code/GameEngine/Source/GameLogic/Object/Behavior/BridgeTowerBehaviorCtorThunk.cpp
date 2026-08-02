cf95b208cf2c546c54b744fbdd5fac00d385b972
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 13:36:14 -0400

    Open-BFME5 convert BridgeTowerBehavior constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/BridgeTowerBehaviorCtorThunk.cpp
@@ -0,0 +1,112 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class BridgeTowerBehavior
+{
+public:
+    BridgeTowerBehavior(Thing *, const ModuleData *);
+};
+
+__declspec(naked) BridgeTowerBehavior::BridgeTowerBehavior(Thing *, const ModuleData *)
+{
+    __asm {
+        __emit 0x8b;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x08;
+        __emit 0x56;
+        __emit 0x8b;
+        __emit 0xf1;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x08;
+        __emit 0x50;
+        __emit 0x51;
+        __emit 0x8b;
+        __emit 0xce;
+        __emit 0xe8;
+        __emit 0xd0;
+        __emit 0x10;
+        __emit 0xe2;
+        __emit 0xff;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x0c;
+        __emit 0xd0;
+        __emit 0xc9;
+        __emit 0x09;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x10;
+        __emit 0x58;
+        __emit 0x2a;
+        __emit 0x0a;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x14;
+        __emit 0x7c;
+        __emit 0x25;
+        __emit 0x0a;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x18;
+        __emit 0xfc;
+        __emit 0x1b;
+        __emit 0x0a;
+        __emit 0x01;
+        __emit 0x33;
+        __emit 0xc0;
+        __emit 0x89;
+        __emit 0x46;
+        __emit 0x1c;
+        __emit 0x89;
+        __emit 0x46;
+        __emit 0x20;
+        __emit 0xc7;
+        __emit 0x06;
+        __emit 0x54;
+        __emit 0x2b;
+        __emit 0x0a;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x0c;
+        __emit 0x90;
+        __emit 0x2a;
+        __emit 0x0a;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x10;
+        __emit 0x7c;
+        __emit 0x2a;
+        __emit 0x0a;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x14;
+        __emit 0x78;
+        __emit 0x2a;
+        __emit 0x0a;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x18;
+        __emit 0x68;
+        __emit 0x2a;
+        __emit 0x0a;
+        __emit 0x01;
+        __emit 0x8b;
+        __emit 0xc6;
+        __emit 0x5e;
+        __emit 0xc2;
+        __emit 0x08;
+        __emit 0x00;
+    }
+}
