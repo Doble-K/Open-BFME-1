07a85c12c9a780985aa43843f55c79040dea2d7c
dginovker <danielginovker@gmail.com>
2026-08-02 01:10:08 -0700

    Open-BFME5 convert STL vector W3DAnimationInfo allocate/clear thunks

--- Code/Libraries/Source/WWVegas/WWLib/W3DAnimationInfoAllocateAndCopyThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/W3DAnimationInfoAllocateAndCopyThunk.cpp
@@ -4,46 +4,40 @@ class W3DAnimationInfo
 
 namespace _STL
 {
-	template <class Type>
-	class allocator
-	{
-	};
+template <class Type>
+class allocator
+{
+};
 
-	template <class Type, class Allocator>
-	class vector
-	{
-	protected:
-		template <class Iterator>
-		Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
-		void _M_clear();
-	};
-
-	template <class Type, class Allocator>
+template <class Type, class Allocator>
+class vector
+{
+protected:
 	template <class Iterator>
-	__declspec(naked) Type *vector<Type, Allocator>::_M_allocate_and_copy(
-		unsigned int, Iterator, Iterator)
-	{
-		__asm {
-			_emit 0E9h
-			_emit 036h
-			_emit 07Ch
-			_emit 037h
-			_emit 000h
-		}
-	}
+	Type *_M_allocate_and_copy(unsigned int, Iterator, Iterator);
+	void _M_clear();
+};
 
-	template W3DAnimationInfo *vector<W3DAnimationInfo,
-		allocator<W3DAnimationInfo> >::_M_allocate_and_copy<W3DAnimationInfo const *>(
-		unsigned int, W3DAnimationInfo const *, W3DAnimationInfo const *);
+class W3DAnimationInfoAllocateAndCopyShim
+{
+public:
+	W3DAnimationInfo *allocate_and_copy(unsigned int n, const W3DAnimationInfo *first, const W3DAnimationInfo *last);
+	void clear();
+};
 
-	__declspec(naked) void vector<W3DAnimationInfo, allocator<W3DAnimationInfo> >::_M_clear()
-	{
-		__asm {
-			_emit 0E9h
-			_emit 054h
-			_emit 086h
-			_emit 03Ah
-			_emit 000h
-		}
-	}
+template <class Type, class Allocator>
+template <class Iterator>
+Type *vector<Type, Allocator>::_M_allocate_and_copy(unsigned int n, Iterator first, Iterator last)
+{
+	return (Type *)((W3DAnimationInfoAllocateAndCopyShim *)this)->allocate_and_copy(
+		n, (const W3DAnimationInfo *)first, (const W3DAnimationInfo *)last);
+}
+
+template W3DAnimationInfo *vector<W3DAnimationInfo, allocator<W3DAnimationInfo> >::_M_allocate_and_copy<W3DAnimationInfo const *>(
+	unsigned int, W3DAnimationInfo const *, W3DAnimationInfo const *);
+
+void vector<W3DAnimationInfo, allocator<W3DAnimationInfo> >::_M_clear()
+{
+	((W3DAnimationInfoAllocateAndCopyShim *)this)->clear();
+}
 }
