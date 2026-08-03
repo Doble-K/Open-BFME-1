1077bc22b777d038e807f215af65811319f69ef0
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:59:35 -0400

    Open-BFME5 convert DefaultModuleTemplate00 copy ctor to clean C++
    Replace naked dump with base/sub copy shims (null-preserving +8) and dual outer vtbls; focused build byte-matches retail 62B.

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/DefaultModuleTemplate00CopyCtorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/DefaultModuleTemplate00CopyCtorThunk.cpp
@@ -1,82 +1,55 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: DefaultModuleTemplate (ledger $00 / N=1) copy ctor
+// Retail: base copy; null-preserving sub copy at +8; dual outer vtbl + sub vtbl.
 
 namespace FXParticleSystem
 {
-template<int Category>
+
+class DefaultModuleTemplate00BaseCopyShim
+{
+public:
+	void construct_from(const void *src);
+};
+
+class DefaultModuleTemplate00SubCopyShim
+{
+public:
+	void construct_from(const void *src_sub_or_null);
+	virtual void dummy();
+};
+
+extern "C" char DefaultModuleTemplate00_vtbl0;
+extern "C" char DefaultModuleTemplate00_vtbl4;
+extern "C" char DefaultModuleTemplate00_sub_vtbl;
+
+template <int N>
 class DefaultModuleTemplate
 {
 public:
-    DefaultModuleTemplate(const DefaultModuleTemplate<Category> &);
+	DefaultModuleTemplate(const DefaultModuleTemplate &);
+
+private:
+	void *m_v0;
+	void *m_v4;
+	char m_sub[4];
 };
 
-template<int Category>
-__declspec(naked) DefaultModuleTemplate<Category>::DefaultModuleTemplate(const DefaultModuleTemplate<Category> &)
+// MSVC mangles DefaultModuleTemplate<1> as @$00
+template <int N>
+DefaultModuleTemplate<N>::DefaultModuleTemplate(const DefaultModuleTemplate &that)
 {
-    __asm {
-        _emit 056h
-        _emit 057h
-        _emit 08Bh
-        _emit 07Ch
-        _emit 024h
-        _emit 00Ch
-        _emit 057h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 0F9h
-        _emit 0D6h
-        _emit 0A6h
-        _emit 0FFh
-        _emit 085h
-        _emit 0FFh
-        _emit 074h
-        _emit 005h
-        _emit 08Dh
-        _emit 047h
-        _emit 008h
-        _emit 0EBh
-        _emit 002h
-        _emit 033h
-        _emit 0C0h
-        _emit 08Dh
-        _emit 07Eh
-        _emit 008h
-        _emit 050h
-        _emit 08Bh
-        _emit 0CFh
-        _emit 0E8h
-        _emit 088h
-        _emit 08Eh
-        _emit 0A4h
-        _emit 0FFh
-        _emit 0C7h
-        _emit 007h
-        _emit 0CCh
-        _emit 008h
-        _emit 011h
-        _emit 001h
-        _emit 05Fh
-        _emit 0C7h
-        _emit 006h
-        _emit 0B8h
-        _emit 008h
-        _emit 011h
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 004h
-        _emit 0B4h
-        _emit 008h
-        _emit 011h
-        _emit 001h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-    }
+	const void *src = &that;
+	((DefaultModuleTemplate00BaseCopyShim *)this)->construct_from(src);
+	const void *sub_src = src ? (const char *)src + 8 : 0;
+	DefaultModuleTemplate00SubCopyShim *sub =
+		(DefaultModuleTemplate00SubCopyShim *)((char *)this + 8);
+	sub->construct_from(sub_src);
+	// Sub vtbl then outer dual vtbls (retail store order).
+	*(void **)sub = &DefaultModuleTemplate00_sub_vtbl;
+	m_v0 = &DefaultModuleTemplate00_vtbl0;
+	m_v4 = &DefaultModuleTemplate00_vtbl4;
 }
 
-template class DefaultModuleTemplate<1>;
+template DefaultModuleTemplate<1>::DefaultModuleTemplate(const DefaultModuleTemplate &);
 }
