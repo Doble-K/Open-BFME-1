7e408ca0b9cf47fbf110741da30a882b604969ce
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:24:43 -0400

    Open-BFME5 convert RenderObjectUpdate ConcreteModuleTemplate::clone to clean C++
    Replace naked factory with new+copy-ctor clone (size 0xa0); pin copy ctor; focused build byte-matches retail 34B.

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/RenderObjectUpdateConcreteModuleTemplateCloneThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/RenderObjectUpdateConcreteModuleTemplateCloneThunk.cpp
@@ -1,82 +1,41 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: RenderObjectUpdate ConcreteModuleTemplate::clone
+// Retail: new(0xa0); copy-ctor; return.
 
 namespace FXParticleSystem
 {
 
-extern const char RENDEROBJECT_UPDATE_MODULE_KEY[1];
-extern const char RENDEROBJECT_UPDATE_MODULE_NAME[1];
-
-template <int Category, const char (&Key)[1], const char (&Name)[1], class Module, class ModuleTemplate, class ParticleModule, class ParticleModuleTemplate>
-class ModuleTag
-{
-};
-
-class RenderObjectUpdateModule
+struct RenderObjectUpdateModuleTag
 {
 };
 
 class RenderObjectUpdateModuleTemplate
 {
+public:
+	virtual ~RenderObjectUpdateModuleTemplate();
 };
 
-class RenderObjectParticleUpdateModule
-{
-};
-
-class RenderObjectParticleUpdateModuleTemplate
-{
-};
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
 
 template <class Tag>
-class ConcreteModuleTemplate
-{
-};
+class ConcreteModuleTemplate;
 
 template <>
-class ConcreteModuleTemplate<ModuleTag<2, RENDEROBJECT_UPDATE_MODULE_KEY, RENDEROBJECT_UPDATE_MODULE_NAME, RenderObjectUpdateModule, RenderObjectUpdateModuleTemplate, RenderObjectParticleUpdateModule, RenderObjectParticleUpdateModuleTemplate> >
+class ConcreteModuleTemplate<RenderObjectUpdateModuleTag> : public RenderObjectUpdateModuleTemplate
 {
 public:
-    virtual RenderObjectUpdateModuleTemplate *clone() const;
+	ConcreteModuleTemplate(const ConcreteModuleTemplate &);
+	virtual RenderObjectUpdateModuleTemplate *clone() const;
+
+private:
+	unsigned char m_pad[0x9c];
 };
 
-__declspec(naked) RenderObjectUpdateModuleTemplate *ConcreteModuleTemplate<ModuleTag<2, RENDEROBJECT_UPDATE_MODULE_KEY, RENDEROBJECT_UPDATE_MODULE_NAME, RenderObjectUpdateModule, RenderObjectUpdateModuleTemplate, RenderObjectParticleUpdateModule, RenderObjectParticleUpdateModuleTemplate> >::clone() const
+RenderObjectUpdateModuleTemplate *ConcreteModuleTemplate<RenderObjectUpdateModuleTag>::clone() const
 {
-    __asm {
-        __emit 0x56
-        __emit 0x68
-        __emit 0xa0
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0xe8
-        __emit 0xf3
-        __emit 0x51
-        __emit 0x2a
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x0a
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xc8
-        __emit 0xe8
-        __emit 0x03
-        __emit 0x43
-        __emit 0xa3
-        __emit 0xff
-        __emit 0x5e
-        __emit 0xc3
-        __emit 0x33
-        __emit 0xc0
-        __emit 0x5e
-        __emit 0xc3
-    }
+	return new ConcreteModuleTemplate<RenderObjectUpdateModuleTag>(*this);
 }
 
 }
