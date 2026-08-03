6ef8fa7f311740875761fcf7bb7e0f5c889781f2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:41:15 -0400

    Open-BFME5 convert Die/Upgrade ModuleData destructors to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Behavior/DieModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/DieModuleDataDestructorThunk.cpp
@@ -1,101 +1,39 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift the exact retail MASM destructor into a C++ thunk.
+// Open-BFME5: lift ModuleData dtor MASM dump to clean C++.
+// Retail SEH dtor stores the derived vftable at entry, destroys two adjacent
+// 4-byte string members at this+0x1C then this+0x18 (reverse declaration
+// order), then stores the base vftable. Member names are inferred from the
+// byte evidence (8 base bytes + four ints + two strings).
 
-class __declspec(novtable) DieModuleData
+class AsciiString
+{
+	char *m_str;
+public:
+	~AsciiString();
+};
+
+class ModuleData
 {
 public:
-    virtual ~DieModuleData();
+	virtual ~ModuleData() {}
+	unsigned int m_04;
+};
+
+class DieModuleData : public ModuleData
+{
+public:
+	virtual ~DieModuleData();
+
+private:
+	int m_field08;
+	int m_field0c;
+	int m_field10;
+	int m_field14;
+	AsciiString m_deathName;
+	AsciiString m_deathSound;
 };
 
 // ??1DieModuleData@@UAE@XZ
-__declspec(naked) DieModuleData::~DieModuleData()
+DieModuleData::~DieModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xe3
-        __emit 0xc8
-        __emit 0xff
-        __emit 0x00
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x58
-        __emit 0x62
-        __emit 0x08
-        __emit 0x01
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x1c
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x5d
-        __emit 0x5a
-        __emit 0x78
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x18
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x50
-        __emit 0x5a
-        __emit 0x78
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xb0
-        __emit 0x61
-        __emit 0x08
-        __emit 0x01
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
