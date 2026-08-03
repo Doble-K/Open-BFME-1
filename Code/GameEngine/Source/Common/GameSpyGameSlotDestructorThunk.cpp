b115b7b2fa5ce325d7ad09a2977ea889dd63fb74
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 13:38:18 -0400

    Open-BFME5 convert GameSpyGameSlot destructor to clean C++

--- Code/GameEngine/Source/Common/GameSpyGameSlotDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/GameSpyGameSlotDestructorThunk.cpp
@@ -1,143 +1,41 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: lift GameSpyGameSlot dtor __emit thunk to clean C++.
+// Five member objects destroyed in reverse order (SEH states 4..0), then the
+// out-of-line GameSlot base dtor (pinned).
 
-class __declspec(novtable) GameSpyGameSlot
+class GGS_Member
+{
+public:
+    ~GGS_Member();
+
+private:
+    unsigned int m_dummy;
+};
+
+class GameSlot
+{
+public:
+    ~GameSlot();
+
+private:
+    unsigned char m_pad[0x48];
+};
+
+class GameSpyGameSlot : public GameSlot
 {
 public:
     ~GameSpyGameSlot();
+
+private:
+    GGS_Member m_a;
+    GGS_Member m_b;
+    GGS_Member m_c;
+    unsigned char m_pad[0x1C];
+    GGS_Member m_d;
+    GGS_Member m_e;
 };
 
 // ??1GameSpyGameSlot@@QAE@XZ
-__declspec(naked) GameSpyGameSlot::~GameSpyGameSlot()
+GameSpyGameSlot::~GameSpyGameSlot()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x54
-        __emit 0xcf
-        __emit 0x02
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x74
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x13
-        __emit 0xd9
-        __emit 0x38
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x70
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x03
-        __emit 0xe8
-        __emit 0x06
-        __emit 0xd9
-        __emit 0x38
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x50
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x02
-        __emit 0xe8
-        __emit 0xf9
-        __emit 0xd8
-        __emit 0x38
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x4c
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x01
-        __emit 0xe8
-        __emit 0xec
-        __emit 0xd8
-        __emit 0x38
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x48
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xdf
-        __emit 0xd8
-        __emit 0x38
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0x18
-        __emit 0x19
-        __emit 0xb1
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
