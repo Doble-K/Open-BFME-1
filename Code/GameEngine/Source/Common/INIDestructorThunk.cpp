096189354e79153ee4f3134094339981b814e20c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:19:31 -0400

    Open-BFME5 convert INI destructor to clean C++
    Dual staggered members at +0x04 and +0x834 with pinned dtor.

--- Code/GameEngine/Source/Common/INIDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/INIDestructorThunk.cpp
@@ -1,95 +1,34 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: INI dtor. members @+0x04 and @+0x834.
 
-class __declspec(novtable) INI
+class Buffer
 {
 public:
-    ~INI();
+	~Buffer();
+private:
+	unsigned char m_pad[4];
+};
+
+class INIMemberA
+{
+public:
+	~INIMemberA();
+private:
+	unsigned char m_pad[4];
+};
+
+class INI
+{
+public:
+	~INI();
+private:
+	unsigned char m_gap0[4];
+	Buffer m_a; // +0x04
+	unsigned char m_gap[0x82c]; // to +0x834
+	INIMemberA m_b; // +0x834
 };
 
 // ??1INI@@QAE@XZ
-__declspec(naked) INI::~INI()
+INI::~INI()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x4b
-        __emit 0x63
-        __emit 0x05
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x8e
-        __emit 0x34
-        __emit 0x08
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xd0
-        __emit 0xac
-        __emit 0x17
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0x20
-        __emit 0x66
-        __emit 0x03
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
