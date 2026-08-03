c8168350cdaec12c775adb54ae49fa68b6fbfb39
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:57:40 -0400

    Open-BFME5 convert INI::parseInGameUIDefinition to clean C++

--- Code/GameEngine/Source/GameClient/INIParseInGameUIDefinitionThunk.cpp
+++ Code/GameEngine/Source/GameClient/INIParseInGameUIDefinitionThunk.cpp
@@ -1,49 +1,118 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: INI::parseInGameUIDefinition
+// Null-check g_theInGameUI, virtual call slot 0x158, then thiscall on INI.
+
+class INI;
+
+class InGameUI
+{
+public:
+	virtual void *v00();
+	virtual void *v01();
+	virtual void *v02();
+	virtual void *v03();
+	virtual void *v04();
+	virtual void *v05();
+	virtual void *v06();
+	virtual void *v07();
+	virtual void *v08();
+	virtual void *v09();
+	virtual void *v10();
+	virtual void *v11();
+	virtual void *v12();
+	virtual void *v13();
+	virtual void *v14();
+	virtual void *v15();
+	virtual void *v16();
+	virtual void *v17();
+	virtual void *v18();
+	virtual void *v19();
+	virtual void *v20();
+	virtual void *v21();
+	virtual void *v22();
+	virtual void *v23();
+	virtual void *v24();
+	virtual void *v25();
+	virtual void *v26();
+	virtual void *v27();
+	virtual void *v28();
+	virtual void *v29();
+	virtual void *v30();
+	virtual void *v31();
+	virtual void *v32();
+	virtual void *v33();
+	virtual void *v34();
+	virtual void *v35();
+	virtual void *v36();
+	virtual void *v37();
+	virtual void *v38();
+	virtual void *v39();
+	virtual void *v40();
+	virtual void *v41();
+	virtual void *v42();
+	virtual void *v43();
+	virtual void *v44();
+	virtual void *v45();
+	virtual void *v46();
+	virtual void *v47();
+	virtual void *v48();
+	virtual void *v49();
+	virtual void *v50();
+	virtual void *v51();
+	virtual void *v52();
+	virtual void *v53();
+	virtual void *v54();
+	virtual void *v55();
+	virtual void *v56();
+	virtual void *v57();
+	virtual void *v58();
+	virtual void *v59();
+	virtual void *v60();
+	virtual void *v61();
+	virtual void *v62();
+	virtual void *v63();
+	virtual void *v64();
+	virtual void *v65();
+	virtual void *v66();
+	virtual void *v67();
+	virtual void *v68();
+	virtual void *v69();
+	virtual void *v70();
+	virtual void *v71();
+	virtual void *v72();
+	virtual void *v73();
+	virtual void *v74();
+	virtual void *v75();
+	virtual void *v76();
+	virtual void *v77();
+	virtual void *v78();
+	virtual void *v79();
+	virtual void *v80();
+	virtual void *v81();
+	virtual void *v82();
+	virtual void *v83();
+	virtual void *v84();
+	virtual void *v85();
+	virtual void *getParseTable();
+};
+
+extern InGameUI *g_theInGameUI;
 
 class INI
 {
 public:
-    static void parseInGameUIDefinition(INI *);
+	static void parseInGameUIDefinition(INI *ini);
+	void applyDefinition(InGameUI *ui, void *parseTable);
 };
 
-__declspec(naked) void INI::parseInGameUIDefinition(INI *ini)
+// ?parseInGameUIDefinition@INI@@SAXPAV1@@Z
+void INI::parseInGameUIDefinition(INI *ini)
 {
-    __asm {
-        _emit 08Bh
-        _emit 00Dh
-        _emit 08Ch
-        _emit 014h
-        _emit 02Fh
-        _emit 001h
-        _emit 085h
-        _emit 0C9h
-        _emit 074h
-        _emit 019h
-        _emit 08Bh
-        _emit 001h
-        _emit 0FFh
-        _emit 090h
-        _emit 058h
-        _emit 001h
-        _emit 000h
-        _emit 000h
-        _emit 08Bh
-        _emit 00Dh
-        _emit 08Ch
-        _emit 014h
-        _emit 02Fh
-        _emit 001h
-        _emit 050h
-        _emit 051h
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 00Ch
-        _emit 0E8h
-        _emit 0DDh
-        _emit 075h
-        _emit 041h
-        _emit 000h
-        _emit 0C3h
-    }
+	InGameUI *ui = g_theInGameUI;
+	if (!ui)
+		return;
+	void *table = ui->getParseTable();
+	InGameUI *ui2 = g_theInGameUI;
+	ini->applyDefinition(ui2, table);
 }
