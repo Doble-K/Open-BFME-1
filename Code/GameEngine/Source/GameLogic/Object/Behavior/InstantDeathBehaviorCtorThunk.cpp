0afb0e9b229aab507e4125e8b1b9eef778a6353d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 05:17:50 -0400

    Open-BFME5 convert InstantDeathBehavior and RunOffMapBehavior constructors to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Behavior/InstantDeathBehaviorCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/InstantDeathBehaviorCtorThunk.cpp
@@ -1,74 +1,49 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Open-BFME5: InstantDeathBehavior module ctor.
+// Base call then interim dual iface vtbls at +0xC/+0x10, then three
+// most-derived vtbls at +0/+0xC/+0x10.
+
 class Thing;
 class ModuleData;
 
-class InstantDeathBehavior
+class BehaviorModule
 {
 public:
-    InstantDeathBehavior(Thing *, const ModuleData *);
+	virtual void behaviorModuleAnchor();
+
+private:
+	unsigned char m_data[8];
 };
 
-__declspec(naked) InstantDeathBehavior::InstantDeathBehavior(Thing *, const ModuleData *)
+class InstantDeathBehaviorIface1
 {
-    __asm _emit(0x8B);
-    __asm _emit(0x44);
-    __asm _emit(0x24);
-    __asm _emit(0x08);
-    __asm _emit(0x56);
-    __asm _emit(0x8B);
-    __asm _emit(0xF1);
-    __asm _emit(0x8B);
-    __asm _emit(0x4C);
-    __asm _emit(0x24);
-    __asm _emit(0x08);
-    __asm _emit(0x50);
-    __asm _emit(0x51);
-    __asm _emit(0x8B);
-    __asm _emit(0xCE);
-    __asm _emit(0xE8);
-    __asm _emit(0x30);
-    __asm _emit(0x65);
-    __asm _emit(0xE1);
-    __asm _emit(0xFF);
-    __asm _emit(0xC7);
-    __asm _emit(0x46);
-    __asm _emit(0x0C);
-    __asm _emit(0xD0);
-    __asm _emit(0xC9);
-    __asm _emit(0x09);
-    __asm _emit(0x01);
-    __asm _emit(0xC7);
-    __asm _emit(0x46);
-    __asm _emit(0x10);
-    __asm _emit(0x7C);
-    __asm _emit(0x25);
-    __asm _emit(0x0A);
-    __asm _emit(0x01);
-    __asm _emit(0xC7);
-    __asm _emit(0x06);
-    __asm _emit(0xBC);
-    __asm _emit(0x4E);
-    __asm _emit(0x0A);
-    __asm _emit(0x01);
-    __asm _emit(0xC7);
-    __asm _emit(0x46);
-    __asm _emit(0x0C);
-    __asm _emit(0xF8);
-    __asm _emit(0x4D);
-    __asm _emit(0x0A);
-    __asm _emit(0x01);
-    __asm _emit(0xC7);
-    __asm _emit(0x46);
-    __asm _emit(0x10);
-    __asm _emit(0xF0);
-    __asm _emit(0x4D);
-    __asm _emit(0x0A);
-    __asm _emit(0x01);
-    __asm _emit(0x8B);
-    __asm _emit(0xC6);
-    __asm _emit(0x5E);
-    __asm _emit(0xC2);
-    __asm _emit(0x08);
-    __asm _emit(0x00);
+public:
+	virtual void instantDeathIface1Anchor();
+};
+
+class InstantDeathBehaviorIface2
+{
+public:
+	virtual void instantDeathIface2Anchor();
+};
+
+class InstantDeathBehaviorBase : public BehaviorModule
+{
+public:
+	InstantDeathBehaviorBase(Thing *thing, const ModuleData *moduleData);
+};
+
+class InstantDeathBehavior : public InstantDeathBehaviorBase,
+	public InstantDeathBehaviorIface1,
+	public InstantDeathBehaviorIface2
+{
+public:
+	InstantDeathBehavior(Thing *thing, const ModuleData *moduleData);
+};
+
+// ??0InstantDeathBehavior@@QAE@PAVThing@@PBVModuleData@@@Z
+InstantDeathBehavior::InstantDeathBehavior(Thing *thing, const ModuleData *moduleData)
+	: InstantDeathBehaviorBase(thing, moduleData)
+{
 }
--- Code/GameEngine/Source/GameLogic/Object/Behavior/RunOffMapBehaviorCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/RunOffMapBehaviorCtorThunk.cpp
@@ -1,74 +1,49 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Open-BFME5: RunOffMapBehavior module ctor.
+// Base call then interim dual iface vtbls at +0xC/+0x10, then three
+// most-derived vtbls at +0/+0xC/+0x10.
+
 class Thing;
 class ModuleData;
 
-class RunOffMapBehavior
+class BehaviorModule
 {
 public:
-    RunOffMapBehavior(Thing *, const ModuleData *);
+	virtual void behaviorModuleAnchor();
+
+private:
+	unsigned char m_data[8];
 };
 
-__declspec(naked) RunOffMapBehavior::RunOffMapBehavior(Thing *, const ModuleData *)
+class RunOffMapBehaviorIface1
 {
-    __asm _emit(0x8B);
-    __asm _emit(0x44);
-    __asm _emit(0x24);
-    __asm _emit(0x08);
-    __asm _emit(0x56);
-    __asm _emit(0x8B);
-    __asm _emit(0xF1);
-    __asm _emit(0x8B);
-    __asm _emit(0x4C);
-    __asm _emit(0x24);
-    __asm _emit(0x08);
-    __asm _emit(0x50);
-    __asm _emit(0x51);
-    __asm _emit(0x8B);
-    __asm _emit(0xCE);
-    __asm _emit(0xE8);
-    __asm _emit(0x70);
-    __asm _emit(0x20);
-    __asm _emit(0xE1);
-    __asm _emit(0xFF);
-    __asm _emit(0xC7);
-    __asm _emit(0x46);
-    __asm _emit(0x0C);
-    __asm _emit(0xD0);
-    __asm _emit(0xC9);
-    __asm _emit(0x09);
-    __asm _emit(0x01);
-    __asm _emit(0xC7);
-    __asm _emit(0x46);
-    __asm _emit(0x10);
-    __asm _emit(0x08);
-    __asm _emit(0x5E);
-    __asm _emit(0x0A);
-    __asm _emit(0x01);
-    __asm _emit(0xC7);
-    __asm _emit(0x06);
-    __asm _emit(0x8C);
-    __asm _emit(0x5F);
-    __asm _emit(0x0A);
-    __asm _emit(0x01);
-    __asm _emit(0xC7);
-    __asm _emit(0x46);
-    __asm _emit(0x0C);
-    __asm _emit(0xC8);
-    __asm _emit(0x5E);
-    __asm _emit(0x0A);
-    __asm _emit(0x01);
-    __asm _emit(0xC7);
-    __asm _emit(0x46);
-    __asm _emit(0x10);
-    __asm _emit(0x0C);
-    __asm _emit(0x5E);
-    __asm _emit(0x0A);
-    __asm _emit(0x01);
-    __asm _emit(0x8B);
-    __asm _emit(0xC6);
-    __asm _emit(0x5E);
-    __asm _emit(0xC2);
-    __asm _emit(0x08);
-    __asm _emit(0x00);
+public:
+	virtual void runOffMapIface1Anchor();
+};
+
+class RunOffMapBehaviorIface2
+{
+public:
+	virtual void runOffMapIface2Anchor();
+};
+
+class RunOffMapBehaviorBase : public BehaviorModule
+{
+public:
+	RunOffMapBehaviorBase(Thing *thing, const ModuleData *moduleData);
+};
+
+class RunOffMapBehavior : public RunOffMapBehaviorBase,
+	public RunOffMapBehaviorIface1,
+	public RunOffMapBehaviorIface2
+{
+public:
+	RunOffMapBehavior(Thing *thing, const ModuleData *moduleData);
+};
+
+// ??0RunOffMapBehavior@@QAE@PAVThing@@PBVModuleData@@@Z
+RunOffMapBehavior::RunOffMapBehavior(Thing *thing, const ModuleData *moduleData)
+	: RunOffMapBehaviorBase(thing, moduleData)
+{
 }
