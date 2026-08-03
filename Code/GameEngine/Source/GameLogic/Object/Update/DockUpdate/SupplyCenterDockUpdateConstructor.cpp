92ded62d6b1e30592f1575b26a7ba2bad2bdae40
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 05:42:54 -0400

    Open-BFME5 convert SupplyCenterDockUpdate constructor to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Update/DockUpdate/SupplyCenterDockUpdateConstructor.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/DockUpdate/SupplyCenterDockUpdateConstructor.cpp
@@ -1,89 +1,58 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+// cl: /DNDEBUG /MD /EHsc
 
-class Thing
-{
-};
+// Open-BFME5: SupplyCenterDockUpdate module ctor.
+// Out-of-line base MI, then four most-derived vtbls at +0/+0xC/+0x10/+0x20.
 
-class ModuleData
-{
-};
+class Thing;
+class ModuleData;
 
-class SupplyCenterDockUpdate
+class BehaviorModule
 {
 public:
-	SupplyCenterDockUpdate( Thing *, const ModuleData * );
+	virtual void behaviorModuleAnchor();
+
+private:
+	unsigned char m_data[8];
 };
 
-__declspec(naked) SupplyCenterDockUpdate::SupplyCenterDockUpdate( Thing *, const ModuleData * )
+class SupplyCenterDockUpdateIface1
 {
-	__asm {
-		_emit 08Bh
-		_emit 044h
-		_emit 024h
-		_emit 008h
-		_emit 056h
-		_emit 08Bh
-		_emit 0F1h
-		_emit 08Bh
-		_emit 04Ch
-		_emit 024h
-		_emit 008h
-		_emit 050h
-		_emit 051h
-		_emit 08Bh
-		_emit 0CEh
-		_emit 0E8h
-		_emit 0A1h
-		_emit 09Dh
-		_emit 0D7h
-		_emit 0FFh
-		_emit 0C7h
-		_emit 006h
-		_emit 0E4h
-		_emit 0B1h
-		_emit 00Ch
-		_emit 001h
-		_emit 0C7h
-		_emit 046h
-		_emit 00Ch
-		_emit 020h
-		_emit 0B1h
-		_emit 00Ch
-		_emit 001h
-		_emit 0C7h
-		_emit 046h
-		_emit 010h
-		_emit 010h
-		_emit 0B1h
-		_emit 00Ch
-		_emit 001h
-		_emit 0C7h
-		_emit 046h
-		_emit 020h
-		_emit 0B0h
-		_emit 0B0h
-		_emit 00Ch
-		_emit 001h
-		_emit 08Bh
-		_emit 0C6h
-		_emit 05Eh
-		_emit 0C2h
-		_emit 008h
-		_emit 000h
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-		_emit 0CCh
-	}
-}
+public:
+	virtual void supplyCenterDockUpdateIface1Anchor();
+};
 
+class SupplyCenterDockUpdateIface2
+{
+public:
+	virtual void supplyCenterDockUpdateIface2Anchor();
+
+private:
+	unsigned char m_pad[0xC];
+};
+
+class SupplyCenterDockUpdateIface3
+{
+public:
+	virtual void supplyCenterDockUpdateIface3Anchor();
+};
+
+class SupplyCenterDockUpdateBase : public BehaviorModule,
+	public SupplyCenterDockUpdateIface1,
+	public SupplyCenterDockUpdateIface2,
+	public SupplyCenterDockUpdateIface3
+{
+public:
+	SupplyCenterDockUpdateBase(Thing *thing, const ModuleData *moduleData);
+};
+
+class SupplyCenterDockUpdate : public SupplyCenterDockUpdateBase
+{
+public:
+	SupplyCenterDockUpdate(Thing *thing, const ModuleData *moduleData);
+};
+
+// ??0SupplyCenterDockUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
+SupplyCenterDockUpdate::SupplyCenterDockUpdate(Thing *thing, const ModuleData *moduleData)
+	: SupplyCenterDockUpdateBase(thing, moduleData)
+{
+}
