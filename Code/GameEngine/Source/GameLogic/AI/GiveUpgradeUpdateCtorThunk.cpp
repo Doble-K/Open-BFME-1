b3260389bbb922bf02e6d60b2f65fdd9639a8369
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:24:20 -0400

    Open-BFME5 convert GiveUpgradeUpdate constructor to clean C++

--- Code/GameEngine/Source/GameLogic/AI/GiveUpgradeUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/GiveUpgradeUpdateCtorThunk.cpp
@@ -1,90 +1,69 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHs-
+// Open-BFME5: lift GiveUpgradeUpdate ctor __emit thunk to clean C++.
+// Retail shape: out-of-line base ctor call with both args, zero the three
+// fields at +0xE8/+0xE9/+0xEC, then the four most-derived vtable stores at
+// +0x00/+0x0C/+0x10/+0x20. The +0x20 subobject is a polymorphic member.
+// /EHs- because retail has no unwind frame.
 
 class Thing;
 class ModuleData;
 
-class GiveUpgradeUpdate
+class GiveUpgradeUpdateGrandBase
+{
+public:
+    GiveUpgradeUpdateGrandBase(Thing *, const ModuleData *);
+    virtual ~GiveUpgradeUpdateGrandBase();
+
+private:
+    unsigned char m_pad[8];
+};
+
+class GiveUpgradeUpdateIface1
+{
+public:
+    virtual ~GiveUpgradeUpdateIface1();
+};
+
+class GiveUpgradeUpdateIface2
+{
+public:
+    virtual ~GiveUpgradeUpdateIface2();
+
+private:
+    unsigned char m_pad[0xC];
+};
+
+class GiveUpgradeUpdateIface3
+{
+public:
+    GiveUpgradeUpdateIface3() {}
+    virtual ~GiveUpgradeUpdateIface3();
+};
+
+class GiveUpgradeUpdateBase : public GiveUpgradeUpdateGrandBase, public GiveUpgradeUpdateIface1, public GiveUpgradeUpdateIface2, public GiveUpgradeUpdateIface3
+{
+public:
+    GiveUpgradeUpdateBase(Thing *, const ModuleData *);
+    virtual ~GiveUpgradeUpdateBase();
+
+private:
+    unsigned char m_pad[0xC4];
+};
+
+class GiveUpgradeUpdate : public GiveUpgradeUpdateBase
 {
 public:
     GiveUpgradeUpdate(Thing *, const ModuleData *);
+
+private:
+    bool m_e8;
+    bool m_e9;
+    unsigned char m_pad2[2];
+    unsigned int m_ec;
 };
 
 // ??0GiveUpgradeUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) GiveUpgradeUpdate::GiveUpgradeUpdate(Thing *, const ModuleData *)
+GiveUpgradeUpdate::GiveUpgradeUpdate(Thing *t, const ModuleData *m)
+    : GiveUpgradeUpdateBase(t, m), m_e8(false), m_e9(false), m_ec(0)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0xae;
-        __emit 0x5a;
-        __emit 0xdb;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x88;
-        __emit 0x86;
-        __emit 0xe8;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x88;
-        __emit 0x86;
-        __emit 0xe9;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x86;
-        __emit 0xec;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x68;
-        __emit 0x53;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xa0;
-        __emit 0x52;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x90;
-        __emit 0x52;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x64;
-        __emit 0x52;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
 }
