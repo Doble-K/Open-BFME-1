061765c3d0ee840702db2821f32a2bddbd6d5442
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 13:30:16 -0400

    Open-BFME5 convert vector ObjectID resize to clean C++

--- Code/GameEngine/Source/Common/VectorObjectIDResizeThunk.cpp
+++ Code/GameEngine/Source/Common/VectorObjectIDResizeThunk.cpp
@@ -1,95 +1,62 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: lift vector<ObjectID>::resize __emit thunk to clean C++.
+// STLport shape: shrink goes through erase(begin()+n, end()), whose
+// copy(__last, end(), __first) range is empty by construction (__last==end()),
+// yielding the dead zero-count move loop; grow calls _M_fill_insert (matched).
 
 enum ObjectID {};
 
-namespace _STL {
-template <class T> class allocator {};
-template <class T, class A> class vector {
-public:
-	void resize(unsigned int, const T &);
+namespace _STL
+{
+
+template <class Type>
+class allocator
+{
 };
-}
+
+template <class Type, class Allocator>
+class vector
+{
+    Type *_M_start;
+    Type *_M_finish;
+    Type *_M_end_of_storage;
+
+    static Type *copy(Type *first, Type *last, Type *result)
+    {
+        int n = (int)(last - first);
+        for (; n > 0; --n) {
+            *result = *first;
+            ++first;
+            ++result;
+        }
+        return result;
+    }
+
+public:
+    unsigned int size() const { return (unsigned int)(_M_finish - _M_start); }
+    Type *begin() { return _M_start; }
+    Type *end() { return _M_finish; }
+
+    Type *erase(Type *first, Type *last)
+    {
+        Type *i = copy(last, end(), first);
+        _M_finish = i;
+        return first;
+    }
+
+    void _M_fill_insert(Type *pos, unsigned int n, const Type &x);
+
+    void resize(unsigned int new_size, const Type x)
+    {
+        if (new_size < size())
+            erase(begin() + new_size, end());
+        else
+            _M_fill_insert(end(), new_size - size(), x);
+    }
+};
 
 // ?resize@?$vector@W4ObjectID@@V?$allocator@W4ObjectID@@@_STL@@@_STL@@QAEXIABW4ObjectID@@@Z
-__declspec(naked) void _STL::vector<ObjectID, _STL::allocator<ObjectID> >::resize(unsigned int, const ObjectID &)
-{
-	__asm {
-		__emit 0x8b
-		__emit 0x41
-		__emit 0x04
-		__emit 0x56
-		__emit 0x8b
-		__emit 0x74
-		__emit 0x24
-		__emit 0x08
-		__emit 0x57
-		__emit 0x8b
-		__emit 0x39
-		__emit 0x8b
-		__emit 0xd0
-		__emit 0x2b
-		__emit 0xd7
-		__emit 0xc1
-		__emit 0xfa
-		__emit 0x02
-		__emit 0x3b
-		__emit 0xf2
-		__emit 0x73
-		__emit 0x23
-		__emit 0x8b
-		__emit 0xd0
-		__emit 0x2b
-		__emit 0xc2
-		__emit 0xc1
-		__emit 0xf8
-		__emit 0x02
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x8d
-		__emit 0x34
-		__emit 0xb7
-		__emit 0x7e
-		__emit 0x0d
-		__emit 0x8b
-		__emit 0x3a
-		__emit 0x89
-		__emit 0x3e
-		__emit 0x83
-		__emit 0xc2
-		__emit 0x04
-		__emit 0x83
-		__emit 0xc6
-		__emit 0x04
-		__emit 0x48
-		__emit 0x75
-		__emit 0xf3
-		__emit 0x5f
-		__emit 0x89
-		__emit 0x71
-		__emit 0x04
-		__emit 0x5e
-		__emit 0xc2
-		__emit 0x08
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x7c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x57
-		__emit 0x2b
-		__emit 0xf2
-		__emit 0x56
-		__emit 0x50
-		__emit 0xe8
-		__emit 0xe1
-		__emit 0x23
-		__emit 0xd4
-		__emit 0xff
-		__emit 0x5f
-		__emit 0x5e
-		__emit 0xc2
-		__emit 0x08
-		__emit 0x00
-	}
+// ?resize@?$vector@W4ObjectID@@V?$allocator@W4ObjectID@@@_STL@@@_STL@@QAEXIW4ObjectID@@@Z
+template void vector<ObjectID, allocator<ObjectID> >::resize(unsigned int, const ObjectID);
+
 }
