ffcc2c32ba240ed058a3056b5524d57f9041bfda
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:00:53 -0400

    Open-BFME5 convert three list-walking ModuleData destructors to clean C++

--- Code/GameEngine/Source/Common/W3DPropDrawModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/W3DPropDrawModuleDataDestructorThunk.cpp
@@ -1,123 +1,41 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: lift ModuleData dtor MASM dump to clean C++.
+// Retail SEH dtor walking the linked list at this+0x08 (m_next at +0x20),
+// deleting each node via its scalar deleting destructor and storing the
+// advanced head back each iteration, then calling the base class dtor
+// (ICF-folded @0x009A1A40).
 
-class __declspec(novtable) W3DPropDrawModuleData
+class SteeringNode
+{
+public:
+	virtual ~SteeringNode();
+	char m_pad[0x1c];
+	SteeringNode *m_next;	// +0x20
+};
+
+class UpdateModuleData
+{
+public:
+	virtual ~UpdateModuleData();	// out-of-line; pinned at 0x009A1A40
+	unsigned int m_04;
+};
+
+class W3DPropDrawModuleData : public UpdateModuleData
 {
 public:
 	virtual ~W3DPropDrawModuleData();
+
+private:
+	SteeringNode *m_head;	// +0x08
 };
 
 // ??1W3DPropDrawModuleData@@UAE@XZ
-__declspec(naked) W3DPropDrawModuleData::~W3DPropDrawModuleData()
+W3DPropDrawModuleData::~W3DPropDrawModuleData()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x48
-		__emit 0x71
-		__emit 0xff
-		__emit 0x00
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x04
-		__emit 0xc7
-		__emit 0x06
-		__emit 0xe0
-		__emit 0x0e
-		__emit 0x08
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x46
-		__emit 0x08
-		__emit 0x85
-		__emit 0xc0
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x1b
-		__emit 0x57
-		__emit 0x8b
-		__emit 0x4e
-		__emit 0x08
-		__emit 0x85
-		__emit 0xc9
-		__emit 0x8b
-		__emit 0x79
-		__emit 0x20
-		__emit 0x74
-		__emit 0x06
-		__emit 0x8b
-		__emit 0x01
-		__emit 0x6a
-		__emit 0x01
-		__emit 0xff
-		__emit 0x10
-		__emit 0x8b
-		__emit 0xc7
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x89
-		__emit 0x7e
-		__emit 0x08
-		__emit 0x75
-		__emit 0xe7
-		__emit 0x5f
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0xd4
-		__emit 0xa7
-		__emit 0x8f
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
+	while (m_head)
+	{
+		SteeringNode *next = m_head->m_next;
+		delete m_head;
+		m_head = next;
 	}
 }
