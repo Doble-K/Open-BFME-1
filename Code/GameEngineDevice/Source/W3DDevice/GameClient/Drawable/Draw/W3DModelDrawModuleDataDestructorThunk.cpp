8e3cbc087c5745b8290d0a681167fd813bd8c5ed
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 04:01:33 -0400

    Open-BFME5 convert W3DModelDrawModuleData destructor to clean C++

--- Code/GameEngineDevice/Source/W3DDevice/GameClient/Drawable/Draw/W3DModelDrawModuleDataDestructorThunk.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Drawable/Draw/W3DModelDrawModuleDataDestructorThunk.cpp
@@ -1,89 +1,31 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: W3DModelDrawModuleData dtor. SEH W3DModelDrawModuleDataMember @+0x5c then base.
 
-class __declspec(novtable) W3DModelDrawModuleData
+class W3DModelDrawModuleDataMember
 {
 public:
-    virtual ~W3DModelDrawModuleData();
+	~W3DModelDrawModuleDataMember();
+private:
+	unsigned char m_pad[4];
 };
 
-__declspec(naked) W3DModelDrawModuleData::~W3DModelDrawModuleData()
+class W3DModelDrawModuleDataBase
 {
-    __asm {
-        _emit 06Ah
-        _emit 0FFh
-        _emit 068h
-        _emit 0A8h
-        _emit 0C7h
-        _emit 000h
-        _emit 001h
-        _emit 064h
-        _emit 0A1h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 050h
-        _emit 064h
-        _emit 089h
-        _emit 025h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 051h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 089h
-        _emit 074h
-        _emit 024h
-        _emit 004h
-        _emit 08Dh
-        _emit 04Eh
-        _emit 05Ch
-        _emit 0C7h
-        _emit 044h
-        _emit 024h
-        _emit 010h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0E8h
-        _emit 084h
-        _emit 06Ch
-        _emit 0E0h
-        _emit 0FFh
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0C7h
-        _emit 044h
-        _emit 024h
-        _emit 010h
-        _emit 0FFh
-        _emit 0FFh
-        _emit 0FFh
-        _emit 0FFh
-        _emit 0E8h
-        _emit 027h
-        _emit 093h
-        _emit 0DFh
-        _emit 0FFh
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 008h
-        _emit 05Eh
-        _emit 064h
-        _emit 089h
-        _emit 00Dh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 010h
-        _emit 0C3h
-    }
+public:
+	virtual ~W3DModelDrawModuleDataBase();
+private:
+	unsigned char m_pad[0x58];
+};
+
+class __declspec(novtable) W3DModelDrawModuleData : public W3DModelDrawModuleDataBase
+{
+public:
+	virtual ~W3DModelDrawModuleData();
+private:
+	W3DModelDrawModuleDataMember m_member;
+};
+
+// ??1W3DModelDrawModuleData@@UAE@XZ
+W3DModelDrawModuleData::~W3DModelDrawModuleData()
+{
 }
