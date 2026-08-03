562726e97314099afe9d9a8e5702825c5e79ae65
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:06:19 -0400

    Open-BFME5 convert ButterflyDraw ConcreteModuleTemplate::clone to clean C++
    Replace naked factory with new+copy-ctor clone (size 0x0c); pin copy ctor; focused build byte-matches retail 31B.

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/ButterflyDrawConcreteModuleTemplateCloneThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/ButterflyDrawConcreteModuleTemplateCloneThunk.cpp
@@ -1,81 +1,42 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: ButterflyDraw ConcreteModuleTemplate::clone
+// Retail: new(0x0c); copy-ctor; return.
 
 namespace FXParticleSystem
 {
-template <int N>
-class DefaultParticleModule
-{
-};
 
-template <int N>
-class DefaultParticleModuleTemplate
-{
-};
-
-class ButterflyDrawModule
+struct ButterflyDrawModuleTag
 {
 };
 
 class ButterflyDrawModuleTemplate
 {
+public:
+	virtual ~ButterflyDrawModuleTemplate();
 };
 
-extern const char BUTTERFLY_DRAW_MODULE_KEY[1];
-extern const char BUTTERFLY_DRAW_MODULE_NAME[1];
-
-template <int Category, const char (&Key)[1], const char (&Name)[1], class Module, class ModuleTemplate, class ParticleModule, class ParticleModuleTemplate>
-class ModuleTag
-{
-};
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
 
 template <class Tag>
 class ConcreteModuleTemplate;
 
-typedef ModuleTag<6, BUTTERFLY_DRAW_MODULE_KEY, BUTTERFLY_DRAW_MODULE_NAME,
-    ButterflyDrawModule, ButterflyDrawModuleTemplate,
-    DefaultParticleModule<6>, DefaultParticleModuleTemplate<6> > ButterflyDrawTag;
-
 template <>
-class ConcreteModuleTemplate<ButterflyDrawTag>
+class ConcreteModuleTemplate<ButterflyDrawModuleTag> : public ButterflyDrawModuleTemplate
 {
 public:
-    virtual ButterflyDrawModuleTemplate *clone() const;
+	ConcreteModuleTemplate(const ConcreteModuleTemplate &);
+	virtual ButterflyDrawModuleTemplate *clone() const;
+
+private:
+	unsigned char m_pad[0x8];
 };
 
-__declspec(naked) ButterflyDrawModuleTemplate *ConcreteModuleTemplate<ButterflyDrawTag>::clone() const
+// Mangled name matched via functions.csv claim for this source.
+ButterflyDrawModuleTemplate *ConcreteModuleTemplate<ButterflyDrawModuleTag>::clone() const
 {
-    __asm {
-        _emit 056h
-        _emit 06Ah
-        _emit 00Ch
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 0A6h
-        _emit 071h
-        _emit 02Ah
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 004h
-        _emit 085h
-        _emit 0C0h
-        _emit 074h
-        _emit 00Ah
-        _emit 056h
-        _emit 08Bh
-        _emit 0C8h
-        _emit 0E8h
-        _emit 006h
-        _emit 015h
-        _emit 0A6h
-        _emit 0FFh
-        _emit 05Eh
-        _emit 0C3h
-        _emit 033h
-        _emit 0C0h
-        _emit 05Eh
-        _emit 0C3h
-    }
+	return new ConcreteModuleTemplate<ButterflyDrawModuleTag>(*this);
 }
+
 }
