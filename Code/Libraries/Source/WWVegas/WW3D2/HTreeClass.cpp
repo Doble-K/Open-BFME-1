bc87af7fe6597c8222cc52f23cdc9105404ead3c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:44:49 -0400

    Open-BFME5 convert HTreeClass default ctor to clean C++
    Replace naked emit with field zeroing and 1.0f scale init; focused build byte-matches retail 20B.

--- Code/Libraries/Source/WWVegas/WW3D2/HTreeClass.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/HTreeClass.cpp
@@ -1,83 +1,77 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
-#include "PreRTS.h"
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: HTreeClass default ctor + dtor
+// Ctor retail 20B: zero +0x10/+0x14, float +0x18 = 1.0f, byte +0 = 0.
 
 class HTreeClass
 {
 public:
-    HTreeClass();
-    ~HTreeClass();
+	HTreeClass();
+	~HTreeClass();
+
+private:
+	unsigned char m_name0;
+	unsigned char m_pad[0xf];
+	void *m_pivots;
+	void *m_pivotCountOrPtr;
+	float m_scale;
 };
 
-__declspec(naked) HTreeClass::HTreeClass()
+// ??0HTreeClass@@QAE@XZ
+HTreeClass::HTreeClass()
 {
-    __asm {
-        _emit 08Bh
-        _emit 0C1h
-        _emit 033h
-        _emit 0C9h
-        _emit 089h
-        _emit 048h
-        _emit 010h
-        _emit 089h
-        _emit 048h
-        _emit 014h
-        _emit 0C7h
-        _emit 040h
-        _emit 018h
-        _emit 000h
-        _emit 000h
-        _emit 080h
-        _emit 03Fh
-        _emit 088h
-        _emit 008h
-        _emit 0C3h
-    }
+	m_pivots = 0;
+	m_pivotCountOrPtr = 0;
+	m_scale = 1.0f;
+	m_name0 = 0;
 }
 
+// Keep dtor as naked until separately converted (still matched via emit).
+// ??1HTreeClass@@QAE@XZ
 __declspec(naked) HTreeClass::~HTreeClass()
 {
-    __asm {
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 08Bh
-        _emit 046h
-        _emit 014h
-        _emit 085h
-        _emit 0C0h
-        _emit 074h
-        _emit 010h
-        _emit 050h
-        _emit 0E8h
-        _emit 010h
-        _emit 0F7h
-        _emit 0F2h
-        _emit 0FFh
-        _emit 083h
-        _emit 0C4h
-        _emit 004h
-        _emit 0C7h
-        _emit 046h
-        _emit 014h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0C7h
-        _emit 046h
-        _emit 010h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0C7h
-        _emit 046h
-        _emit 018h
-        _emit 000h
-        _emit 000h
-        _emit 080h
-        _emit 03Fh
-        _emit 05Eh
-        _emit 0C3h
-    }
+	__asm {
+		_emit 056h
+		_emit 08Bh
+		_emit 0F1h
+		_emit 08Bh
+		_emit 046h
+		_emit 014h
+		_emit 085h
+		_emit 0C0h
+		_emit 074h
+		_emit 010h
+		_emit 050h
+		_emit 0E8h
+		_emit 010h
+		_emit 0F7h
+		_emit 0F2h
+		_emit 0FFh
+		_emit 083h
+		_emit 0C4h
+		_emit 004h
+		_emit 0C7h
+		_emit 046h
+		_emit 014h
+		_emit 000h
+		_emit 000h
+		_emit 000h
+		_emit 000h
+		_emit 0C7h
+		_emit 046h
+		_emit 010h
+		_emit 000h
+		_emit 000h
+		_emit 000h
+		_emit 000h
+		_emit 0C7h
+		_emit 046h
+		_emit 018h
+		_emit 000h
+		_emit 000h
+		_emit 080h
+		_emit 03Fh
+		_emit 05Eh
+		_emit 0C3h
+	}
 }
