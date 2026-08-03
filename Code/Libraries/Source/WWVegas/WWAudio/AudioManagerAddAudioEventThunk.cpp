708511d14fa95af2c2f3233bfe61bf5d367bf85d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:48:55 -0400

    Open-BFME5 convert AudioManager::addAudioEvent to clean C++
    Replace naked thunk with wrapper calling addAudioEventEx(event 0 1 1); pin impl; focused build byte-matches retail 19B.

--- Code/Libraries/Source/WWVegas/WWAudio/AudioManagerAddAudioEventThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWAudio/AudioManagerAddAudioEventThunk.cpp
@@ -1,36 +1,19 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-#include "PreRTS.h"
+// Open-BFME5: AudioManager::addAudioEvent
+// Retail 19B: push defaults (1,1,0) then event; thiscall impl; ret 4.
 
 class AudioEventRTS;
 
 class AudioManager
 {
 public:
-    virtual unsigned int addAudioEvent(const AudioEventRTS *);
+	virtual unsigned int addAudioEvent(const AudioEventRTS *event);
+	unsigned int addAudioEventEx(const AudioEventRTS *event, int a, int b, int c);
 };
 
-__declspec(naked) unsigned int AudioManager::addAudioEvent(const AudioEventRTS *)
+// ?addAudioEvent@AudioManager@@UAEIPBVAudioEventRTS@@@Z
+unsigned int AudioManager::addAudioEvent(const AudioEventRTS *event)
 {
-    __asm {
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 004h
-        _emit 06Ah
-        _emit 001h
-        _emit 06Ah
-        _emit 001h
-        _emit 06Ah
-        _emit 000h
-        _emit 050h
-        _emit 0E8h
-        _emit 0B8h
-        _emit 070h
-        _emit 096h
-        _emit 0FFh
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-    }
+	return addAudioEventEx(event, 0, 1, 1);
 }
