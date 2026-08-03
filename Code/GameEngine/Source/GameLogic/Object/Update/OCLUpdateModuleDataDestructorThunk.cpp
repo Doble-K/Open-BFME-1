008c4e68ff1bb913714ac45fdfb2d68dbdf58cc4
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:16:09 -0400

    Open-BFME5 convert OCLUpdateModuleData destructor to clean C++
    Empty virtual dtor without base class emits retail 7B sequence
    (mov [ecx], vtbl; ret). Fix object-symbol pin to UAE@XZ.

--- Code/GameEngine/Source/GameLogic/Object/Update/OCLUpdateModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/OCLUpdateModuleDataDestructorThunk.cpp
@@ -1,22 +1,15 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+// cl: /DNDEBUG /MD /EHsc
 
-#include "PreRTS.h"
+// Open-BFME5: empty virtual dtor emits mov [ecx], vtbl; ret (7B).
+// No base class: inheritance would emit jmp to base dtor.
 
 class OCLUpdateModuleData
 {
 public:
-    ~OCLUpdateModuleData();
+	virtual ~OCLUpdateModuleData();
 };
 
-__declspec(naked) OCLUpdateModuleData::~OCLUpdateModuleData()
+// ??1OCLUpdateModuleData@@UAE@XZ
+OCLUpdateModuleData::~OCLUpdateModuleData()
 {
-    __asm {
-        _emit 0C7h
-        _emit 001h
-        _emit 044h
-        _emit 037h
-        _emit 007h
-        _emit 001h
-        _emit 0C3h
-    }
 }
