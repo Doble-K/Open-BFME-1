21524f67983df7de083be106d03e523a970c535a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:29:14 -0400

    Open-BFME5 convert OathbreakersFadeAwayBehaviorModuleData dtor to clean C++
    Empty dual-Buffer SEH destructor (members @+0x8/+0xc) byte-matches retail 80B body.

--- Code/GameEngine/Source/GameLogic/AI/OathbreakersFadeAwayBehaviorModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/OathbreakersFadeAwayBehaviorModuleDataDestructorThunk.cpp
@@ -1,95 +1,37 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift the exact retail ICF-primary destructor into a C++ thunk.
+// Open-BFME5: OathbreakersFadeAwayBehaviorModuleData dtor.
+// Retail 80B SEH: destroy Buffer @+0xc then @+0x8, base vtbl store. Buffer pin 0x887940.
 
-class __declspec(novtable) OathbreakersFadeAwayBehaviorModuleData
+class Buffer
 {
 public:
-    virtual ~OathbreakersFadeAwayBehaviorModuleData();
+	~Buffer();
+
+private:
+	unsigned char m_pad[4];
+};
+
+class OathbreakersFadeAwayBehaviorModuleDataBase
+{
+public:
+	virtual ~OathbreakersFadeAwayBehaviorModuleDataBase() {}
+
+private:
+	unsigned char m_pad[0x4];
+};
+
+class __declspec(novtable) OathbreakersFadeAwayBehaviorModuleData
+	: public OathbreakersFadeAwayBehaviorModuleDataBase
+{
+public:
+	virtual ~OathbreakersFadeAwayBehaviorModuleData();
+
+private:
+	Buffer m_a;
+	Buffer m_b;
 };
 
 // ??1OathbreakersFadeAwayBehaviorModuleData@@UAE@XZ
-__declspec(naked) OathbreakersFadeAwayBehaviorModuleData::~OathbreakersFadeAwayBehaviorModuleData()
+OathbreakersFadeAwayBehaviorModuleData::~OathbreakersFadeAwayBehaviorModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xd3
-        __emit 0x1a
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x0c
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x13
-        __emit 0x0a
-        __emit 0x76
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x08
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x06
-        __emit 0x0a
-        __emit 0x76
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
