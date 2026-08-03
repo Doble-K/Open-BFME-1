7ffa745d8c02e7657bc7f26c3e57b5dc2a96aa2b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:35:51 -0400

    Open-BFME5 convert HorseHordeContain constructor to clean multi-vtbl MI C++

--- Code/GameEngine/Source/GameLogic/AI/HorseHordeContainCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/HorseHordeContainCtorThunk.cpp
@@ -1,124 +1,115 @@
-// cl: /DNDEBUG /MD /EHsc
-
-class Thing;
-class ModuleData;
-
-class HorseHordeContain
-{
-public:
-    HorseHordeContain(Thing *, const ModuleData *);
-};
-
-__declspec(naked) HorseHordeContain::HorseHordeContain(Thing *, const ModuleData *)
-{
-    __asm {
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 008h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 008h
-        _emit 050h
-        _emit 051h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0E8h
-        _emit 0CBh
-        _emit 0D6h
-        _emit 0DEh
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 050h
-        _emit 00Dh
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 00Ch
-        _emit 088h
-        _emit 00Ch
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 010h
-        _emit 078h
-        _emit 00Ch
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 020h
-        _emit 0D0h
-        _emit 00Ah
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 024h
-        _emit 0B0h
-        _emit 00Ah
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 028h
-        _emit 0ACh
-        _emit 00Ah
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 02Ch
-        _emit 09Ch
-        _emit 00Ah
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 030h
-        _emit 060h
-        _emit 00Ah
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 034h
-        _emit 050h
-        _emit 00Ah
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 086h
-        _emit 0D4h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 04Ch
-        _emit 00Ah
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 086h
-        _emit 0E4h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0E0h
-        _emit 007h
-        _emit 00Bh
-        _emit 001h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C2h
-        _emit 008h
-        _emit 000h
-    }
-}
+// cl: /DNDEBUG /MD /EHsc
+
+// Open-BFME5: HorseHordeContain module ctor.
+// Out-of-line base MI, then eleven most-derived vtbls at
+// +0/+0xC/+0x10/+0x20/+0x24/+0x28/+0x2C/+0x30/+0x34/+0xD4/+0xE4.
+
+class Thing;
+class ModuleData;
+
+class BehaviorModule
+{
+public:
+virtual void behaviorModuleAnchor();
+
+private:
+unsigned char m_data[8];
+};
+
+class HorseHordeContainIface1
+{
+public:
+virtual void horseHordeIface1Anchor();
+};
+
+class HorseHordeContainIface2
+{
+public:
+virtual void horseHordeIface2Anchor();
+
+private:
+unsigned char m_pad[0xC];
+};
+
+class HorseHordeContainIface3
+{
+public:
+virtual void horseHordeIface3Anchor();
+};
+
+class HorseHordeContainIface4
+{
+public:
+virtual void horseHordeIface4Anchor();
+};
+
+class HorseHordeContainIface5
+{
+public:
+virtual void horseHordeIface5Anchor();
+};
+
+class HorseHordeContainIface6
+{
+public:
+virtual void horseHordeIface6Anchor();
+};
+
+class HorseHordeContainIface7
+{
+public:
+virtual void horseHordeIface7Anchor();
+};
+
+class HorseHordeContainIface8
+{
+public:
+virtual void horseHordeIface8Anchor();
+
+private:
+// span from after +0x34 vtbl to +0xD4
+unsigned char m_pad[0x9C];
+};
+
+class HorseHordeContainIface9
+{
+public:
+virtual void horseHordeIface9Anchor();
+
+private:
+unsigned char m_pad[0xC];
+};
+
+class HorseHordeContainIface10
+{
+public:
+virtual void horseHordeIface10Anchor();
+};
+
+class HorseHordeContainBase : public BehaviorModule,
+public HorseHordeContainIface1,
+public HorseHordeContainIface2,
+public HorseHordeContainIface3,
+public HorseHordeContainIface4,
+public HorseHordeContainIface5,
+public HorseHordeContainIface6,
+public HorseHordeContainIface7,
+public HorseHordeContainIface8,
+public HorseHordeContainIface9,
+public HorseHordeContainIface10
+{
+public:
+HorseHordeContainBase(Thing *thing, const ModuleData *moduleData);
+};
+
+class HorseHordeContain : public HorseHordeContainBase
+{
+public:
+HorseHordeContain(Thing *thing, const ModuleData *moduleData);
+};
+
+// ??0HorseHordeContain@@QAE@PAVThing@@PBVModuleData@@@Z
+HorseHordeContain::HorseHordeContain(Thing *thing, const ModuleData *moduleData)
+: HorseHordeContainBase(thing, moduleData)
+{
+}
