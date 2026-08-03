1127e8c573ef0b4f32d365e2a9deb9d3715a22fb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:15:56 -0400

    Open-BFME5 convert five ScriptActions helpers MASM

---
+++ Code/GameEngine/Source/Common/ScriptActions_doNamedFlash_Thunk.cpp
@@ -0,0 +1,141 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to standalone C++ thunk.
+
+class AsciiString;
+struct RGBColor;
+class ScriptActions
+{
+protected:
+	void doNamedFlash(const AsciiString &, int, const RGBColor *);
+};
+
+// ?doNamedFlash@ScriptActions@@IAEXABVAsciiString@@HPBURGBColor@@@Z
+__declspec(naked) void ScriptActions::doNamedFlash(const AsciiString &, int, const RGBColor *)
+{
+	__asm {
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x6c
+        __emit 0x07
+        __emit 0x2f
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x54
+        __emit 0x24
+        __emit 0x04
+        __emit 0x8b
+        __emit 0x01
+        __emit 0x57
+        __emit 0x52
+        __emit 0xff
+        __emit 0x50
+        __emit 0x68
+        __emit 0x8b
+        __emit 0xf8
+        __emit 0x85
+        __emit 0xff
+        __emit 0x74
+        __emit 0x61
+        __emit 0x8b
+        __emit 0x07
+        __emit 0x53
+        __emit 0x8b
+        __emit 0xcf
+        __emit 0xff
+        __emit 0x50
+        __emit 0x28
+        __emit 0x8b
+        __emit 0xd8
+        __emit 0x85
+        __emit 0xdb
+        __emit 0x74
+        __emit 0x52
+        __emit 0x8b
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x85
+        __emit 0xc0
+        __emit 0x7e
+        __emit 0x4a
+        __emit 0x8d
+        __emit 0x0c
+        __emit 0x80
+        __emit 0xb8
+        __emit 0x89
+        __emit 0x88
+        __emit 0x88
+        __emit 0x88
+        __emit 0xf7
+        __emit 0xe9
+        __emit 0x03
+        __emit 0xd1
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x14
+        __emit 0xc1
+        __emit 0xfa
+        __emit 0x03
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xf2
+        __emit 0xc1
+        __emit 0xee
+        __emit 0x1f
+        __emit 0x03
+        __emit 0xf2
+        __emit 0x85
+        __emit 0xc9
+        __emit 0x75
+        __emit 0x19
+        __emit 0x8b
+        __emit 0xcf
+        __emit 0xe8
+        __emit 0x8d
+        __emit 0xb4
+        __emit 0xd1
+        __emit 0xff
+        __emit 0x89
+        __emit 0xb3
+        __emit 0x60
+        __emit 0x01
+        __emit 0x00
+        __emit 0x00
+        __emit 0x5e
+        __emit 0x89
+        __emit 0x83
+        __emit 0x64
+        __emit 0x01
+        __emit 0x00
+        __emit 0x00
+        __emit 0x5b
+        __emit 0x5f
+        __emit 0xc2
+        __emit 0x0c
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x5f
+        __emit 0xf1
+        __emit 0xd3
+        __emit 0xff
+        __emit 0x89
+        __emit 0xb3
+        __emit 0x60
+        __emit 0x01
+        __emit 0x00
+        __emit 0x00
+        __emit 0x89
+        __emit 0x83
+        __emit 0x64
+        __emit 0x01
+        __emit 0x00
+        __emit 0x00
+        __emit 0x5e
+        __emit 0x5b
+        __emit 0x5f
+        __emit 0xc2
+        __emit 0x0c
+        __emit 0x00
+	}
+}
---
+++ Code/GameEngine/Source/Common/ScriptActions_doPlayerSetScienceAvailability_Thunk.cpp
@@ -0,0 +1,142 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to standalone C++ thunk.
+
+class AsciiString;
+class ScriptActions
+{
+protected:
+	void doPlayerSetScienceAvailability(const AsciiString &, const AsciiString &, const AsciiString &);
+};
+
+// ?doPlayerSetScienceAvailability@ScriptActions@@IAEXABVAsciiString@@00@Z
+__declspec(naked) void ScriptActions::doPlayerSetScienceAvailability(const AsciiString &, const AsciiString &, const AsciiString &)
+{
+	__asm {
+        __emit 0x8b
+        __emit 0x44
+        __emit 0x24
+        __emit 0x04
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x6c
+        __emit 0x07
+        __emit 0x2f
+        __emit 0x01
+        __emit 0x6a
+        __emit 0x00
+        __emit 0x50
+        __emit 0xe8
+        __emit 0xbe
+        __emit 0xb0
+        __emit 0xd5
+        __emit 0xff
+        __emit 0x66
+        __emit 0x85
+        __emit 0xc0
+        __emit 0x89
+        __emit 0x44
+        __emit 0x24
+        __emit 0x04
+        __emit 0x74
+        __emit 0x60
+        __emit 0x53
+        __emit 0x8b
+        __emit 0x5c
+        __emit 0x24
+        __emit 0x10
+        __emit 0x55
+        __emit 0x8b
+        __emit 0x6c
+        __emit 0x24
+        __emit 0x10
+        __emit 0x56
+        __emit 0x57
+        __emit 0xeb
+        __emit 0x07
+        __emit 0x8d
+        __emit 0xa4
+        __emit 0x24
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x8d
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x14
+        __emit 0x51
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x48
+        __emit 0xd7
+        __emit 0x2e
+        __emit 0x01
+        __emit 0xe8
+        __emit 0x60
+        __emit 0xec
+        __emit 0xd3
+        __emit 0xff
+        __emit 0x8b
+        __emit 0xf8
+        __emit 0x85
+        __emit 0xff
+        __emit 0x74
+        __emit 0x29
+        __emit 0x53
+        __emit 0x8b
+        __emit 0xcf
+        __emit 0xe8
+        __emit 0x52
+        __emit 0x00
+        __emit 0xd4
+        __emit 0xff
+        __emit 0x8b
+        __emit 0xf0
+        __emit 0x83
+        __emit 0xfe
+        __emit 0xff
+        __emit 0x74
+        __emit 0x1a
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0xac
+        __emit 0xd7
+        __emit 0x2e
+        __emit 0x01
+        __emit 0x55
+        __emit 0xe8
+        __emit 0x4b
+        __emit 0xb1
+        __emit 0xd5
+        __emit 0xff
+        __emit 0x83
+        __emit 0xf8
+        __emit 0xff
+        __emit 0x74
+        __emit 0x09
+        __emit 0x56
+        __emit 0x50
+        __emit 0x8b
+        __emit 0xcf
+        __emit 0xe8
+        __emit 0xbe
+        __emit 0x0b
+        __emit 0xd2
+        __emit 0xff
+        __emit 0x66
+        __emit 0x83
+        __emit 0x7c
+        __emit 0x24
+        __emit 0x14
+        __emit 0x00
+        __emit 0x75
+        __emit 0xb9
+        __emit 0x5f
+        __emit 0x5e
+        __emit 0x5d
+        __emit 0x5b
+        __emit 0xc2
+        __emit 0x0c
+        __emit 0x00
+	}
+}
