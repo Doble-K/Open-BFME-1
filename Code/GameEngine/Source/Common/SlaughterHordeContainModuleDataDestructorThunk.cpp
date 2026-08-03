b5518d72795b4a31adf5980ff52cdb983fca3304
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:26:10 -0400

    Open-BFME5 convert SlaughterHordeContainModuleData destructor to clean C++
    8-element array member then base dtor; novtable derived.

--- Code/GameEngine/Source/Common/SlaughterHordeContainModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/SlaughterHordeContainModuleDataDestructorThunk.cpp
@@ -1,104 +1,33 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: SlaughterHordeContainModuleData dtor.
+// 8-element array of 0x18 elems at +0x24c, then base dtor.
+
+class SlaughterHordeElement
+{
+public:
+	~SlaughterHordeElement();
+private:
+	unsigned char m_pad[0x18];
+};
+
+class SlaughterHordeContainModuleDataBase
+{
+public:
+	virtual ~SlaughterHordeContainModuleDataBase();
+private:
+	unsigned char m_pad[0x248];
+};
 
 class __declspec(novtable) SlaughterHordeContainModuleData
+	: public SlaughterHordeContainModuleDataBase
 {
 public:
 	virtual ~SlaughterHordeContainModuleData();
+private:
+	SlaughterHordeElement m_arr[8]; // +0x24c
 };
 
 // ??1SlaughterHordeContainModuleData@@UAE@XZ
-__declspec(naked) SlaughterHordeContainModuleData::~SlaughterHordeContainModuleData()
+SlaughterHordeContainModuleData::~SlaughterHordeContainModuleData()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0xd8
-		__emit 0xd5
-		__emit 0x00
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x04
-		__emit 0x68
-		__emit 0xf4
-		__emit 0xf7
-		__emit 0x40
-		__emit 0x00
-		__emit 0x6a
-		__emit 0x08
-		__emit 0x6a
-		__emit 0x18
-		__emit 0x8d
-		__emit 0x86
-		__emit 0x4c
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x20
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x1c
-		__emit 0xbb
-		__emit 0x7c
-		__emit 0x00
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x3b
-		__emit 0xbf
-		__emit 0xe1
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
-	}
 }
