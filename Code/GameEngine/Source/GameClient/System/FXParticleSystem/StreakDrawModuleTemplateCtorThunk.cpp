0ed0acb61316c370fdf2d2db6b66b59a543d3ff8
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 23:02:43 -0400

    Open-BFME5 convert StreakDrawModuleTemplate constructor

---
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/StreakDrawModuleTemplateCtorThunk.cpp
@@ -0,0 +1,125 @@
+// cl: /DNDEBUG /MD /EHsc
+
+namespace FXParticleSystem
+{
+
+class StreakDrawModuleTemplate
+{
+public:
+    StreakDrawModuleTemplate();
+};
+
+// ??0StreakDrawModuleTemplate@FXParticleSystem@@QAE@XZ
+__declspec(naked) StreakDrawModuleTemplate::StreakDrawModuleTemplate()
+{
+    __asm {
+        __emit 0x6a;
+        __emit 0xff;
+        __emit 0x68;
+        __emit 0x98;
+        __emit 0xab;
+        __emit 0x03;
+        __emit 0x01;
+        __emit 0x64;
+        __emit 0xa1;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x50;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x25;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x51;
+        __emit 0x56;
+        __emit 0x8b;
+        __emit 0xf1;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x04;
+        __emit 0x7c;
+        __emit 0xf9;
+        __emit 0x10;
+        __emit 0x01;
+        __emit 0x57;
+        __emit 0x89;
+        __emit 0x74;
+        __emit 0x24;
+        __emit 0x08;
+        __emit 0xc7;
+        __emit 0x06;
+        __emit 0x50;
+        __emit 0xfc;
+        __emit 0x10;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x04;
+        __emit 0x4c;
+        __emit 0xfc;
+        __emit 0x10;
+        __emit 0x01;
+        __emit 0x8d;
+        __emit 0x7e;
+        __emit 0x08;
+        __emit 0x8b;
+        __emit 0xcf;
+        __emit 0xc7;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x14;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0xe8;
+        __emit 0x92;
+        __emit 0x00;
+        __emit 0xa4;
+        __emit 0xff;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x0c;
+        __emit 0xc7;
+        __emit 0x07;
+        __emit 0xa4;
+        __emit 0x0c;
+        __emit 0x11;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x06;
+        __emit 0x90;
+        __emit 0x0c;
+        __emit 0x11;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x04;
+        __emit 0x8c;
+        __emit 0x0c;
+        __emit 0x11;
+        __emit 0x01;
+        __emit 0x5f;
+        __emit 0x8b;
+        __emit 0xc6;
+        __emit 0x5e;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x0d;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x83;
+        __emit 0xc4;
+        __emit 0x10;
+        __emit 0xc3;
+    }
+}
+
+}
