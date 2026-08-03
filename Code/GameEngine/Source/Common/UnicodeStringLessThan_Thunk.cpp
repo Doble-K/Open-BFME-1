00489fc4a8c44bdc69718dd34d0d79a13794008e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:47:26 -0400

    Open-BFME5 convert UnicodeStringLessThan operator() MASM

---
+++ Code/GameEngine/Source/Common/UnicodeStringLessThan_Thunk.cpp
@@ -0,0 +1,116 @@
+﻿// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to standalone C++ thunk.
+
+class UnicodeString
+{
+public:
+	unsigned char pad[16];
+};
+
+class UnicodeStringLessThan
+{
+public:
+	bool operator()(UnicodeString, UnicodeString) const;
+};
+
+// ??RUnicodeStringLessThan@@QBE_NVUnicodeString@@0@Z
+__declspec(naked) bool UnicodeStringLessThan::operator()(UnicodeString, UnicodeString) const
+{
+	__asm {
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0x98
+        __emit 0x37
+        __emit 0x02
+        __emit 0x01
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x53
+        __emit 0x8d
+        __emit 0x44
+        __emit 0x24
+        __emit 0x18
+        __emit 0x50
+        __emit 0x8d
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x18
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0xd3
+        __emit 0x8d
+        __emit 0xbc
+        __emit 0xff
+        __emit 0x85
+        __emit 0xc0
+        __emit 0x8d
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x14
+        __emit 0x0f
+        __emit 0x9c
+        __emit 0xc3
+        __emit 0xe8
+        __emit 0xf6
+        __emit 0xae
+        __emit 0x43
+        __emit 0x00
+        __emit 0x8d
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x18
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x0c
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xe8
+        __emit 0xe5
+        __emit 0xae
+        __emit 0x43
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x04
+        __emit 0x8a
+        __emit 0xc3
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x5b
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x0c
+        __emit 0xc2
+        __emit 0x08
+        __emit 0x00
+	}
+}
