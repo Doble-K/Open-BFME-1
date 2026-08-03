3cddf6460859c4a2166bc6e86896a0f815b6d054
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 05:29:01 -0400

    Open-BFME5 convert RespawnBody and ExperienceLevelCreate constructors to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Behavior/RespawnBodyCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/RespawnBodyCtorThunk.cpp
@@ -1,63 +1,48 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Open-BFME5: RespawnBody module ctor.
+// Out-of-line base MI, then three most-derived vtbls at +0/+0xC/+0x10.
+
 class Thing;
 class ModuleData;
 
-class RespawnBody
+class BehaviorModule
 {
 public:
-    RespawnBody(Thing *, const ModuleData *);
+	virtual void behaviorModuleAnchor();
+
+private:
+	unsigned char m_data[8];
 };
 
-__declspec(naked) RespawnBody::RespawnBody(Thing *, const ModuleData *)
+class RespawnBodyIface1
 {
-    __asm {
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 008h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 008h
-        _emit 050h
-        _emit 051h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0E8h
-        _emit 0A5h
-        _emit 00Eh
-        _emit 0E1h
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 068h
-        _emit 092h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 00Ch
-        _emit 0A0h
-        _emit 091h
-        _emit 00Ah
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 010h
-        _emit 0D0h
-        _emit 090h
-        _emit 00Ah
-        _emit 001h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C2h
-        _emit 008h
-        _emit 000h
-    }
-}
+public:
+	virtual void respawnBodyIface1Anchor();
+};
 
+class RespawnBodyIface2
+{
+public:
+	virtual void respawnBodyIface2Anchor();
+};
+
+class RespawnBodyBase : public BehaviorModule,
+	public RespawnBodyIface1,
+	public RespawnBodyIface2
+{
+public:
+	RespawnBodyBase(Thing *thing, const ModuleData *moduleData);
+};
+
+class RespawnBody : public RespawnBodyBase
+{
+public:
+	RespawnBody(Thing *thing, const ModuleData *moduleData);
+};
+
+// ??0RespawnBody@@QAE@PAVThing@@PBVModuleData@@@Z
+RespawnBody::RespawnBody(Thing *thing, const ModuleData *moduleData)
+	: RespawnBodyBase(thing, moduleData)
+{
+}
--- Code/GameEngine/Source/GameLogic/Object/Create/ExperienceLevelCreateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Create/ExperienceLevelCreateCtorThunk.cpp
@@ -1,63 +1,48 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Open-BFME5: ExperienceLevelCreate module ctor.
+// Out-of-line base MI, then three most-derived vtbls at +0/+0xC/+0x10.
+
 class Thing;
 class ModuleData;
 
-class ExperienceLevelCreate
+class BehaviorModule
 {
 public:
-    ExperienceLevelCreate(Thing *, const ModuleData *);
+	virtual void behaviorModuleAnchor();
+
+private:
+	unsigned char m_data[8];
 };
 
-__declspec(naked) ExperienceLevelCreate::ExperienceLevelCreate(Thing *, const ModuleData *)
+class ExperienceLevelCreateIface1
 {
-    __asm {
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 008h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 008h
-        _emit 050h
-        _emit 051h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0E8h
-        _emit 037h
-        _emit 077h
-        _emit 0DDh
-        _emit 0FFh
-        _emit 0C7h
-        _emit 006h
-        _emit 01Ch
-        _emit 015h
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 00Ch
-        _emit 058h
-        _emit 014h
-        _emit 00Bh
-        _emit 001h
-        _emit 0C7h
-        _emit 046h
-        _emit 010h
-        _emit 044h
-        _emit 014h
-        _emit 00Bh
-        _emit 001h
-        _emit 08Bh
-        _emit 0C6h
-        _emit 05Eh
-        _emit 0C2h
-        _emit 008h
-        _emit 000h
-    }
-}
+public:
+	virtual void experienceLevelCreateIface1Anchor();
+};
 
+class ExperienceLevelCreateIface2
+{
+public:
+	virtual void experienceLevelCreateIface2Anchor();
+};
+
+class ExperienceLevelCreateBase : public BehaviorModule,
+	public ExperienceLevelCreateIface1,
+	public ExperienceLevelCreateIface2
+{
+public:
+	ExperienceLevelCreateBase(Thing *thing, const ModuleData *moduleData);
+};
+
+class ExperienceLevelCreate : public ExperienceLevelCreateBase
+{
+public:
+	ExperienceLevelCreate(Thing *thing, const ModuleData *moduleData);
+};
+
+// ??0ExperienceLevelCreate@@QAE@PAVThing@@PBVModuleData@@@Z
+ExperienceLevelCreate::ExperienceLevelCreate(Thing *thing, const ModuleData *moduleData)
+	: ExperienceLevelCreateBase(thing, moduleData)
+{
+}
