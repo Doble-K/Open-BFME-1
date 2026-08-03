c7cca0769b3f12975a2f33e3ccd4b8321e1df206
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:10:58 -0400

    Open-BFME5 convert OutwardEmissionVelocity clone to clean C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/OutwardEmissionVelocityConcreteModuleTemplateCloneThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/OutwardEmissionVelocityConcreteModuleTemplateCloneThunk.cpp
@@ -1,81 +1,41 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: OutwardEmissionVelocity ConcreteModuleTemplate::clone
 
 namespace FXParticleSystem
 {
-template <int N>
-class DefaultParticleModule
-{
-};
 
-template <int N>
-class DefaultParticleModuleTemplate
-{
-};
-
-class OutwardEmissionVelocityModule
+struct OutwardEmissionVelocityModuleTag
 {
 };
 
 class OutwardEmissionVelocityModuleTemplate
 {
+public:
+	virtual ~OutwardEmissionVelocityModuleTemplate();
 };
 
-extern const char OUTWARD_EMISSION_VELOCITY_MODULE_KEY[1];
-extern const char OUTWARD_EMISSION_VELOCITY_MODULE_NAME[1];
-
-template <int Category, const char (&Key)[1], const char (&Name)[1], class Module, class ModuleTemplate, class ParticleModule, class ParticleModuleTemplate>
-class ModuleTag
-{
-};
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
 
 template <class Tag>
 class ConcreteModuleTemplate;
 
-typedef ModuleTag<4, OUTWARD_EMISSION_VELOCITY_MODULE_KEY, OUTWARD_EMISSION_VELOCITY_MODULE_NAME,
-    OutwardEmissionVelocityModule, OutwardEmissionVelocityModuleTemplate,
-    DefaultParticleModule<4>, DefaultParticleModuleTemplate<4> > OutwardEmissionVelocityTag;
-
 template <>
-class ConcreteModuleTemplate<OutwardEmissionVelocityTag>
+class ConcreteModuleTemplate<OutwardEmissionVelocityModuleTag> : public OutwardEmissionVelocityModuleTemplate
 {
 public:
-    virtual OutwardEmissionVelocityModuleTemplate *clone() const;
+	ConcreteModuleTemplate(const ConcreteModuleTemplate &);
+	virtual OutwardEmissionVelocityModuleTemplate *clone() const;
+
+private:
+	unsigned char m_pad[0x20];
 };
 
-__declspec(naked) OutwardEmissionVelocityModuleTemplate *ConcreteModuleTemplate<OutwardEmissionVelocityTag>::clone() const
+// ?clone@?$ConcreteModuleTemplate@UOutwardEmissionVelocityModuleTag@FXParticleSystem@@@FXParticleSystem@@UBEPAVOutwardEmissionVelocityModuleTemplate@2@XZ
+OutwardEmissionVelocityModuleTemplate *ConcreteModuleTemplate<OutwardEmissionVelocityModuleTag>::clone() const
 {
-    __asm {
-        _emit 056h
-        _emit 06Ah
-        _emit 024h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 006h
-        _emit 051h
-        _emit 02Ah
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 004h
-        _emit 085h
-        _emit 0C0h
-        _emit 074h
-        _emit 00Ah
-        _emit 056h
-        _emit 08Bh
-        _emit 0C8h
-        _emit 0E8h
-        _emit 04Bh
-        _emit 017h
-        _emit 0A3h
-        _emit 0FFh
-        _emit 05Eh
-        _emit 0C3h
-        _emit 033h
-        _emit 0C0h
-        _emit 05Eh
-        _emit 0C3h
-    }
+	return new ConcreteModuleTemplate<OutwardEmissionVelocityModuleTag>(*this);
 }
+
 }
