2c6851008edcb1ded17d9132ba0c8a28bb851737
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 05:34:31 -0400

    Open-BFME5 convert RAMFile destructor to clean C++

--- Code/Libraries/Source/WWVegas/WWLib/RAMFileDestructorThunk.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/RAMFileDestructorThunk.cpp
@@ -1,108 +1,31 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: lift RAMFile dtor __emit thunk to clean C++. Retail stores its
+// own vtable, operator-deletes the buffer member at this+0x14, calls
+// File::close() on this (virtual, resolved directly in dtor context), then
+// drops EH state to -1 and calls the out-of-line File dtor.
 
-class __declspec(novtable) RAMFile
+class File
+{
+public:
+    virtual ~File();
+    virtual void close();
+
+private:
+    unsigned char m_pad[0x10];
+};
+
+class RAMFile : public File
 {
 protected:
     virtual ~RAMFile();
+
+private:
+    char *m_buffer;
 };
 
 // ??1RAMFile@@MAE@XZ
-__declspec(naked) RAMFile::~RAMFile()
+RAMFile::~RAMFile()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x58
-        __emit 0x0e
-        __emit 0x06
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x58
-        __emit 0x3c
-        __emit 0x14
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x14
-        __emit 0x50
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x1c
-        __emit 0x05
-        __emit 0xeb
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xe8
-        __emit 0xa2
-        __emit 0x9e
-        __emit 0xff
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0xd3
-        __emit 0x9e
-        __emit 0xff
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
+    delete m_buffer;
+    close();
 }
