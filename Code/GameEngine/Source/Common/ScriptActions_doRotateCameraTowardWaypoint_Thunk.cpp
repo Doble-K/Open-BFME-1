83d36b922082283ba4e0a189a5c04f7ee4b11387
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:17:43 -0400

    Open-BFME5 convert doRotateCameraTowardWaypoint and two friend_newModuleInstance MASM

---
+++ Code/GameEngine/Source/Common/ScriptActions_doRotateCameraTowardWaypoint_Thunk.cpp
@@ -0,0 +1,132 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to standalone C++ thunk.
+
+class AsciiString;
+class ScriptActions
+{
+protected:
+	void doRotateCameraTowardWaypoint(const AsciiString &, float, float, float, bool);
+};
+
+// ?doRotateCameraTowardWaypoint@ScriptActions@@IAEXABVAsciiString@@MMM_N@Z
+__declspec(naked) void ScriptActions::doRotateCameraTowardWaypoint(const AsciiString &, float, float, float, bool)
+{
+	__asm {
+        __emit 0x51
+        __emit 0x8b
+        __emit 0x44
+        __emit 0x24
+        __emit 0x08
+        __emit 0x53
+        __emit 0x56
+        __emit 0x57
+        __emit 0x51
+        __emit 0x89
+        __emit 0x64
+        __emit 0x24
+        __emit 0x10
+        __emit 0x8b
+        __emit 0xcc
+        __emit 0x50
+        __emit 0xe8
+        __emit 0x5b
+        __emit 0x54
+        __emit 0x59
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0xcc
+        __emit 0xf4
+        __emit 0x2e
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x11
+        __emit 0xff
+        __emit 0x52
+        __emit 0x7c
+        __emit 0x8b
+        __emit 0xf0
+        __emit 0x85
+        __emit 0xf6
+        __emit 0x74
+        __emit 0x47
+        __emit 0x8b
+        __emit 0x44
+        __emit 0x24
+        __emit 0x24
+        __emit 0xd9
+        __emit 0x44
+        __emit 0x24
+        __emit 0x20
+        __emit 0xd8
+        __emit 0x0d
+        __emit 0x68
+        __emit 0x5c
+        __emit 0x07
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x3d
+        __emit 0x00
+        __emit 0x16
+        __emit 0x2f
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x1f
+        __emit 0x50
+        __emit 0x83
+        __emit 0xec
+        __emit 0x08
+        __emit 0xd9
+        __emit 0x5c
+        __emit 0x24
+        __emit 0x04
+        __emit 0xd9
+        __emit 0x44
+        __emit 0x24
+        __emit 0x28
+        __emit 0xd8
+        __emit 0x0d
+        __emit 0x68
+        __emit 0x5c
+        __emit 0x07
+        __emit 0x01
+        __emit 0xd9
+        __emit 0x1c
+        __emit 0x24
+        __emit 0xd9
+        __emit 0x44
+        __emit 0x24
+        __emit 0x24
+        __emit 0xd8
+        __emit 0x0d
+        __emit 0x68
+        __emit 0x5c
+        __emit 0x07
+        __emit 0x01
+        __emit 0xe8
+        __emit 0xe8
+        __emit 0x46
+        __emit 0x70
+        __emit 0x00
+        __emit 0x50
+        __emit 0x83
+        __emit 0xc6
+        __emit 0x0c
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xcf
+        __emit 0xff
+        __emit 0x93
+        __emit 0xd0
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x5f
+        __emit 0x5e
+        __emit 0x5b
+        __emit 0x59
+        __emit 0xc2
+        __emit 0x14
+        __emit 0x00
+	}
+}
---
+++ Code/GameEngine/Source/Common/WoundArrowUpdate_friend_newModuleInstance_Thunk.cpp
@@ -0,0 +1,117 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to standalone C++ thunk.
+
+class Module;
+class Thing;
+class ModuleData;
+class WoundArrowUpdate
+{
+public:
+	static Module * __cdecl friend_newModuleInstance(Thing *, const ModuleData *);
+};
+
+// ?friend_newModuleInstance@WoundArrowUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
+__declspec(naked) Module * __cdecl WoundArrowUpdate::friend_newModuleInstance(Thing *, const ModuleData *)
+{
+	__asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0xcb
+        __emit 0x0e
+        __emit 0x00
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x51
+        __emit 0x68
+        __emit 0xec
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0x80
+        __emit 0x0f
+        __emit 0x76
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x04
+        __emit 0x89
+        __emit 0x04
+        __emit 0x24
+        __emit 0x85
+        __emit 0xc0
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x0c
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x74
+        __emit 0x20
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x18
+        __emit 0x8b
+        __emit 0x54
+        __emit 0x24
+        __emit 0x14
+        __emit 0x51
+        __emit 0x52
+        __emit 0x8b
+        __emit 0xc8
+        __emit 0xe8
+        __emit 0x45
+        __emit 0x16
+        __emit 0xf2
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x04
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc3
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x04
+        __emit 0x33
+        __emit 0xc0
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc3
+	}
+}
