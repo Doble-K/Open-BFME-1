cd84c62311ac013facf20fbb30b582b2232c137a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:31:56 -0400

    Open-BFME5 convert CastleUpgrade::friend_newModuleData to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Upgrade/CastleUpgradeFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/CastleUpgradeFriendNewModuleDataThunk.cpp
@@ -1,82 +1,66 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class INI;
+// Open-BFME5: CastleUpgrade::friend_newModuleData
+// Retail: new(0x74); sub-ctor at +8; outer vtbl; dword +0x70 = 0.
+
 class ModuleData;
+
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class UpgradeModuleDataSub
+{
+public:
+	UpgradeModuleDataSub();
+
+private:
+	char opaque[0x68];
+};
+
+class CastleUpgradeModuleData
+{
+public:
+	CastleUpgradeModuleData()
+	{
+		m_extra = 0;
+	}
+	virtual void dummy();
+
+private:
+	int m_pad;
+	char m_sub_space[0x68];
+	unsigned int m_extra;
+};
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+extern "C" char CastleUpgradeFieldParse;
 
 class CastleUpgrade
 {
 public:
-    static ModuleData *friend_newModuleData(INI *ini);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
 // ?friend_newModuleData@CastleUpgrade@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *CastleUpgrade::friend_newModuleData(INI *)
+ModuleData *CastleUpgrade::friend_newModuleData(INI *ini)
 {
-    __asm {
-        __emit 0x56
-        __emit 0x6a
-        __emit 0x74
-        __emit 0xe8
-        __emit 0xa8
-        __emit 0x64
-        __emit 0x75
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x74
-        __emit 0x17
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x08
-        __emit 0xe8
-        __emit 0x0d
-        __emit 0x36
-        __emit 0xee
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xf8
-        __emit 0xf5
-        __emit 0x08
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x70
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x68
-        __emit 0x6c
-        __emit 0xd5
-        __emit 0x40
-        __emit 0x00
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x73
-        __emit 0x66
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc3
-    }
+	CastleUpgradeModuleData *data =
+		(CastleUpgradeModuleData *)operator new(0x74);
+	if (data)
+	{
+		UpgradeModuleDataSub *sub =
+			(UpgradeModuleDataSub *)((char *)data + 8);
+		sub->UpgradeModuleDataSub::UpgradeModuleDataSub();
+		data->CastleUpgradeModuleData::CastleUpgradeModuleData();
+	}
+	else
+		data = 0;
+	if (ini)
+		ini->initFromINI(data, &CastleUpgradeFieldParse);
+	return (ModuleData *)data;
 }
