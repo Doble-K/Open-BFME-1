f14ebcd3196352d7ee8f52fa67fb3014b656e269
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 05:52:15 -0400

    Open-BFME5 convert VeterancyCrateCollide::friend_newModuleData to clean C++
    Base construct, outer vtbl, zero/ten field inits; pin BaseShim construct.

--- Code/GameEngine/Source/GameLogic/Object/Collide/CrateCollide/VeterancyCrateCollideFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Collide/CrateCollide/VeterancyCrateCollideFriendNewModuleDataThunk.cpp
@@ -1,146 +1,68 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: VeterancyCrateCollide::friend_newModuleData factory.
 
-class INI;
 class ModuleData;
+
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class VeterancyCrateCollideModuleDataBaseShim
+{
+public:
+	void construct();
+};
+
+class VeterancyCrateCollideModuleDataVtbl
+{
+public:
+	VeterancyCrateCollideModuleDataVtbl() {}
+	virtual void dummy();
+
+private:
+	unsigned char m_pad[0x5c];
+};
+
+class __declspec(novtable) VeterancyCrateCollideModuleData
+{
+public:
+	VeterancyCrateCollideModuleData();
+	virtual void dummy();
+
+private:
+	unsigned char m_pad[0x5c];
+};
+
+VeterancyCrateCollideModuleData::VeterancyCrateCollideModuleData()
+{
+	((VeterancyCrateCollideModuleDataBaseShim *)this)->construct();
+	((VeterancyCrateCollideModuleDataVtbl *)this)
+		->VeterancyCrateCollideModuleDataVtbl::VeterancyCrateCollideModuleDataVtbl();
+	unsigned int z = 0;
+	*(unsigned int *)((char *)this + 0x54) = z;
+	*((unsigned char *)this + 0x58) = (unsigned char)z;
+	*((unsigned char *)this + 0x59) = (unsigned char)z;
+	*(unsigned int *)((char *)this + 0x5c) = 0xa;
+}
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+extern "C" char VeterancyCrateCollideFieldParse;
 
 class VeterancyCrateCollide
 {
 public:
-    static ModuleData *friend_newModuleData(INI *);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
 // ?friend_newModuleData@VeterancyCrateCollide@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *VeterancyCrateCollide::friend_newModuleData(INI *)
+ModuleData *VeterancyCrateCollide::friend_newModuleData(INI *ini)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0xab;
-        __emit 0x18;
-        __emit 0x00;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x53;
-        __emit 0x56;
-        __emit 0x6a;
-        __emit 0x60;
-        __emit 0xe8;
-        __emit 0x41;
-        __emit 0xbf;
-        __emit 0x75;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0xf0;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x33;
-        __emit 0xdb;
-        __emit 0x3b;
-        __emit 0xf3;
-        __emit 0x89;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x74;
-        __emit 0x1f;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0xc1;
-        __emit 0xe1;
-        __emit 0xf1;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x40;
-        __emit 0xe9;
-        __emit 0x08;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x54;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x58;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x59;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x5c;
-        __emit 0x0a;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xeb;
-        __emit 0x02;
-        __emit 0x33;
-        __emit 0xf6;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0x3b;
-        __emit 0xcb;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0x74;
-        __emit 0x0b;
-        __emit 0x68;
-        __emit 0x51;
-        __emit 0x27;
-        __emit 0x41;
-        __emit 0x00;
-        __emit 0x56;
-        __emit 0xe8;
-        __emit 0xf2;
-        __emit 0xc0;
-        __emit 0x72;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x5b;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+	VeterancyCrateCollideModuleData *data = new VeterancyCrateCollideModuleData;
+	if (ini)
+		ini->initFromINI(data, &VeterancyCrateCollideFieldParse);
+	return (ModuleData *)data;
 }
