fe2eabcd26f64257148bc14b9d89538ebdcc33bb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:16:47 -0400

    Open-BFME5 convert W3DDisplayString destructor to clean C++
    Protected MAE dtor: early vtbl, three staggered members, pinned base.

--- Code/GameEngine/Source/Common/W3DDisplayStringDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/W3DDisplayStringDestructorThunk.cpp
@@ -1,129 +1,43 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: W3DDisplayString dtor (protected MAE).
+// Early vtbl, members @+0x14/+0xe0/+0x1b0, base dtor.
 
-class __declspec(novtable) W3DDisplayString
+class W3DDisplayStringMemberB
+{
+public:
+	~W3DDisplayStringMemberB();
+private:
+	unsigned char m_pad[4];
+};
+
+class CountUpBuffer
+{
+public:
+	~CountUpBuffer();
+private:
+	unsigned char m_pad[4];
+};
+
+class W3DDisplayStringBase
+{
+public:
+	virtual ~W3DDisplayStringBase();
+};
+
+class W3DDisplayString : public W3DDisplayStringBase
 {
 protected:
 	virtual ~W3DDisplayString();
+private:
+	unsigned char m_gap[0x10];
+	W3DDisplayStringMemberB m_a;
+	unsigned char m_gap2[0xc8];
+	W3DDisplayStringMemberB m_b;
+	unsigned char m_gap3[0xcc];
+	CountUpBuffer m_c;
 };
 
 // ??1W3DDisplayString@@MAE@XZ
-__declspec(naked) W3DDisplayString::~W3DDisplayString()
+W3DDisplayString::~W3DDisplayString()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x31
-		__emit 0xba
-		__emit 0x04
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x04
-		__emit 0xc7
-		__emit 0x06
-		__emit 0xa8
-		__emit 0xfe
-		__emit 0x11
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x8e
-		__emit 0xb0
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x4a
-		__emit 0x35
-		__emit 0x19
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x8e
-		__emit 0xe0
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x01
-		__emit 0xe8
-		__emit 0x5a
-		__emit 0xbd
-		__emit 0x24
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0x14
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x4d
-		__emit 0xbd
-		__emit 0x24
-		__emit 0x00
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x71
-		__emit 0x8b
-		__emit 0x91
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
-	}
 }
