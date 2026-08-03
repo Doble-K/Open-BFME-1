6713afb2e883ab3620eab6c32d4d67ce32d528b3
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 04:21:25 -0400

    Open-BFME5 convert LaserUpdateModuleData destructor to clean C++

--- Code/GameEngine/Source/Common/LaserUpdateModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/LaserUpdateModuleDataDestructorThunk.cpp
@@ -1,108 +1,41 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: LaserUpdateModuleData dtor. members @+8/+0xc/+0x14 pin 0x887940.
 
-class __declspec(novtable) LaserUpdateModuleData
+class LaserUpdateModuleDataMember4
 {
 public:
-    virtual ~LaserUpdateModuleData();
+	~LaserUpdateModuleDataMember4();
+private:
+	unsigned char m_pad[4];
+};
+
+class LaserUpdateModuleDataMember8
+{
+public:
+	~LaserUpdateModuleDataMember8();
+private:
+	unsigned char m_pad[8];
+};
+
+class LaserUpdateModuleDataBase
+{
+public:
+	virtual ~LaserUpdateModuleDataBase() {}
+private:
+	unsigned char m_pad[0x4];
+};
+
+class __declspec(novtable) LaserUpdateModuleData : public LaserUpdateModuleDataBase
+{
+public:
+	virtual ~LaserUpdateModuleData();
+private:
+	LaserUpdateModuleDataMember4 m_a;
+	LaserUpdateModuleDataMember8 m_b;
+	LaserUpdateModuleDataMember4 m_c;
 };
 
 // ??1LaserUpdateModuleData@@UAE@XZ
-__declspec(naked) LaserUpdateModuleData::~LaserUpdateModuleData()
+LaserUpdateModuleData::~LaserUpdateModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x1e
-        __emit 0xd5
-        __emit 0x03
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x14
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x33
-        __emit 0x35
-        __emit 0x28
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x0c
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x01
-        __emit 0xe8
-        __emit 0x26
-        __emit 0x35
-        __emit 0x28
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x08
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x19
-        __emit 0x35
-        __emit 0x28
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
