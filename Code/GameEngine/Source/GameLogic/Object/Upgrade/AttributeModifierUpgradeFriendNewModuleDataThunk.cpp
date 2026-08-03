69b4f375995b722700f78f8257ca80dc7f4f956f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:35:07 -0400

    Open-BFME5 convert AttributeModifierUpgrade::friend_newModuleData to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Upgrade/AttributeModifierUpgradeFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/AttributeModifierUpgradeFriendNewModuleDataThunk.cpp
@@ -1,82 +1,66 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class INI;
+// Open-BFME5: AttributeModifierUpgrade::friend_newModuleData
+// Retail: new(0x74); sub-ctor at +8; outer vtbl; dword +0x70 = 0.
+
 class ModuleData;
+
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class UpgradeModuleDataSub
+{
+public:
+	UpgradeModuleDataSub();
+
+private:
+	char opaque[0x68];
+};
+
+class AttributeModifierUpgradeModuleData
+{
+public:
+	AttributeModifierUpgradeModuleData()
+	{
+		m_extra = 0;
+	}
+	virtual void dummy();
+
+private:
+	int m_pad;
+	char m_sub_space[0x68];
+	unsigned int m_extra;
+};
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+extern "C" char AttributeModifierUpgradeFieldParse;
 
 class AttributeModifierUpgrade
 {
 public:
-    static ModuleData *friend_newModuleData(INI *ini);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
 // ?friend_newModuleData@AttributeModifierUpgrade@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *AttributeModifierUpgrade::friend_newModuleData(INI *)
+ModuleData *AttributeModifierUpgrade::friend_newModuleData(INI *ini)
 {
-    __asm {
-        __emit 0x56
-        __emit 0x6a
-        __emit 0x74
-        __emit 0xe8
-        __emit 0xd8
-        __emit 0x65
-        __emit 0x75
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x74
-        __emit 0x17
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x08
-        __emit 0xe8
-        __emit 0x3d
-        __emit 0x37
-        __emit 0xee
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xa0
-        __emit 0xf5
-        __emit 0x08
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x70
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x68
-        __emit 0xb6
-        __emit 0x25
-        __emit 0x42
-        __emit 0x00
-        __emit 0x56
-        __emit 0xe8
-        __emit 0xa3
-        __emit 0x67
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc3
-    }
+	AttributeModifierUpgradeModuleData *data =
+		(AttributeModifierUpgradeModuleData *)operator new(0x74);
+	if (data)
+	{
+		UpgradeModuleDataSub *sub =
+			(UpgradeModuleDataSub *)((char *)data + 8);
+		sub->UpgradeModuleDataSub::UpgradeModuleDataSub();
+		data->AttributeModifierUpgradeModuleData::AttributeModifierUpgradeModuleData();
+	}
+	else
+		data = 0;
+	if (ini)
+		ini->initFromINI(data, &AttributeModifierUpgradeFieldParse);
+	return (ModuleData *)data;
 }
