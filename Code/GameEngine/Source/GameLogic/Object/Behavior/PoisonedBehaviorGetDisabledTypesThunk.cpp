7b917c72be7d50d59a08181667fec003366794b3
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:11:25 -0400

    Open-BFME5 convert PoisonedBehavior::getDisabledTypesToProcess to clean C++
    Replace naked dump with return DISABLEDMASK_ALL; non-trivial BitFlags copy
    ctor forces the MSVC 7.1 hidden return-slot ABI that retail uses.

--- Code/GameEngine/Source/GameLogic/Object/Behavior/PoisonedBehaviorGetDisabledTypesThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/PoisonedBehaviorGetDisabledTypesThunk.cpp
@@ -1,34 +1,29 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Standalone TU for PoisonedBehavior::getDisabledTypesToProcess.
+// Retail: copy DISABLEDMASK_ALL into hidden return slot (ret 4).
+
 template <int Bits>
 class BitFlags
 {
+public:
+	unsigned int m_bits;
+
+	// Non-trivial copy forces MSVC 7.1 hidden return-pointer ABI.
+	BitFlags(const BitFlags &other) : m_bits(other.m_bits) {}
 };
+
+// ?DISABLEDMASK_ALL@@3V?$BitFlags@$0N@@@A
+extern BitFlags<13> DISABLEDMASK_ALL;
 
 class PoisonedBehavior
 {
 public:
-    virtual BitFlags<13> getDisabledTypesToProcess() const;
+	virtual BitFlags<13> getDisabledTypesToProcess() const;
 };
 
 // ?getDisabledTypesToProcess@PoisonedBehavior@@UBE?AV?$BitFlags@$0N@@@XZ
-__declspec(naked) BitFlags<13> PoisonedBehavior::getDisabledTypesToProcess() const
+BitFlags<13> PoisonedBehavior::getDisabledTypesToProcess() const
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0x8b;
-        __emit 0x0d;
-        __emit 0x5c;
-        __emit 0xd8;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x08;
-        __emit 0xc2;
-        __emit 0x04;
-        __emit 0x00;
-    }
+	return DISABLEDMASK_ALL;
 }
