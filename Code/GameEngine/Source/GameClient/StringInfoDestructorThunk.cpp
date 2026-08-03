8abe4bc7194daf8009364f85275119a45a20b214
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:28:44 -0400

    Open-BFME5 convert StringInfo and GameSpyGroupRoom destructors to clean C++
    AsciiString at +0 and UnicodeString at +4 dual-member SEH dtors; focused
    build byte-matches both 76B bodies.

--- Code/GameEngine/Source/GameClient/StringInfoDestructorThunk.cpp
+++ Code/GameEngine/Source/GameClient/StringInfoDestructorThunk.cpp
@@ -1,89 +1,35 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: StringInfo dtor. AsciiString @+0 then UnicodeString @+4.
+
+class AsciiString
+{
+public:
+	~AsciiString();
+
+private:
+	unsigned char m_pad[4];
+};
+
+class UnicodeString
+{
+public:
+	~UnicodeString();
+
+private:
+	unsigned char m_pad[4];
+};
 
 class StringInfo
 {
 public:
-    ~StringInfo();
+	~StringInfo();
+
+private:
+	AsciiString m_ascii;
+	UnicodeString m_unicode;
 };
 
-__declspec(naked) StringInfo::~StringInfo()
+// ??1StringInfo@@QAE@XZ
+StringInfo::~StringInfo()
 {
-    __asm {
-        _emit 06Ah
-        _emit 0FFh
-        _emit 068h
-        _emit 0A8h
-        _emit 021h
-        _emit 002h
-        _emit 001h
-        _emit 064h
-        _emit 0A1h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 050h
-        _emit 064h
-        _emit 089h
-        _emit 025h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 051h
-        _emit 056h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 089h
-        _emit 074h
-        _emit 024h
-        _emit 004h
-        _emit 08Dh
-        _emit 04Eh
-        _emit 004h
-        _emit 0C7h
-        _emit 044h
-        _emit 024h
-        _emit 010h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0E8h
-        _emit 0B3h
-        _emit 018h
-        _emit 045h
-        _emit 000h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0C7h
-        _emit 044h
-        _emit 024h
-        _emit 010h
-        _emit 0FFh
-        _emit 0FFh
-        _emit 0FFh
-        _emit 0FFh
-        _emit 0E8h
-        _emit 014h
-        _emit 010h
-        _emit 045h
-        _emit 000h
-        _emit 08Bh
-        _emit 04Ch
-        _emit 024h
-        _emit 008h
-        _emit 05Eh
-        _emit 064h
-        _emit 089h
-        _emit 00Dh
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 010h
-        _emit 0C3h
-    }
 }
