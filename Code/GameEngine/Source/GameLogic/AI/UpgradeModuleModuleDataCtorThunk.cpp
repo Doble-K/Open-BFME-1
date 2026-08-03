22b3b95b7731978a98ef0a61770494bfb32a1c27
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 20:33:37 -0400

    Open-BFME5 convert UpgradeModuleModuleData constructor

---
+++ Code/GameEngine/Source/GameLogic/AI/UpgradeModuleModuleDataCtorThunk.cpp
@@ -0,0 +1,106 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class UpgradeModuleModuleData
+{
+public:
+    UpgradeModuleModuleData();
+};
+
+// ??0UpgradeModuleModuleData@@QAE@XZ
+__declspec(naked) UpgradeModuleModuleData::UpgradeModuleModuleData()
+{
+    __asm {
+        __emit 0x6a;
+        __emit 0xff;
+        __emit 0x68;
+        __emit 0xa3;
+        __emit 0x1d;
+        __emit 0x00;
+        __emit 0x01;
+        __emit 0x64;
+        __emit 0xa1;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x50;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x25;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x51;
+        __emit 0x56;
+        __emit 0x8b;
+        __emit 0xf1;
+        __emit 0x89;
+        __emit 0x74;
+        __emit 0x24;
+        __emit 0x04;
+        __emit 0xe8;
+        __emit 0xd6;
+        __emit 0xda;
+        __emit 0xf0;
+        __emit 0xff;
+        __emit 0x8d;
+        __emit 0x4e;
+        __emit 0x5c;
+        __emit 0xc7;
+        __emit 0x06;
+        __emit 0x78;
+        __emit 0xec;
+        __emit 0x08;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x10;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0xc7;
+        __emit 0x01;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x6a;
+        __emit 0x2f;
+        __emit 0x68;
+        __emit 0x3c;
+        __emit 0xec;
+        __emit 0x08;
+        __emit 0x01;
+        __emit 0xc6;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x18;
+        __emit 0x01;
+        __emit 0xe8;
+        __emit 0xb6;
+        __emit 0xef;
+        __emit 0x75;
+        __emit 0x00;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x08;
+        __emit 0x8b;
+        __emit 0xc6;
+        __emit 0x5e;
+        __emit 0x64;
+        __emit 0x89;
+        __emit 0x0d;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x83;
+        __emit 0xc4;
+        __emit 0x10;
+        __emit 0xc3;
+    }
+}
