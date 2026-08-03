f2a7a521809bdc6d9c228a3cf1a3bf218d099004
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 05:23:41 -0400

    Open-BFME5 convert WeaponStore::allocateNewWeapon to clean C++
    SEH factory: new Weapon(template, slot) size 0x5c; Weapon ctor pin 0x2B03.

--- Code/GameEngine/Source/Common/WeaponStore_allocateNewWeapon_Thunk.cpp
+++ Code/GameEngine/Source/Common/WeaponStore_allocateNewWeapon_Thunk.cpp
@@ -1,9 +1,21 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: WeaponStore::allocateNewWeapon factory.
 
 class Weapon;
 class WeaponTemplate;
-enum WeaponSlotType { };
+enum WeaponSlotType { WEAPON_SLOT_PLACEHOLDER };
+
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class Weapon
+{
+public:
+	Weapon(const WeaponTemplate *, WeaponSlotType);
+private:
+	unsigned char m_pad[0x5c];
+};
+
 class WeaponStore
 {
 public:
@@ -11,108 +23,7 @@ public:
 };
 
 // ?allocateNewWeapon@WeaponStore@@QBEPAVWeapon@@PBVWeaponTemplate@@W4WeaponSlotType@@@Z
-__declspec(naked) Weapon *WeaponStore::allocateNewWeapon(const WeaponTemplate *, WeaponSlotType) const
+Weapon *WeaponStore::allocateNewWeapon(const WeaponTemplate *tmpl, WeaponSlotType slot) const
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0xbb
-		__emit 0x8c
-		__emit 0x00
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x6a
-		__emit 0x5c
-		__emit 0xe8
-		__emit 0x83
-		__emit 0x41
-		__emit 0x6c
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x04
-		__emit 0x89
-		__emit 0x04
-		__emit 0x24
-		__emit 0x85
-		__emit 0xc0
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x22
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x18
-		__emit 0x8b
-		__emit 0x54
-		__emit 0x24
-		__emit 0x14
-		__emit 0x51
-		__emit 0x52
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0xe8
-		__emit 0x33
-		__emit 0x4d
-		__emit 0xe4
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x04
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc2
-		__emit 0x08
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x04
-		__emit 0x33
-		__emit 0xc0
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc2
-		__emit 0x08
-		__emit 0x00
-	}
+	return new Weapon(tmpl, slot);
 }
