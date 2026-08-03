60b57bd204ac0ccc9301f16fb450fab2de6c70cc
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 23:04:58 -0400

    Open-BFME5 convert WindModuleInfo constructor

---
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/WindModuleInfoCtorThunk.cpp
@@ -0,0 +1,131 @@
+// cl: /DNDEBUG /MD /EHsc
+
+namespace FXParticleSystem
+{
+
+class WindModuleInfo
+{
+public:
+    WindModuleInfo();
+};
+
+// ??0WindModuleInfo@FXParticleSystem@@QAE@XZ
+__declspec(naked) WindModuleInfo::WindModuleInfo()
+{
+    __asm {
+        __emit 0x8b;
+        __emit 0xc1;
+        __emit 0x33;
+        __emit 0xc9;
+        __emit 0xba;
+        __emit 0x9a;
+        __emit 0x99;
+        __emit 0x19;
+        __emit 0x3e;
+        __emit 0xc7;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0xfa;
+        __emit 0x10;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x40;
+        __emit 0x04;
+        __emit 0x01;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0xc7;
+        __emit 0x40;
+        __emit 0x08;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x40;
+        __emit 0xc7;
+        __emit 0x40;
+        __emit 0x0c;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x96;
+        __emit 0x42;
+        __emit 0xc7;
+        __emit 0x40;
+        __emit 0x10;
+        __emit 0x00;
+        __emit 0x00;
+        __emit 0x48;
+        __emit 0x43;
+        __emit 0x89;
+        __emit 0x48;
+        __emit 0x14;
+        __emit 0x89;
+        __emit 0x50;
+        __emit 0x18;
+        __emit 0x89;
+        __emit 0x50;
+        __emit 0x1c;
+        __emit 0xc7;
+        __emit 0x40;
+        __emit 0x20;
+        __emit 0x66;
+        __emit 0x66;
+        __emit 0xe6;
+        __emit 0x3e;
+        __emit 0x89;
+        __emit 0x48;
+        __emit 0x28;
+        __emit 0xc7;
+        __emit 0x40;
+        __emit 0x2c;
+        __emit 0xdb;
+        __emit 0x0f;
+        __emit 0x49;
+        __emit 0x3f;
+        __emit 0x8b;
+        __emit 0x15;
+        __emit 0x04;
+        __emit 0x70;
+        __emit 0x2f;
+        __emit 0x01;
+        __emit 0x89;
+        __emit 0x50;
+        __emit 0x24;
+        __emit 0xc7;
+        __emit 0x40;
+        __emit 0x34;
+        __emit 0xe0;
+        __emit 0xed;
+        __emit 0xaf;
+        __emit 0x40;
+        __emit 0xc7;
+        __emit 0x40;
+        __emit 0x38;
+        __emit 0xdb;
+        __emit 0x0f;
+        __emit 0xc9;
+        __emit 0x40;
+        __emit 0x8b;
+        __emit 0x15;
+        __emit 0x00;
+        __emit 0x8e;
+        __emit 0x2b;
+        __emit 0x01;
+        __emit 0x89;
+        __emit 0x50;
+        __emit 0x30;
+        __emit 0xc6;
+        __emit 0x40;
+        __emit 0x3c;
+        __emit 0x01;
+        __emit 0x89;
+        __emit 0x48;
+        __emit 0x40;
+        __emit 0x89;
+        __emit 0x48;
+        __emit 0x44;
+        __emit 0xc3;
+    }
+}
+
+}
