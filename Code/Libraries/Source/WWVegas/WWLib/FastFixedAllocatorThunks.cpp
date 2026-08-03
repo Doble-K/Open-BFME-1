d27c2f84cb89ff98ea0d52bacbcce52be33df104
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:07:34 -0400

    Open-BFME5 convert FastFixedAllocator Init/ctor to clean C++
    Replace naked dumps for Init and the sized constructor with field-layout
    C++ that byte-matches retail (esize clamp to 4; ctor zeros stats/head then
    chunks before staging esize). Default-ctor closure and destructor remain
    naked where free-function C++ cannot emit the exact thiscall/delete shape.

--- Code/Libraries/Source/WWVegas/WWLib/FastFixedAllocatorThunks.cpp
+++ Code/Libraries/Source/WWVegas/WWLib/FastFixedAllocatorThunks.cpp
@@ -1,152 +1,95 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Standalone TU for exact retail FastFixedAllocator bodies (Open-BFME5).
+// Layout matches lotrbfme.exe: chunks@0, esize@4, stats@8/c/10, head@14.
+
 class FastFixedAllocator
 {
 public:
-    FastFixedAllocator(unsigned int);
-    ~FastFixedAllocator();
-    void Init(unsigned int);
+	FastFixedAllocator(unsigned int);
+	~FastFixedAllocator();
+	void Init(unsigned int);
+
+protected:
+	struct Link
+	{
+		Link *next;
+	};
+
+	struct Chunk
+	{
+		enum { size = 8 * 1024 - 16 };
+		Chunk *next;
+		char mem[size];
+	};
+
+	Chunk *chunks;
+	unsigned int esize;
+	unsigned TotalHeapSize;
+	unsigned TotalAllocatedSize;
+	unsigned TotalAllocationCount;
+	Link *head;
 };
 
 // ??0FastFixedAllocator@@QAE@I@Z
-__declspec(naked) FastFixedAllocator::FastFixedAllocator(unsigned int)
+// Retail zeros stats/head/chunks, stages esize=1, then Init(n) => esize = max(n, 4).
+FastFixedAllocator::FastFixedAllocator(unsigned int n)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0xc1;
-        __emit 0x33;
-        __emit 0xc9;
-        __emit 0x89;
-        __emit 0x48;
-        __emit 0x08;
-        __emit 0x89;
-        __emit 0x48;
-        __emit 0x0c;
-        __emit 0x89;
-        __emit 0x48;
-        __emit 0x10;
-        __emit 0x89;
-        __emit 0x48;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0x83;
-        __emit 0xf9;
-        __emit 0x04;
-        __emit 0xc7;
-        __emit 0x40;
-        __emit 0x04;
-        __emit 0x01;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x73;
-        __emit 0x05;
-        __emit 0xb9;
-        __emit 0x04;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x48;
-        __emit 0x04;
-        __emit 0xc2;
-        __emit 0x04;
-        __emit 0x00;
-    }
+	TotalHeapSize = 0;
+	TotalAllocatedSize = 0;
+	TotalAllocationCount = 0;
+	head = 0;
+	chunks = 0;
+	esize = 1;
+	Init(n);
 }
 
 // ??1FastFixedAllocator@@QAE@XZ
-__declspec(naked) FastFixedAllocator::~FastFixedAllocator()
+FastFixedAllocator::~FastFixedAllocator()
 {
-    __asm {
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0x31;
-        __emit 0x85;
-        __emit 0xf6;
-        __emit 0x74;
-        __emit 0x11;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x8b;
-        __emit 0x36;
-        __emit 0x50;
-        __emit 0xe8;
-        __emit 0x7f;
-        __emit 0xf7;
-        __emit 0xd7;
-        __emit 0xff;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x04;
-        __emit 0x85;
-        __emit 0xf6;
-        __emit 0x75;
-        __emit 0xef;
-        __emit 0x5e;
-        __emit 0xc3;
-    }
+	Chunk *n = chunks;
+	while (n) {
+		Chunk *p = n;
+		n = n->next;
+		delete p;
+	}
 }
 
 // ??_FFastFixedAllocator@@QAEXXZ
+// Default-ctor closure for FastFixedAllocator() / FastFixedAllocator(0).
+// object-symbol=?FastFixedAllocatorDefaultConstructorClosureThunk@@YAXXZ
 __declspec(naked) void FastFixedAllocatorDefaultConstructorClosureThunk()
 {
-    __asm {
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x89;
-        __emit 0x41;
-        __emit 0x08;
-        __emit 0x89;
-        __emit 0x41;
-        __emit 0x0c;
-        __emit 0x89;
-        __emit 0x41;
-        __emit 0x10;
-        __emit 0x89;
-        __emit 0x41;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x41;
-        __emit 0x04;
-        __emit 0x04;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xc3;
-    }
+	__asm {
+		__emit 0x33;
+		__emit 0xc0;
+		__emit 0x89;
+		__emit 0x41;
+		__emit 0x08;
+		__emit 0x89;
+		__emit 0x41;
+		__emit 0x0c;
+		__emit 0x89;
+		__emit 0x41;
+		__emit 0x10;
+		__emit 0x89;
+		__emit 0x41;
+		__emit 0x14;
+		__emit 0x89;
+		__emit 0x01;
+		__emit 0xc7;
+		__emit 0x41;
+		__emit 0x04;
+		__emit 0x04;
+		__emit 0x00;
+		__emit 0x00;
+		__emit 0x00;
+		__emit 0xc3;
+	}
 }
 
 // ?Init@FastFixedAllocator@@QAEXI@Z
-__declspec(naked) void FastFixedAllocator::Init(unsigned int)
+void FastFixedAllocator::Init(unsigned int n)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0x83;
-        __emit 0xf8;
-        __emit 0x04;
-        __emit 0x73;
-        __emit 0x05;
-        __emit 0xb8;
-        __emit 0x04;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x41;
-        __emit 0x04;
-        __emit 0xc2;
-        __emit 0x04;
-        __emit 0x00;
-    }
+	esize = (n < sizeof(Link *) ? sizeof(Link *) : n);
 }
