fea1f07d66cccab724ea3421af7f3656bc7e0359
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:48:45 -0400

    Open-BFME5 convert PropagandaTowerBehavior destructor to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Behavior/PropagandaTowerBehaviorDestructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/PropagandaTowerBehaviorDestructorThunk.cpp
@@ -1,81 +1,34 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift PropagandaTowerBehavior dtor __emit thunk to clean C++.
+// Retail destroys the member subobject at this+0x18 under an SEH frame, then
+// runs the inlined base dtor (base vtable store) on the normal and unwind
+// paths. The derived class emits no vtable store of its own.
 
-class __declspec(novtable) PropagandaTowerBehavior
+class PropagandaTowerBehaviorMember
+{
+public:
+    ~PropagandaTowerBehaviorMember();
+};
+
+class PropagandaTowerBehaviorBase
+{
+public:
+    virtual ~PropagandaTowerBehaviorBase() {}
+
+private:
+    unsigned char m_pad[0x14];
+};
+
+class __declspec(novtable) PropagandaTowerBehavior : public PropagandaTowerBehaviorBase
 {
 public:
     virtual ~PropagandaTowerBehavior();
+
+private:
+    PropagandaTowerBehaviorMember m_18;
 };
 
 // ??1PropagandaTowerBehavior@@UAE@XZ
-__declspec(naked) PropagandaTowerBehavior::~PropagandaTowerBehavior()
+PropagandaTowerBehavior::~PropagandaTowerBehavior()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x98
-        __emit 0xba
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x18
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x23
-        __emit 0x44
-        __emit 0x68
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
