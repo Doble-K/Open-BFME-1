648de48114653667e1d2bf8497b7a0093ea07148
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 04:18:47 -0400

    Open-BFME5 convert dual-member-then-base ModuleData dtors (ElvenWood/HordeSiege/RousingSpeech/Tunnel)

--- Code/GameEngine/Source/Common/HordeSiegeEngineContainModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/HordeSiegeEngineContainModuleDataDestructorThunk.cpp
@@ -1,110 +1,40 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: HordeSiegeEngineContainModuleData dtor.
 
-class __declspec(novtable) HordeSiegeEngineContainModuleData
+class HordeSiegeEngineContainModuleDataMemberA
 {
 public:
-    virtual ~HordeSiegeEngineContainModuleData();
+	~HordeSiegeEngineContainModuleDataMemberA();
+private:
+	unsigned char m_pad[0x8];
+};
+
+class HordeSiegeEngineContainModuleDataMemberB
+{
+public:
+	~HordeSiegeEngineContainModuleDataMemberB();
+private:
+	unsigned char m_pad[4];
+};
+
+class HordeSiegeEngineContainModuleDataBase
+{
+public:
+	virtual ~HordeSiegeEngineContainModuleDataBase();
+private:
+	unsigned char m_pad[0x220];
+};
+
+class __declspec(novtable) HordeSiegeEngineContainModuleData : public HordeSiegeEngineContainModuleDataBase
+{
+public:
+	virtual ~HordeSiegeEngineContainModuleData();
+private:
+	HordeSiegeEngineContainModuleDataMemberA m_a;
+	HordeSiegeEngineContainModuleDataMemberB m_b;
 };
 
 // ??1HordeSiegeEngineContainModuleData@@UAE@XZ
-__declspec(naked) HordeSiegeEngineContainModuleData::~HordeSiegeEngineContainModuleData()
+HordeSiegeEngineContainModuleData::~HordeSiegeEngineContainModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x96
-        __emit 0xd5
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x8e
-        __emit 0x2c
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x30
-        __emit 0xc8
-        __emit 0x65
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x8e
-        __emit 0x24
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xe1
-        __emit 0xf2
-        __emit 0xde
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0x89
-        __emit 0xc8
-        __emit 0xde
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
--- Code/GameEngine/Source/Common/RousingSpeechUpdateModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/RousingSpeechUpdateModuleDataDestructorThunk.cpp
@@ -1,110 +1,40 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: RousingSpeechUpdateModuleData dtor.
 
-class __declspec(novtable) RousingSpeechUpdateModuleData
+class RousingSpeechUpdateModuleDataMemberA
+{
+public:
+	~RousingSpeechUpdateModuleDataMemberA();
+private:
+	unsigned char m_pad[0x10];
+};
+
+class RousingSpeechUpdateModuleDataMemberB
+{
+public:
+	~RousingSpeechUpdateModuleDataMemberB();
+private:
+	unsigned char m_pad[4];
+};
+
+class RousingSpeechUpdateModuleDataBase
+{
+public:
+	virtual ~RousingSpeechUpdateModuleDataBase();
+private:
+	unsigned char m_pad[0x26c];
+};
+
+class __declspec(novtable) RousingSpeechUpdateModuleData : public RousingSpeechUpdateModuleDataBase
 {
 public:
 	virtual ~RousingSpeechUpdateModuleData();
+private:
+	RousingSpeechUpdateModuleDataMemberA m_a;
+	RousingSpeechUpdateModuleDataMemberB m_b;
 };
 
 // ??1RousingSpeechUpdateModuleData@@UAE@XZ
-__declspec(naked) RousingSpeechUpdateModuleData::~RousingSpeechUpdateModuleData()
+RousingSpeechUpdateModuleData::~RousingSpeechUpdateModuleData()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x76
-		__emit 0xfb
-		__emit 0x00
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x04
-		__emit 0x8d
-		__emit 0x8e
-		__emit 0x80
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0xd1
-		__emit 0x4b
-		__emit 0xdb
-		__emit 0xff
-		__emit 0x8d
-		__emit 0x8e
-		__emit 0x70
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x72
-		__emit 0x12
-		__emit 0xdc
-		__emit 0xff
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0xbe
-		__emit 0x3f
-		__emit 0xdb
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
-	}
 }
