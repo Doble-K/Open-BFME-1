f56c58901fb36d83bb3a3dd036685b2d4c324613
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 04:15:57 -0400

    Open-BFME5 convert ScriptConditions::evaluateNamedOwnedByPlayer MASM

---
+++ Code/GameEngine/Source/Common/ScriptConditions_evaluateNamedOwnedByPlayer_Thunk.cpp
@@ -0,0 +1,131 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to standalone C++ thunk.
+
+class Parameter;
+class ScriptConditions
+{
+protected:
+	bool evaluateNamedOwnedByPlayer(Parameter *, Parameter *);
+};
+
+// ?evaluateNamedOwnedByPlayer@ScriptConditions@@IAE_NPAVParameter@@0@Z
+__declspec(naked) bool ScriptConditions::evaluateNamedOwnedByPlayer(Parameter *, Parameter *)
+{
+	__asm {
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x6c
+        __emit 0x07
+        __emit 0x2f
+        __emit 0x01
+        __emit 0x8b
+        __emit 0x54
+        __emit 0x24
+        __emit 0x04
+        __emit 0x8b
+        __emit 0x01
+        __emit 0x57
+        __emit 0x52
+        __emit 0xff
+        __emit 0x50
+        __emit 0x68
+        __emit 0x8b
+        __emit 0xf8
+        __emit 0x85
+        __emit 0xff
+        __emit 0x75
+        __emit 0x06
+        __emit 0x32
+        __emit 0xc0
+        __emit 0x5f
+        __emit 0xc2
+        __emit 0x08
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x44
+        __emit 0x24
+        __emit 0x0c
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x6c
+        __emit 0x07
+        __emit 0x2f
+        __emit 0x01
+        __emit 0x56
+        __emit 0x50
+        __emit 0xe8
+        __emit 0x07
+        __emit 0x09
+        __emit 0xd0
+        __emit 0xff
+        __emit 0x66
+        __emit 0x85
+        __emit 0xc0
+        __emit 0x89
+        __emit 0x44
+        __emit 0x24
+        __emit 0x0c
+        __emit 0x74
+        __emit 0x2e
+        __emit 0xeb
+        __emit 0x07
+        __emit 0x8d
+        __emit 0xa4
+        __emit 0x24
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x8d
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x0c
+        __emit 0x51
+        __emit 0x8b
+        __emit 0x0d
+        __emit 0x48
+        __emit 0xd7
+        __emit 0x2e
+        __emit 0x01
+        __emit 0xe8
+        __emit 0x90
+        __emit 0xc6
+        __emit 0xd0
+        __emit 0xff
+        __emit 0x8b
+        __emit 0xcf
+        __emit 0x8b
+        __emit 0xf0
+        __emit 0xe8
+        __emit 0x4b
+        __emit 0xe0
+        __emit 0xcf
+        __emit 0xff
+        __emit 0x3b
+        __emit 0xc6
+        __emit 0x74
+        __emit 0x0f
+        __emit 0x66
+        __emit 0x83
+        __emit 0x7c
+        __emit 0x24
+        __emit 0x0c
+        __emit 0x00
+        __emit 0x75
+        __emit 0xdb
+        __emit 0x5e
+        __emit 0x32
+        __emit 0xc0
+        __emit 0x5f
+        __emit 0xc2
+        __emit 0x08
+        __emit 0x00
+        __emit 0x5e
+        __emit 0xb0
+        __emit 0x01
+        __emit 0x5f
+        __emit 0xc2
+        __emit 0x08
+        __emit 0x00
+	}
+}
