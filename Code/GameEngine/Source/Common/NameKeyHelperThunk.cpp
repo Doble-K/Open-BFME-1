f69aa6fcae276e71686171f13cdb065cb0330be9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 05:23:13 -0400

    Open-BFME5 convert NAMEKEY(AsciiString) helper to clean C++

--- Code/GameEngine/Source/Common/NameKeyHelperThunk.cpp
+++ Code/GameEngine/Source/Common/NameKeyHelperThunk.cpp
@@ -1,60 +1,43 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+// Open-BFME5: NAMEKEY(const AsciiString&)
+// Retail: if data non-null, nameToKey(data+8); else nameToKey(empty literal).
 
 enum NameKeyType
 {
-    NAMEKEY_INVALID = 0
+	NAMEKEY_INVALID = 0
+};
+
+struct AsciiStringData
+{
+	int m_refCount;
+	int m_numChars;
+	// char payload follows at +8
 };
 
 class AsciiString
 {
+public:
+	AsciiStringData *m_data;
 };
 
-NameKeyType NAMEKEY(const AsciiString &)
+class NameKeyGenerator
 {
-    __asm {
-        _emit 08Bh
-        _emit 044h
-        _emit 024h
-        _emit 004h
-        _emit 08Bh
-        _emit 000h
-        _emit 085h
-        _emit 0C0h
-        _emit 074h
-        _emit 010h
-        _emit 08Bh
-        _emit 00Dh
-        _emit 000h
-        _emit 0D6h
-        _emit 02Eh
-        _emit 001h
-        _emit 083h
-        _emit 0C0h
-        _emit 008h
-        _emit 050h
-        _emit 0E8h
-        _emit 00Eh
-        _emit 073h
-        _emit 0F9h
-        _emit 0FFh
-        _emit 0C3h
-        _emit 08Bh
-        _emit 00Dh
-        _emit 000h
-        _emit 0D6h
-        _emit 02Eh
-        _emit 001h
-        _emit 0B8h
-        _emit 08Bh
-        _emit 038h
-        _emit 007h
-        _emit 001h
-        _emit 050h
-        _emit 0E8h
-        _emit 0FCh
-        _emit 072h
-        _emit 0F9h
-        _emit 0FFh
-        _emit 0C3h
-    }
+public:
+	NameKeyType nameToKey(const char *s);
+};
+
+// DIR32: TheNameKeyGenerator
+extern NameKeyGenerator *g_theNameKeyGenerator;
+// empty string literal for null AsciiString data
+extern "C" char g_NAMEKEY_empty_string;
+
+// ?NAMEKEY@@YA?AW4NameKeyType@@ABVAsciiString@@@Z
+NameKeyType NAMEKEY(const AsciiString &s)
+{
+	const char *p;
+	if (s.m_data)
+		p = (const char *)s.m_data + 8;
+	else
+		p = &g_NAMEKEY_empty_string;
+	return g_theNameKeyGenerator->nameToKey(p);
 }
