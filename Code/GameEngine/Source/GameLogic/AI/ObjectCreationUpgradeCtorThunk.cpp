852ee5fa267f41a66c6673a5604505e2aae33554
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 20:12:24 -0400

    Open-BFME5 convert ObjectCreationUpgrade constructor

---
+++ Code/GameEngine/Source/GameLogic/AI/ObjectCreationUpgradeCtorThunk.cpp
@@ -0,0 +1,133 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Thing;
+class ModuleData;
+
+class ObjectCreationUpgrade
+{
+public:
+    ObjectCreationUpgrade(Thing *, const ModuleData *);
+};
+
+// ??0ObjectCreationUpgrade@@QAE@PAVThing@@PBVModuleData@@@Z
+__declspec(naked) ObjectCreationUpgrade::ObjectCreationUpgrade(Thing *, const ModuleData *)
+{
+    __asm {
+        __emit 0x56;
+        __emit 0x57;
+        __emit 0x8b;
+        __emit 0xf9;
+        __emit 0xe8;
+        __emit 0xb5;
+        __emit 0x64;
+        __emit 0xd6;
+        __emit 0xff;
+        __emit 0x8b;
+        __emit 0x44;
+        __emit 0x24;
+        __emit 0x10;
+        __emit 0x8b;
+        __emit 0x4c;
+        __emit 0x24;
+        __emit 0x0c;
+        __emit 0x50;
+        __emit 0x8d;
+        __emit 0x77;
+        __emit 0x08;
+        __emit 0x51;
+        __emit 0x8b;
+        __emit 0xce;
+        __emit 0xe8;
+        __emit 0x37;
+        __emit 0x03;
+        __emit 0xd4;
+        __emit 0xff;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x0c;
+        __emit 0xd0;
+        __emit 0xc9;
+        __emit 0x09;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x10;
+        __emit 0xa0;
+        __emit 0xcb;
+        __emit 0x09;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x06;
+        __emit 0x94;
+        __emit 0xcd;
+        __emit 0x09;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x0c;
+        __emit 0xd0;
+        __emit 0xcc;
+        __emit 0x09;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x46;
+        __emit 0x10;
+        __emit 0xac;
+        __emit 0xcb;
+        __emit 0x09;
+        __emit 0x01;
+        __emit 0x33;
+        __emit 0xc0;
+        __emit 0x89;
+        __emit 0x46;
+        __emit 0x14;
+        __emit 0x83;
+        __emit 0xc9;
+        __emit 0xff;
+        __emit 0x89;
+        __emit 0x4e;
+        __emit 0x18;
+        __emit 0x89;
+        __emit 0x4e;
+        __emit 0x1c;
+        __emit 0x89;
+        __emit 0x47;
+        __emit 0x28;
+        __emit 0x88;
+        __emit 0x47;
+        __emit 0x2c;
+        __emit 0xc7;
+        __emit 0x07;
+        __emit 0x20;
+        __emit 0xd6;
+        __emit 0x0c;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x47;
+        __emit 0x14;
+        __emit 0x58;
+        __emit 0xd5;
+        __emit 0x0c;
+        __emit 0x01;
+        __emit 0xc7;
+        __emit 0x47;
+        __emit 0x18;
+        __emit 0x4c;
+        __emit 0xd5;
+        __emit 0x0c;
+        __emit 0x01;
+        __emit 0x8b;
+        __emit 0xc7;
+        __emit 0x5f;
+        __emit 0xc7;
+        __emit 0x06;
+        __emit 0x18;
+        __emit 0xd5;
+        __emit 0x0c;
+        __emit 0x01;
+        __emit 0x5e;
+        __emit 0xc2;
+        __emit 0x08;
+        __emit 0x00;
+    }
+}
