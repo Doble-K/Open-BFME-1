7fd3d834f8e96d45eae27dd4c1ef965538c8e907
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:01:09 -0400

    Open-BFME5 convert SpawnUnitBehavior constructor to clean C++

--- Code/GameEngine/Source/GameLogic/AI/SpawnUnitBehaviorCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/SpawnUnitBehaviorCtorThunk.cpp
@@ -1,116 +1,70 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHs-
+// Open-BFME5: lift SpawnUnitBehavior ctor __emit thunk to clean C++.
+// Retail shape: grandbase ctor call with both args, interface-base vtable
+// stores at +0x0C/+0x10, field inits (+0x18/+0x1C = -1, +0x14 = 0), vtable
+// store for this class's own interface base at +0x20, then the four
+// most-derived vtable stores and a byte copied from the object at this+4.
+// /EHs- because retail has no unwind frame.
 
 class Thing;
 class ModuleData;
 
-class SpawnUnitBehavior
+class SpawnUnitBehaviorGrandBase
+{
+public:
+    SpawnUnitBehaviorGrandBase(Thing *, const ModuleData *);
+    virtual ~SpawnUnitBehaviorGrandBase();
+
+protected:
+    unsigned int m_04;
+
+private:
+    unsigned char m_pad[4];
+};
+
+class SpawnUnitBehaviorIface1
+{
+public:
+    virtual ~SpawnUnitBehaviorIface1();
+};
+
+class SpawnUnitBehaviorIface2
+{
+public:
+    virtual ~SpawnUnitBehaviorIface2();
+};
+
+class SpawnUnitBehaviorIface3
+{
+public:
+    virtual ~SpawnUnitBehaviorIface3();
+};
+
+class SpawnUnitBehaviorBase : public SpawnUnitBehaviorGrandBase, public SpawnUnitBehaviorIface1, public SpawnUnitBehaviorIface2
+{
+public:
+    SpawnUnitBehaviorBase(Thing *t, const ModuleData *m)
+        : SpawnUnitBehaviorGrandBase(t, m), m_14(0), m_18(-1), m_1C(-1) {}
+    virtual ~SpawnUnitBehaviorBase();
+
+private:
+    unsigned int m_14;
+    int m_18;
+    int m_1C;
+};
+
+class SpawnUnitBehavior : public SpawnUnitBehaviorBase, public SpawnUnitBehaviorIface3
 {
 public:
     SpawnUnitBehavior(Thing *, const ModuleData *);
+
+private:
+    unsigned char m_24;
 };
 
 // ??0SpawnUnitBehavior@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) SpawnUnitBehavior::SpawnUnitBehavior(Thing *, const ModuleData *)
+SpawnUnitBehavior::SpawnUnitBehavior(Thing *t, const ModuleData *m)
+    : SpawnUnitBehaviorBase(t, m)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0xe0;
-        __emit 0x9f;
-        __emit 0xe0;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc8;
-        __emit 0xff;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x1c;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0xbc;
-        __emit 0x6e;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0x56;
-        __emit 0x04;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x94;
-        __emit 0x6f;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0x6e;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xc4;
-        __emit 0x6e;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0xc0;
-        __emit 0x6e;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0x8a;
-        __emit 0x42;
-        __emit 0x1c;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+    m_24 = *((unsigned char *)m_04 + 0x1C);
 }
