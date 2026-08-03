06e280d93d0b49d80b627f4d56c5a8400d920bb7
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-02 22:41:04 -0400

    Open-BFME5 convert RenderObjectUpdateModuleTemplate copy constructor

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/fx_particle_system_bulk.cpp
@@ -14373,74 +14373,9 @@ __declspec(naked) RenderObjectUpdateModuleInfo::RenderObjectUpdateModuleInfo()
     }
 }
 
-// ??0RenderObjectUpdateModuleTemplate@FXParticleSystem@@QAE@ABV01@@Z
-__declspec(naked) RenderObjectUpdateModuleTemplate::RenderObjectUpdateModuleTemplate(const RenderObjectUpdateModuleTemplate &that)
-{
-    __asm {
-        __emit 0x56
-        __emit 0x57
-        __emit 0x8b
-        __emit 0x7c
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0xe8
-        __emit 0x07
-        __emit 0x3d
-        __emit 0xa4
-        __emit 0xff
-        __emit 0x85
-        __emit 0xff
-        __emit 0x74
-        __emit 0x05
-        __emit 0x8d
-        __emit 0x47
-        __emit 0x08
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xc0
-        __emit 0x8d
-        __emit 0x7e
-        __emit 0x08
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xe8
-        __emit 0xb8
-        __emit 0x79
-        __emit 0xa5
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x07
-        __emit 0xa4
-        __emit 0x0d
-        __emit 0x11
-        __emit 0x01
-        __emit 0x5f
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x90
-        __emit 0x0d
-        __emit 0x11
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x04
-        __emit 0x8c
-        __emit 0x0d
-        __emit 0x11
-        __emit 0x01
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-    }
-}
+// ??0RenderObjectUpdateModuleTemplate@FXParticleSystem@@QAE@ABV01@@Z is
+// implemented by the exact retail thunk in
+// RenderObjectUpdateModuleTemplateCopyCtorThunk.cpp.
 
 // ??0RenderObjectUpdateModuleTemplate@FXParticleSystem@@QAE@XZ
 __declspec(naked) RenderObjectUpdateModuleTemplate::RenderObjectUpdateModuleTemplate()
