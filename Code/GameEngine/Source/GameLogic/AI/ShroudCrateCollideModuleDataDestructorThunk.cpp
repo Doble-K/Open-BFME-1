0afc92253394bea8237d6e2ed489ed155a030426
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:05:01 -0400

    Open-BFME5 convert six more Buffer-member ModuleData SEH dtors
    Heal/Money/Shroud/ConvertToCarBomb crate + FadeAndDieOrnament + Topple;
    PropagandaTower pattern with Buffer pin.

--- Code/GameEngine/Source/GameLogic/AI/ShroudCrateCollideModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/ShroudCrateCollideModuleDataDestructorThunk.cpp
@@ -1,81 +1,29 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: ShroudCrateCollideModuleData dtor. SEH Buffer member pin body 0x887940.
 
-class __declspec(novtable) ShroudCrateCollideModuleData
+class Buffer
 {
 public:
-    virtual ~ShroudCrateCollideModuleData();
+	~Buffer();
+};
+
+class ShroudCrateCollideModuleDataBase
+{
+public:
+	virtual ~ShroudCrateCollideModuleDataBase() {}
+private:
+	unsigned char m_pad[0x40];
+};
+
+class __declspec(novtable) ShroudCrateCollideModuleData : public ShroudCrateCollideModuleDataBase
+{
+public:
+	virtual ~ShroudCrateCollideModuleData();
+private:
+	Buffer m_buffer;
 };
 
 // ??1ShroudCrateCollideModuleData@@UAE@XZ
-__declspec(naked) ShroudCrateCollideModuleData::~ShroudCrateCollideModuleData()
+ShroudCrateCollideModuleData::~ShroudCrateCollideModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x38
-        __emit 0x18
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x44
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xd3
-        __emit 0x1a
-        __emit 0x76
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
--- Code/GameEngine/Source/GameLogic/Object/Die/FadeAndDieOrnamentUpdateModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Die/FadeAndDieOrnamentUpdateModuleDataDestructorThunk.cpp
@@ -1,82 +1,29 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift ModuleData dtor MASM dump to standalone C++ thunk.
+// Open-BFME5: FadeAndDieOrnamentUpdateModuleData dtor. SEH Buffer member pin body 0x887940.
 
-class __declspec(novtable) FadeAndDieOrnamentUpdateModuleData
+class Buffer
 {
 public:
-    virtual ~FadeAndDieOrnamentUpdateModuleData();
+	~Buffer();
+};
+
+class FadeAndDieOrnamentUpdateModuleDataBase
+{
+public:
+	virtual ~FadeAndDieOrnamentUpdateModuleDataBase() {}
+private:
+	unsigned char m_pad[0x4];
+};
+
+class __declspec(novtable) FadeAndDieOrnamentUpdateModuleData : public FadeAndDieOrnamentUpdateModuleDataBase
+{
+public:
+	virtual ~FadeAndDieOrnamentUpdateModuleData();
+private:
+	Buffer m_buffer;
 };
 
 // ??1FadeAndDieOrnamentUpdateModuleData@@UAE@XZ
-__declspec(naked) FadeAndDieOrnamentUpdateModuleData::~FadeAndDieOrnamentUpdateModuleData()
+FadeAndDieOrnamentUpdateModuleData::~FadeAndDieOrnamentUpdateModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xa8
-        __emit 0x1a
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xa3
-        __emit 0x0a
-        __emit 0x76
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
