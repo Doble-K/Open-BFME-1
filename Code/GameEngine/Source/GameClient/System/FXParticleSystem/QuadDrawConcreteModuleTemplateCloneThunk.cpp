6ecf53ad3788e94e73e35a63fae81406ea7330e2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:07:43 -0400

    Open-BFME5 convert QuadDrawModuleTemplate clone to clean C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/QuadDrawConcreteModuleTemplateCloneThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/QuadDrawConcreteModuleTemplateCloneThunk.cpp
@@ -1,4 +1,7 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: QuadDrawModuleTemplate ConcreteModuleTemplate::clone
+// MSVC 7.1: integral N mangles as $(N-1) for N>=1 ($03 => N=4).
 
 namespace FXParticleSystem
 {
@@ -18,6 +21,8 @@ class QuadDrawModule
 
 class QuadDrawModuleTemplate
 {
+public:
+	virtual ~QuadDrawModuleTemplate();
 };
 
 extern const char QUAD_DRAW_MODULE_KEY[1];
@@ -32,50 +37,26 @@ template <class Tag>
 class ConcreteModuleTemplate;
 
 typedef ModuleTag<6, QUAD_DRAW_MODULE_KEY, QUAD_DRAW_MODULE_NAME,
-    QuadDrawModule, QuadDrawModuleTemplate,
-    DefaultParticleModule<6>, DefaultParticleModuleTemplate<6> > QuadDrawTag;
+	QuadDrawModule, QuadDrawModuleTemplate,
+	DefaultParticleModule<6>, DefaultParticleModuleTemplate<6> > QuadDrawTag;
+
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
 
 template <>
-class ConcreteModuleTemplate<QuadDrawTag>
+class ConcreteModuleTemplate<QuadDrawTag> : public QuadDrawModuleTemplate
 {
 public:
-    virtual QuadDrawModuleTemplate *clone() const;
+	ConcreteModuleTemplate(const ConcreteModuleTemplate &);
+	virtual QuadDrawModuleTemplate *clone() const;
+
+private:
+	unsigned char m_pad[0x8];
 };
 
-__declspec(naked) QuadDrawModuleTemplate *ConcreteModuleTemplate<QuadDrawTag>::clone() const
+QuadDrawModuleTemplate *ConcreteModuleTemplate<QuadDrawTag>::clone() const
 {
-    __asm {
-        _emit 056h
-        _emit 06Ah
-        _emit 00Ch
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 0D6h
-        _emit 071h
-        _emit 02Ah
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 004h
-        _emit 085h
-        _emit 0C0h
-        _emit 074h
-        _emit 00Ah
-        _emit 056h
-        _emit 08Bh
-        _emit 0C8h
-        _emit 0E8h
-        _emit 024h
-        _emit 074h
-        _emit 0A4h
-        _emit 0FFh
-        _emit 05Eh
-        _emit 0C3h
-        _emit 033h
-        _emit 0C0h
-        _emit 05Eh
-        _emit 0C3h
-    }
+	return new ConcreteModuleTemplate<QuadDrawTag>(*this);
 }
+
 }
