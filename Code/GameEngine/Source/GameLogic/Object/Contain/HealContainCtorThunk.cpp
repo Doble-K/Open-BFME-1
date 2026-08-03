cc31202854deaa479620c1b46ec8e1b8ef8a0017
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 04:22:45 -0400

    Open-BFME5 convert HealContain constructor to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Contain/HealContainCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Contain/HealContainCtorThunk.cpp
@@ -1,105 +1,55 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHs-
+// Open-BFME5: lift HealContain ctor __emit thunk to clean C++. Retail calls
+// the out-of-line base ctor with both args, then batches nine most-derived
+// vtable stores at +0x00/+0x0C/+0x10/+0x20/+0x24/+0x28/+0x2C/+0x30/+0x34;
+// every polymorphic subobject is nested inside the base. /EHs- because
+// retail has no unwind frame.
 
 class Thing;
 class ModuleData;
 
-class HealContain
+class HealContainGrandBase
+{
+public:
+    HealContainGrandBase(Thing *, const ModuleData *);
+    virtual ~HealContainGrandBase();
+
+private:
+    unsigned char m_pad[8];
+};
+
+class HealContainIface1 { public: virtual ~HealContainIface1(); };
+class HealContainIface2 { public: virtual ~HealContainIface2(); private: unsigned char m_pad[0xC]; };
+class HealContainIface3 { public: virtual ~HealContainIface3(); };
+class HealContainIface4 { public: virtual ~HealContainIface4(); };
+class HealContainIface5 { public: virtual ~HealContainIface5(); };
+class HealContainIface6 { public: virtual ~HealContainIface6(); };
+class HealContainIface7 { public: virtual ~HealContainIface7(); };
+class HealContainIface8 { public: virtual ~HealContainIface8(); };
+
+class HealContainBase : public HealContainGrandBase,
+                        public HealContainIface1,
+                        public HealContainIface2,
+                        public HealContainIface3,
+                        public HealContainIface4,
+                        public HealContainIface5,
+                        public HealContainIface6,
+                        public HealContainIface7,
+                        public HealContainIface8
+{
+public:
+    HealContainBase(Thing *, const ModuleData *);
+    virtual ~HealContainBase();
+};
+
+class HealContain : public HealContainBase
 {
 public:
     HealContain(Thing *, const ModuleData *);
 };
 
 // ??0HealContain@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) HealContain::HealContain(Thing *, const ModuleData *)
+HealContain::HealContain(Thing *t, const ModuleData *m)
+    : HealContainBase(t, m)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0xa5;
-        __emit 0x6c;
-        __emit 0xe1;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x78;
-        __emit 0xbc;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xb0;
-        __emit 0xbb;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xbb;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0xf8;
-        __emit 0xb9;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0xdc;
-        __emit 0xb9;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x28;
-        __emit 0xd8;
-        __emit 0xb9;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x2c;
-        __emit 0xc8;
-        __emit 0xb9;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x30;
-        __emit 0x8c;
-        __emit 0xb9;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x34;
-        __emit 0x7c;
-        __emit 0xb9;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
 }
