ae1c0813a09c42bb6dadae78f6b72f1ea0d86b8c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 04:06:57 -0400

    Open-BFME5 convert AutoPickUp/BroadcastStealth ModuleData dual-member dtors
    Two different size-4 members with distinct dtor pins; intermediate base
    pad for member placement. Focused MSVC 7.1 byte-match.

--- Code/GameEngine/Source/Common/BroadcastStealthUpdateModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/BroadcastStealthUpdateModuleDataDestructorThunk.cpp
@@ -1,95 +1,40 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: BroadcastStealthUpdateModuleData dtor. dual different members SEH.
 
-class __declspec(novtable) BroadcastStealthUpdateModuleData
+class BroadcastStealthUpdateModuleDataMemberA
 {
 public:
-    virtual ~BroadcastStealthUpdateModuleData();
+	~BroadcastStealthUpdateModuleDataMemberA();
+private:
+	unsigned char m_pad[4];
+};
+
+class BroadcastStealthUpdateModuleDataMemberB
+{
+public:
+	~BroadcastStealthUpdateModuleDataMemberB();
+private:
+	unsigned char m_pad[4];
+};
+
+class BroadcastStealthUpdateModuleDataBase
+{
+public:
+	virtual ~BroadcastStealthUpdateModuleDataBase() {}
+private:
+	unsigned char m_pad[0x24];
+};
+
+class __declspec(novtable) BroadcastStealthUpdateModuleData : public BroadcastStealthUpdateModuleDataBase
+{
+public:
+	virtual ~BroadcastStealthUpdateModuleData();
+private:
+	BroadcastStealthUpdateModuleDataMemberA m_a;
+	BroadcastStealthUpdateModuleDataMemberB m_b;
 };
 
 // ??1BroadcastStealthUpdateModuleData@@UAE@XZ
-__declspec(naked) BroadcastStealthUpdateModuleData::~BroadcastStealthUpdateModuleData()
+BroadcastStealthUpdateModuleData::~BroadcastStealthUpdateModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x63
-        __emit 0x23
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x2c
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x60
-        __emit 0x0b
-        __emit 0xef
-        __emit 0xff
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x28
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x16
-        __emit 0xcb
-        __emit 0x75
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
