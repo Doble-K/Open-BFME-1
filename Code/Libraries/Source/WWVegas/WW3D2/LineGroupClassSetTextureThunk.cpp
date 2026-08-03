b2371dcceb60e9f886ea183b8f63f41c492a284c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 01:09:06 -0400

    Open-BFME5 convert FastFixedAllocator default-ctor closure to clean C++
    Replace residual naked ??_F dump with member DefaultConstructorClosure
    that zeros stats/head/chunks and sets esize=4; focused build byte-matches.

--- Code/Libraries/Source/WWVegas/WW3D2/LineGroupClassSetTextureThunk.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/LineGroupClassSetTextureThunk.cpp
@@ -1,66 +1,34 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class TextureClass;
+class TextureClass
+{
+public:
+	void Release_Ref();
+};
 
 class LineGroupClass
 {
 public:
-    void Set_Texture(TextureClass *texture);
+	void Set_Texture(TextureClass *texture);
+
+private:
+	char pad[0x24];
+	TextureClass *Texture;
 };
 
 // ?Set_Texture@LineGroupClass@@QAEXPAVTextureClass@@@Z
-__declspec(naked) void LineGroupClass::Set_Texture(TextureClass *)
+void LineGroupClass::Set_Texture(TextureClass *texture)
 {
-    __asm {
-        __emit 0x56
-        __emit 0x57
-        __emit 0x8b
-        __emit 0x7c
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x8b
-        __emit 0x07
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x74
-        __emit 0x04
-        __emit 0x66
-        __emit 0xff
-        __emit 0x40
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x4e
-        __emit 0x24
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0f
-        __emit 0xe8
-        __emit 0x02
-        __emit 0xcb
-        __emit 0x05
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x07
-        __emit 0x5f
-        __emit 0x89
-        __emit 0x46
-        __emit 0x24
-        __emit 0x5e
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x0f
-        __emit 0x5f
-        __emit 0x89
-        __emit 0x4e
-        __emit 0x24
-        __emit 0x5e
-        __emit 0xc2
-        __emit 0x04
-        __emit 0x00
-    }
+	TextureClass **next = reinterpret_cast<TextureClass **>(texture);
+	if (*next)
+		++*reinterpret_cast<unsigned short *>(reinterpret_cast<char *>(*next) + 4);
+	if (Texture)
+	{
+		Texture->Release_Ref();
+		Texture = *next;
+	}
+	else
+	{
+		Texture = *next;
+	}
 }
