783d91834bc60825fd411d0727cc3c4341dcb7ef
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:12:46 -0400

    Open-BFME5 convert CountUpTransition destructor to clean C++
    Early vtbl, zero field, dual Buffer members, pinned base dtor.

--- Code/GameEngine/Source/Common/CountUpTransitionDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/CountUpTransitionDestructorThunk.cpp
@@ -1,117 +1,35 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: CountUpTransition dtor.
+// Early derived vtbl, zero +0x0c, dual Buffer @+0x2c/+0x30, base dtor.
 
-class __declspec(novtable) CountUpTransition
+class CountUpBuffer
 {
 public:
-    virtual ~CountUpTransition();
+	~CountUpBuffer();
+private:
+	unsigned char m_pad[4];
+};
+
+class CountUpTransitionBase
+{
+public:
+	virtual ~CountUpTransitionBase();
+};
+
+class CountUpTransition : public CountUpTransitionBase
+{
+public:
+	virtual ~CountUpTransition();
+private:
+	unsigned char m_gap[8]; // +0x04 .. +0x0b
+	unsigned int m_zero; // +0x0c
+	unsigned char m_gap2[0x1c]; // +0x10 .. +0x2b
+	CountUpBuffer m_a; // +0x2c
+	CountUpBuffer m_b; // +0x30
 };
 
 // ??1CountUpTransition@@UAE@XZ
-__declspec(naked) CountUpTransition::~CountUpTransition()
+CountUpTransition::~CountUpTransition()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xd3
-        __emit 0x81
-        __emit 0x03
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x88
-        __emit 0xc6
-        __emit 0x10
-        __emit 0x01
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x30
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x36
-        __emit 0xc4
-        __emit 0x2e
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x2c
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x29
-        __emit 0xc4
-        __emit 0x2e
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0xe5
-        __emit 0xec
-        __emit 0xa7
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
+	m_zero = 0;
 }
