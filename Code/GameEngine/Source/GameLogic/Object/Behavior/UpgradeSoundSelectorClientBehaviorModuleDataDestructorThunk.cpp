2e302aab83a1abcdd42b3b96c2e095965d8082b1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:09:36 -0400

    Open-BFME5 convert six ModuleData SEH dtors (PropagandaTower pattern)
    CreateObjectDie/UpgradeDie Buffer; AnimationSound/Enraged/sound-selector
    members with pinned dtors; focused build byte-matches retail 67B.

--- Code/GameEngine/Source/GameLogic/Object/Behavior/UpgradeSoundSelectorClientBehaviorModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/UpgradeSoundSelectorClientBehaviorModuleDataDestructorThunk.cpp
@@ -1,82 +1,29 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift ModuleData dtor MASM dump to standalone C++ thunk.
+// Open-BFME5: UpgradeSoundSelectorClientBehaviorModuleData dtor. PropagandaTower SEH pattern.
 
-class __declspec(novtable) UpgradeSoundSelectorClientBehaviorModuleData
+class UpgradeSoundSelectorClientBehaviorModuleDataMember
 {
 public:
-    virtual ~UpgradeSoundSelectorClientBehaviorModuleData();
+	~UpgradeSoundSelectorClientBehaviorModuleDataMember();
+};
+
+class UpgradeSoundSelectorClientBehaviorModuleDataBase
+{
+public:
+	virtual ~UpgradeSoundSelectorClientBehaviorModuleDataBase() {}
+private:
+	unsigned char m_pad[0x4];
+};
+
+class __declspec(novtable) UpgradeSoundSelectorClientBehaviorModuleData : public UpgradeSoundSelectorClientBehaviorModuleDataBase
+{
+public:
+	virtual ~UpgradeSoundSelectorClientBehaviorModuleData();
+private:
+	UpgradeSoundSelectorClientBehaviorModuleDataMember m_member;
 };
 
 // ??1UpgradeSoundSelectorClientBehaviorModuleData@@UAE@XZ
-__declspec(naked) UpgradeSoundSelectorClientBehaviorModuleData::~UpgradeSoundSelectorClientBehaviorModuleData()
+UpgradeSoundSelectorClientBehaviorModuleData::~UpgradeSoundSelectorClientBehaviorModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x08
-        __emit 0xda
-        __emit 0x03
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xea
-        __emit 0x5a
-        __emit 0xa1
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
