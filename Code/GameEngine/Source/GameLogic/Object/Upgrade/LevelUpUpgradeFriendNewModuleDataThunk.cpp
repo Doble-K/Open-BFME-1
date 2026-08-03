319426d780422809ec869a883b714d0d838d15af
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:36:40 -0400

    Open-BFME5 convert LevelUpUpgrade::friend_newModuleData to clean C++
    new(0x78); UpgradeModuleDataSub ctor at +8; outer vtbl; zero
    dwords at +0x70 and +0x74 (CastleUpgrade-style factory layout).

--- Code/GameEngine/Source/GameLogic/Object/Upgrade/LevelUpUpgradeFriendNewModuleDataThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/LevelUpUpgradeFriendNewModuleDataThunk.cpp
@@ -1,89 +1,68 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-class INI;
+// Open-BFME5: LevelUpUpgrade::friend_newModuleData
+// Retail: new(0x78); sub-ctor at +8; outer vtbl; dword +0x70/+0x74 = 0.
+
 class ModuleData;
+
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
+
+class UpgradeModuleDataSub
+{
+public:
+	UpgradeModuleDataSub();
+
+private:
+	char opaque[0x68];
+};
+
+class LevelUpUpgradeModuleData
+{
+public:
+	LevelUpUpgradeModuleData()
+	{
+		m_extra0 = 0;
+		m_extra1 = 0;
+	}
+	virtual void dummy();
+
+private:
+	int m_pad;
+	char m_sub_space[0x68];
+	unsigned int m_extra0;
+	unsigned int m_extra1;
+};
+
+class INI
+{
+public:
+	void initFromINI(void *what, const void *parseTable);
+};
+
+extern "C" char LevelUpUpgradeFieldParse;
 
 class LevelUpUpgrade
 {
 public:
-    static ModuleData *friend_newModuleData(INI *ini);
+	static ModuleData *friend_newModuleData(INI *ini);
 };
 
 // ?friend_newModuleData@LevelUpUpgrade@@SAPAVModuleData@@PAVINI@@@Z
-__declspec(naked) ModuleData *LevelUpUpgrade::friend_newModuleData(INI *)
+ModuleData *LevelUpUpgrade::friend_newModuleData(INI *ini)
 {
-    __asm {
-        __emit 0x56
-        __emit 0x6a
-        __emit 0x78
-        __emit 0xe8
-        __emit 0x08
-        __emit 0x6c
-        __emit 0x75
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xf0
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x74
-        __emit 0x1e
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x08
-        __emit 0xe8
-        __emit 0x6d
-        __emit 0x3d
-        __emit 0xee
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xe8
-        __emit 0xf3
-        __emit 0x08
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x70
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x74
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x68
-        __emit 0xf8
-        __emit 0xcf
-        __emit 0x41
-        __emit 0x00
-        __emit 0x56
-        __emit 0xe8
-        __emit 0xcc
-        __emit 0x6d
-        __emit 0x72
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0xc3
-    }
+	LevelUpUpgradeModuleData *data =
+		(LevelUpUpgradeModuleData *)operator new(0x78);
+	if (data)
+	{
+		UpgradeModuleDataSub *sub =
+			(UpgradeModuleDataSub *)((char *)data + 8);
+		sub->UpgradeModuleDataSub::UpgradeModuleDataSub();
+		data->LevelUpUpgradeModuleData::LevelUpUpgradeModuleData();
+	}
+	else
+		data = 0;
+	if (ini)
+		ini->initFromINI(data, &LevelUpUpgradeFieldParse);
+	return (ModuleData *)data;
 }
