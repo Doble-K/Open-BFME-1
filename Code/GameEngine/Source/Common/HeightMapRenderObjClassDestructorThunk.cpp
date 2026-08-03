1e3b78863b1dea43322a18c8d26a37bc5e451ca9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:11:36 -0400

    Open-BFME5 convert HeightMapRenderObjClass destructor MASM
    Lift 116B retail destructor from masm_dumps into standalone C++ thunk.

---
+++ Code/GameEngine/Source/Common/HeightMapRenderObjClassDestructorThunk.cpp
@@ -0,0 +1,131 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to standalone C++ thunk.
+
+class __declspec(novtable) HeightMapRenderObjClass
+{
+public:
+    virtual ~HeightMapRenderObjClass();
+};
+
+// ??1HeightMapRenderObjClass@@UAE@XZ
+__declspec(naked) HeightMapRenderObjClass::~HeightMapRenderObjClass()
+{
+    __asm {
+        __emit 0x6a
+        __emit 0xff
+        __emit 0x68
+        __emit 0x78
+        __emit 0xa7
+        __emit 0x04
+        __emit 0x01
+        __emit 0x64
+        __emit 0xa1
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x50
+        __emit 0x64
+        __emit 0x89
+        __emit 0x25
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x51
+        __emit 0x56
+        __emit 0x8b
+        __emit 0xf1
+        __emit 0x89
+        __emit 0x74
+        __emit 0x24
+        __emit 0x04
+        __emit 0xc7
+        __emit 0x06
+        __emit 0x88
+        __emit 0xdc
+        __emit 0x11
+        __emit 0x01
+        __emit 0xc7
+        __emit 0x46
+        __emit 0x08
+        __emit 0x80
+        __emit 0xdc
+        __emit 0x11
+        __emit 0x01
+        __emit 0xc7
+        __emit 0x86
+        __emit 0xc8
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x74
+        __emit 0xdc
+        __emit 0x11
+        __emit 0x01
+        __emit 0xc7
+        __emit 0x86
+        __emit 0xcc
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x60
+        __emit 0xdc
+        __emit 0x11
+        __emit 0x01
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xe8
+        __emit 0xd9
+        __emit 0xf1
+        __emit 0x94
+        __emit 0xff
+        __emit 0x8b
+        __emit 0xce
+        __emit 0xc7
+        __emit 0x05
+        __emit 0x08
+        __emit 0x80
+        __emit 0x2f
+        __emit 0x01
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0xc7
+        __emit 0x44
+        __emit 0x24
+        __emit 0x10
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xff
+        __emit 0xe8
+        __emit 0x38
+        __emit 0xf4
+        __emit 0x92
+        __emit 0xff
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x08
+        __emit 0x5e
+        __emit 0x64
+        __emit 0x89
+        __emit 0x0d
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0xc3
+    }
+}
