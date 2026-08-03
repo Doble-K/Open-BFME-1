ed72f3a474f39791859d497fd439b55870005e3f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:19:46 -0400

    Open-BFME5 convert HideQuitMenu to clean C++
    Replace naked dump with null-check of quit layout then tail-call WindowManager method; pin globals and callee; focused build byte-matches retail 21B.

--- Code/GameEngine/Source/GameClient/GUI/GUICallbacks/HideQuitMenu.cpp
+++ Code/GameEngine/Source/GameClient/GUI/GUICallbacks/HideQuitMenu.cpp
@@ -1,29 +1,18 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-__declspec(naked) void HideQuitMenu()
+class WindowManager
 {
-	__asm {
-		_emit 0A1h
-		_emit 0D4h
-		_emit 04Ah
-		_emit 02Fh
-		_emit 001h
-		_emit 085h
-		_emit 0C0h
-		_emit 074h
-		_emit 00Bh
-		_emit 08Bh
-		_emit 00Dh
-		_emit 0E8h
-		_emit 019h
-		_emit 02Fh
-		_emit 001h
-		_emit 0E9h
-		_emit 03Eh
-		_emit 0B5h
-		_emit 0ACh
-		_emit 0FFh
-		_emit 0C3h
-	}
-}
+public:
+	void hideQuitMenu();
+};
 
+// Globals filled by DIR32 from retail.
+extern WindowManager *g_theWindowManager;
+extern void *g_quitMenuLayout;
+
+// ?HideQuitMenu@@YAXXZ
+void HideQuitMenu()
+{
+	if (g_quitMenuLayout)
+		g_theWindowManager->hideQuitMenu();
+}
