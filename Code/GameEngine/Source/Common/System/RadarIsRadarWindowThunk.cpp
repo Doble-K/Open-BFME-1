a1e9ee7abe9f2d29f1001b3aaff2066c472de78c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:45:24 -0400

    Open-BFME5 convert Radar::isRadarWindow to clean C++

--- Code/GameEngine/Source/Common/System/RadarIsRadarWindowThunk.cpp
+++ Code/GameEngine/Source/Common/System/RadarIsRadarWindowThunk.cpp
@@ -1,46 +1,22 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
-#include "PreRTS.h"
+// Open-BFME5: Radar::isRadarWindow
 
 class GameWindow;
 
 class Radar
 {
 public:
-    bool isRadarWindow(GameWindow *);
+	bool isRadarWindow(GameWindow *window);
+
+private:
+	unsigned char m_pad[0x1438];
+	GameWindow *m_radarWindow;
 };
 
-__declspec(naked) bool Radar::isRadarWindow(GameWindow *)
+// ?isRadarWindow@Radar@@QAE_NPAVGameWindow@@@Z
+bool Radar::isRadarWindow(GameWindow *window)
 {
-    __asm {
-        _emit 08Bh
-        _emit 081h
-        _emit 038h
-        _emit 014h
-        _emit 000h
-        _emit 000h
-        _emit 03Bh
-        _emit 044h
-        _emit 024h
-        _emit 004h
-        _emit 075h
-        _emit 00Ch
-        _emit 085h
-        _emit 0C0h
-        _emit 074h
-        _emit 008h
-        _emit 0B8h
-        _emit 001h
-        _emit 000h
-        _emit 000h
-        _emit 000h
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-        _emit 033h
-        _emit 0C0h
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-    }
+	GameWindow *r = m_radarWindow;
+	return r == window && r != 0;
 }
