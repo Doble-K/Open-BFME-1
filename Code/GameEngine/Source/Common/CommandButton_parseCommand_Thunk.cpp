cef915fa7b9da5bd69bbce8b5cdd67cc7a8dd5e2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:25:33 -0400

    Open-BFME5 convert CommandButton parseCommand and ShellMenuSchemeManager parseLinePart MASM

---
+++ Code/GameEngine/Source/Common/CommandButton_parseCommand_Thunk.cpp
@@ -0,0 +1,140 @@
+// cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift MASM dump to standalone C++ thunk.
+
+class INI;
+class CommandButton
+{
+public:
+	static void __cdecl parseCommand(INI *, void *, void *, const void *);
+};
+
+// ?parseCommand@CommandButton@@SAXPAVINI@@PAX1PBX@Z
+__declspec(naked) void __cdecl CommandButton::parseCommand(INI *, void *, void *, const void *)
+{
+	__asm {
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x04
+        __emit 0x83
+        __emit 0xec
+        __emit 0x08
+        __emit 0x53
+        __emit 0x55
+        __emit 0x56
+        __emit 0x57
+        __emit 0x6a
+        __emit 0x00
+        __emit 0xe8
+        __emit 0xce
+        __emit 0x5d
+        __emit 0x3b
+        __emit 0x00
+        __emit 0x8b
+        __emit 0xd8
+        __emit 0xa1
+        __emit 0x80
+        __emit 0x5f
+        __emit 0x2b
+        __emit 0x01
+        __emit 0x33
+        __emit 0xff
+        __emit 0x85
+        __emit 0xc0
+        __emit 0x74
+        __emit 0x2b
+        __emit 0x8b
+        __emit 0x2d
+        __emit 0x3c
+        __emit 0x93
+        __emit 0x35
+        __emit 0x01
+        __emit 0xb8
+        __emit 0x80
+        __emit 0x5f
+        __emit 0x2b
+        __emit 0x01
+        __emit 0x8b
+        __emit 0xf0
+        __emit 0x8d
+        __emit 0x64
+        __emit 0x24
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x00
+        __emit 0x53
+        __emit 0x50
+        __emit 0xff
+        __emit 0xd5
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x08
+        __emit 0x85
+        __emit 0xc0
+        __emit 0x74
+        __emit 0x31
+        __emit 0x8b
+        __emit 0x4e
+        __emit 0x04
+        __emit 0x83
+        __emit 0xc6
+        __emit 0x04
+        __emit 0x47
+        __emit 0x85
+        __emit 0xc9
+        __emit 0x8b
+        __emit 0xc6
+        __emit 0x75
+        __emit 0xe6
+        __emit 0x53
+        __emit 0x68
+        __emit 0xc0
+        __emit 0xb5
+        __emit 0x0f
+        __emit 0x01
+        __emit 0x8d
+        __emit 0x54
+        __emit 0x24
+        __emit 0x18
+        __emit 0x6a
+        __emit 0x03
+        __emit 0x52
+        __emit 0xe8
+        __emit 0x14
+        __emit 0x5a
+        __emit 0x3b
+        __emit 0x00
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x10
+        __emit 0x68
+        __emit 0x30
+        __emit 0xfc
+        __emit 0x1d
+        __emit 0x01
+        __emit 0x8d
+        __emit 0x44
+        __emit 0x24
+        __emit 0x14
+        __emit 0x50
+        __emit 0xe8
+        __emit 0x02
+        __emit 0xc1
+        __emit 0x55
+        __emit 0x00
+        __emit 0x8b
+        __emit 0x4c
+        __emit 0x24
+        __emit 0x24
+        __emit 0x89
+        __emit 0x39
+        __emit 0x5f
+        __emit 0x5e
+        __emit 0x5d
+        __emit 0x5b
+        __emit 0x83
+        __emit 0xc4
+        __emit 0x08
+        __emit 0xc3
+	}
+}
