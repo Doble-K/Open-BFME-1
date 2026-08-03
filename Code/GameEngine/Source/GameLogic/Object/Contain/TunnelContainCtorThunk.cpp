d51dc6ba781b5e4c82e9df43dc1472a7a984f2aa
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 08:20:05 -0400

    Open-BFME5 convert TunnelContain constructor to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Contain/TunnelContainCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Contain/TunnelContainCtorThunk.cpp
@@ -1,119 +1,62 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHs-
+// Open-BFME5: lift TunnelContain ctor __emit thunk to clean C++. Retail:
+// out-of-line base ctor call, nine most-derived vtable stores (+0x00/+0x0C/
+// +0x10/+0x20/+0x24/+0x28/+0x2C/+0x30/+0x34), then byte at +0xD4 = 1 and
+// byte at +0xD5 = 0. Interfaces nested in the base; /EHs- because retail
+// has no unwind frame.
 
 class Thing;
 class ModuleData;
 
-class TunnelContain
+class TC_GrandBase
+{
+public:
+    TC_GrandBase(Thing *, const ModuleData *);
+    virtual ~TC_GrandBase();
+
+private:
+    unsigned char m_pad[8];
+};
+
+class TC_Iface1 { public: virtual ~TC_Iface1(); };
+class TC_Iface2 { public: virtual ~TC_Iface2(); private: unsigned char m_pad[0xC]; };
+class TC_Iface3 { public: virtual ~TC_Iface3(); };
+class TC_Iface4 { public: virtual ~TC_Iface4(); };
+class TC_Iface5 { public: virtual ~TC_Iface5(); };
+class TC_Iface6 { public: virtual ~TC_Iface6(); };
+class TC_Iface7 { public: virtual ~TC_Iface7(); };
+class TC_Iface8 { public: virtual ~TC_Iface8(); };
+
+class TC_Base : public TC_GrandBase,
+                public TC_Iface1,
+                public TC_Iface2,
+                public TC_Iface3,
+                public TC_Iface4,
+                public TC_Iface5,
+                public TC_Iface6,
+                public TC_Iface7,
+                public TC_Iface8
+{
+public:
+    TC_Base(Thing *, const ModuleData *);
+    virtual ~TC_Base();
+
+private:
+    unsigned char m_pad[0x9C];
+};
+
+class TunnelContain : public TC_Base
 {
 public:
     TunnelContain(Thing *, const ModuleData *);
+
+private:
+    bool m_d4;
+    bool m_d5;
 };
 
 // ??0TunnelContain@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) TunnelContain::TunnelContain(Thing *, const ModuleData *)
+TunnelContain::TunnelContain(Thing *t, const ModuleData *m)
+    : TC_Base(t, m), m_d4(true), m_d5(false)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0xc5;
-        __emit 0x7f;
-        __emit 0xe0;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xb8;
-        __emit 0xdd;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xf0;
-        __emit 0xdc;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xe0;
-        __emit 0xdc;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x38;
-        __emit 0xdb;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0xdb;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x28;
-        __emit 0x14;
-        __emit 0xdb;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x2c;
-        __emit 0x04;
-        __emit 0xdb;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x30;
-        __emit 0xc8;
-        __emit 0xda;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x34;
-        __emit 0xb8;
-        __emit 0xda;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc6;
-        __emit 0x86;
-        __emit 0xd4;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x01;
-        __emit 0xc6;
-        __emit 0x86;
-        __emit 0xd5;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
 }
