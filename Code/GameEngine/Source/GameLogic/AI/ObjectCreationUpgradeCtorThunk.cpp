0e5af890764946c49ea65fff8b320701f80992a3
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 08:35:01 -0400

    Open-BFME5 convert ObjectCreationUpgrade constructor to clean C++

--- Code/GameEngine/Source/GameLogic/AI/ObjectCreationUpgradeCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/ObjectCreationUpgradeCtorThunk.cpp
@@ -1,133 +1,71 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHs-
+// Open-BFME5: lift ObjectCreationUpgrade ctor __emit thunk to clean C++.
+// Retail: grandbase ctor call at +0, then the middle base at +8 (args ctor
+// call, interface vtable pairs at +0x14/+0x18, middle finals and fields 0,
+// -1, -1), then this class's fields (+0x28, byte +0x2C) and the four
+// most-derived vtable stores (+0x00, +0x14, +0x18, +0x08).
 
 class Thing;
 class ModuleData;
 
-class ObjectCreationUpgrade
+class OCU_GrandBase
+{
+public:
+    OCU_GrandBase();
+    virtual ~OCU_GrandBase();
+
+private:
+    unsigned int m_04;
+};
+
+class OCU_MidGrand
+{
+public:
+    OCU_MidGrand(Thing *, const ModuleData *);
+    virtual ~OCU_MidGrand();
+
+private:
+    unsigned int m_04;
+    unsigned char m_pad[4];
+};
+
+class OCU_Iface1
+{
+public:
+    virtual ~OCU_Iface1();
+};
+
+class OCU_Iface2
+{
+public:
+    virtual ~OCU_Iface2();
+};
+
+class OCU_MiddleBase : public OCU_MidGrand, public OCU_Iface1, public OCU_Iface2
+{
+public:
+    OCU_MiddleBase(Thing *t, const ModuleData *m)
+        : OCU_MidGrand(t, m), m_1C(0), m_20(-1), m_24(-1) {}
+    virtual ~OCU_MiddleBase();
+
+private:
+    unsigned int m_1C;
+    int m_20;
+    int m_24;
+};
+
+class ObjectCreationUpgrade : public OCU_GrandBase, public OCU_MiddleBase
 {
 public:
     ObjectCreationUpgrade(Thing *, const ModuleData *);
+
+private:
+    unsigned int m_28;
+    bool m_2C;
 };
 
 // ??0ObjectCreationUpgrade@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) ObjectCreationUpgrade::ObjectCreationUpgrade(Thing *, const ModuleData *)
+ObjectCreationUpgrade::ObjectCreationUpgrade(Thing *t, const ModuleData *m)
+    : OCU_MiddleBase(t, m), m_28(0), m_2C(false)
 {
-    __asm {
-        __emit 0x56;
-        __emit 0x57;
-        __emit 0x8b;
-        __emit 0xf9;
-        __emit 0xe8;
-        __emit 0xb5;
-        __emit 0x64;
-        __emit 0xd6;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x50;
-        __emit 0x8d;
-        __emit 0x77;
-        __emit 0x08;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0x37;
-        __emit 0x03;
-        __emit 0xd4;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x94;
-        __emit 0xcd;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xcc;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xac;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x83;
-        __emit 0xc9;
-        __emit 0xff;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x1c;
-        __emit 0x89;
-        __emit 0x47;
-        __emit 0x28;
-        __emit 0x88;
-        __emit 0x47;
-        __emit 0x2c;
-        __emit 0xc7;
-        __emit 0x07;
-        __emit 0x20;
-        __emit 0xd6;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x47;
-        __emit 0x14;
-        __emit 0x58;
-        __emit 0xd5;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x47;
-        __emit 0x18;
-        __emit 0x4c;
-        __emit 0xd5;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0xc7;
-        __emit 0x5f;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x18;
-        __emit 0xd5;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
 }
