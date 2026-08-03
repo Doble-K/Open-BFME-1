92cddee5f566cc0542d31639c7cbd1fa83c4d107
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 02:47:24 -0400

    Open-BFME5 convert HTreeClass destructor to clean C++

--- Code/Libraries/Source/WWVegas/WW3D2/HTreeClass.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/HTreeClass.cpp
@@ -1,7 +1,6 @@
 // cl: /DNDEBUG /MD /GX- /O2 /Ob2
 
 // Open-BFME5: HTreeClass default ctor + dtor
-// Ctor retail 20B: zero +0x10/+0x14, float +0x18 = 1.0f, byte +0 = 0.
 
 class HTreeClass
 {
@@ -17,6 +16,8 @@ private:
 	float m_scale;
 };
 
+void __cdecl operator delete(void *);
+
 // ??0HTreeClass@@QAE@XZ
 HTreeClass::HTreeClass()
 {
@@ -26,52 +27,14 @@ HTreeClass::HTreeClass()
 	m_name0 = 0;
 }
 
-// Keep dtor as naked until separately converted (still matched via emit).
 // ??1HTreeClass@@QAE@XZ
-__declspec(naked) HTreeClass::~HTreeClass()
+HTreeClass::~HTreeClass()
 {
-	__asm {
-		_emit 056h
-		_emit 08Bh
-		_emit 0F1h
-		_emit 08Bh
-		_emit 046h
-		_emit 014h
-		_emit 085h
-		_emit 0C0h
-		_emit 074h
-		_emit 010h
-		_emit 050h
-		_emit 0E8h
-		_emit 010h
-		_emit 0F7h
-		_emit 0F2h
-		_emit 0FFh
-		_emit 083h
-		_emit 0C4h
-		_emit 004h
-		_emit 0C7h
-		_emit 046h
-		_emit 014h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 0C7h
-		_emit 046h
-		_emit 010h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 000h
-		_emit 0C7h
-		_emit 046h
-		_emit 018h
-		_emit 000h
-		_emit 000h
-		_emit 080h
-		_emit 03Fh
-		_emit 05Eh
-		_emit 0C3h
+	if (m_pivotCountOrPtr)
+	{
+		operator delete(m_pivotCountOrPtr);
+		m_pivotCountOrPtr = 0;
 	}
+	m_pivots = 0;
+	m_scale = 1.0f;
 }
