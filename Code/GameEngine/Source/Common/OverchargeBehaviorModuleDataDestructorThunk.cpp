93ea6db2a82753b87eb45c5f665c5d23967afb4b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 06:09:05 -0400

    Open-BFME5 convert TechBuildingBehavior and OverchargeBehavior ModuleData dtors
    List-walk at +0x08 with next at +0x04, zero node+0x0c before delete, base dtor.

--- Code/GameEngine/Source/Common/OverchargeBehaviorModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/OverchargeBehaviorModuleDataDestructorThunk.cpp
@@ -1,120 +1,39 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: OverchargeBehaviorModuleData dtor (ICF TechBuilding).
 
-class __declspec(novtable) OverchargeBehaviorModuleData
+class OverchargeNode
 {
 public:
-    virtual ~OverchargeBehaviorModuleData();
+	virtual ~OverchargeNode();
+	OverchargeNode *m_next; // +0x04
+	unsigned char m_pad[4];
+	unsigned int m_zero; // +0x0c cleared before delete
+};
+
+class UpdateModuleData
+{
+public:
+	virtual ~UpdateModuleData();
+	unsigned int m_04;
+};
+
+class OverchargeBehaviorModuleData : public UpdateModuleData
+{
+public:
+	virtual ~OverchargeBehaviorModuleData();
+private:
+	OverchargeNode *m_head; // +0x08
 };
 
 // ??1OverchargeBehaviorModuleData@@UAE@XZ
-__declspec(naked) OverchargeBehaviorModuleData::~OverchargeBehaviorModuleData()
+OverchargeBehaviorModuleData::~OverchargeBehaviorModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xa8
-        __emit 0x57
-        __emit 0xff
-        __emit 0x00
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xf9
-        __emit 0x89
-        __emit 0x7c
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x07
-        __emit 0x34
-        __emit 0xd5
-        __emit 0x07
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x4f
-        __emit 0x08
-        __emit 0x85
-        __emit 0xc9
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x18
-        __emit 0x56
-        __emit 0x8b
-        __emit 0x71
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x41
-        __emit 0x0c
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x01
-        __emit 0x6a
-        __emit 0x01
-        __emit 0xff
-        __emit 0x10
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x8b
-        __emit 0xce
-        __emit 0x75
-        __emit 0xea
-        __emit 0x5e
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0x97
-        __emit 0x75
-        __emit 0x91
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5f
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
+	OverchargeNode *p = m_head;
+	while (p)
+	{
+		OverchargeNode *next = p->m_next;
+		p->m_zero = 0;
+		delete p;
+		p = next;
+	}
 }
--- Code/GameEngine/Source/Common/TechBuildingBehaviorModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/TechBuildingBehaviorModuleDataDestructorThunk.cpp
@@ -1,120 +1,40 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: TechBuildingBehaviorModuleData dtor.
+// Walk list at +0x08 (next at +0x04), zero +0x0c, delete, base dtor.
 
-class __declspec(novtable) TechBuildingBehaviorModuleData
+class TechBuildingNode
+{
+public:
+	virtual ~TechBuildingNode();
+	TechBuildingNode *m_next; // +0x04
+	unsigned char m_pad[4];
+	unsigned int m_zero; // +0x0c cleared before delete
+};
+
+class UpdateModuleData
+{
+public:
+	virtual ~UpdateModuleData();
+	unsigned int m_04;
+};
+
+class TechBuildingBehaviorModuleData : public UpdateModuleData
 {
 public:
 	virtual ~TechBuildingBehaviorModuleData();
+private:
+	TechBuildingNode *m_head; // +0x08
 };
 
 // ??1TechBuildingBehaviorModuleData@@UAE@XZ
-__declspec(naked) TechBuildingBehaviorModuleData::~TechBuildingBehaviorModuleData()
+TechBuildingBehaviorModuleData::~TechBuildingBehaviorModuleData()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0xa8
-		__emit 0x57
-		__emit 0xff
-		__emit 0x00
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x57
-		__emit 0x8b
-		__emit 0xf9
-		__emit 0x89
-		__emit 0x7c
-		__emit 0x24
-		__emit 0x04
-		__emit 0xc7
-		__emit 0x07
-		__emit 0x34
-		__emit 0xd5
-		__emit 0x07
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x4f
-		__emit 0x08
-		__emit 0x85
-		__emit 0xc9
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x18
-		__emit 0x56
-		__emit 0x8b
-		__emit 0x71
-		__emit 0x04
-		__emit 0xc7
-		__emit 0x41
-		__emit 0x0c
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x01
-		__emit 0x6a
-		__emit 0x01
-		__emit 0xff
-		__emit 0x10
-		__emit 0x85
-		__emit 0xf6
-		__emit 0x8b
-		__emit 0xce
-		__emit 0x75
-		__emit 0xea
-		__emit 0x5e
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x97
-		__emit 0x75
-		__emit 0x91
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x5f
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
+	TechBuildingNode *p = m_head;
+	while (p)
+	{
+		TechBuildingNode *next = p->m_next;
+		p->m_zero = 0;
+		delete p;
+		p = next;
 	}
 }
