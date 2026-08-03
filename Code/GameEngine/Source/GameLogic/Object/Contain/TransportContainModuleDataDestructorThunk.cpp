eab7f7f1a9e8f678af9a5acba11a5a2976e92613
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:57:09 -0400

    Open-BFME5 convert Buffer-then-base ModuleData destructors to clean C++
    SEH destructors that destroy a trailing Buffer then call a pinned base
    dtor (WeaponModeSpecialPowerUpdate and four same-shape SpecialAbility/
    Update ModuleData types). symbols.csv pins for invented base names.

--- Code/GameEngine/Source/GameLogic/Object/Contain/TransportContainModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Contain/TransportContainModuleDataDestructorThunk.cpp
@@ -1,94 +1,31 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: TransportContainModuleData dtor. SEH Buffer @+0x174 then base.
 
-class __declspec(novtable) TransportContainModuleData
+class Buffer
 {
 public:
-    virtual ~TransportContainModuleData();
+	~Buffer();
+private:
+	unsigned char m_pad[4];
+};
+
+class TransportContainModuleDataBase
+{
+public:
+	virtual ~TransportContainModuleDataBase();
+private:
+	unsigned char m_pad[0x170];
+};
+
+class __declspec(novtable) TransportContainModuleData : public TransportContainModuleDataBase
+{
+public:
+	virtual ~TransportContainModuleData();
+private:
+	Buffer m_buffer;
 };
 
 // ??1TransportContainModuleData@@UAE@XZ
-__declspec(naked) TransportContainModuleData::~TransportContainModuleData()
+TransportContainModuleData::~TransportContainModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x98
-        __emit 0xce
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x8e
-        __emit 0x74
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xb0
-        __emit 0xa3
-        __emit 0x66
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0x42
-        __emit 0xa1
-        __emit 0xe1
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
--- Code/GameEngine/Source/GameLogic/Object/Update/HeroModeSpecialAbilityUpdateModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/HeroModeSpecialAbilityUpdateModuleDataDestructorThunk.cpp
@@ -1,94 +1,31 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: HeroModeSpecialAbilityUpdateModuleData dtor. SEH Buffer @+0x254 then base.
 
-class __declspec(novtable) HeroModeSpecialAbilityUpdateModuleData
+class Buffer
 {
 public:
-    virtual ~HeroModeSpecialAbilityUpdateModuleData();
+	~Buffer();
+private:
+	unsigned char m_pad[4];
+};
+
+class HeroModeSpecialAbilityUpdateModuleDataBase
+{
+public:
+	virtual ~HeroModeSpecialAbilityUpdateModuleDataBase();
+private:
+	unsigned char m_pad[0x250];
+};
+
+class __declspec(novtable) HeroModeSpecialAbilityUpdateModuleData : public HeroModeSpecialAbilityUpdateModuleDataBase
+{
+public:
+	virtual ~HeroModeSpecialAbilityUpdateModuleData();
+private:
+	Buffer m_buffer;
 };
 
 // ??1HeroModeSpecialAbilityUpdateModuleData@@UAE@XZ
-__declspec(naked) HeroModeSpecialAbilityUpdateModuleData::~HeroModeSpecialAbilityUpdateModuleData()
+HeroModeSpecialAbilityUpdateModuleData::~HeroModeSpecialAbilityUpdateModuleData()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x78
-        __emit 0xf5
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0x8d
-        __emit 0x8e
-        __emit 0x54
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x90
-        __emit 0x7d
-        __emit 0x62
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0x4e
-        __emit 0x9c
-        __emit 0xdb
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
