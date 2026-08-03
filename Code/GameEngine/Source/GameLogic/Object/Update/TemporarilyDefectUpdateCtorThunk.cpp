6b65653ad0e11a29daaf2faa9cae011bac6ab8aa
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 11:48:05 -0400

    Open-BFME5 convert TemporarilyDefectUpdate constructor to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Update/TemporarilyDefectUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/TemporarilyDefectUpdateCtorThunk.cpp
@@ -1,163 +1,60 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift TemporarilyDefectUpdate ctor __emit thunk to clean C++.
+// ICF-identical to the PoisonedBehavior ctor: deep base ctor (out-of-line),
+// UpdateModule's inline ctor stores the two interface subobjects' vtables at
+// +0x0C/+0x10 and inits its three scalars; the most-derived vtable batch, four
+// member zeroings, then setWakeFrame(getObject(), 0x3fffffff).
 
 class Thing;
 class ModuleData;
+class Object;
+enum UpdateSleepTime { UPDATE_SLEEP_DUMMY };
 
-class TemporarilyDefectUpdate
+class PB_DeepBase
+{
+public:
+    PB_DeepBase(Thing *, const ModuleData *);
+    virtual ~PB_DeepBase();
+
+protected:
+    void *m_p4;
+    Object *m_object;
+};
+
+class PB_Iface1 { public: virtual void slot(); };
+class PB_Iface2 { public: virtual void slot(); };
+
+class UpdateModule : public PB_DeepBase, public PB_Iface1, public PB_Iface2
+{
+public:
+    UpdateModule(Thing *thing, const ModuleData *moduleData)
+        : PB_DeepBase(thing, moduleData), m_f14(0), m_f18(-1), m_f1C(-1) {}
+
+protected:
+    void setWakeFrame(Object *, UpdateSleepTime);
+    Object *getObject() const { return m_object; }
+
+private:
+    unsigned int m_f14;
+    int m_f18;
+    int m_f1C;
+};
+
+class TemporarilyDefectUpdate : public UpdateModule
 {
 public:
     TemporarilyDefectUpdate(Thing *, const ModuleData *);
+
+private:
+    unsigned int m_f20;
+    unsigned int m_f24;
+    unsigned int m_f28;
+    unsigned char m_f2C;
 };
 
 // ??0TemporarilyDefectUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) TemporarilyDefectUpdate::TemporarilyDefectUpdate(Thing *, const ModuleData *)
+TemporarilyDefectUpdate::TemporarilyDefectUpdate(Thing *thing, const ModuleData *moduleData)
+    : UpdateModule(thing, moduleData), m_f20(0), m_f24(0), m_f28(0), m_f2C(0)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x58;
-        __emit 0x2f;
-        __emit 0x01;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0xe8;
-        __emit 0xe6;
-        __emit 0x65;
-        __emit 0xd6;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc9;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x1c;
-        __emit 0x8b;
-        __emit 0x56;
-        __emit 0x08;
-        __emit 0x68;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0x3f;
-        __emit 0x52;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xcc;
-        __emit 0x4d;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0x08;
-        __emit 0x4d;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xf8;
-        __emit 0x4c;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x28;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x2c;
-        __emit 0xe8;
-        __emit 0x8c;
-        __emit 0x4c;
-        __emit 0xd6;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+    setWakeFrame(getObject(), (UpdateSleepTime)0x3fffffff);
 }
