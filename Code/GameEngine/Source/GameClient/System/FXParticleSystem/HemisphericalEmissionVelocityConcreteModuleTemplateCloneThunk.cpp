e44de339a843bfc71caf70fa0a6f4fbba0371b41
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:12:53 -0400

    Open-BFME5 convert HemisphericalEmissionVelocity clone to clean C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/HemisphericalEmissionVelocityConcreteModuleTemplateCloneThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/HemisphericalEmissionVelocityConcreteModuleTemplateCloneThunk.cpp
@@ -1,81 +1,41 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /GX- /O2 /Ob2
+
+// Open-BFME5: HemisphericalEmissionVelocity ConcreteModuleTemplate::clone
 
 namespace FXParticleSystem
 {
-template <int N>
-class DefaultParticleModule
-{
-};
 
-template <int N>
-class DefaultParticleModuleTemplate
-{
-};
-
-class HemisphericalEmissionVelocityModule
+struct HemisphericalEmissionVelocityModuleTag
 {
 };
 
 class HemisphericalEmissionVelocityModuleTemplate
 {
+public:
+	virtual ~HemisphericalEmissionVelocityModuleTemplate();
 };
 
-extern const char HEMISPHERICAL_EMISSION_VELOCITY_MODULE_KEY[1];
-extern const char HEMISPHERICAL_EMISSION_VELOCITY_MODULE_NAME[1];
-
-template <int Category, const char (&Key)[1], const char (&Name)[1], class Module, class ModuleTemplate, class ParticleModule, class ParticleModuleTemplate>
-class ModuleTag
-{
-};
+void *__cdecl operator new(unsigned int);
+void __cdecl operator delete(void *);
 
 template <class Tag>
 class ConcreteModuleTemplate;
 
-typedef ModuleTag<4, HEMISPHERICAL_EMISSION_VELOCITY_MODULE_KEY, HEMISPHERICAL_EMISSION_VELOCITY_MODULE_NAME,
-    HemisphericalEmissionVelocityModule, HemisphericalEmissionVelocityModuleTemplate,
-    DefaultParticleModule<4>, DefaultParticleModuleTemplate<4> > HemisphericalEmissionVelocityTag;
-
 template <>
-class ConcreteModuleTemplate<HemisphericalEmissionVelocityTag>
+class ConcreteModuleTemplate<HemisphericalEmissionVelocityModuleTag> : public HemisphericalEmissionVelocityModuleTemplate
 {
 public:
-    virtual HemisphericalEmissionVelocityModuleTemplate *clone() const;
+	ConcreteModuleTemplate(const ConcreteModuleTemplate &);
+	virtual HemisphericalEmissionVelocityModuleTemplate *clone() const;
+
+private:
+	unsigned char m_pad[0x14];
 };
 
-__declspec(naked) HemisphericalEmissionVelocityModuleTemplate *ConcreteModuleTemplate<HemisphericalEmissionVelocityTag>::clone() const
+// ?clone@?$ConcreteModuleTemplate@UHemisphericalEmissionVelocityModuleTag@FXParticleSystem@@@FXParticleSystem@@UBEPAVHemisphericalEmissionVelocityModuleTemplate@2@XZ
+HemisphericalEmissionVelocityModuleTemplate *ConcreteModuleTemplate<HemisphericalEmissionVelocityModuleTag>::clone() const
 {
-    __asm {
-        _emit 056h
-        _emit 06Ah
-        _emit 018h
-        _emit 08Bh
-        _emit 0F1h
-        _emit 0E8h
-        _emit 066h
-        _emit 051h
-        _emit 02Ah
-        _emit 000h
-        _emit 083h
-        _emit 0C4h
-        _emit 004h
-        _emit 085h
-        _emit 0C0h
-        _emit 074h
-        _emit 00Ah
-        _emit 056h
-        _emit 08Bh
-        _emit 0C8h
-        _emit 0E8h
-        _emit 0CAh
-        _emit 031h
-        _emit 0A6h
-        _emit 0FFh
-        _emit 05Eh
-        _emit 0C3h
-        _emit 033h
-        _emit 0C0h
-        _emit 05Eh
-        _emit 0C3h
-    }
+	return new ConcreteModuleTemplate<HemisphericalEmissionVelocityModuleTag>(*this);
 }
+
 }
