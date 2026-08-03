d3aab74aa2d82ede6ec8f561114fcc0f2045bd32
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-03 03:55:09 -0400

    Open-BFME5 convert WeaponSet destructor to clean C++

--- Code/GameEngine/Source/Common/WeaponSetDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/WeaponSetDestructorThunk.cpp
@@ -1,110 +1,42 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: lift WeaponSet dtor __emit thunk to clean C++.
+// Retail stores its own vtable at entry, loops over the four pointer slots at
+// this+8 (virtual delete of each non-null element) under an SEH frame, then
+// runs the inlined base dtor (base vtable store) on the unwind tail.
 
-class __declspec(novtable) WeaponSet
+class WeaponSetElement
 {
 public:
+    virtual ~WeaponSetElement();
+};
+
+class WeaponSetBase
+{
+public:
+    virtual void weaponSetBaseVslot();
+    ~WeaponSetBase() {}
+
+private:
+    unsigned int m_04;
+};
+
+class WeaponSet : public WeaponSetBase
+{
+public:
+    virtual void weaponSetVslot();
     ~WeaponSet();
+
+private:
+    WeaponSetElement *m_slots[4];
 };
 
 // ??1WeaponSet@@QAE@XZ
-__declspec(naked) WeaponSet::~WeaponSet()
+WeaponSet::~WeaponSet()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x78
-        __emit 0xa6
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x53
-        __emit 0x56
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xf9
-        __emit 0x89
-        __emit 0x7c
-        __emit 0x24
-        __emit 0x0c
-        __emit 0xc7
-        __emit 0x07
-        __emit 0x78
-        __emit 0x16
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x18
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x77
-        __emit 0x08
-        __emit 0xbb
-        __emit 0x04
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x0e
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x06
-        __emit 0x8b
-        __emit 0x01
-        __emit 0x6a
-        __emit 0x01
-        __emit 0xff
-        __emit 0x10
-        __emit 0x83
-        __emit 0xc6
-        __emit 0x04
-        __emit 0x4b
-        __emit 0x75
-        __emit 0xee
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x10
-        __emit 0xc7
-        __emit 0x07
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5b
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
+    for (int i = 0; i < 4; ++i)
+    {
+        if (m_slots[i]) {
+            delete m_slots[i];
+        }
     }
 }
