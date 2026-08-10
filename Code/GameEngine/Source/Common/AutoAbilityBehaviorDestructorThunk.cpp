c6c20d54261d559d7b9664fd104495115882bf14
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 23:04:09 +0000

    Open-BFME5: convert __declspec(naked) AutoAbilityBehavior::~AutoAbilityBehavior() to exact C++

--- Code/GameEngine/Source/Common/AutoAbilityBehaviorDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/AutoAbilityBehaviorDestructorThunk.cpp
@@ -1,131 +1,68 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-class __declspec(novtable) AutoAbilityBehavior
+class Gen_dtor_00113f20
 {
 public:
-    virtual ~AutoAbilityBehavior();
+	virtual ~Gen_dtor_00113f20();
+
+private:
+	const void *m_moduleData;
+};
+
+class ObjectModule : public Gen_dtor_00113f20
+{
+private:
+	void *m_object;
+};
+
+class BehaviorModuleInterface
+{
+public:
+	virtual void behaviorModuleInterfaceAnchor() = 0;
+};
+
+class UpdateModuleInterface
+{
+public:
+	virtual void updateModuleInterfaceAnchor() = 0;
+};
+
+class BehaviorModule : public ObjectModule, public BehaviorModuleInterface
+{
+public:
+	virtual ~BehaviorModule() {}
+};
+
+class UpdateModule : public BehaviorModule, public UpdateModuleInterface
+{
+public:
+	virtual ~UpdateModule() {}
+
+private:
+	unsigned int m_14;
+	int m_18;
+	int m_1c;
+};
+
+class AsciiString
+{
+public:
+	~AsciiString();
+
+private:
+	char *m_data;
+};
+
+class AutoAbilityBehavior : public UpdateModule
+{
+public:
+	virtual ~AutoAbilityBehavior();
+
+private:
+	AsciiString m_20;
 };
 
 // ??1AutoAbilityBehavior@@UAE@XZ
-__declspec(naked) AutoAbilityBehavior::~AutoAbilityBehavior()
+AutoAbilityBehavior::~AutoAbilityBehavior()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x18
-        __emit 0xa7
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xc4
-        __emit 0x1b
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x00
-        __emit 0x1b
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0xf4
-        __emit 0x1a
-        __emit 0x0a
-        __emit 0x01
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x20
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x1f
-        __emit 0xa2
-        __emit 0x69
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0xac
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x5c
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x98
-        __emit 0xca
-        __emit 0x09
-        __emit 0x01
-        __emit 0xe8
-        __emit 0x0f
-        __emit 0xa5
-        __emit 0xe5
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
 }
