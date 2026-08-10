07ace95a32cbbb3d9abe41715f28f718abd818fe
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 16:59:51 +0000

    Open-BFME5: convert __declspec(naked) CritterEmitterUpdate::CritterEmitterUpdate(Thing *, const ModuleData *) to exact C++

--- Code/GameEngine/Source/GameLogic/AI/CritterEmitterUpdateConstructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/CritterEmitterUpdateConstructorThunk.cpp
@@ -3,110 +3,70 @@
 class Thing;
 class ModuleData;
 
-class CritterEmitterUpdate
+class ObjectModule
+{
+public:
+    ObjectModule(Thing *, const ModuleData *);
+    virtual ~ObjectModule();
+
+protected:
+    unsigned int m_04;
+
+private:
+    unsigned char m_pad[4];
+};
+
+class CritterEmitterUpdateIface1
+{
+public:
+    virtual ~CritterEmitterUpdateIface1();
+};
+
+class CritterEmitterUpdateIface2
+{
+public:
+    virtual ~CritterEmitterUpdateIface2();
+};
+
+class CritterEmitterUpdateBase : public ObjectModule,
+    public CritterEmitterUpdateIface1,
+    public CritterEmitterUpdateIface2
+{
+public:
+    CritterEmitterUpdateBase(Thing *thing, const ModuleData *moduleData)
+        : ObjectModule(thing, moduleData), m_14(0), m_18(-1), m_1c(-1)
+    {
+    }
+
+    virtual ~CritterEmitterUpdateBase();
+
+private:
+    unsigned int m_14;
+    int m_18;
+    int m_1c;
+};
+
+class CritterEmitterUpdateIface3
+{
+public:
+    virtual ~CritterEmitterUpdateIface3();
+};
+
+class CritterEmitterUpdate : public CritterEmitterUpdateBase,
+    public CritterEmitterUpdateIface3
 {
 public:
     CritterEmitterUpdate(Thing *, const ModuleData *);
+
+private:
+    unsigned char m_24;
+    unsigned char m_25;
 };
 
 // ??0CritterEmitterUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) CritterEmitterUpdate::CritterEmitterUpdate(Thing *, const ModuleData *)
+CritterEmitterUpdate::CritterEmitterUpdate(Thing *thing, const ModuleData *moduleData)
+    : CritterEmitterUpdateBase(thing, moduleData)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0x30;
-        __emit 0xba;
-        __emit 0xd8;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc8;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xc9;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x1c;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x14;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0xe4;
-        __emit 0x1d;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x24;
-        __emit 0xd1;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0x60;
-        __emit 0xd0;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x54;
-        __emit 0xd0;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x38;
-        __emit 0xd0;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x88;
-        __emit 0x4e;
-        __emit 0x24;
-        __emit 0xc6;
-        __emit 0x46;
-        __emit 0x25;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+    m_24 = 0;
+    m_25 = 1;
 }
