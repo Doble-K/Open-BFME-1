ab50a67cf60b0b20063e264b7fd81acfa99e0c98
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 18:57:24 +0000

    Open-BFME5: convert __declspec(naked) TooltipUpgradeModuleData::~TooltipUpgradeModuleData() to exact C++

--- Code/GameEngine/Source/Common/TooltipUpgradeModuleDataDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/TooltipUpgradeModuleDataDestructorThunk.cpp
@@ -1,111 +1,51 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: clean C++ lift of the ModuleData destructor.
+
+class UpgradeModuleDataSub
+{
+public:
+	~UpgradeModuleDataSub();
+
+private:
+	char m_opaque[0x68];
+};
+
+class AsciiString
+{
+public:
+	~AsciiString();
+
+private:
+	char *m_data;
+};
+
+class TooltipUpgradeModuleDataPrimaryBase
+{
+public:
+	virtual ~TooltipUpgradeModuleDataPrimaryBase() {}
+
+private:
+	unsigned int m_04;
+};
+
+class __declspec(novtable) TooltipUpgradeModuleDataIntermediateBase
+	: public TooltipUpgradeModuleDataPrimaryBase
+{
+	UpgradeModuleDataSub m_sub;
+};
 
 class __declspec(novtable) TooltipUpgradeModuleData
+	: public TooltipUpgradeModuleDataIntermediateBase
 {
 public:
 	virtual ~TooltipUpgradeModuleData();
+
+private:
+	AsciiString m_70;
+	AsciiString m_74;
 };
 
 // ??1TooltipUpgradeModuleData@@UAE@XZ
-__declspec(naked) TooltipUpgradeModuleData::~TooltipUpgradeModuleData()
+TooltipUpgradeModuleData::~TooltipUpgradeModuleData()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0xcb
-		__emit 0x25
-		__emit 0x00
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x04
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0x74
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x63
-		__emit 0xbd
-		__emit 0x75
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0x70
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x56
-		__emit 0xbd
-		__emit 0x75
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0x08
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x83
-		__emit 0xfd
-		__emit 0xee
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0xc7
-		__emit 0x06
-		__emit 0x44
-		__emit 0x37
-		__emit 0x07
-		__emit 0x01
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
-	}
 }
