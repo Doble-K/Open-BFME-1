b7071109186d34d04af848a412b95f3b7e82e676
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 21:38:01 +0000

    Open-BFME5: convert __declspec(naked) TerrainCollisionModuleInfo::TerrainCollisionModuleInfo() to exact C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/TerrainCollisionModuleInfoCtorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/TerrainCollisionModuleInfoCtorThunk.cpp
@@ -1,112 +1,37 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /ICode/GameEngine/Source/Common/System /ICode/Libraries/Source/WWVegas/WWLib
+
+#include "ascii_string.h"
+#include "game_client_random_variable.h"
+#include "snapshot.h"
+
+class Xfer;
+class FXList;
 
 namespace FXParticleSystem
 {
-class TerrainCollisionModuleInfo
+class TerrainCollisionModuleInfo : public Snapshot
 {
 public:
     TerrainCollisionModuleInfo();
+    virtual ~TerrainCollisionModuleInfo();
+    virtual const char *GetSnapshotName();
+    virtual void LoadPostProcess();
+    virtual void DoXfer(Xfer &xfer);
+    TerrainCollisionModuleInfo &operator=(const TerrainCollisionModuleInfo &that);
+    const FXList *getEventFX();
+
+private:
+    AsciiString m_eventName;
+    GameClientRandomVariable m_eventTime;
+    bool m_flag;
+    const FXList *m_cached;
 };
 
 // ??0TerrainCollisionModuleInfo@FXParticleSystem@@QAE@XZ
-__declspec(naked) TerrainCollisionModuleInfo::TerrainCollisionModuleInfo()
+TerrainCollisionModuleInfo::TerrainCollisionModuleInfo()
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0xd3;
-        __emit 0xc9;
-        __emit 0x03;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x53;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x33;
-        __emit 0xdb;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xf8;
-        __emit 0x12;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x04;
-        __emit 0x6a;
-        __emit 0x01;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x08;
-        __emit 0x53;
-        __emit 0x89;
-        __emit 0x19;
-        __emit 0x89;
-        __emit 0x59;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x59;
-        __emit 0x08;
-        __emit 0x53;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x20;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x18;
-        __emit 0xe8;
-        __emit 0xdf;
-        __emit 0x99;
-        __emit 0xa0;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x14;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x5b;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+    m_cached = 0;
+    m_eventTime.setRange(0.0f, 0.0f, GameClientRandomVariable::UNIFORM);
+    m_flag = false;
 }
 }
