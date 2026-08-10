fae78e700f1be562752012cf21aea395ef8f35f1
Open-BFME5 direct-05 <supportforcodemaster@gmail.com>
2026-08-10 14:47:51 +0000

    Open-BFME5: convert timed reverse animation initializer to clean C++

--- Code/GameEngine/Source/Common/ProcessAnimateWindowSlideFromBottomTimed_initReverseAnimateWindow_Thunk.cpp
+++ Code/GameEngine/Source/Common/ProcessAnimateWindowSlideFromBottomTimed_initReverseAnimateWindow_Thunk.cpp
@@ -1,166 +1,111 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: convert the retail animation initializer to clean C++.
 
-class AnimateWindow;
+typedef unsigned int UnsignedInt;
+typedef int Int;
+typedef float Real;
+
+struct ICoord2D
+{
+	Int x;
+	Int y;
+};
+
+struct BFMECoord2D
+{
+	Real x;
+	Real y;
+
+	BFMECoord2D( void ) {}
+	BFMECoord2D( Real x_, Real y_ ) : x(x_), y(y_) {}
+	BFMECoord2D( const BFMECoord2D &that ) : x(that.x), y(that.y) {}
+	~BFMECoord2D( void ) {}
+};
+
+class GameWindow
+{
+public:
+	Int winSetPosition( Int x, Int y );
+};
+
+class AnimateWindow
+{
+public:
+	virtual void unused( void ) = 0;
+
+	GameWindow *getGameWindow( void ) { return m_gameWindow; }
+	ICoord2D getRestPos( void ) { return m_restPos; }
+
+private:
+	char m_padding[0x1c];
+	ICoord2D m_restPos;
+	GameWindow *m_gameWindow;
+};
+
+class Display
+{
+public:
+	virtual void slot00( void ) = 0;
+	virtual void slot01( void ) = 0;
+	virtual void slot02( void ) = 0;
+	virtual void slot03( void ) = 0;
+	virtual void slot04( void ) = 0;
+	virtual void slot05( void ) = 0;
+	virtual void slot06( void ) = 0;
+	virtual void slot07( void ) = 0;
+	virtual void slot08( void ) = 0;
+	virtual void slot09( void ) = 0;
+	virtual void slot10( void ) = 0;
+	virtual UnsignedInt getWidth( void ) = 0;
+};
+
+class BFMEAnimateWindowLayout
+{
+public:
+	void setAnimData( ICoord2D startPos, ICoord2D endPos, ICoord2D curPos,
+		ICoord2D restPos, BFMECoord2D vel, UnsignedInt startTime,
+		UnsignedInt endTime );
+};
+
+extern Display *TheDisplay;
+extern "C" UnsignedInt __stdcall bfme_timeGetTime( void );
+
 class ProcessAnimateWindowSlideFromBottomTimed
 {
 public:
-	virtual void initReverseAnimateWindow(AnimateWindow *, unsigned int);
+	virtual void initReverseAnimateWindow( AnimateWindow *, UnsignedInt );
+
+	UnsignedInt m_maxDuration;
 };
 
 // ?initReverseAnimateWindow@ProcessAnimateWindowSlideFromBottomTimed@@UAEXPAVAnimateWindow@@I@Z
-__declspec(naked) void ProcessAnimateWindowSlideFromBottomTimed::initReverseAnimateWindow(AnimateWindow *, unsigned int)
+void ProcessAnimateWindowSlideFromBottomTimed::initReverseAnimateWindow( AnimateWindow *animWin,
+	UnsignedInt maxDelay )
 {
-	__asm {
-		__emit 0x83
-		__emit 0xec
-		__emit 0x0c
-		__emit 0x53
-		__emit 0x8b
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x55
-		__emit 0x89
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x33
-		__emit 0xc9
-		__emit 0x3b
-		__emit 0xd9
-		__emit 0x56
-		__emit 0x89
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x89
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x89
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x89
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x57
-		__emit 0x89
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x89
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x18
-		__emit 0x74
-		__emit 0x5f
-		__emit 0x8b
-		__emit 0x43
-		__emit 0x28
-		__emit 0x3b
-		__emit 0xc1
-		__emit 0x89
-		__emit 0x44
-		__emit 0x24
-		__emit 0x20
-		__emit 0x74
-		__emit 0x54
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x70
-		__emit 0x12
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x73
-		__emit 0x20
-		__emit 0x8b
-		__emit 0x7b
-		__emit 0x24
-		__emit 0xff
-		__emit 0x50
-		__emit 0x2c
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x20
-		__emit 0x57
-		__emit 0x56
-		__emit 0x8d
-		__emit 0x2c
-		__emit 0x38
-		__emit 0xe8
-		__emit 0xe6
-		__emit 0x2b
-		__emit 0xb8
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x28
-		__emit 0x19
-		__emit 0xbb
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x8b
-		__emit 0x51
-		__emit 0x04
-		__emit 0x03
-		__emit 0xd0
-		__emit 0x52
-		__emit 0x50
-		__emit 0x83
-		__emit 0xec
-		__emit 0x08
-		__emit 0x8b
-		__emit 0xc4
-		__emit 0x89
-		__emit 0x64
-		__emit 0x24
-		__emit 0x30
-		__emit 0x57
-		__emit 0x56
-		__emit 0x57
-		__emit 0x56
-		__emit 0x55
-		__emit 0x56
-		__emit 0x57
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xcb
-		__emit 0xc7
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc7
-		__emit 0x40
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0xcd
-		__emit 0x6c
-		__emit 0xba
-		__emit 0xff
-		__emit 0x5f
-		__emit 0x5e
-		__emit 0x5d
-		__emit 0x5b
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x0c
-		__emit 0xc2
-		__emit 0x08
-		__emit 0x00
-	}
+	ICoord2D restPos = {0,0};
+	ICoord2D startPos = {0,0};
+	ICoord2D curPos = {0,0};
+	ICoord2D endPos = {0,0};
+	BFMECoord2D vel(0.0f,0.0f);
+
+	if(!animWin)
+		return;
+
+	GameWindow *win = animWin->getGameWindow();
+	if(!win)
+		return;
+
+	restPos = animWin->getRestPos();
+	startPos.x = restPos.x;
+	curPos.y = startPos.y = restPos.y;
+
+	Int travelDistance = TheDisplay->getWidth();
+	endPos.x = curPos.x = restPos.x;
+	endPos.y = restPos.y + travelDistance;
+
+	win->winSetPosition(startPos.x, startPos.y);
+
+	UnsignedInt now = bfme_timeGetTime();
+	((BFMEAnimateWindowLayout *)animWin)->setAnimData(startPos, endPos, curPos,
+		restPos, vel, now, now + m_maxDuration);
 }
