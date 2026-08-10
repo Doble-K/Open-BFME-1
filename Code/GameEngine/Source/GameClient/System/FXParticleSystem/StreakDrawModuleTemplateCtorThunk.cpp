e858e823b6f016667cdca68ddf0ca304ec9b4636
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 18:45:07 +0000

    Open-BFME5: convert __declspec(naked) StreakDrawModuleTemplate::StreakDrawModuleTemplate() to exact C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/StreakDrawModuleTemplateCtorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/StreakDrawModuleTemplateCtorThunk.cpp
@@ -1,125 +1,65 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// Open-BFME5: clean-C++ StreakDrawModuleTemplate default constructor.
 
 namespace FXParticleSystem
 {
 
-class StreakDrawModuleTemplate
+class ModuleTemplate
+{
+public:
+    ModuleTemplate() {}
+    virtual ~ModuleTemplate() {}
+};
+
+template <int Category>
+class CategoryModuleInfo
+{
+public:
+    CategoryModuleInfo() {}
+    virtual void unusedVirtual();
+
+protected:
+    ~CategoryModuleInfo() {}
+};
+
+template <int Category>
+class CategoryModuleTemplateBase : public ModuleTemplate,
+    public CategoryModuleInfo<Category>
+{
+public:
+    CategoryModuleTemplateBase() {}
+    virtual ~CategoryModuleTemplateBase() {}
+};
+
+template <int Category>
+class CategoryModuleTemplate : public CategoryModuleTemplateBase<Category>
+{
+public:
+    CategoryModuleTemplate() {}
+    virtual ~CategoryModuleTemplate() {}
+};
+
+class StreakDrawModuleInfo
+{
+public:
+    StreakDrawModuleInfo();
+    virtual ~StreakDrawModuleInfo();
+
+private:
+    unsigned char m_body[0x2c];
+};
+
+class StreakDrawModuleTemplate : public CategoryModuleTemplate<6>,
+    public StreakDrawModuleInfo
 {
 public:
     StreakDrawModuleTemplate();
 };
 
 // ??0StreakDrawModuleTemplate@FXParticleSystem@@QAE@XZ
-__declspec(naked) StreakDrawModuleTemplate::StreakDrawModuleTemplate()
+StreakDrawModuleTemplate::StreakDrawModuleTemplate()
+    : CategoryModuleTemplate<6>(), StreakDrawModuleInfo()
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x98;
-        __emit 0xab;
-        __emit 0x03;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x7c;
-        __emit 0xf9;
-        __emit 0x10;
-        __emit 0x01;
-        __emit 0x57;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x50;
-        __emit 0xfc;
-        __emit 0x10;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x4c;
-        __emit 0xfc;
-        __emit 0x10;
-        __emit 0x01;
-        __emit 0x8d;
-        __emit 0x7e;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0xcf;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xe8;
-        __emit 0x92;
-        __emit 0x00;
-        __emit 0xa4;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0xc7;
-        __emit 0x07;
-        __emit 0xa4;
-        __emit 0x0c;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x90;
-        __emit 0x0c;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x8c;
-        __emit 0x0c;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0x5f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
 }
 
 }
