ed2d56d76f6f7890e7c707f8a1fac6baa634edd8
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 22:31:55 +0000

    Open-BFME5: convert __declspec(naked) SpecialPowerTemplate::~SpecialPowerTemplate() to exact C++

--- Code/GameEngine/Source/Common/SpecialPowerTemplateDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/SpecialPowerTemplateDestructorThunk.cpp
@@ -1,161 +1,68 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: retail-layout C++ conversion of the SpecialPowerTemplate destructor.
 
-class __declspec(novtable) SpecialPowerTemplate
+class AsciiString
+{
+public:
+	~AsciiString();
+
+private:
+	char *m_text;
+};
+
+class AudioEventRTS
+{
+private:
+	unsigned char m_data[0x6c];
+
+public:
+	virtual ~AudioEventRTS();
+};
+
+class Overridable
+{
+protected:
+	__forceinline virtual ~Overridable()
+	{
+		if (m_nextOverride)
+			m_nextOverride->deleteInstance();
+		m_nextOverride = 0;
+	}
+
+	void deleteInstance()
+	{
+		delete this;
+	}
+
+	Overridable *m_nextOverride;
+	bool m_isOverride;
+};
+
+class SpecialPowerTemplate : public Overridable
 {
 protected:
 	virtual ~SpecialPowerTemplate();
+
+private:
+	AsciiString m_name;
+	int m_id;
+	int m_type;
+	int m_reloadTime;
+	int m_requiredScience;
+	int m_academyClassificationType;
+	AudioEventRTS m_initiateSound;
+	AudioEventRTS m_initiateAtLocationSound;
+	int m_detectionTime;
+	int m_viewObjectDuration;
+	float m_viewObjectRange;
+	float m_radiusCursorRadius;
+	bool m_publicTimer;
+	bool m_sharedNSync;
+	bool m_shortcutPower;
+	AsciiString m_tail;
 };
 
 // ??1SpecialPowerTemplate@@MAE@XZ
-__declspec(naked) SpecialPowerTemplate::~SpecialPowerTemplate()
+SpecialPowerTemplate::~SpecialPowerTemplate()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x3c
-		__emit 0xb5
-		__emit 0xff
-		__emit 0x00
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x04
-		__emit 0xc7
-		__emit 0x06
-		__emit 0xc0
-		__emit 0x5c
-		__emit 0x08
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x8e
-		__emit 0x18
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x03
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x3a
-		__emit 0xc3
-		__emit 0x79
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x8e
-		__emit 0x94
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x02
-		__emit 0xe8
-		__emit 0x1f
-		__emit 0xb9
-		__emit 0xf3
-		__emit 0xff
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0x24
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x01
-		__emit 0xe8
-		__emit 0x12
-		__emit 0xb9
-		__emit 0xf3
-		__emit 0xff
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0x0c
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x10
-		__emit 0xc3
-		__emit 0x79
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4e
-		__emit 0x04
-		__emit 0x85
-		__emit 0xc9
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xc7
-		__emit 0x06
-		__emit 0xb0
-		__emit 0xfc
-		__emit 0x07
-		__emit 0x01
-		__emit 0x74
-		__emit 0x06
-		__emit 0x8b
-		__emit 0x01
-		__emit 0x6a
-		__emit 0x01
-		__emit 0xff
-		__emit 0x10
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
-	}
 }
