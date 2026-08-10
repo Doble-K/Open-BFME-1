5b83612c3096acadb5fec77f3df3b81c8e8dd3b8
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 22:33:00 +0000

    Open-BFME5: convert __declspec(naked) LANPlayer *LANGameSlot::getUser() to exact C++

--- Code/GameEngine/Source/Common/LANGameSlot_getUser_Thunk.cpp
+++ Code/GameEngine/Source/Common/LANGameSlot_getUser_Thunk.cpp
@@ -1,162 +1,86 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /MD /EHsc /Ireference/shims/languagefilter /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib
 
-class LANPlayer;
+#include "Common/UnicodeString.h"
 
-class LANGameSlot
+enum SlotState
+{
+	SLOT_OPEN,
+	SLOT_CLOSED,
+	SLOT_EASY_AI,
+	SLOT_MED_AI,
+	SLOT_BRUTAL_AI,
+	SLOT_PLAYER
+};
+
+struct LANPlayerAddress
+{
+	UnsignedInt m_IP;
+	UnsignedInt m_bfmeLayoutPad;
+};
+
+class GameSlot
 {
 public:
-	LANPlayer *getUser();
+	virtual void reset(void) = 0;
+	Bool isHuman(void) const;
+	UnicodeString getName(void) const;
+
+protected:
+	SlotState m_state;
+	Bool m_isAccepted;
+	Bool m_hasMap;
+	Bool m_isMuted;
+	Int m_color;
+	Int m_startPos;
+	Int m_playerTemplate;
+	Int m_teamNumber;
+	Int m_origColor;
+	Int m_origStartPos;
+	Int m_origPlayerTemplate;
+	UnicodeString m_name;
+	UnsignedInt m_bfmeNamePad;
+	LANPlayerAddress m_address;
+	UnsignedInt m_lastFrameInGame;
+	Bool m_disconnected;
+	UnsignedInt m_bfmeLayoutPad;
+};
+
+class LANPlayer
+{
+public:
+	void setLastHeard(UnsignedInt lastHeard) { m_lastHeard = lastHeard; }
+	void setNext(LANPlayer *next) { m_next = next; }
+
+	UnicodeString m_name;
+	UnicodeString m_login;
+	UnicodeString m_host;
+	UnsignedInt m_lastHeard;
+	LANPlayer *m_next;
+	LANPlayerAddress m_address;
+};
+
+class LANGameSlot : public GameSlot
+{
+public:
+	LANPlayer *getUser(void);
+	UnsignedInt getLastHeard(void) const { return m_lastHeard; }
+
+private:
+	LANPlayer m_user;
+	UnsignedInt m_serial;
+	UnsignedInt m_lastHeard;
 };
 
 // ?getUser@LANGameSlot@@QAEPAVLANPlayer@@XZ
-__declspec(naked) LANPlayer *LANGameSlot::getUser()
+LANPlayer *LANGameSlot::getUser(void)
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x78
-		__emit 0x6c
-		__emit 0x04
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0xe8
-		__emit 0x8d
-		__emit 0x9b
-		__emit 0x99
-		__emit 0xff
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x74
-		__emit 0x5d
-		__emit 0x8b
-		__emit 0x46
-		__emit 0x30
-		__emit 0x8b
-		__emit 0x4e
-		__emit 0x34
-		__emit 0x57
-		__emit 0x89
-		__emit 0x46
-		__emit 0x58
-		__emit 0x8b
-		__emit 0x46
-		__emit 0x64
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x08
-		__emit 0x89
-		__emit 0x4e
-		__emit 0x5c
-		__emit 0x52
-		__emit 0x8b
-		__emit 0xce
-		__emit 0x89
-		__emit 0x46
-		__emit 0x50
-		__emit 0xe8
-		__emit 0xaa
-		__emit 0xc3
-		__emit 0x9a
-		__emit 0xff
-		__emit 0x8d
-		__emit 0x7e
-		__emit 0x44
-		__emit 0x50
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x18
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0xbc
-		__emit 0xa6
-		__emit 0x1f
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x4b
-		__emit 0xa3
-		__emit 0x1f
-		__emit 0x00
-		__emit 0x8b
-		__emit 0xc7
-		__emit 0x5f
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x54
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5e
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x04
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x33
-		__emit 0xc0
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
+	if (isHuman())
+	{
+		m_user.m_address = m_address;
+		m_user.setLastHeard(getLastHeard());
+		m_user.m_name = getName();
+		m_user.setNext(NULL);
+		return &m_user;
 	}
+	return NULL;
 }
