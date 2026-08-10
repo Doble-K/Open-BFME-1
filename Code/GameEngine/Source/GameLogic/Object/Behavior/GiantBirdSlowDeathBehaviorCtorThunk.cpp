9ca26a53eea2aab00dc42dc497dd1812d85b4d9a
Open-BFME5 direct-01 <supportforcodemaster@gmail.com>
2026-08-10 14:23:23 +0000

    Open-BFME5: convert GiantBirdSlowDeathBehavior ctor to C++

--- Code/GameEngine/Source/GameLogic/Object/Behavior/GiantBirdSlowDeathBehaviorCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/GiantBirdSlowDeathBehaviorCtorThunk.cpp
@@ -3,152 +3,97 @@
 class Thing;
 class ModuleData;
 
-class GiantBirdSlowDeathBehavior
+class AsciiString
+{
+public:
+    static AsciiString TheEmptyString;
+};
+
+enum ObjectID
+{
+    INVALID_OBJECT_ID = 0
+};
+
+class AudioEventRTS
+{
+public:
+    void init(const AsciiString &, ObjectID);
+
+private:
+    void *m_vtable;
+    unsigned char m_data[0x6c];
+};
+
+class SlowDeathBehaviorPrimary
+{
+public:
+    virtual void slowDeathBehaviorPrimaryAnchor();
+
+private:
+    unsigned char m_data[8];
+};
+
+class SlowDeathBehaviorSecondary
+{
+public:
+    virtual void slowDeathBehaviorSecondaryAnchor();
+};
+
+class SlowDeathBehaviorTertiary
+{
+public:
+    virtual void slowDeathBehaviorTertiaryAnchor();
+
+private:
+    unsigned char m_data[0xc];
+};
+
+class SlowDeathBehaviorDieInterface
+{
+public:
+    virtual void slowDeathBehaviorDieAnchor();
+};
+
+class SlowDeathBehaviorInterface
+{
+public:
+    virtual void slowDeathBehaviorAnchor();
+};
+
+class SlowDeathBehavior : public SlowDeathBehaviorPrimary,
+    public SlowDeathBehaviorSecondary,
+    public SlowDeathBehaviorTertiary,
+    public SlowDeathBehaviorDieInterface,
+    public SlowDeathBehaviorInterface
+{
+public:
+    SlowDeathBehavior(Thing *, const ModuleData *);
+    virtual ~SlowDeathBehavior();
+
+private:
+    unsigned char m_data[0x28];
+};
+
+class GiantBirdSlowDeathBehavior : public SlowDeathBehavior
 {
 public:
     GiantBirdSlowDeathBehavior(Thing *, const ModuleData *);
+
+private:
+    unsigned int m_acceleratedTimeScale;
+    AudioEventRTS m_deathSound;
+    unsigned char m_pad[0xc];
+    unsigned int m_unknownD0;
+    unsigned char m_unknownD4;
 };
 
 // ??0GiantBirdSlowDeathBehavior@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) GiantBirdSlowDeathBehavior::GiantBirdSlowDeathBehavior(Thing *, const ModuleData *)
+GiantBirdSlowDeathBehavior::GiantBirdSlowDeathBehavior(Thing *thing, const ModuleData *moduleData)
+    : SlowDeathBehavior(thing, moduleData)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x18;
-        __emit 0xb7;
-        __emit 0x00;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x53;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0xe8;
-        __emit 0x8c;
-        __emit 0x4c;
-        __emit 0xe4;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xdb;
-        __emit 0x53;
-        __emit 0x68;
-        __emit 0x50;
-        __emit 0x6e;
-        __emit 0x33;
-        __emit 0x01;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x54;
-        __emit 0x89;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x84;
-        __emit 0x49;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xc0;
-        __emit 0x48;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xb4;
-        __emit 0x48;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0xb0;
-        __emit 0x48;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0x9c;
-        __emit 0x48;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0x61;
-        __emit 0x57;
-        __emit 0xe2;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x50;
-        __emit 0x89;
-        __emit 0x9e;
-        __emit 0xd0;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x88;
-        __emit 0x9e;
-        __emit 0xd4;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x5b;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+    unsigned int zero = 0;
+    m_deathSound.init(AsciiString::TheEmptyString, (ObjectID)zero);
+    m_acceleratedTimeScale = zero;
+    m_unknownD0 = zero;
+    m_unknownD4 = (unsigned char)zero;
 }
