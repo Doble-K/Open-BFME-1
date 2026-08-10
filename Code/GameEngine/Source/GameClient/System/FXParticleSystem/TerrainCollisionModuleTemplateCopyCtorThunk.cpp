a2331f8d827a094c6cae26c2fb9d3f52c595a03a
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 20:23:03 +0000

    Open-BFME5: convert __declspec(naked) TerrainCollisionModuleTemplate::TerrainCollisionModuleTemplate(const TerrainCollisionModuleTemplate &) to exact C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/TerrainCollisionModuleTemplateCopyCtorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/TerrainCollisionModuleTemplateCopyCtorThunk.cpp
@@ -1,174 +1,67 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /O2 /Ob2
 
 namespace FXParticleSystem
 {
 
-class TerrainCollisionModuleTemplate
+class TerrainCollisionModuleInfo
 {
 public:
-    TerrainCollisionModuleTemplate(const TerrainCollisionModuleTemplate &);
+    TerrainCollisionModuleInfo(const TerrainCollisionModuleInfo &);
+    virtual ~TerrainCollisionModuleInfo();
+};
+
+class TerrainCollisionCategoryBaseA
+{
+public:
+    virtual ~TerrainCollisionCategoryBaseA();
+};
+
+class TerrainCollisionCategoryBaseB
+{
+public:
+    virtual ~TerrainCollisionCategoryBaseB();
+};
+
+class TerrainCollisionCategoryBase
+    : public TerrainCollisionCategoryBaseA,
+      public TerrainCollisionCategoryBaseB
+{
+public:
+    TerrainCollisionCategoryBase(const TerrainCollisionCategoryBase &) {}
+    virtual ~TerrainCollisionCategoryBase();
+};
+
+class TerrainCollisionCategoryTemplate : public TerrainCollisionCategoryBase
+{
+public:
+    TerrainCollisionCategoryTemplate(const TerrainCollisionCategoryTemplate &that)
+        : TerrainCollisionCategoryBase(that)
+    {
+        const void *source = &that;
+        const void *word_source = source ? (const unsigned char *)source + 8 : 0;
+        m_word = *(const unsigned short *)word_source;
+        *(volatile unsigned int *)this = 0x01073848;
+        *(volatile unsigned int *)((unsigned char *)this + 4) = 0x01073844;
+    }
+    virtual ~TerrainCollisionCategoryTemplate();
+
+private:
+    volatile unsigned short m_word;
+};
+
+class TerrainCollisionModuleTemplate : public TerrainCollisionCategoryTemplate,
+                                       public TerrainCollisionModuleInfo
+{
+public:
+    TerrainCollisionModuleTemplate(const TerrainCollisionModuleTemplate &that);
+    virtual ~TerrainCollisionModuleTemplate();
 };
 
 // ??0TerrainCollisionModuleTemplate@FXParticleSystem@@QAE@ABV01@@Z
-__declspec(naked) TerrainCollisionModuleTemplate::TerrainCollisionModuleTemplate(const TerrainCollisionModuleTemplate &)
+TerrainCollisionModuleTemplate::TerrainCollisionModuleTemplate(const TerrainCollisionModuleTemplate &that)
+    : TerrainCollisionCategoryTemplate(that),
+      TerrainCollisionModuleInfo(that)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x98;
-        __emit 0xaf;
-        __emit 0x03;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x85;
-        __emit 0xc0;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x5c;
-        __emit 0x37;
-        __emit 0x07;
-        __emit 0x01;
-        __emit 0x57;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x30;
-        __emit 0x38;
-        __emit 0x07;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x2c;
-        __emit 0x38;
-        __emit 0x07;
-        __emit 0x01;
-        __emit 0x74;
-        __emit 0x05;
-        __emit 0x8d;
-        __emit 0x48;
-        __emit 0x08;
-        __emit 0xeb;
-        __emit 0x02;
-        __emit 0x33;
-        __emit 0xc9;
-        __emit 0x66;
-        __emit 0x8b;
-        __emit 0x09;
-        __emit 0x66;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x08;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x48;
-        __emit 0x38;
-        __emit 0x07;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x44;
-        __emit 0x38;
-        __emit 0x07;
-        __emit 0x01;
-        __emit 0x85;
-        __emit 0xc0;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x74;
-        __emit 0x05;
-        __emit 0x83;
-        __emit 0xc0;
-        __emit 0x0c;
-        __emit 0xeb;
-        __emit 0x02;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x8d;
-        __emit 0x7e;
-        __emit 0x0c;
-        __emit 0x50;
-        __emit 0x8b;
-        __emit 0xcf;
-        __emit 0xe8;
-        __emit 0x63;
-        __emit 0x9f;
-        __emit 0xa2;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0xc7;
-        __emit 0x07;
-        __emit 0x24;
-        __emit 0x13;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x10;
-        __emit 0x13;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x0c;
-        __emit 0x13;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0x5f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x04;
-        __emit 0x00;
-    }
 }
 
 }
