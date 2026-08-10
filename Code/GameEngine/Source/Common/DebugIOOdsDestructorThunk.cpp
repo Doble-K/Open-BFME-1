51747b04b700b942e87730f510efaf3dcad66e7e
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 21:07:23 +0000

    Open-BFME5: convert __declspec(naked) DebugIOOds::~DebugIOOds() to exact C++

--- Code/GameEngine/Source/Common/DebugIOOdsDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/DebugIOOdsDestructorThunk.cpp
@@ -1,157 +1,64 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /MD /EHa /Oy-
+// Open-BFME5: clean C++ reconstruction of the ICF-shared DebugIOFlat body.
 
-class __declspec(novtable) DebugIOOds
+void DebugFreeMemory(void *ptr);
+
+class DebugIOFlat
 {
+public:
+    class OutputStream
+    {
+    public:
+        void Delete(const char *path);
+    };
+};
+
+class DebugIOInterface
+{
+public:
+    virtual ~DebugIOInterface() {}
+};
+
+class DebugIOOds : public DebugIOInterface
+{
+    struct SplitListEntry
+    {
+        SplitListEntry *next;
+    };
+
+    struct StreamListEntry
+    {
+        StreamListEntry *next;
+        DebugIOFlat::OutputStream *stream;
+    };
+
+    SplitListEntry *m_firstSplit;
+    SplitListEntry **m_lastSplitPtr;
+    StreamListEntry *m_firstStream;
+    StreamListEntry **m_lastStreamPtr;
+    char m_baseFilename[256];
+    char m_copyDir[256];
+
 public:
     virtual ~DebugIOOds();
 };
 
 // ??1DebugIOOds@@UAE@XZ
-__declspec(naked) DebugIOOds::~DebugIOOds()
+DebugIOOds::~DebugIOOds()
 {
-    __asm {
-        __emit 0x55
-        __emit 0x8b
-        __emit 0xec
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x78
-        __emit 0x6b
-        __emit 0x05
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x53
-        __emit 0x8b
-        __emit 0xd9
-        __emit 0x56
-        __emit 0x57
-        __emit 0x89
-        __emit 0x5d
-        __emit 0xf0
-        __emit 0xc7
-        __emit 0x03
-        __emit 0x7c
-        __emit 0x51
-        __emit 0x13
-        __emit 0x01
-        __emit 0x33
-        __emit 0xff
-        __emit 0x89
-        __emit 0x7d
-        __emit 0xfc
-        __emit 0x8b
-        __emit 0x73
-        __emit 0x04
-        __emit 0x3b
-        __emit 0xf7
-        __emit 0x74
-        __emit 0x11
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x8b
-        __emit 0x36
-        __emit 0x50
-        __emit 0xe8
-        __emit 0x13
-        __emit 0xe3
-        __emit 0xff
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x3b
-        __emit 0xf7
-        __emit 0x75
-        __emit 0xef
-        __emit 0x8b
-        __emit 0x73
-        __emit 0x0c
-        __emit 0x3b
-        __emit 0xf7
-        __emit 0x89
-        __emit 0x7b
-        __emit 0x04
-        __emit 0x74
-        __emit 0x22
-        __emit 0x8b
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xfe
-        __emit 0x8b
-        __emit 0x4f
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x36
-        __emit 0x8d
-        __emit 0x83
-        __emit 0x14
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0xe8
-        __emit 0xbd
-        __emit 0xfc
-        __emit 0xff
-        __emit 0xff
-        __emit 0x57
-        __emit 0xe8
-        __emit 0xe7
-        __emit 0xe2
-        __emit 0xff
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x75
-        __emit 0xe0
-        __emit 0xc7
-        __emit 0x45
-        __emit 0xfc
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4d
-        __emit 0xf4
-        __emit 0x5f
-        __emit 0xc7
-        __emit 0x03
-        __emit 0x44
-        __emit 0x50
-        __emit 0x13
-        __emit 0x01
-        __emit 0x5e
-        __emit 0x5b
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xe5
-        __emit 0x5d
-        __emit 0xc3
+    for (SplitListEntry *cur=m_firstSplit;cur;)
+    {
+        SplitListEntry *kill=cur;
+        cur=cur->next;
+        DebugFreeMemory(kill);
+    }
+    m_firstSplit=0;
+
+    for (StreamListEntry *stream=m_firstStream;stream;)
+    {
+        StreamListEntry *kill=stream;
+        stream=stream->next;
+        kill->stream->Delete(m_copyDir);
+        DebugFreeMemory(kill);
     }
 }
