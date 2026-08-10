c4261dcd884e15ed9db44b872346912b2298585c
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 19:06:49 +0000

    Open-BFME5: convert __declspec(naked) TextTypeTransition::~TextTypeTransition() to exact C++

--- Code/GameEngine/Source/Common/TextTypeTransitionDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/TextTypeTransitionDestructorThunk.cpp
@@ -1,148 +1,60 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: TextTypeTransition destructor lifted to clean C++.
 
-class __declspec(novtable) TextTypeTransition
+class UnicodeString
 {
 public:
-    virtual ~TextTypeTransition();
+	~UnicodeString();
+
+private:
+	unsigned char m_pad[4];
+};
+
+class DisplayString;
+
+class DisplayStringManager
+{
+public:
+	virtual void unused00();
+	virtual void unused01();
+	virtual void unused02();
+	virtual void unused03();
+	virtual void unused04();
+	virtual void unused05();
+	virtual void unused06();
+	virtual void unused07();
+	virtual void unused08();
+	virtual void unused09();
+	virtual void freeDisplayString(DisplayString *string);
+};
+
+extern DisplayStringManager *TheDisplayStringManager;
+
+class CountUpTransitionBase
+{
+public:
+	virtual ~CountUpTransitionBase();
+};
+
+class TextTypeTransition : public CountUpTransitionBase
+{
+public:
+	virtual ~TextTypeTransition();
+
+private:
+	unsigned char m_gap0[8];
+	void *m_win;
+	unsigned char m_gap1[0x1c];
+	UnicodeString m_fullText;
+	UnicodeString m_partialText;
+	DisplayString *m_dStr;
 };
 
 // ??1TextTypeTransition@@UAE@XZ
-__declspec(naked) TextTypeTransition::~TextTypeTransition()
+TextTypeTransition::~TextTypeTransition()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xbe
-        __emit 0x84
-        __emit 0x03
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xfc
-        __emit 0xcc
-        __emit 0x10
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x34
-        __emit 0x85
-        __emit 0xc0
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x0c
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0xcc
-        __emit 0x12
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x11
-        __emit 0x50
-        __emit 0xff
-        __emit 0x52
-        __emit 0x28
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x30
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x34
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x01
-        __emit 0xe8
-        __emit 0x07
-        __emit 0x7b
-        __emit 0x2e
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x2c
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xfa
-        __emit 0x7a
-        __emit 0x2e
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0xb6
-        __emit 0xa3
-        __emit 0xa7
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
+	m_win = 0;
+	if (m_dStr)
+		TheDisplayStringManager->freeDisplayString(m_dStr);
+	m_dStr = 0;
 }
