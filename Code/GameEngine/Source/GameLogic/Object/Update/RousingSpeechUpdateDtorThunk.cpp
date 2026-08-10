a0e5ead949d4eec178c9baf0179a0fa9e912491d
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 21:26:10 +0000

    Open-BFME5: convert __declspec(naked) RousingSpeechUpdate::~RousingSpeechUpdate() to exact C++

--- Code/GameEngine/Source/GameLogic/Object/Update/RousingSpeechUpdateDtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/RousingSpeechUpdateDtorThunk.cpp
@@ -1,173 +1,93 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS
+// stlport
 
-class __declspec(novtable) RousingSpeechUpdate
+#include <list>
+
+class Gen_dtor_00113f20
+{
+public:
+    virtual ~Gen_dtor_00113f20();
+
+private:
+    unsigned char m_pad[8];
+};
+
+class SpecialAbilityUpdateBaseInterface1
+{
+public:
+    virtual void slot();
+};
+
+class SpecialAbilityUpdateBaseInterface2
+{
+public:
+    virtual void slot();
+
+private:
+    unsigned char m_pad[0xC];
+};
+
+class SpecialAbilityUpdateObjectModule : public Gen_dtor_00113f20
+{
+};
+
+class SpecialAbilityUpdateBehaviorModule : public SpecialAbilityUpdateObjectModule,
+    public SpecialAbilityUpdateBaseInterface1
+{
+public:
+    virtual ~SpecialAbilityUpdateBehaviorModule() {}
+};
+
+class SpecialAbilityUpdateUpdateModule : public SpecialAbilityUpdateBehaviorModule,
+    public SpecialAbilityUpdateBaseInterface2
+{
+public:
+    virtual ~SpecialAbilityUpdateUpdateModule() {}
+};
+
+class SpecialAbilityUpdateInterface
+{
+public:
+    virtual void slot();
+};
+
+class AudioEventRTS
+{
+public:
+    virtual ~AudioEventRTS();
+
+private:
+    unsigned char m_pad[0x94];
+};
+
+class Gen_dtor_002aaec0 : public SpecialAbilityUpdateUpdateModule,
+    public SpecialAbilityUpdateInterface
+{
+public:
+    virtual ~Gen_dtor_002aaec0();
+
+private:
+    unsigned char m_pad[0x10];
+    AudioEventRTS m_prepSoundLoop;
+    _STL::list<int> m_specialObjectIDList;
+    unsigned char m_tail[0x18];
+};
+
+class RousingSpeechUpdate : public Gen_dtor_002aaec0
 {
 public:
     virtual ~RousingSpeechUpdate();
+
+private:
+    _STL::list<int> m_speechObjectIDList;
+    unsigned int m_a;
+    unsigned char m_b;
+    unsigned int m_c;
+    unsigned int m_d;
 };
 
 // ??1RousingSpeechUpdate@@UAE@XZ
-__declspec(naked) RousingSpeechUpdate::~RousingSpeechUpdate()
+RousingSpeechUpdate::~RousingSpeechUpdate()
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0xa6;
-        __emit 0xfa;
-        __emit 0x00;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x53;
-        __emit 0x8b;
-        __emit 0xd9;
-        __emit 0x56;
-        __emit 0x57;
-        __emit 0x89;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0xc7;
-        __emit 0x03;
-        __emit 0x90;
-        __emit 0x6d;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x43;
-        __emit 0x0c;
-        __emit 0xc8;
-        __emit 0x6c;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x43;
-        __emit 0x10;
-        __emit 0xb8;
-        __emit 0x6c;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x43;
-        __emit 0x20;
-        __emit 0x8c;
-        __emit 0x6c;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0x83;
-        __emit 0xe8;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0x30;
-        __emit 0x3b;
-        __emit 0xf0;
-        __emit 0x8d;
-        __emit 0xbb;
-        __emit 0xe8;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x01;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x74;
-        __emit 0x15;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x8b;
-        __emit 0x36;
-        __emit 0x6a;
-        __emit 0x0c;
-        __emit 0x50;
-        __emit 0xe8;
-        __emit 0xf0;
-        __emit 0x97;
-        __emit 0x5c;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0x07;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x08;
-        __emit 0x3b;
-        __emit 0xf0;
-        __emit 0x75;
-        __emit 0xeb;
-        __emit 0x8b;
-        __emit 0x07;
-        __emit 0x89;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0x07;
-        __emit 0x8b;
-        __emit 0xcf;
-        __emit 0x89;
-        __emit 0x40;
-        __emit 0x04;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x00;
-        __emit 0xe8;
-        __emit 0x2b;
-        __emit 0xe6;
-        __emit 0xda;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0xcb;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0xe8;
-        __emit 0x7a;
-        __emit 0xf5;
-        __emit 0xdb;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0x5f;
-        __emit 0x5e;
-        __emit 0x5b;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+	m_speechObjectIDList.clear();
 }
