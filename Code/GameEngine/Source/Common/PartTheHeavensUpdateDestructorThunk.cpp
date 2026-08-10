93783be1fa42b46b5dcd5d9feda9b08877f075a8
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 18:47:56 +0000

    Open-BFME5: convert __declspec(naked) PartTheHeavensUpdate::~PartTheHeavensUpdate() to exact C++

--- Code/GameEngine/Source/Common/PartTheHeavensUpdateDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/PartTheHeavensUpdateDestructorThunk.cpp
@@ -1,135 +1,74 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-class __declspec(novtable) PartTheHeavensUpdate
+// The retail object is an UpdateModule-shaped multiple-inheritance object.  The
+// declarations are deliberately TU-local: only the ABI-relevant base layout and
+// the one explicitly destroyed pointee are needed here.
+
+class Gen_dtor_00113f20
 {
 public:
-    virtual ~PartTheHeavensUpdate();
+	virtual ~Gen_dtor_00113f20();
+
+private:
+	const void *m_moduleData;
+};
+
+class BehaviorModuleInterface
+{
+public:
+	virtual void getBehaviorModuleInterface() = 0;
+};
+
+class UpdateModuleInterface
+{
+public:
+	virtual void updateModuleInterface() = 0;
+};
+
+class ObjectModule : public Gen_dtor_00113f20
+{
+private:
+	void *m_object;
+};
+
+class BehaviorModule : public ObjectModule, public BehaviorModuleInterface
+{
+public:
+	virtual ~BehaviorModule() {}
+};
+
+class UpdateModule : public BehaviorModule, public UpdateModuleInterface
+{
+private:
+	unsigned int m_nextCallFrameAndPhase;
+	int m_indexInLogic;
+	unsigned int m_updateState;
+	unsigned int m_nextUpdateFrame;
+
+public:
+	virtual ~UpdateModule() {}
+};
+
+class PartTheHeavensUpdatePointee
+{
+public:
+	virtual void pointeeSlot0();
+	virtual void pointeeSlot1();
+	virtual void releasePointee();
+};
+
+class PartTheHeavensUpdate : public UpdateModule
+{
+public:
+	virtual ~PartTheHeavensUpdate();
+
+private:
+	PartTheHeavensUpdatePointee *m_pointee;
 };
 
 // ??1PartTheHeavensUpdate@@UAE@XZ
-__declspec(naked) PartTheHeavensUpdate::~PartTheHeavensUpdate()
+PartTheHeavensUpdate::~PartTheHeavensUpdate()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xd8
-        __emit 0x20
-        __emit 0x01
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xec
-        __emit 0x07
-        __emit 0x0c
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x28
-        __emit 0x07
-        __emit 0x0c
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0x18
-        __emit 0x07
-        __emit 0x0c
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x4e
-        __emit 0x24
-        __emit 0x85
-        __emit 0xc9
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x05
-        __emit 0x8b
-        __emit 0x01
-        __emit 0xff
-        __emit 0x50
-        __emit 0x08
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0xac
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x5c
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x98
-        __emit 0xca
-        __emit 0x09
-        __emit 0x01
-        __emit 0xe8
-        __emit 0xcb
-        __emit 0xe3
-        __emit 0xda
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
+	if (m_pointee)
+		m_pointee->releasePointee();
 }
