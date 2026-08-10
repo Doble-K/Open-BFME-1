7d7032e0a11b5a601968b565895e14b4822a8ca5
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 20:30:37 +0000

    Open-BFME5: convert __declspec(naked) FXListStore::~FXListStore() to exact C++

--- Code/GameEngine/Source/GameClient/FXListStoreDestructorThunk.cpp
+++ Code/GameEngine/Source/GameClient/FXListStoreDestructorThunk.cpp
@@ -1,130 +1,45 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
-// Open-BFME5: lift the retail FXListStore destructor MASM body to C++.
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
 
-class __declspec(novtable) FXListStore
+#include <map>
+
+class SubsystemInterface
 {
 public:
-    virtual ~FXListStore();
+	virtual ~SubsystemInterface();
+
+private:
+	void *m_name;
+};
+
+class Snapshot
+{
+public:
+	~Snapshot() {}
+	virtual void crc() = 0;
+	virtual void xfer() = 0;
+	virtual void loadPostProcess() = 0;
+};
+
+enum NameKeyType
+{
+	NAMEKEY_INVALID = 0
+};
+
+class TeamPrototype;
+typedef std::map<NameKeyType, TeamPrototype *, std::less<NameKeyType> > FXListMap;
+
+class FXListStore : public SubsystemInterface, public Snapshot
+{
+public:
+	virtual ~FXListStore();
+
+private:
+	FXListMap *m_fxmap;
 };
 
 // ??1FXListStore@@UAE@XZ
-__declspec(naked) FXListStore::~FXListStore()
+FXListStore::~FXListStore()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x3f
-        __emit 0x1e
-        __emit 0x06
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xec
-        __emit 0x08
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x57
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x0c
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xf8
-        __emit 0x57
-        __emit 0x14
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x08
-        __emit 0xe8
-        __emit 0x57
-        __emit 0x14
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x7e
-        __emit 0x0c
-        __emit 0x85
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x18
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x10
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xe8
-        __emit 0x2d
-        __emit 0x31
-        __emit 0x00
-        __emit 0x00
-        __emit 0x57
-        __emit 0xe8
-        __emit 0x67
-        __emit 0xf6
-        __emit 0xe8
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x08
-        __emit 0x44
-        __emit 0x37
-        __emit 0x07
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x18
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0xde
-        __emit 0xf1
-        __emit 0xfa
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x10
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x14
-        __emit 0xc3
-    }
+	delete m_fxmap;
 }
