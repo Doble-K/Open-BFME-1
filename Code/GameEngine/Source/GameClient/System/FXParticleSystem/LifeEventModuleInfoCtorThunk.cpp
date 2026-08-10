d02a093afd02ee39bc1b8805dfc4be6631e54ee5
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 16:52:46 +0000

    Open-BFME5: convert __declspec(naked) LifeEventModuleInfo::LifeEventModuleInfo() to exact C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/LifeEventModuleInfoCtorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/LifeEventModuleInfoCtorThunk.cpp
@@ -1,107 +1,35 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /ICode/GameEngine/Source/Common/System /ICode/Libraries/Source/WWVegas/WWLib
+
+#include "ascii_string.h"
+#include "game_client_random_variable.h"
+#include "snapshot.h"
+
+class Xfer;
+class FXList;
 
 namespace FXParticleSystem
 {
-class LifeEventModuleInfo
+class LifeEventModuleInfo : public Snapshot
 {
 public:
     LifeEventModuleInfo();
+    virtual ~LifeEventModuleInfo();
+    virtual const char *GetSnapshotName();
+    virtual void LoadPostProcess();
+    virtual void DoXfer(Xfer &xfer);
+    LifeEventModuleInfo &operator=(const LifeEventModuleInfo &that);
+    const FXList *getEventFX();
+
+private:
+    AsciiString m_eventName;
+    GameClientRandomVariable m_eventTime;
+    const FXList *m_cached;
 };
 
 // ??0LifeEventModuleInfo@FXParticleSystem@@QAE@XZ
-__declspec(naked) LifeEventModuleInfo::LifeEventModuleInfo()
+LifeEventModuleInfo::LifeEventModuleInfo()
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0xe3;
-        __emit 0xc8;
-        __emit 0x03;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x78;
-        __emit 0x12;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x6a;
-        __emit 0x01;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0x89;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x41;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x41;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0xe8;
-        __emit 0x00;
-        __emit 0xa5;
-        __emit 0xa0;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+    m_cached = 0;
+    m_eventTime.setRange(0.0f, 0.0f, GameClientRandomVariable::UNIFORM);
 }
 }
