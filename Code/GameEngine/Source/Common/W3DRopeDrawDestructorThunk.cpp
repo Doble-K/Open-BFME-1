9e2e8c8feb5ce64fb91da2981684519964edc559
Open-BFME5 direct-04 <supportforcodemaster@gmail.com>
2026-08-10 16:32:52 +0000

    Open-BFME5 convert W3DRopeDraw destructor to C++

--- Code/GameEngine/Source/Common/W3DRopeDrawDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/W3DRopeDrawDestructorThunk.cpp
@@ -1,158 +1,54 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS
+// stlport
+// Open-BFME5: convert the retail W3DRopeDraw destructor to clean C++.
 
-class __declspec(novtable) W3DRopeDraw
+#include <vector>
+
+// The drawable module's root base occupies twelve bytes before W3DRopeDraw's
+// secondary interface subobject.  The middle base destructor resets the
+// primary vtable, then calls this root destructor through 0x0002B8C8.
+class W3DRopeDrawRootBase
 {
 public:
-    virtual ~W3DRopeDraw();
+	virtual ~W3DRopeDrawRootBase();
+
+private:
+	unsigned char m_pad[8];
+};
+
+class W3DRopeDrawDrawModule : public W3DRopeDrawRootBase
+{
+public:
+	virtual ~W3DRopeDrawDrawModule() {}
+};
+
+class W3DRopeDrawInterface
+{
+public:
+	virtual void initRopeParms() = 0;
+};
+
+struct W3DRopeDrawSegInfo
+{
+	void *line;
+	void *softLine;
+	float wobbleAxisX;
+	float wobbleAxisY;
+};
+
+class W3DRopeDraw : public W3DRopeDrawDrawModule, public W3DRopeDrawInterface
+{
+public:
+	virtual ~W3DRopeDraw();
+
+private:
+	std::vector<W3DRopeDrawSegInfo> m_segments;
+
+	void tossSegments();
 };
 
 // ??1W3DRopeDraw@@UAE@XZ
-__declspec(naked) W3DRopeDraw::~W3DRopeDraw()
+W3DRopeDraw::~W3DRopeDraw()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x43
-        __emit 0xec
-        __emit 0x04
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xe8
-        __emit 0x34
-        __emit 0x12
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0xd4
-        __emit 0x34
-        __emit 0x12
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0xb9
-        __emit 0x18
-        __emit 0x8d
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4e
-        __emit 0x10
-        __emit 0x85
-        __emit 0xc9
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x74
-        __emit 0x27
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x18
-        __emit 0x2b
-        __emit 0xc1
-        __emit 0xc1
-        __emit 0xf8
-        __emit 0x04
-        __emit 0xc1
-        __emit 0xe0
-        __emit 0x04
-        __emit 0x3d
-        __emit 0x80
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x76
-        __emit 0x0b
-        __emit 0x51
-        __emit 0xe8
-        __emit 0x25
-        __emit 0x77
-        __emit 0x12
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0xeb
-        __emit 0x0a
-        __emit 0x50
-        __emit 0x51
-        __emit 0xe8
-        __emit 0x59
-        __emit 0x3e
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x08
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xa8
-        __emit 0x1b
-        __emit 0x12
-        __emit 0x01
-        __emit 0xe8
-        __emit 0x19
-        __emit 0x11
-        __emit 0x8d
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
+	tossSegments();
 }
