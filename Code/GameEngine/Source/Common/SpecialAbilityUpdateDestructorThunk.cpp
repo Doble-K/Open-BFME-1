5d8827e0f967678589dff0a181f57fe9cfc1c5f4
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 21:00:58 +0000

    Open-BFME5: convert __declspec(naked) SpecialAbilityUpdate::~SpecialAbilityUpdate() to exact C++

--- Code/GameEngine/Source/Common/SpecialAbilityUpdateDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/SpecialAbilityUpdateDestructorThunk.cpp
@@ -1,168 +1,81 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS
+// stlport
 
-class __declspec(novtable) SpecialAbilityUpdate
+#include <list>
+
+class Gen_dtor_00113f20
+{
+public:
+	virtual ~Gen_dtor_00113f20();
+
+private:
+	unsigned char m_pad[8];
+};
+
+class SpecialAbilityUpdateBaseInterface1
+{
+public:
+	virtual void slot();
+};
+
+class SpecialAbilityUpdateBaseInterface2
+{
+public:
+	virtual void slot();
+
+private:
+	unsigned char m_pad[0xC];
+};
+
+class SpecialAbilityUpdateObjectModule : public Gen_dtor_00113f20
+{
+};
+
+class SpecialAbilityUpdateBehaviorModule : public SpecialAbilityUpdateObjectModule,
+	public SpecialAbilityUpdateBaseInterface1
+{
+public:
+	virtual ~SpecialAbilityUpdateBehaviorModule() {}
+};
+
+class SpecialAbilityUpdateUpdateModule : public SpecialAbilityUpdateBehaviorModule,
+	public SpecialAbilityUpdateBaseInterface2
+{
+public:
+	virtual ~SpecialAbilityUpdateUpdateModule() {}
+};
+
+class SpecialAbilityUpdateInterface
+{
+public:
+	virtual void slot();
+};
+
+class AudioEventRTS
+{
+public:
+	virtual ~AudioEventRTS();
+
+private:
+	unsigned char m_pad[0x94];
+};
+
+class SpecialAbilityUpdate : public SpecialAbilityUpdateUpdateModule,
+	public SpecialAbilityUpdateInterface
 {
 public:
 	virtual ~SpecialAbilityUpdate();
+
+private:
+	void onExit(bool, bool);
+
+	unsigned char m_pad[0x10];
+	AudioEventRTS m_prepSoundLoop;
+	_STL::list<int> m_specialObjectIDList;
 };
 
 // ??1SpecialAbilityUpdate@@UAE@XZ
-__declspec(naked) SpecialAbilityUpdate::~SpecialAbilityUpdate()
+SpecialAbilityUpdate::~SpecialAbilityUpdate()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0xe1
-		__emit 0x2a
-		__emit 0x01
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x04
-		__emit 0xc7
-		__emit 0x06
-		__emit 0xb8
-		__emit 0x37
-		__emit 0x0c
-		__emit 0x01
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x0c
-		__emit 0xf0
-		__emit 0x36
-		__emit 0x0c
-		__emit 0x01
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x10
-		__emit 0xe4
-		__emit 0x36
-		__emit 0x0c
-		__emit 0x01
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x20
-		__emit 0xb8
-		__emit 0x36
-		__emit 0x0c
-		__emit 0x01
-		__emit 0x6a
-		__emit 0x01
-		__emit 0x6a
-		__emit 0x01
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x18
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x1c
-		__emit 0x50
-		__emit 0xd6
-		__emit 0xff
-		__emit 0x8d
-		__emit 0x8e
-		__emit 0xcc
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x01
-		__emit 0xe8
-		__emit 0xe0
-		__emit 0x8a
-		__emit 0xd6
-		__emit 0xff
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0x34
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x00
-		__emit 0xe8
-		__emit 0xbf
-		__emit 0xc5
-		__emit 0xd7
-		__emit 0xff
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x10
-		__emit 0xac
-		__emit 0xcb
-		__emit 0x09
-		__emit 0x01
-		__emit 0xc7
-		__emit 0x06
-		__emit 0x5c
-		__emit 0xcb
-		__emit 0x09
-		__emit 0x01
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x0c
-		__emit 0x98
-		__emit 0xca
-		__emit 0x09
-		__emit 0x01
-		__emit 0xe8
-		__emit 0xba
-		__emit 0xd2
-		__emit 0xd9
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
-	}
+	onExit(true, true);
 }
