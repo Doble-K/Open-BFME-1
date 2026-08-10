da1ef9253894444e52e2c70d6216d61eb3baf98b
Open-BFME5 direct-04 <supportforcodemaster@gmail.com>
2026-08-10 14:24:02 +0000

    Open-BFME5: convert CleanupHazardUpdate constructor to C++

--- Code/GameEngine/Source/GameLogic/Module/CleanupHazardUpdateConstructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Module/CleanupHazardUpdateConstructorThunk.cpp
@@ -1,142 +1,102 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: clean-C++ reconstruction of the retail CleanupHazardUpdate ctor.
+#pragma intrinsic(_ReadWriteBarrier)
+extern "C" void _ReadWriteBarrier(void);
 
 class Thing;
 class ModuleData;
+class Object;
 
-class CleanupHazardUpdate
+class PB_DeepBase
+{
+public:
+    PB_DeepBase(Thing *, const ModuleData *);
+    virtual ~PB_DeepBase();
+
+protected:
+    const ModuleData *m_moduleData;
+    Object *m_object;
+};
+
+class CleanupHazardUpdateBehaviorInterface
+{
+public:
+    virtual void slot();
+};
+
+class CleanupHazardUpdateUpdateInterface
+{
+public:
+    virtual void slot();
+};
+
+class CleanupHazardUpdateModule
+    : public PB_DeepBase,
+      public CleanupHazardUpdateBehaviorInterface,
+      public CleanupHazardUpdateUpdateInterface
+{
+public:
+    CleanupHazardUpdateModule(Thing *thing, const ModuleData *moduleData)
+        : PB_DeepBase(thing, moduleData),
+          m_nextCallFrameAndPhase(0),
+          m_indexInLogic(-1),
+          m_reserved(-1)
+    {
+    }
+
+protected:
+    volatile unsigned int m_nextCallFrameAndPhase;
+    volatile int m_indexInLogic;
+    volatile int m_reserved;
+};
+
+class __declspec(novtable) CleanupHazardUpdateInterface
+{
+public:
+    virtual void slot();
+    virtual ~CleanupHazardUpdateInterface();
+
+    CleanupHazardUpdateInterface()
+    {
+    }
+};
+
+class __declspec(novtable) CleanupHazardUpdate : public CleanupHazardUpdateModule, public CleanupHazardUpdateInterface
 {
 public:
     CleanupHazardUpdate(Thing *, const ModuleData *);
+
+private:
+    unsigned int m_f24;
+    unsigned int m_f28;
+    unsigned int m_f2c;
+    unsigned int m_f30;
+    unsigned char m_f34;
+    unsigned int m_f38;
+    const void *m_f3c;
+    unsigned int m_f40;
 };
 
 // ??0CleanupHazardUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) CleanupHazardUpdate::CleanupHazardUpdate(Thing *, const ModuleData *)
+CleanupHazardUpdate::CleanupHazardUpdate(Thing *thing, const ModuleData *moduleData)
+    : CleanupHazardUpdateModule(thing, moduleData)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0xb0;
-        __emit 0x6a;
-        __emit 0xd4;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x83;
-        __emit 0xc9;
-        __emit 0xff;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x1c;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x08;
-        __emit 0xbf;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x38;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x64;
-        __emit 0xb7;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xa0;
-        __emit 0xb6;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x90;
-        __emit 0xb6;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x54;
-        __emit 0xb6;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x28;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x2c;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x30;
-        __emit 0x8b;
-        __emit 0x4e;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x40;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x34;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x3c;
-        __emit 0x3b;
-        __emit 0xc8;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x74;
-        __emit 0x06;
-        __emit 0x8b;
-        __emit 0x51;
-        __emit 0x28;
-        __emit 0x89;
-        __emit 0x56;
-        __emit 0x3c;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+    volatile unsigned int *words = (volatile unsigned int *)this;
+    words[8] = 0x010ABF08;
+    words[9] = 0;
+    words[14] = 0;
+    words[0] = 0x010CB764;
+    words[3] = 0x010CB6A0;
+    words[4] = 0x010CB690;
+    words[8] = 0x010CB654;
+    words[10] = 0;
+    words[11] = 0;
+    words[12] = 0;
+    const ModuleData *data = *(const ModuleData * volatile *)((const char *)this + 4);
+    words[16] = 0;
+    *(volatile unsigned char *)((char *)this + 0x34) = 0;
+    *(const void * volatile *)&m_f3c = 0;
+    _ReadWriteBarrier();
+    if (data != 0)
+        m_f3c = *(const void **)((const char *)data + 0x28);
 }
