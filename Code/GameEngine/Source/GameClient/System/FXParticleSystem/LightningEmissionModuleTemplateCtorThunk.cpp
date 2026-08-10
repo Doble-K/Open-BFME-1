97db83919e21e902589119cd72c121e4427438ea
Open-BFME5 direct-06 <supportforcodemaster@gmail.com>
2026-08-10 15:49:00 +0000

    Open-BFME5: convert LightningEmissionModuleTemplate ctor to clean C++

--- Code/GameEngine/Source/GameClient/System/FXParticleSystem/LightningEmissionModuleTemplateCtorThunk.cpp
+++ Code/GameEngine/Source/GameClient/System/FXParticleSystem/LightningEmissionModuleTemplateCtorThunk.cpp
@@ -1,167 +1,65 @@
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// Open-BFME5: clean-C++ LightningEmissionModuleTemplate default constructor.
 
-namespace FXParticleSystem {
+namespace FXParticleSystem
+{
 
-class LightningEmissionModuleTemplate
+class ModuleTemplate
+{
+public:
+    ModuleTemplate() {}
+    virtual ~ModuleTemplate() {}
+};
+
+template <int Category>
+class CategoryModuleInfo
+{
+public:
+    CategoryModuleInfo() {}
+    virtual void unusedVirtual();
+
+protected:
+    ~CategoryModuleInfo() {}
+};
+
+template <int Category>
+class CategoryModuleTemplateBase : public ModuleTemplate,
+    public CategoryModuleInfo<Category>
+{
+public:
+    CategoryModuleTemplateBase() {}
+    virtual ~CategoryModuleTemplateBase() {}
+};
+
+template <int Category>
+class CategoryModuleTemplate : public CategoryModuleTemplateBase<Category>
+{
+public:
+    CategoryModuleTemplate() {}
+    virtual ~CategoryModuleTemplate() {}
+};
+
+class LightningEmissionInfo
+{
+public:
+    LightningEmissionInfo();
+    virtual ~LightningEmissionInfo();
+
+private:
+    unsigned char m_body[0x7c];
+};
+
+class LightningEmissionModuleTemplate : public CategoryModuleTemplate<5>,
+    public LightningEmissionInfo
 {
 public:
     LightningEmissionModuleTemplate();
 };
 
 // ??0LightningEmissionModuleTemplate@FXParticleSystem@@QAE@XZ
-__declspec(naked) LightningEmissionModuleTemplate::LightningEmissionModuleTemplate()
+LightningEmissionModuleTemplate::LightningEmissionModuleTemplate()
+    : CategoryModuleTemplate<5>(), LightningEmissionInfo()
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x78
-        __emit 0xaa
-        __emit 0x03
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x04
-        __emit 0xac
-        __emit 0xf9
-        __emit 0x10
-        __emit 0x01
-        __emit 0x57
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x08
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x20
-        __emit 0xfc
-        __emit 0x10
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x04
-        __emit 0x1c
-        __emit 0xfc
-        __emit 0x10
-        __emit 0x01
-        __emit 0x8d
-        __emit 0x7e
-        __emit 0x08
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x16
-        __emit 0xb3
-        __emit 0xa6
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x0c
-        __emit 0xc7
-        __emit 0x07
-        __emit 0xb8
-        __emit 0x0a
-        __emit 0x11
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xa4
-        __emit 0x0a
-        __emit 0x11
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x04
-        __emit 0xa0
-        __emit 0x0a
-        __emit 0x11
-        __emit 0x01
-        __emit 0x5f
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-        __emit 0xcc
-    }
 }
 
 }
