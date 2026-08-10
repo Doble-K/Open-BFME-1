874a06b988926ee0b7dec8da6e739f6b676a9130
Open-BFME5 Integrator <supportforcodemaster@gmail.com>
2026-08-10 22:45:05 +0000

    Open-BFME5: convert __declspec(naked) void SoundFXNugget::doFXObj(const Object *, const Object *) const to exact C++

--- Code/GameEngine/Source/Common/SoundFXNugget_doFXObj_Thunk.cpp
+++ Code/GameEngine/Source/Common/SoundFXNugget_doFXObj_Thunk.cpp
@@ -1,162 +1,108 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-class Object;
+class AsciiString
+{
+private:
+	void *m_data;
+};
+
+enum ObjectID
+{
+	OBJECT_ID_UNUSED = 0
+};
+
+struct Coord3D
+{
+	float x;
+	float y;
+	float z;
+};
+
+class Player
+{
+public:
+	int getPlayerIndex() const
+	{
+		return m_playerIndex;
+	}
+
+private:
+	unsigned char m_pad[0x24];
+	int m_playerIndex;
+};
+
+class Object
+{
+public:
+	Player *getControllingPlayer() const;
+
+	const Coord3D *getPosition() const
+	{
+		return &m_cachedPos;
+	}
+
+private:
+	unsigned char m_pad[0x38];
+	Coord3D m_cachedPos;
+};
+
+class AudioEventRTS
+{
+public:
+	AudioEventRTS(const AsciiString& eventName, ObjectID ownerID);
+	virtual ~AudioEventRTS();
+
+	void setPlayerIndex(int playerIndex);
+	void setPosition(const Coord3D *position);
+
+private:
+	unsigned char m_pad[0x6c];
+};
+
+class AudioManager
+{
+public:
+	virtual void unused00();
+	virtual void unused01();
+	virtual void unused02();
+	virtual void unused03();
+	virtual void unused04();
+	virtual void unused05();
+	virtual void unused06();
+	virtual void unused07();
+	virtual void unused08();
+	virtual void unused09();
+	virtual void unused10();
+	virtual void unused11();
+	virtual void unused12();
+	virtual void unused13();
+	virtual void unused14();
+	virtual void unused15();
+	virtual void unused16();
+	virtual unsigned int addAudioEvent(const AudioEventRTS *event);
+};
+
+extern AudioManager *TheAudio;
+
 class SoundFXNugget
 {
 public:
-	virtual void doFXObj(const Object *, const Object *) const;
+	virtual void doFXObj(const Object *primary, const Object *secondary) const;
+
+private:
+	unsigned char m_pad[0xb0];
+	AsciiString m_soundName;
 };
 
 // ?doFXObj@SoundFXNugget@@UBEXPBVObject@@0@Z
-__declspec(naked) void SoundFXNugget::doFXObj(const Object *, const Object *) const
+void SoundFXNugget::doFXObj(const Object *primary, const Object *) const
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x58
-		__emit 0x12
-		__emit 0x02
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xec
-		__emit 0x70
-		__emit 0x56
-		__emit 0x81
-		__emit 0xc1
-		__emit 0xb4
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x6a
-		__emit 0x00
-		__emit 0x51
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x0c
-		__emit 0xe8
-		__emit 0x7b
-		__emit 0xc8
-		__emit 0xbf
-		__emit 0xff
-		__emit 0x8b
-		__emit 0xb4
-		__emit 0x24
-		__emit 0x84
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x85
-		__emit 0xf6
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x7c
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x21
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xe8
-		__emit 0x7f
-		__emit 0x7d
-		__emit 0xbf
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x40
-		__emit 0x24
-		__emit 0x50
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0xe8
-		__emit 0xd6
-		__emit 0x21
-		__emit 0xc1
-		__emit 0xff
-		__emit 0x83
-		__emit 0xc6
-		__emit 0x38
-		__emit 0x56
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x08
-		__emit 0xe8
-		__emit 0xc9
-		__emit 0x93
-		__emit 0xbd
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x68
-		__emit 0xd6
-		__emit 0x2e
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x04
-		__emit 0x52
-		__emit 0xff
-		__emit 0x50
-		__emit 0x44
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x04
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x7c
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x55
-		__emit 0xe4
-		__emit 0xbf
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x74
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x7c
-		__emit 0xc2
-		__emit 0x08
-		__emit 0x00
+	AudioEventRTS sound(m_soundName, OBJECT_ID_UNUSED);
+	if (primary)
+	{
+		sound.setPlayerIndex(primary->getControllingPlayer()->getPlayerIndex());
+		sound.setPosition(primary->getPosition());
 	}
+
+	TheAudio->addAudioEvent(&sound);
 }
