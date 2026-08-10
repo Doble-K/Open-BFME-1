a77a4cf58e92ff0b05ac631c8fc284862b3f7bec
Open-BFME5 direct-02 <supportforcodemaster@gmail.com>
2026-08-10 15:33:23 +0000

    Open-BFME5: convert MissingTextureClass constructor to clean C++

--- Code/GameEngineDevice/Source/W3DDevice/GameClient/Shadow/MissingTextureClassCtorThunk.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/Shadow/MissingTextureClassCtorThunk.cpp
@@ -1,139 +1,18 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /D_MBCS /MD /EHsc /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad
 
-class MissingTextureClass
+#include "hash.h"
+#include "wwstring.h"
+
+class MissingTextureClass : public HashableClass
 {
 public:
-    MissingTextureClass(const char *);
+    MissingTextureClass(const char *name);
+    virtual ~MissingTextureClass(void);
+    virtual const char *Get_Key(void) { return Name; }
+
+private:
+    StringClass Name;
 };
 
 // ??0MissingTextureClass@@QAE@PBD@Z
-__declspec(naked) MissingTextureClass::MissingTextureClass(const char *)
-{
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x88;
-        __emit 0x27;
-        __emit 0x05;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x53;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x57;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x20;
-        __emit 0x85;
-        __emit 0xdb;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x60;
-        __emit 0x84;
-        __emit 0x12;
-        __emit 0x01;
-        __emit 0xa1;
-        __emit 0x24;
-        __emit 0x91;
-        __emit 0x2d;
-        __emit 0x01;
-        __emit 0x8d;
-        __emit 0x7e;
-        __emit 0x08;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x07;
-        __emit 0x74;
-        __emit 0x19;
-        __emit 0x53;
-        __emit 0xff;
-        __emit 0x15;
-        __emit 0x0c;
-        __emit 0x93;
-        __emit 0x35;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x04;
-        __emit 0x85;
-        __emit 0xc0;
-        __emit 0x7e;
-        __emit 0x0b;
-        __emit 0x6a;
-        __emit 0x00;
-        __emit 0x40;
-        __emit 0x50;
-        __emit 0x8b;
-        __emit 0xcf;
-        __emit 0xe8;
-        __emit 0xe1;
-        __emit 0xa7;
-        __emit 0x22;
-        __emit 0x00;
-        __emit 0x53;
-        __emit 0x8b;
-        __emit 0xcf;
-        __emit 0xe8;
-        __emit 0x2c;
-        __emit 0xf2;
-        __emit 0x86;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0x5f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x5b;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x04;
-        __emit 0x00;
-    }
-}
+MissingTextureClass::MissingTextureClass(const char *name) : Name(name) {}
