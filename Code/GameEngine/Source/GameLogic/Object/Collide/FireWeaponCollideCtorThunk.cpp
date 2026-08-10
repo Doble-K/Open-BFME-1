9dfc0dbccb9efc54b4d1c45d960e56819085e8d6
Open-BFME5 direct-01 <supportforcodemaster@gmail.com>
2026-08-10 14:52:56 +0000

    Open-BFME5: convert FireWeaponCollide constructor to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Collide/FireWeaponCollideCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Collide/FireWeaponCollideCtorThunk.cpp
@@ -1,168 +1,131 @@
 // cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// Open-BFME5: clean C++ reconstruction of the retail FireWeaponCollide ctor.
+//
+// The BFME module hierarchy predates the ZH reference headers used by the sweep
+// build.  In particular, BFME's Module has no Snapshot secondary base, so the
+// retail object puts the derived interface vptr at +0x10 and its fields at
+// +0x14/+0x18.  These TU-local declarations preserve that ABI without changing
+// the shared reference headers.
 
-class Thing;
 class ModuleData;
+class WeaponTemplate;
 
-class FireWeaponCollide
+class Thing
+{
+private:
+	unsigned char m_pad[0x74];
+
+public:
+	unsigned int m_weaponStatus;
+};
+
+class Module
 {
 public:
-    FireWeaponCollide(Thing *, const ModuleData *);
+	Module(const ModuleData *);
+	virtual ~Module();
+
+private:
+	const ModuleData *m_moduleData;
+};
+
+class ObjectModule : public Module
+{
+public:
+	ObjectModule(Thing *, const ModuleData *);
+
+protected:
+	Thing *getObject() const { return m_thing; }
+
+private:
+	Thing *m_thing;
+};
+
+class BehaviorModuleInterface
+{
+public:
+	virtual void behaviorModuleInterfaceAnchor() = 0;
+};
+
+class BehaviorModule : public ObjectModule, public BehaviorModuleInterface
+{
+public:
+	BehaviorModule(Thing *thing, const ModuleData *moduleData) : ObjectModule(thing, moduleData)
+	{
+	}
+};
+
+class CollideModuleInterface
+{
+public:
+	virtual void collideModuleInterfaceAnchor() = 0;
+};
+
+class CollideModule : public BehaviorModule, public CollideModuleInterface
+{
+public:
+	CollideModule(Thing *thing, const ModuleData *moduleData) : BehaviorModule(thing, moduleData)
+	{
+	}
+};
+
+class FireWeaponCollideModuleData
+{
+public:
+	virtual ~FireWeaponCollideModuleData();
+
+private:
+	unsigned char m_pad[4];
+
+public:
+	const WeaponTemplate *m_collideWeaponTemplate;
+};
+
+enum WeaponSlotType
+{
+	PRIMARY_WEAPON = 0
+};
+
+class Weapon
+{
+private:
+	unsigned char m_pad[8];
+
+public:
+	unsigned int m_status;
+};
+
+class WeaponStore
+{
+public:
+	Weapon *allocateNewWeapon(const WeaponTemplate *, WeaponSlotType) const;
+};
+
+extern WeaponStore *TheWeaponStore;
+
+class FireWeaponCollide : public CollideModule
+{
+public:
+	FireWeaponCollide(Thing *, const ModuleData *);
+
+private:
+	const FireWeaponCollideModuleData *getFireWeaponCollideModuleData() const
+	{
+		return reinterpret_cast<const FireWeaponCollideModuleData *>(
+			*reinterpret_cast<const ModuleData *const *>(reinterpret_cast<const char *>(this) + 4));
+	}
+
+	Weapon *m_collideWeapon;
+	bool m_everFired;
 };
 
 // ??0FireWeaponCollide@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) FireWeaponCollide::FireWeaponCollide(Thing *, const ModuleData *)
+FireWeaponCollide::FireWeaponCollide(Thing *thing, const ModuleData *moduleData) :
+	CollideModule(thing, moduleData),
+	m_collideWeapon(0)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x68;
-        __emit 0xca;
-        __emit 0x00;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0xe8;
-        __emit 0x86;
-        __emit 0x0c;
-        __emit 0xe0;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xe4;
-        __emit 0x1d;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xf4;
-        __emit 0x9b;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0x30;
-        __emit 0x9b;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x14;
-        __emit 0x9b;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0x50;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0x0d;
-        __emit 0x38;
-        __emit 0xf7;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x6a;
-        __emit 0x00;
-        __emit 0x52;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xe8;
-        __emit 0x1c;
-        __emit 0xbe;
-        __emit 0xdf;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4e;
-        __emit 0x08;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x8b;
-        __emit 0x51;
-        __emit 0x74;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x89;
-        __emit 0x50;
-        __emit 0x08;
-        __emit 0xc6;
-        __emit 0x46;
-        __emit 0x18;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+	m_collideWeapon = TheWeaponStore->allocateNewWeapon(
+		getFireWeaponCollideModuleData()->m_collideWeaponTemplate, PRIMARY_WEAPON);
+	m_collideWeapon->m_status = getObject()->m_weaponStatus;
+	m_everFired = false;
 }
