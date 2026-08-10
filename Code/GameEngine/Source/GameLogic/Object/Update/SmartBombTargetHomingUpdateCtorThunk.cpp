1bbb542cb303b798a3d22f13ad407085d90f0a4c
Open-BFME5 direct-04 <supportforcodemaster@gmail.com>
2026-08-10 15:18:48 +0000

    Open-BFME5: convert SmartBombTargetHomingUpdate ctor to clean C++

--- Code/GameEngine/Source/GameLogic/Object/Update/SmartBombTargetHomingUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/SmartBombTargetHomingUpdateCtorThunk.cpp
@@ -1,171 +1,76 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift the SmartBombTargetHomingUpdate constructor to clean C++.
 
 class Thing;
 class ModuleData;
+class Object;
 
-class SmartBombTargetHomingUpdate
+enum UpdateSleepTime
+{
+    UPDATE_SLEEP_NONE = 1
+};
+
+class PB_DeepBase
+{
+public:
+    PB_DeepBase(Thing *, const ModuleData *);
+    virtual ~PB_DeepBase();
+
+protected:
+    void *m_f04;
+    Object *m_object;
+};
+
+class PB_Iface1
+{
+public:
+    virtual void slot();
+};
+
+class PB_Iface2
+{
+public:
+    virtual void slot();
+};
+
+class UpdateModule : public PB_DeepBase, public PB_Iface1, public PB_Iface2
+{
+public:
+    UpdateModule(Thing *thing, const ModuleData *moduleData)
+        : PB_DeepBase(thing, moduleData), m_f14(0), m_f18(-1), m_f1c(-1) {}
+
+protected:
+    void setWakeFrame(Object *, UpdateSleepTime);
+    Object *getObject() const { return m_object; }
+
+private:
+    unsigned int m_f14;
+    int m_f18;
+    int m_f1c;
+};
+
+class SmartBombTargetHomingUpdateIface
+{
+public:
+    virtual void slot();
+};
+
+class SmartBombTargetHomingUpdate : public UpdateModule,
+    public SmartBombTargetHomingUpdateIface
 {
 public:
     SmartBombTargetHomingUpdate(Thing *, const ModuleData *);
+
+private:
+    unsigned int m_targetX;
+    unsigned int m_targetY;
+    unsigned int m_targetZ;
 };
 
 // ??0SmartBombTargetHomingUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) SmartBombTargetHomingUpdate::SmartBombTargetHomingUpdate(Thing *, const ModuleData *)
+SmartBombTargetHomingUpdate::SmartBombTargetHomingUpdate(
+    Thing *thing, const ModuleData *moduleData)
+    : UpdateModule(thing, moduleData), m_targetX(0), m_targetY(0), m_targetZ(0)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0xd8;
-        __emit 0x0e;
-        __emit 0x01;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0xe8;
-        __emit 0x36;
-        __emit 0x01;
-        __emit 0xd9;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc9;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x1c;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x50;
-        __emit 0xc6;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0x56;
-        __emit 0x08;
-        __emit 0x6a;
-        __emit 0x01;
-        __emit 0x52;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x6c;
-        __emit 0xc8;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xa8;
-        __emit 0xc7;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x98;
-        __emit 0xc7;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x84;
-        __emit 0xc6;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x28;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x2c;
-        __emit 0xe8;
-        __emit 0xd4;
-        __emit 0xe7;
-        __emit 0xd8;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+    setWakeFrame(getObject(), UPDATE_SLEEP_NONE);
 }
