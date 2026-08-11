1756a077948ba13e2579c27108d64fecd4febfe5
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 17:11:37 -0400

    Open-BFME5 convert isValidMap to C++

--- Code/GameEngine/Source/GameClient/MapUtil_isValidMap_Thunk.cpp
+++ Code/GameEngine/Source/GameClient/MapUtil_isValidMap_Thunk.cpp
@@ -1,182 +1,50 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: clean C++ reconstruction of map-cache validation.
 
-class AsciiString {};
-bool isValidMap(AsciiString, bool);
-
-// ?isValidMap@@YA_NVAsciiString@@_N@Z
-__declspec(naked) bool isValidMap(AsciiString, bool)
+struct AsciiStringData
 {
-	__asm {
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x18
-		__emit 0x43
-		__emit 0x02
-		__emit 0x01
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x56
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x94
-		__emit 0x15
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x85
-		__emit 0xc9
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x5f
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x74
-		__emit 0x57
-		__emit 0x66
-		__emit 0x83
-		__emit 0x78
-		__emit 0x04
-		__emit 0x00
-		__emit 0x74
-		__emit 0x50
-		__emit 0xe8
-		__emit 0x00
-		__emit 0x10
-		__emit 0xbc
-		__emit 0xff
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0xe8
-		__emit 0x8b
-		__emit 0xfe
-		__emit 0x42
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x35
-		__emit 0x94
-		__emit 0x15
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x50
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xe8
-		__emit 0xb0
-		__emit 0xe4
-		__emit 0xbc
-		__emit 0xff
-		__emit 0x3b
-		__emit 0x06
-		__emit 0x74
-		__emit 0x2c
-		__emit 0x8a
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x18
-		__emit 0x3a
-		__emit 0x48
-		__emit 0x38
-		__emit 0x75
-		__emit 0x23
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0xfb
-		__emit 0xf9
-		__emit 0x42
-		__emit 0x00
-		__emit 0xb0
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x04
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5e
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x0c
-		__emit 0xc3
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0xd8
-		__emit 0xf9
-		__emit 0x42
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x04
-		__emit 0x32
-		__emit 0xc0
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5e
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x0c
-		__emit 0xc3
-	}
+	unsigned short m_refCount;
+	unsigned short m_allocated;
+	unsigned short m_length;
+};
+
+class AsciiString
+{
+public:
+	~AsciiString();
+	bool isEmpty() const { return m_data == 0 || m_data->m_length == 0; }
+	void toLower();
+private:
+	AsciiStringData *m_data;
+};
+
+struct MapCacheNode
+{
+	unsigned char m_pad[0x38];
+	bool m_isMultiplayer;
+};
+
+class MapCache
+{
+public:
+	void updateCache();
+	MapCacheNode *find(const AsciiString &name);
+	MapCacheNode *m_end;
+};
+
+extern MapCache *TheMapCache;
+
+bool isValidMap(AsciiString mapName, bool isMultiplayer)
+{
+	if (!TheMapCache || mapName.isEmpty())
+		return false;
+
+	TheMapCache->updateCache();
+	mapName.toLower();
+	MapCache *cache = TheMapCache;
+	MapCacheNode *it = cache->find(mapName);
+	if (it != cache->m_end && isMultiplayer == it->m_isMultiplayer)
+		return true;
+
+	return false;
 }
