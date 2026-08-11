ff4aadc91c0bcbbc69a2ed9286cc3e59d388e9d9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 11:52:34 -0400

    Open-BFME5: convert weapon template lookup to C++

--- Code/GameEngine/Source/Common/WeaponStore_findWeaponTemplate_Thunk.cpp
+++ Code/GameEngine/Source/Common/WeaponStore_findWeaponTemplate_Thunk.cpp
@@ -1,202 +1,45 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+#include <string.h>
 
 class AsciiString
 {
+public:
+	~AsciiString();
+
+	const char *str() const
+	{
+		return m_data ? reinterpret_cast<const char *>(m_data) + 8 : "";
+	}
+
+private:
+	void *m_data;
 };
+
 class WeaponTemplate;
+enum NameKeyType { INVALID_NAME_KEY = 0 };
+
+class NameKeyGenerator
+{
+public:
+	NameKeyType nameToKey(const char *);
+};
+
+extern NameKeyGenerator *TheNameKeyGenerator;
+
 class WeaponStore
 {
 public:
 	const WeaponTemplate *findWeaponTemplate(AsciiString) const;
+
+protected:
+	WeaponTemplate *findWeaponTemplatePrivate(NameKeyType) const;
 };
 
 // ?findWeaponTemplate@WeaponStore@@QBEPBVWeaponTemplate@@VAsciiString@@@Z
-__declspec(naked) const WeaponTemplate *WeaponStore::findWeaponTemplate(AsciiString) const
+const WeaponTemplate *WeaponStore::findWeaponTemplate(AsciiString name) const
 {
-	__asm {
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x88
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x01
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x85
-		__emit 0xc0
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x05
-		__emit 0x83
-		__emit 0xc0
-		__emit 0x08
-		__emit 0xeb
-		__emit 0x05
-		__emit 0xb8
-		__emit 0x8b
-		__emit 0x38
-		__emit 0x07
-		__emit 0x01
-		__emit 0x68
-		__emit 0xec
-		__emit 0x2e
-		__emit 0x08
-		__emit 0x01
-		__emit 0x50
-		__emit 0xff
-		__emit 0x15
-		__emit 0x3c
-		__emit 0x93
-		__emit 0x35
-		__emit 0x01
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x08
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x75
-		__emit 0x25
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x9a
-		__emit 0x29
-		__emit 0x6a
-		__emit 0x00
-		__emit 0x33
-		__emit 0xc0
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x04
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5e
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x0c
-		__emit 0xc2
-		__emit 0x04
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x74
-		__emit 0x05
-		__emit 0x83
-		__emit 0xc0
-		__emit 0x08
-		__emit 0xeb
-		__emit 0x05
-		__emit 0xb8
-		__emit 0x8b
-		__emit 0x38
-		__emit 0x07
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x00
-		__emit 0xd6
-		__emit 0x2e
-		__emit 0x01
-		__emit 0x50
-		__emit 0xe8
-		__emit 0xff
-		__emit 0x5d
-		__emit 0xe5
-		__emit 0xff
-		__emit 0x50
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xe8
-		__emit 0xa6
-		__emit 0x84
-		__emit 0xe2
-		__emit 0xff
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x8b
-		__emit 0xf0
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x4d
-		__emit 0x29
-		__emit 0x6a
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x04
-		__emit 0x8b
-		__emit 0xc6
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5e
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x0c
-		__emit 0xc2
-		__emit 0x04
-		__emit 0x00
+	if (_stricmp(name.str(), "None") == 0) {
+		return 0;
 	}
+	return findWeaponTemplatePrivate(TheNameKeyGenerator->nameToKey(name.str()));
 }
