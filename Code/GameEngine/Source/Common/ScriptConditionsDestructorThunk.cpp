c0ea9dc5c994cab42cb86765ba5742b086917064
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 16:26:32 -0400

    Open-BFME5 convert ScriptConditions destructor to C++

--- Code/GameEngine/Source/Common/ScriptConditionsDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/ScriptConditionsDestructorThunk.cpp
@@ -1,121 +1,34 @@
 // cl: /DNDEBUG /MD /EHsc
 // Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-class __declspec(novtable) ScriptConditions
+class SubsystemInterface
 {
 public:
-    virtual ~ScriptConditions();
+	virtual ~SubsystemInterface();
 };
 
-// ??1ScriptConditions@@UAE@XZ
-__declspec(naked) ScriptConditions::~ScriptConditions()
+class ScriptConditionsInterface : public SubsystemInterface
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xa8
-        __emit 0x6e
-        __emit 0x01
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x44
-        __emit 0x1f
-        __emit 0x0e
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0xac
-        __emit 0x06
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x85
-        __emit 0xc9
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x06
-        __emit 0x8b
-        __emit 0x01
-        __emit 0x6a
-        __emit 0x01
-        __emit 0xff
-        __emit 0x10
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x05
-        __emit 0xac
-        __emit 0x06
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xfc
-        __emit 0x1e
-        __emit 0x0e
-        __emit 0x01
-        __emit 0xe8
-        __emit 0x36
-        __emit 0xd5
-        __emit 0x67
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
+public:
+	virtual ~ScriptConditionsInterface() {}
+};
+
+class TransportStatus
+{
+public:
+	virtual ~TransportStatus();
+};
+
+static TransportStatus *s_transportStatuses;
+
+class ScriptConditions : public ScriptConditionsInterface
+{
+public:
+	virtual ~ScriptConditions();
+};
+
+ScriptConditions::~ScriptConditions()
+{
+	delete s_transportStatuses;
+	s_transportStatuses = 0;
 }
