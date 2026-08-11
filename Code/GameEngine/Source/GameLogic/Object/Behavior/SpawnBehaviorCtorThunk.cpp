733abb28ff30e0bc95a7908d961d8e9ab3257626
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 13:43:57 -0400

    Open-BFME5: convert spawn behavior constructor to C++

--- Code/GameEngine/Source/GameLogic/Object/Behavior/SpawnBehaviorCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/SpawnBehaviorCtorThunk.cpp
@@ -1,393 +1,143 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump SpawnBehavior ctor to C++ thunk.
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+#include <list>
 
+typedef unsigned int UnsignedInt;
 class Thing;
 class ModuleData;
-class SpawnBehavior
+class ThingTemplate;
+class AsciiString;
+
+class ThingFactory
 {
 public:
-SpawnBehavior(Thing *, const ModuleData *);
+	ThingTemplate *findTemplate(const AsciiString &name);
+};
+extern ThingFactory *TheThingFactory;
+
+class ObjectModule
+{
+public:
+	ObjectModule(Thing *thing, const ModuleData *moduleData);
+	virtual ~ObjectModule();
+protected:
+	const ModuleData *m_moduleData;
+	Thing *m_object;
 };
 
-// ??0SpawnBehavior@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) SpawnBehavior::SpawnBehavior(Thing *, const ModuleData *)
+class BehaviorModuleInterface
 {
-__asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x1e
-        __emit 0xc2
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x18
-        __emit 0x53
-        __emit 0x55
-        __emit 0x56
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x24
-        __emit 0x50
-        __emit 0x51
-        __emit 0x8b
-        __emit 0xce
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x18
-        __emit 0xe8
-        __emit 0x83
-        __emit 0xc2
-        __emit 0xe0
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0xd0
-        __emit 0xc9
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0xa0
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0x83
-        __emit 0xc8
-        __emit 0xff
-        __emit 0x33
-        __emit 0xdb
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x94
-        __emit 0xcd
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0xd0
-        __emit 0xcc
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0xac
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0x89
-        __emit 0x5e
-        __emit 0x14
-        __emit 0x89
-        __emit 0x46
-        __emit 0x18
-        __emit 0x89
-        __emit 0x46
-        __emit 0x1c
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x20
-        __emit 0x40
-        __emit 0x6b
-        __emit 0x0a
-        __emit 0x01
-        __emit 0x8d
-        __emit 0x7e
-        __emit 0x2c
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x24
-        __emit 0x7c
-        __emit 0x25
-        __emit 0x0a
-        __emit 0x01
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0x89
-        __emit 0x5c
-        __emit 0x24
-        __emit 0x1c
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x28
-        __emit 0xfc
-        __emit 0x1b
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xe8
-        __emit 0x9a
-        __emit 0x23
-        __emit 0xe3
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xcc
-        __emit 0x6c
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x08
-        __emit 0x6c
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0xfc
-        __emit 0x6b
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x20
-        __emit 0xd0
-        __emit 0x6b
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x24
-        __emit 0xcc
-        __emit 0x6b
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x28
-        __emit 0xbc
-        __emit 0x6b
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x07
-        __emit 0x70
-        __emit 0x6b
-        __emit 0x0a
-        __emit 0x01
-        __emit 0x6a
-        __emit 0x0c
-        __emit 0x89
-        __emit 0x5e
-        __emit 0x44
-        __emit 0xe8
-        __emit 0x53
-        __emit 0x36
-        __emit 0x62
-        __emit 0x00
-        __emit 0x89
-        __emit 0x00
-        __emit 0x89
-        __emit 0x40
-        __emit 0x04
-        __emit 0x89
-        __emit 0x46
-        __emit 0x44
-        __emit 0x6a
-        __emit 0x0c
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x24
-        __emit 0x01
-        __emit 0x89
-        __emit 0x5e
-        __emit 0x48
-        __emit 0xe8
-        __emit 0x3c
-        __emit 0x36
-        __emit 0x62
-        __emit 0x00
-        __emit 0x89
-        __emit 0x00
-        __emit 0x89
-        __emit 0x40
-        __emit 0x04
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x08
-        __emit 0x89
-        __emit 0x46
-        __emit 0x48
-        __emit 0x8b
-        __emit 0x6e
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x45
-        __emit 0x20
-        __emit 0x89
-        __emit 0x46
-        __emit 0x5c
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0xd8
-        __emit 0xf1
-        __emit 0x2e
-        __emit 0x01
-        __emit 0x50
-        __emit 0xc6
-        __emit 0x44
-        __emit 0x24
-        __emit 0x20
-        __emit 0x02
-        __emit 0xe8
-        __emit 0x37
-        __emit 0xd6
-        __emit 0xe1
-        __emit 0xff
-        __emit 0x89
-        __emit 0x46
-        __emit 0x34
-        __emit 0x89
-        __emit 0x5e
-        __emit 0x3c
-        __emit 0x89
-        __emit 0x5e
-        __emit 0x40
-        __emit 0x38
-        __emit 0x5d
-        __emit 0x14
-        __emit 0x74
-        __emit 0x08
-        __emit 0x8b
-        __emit 0x55
-        __emit 0x08
-        __emit 0x89
-        __emit 0x56
-        __emit 0x38
-        __emit 0xeb
-        __emit 0x07
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x38
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xc6
-        __emit 0x46
-        __emit 0x4c
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x44
-        __emit 0x8b
-        __emit 0x38
-        __emit 0x3b
-        __emit 0xf8
-        __emit 0x74
-        __emit 0x16
-        __emit 0x8b
-        __emit 0xc7
-        __emit 0x8b
-        __emit 0x3f
-        __emit 0x6a
-        __emit 0x0c
-        __emit 0x50
-        __emit 0xe8
-        __emit 0x91
-        __emit 0x36
-        __emit 0x62
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x44
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x08
-        __emit 0x3b
-        __emit 0xf8
-        __emit 0x75
-        __emit 0xea
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x44
-        __emit 0x89
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x44
-        __emit 0x89
-        __emit 0x40
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x45
-        __emit 0x10
-        __emit 0x89
-        __emit 0x46
-        __emit 0x58
-        __emit 0x88
-        __emit 0x5e
-        __emit 0x4e
-        __emit 0x8a
-        __emit 0x4d
-        __emit 0x16
-        __emit 0x5f
-        __emit 0x88
-        __emit 0x4e
-        __emit 0x4d
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x10
-        __emit 0x89
-        __emit 0x5e
-        __emit 0x54
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x50
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xc6
-        __emit 0x46
-        __emit 0x4c
-        __emit 0x01
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0x5d
-        __emit 0x5b
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc2
-        __emit 0x08
-        __emit 0x00
-}
+public:
+	virtual void getBehaviorModuleInterface() = 0;
+};
+
+class UpdateModuleInterface
+{
+public:
+	virtual void updateModuleInterface() = 0;
+};
+
+class BehaviorModule : public ObjectModule, public BehaviorModuleInterface
+{
+public:
+	BehaviorModule(Thing *thing, const ModuleData *moduleData)
+		: ObjectModule(thing, moduleData)
+	{
+	}
+	virtual ~BehaviorModule() {}
+};
+
+class UpdateModule : public BehaviorModule, public UpdateModuleInterface
+{
+public:
+	UpdateModule(Thing *thing, const ModuleData *moduleData)
+		: BehaviorModule(thing, moduleData),
+		  m_nextCallFrameAndPhase(0),
+		  m_indexInLogic(-1),
+		  m_updateState(-1)
+	{
+	}
+	virtual ~UpdateModule() {}
+protected:
+	UnsignedInt m_nextCallFrameAndPhase;
+	int m_indexInLogic;
+	UnsignedInt m_updateState;
+};
+
+template <int Number>
+class SpawnBehaviorSecondaryBase
+{
+public:
+	virtual void slot();
+};
+
+class SpawnBehaviorFourthBase
+{
+public:
+	SpawnBehaviorFourthBase();
+	virtual void slot();
+};
+
+struct SpawnBehaviorModuleDataView
+{
+	unsigned char pad00[8];
+	UnsignedInt m_spawnNumberData;
+	unsigned char pad0c[4];
+	UnsignedInt m_initialBurst;
+	unsigned char m_isOneShotData;
+	unsigned char pad15;
+	unsigned char m_aggregateHealth;
+	unsigned char pad17[9];
+	AsciiString *m_spawnTemplateBegin;
+};
+
+class SpawnBehavior
+	: public UpdateModule,
+	  public SpawnBehaviorSecondaryBase<1>,
+	  public SpawnBehaviorSecondaryBase<2>,
+	  public SpawnBehaviorSecondaryBase<3>,
+	  public SpawnBehaviorFourthBase
+{
+public:
+	SpawnBehavior(Thing *thing, const ModuleData *moduleData);
+	virtual ~SpawnBehavior() {}
+private:
+	void *m_unknown30;
+	ThingTemplate *m_spawnTemplate;
+	int m_oneShotCountdown;
+	UnsignedInt m_framesToWait;
+	UnsignedInt m_firstBatchCount;
+	_STL::list<int> m_replacementTimes;
+	_STL::list<int> m_otherList;
+	bool m_active;
+	unsigned char m_aggregateHealth;
+	bool m_initialBurstTimesInited;
+	unsigned char m_pad4f;
+	UnsignedInt m_spawnCount;
+	UnsignedInt m_selfTaskingSpawnCount;
+	UnsignedInt m_initialBurstCountdown;
+	AsciiString *m_templateNameIterator;
+};
+
+SpawnBehavior::SpawnBehavior(Thing *thing, const ModuleData *moduleData)
+	: UpdateModule(thing, moduleData)
+{
+	const SpawnBehaviorModuleDataView *md =
+		reinterpret_cast<const SpawnBehaviorModuleDataView *>(m_moduleData);
+	m_templateNameIterator = md->m_spawnTemplateBegin;
+	m_spawnTemplate = TheThingFactory->findTemplate(*m_templateNameIterator);
+	m_framesToWait = 0;
+	m_firstBatchCount = 0;
+	if (md->m_isOneShotData)
+		m_oneShotCountdown = md->m_spawnNumberData;
+	else
+		m_oneShotCountdown = -1;
+	m_active = true;
+	m_replacementTimes.clear();
+	m_initialBurstCountdown = md->m_initialBurst;
+	m_initialBurstTimesInited = false;
+	m_aggregateHealth = md->m_aggregateHealth;
+	m_spawnCount = 0xffffffff;
+	m_active = true;
+	m_selfTaskingSpawnCount = 0;
 }
