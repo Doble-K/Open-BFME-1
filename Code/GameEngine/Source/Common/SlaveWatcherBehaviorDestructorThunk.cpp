6bedbc4e430ae01b6d6d999c78a298a7a901d571
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 16:39:58 -0400

    Open-BFME5 convert SlaveWatcherBehavior destructor to C++

--- Code/GameEngine/Source/Common/SlaveWatcherBehaviorDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/SlaveWatcherBehaviorDestructorThunk.cpp
@@ -1,157 +1,85 @@
 // cl: /DNDEBUG /MD /EHsc
 // Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-class __declspec(novtable) SlaveWatcherBehavior
+class Thing;
+class ModuleData;
+
+enum DamageType
 {
-public:
-    virtual ~SlaveWatcherBehavior();
+	DAMAGE_NORMAL = 0
 };
 
-// ??1SlaveWatcherBehavior@@UAE@XZ
-__declspec(naked) SlaveWatcherBehavior::~SlaveWatcherBehavior()
+enum DeathType
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x78
-        __emit 0xbf
-        __emit 0x00
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xdc
-        __emit 0x64
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x18
-        __emit 0x64
-        __emit 0x0a
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0x08
-        __emit 0x64
-        __emit 0x0a
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x46
-        __emit 0x20
-        __emit 0x85
-        __emit 0xc0
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x1b
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x98
-        __emit 0x08
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x50
-        __emit 0xe8
-        __emit 0x87
-        __emit 0x7c
-        __emit 0xe1
-        __emit 0xff
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x6a
-        __emit 0x0d
-        __emit 0x6a
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xc8
-        __emit 0xe8
-        __emit 0x2b
-        __emit 0xcf
-        __emit 0xe0
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0xac
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x5c
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x98
-        __emit 0xca
-        __emit 0x09
-        __emit 0x01
-        __emit 0xe8
-        __emit 0x55
-        __emit 0x06
-        __emit 0xe4
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
+	DEATH_SLAVE_WATCHER_RELEASE = 13
+};
+
+class Object
+{
+public:
+	void kill(DamageType, DeathType);
+};
+
+class GameLogic
+{
+public:
+	Object *findObjectByID(int);
+};
+
+extern GameLogic *TheGameLogic;
+
+class PB_DeepBase
+{
+public:
+	virtual ~PB_DeepBase();
+
+protected:
+	void *m_p4;
+	Object *m_object;
+};
+
+class SlaveWatcherBehaviorIface1
+{
+public:
+	virtual void slot() = 0;
+};
+
+class SlaveWatcherBehaviorIface2
+{
+public:
+	virtual void slot() = 0;
+};
+
+class UpdateModule : public PB_DeepBase, public SlaveWatcherBehaviorIface1
+{
+public:
+	virtual ~UpdateModule() {}
+};
+
+class SlaveWatcherUpdateModule : public UpdateModule, public SlaveWatcherBehaviorIface2
+{
+public:
+	virtual ~SlaveWatcherUpdateModule() {}
+};
+
+class SlaveWatcherBehavior : public SlaveWatcherUpdateModule
+{
+public:
+	virtual ~SlaveWatcherBehavior();
+
+private:
+	unsigned int m_f14;
+	int m_f18;
+	int m_f1c;
+	int m_slaveID;
+};
+
+SlaveWatcherBehavior::~SlaveWatcherBehavior()
+{
+	if (m_slaveID)
+	{
+		Object *slave = TheGameLogic->findObjectByID(m_slaveID);
+		if (slave)
+			slave->kill(DAMAGE_NORMAL, DEATH_SLAVE_WATCHER_RELEASE);
+	}
 }
