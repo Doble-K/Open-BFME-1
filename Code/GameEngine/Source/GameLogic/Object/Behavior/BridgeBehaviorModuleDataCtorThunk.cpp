7ffbc52ae22c6c5ecfa3f04162f6b63cb9c80910
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 15:40:37 -0400

    Open-BFME5 convert BridgeBehavior data constructor

--- Code/GameEngine/Source/GameLogic/Object/Behavior/BridgeBehaviorModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/BridgeBehaviorModuleDataCtorThunk.cpp
@@ -1,18 +1,65 @@
-// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+// Open-BFME5: clean C++ lift of the retail BridgeBehavior module-data constructor.
 
-class BridgeBehaviorModuleData
+#include <list>
+
+typedef unsigned int UnsignedInt;
+
+class AsciiString
 {
 public:
-    BridgeBehaviorModuleData();
+	~AsciiString();
+
+private:
+	char *m_data;
 };
 
-__declspec(naked) BridgeBehaviorModuleData::BridgeBehaviorModuleData()
+struct TimeAndLocationInfo
 {
-    __asm {
-        _emit 0E9h
-        _emit 0A8h
-        _emit 05Bh
-        _emit 01Dh
-        _emit 000h
-    }
+	UnsignedInt delay;
+	AsciiString boneName;
+};
+
+class FXList;
+class ObjectCreationList;
+
+struct BridgeFXInfo
+{
+	const FXList *fx;
+	TimeAndLocationInfo timeAndLocationInfo;
+};
+
+struct BridgeOCLInfo
+{
+	const ObjectCreationList *ocl;
+	TimeAndLocationInfo timeAndLocationInfo;
+};
+
+class BehaviorModuleData
+{
+public:
+	virtual ~BehaviorModuleData() {}
+
+private:
+	int m_data;
+};
+
+class BridgeBehaviorModuleData : public BehaviorModuleData
+{
+public:
+	BridgeBehaviorModuleData();
+	virtual ~BridgeBehaviorModuleData();
+
+private:
+	float m_lateralScaffoldSpeed;
+	float m_verticalScaffoldSpeed;
+	std::list<BridgeFXInfo> m_fx;
+	std::list<BridgeOCLInfo> m_ocl;
+};
+
+BridgeBehaviorModuleData::BridgeBehaviorModuleData()
+{
+	m_lateralScaffoldSpeed = 1.0f;
+	m_verticalScaffoldSpeed = 1.0f;
 }
