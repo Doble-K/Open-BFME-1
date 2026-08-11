c9e7d6b80709c5bbe8d63f5c234b7d1b671a4fb5
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 15:12:07 -0400

    Open-BFME5 convert MaterialCollector constructor to exact C++

--- Code/Libraries/Source/WWVegas/WW3D2/MaterialCollectorClass_ctor_Thunk.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/MaterialCollectorClass_ctor_Thunk.cpp
@@ -1,177 +1,106 @@
 // cl: /DNDEBUG /MD /EHsc
 // Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-class MaterialCollectorClass {
+class ShaderVectorBaseCtorShim
+{
+public:
+	void construct(int, int);
+};
+
+class TextureVectorBaseCtorShim
+{
+public:
+	void construct(int, int);
+};
+
+class ShaderVector
+{
+public:
+	__forceinline ShaderVector()
+	{
+		((ShaderVectorBaseCtorShim *)this)->construct(0, 0);
+		*(unsigned int *)this = 0x0113c644;
+		*(unsigned int *)((unsigned char *)this + 0x14) = 10;
+		*(unsigned int *)((unsigned char *)this + 0x10) = 0;
+	}
+	~ShaderVector();
+
+private:
+	unsigned char m_data[0x18];
+};
+
+class MaterialVector
+{
+public:
+	__forceinline MaterialVector()
+	{
+		unsigned char *self = (unsigned char *)this;
+		*(unsigned int *)(self + 4) = 0;
+		*(unsigned int *)(self + 8) = 0;
+		*(unsigned char *)(self + 0x0c) = 1;
+		*(unsigned char *)(self + 0x0d) = 0;
+		*(unsigned int *)self = 0x0113c614;
+		*(unsigned int *)(self + 0x14) = 10;
+		*(unsigned int *)(self + 0x10) = 0;
+	}
+	~MaterialVector();
+
+private:
+	unsigned char m_data[0x18];
+};
+
+class TextureVector
+{
+public:
+	__forceinline TextureVector()
+	{
+		((TextureVectorBaseCtorShim *)this)->construct(0, 0);
+		*(unsigned int *)((unsigned char *)this + 0x10) = 0;
+		*(unsigned int *)this = 0x0113c62c;
+		*(unsigned int *)((unsigned char *)this + 0x14) = 10;
+	}
+	~TextureVector();
+
+private:
+	unsigned char m_data[0x18];
+};
+
+class ShaderValue
+{
+public:
+	__forceinline ShaderValue()
+	{
+		*(volatile unsigned int *)&m_value = 0x0010441b;
+	}
+
+	__forceinline ShaderValue &operator=(unsigned int value)
+	{
+		m_value = value;
+		return *this;
+	}
+
+private:
+	unsigned int m_value;
+};
+
+class MaterialCollectorClass
+{
 public:
 	MaterialCollectorClass();
+
+private:
+	ShaderVector m_shaders;
+	MaterialVector m_materials;
+	TextureVector m_textures;
+	ShaderValue LastShader;
+	void *LastMaterial;
+	void *LastTexture;
 };
 
 // ??0MaterialCollectorClass@@QAE@XZ
-__declspec(naked) MaterialCollectorClass::MaterialCollectorClass()
+MaterialCollectorClass::MaterialCollectorClass()
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0xf3
-		__emit 0xce
-		__emit 0x05
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x53
-		__emit 0x56
-		__emit 0x57
-		__emit 0x33
-		__emit 0xdb
-		__emit 0x53
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x53
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x14
-		__emit 0xe8
-		__emit 0x38
-		__emit 0xf5
-		__emit 0xff
-		__emit 0xff
-		__emit 0xc7
-		__emit 0x06
-		__emit 0x44
-		__emit 0xc6
-		__emit 0x13
-		__emit 0x01
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x14
-		__emit 0x0a
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x89
-		__emit 0x5e
-		__emit 0x10
-		__emit 0xb0
-		__emit 0x01
-		__emit 0x89
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x18
-		__emit 0x89
-		__emit 0x5e
-		__emit 0x1c
-		__emit 0x89
-		__emit 0x5e
-		__emit 0x20
-		__emit 0x88
-		__emit 0x46
-		__emit 0x24
-		__emit 0x88
-		__emit 0x5e
-		__emit 0x25
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x18
-		__emit 0x14
-		__emit 0xc6
-		__emit 0x13
-		__emit 0x01
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x2c
-		__emit 0x0a
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x89
-		__emit 0x5e
-		__emit 0x28
-		__emit 0x53
-		__emit 0x8d
-		__emit 0x7e
-		__emit 0x30
-		__emit 0x53
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0x88
-		__emit 0x44
-		__emit 0x24
-		__emit 0x20
-		__emit 0xe8
-		__emit 0xc5
-		__emit 0xea
-		__emit 0xff
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x89
-		__emit 0x5f
-		__emit 0x10
-		__emit 0xc7
-		__emit 0x07
-		__emit 0x2c
-		__emit 0xc6
-		__emit 0x13
-		__emit 0x01
-		__emit 0xc7
-		__emit 0x47
-		__emit 0x14
-		__emit 0x0a
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x48
-		__emit 0x1b
-		__emit 0x44
-		__emit 0x10
-		__emit 0x00
-		__emit 0x89
-		__emit 0x5e
-		__emit 0x50
-		__emit 0x89
-		__emit 0x5e
-		__emit 0x4c
-		__emit 0x5f
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x48
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0x8b
-		__emit 0xc6
-		__emit 0x5e
-		__emit 0x5b
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
-	}
+	LastTexture = 0;
+	LastMaterial = 0;
+	LastShader = 0xffffffff;
 }
