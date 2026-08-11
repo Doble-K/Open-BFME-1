2627f004d3fc88399dbe2c0f548aad09044e95bb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 16:42:49 -0400

    Open-BFME5 convert W3DProjectileStreamDraw destructor to C++

--- Code/GameEngine/Source/Common/W3DProjectileStreamDrawDestructorThunk.cpp
+++ Code/GameEngine/Source/Common/W3DProjectileStreamDrawDestructorThunk.cpp
@@ -1,131 +1,56 @@
 // cl: /DNDEBUG /MD /EHsc
 // Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-class __declspec(novtable) W3DProjectileStreamDraw
+class PB_DeepBase
 {
 public:
-    virtual ~W3DProjectileStreamDraw();
+	virtual ~PB_DeepBase();
+
+private:
+	void *m_p4;
+	void *m_p8;
 };
 
-// ??1W3DProjectileStreamDraw@@UAE@XZ
-__declspec(naked) W3DProjectileStreamDraw::~W3DProjectileStreamDraw()
+class DrawModuleInterface1
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x78
-        __emit 0xa3
-        __emit 0x01
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0xdc
-        __emit 0x92
-        __emit 0x0e
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x18
-        __emit 0x92
-        __emit 0x0e
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0x08
-        __emit 0x92
-        __emit 0x0e
-        __emit 0x01
-        __emit 0x8d
-        __emit 0x4e
-        __emit 0x20
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x7c
-        __emit 0xe1
-        __emit 0xcb
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x10
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0xac
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x5c
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x98
-        __emit 0xca
-        __emit 0x09
-        __emit 0x01
-        __emit 0xe8
-        __emit 0xff
-        __emit 0xde
-        __emit 0xcd
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
-    }
+public:
+	virtual void slot() = 0;
+};
+
+class DrawModuleInterface2
+{
+public:
+	virtual void slot() = 0;
+};
+
+class DrawModule : public PB_DeepBase, public DrawModuleInterface1
+{
+public:
+	virtual ~DrawModule() {}
+};
+
+class ProjectileStreamDrawBase : public DrawModule, public DrawModuleInterface2
+{
+public:
+	virtual ~ProjectileStreamDrawBase() {}
+};
+
+class W3DProjectileStreamResources
+{
+public:
+	~W3DProjectileStreamResources();
+};
+
+class W3DProjectileStreamDraw : public ProjectileStreamDrawBase
+{
+public:
+	virtual ~W3DProjectileStreamDraw();
+
+private:
+	unsigned char m_pad14[0x0c];
+	W3DProjectileStreamResources m_resources;
+};
+
+W3DProjectileStreamDraw::~W3DProjectileStreamDraw()
+{
 }
