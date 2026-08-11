0e9fd63d5e3b5bb707cea17b74284948f6cb1ef8
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 13:03:48 -0400

    Open-BFME5: convert combo list hide helper to C++

--- Code/GameEngine/Source/Common/HideListBox_Thunk.cpp
+++ Code/GameEngine/Source/Common/HideListBox_Thunk.cpp
@@ -1,175 +1,77 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
 
-class GameWindow;
+class GameWindow
+{
+public:
+	void *winGetUserData();
+	bool winIsHidden();
+	int winHide(int hide);
+	int winGetSize(int *width, int *height);
+	int winSetSize(int width, int height);
+};
 
-void HideListBox(GameWindow *);
+struct ComboBoxData
+{
+	unsigned char m_pad[0x28];
+	GameWindow *editBox;
+	GameWindow *listBox;
+};
+
+class GameWindowManager
+{
+public:
+	virtual void slot00(); virtual void slot01(); virtual void slot02(); virtual void slot03();
+	virtual void slot04(); virtual void slot05(); virtual void slot06(); virtual void slot07();
+	virtual void slot08(); virtual void slot09(); virtual void slot10(); virtual void slot11();
+	virtual void slot12(); virtual void slot13(); virtual void slot14(); virtual void slot15();
+	virtual void slot16(); virtual void slot17(); virtual void slot18(); virtual void slot19();
+	virtual void slot20(); virtual void slot21(); virtual void slot22(); virtual void slot23();
+	virtual void slot24(); virtual void slot25(); virtual void slot26(); virtual void slot27();
+	virtual void slot28(); virtual void slot29(); virtual void slot30(); virtual void slot31();
+	virtual void slot32(); virtual void slot33(); virtual void slot34(); virtual void slot35();
+	virtual void slot36(); virtual void slot37(); virtual void slot38(); virtual void slot39();
+	virtual void slot40(); virtual void slot41(); virtual void slot42(); virtual void slot43();
+	virtual void slot44(); virtual void slot45(); virtual void slot46();
+	virtual void winSetFocus(GameWindow *window);
+	virtual GameWindow *winGetFocus();
+};
+
+extern GameWindowManager *TheWindowManager;
+
+struct ICoord2D
+{
+	int x, y;
+};
+
+static __forceinline GameWindow *getListBox(GameWindow *window)
+{
+	ComboBoxData *data = (ComboBoxData *)window->winGetUserData();
+	return data ? data->listBox : 0;
+}
+
+static __forceinline GameWindow *getEditBox(GameWindow *window)
+{
+	ComboBoxData *data = (ComboBoxData *)window->winGetUserData();
+	return data && data->editBox ? data->editBox : 0;
+}
 
 // ?HideListBox@@YAXPAVGameWindow@@@Z
-__declspec(naked) void HideListBox(GameWindow *)
+void HideListBox(GameWindow *window)
 {
-	__asm {
-		__emit 0x83
-		__emit 0xec
-		__emit 0x10
-		__emit 0x57
-		__emit 0x8b
-		__emit 0x7c
-		__emit 0x24
-		__emit 0x18
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0xe8
-		__emit 0xc9
-		__emit 0x27
-		__emit 0xb9
-		__emit 0xff
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x0f
-		__emit 0x84
-		__emit 0x86
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x56
-		__emit 0x8b
-		__emit 0x70
-		__emit 0x2c
-		__emit 0x85
-		__emit 0xf6
-		__emit 0x74
-		__emit 0x7d
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xe8
-		__emit 0x31
-		__emit 0x68
-		__emit 0xb8
-		__emit 0xff
-		__emit 0x84
-		__emit 0xc0
-		__emit 0x75
-		__emit 0x72
-		__emit 0x6a
-		__emit 0x01
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xe8
-		__emit 0x97
-		__emit 0x41
-		__emit 0xb7
-		__emit 0xff
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0xe8
-		__emit 0x9e
-		__emit 0x27
-		__emit 0xb9
-		__emit 0xff
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x74
-		__emit 0x07
-		__emit 0x8b
-		__emit 0x40
-		__emit 0x28
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x75
-		__emit 0x02
-		__emit 0x33
-		__emit 0xc0
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x51
-		__emit 0x8d
-		__emit 0x54
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x52
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0xe8
-		__emit 0x04
-		__emit 0x31
-		__emit 0xb8
-		__emit 0xff
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x14
-		__emit 0x50
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x14
-		__emit 0x51
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0xe8
-		__emit 0xf3
-		__emit 0x30
-		__emit 0xb8
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x54
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x10
-		__emit 0x52
-		__emit 0x50
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0xe8
-		__emit 0xd2
-		__emit 0x44
-		__emit 0xb9
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x40
-		__emit 0x1b
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x11
-		__emit 0xff
-		__emit 0x92
-		__emit 0xc0
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x3b
-		__emit 0xc7
-		__emit 0x75
-		__emit 0x10
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x40
-		__emit 0x1b
-		__emit 0x2f
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x01
-		__emit 0x6a
-		__emit 0x00
-		__emit 0xff
-		__emit 0x90
-		__emit 0xbc
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5e
-		__emit 0x5f
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
+	ICoord2D winSize;
+	ICoord2D newSize;
+	GameWindow *listBox = getListBox(window);
+	if (!listBox)
+		return;
+
+	if (!listBox->winIsHidden())
+	{
+		listBox->winHide(1);
+		GameWindow *editBox = getEditBox(window);
+		editBox->winGetSize(&winSize.x, &winSize.y);
+		window->winGetSize(&newSize.x, &newSize.y);
+		window->winSetSize(newSize.x, winSize.y);
+		if (TheWindowManager->winGetFocus() == window)
+			TheWindowManager->winSetFocus(0);
 	}
 }
