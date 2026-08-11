f8135805347d9aa9620a425be6f2697a0e57aa20
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 11:59:11 -0400

    Open-BFME5 convert DX8 device caps constructor to exact C++

--- Code/Libraries/Source/WWVegas/WW3D2/DX8Caps_ctor_device_Thunk.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/DX8Caps_ctor_device_Thunk.cpp
@@ -1,251 +1,42 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /MD /EHsc /ICode/Libraries/Source/WWVegas/WWLib
+// Open-BFME5: lift the device-backed DX8 capability constructor to clean C++.
+
+#include "wwstring.h"
 
 struct IDirect3D8;
 struct IDirect3DDevice8;
 struct _D3DADAPTER_IDENTIFIER8;
 enum WW3DFormat { WW3DFormat_dummy = 0 };
-class DX8Caps {
+
+class DX8Caps
+{
 public:
-	DX8Caps(IDirect3D8 *, IDirect3DDevice8 *, WW3DFormat, const _D3DADAPTER_IDENTIFIER8 &);
+	DX8Caps(IDirect3D8 *, IDirect3DDevice8 *, WW3DFormat,
+		const _D3DADAPTER_IDENTIFIER8 &);
+
+private:
+	void Init_Caps(IDirect3DDevice8 *);
+	void Compute_Caps(WW3DFormat, const _D3DADAPTER_IDENTIFIER8 &);
+
+	int m_maxDisplayWidth;
+	int m_maxDisplayHeight;
+	unsigned char m_unreconstructed_008[0x284];
+	int m_vendorId;
+	unsigned char m_unreconstructed_290[0x0C];
+	StringClass m_driverDLL;
+	IDirect3D8 *m_direct3D;
+	StringClass m_capsLog;
+	StringClass m_compactLog;
 };
 
 // ??0DX8Caps@@QAE@PAUIDirect3D8@@PAUIDirect3DDevice8@@W4WW3DFormat@@ABU_D3DADAPTER_IDENTIFIER8@@@Z
-__declspec(naked) DX8Caps::DX8Caps(IDirect3D8 *, IDirect3DDevice8 *, WW3DFormat, const _D3DADAPTER_IDENTIFIER8 &)
+DX8Caps::DX8Caps(IDirect3D8 *direct3D, IDirect3DDevice8 *device,
+	WW3DFormat displayFormat, const _D3DADAPTER_IDENTIFIER8 &adapterIdentifier)
+	: m_maxDisplayWidth(0),
+	  m_maxDisplayHeight(0),
+	  m_vendorId(0),
+	  m_direct3D(direct3D)
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x0a
-		__emit 0xbf
-		__emit 0x05
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x8b
-		__emit 0xf1
-		__emit 0x57
-		__emit 0xc7
-		__emit 0x06
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x04
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xc7
-		__emit 0x86
-		__emit 0x8c
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xa1
-		__emit 0x24
-		__emit 0x91
-		__emit 0x2d
-		__emit 0x01
-		__emit 0x8d
-		__emit 0xbe
-		__emit 0x9c
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x6a
-		__emit 0x00
-		__emit 0x6a
-		__emit 0x00
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x10
-		__emit 0x89
-		__emit 0x07
-		__emit 0xe8
-		__emit 0x43
-		__emit 0xed
-		__emit 0x0b
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x0f
-		__emit 0x8a
-		__emit 0x15
-		__emit 0xc8
-		__emit 0xec
-		__emit 0x34
-		__emit 0x01
-		__emit 0x88
-		__emit 0x11
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x1c
-		__emit 0x89
-		__emit 0x86
-		__emit 0xa0
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x24
-		__emit 0x91
-		__emit 0x2d
-		__emit 0x01
-		__emit 0x8d
-		__emit 0xbe
-		__emit 0xa4
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x6a
-		__emit 0x00
-		__emit 0x89
-		__emit 0x0f
-		__emit 0x6a
-		__emit 0x00
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x1c
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0x0e
-		__emit 0xed
-		__emit 0x0b
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x17
-		__emit 0xa0
-		__emit 0xc8
-		__emit 0xec
-		__emit 0x34
-		__emit 0x01
-		__emit 0x88
-		__emit 0x02
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x24
-		__emit 0x91
-		__emit 0x2d
-		__emit 0x01
-		__emit 0x8d
-		__emit 0xbe
-		__emit 0xa8
-		__emit 0x02
-		__emit 0x00
-		__emit 0x00
-		__emit 0x6a
-		__emit 0x00
-		__emit 0x89
-		__emit 0x0f
-		__emit 0x6a
-		__emit 0x00
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x1c
-		__emit 0x01
-		__emit 0xe8
-		__emit 0xe7
-		__emit 0xec
-		__emit 0x0b
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x17
-		__emit 0xa0
-		__emit 0xc8
-		__emit 0xec
-		__emit 0x34
-		__emit 0x01
-		__emit 0x88
-		__emit 0x02
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x20
-		__emit 0x51
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xc6
-		__emit 0x44
-		__emit 0x24
-		__emit 0x18
-		__emit 0x02
-		__emit 0xe8
-		__emit 0x1d
-		__emit 0xeb
-		__emit 0xff
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x54
-		__emit 0x24
-		__emit 0x28
-		__emit 0x8b
-		__emit 0x44
-		__emit 0x24
-		__emit 0x24
-		__emit 0x52
-		__emit 0x50
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xe8
-		__emit 0x5c
-		__emit 0xf6
-		__emit 0xff
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x5f
-		__emit 0x8b
-		__emit 0xc6
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc2
-		__emit 0x10
-		__emit 0x00
-	}
+	Init_Caps(device);
+	Compute_Caps(displayFormat, adapterIdentifier);
 }
