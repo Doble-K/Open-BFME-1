793b758a09323318df53a574c70c2e0f250506ec
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 11:09:24 -0400

    Open-BFME5 convert skirmish game info ctor to exact C++

--- Code/GameEngine/Source/GameNetwork/SkirmishGameInfoCtorThunk.cpp
+++ Code/GameEngine/Source/GameNetwork/SkirmishGameInfoCtorThunk.cpp
@@ -1,148 +1,45 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: lift the retail skirmish slot-array constructor to C++.
 
-class SkirmishGameInfo
+class GameSlot
 {
 public:
-    SkirmishGameInfo();
+	GameSlot();
+	~GameSlot();
+private:
+	unsigned char m_storage[0x44];
 };
 
-// ??0SkirmishGameInfo@@QAE@XZ
-__declspec(naked) SkirmishGameInfo::SkirmishGameInfo()
+class GameInfo
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x19;
-        __emit 0xeb;
-        __emit 0x03;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x53;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x57;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0xe8;
-        __emit 0x7b;
-        __emit 0x3e;
-        __emit 0xa1;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xff;
-        __emit 0x89;
-        __emit 0x7c;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x58;
-        __emit 0x44;
-        __emit 0x37;
-        __emit 0x07;
-        __emit 0x01;
-        __emit 0x68;
-        __emit 0x88;
-        __emit 0xb9;
-        __emit 0x40;
-        __emit 0x00;
-        __emit 0x68;
-        __emit 0x58;
-        __emit 0x3e;
-        __emit 0x41;
-        __emit 0x00;
-        __emit 0x6a;
-        __emit 0x08;
-        __emit 0x6a;
-        __emit 0x44;
-        __emit 0x8d;
-        __emit 0x5e;
-        __emit 0x5c;
-        __emit 0x53;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x2c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xdc;
-        __emit 0x71;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x58;
-        __emit 0xc8;
-        __emit 0x71;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0x6a;
-        __emit 0xd7;
-        __emit 0x3d;
-        __emit 0x00;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x02;
-        __emit 0x90;
-        __emit 0x53;
-        __emit 0x57;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0xcc;
-        __emit 0x4c;
-        __emit 0xa2;
-        __emit 0xff;
-        __emit 0x47;
-        __emit 0x83;
-        __emit 0xc3;
-        __emit 0x44;
-        __emit 0x83;
-        __emit 0xff;
-        __emit 0x08;
-        __emit 0x7c;
-        __emit 0xee;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0x5f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x5b;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+public:
+	GameInfo();
+	virtual ~GameInfo();
+	void setSlotPointer(int index, GameSlot *slot);
+private:
+	unsigned char m_storage[0x54];
+};
+
+class Snapshot
+{
+public:
+	virtual ~Snapshot();
+};
+
+class SkirmishGameInfo : public GameInfo, public Snapshot
+{
+public:
+	SkirmishGameInfo();
+	virtual ~SkirmishGameInfo();
+private:
+	virtual void crc(void *);
+	virtual void xfer(void *);
+	virtual void loadPostProcess();
+	GameSlot m_skirmishSlot[8];
+};
+
+SkirmishGameInfo::SkirmishGameInfo()
+{
+	for (int i = 0; i < 8; ++i)
+		setSlotPointer(i, &m_skirmishSlot[i]);
 }
