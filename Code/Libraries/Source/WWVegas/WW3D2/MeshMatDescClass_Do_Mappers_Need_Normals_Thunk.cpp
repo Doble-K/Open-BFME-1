a4bf47340aa6d86652e0896a0dc15a41407d7260
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 11:48:44 -0400

    Open-BFME5: convert mapper normals scan to C++

--- Code/Libraries/Source/WWVegas/WW3D2/MeshMatDescClass_Do_Mappers_Need_Normals_Thunk.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/MeshMatDescClass_Do_Mappers_Need_Normals_Thunk.cpp
@@ -1,282 +1,43 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/debug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main
+#define Matrix4x4 Matrix4
 
-class MeshMatDescClass
-{
-public:
-	bool Do_Mappers_Need_Normals();
-};
+#include "meshmatdesc.h"
+#include "vertmaterial.h"
+#include "dx8wrapper.h"
+#include "dx8caps.h"
 
 // ?Do_Mappers_Need_Normals@MeshMatDescClass@@QAE_NXZ
-__declspec(naked) bool MeshMatDescClass::Do_Mappers_Need_Normals()
+bool MeshMatDescClass::Do_Mappers_Need_Normals()
 {
-	__asm {
-        __emit 0x83
-        __emit 0xec
-        __emit 0x0c
-        __emit 0x8b
-        __emit 0xc1
-        __emit 0x8a
-        __emit 0x0d
-        __emit 0x0c
-        __emit 0x05
-        __emit 0x34
-        __emit 0x01
-        __emit 0x84
-        __emit 0xc9
-        __emit 0x89
-        __emit 0x44
-        __emit 0x24
-        __emit 0x08
-        __emit 0x74
-        __emit 0x1f
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x78
-        __emit 0x05
-        __emit 0x34
-        __emit 0x01
-        __emit 0x8a
-        __emit 0x91
-        __emit 0x3b
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x84
-        __emit 0xd2
-        __emit 0x74
-        __emit 0x0f
-        __emit 0x83
-        __emit 0x3d
-        __emit 0x8c
-        __emit 0x6d
-        __emit 0x2d
-        __emit 0x01
-        __emit 0x01
-        __emit 0x76
-        __emit 0x06
-        __emit 0xb0
-        __emit 0x01
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x0c
-        __emit 0xc3
-        __emit 0x83
-        __emit 0x38
-        __emit 0x00
-        __emit 0x53
-        __emit 0x55
-        __emit 0x56
-        __emit 0x57
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x0f
-        __emit 0x8e
-        __emit 0xb3
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x98
-        __emit 0xa4
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x89
-        __emit 0x5c
-        __emit 0x24
-        __emit 0x10
-        __emit 0x8b
-        __emit 0x33
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x74
-        __emit 0x29
-        __emit 0x33
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc6
-        __emit 0x20
-        __emit 0x8d
-        __emit 0x64
-        __emit 0x24
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x06
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x0f
-        __emit 0x8b
-        __emit 0xc8
-        __emit 0x8b
-        __emit 0x11
-        __emit 0xff
-        __emit 0x52
-        __emit 0x1c
-        __emit 0x84
-        __emit 0xc0
-        __emit 0x0f
-        __emit 0x85
-        __emit 0x8f
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x47
-        __emit 0x83
-        __emit 0xc6
-        __emit 0x04
-        __emit 0x83
-        __emit 0xff
-        __emit 0x08
-        __emit 0x7c
-        __emit 0xe2
-        __emit 0xeb
-        __emit 0x5c
-        __emit 0x8b
-        __emit 0x50
-        __emit 0x04
-        __emit 0x33
-        __emit 0xc9
-        __emit 0x33
-        __emit 0xed
-        __emit 0x85
-        __emit 0xd2
-        __emit 0x7e
-        __emit 0x51
-        __emit 0xeb
-        __emit 0x03
-        __emit 0x8d
-        __emit 0x49
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x43
-        __emit 0x30
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x08
-        __emit 0x8b
-        __emit 0x40
-        __emit 0x08
-        __emit 0x8b
-        __emit 0x1c
-        __emit 0xa8
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x8b
-        __emit 0x1b
-        __emit 0x3b
-        __emit 0xd9
-        __emit 0x74
-        __emit 0x27
-        __emit 0x85
-        __emit 0xdb
-        __emit 0x74
-        __emit 0x23
-        __emit 0x33
-        __emit 0xff
-        __emit 0x8d
-        __emit 0x73
-        __emit 0x20
-        __emit 0x8b
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x06
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x8b
-        __emit 0xc8
-        __emit 0x8b
-        __emit 0x11
-        __emit 0xff
-        __emit 0x52
-        __emit 0x1c
-        __emit 0x84
-        __emit 0xc0
-        __emit 0x75
-        __emit 0x43
-        __emit 0x47
-        __emit 0x83
-        __emit 0xc6
-        __emit 0x04
-        __emit 0x83
-        __emit 0xff
-        __emit 0x08
-        __emit 0x7c
-        __emit 0xe6
-        __emit 0x8b
-        __emit 0xcb
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x18
-        __emit 0x8b
-        __emit 0x50
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x5c
-        __emit 0x24
-        __emit 0x10
-        __emit 0x45
-        __emit 0x3b
-        __emit 0xea
-        __emit 0x7c
-        __emit 0xb4
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x14
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x18
-        __emit 0x8b
-        __emit 0x10
-        __emit 0x41
-        __emit 0x83
-        __emit 0xc3
-        __emit 0x04
-        __emit 0x3b
-        __emit 0xca
-        __emit 0x89
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x14
-        __emit 0x89
-        __emit 0x5c
-        __emit 0x24
-        __emit 0x10
-        __emit 0x0f
-        __emit 0x8c
-        __emit 0x57
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5d
-        __emit 0x32
-        __emit 0xc0
-        __emit 0x5b
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x0c
-        __emit 0xc3
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5d
-        __emit 0xb0
-        __emit 0x01
-        __emit 0x5b
-        __emit 0x83
+	if (*reinterpret_cast<bool *>(0x0134050c)
+		&& *reinterpret_cast<bool *>(*reinterpret_cast<unsigned char **>(0x01340578) + 0x13b)
+		&& *reinterpret_cast<unsigned int *>(0x012d6d8c) > 1) {
+		return true;
 	}
+
+	for (int pass = 0; pass < PassCount; ++pass) {
+		if (Material[pass] != 0) {
+			VertexMaterialClass *retailMaterial = reinterpret_cast<VertexMaterialClass *>(
+				reinterpret_cast<unsigned char *>(Material[pass]) - 4);
+			if (retailMaterial->Do_Mappers_Need_Normals()) return true;
+		} else {
+			VertexMaterialClass *prev_mtl = 0;
+			VertexMaterialClass *mtl;
+			for (int vidx = 0; vidx < VertexCount; ++vidx) {
+				if (MaterialArray[pass]) {
+					VertexMaterialClass **items = *reinterpret_cast<VertexMaterialClass ***>((unsigned char *)MaterialArray[pass] + 8);
+					mtl = items[vidx];
+				} else {
+					mtl = Material[pass];
+				}
+				if (mtl != prev_mtl && mtl != 0) {
+					VertexMaterialClass *retailMaterial = reinterpret_cast<VertexMaterialClass *>(
+						reinterpret_cast<unsigned char *>(mtl) - 4);
+					if (retailMaterial->Do_Mappers_Need_Normals()) return true;
+					prev_mtl = mtl;
+				}
+			}
+		}
+	}
+	return false;
 }
