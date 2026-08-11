27d317389d6706e3ee346f94375eac7361b2d2ff
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-11 11:03:58 -0400

    Open-BFME5 convert upgrade allocation to exact C++

--- Code/GameEngine/Source/Common/UpgradeCenter_newUpgrade_Thunk.cpp
+++ Code/GameEngine/Source/Common/UpgradeCenter_newUpgrade_Thunk.cpp
@@ -1,265 +1,72 @@
 // cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// Open-BFME5: lift the retail upgrade allocation/link path to standalone C++.
+
+typedef int NameKeyType;
 
 class AsciiString
 {
+public:
+	AsciiString(const char *text);
+	~AsciiString();
+	AsciiString &operator=(const AsciiString &other);
+	const char *str() const { return m_data ? m_data + 8 : ""; }
+private:
+	const char *m_data;
 };
-class UpgradeTemplate;
+
+class NameKeyGenerator
+{
+public:
+	NameKeyType nameToKey(const char *name);
+};
+
+extern NameKeyGenerator *TheNameKeyGenerator;
+
+class UpgradeTemplate
+{
+public:
+	UpgradeTemplate();
+	virtual ~UpgradeTemplate();
+	UpgradeTemplate &operator=(const UpgradeTemplate &other);
+private:
+	friend class UpgradeCenter;
+	int m_type;
+	AsciiString m_name;
+	NameKeyType m_nameKey;
+	char m_pad10[0x20 - 0x10];
+	int m_upgradeMaskIndex;
+	char m_pad24[0x108 - 0x24];
+	UpgradeTemplate *m_next;
+	UpgradeTemplate *m_prev;
+	char m_tail[0x120 - 0x110];
+};
+
 class UpgradeCenter
 {
 public:
-	UpgradeTemplate *newUpgrade(const AsciiString &);
+	UpgradeTemplate *newUpgrade(const AsciiString &name);
+	const UpgradeTemplate *findUpgrade(const AsciiString &name) const;
+private:
+	virtual void reserved();
+	int m_basePad;
+	UpgradeTemplate *m_upgradeList;
+	int m_nextTemplateMaskBit;
 };
 
-// ?newUpgrade@UpgradeCenter@@QAEPAVUpgradeTemplate@@ABVAsciiString@@@Z
-__declspec(naked) UpgradeTemplate *UpgradeCenter::newUpgrade(const AsciiString &)
+UpgradeTemplate *UpgradeCenter::newUpgrade(const AsciiString &name)
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x23
-		__emit 0xd4
-		__emit 0xff
-		__emit 0x00
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x56
-		__emit 0x57
-		__emit 0x68
-		__emit 0x20
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0xf9
-		__emit 0xe8
-		__emit 0xcc
-		__emit 0x6d
-		__emit 0x77
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x04
-		__emit 0x89
-		__emit 0x44
-		__emit 0x24
-		__emit 0x08
-		__emit 0x33
-		__emit 0xf6
-		__emit 0x3b
-		__emit 0xc6
-		__emit 0x89
-		__emit 0x74
-		__emit 0x24
-		__emit 0x14
-		__emit 0x74
-		__emit 0x09
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0xe8
-		__emit 0x1e
-		__emit 0x22
-		__emit 0xf1
-		__emit 0xff
-		__emit 0x8b
-		__emit 0xf0
-		__emit 0x53
-		__emit 0x68
-		__emit 0xa4
-		__emit 0x8d
-		__emit 0x08
-		__emit 0x01
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x1c
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x2b
-		__emit 0xda
-		__emit 0x77
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x50
-		__emit 0x8b
-		__emit 0xcf
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x1c
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0xe8
-		__emit 0xb1
-		__emit 0x47
-		__emit 0xf2
-		__emit 0xff
-		__emit 0x8d
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x8b
-		__emit 0xd8
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x18
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x84
-		__emit 0xc7
-		__emit 0x77
-		__emit 0x00
-		__emit 0x85
-		__emit 0xdb
-		__emit 0x74
-		__emit 0x08
-		__emit 0x53
-		__emit 0x8b
-		__emit 0xce
-		__emit 0xe8
-		__emit 0xec
-		__emit 0x95
-		__emit 0xef
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x20
-		__emit 0x53
-		__emit 0x8d
-		__emit 0x4e
-		__emit 0x08
-		__emit 0xe8
-		__emit 0xbb
-		__emit 0xca
-		__emit 0x77
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x03
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x5b
-		__emit 0x74
-		__emit 0x05
-		__emit 0x83
-		__emit 0xc0
-		__emit 0x08
-		__emit 0xeb
-		__emit 0x05
-		__emit 0xb8
-		__emit 0x8b
-		__emit 0x38
-		__emit 0x07
-		__emit 0x01
-		__emit 0x8b
-		__emit 0x0d
-		__emit 0x00
-		__emit 0xd6
-		__emit 0x2e
-		__emit 0x01
-		__emit 0x50
-		__emit 0xe8
-		__emit 0xe5
-		__emit 0xfb
-		__emit 0xf2
-		__emit 0xff
-		__emit 0x89
-		__emit 0x46
-		__emit 0x0c
-		__emit 0x8b
-		__emit 0x4f
-		__emit 0x0c
-		__emit 0x89
-		__emit 0x4e
-		__emit 0x20
-		__emit 0xff
-		__emit 0x47
-		__emit 0x0c
-		__emit 0xc7
-		__emit 0x86
-		__emit 0x0c
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x57
-		__emit 0x08
-		__emit 0x89
-		__emit 0x96
-		__emit 0x08
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x47
-		__emit 0x08
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x74
-		__emit 0x06
-		__emit 0x89
-		__emit 0xb0
-		__emit 0x0c
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x89
-		__emit 0x77
-		__emit 0x08
-		__emit 0x5f
-		__emit 0x8b
-		__emit 0xc6
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc2
-		__emit 0x04
-		__emit 0x00
-	}
+	UpgradeTemplate *newUpgrade = new UpgradeTemplate;
+	const UpgradeTemplate *defaultUpgrade = findUpgrade("DefaultUpgrade");
+	if (defaultUpgrade)
+		*newUpgrade = *defaultUpgrade;
+
+	newUpgrade->m_name = name;
+	newUpgrade->m_nameKey = TheNameKeyGenerator->nameToKey(name.str());
+	newUpgrade->m_upgradeMaskIndex = m_nextTemplateMaskBit++;
+	newUpgrade->m_prev = 0;
+	newUpgrade->m_next = m_upgradeList;
+	if (m_upgradeList)
+		m_upgradeList->m_prev = newUpgrade;
+	m_upgradeList = newUpgrade;
+	return newUpgrade;
 }
