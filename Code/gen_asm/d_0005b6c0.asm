d5153f6cffff32ddf59404cbbf0a9f2b4971c889
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 04:47:40 -0400

    Open-BFME5 convert second cdecl pointer identity to C++

--- Code/gen_asm/d_0005b6c0.asm
+++ Code/gen_asm/d_0005b6c0.asm
@@ -2423,12 +2423,6 @@ public ?d_0006b7f0@@YAXXZ
     db 8Bh, 0C1h, 8Bh, 4Ch, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
 ?d_0006b7f0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0006B800 size 5
-public ?d_0006b800@@YAXXZ
-?d_0006b800@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 0C3h
-?d_0006b800@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0006B810 size 5
 public ?d_0006b810@@YAXXZ
 ?d_0006b810@@YAXXZ PROC
