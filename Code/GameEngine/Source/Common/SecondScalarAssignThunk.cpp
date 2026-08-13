fcf0bbf06a05b71832d1088b18f62bbb06beb17e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 23:19:22 -0400

    Open-BFME5 convert alternate second argument assignment to C++

--- Code/GameEngine/Source/Common/SecondScalarAssignThunk.cpp
+++ Code/GameEngine/Source/Common/SecondScalarAssignThunk.cpp
@@ -3,9 +3,16 @@ struct SecondScalarAssignThunk
     unsigned long value;
 
     SecondScalarAssignThunk &assign(unsigned long ignored, unsigned long newValue);
+    SecondScalarAssignThunk &assignAlternate(unsigned long ignored, unsigned long newValue);
 };
 
 SecondScalarAssignThunk &SecondScalarAssignThunk::assign(unsigned long, unsigned long newValue)
+{
+    value = newValue;
+    return *this;
+}
+
+SecondScalarAssignThunk &SecondScalarAssignThunk::assignAlternate(unsigned long, unsigned long newValue)
 {
     value = newValue;
     return *this;
