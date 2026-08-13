25822bd259cb3f495ddd6a5f94da1113fa99924c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 02:45:54 -0400

    Open-BFME5 convert ninety-eighth vtable slot return to C++

--- Code/GameEngine/Source/Common/VtableSlot2StoreThunk.cpp
+++ Code/GameEngine/Source/Common/VtableSlot2StoreThunk.cpp
@@ -104,6 +104,7 @@ struct VtableSlot2StoreThunk
     VtableSlot2Result getNinetyFifth() const;
     VtableSlot2Result getNinetySixth() const;
     VtableSlot2Result getNinetySeventh() const;
+    VtableSlot2Result getNinetyEighth() const;
 };
 
 VtableSlot2Result VtableSlot2StoreThunk::get() const
@@ -683,6 +684,12 @@ VtableSlot2Result VtableSlot2StoreThunk::getNinetySixth() const
 }
 
 VtableSlot2Result VtableSlot2StoreThunk::getNinetySeventh() const
+{
+    VtableSlot2Result result = { vtable[2] };
+    return result;
+}
+
+VtableSlot2Result VtableSlot2StoreThunk::getNinetyEighth() const
 {
     VtableSlot2Result result = { vtable[2] };
     return result;
--- Code/gen_asm/d_003b74b0.asm
+++ Code/gen_asm/d_003b74b0.asm
@@ -3699,12 +3699,6 @@ public ?d_003d32c0@@YAXXZ
     db 8Bh, 01h, 83h, 0C0h, 10h, 0C3h
 ?d_003d32c0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x003D32E0 size 14
-public ?d_003d32e0@@YAXXZ
-?d_003d32e0@@YAXXZ PROC
-    db 8Bh, 01h, 8Bh, 48h, 08h, 8Bh, 44h, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
-?d_003d32e0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x003D3300 size 11
 public ?d_003d3300@@YAXXZ
 ?d_003d3300@@YAXXZ PROC
