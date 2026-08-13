db6702989bdc369c0ede99c1ee925158d12cf582
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 09:51:13 -0400

    Open-BFME5: convert RVA 13E830 value copy to C++

--- Code/gen_asm/d_00138250.asm
+++ Code/gen_asm/d_00138250.asm
@@ -1089,12 +1089,6 @@ public ?d_0013e810@@YAXXZ
     db 89h, 40h, 08h, 8Bh, 09h, 89h, 49h, 0Ch, 0C3h
 ?d_0013e810@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0013E830 size 11
-public ?d_0013e830@@YAXXZ
-?d_0013e830@@YAXXZ PROC
-    db 8Bh, 09h, 8Bh, 44h, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
-?d_0013e830@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0013E840 size 11
 public ?d_0013e840@@YAXXZ
 ?d_0013e840@@YAXXZ PROC
