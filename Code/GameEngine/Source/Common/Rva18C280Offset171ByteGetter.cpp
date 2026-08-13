774efc5489b7de9016b686b82700be96048dd6eb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 08:29:16 -0400

    Open-BFME5 convert RVA 18C280 byte getter to C++

---
+++ Code/GameEngine/Source/Common/Rva18C280Offset171ByteGetter.cpp
@@ -0,0 +1,12 @@
+struct Rva18C280Offset171ByteGetter
+{
+    unsigned char unused[171];
+    unsigned char value;
+
+    unsigned char get_value();
+};
+
+unsigned char Rva18C280Offset171ByteGetter::get_value()
+{
+    return value;
+}
