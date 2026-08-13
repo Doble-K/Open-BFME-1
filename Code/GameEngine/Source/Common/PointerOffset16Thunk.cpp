eca6f1350a82b50a86d16419b1ae92da37bcf747
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 23:25:23 -0400

    Open-BFME5 convert fourth pointer offset sixteen helper to C++

--- Code/GameEngine/Source/Common/PointerOffset16Thunk.cpp
+++ Code/GameEngine/Source/Common/PointerOffset16Thunk.cpp
@@ -12,3 +12,8 @@ void *pointerOffset16Third(void *value)
 {
     return static_cast<char *>(value) + 16;
 }
+
+void *pointerOffset16Fourth(void *value)
+{
+    return static_cast<char *>(value) + 16;
+}
