cbc28c180c3adf7555def2924cd95eeaa05a1c5b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 23:10:24 -0400

    Open-BFME5 convert alternate scalar value return to C++

--- Code/GameEngine/Source/Common/ScalarValueReturnThunk.cpp
+++ Code/GameEngine/Source/Common/ScalarValueReturnThunk.cpp
@@ -8,9 +8,16 @@ struct ScalarValueReturnThunk
     unsigned long value;
 
     ScalarValueResult get() const;
+    ScalarValueResult getAlternate() const;
 };
 
 ScalarValueResult ScalarValueReturnThunk::get() const
+{
+    ScalarValueResult result = { value };
+    return result;
+}
+
+ScalarValueResult ScalarValueReturnThunk::getAlternate() const
 {
     ScalarValueResult result = { value };
     return result;
--- Code/gen_asm/d_003519c0.asm
+++ Code/gen_asm/d_003519c0.asm
@@ -3153,12 +3153,6 @@ public ?d_0036cfd0@@YAXXZ
     db 0C3h
 ?d_0036cfd0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0036D150 size 11
-public ?d_0036d150@@YAXXZ
-?d_0036d150@@YAXXZ PROC
-    db 8Bh, 09h, 8Bh, 44h, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
-?d_0036d150@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0036D1F0 size 14
 public ?d_0036d1f0@@YAXXZ
 ?d_0036d1f0@@YAXXZ PROC
