5b7cd4854bf164c19f1aa7e41a4e4b4b51a5a6ac
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 12:55:28 -0400

    Open-BFME5 convert return-this member at 001B4DE0 to C++

---
+++ Code/GameEngine/Source/Common/ReturnThis001B4DE0.cpp
@@ -0,0 +1,12 @@
+// cl: /DNDEBUG /MD
+
+class AddressObject001B4DE0
+{
+public:
+	AddressObject001B4DE0 *returnThis(void *unused);
+};
+
+AddressObject001B4DE0 *AddressObject001B4DE0::returnThis(void *unused)
+{
+	return this;
+}
--- Code/gen_asm/d_001adb10.asm
+++ Code/gen_asm/d_001adb10.asm
@@ -830,12 +830,6 @@ public ?d_001b4dd0@@YAXXZ
     db 8Bh, 0C1h, 8Bh, 4Ch, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
 ?d_001b4dd0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x001B4DE0 size 5
-public ?d_001b4de0@@YAXXZ
-?d_001b4de0@@YAXXZ PROC
-    db 8Bh, 0C1h, 0C2h, 04h, 00h
-?d_001b4de0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x001B4DF0 size 5
 public ?d_001b4df0@@YAXXZ
 ?d_001b4df0@@YAXXZ PROC
