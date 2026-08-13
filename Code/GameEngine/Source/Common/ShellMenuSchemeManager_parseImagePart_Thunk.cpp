7a368609b4d5a73dca18c33215922776617a9443
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:57:07 -0400

    Open-BFME5 convert shell menu image parser to C++

--- Code/GameEngine/Source/Common/ShellMenuSchemeManager_parseImagePart_Thunk.cpp
+++ Code/GameEngine/Source/Common/ShellMenuSchemeManager_parseImagePart_Thunk.cpp
@@ -1,7 +1,37 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS
+// stlport
 
-class INI;
+#include <list>
+
+class INI
+{
+public:
+	void initFromINI(void *, const void *);
+};
+
+class ShellMenuSchemeImage
+{
+public:
+	ShellMenuSchemeImage();
+
+private:
+	unsigned char m_name[4];
+	int m_position[2];
+	int m_size[2];
+	void *m_image;
+};
+
+class ShellMenuScheme
+{
+private:
+	unsigned char m_pad00[4];
+	_STL::list<ShellMenuSchemeImage *> m_imageList;
+
+	friend class ShellMenuSchemeManager;
+};
+
+extern const unsigned char g_shellMenuSchemeImageFieldParse[];
+
 class ShellMenuSchemeManager
 {
 public:
@@ -9,154 +39,15 @@ public:
 };
 
 // ?parseImagePart@ShellMenuSchemeManager@@SAXPAVINI@@PAX1PBX@Z
-__declspec(naked) void __cdecl ShellMenuSchemeManager::parseImagePart(INI *, void *, void *, const void *)
+void __cdecl ShellMenuSchemeManager::parseImagePart(INI *ini, void *instance,
+	void *, const void *)
 {
-	__asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xcb
-        __emit 0x69
-        __emit 0x03
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x56
-        __emit 0x57
-        __emit 0x6a
-        __emit 0x18
-        __emit 0xe8
-        __emit 0x91
-        __emit 0x0a
-        __emit 0x30
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x89
-        __emit 0x44
-        __emit 0x24
-        __emit 0x08
-        __emit 0x85
-        __emit 0xc0
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x74
-        __emit 0x0b
-        __emit 0x8b
-        __emit 0xc8
-        __emit 0xe8
-        __emit 0xc6
-        __emit 0xc5
-        __emit 0xa9
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xf8
-        __emit 0xeb
-        __emit 0x02
-        __emit 0x33
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x1c
-        __emit 0x68
-        __emit 0x38
-        __emit 0xb6
-        __emit 0x10
-        __emit 0x01
-        __emit 0x57
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x1c
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0xca
-        __emit 0x0b
-        __emit 0x2d
-        __emit 0x00
-        __emit 0x85
-        __emit 0xff
-        __emit 0x74
-        __emit 0x27
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x20
-        __emit 0x8b
-        __emit 0x70
-        __emit 0x04
-        __emit 0x6a
-        __emit 0x0c
-        __emit 0xe8
-        __emit 0x58
-        __emit 0xd0
-        __emit 0x2a
-        __emit 0x00
-        __emit 0x8d
-        __emit 0x48
-        __emit 0x08
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x04
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x02
-        __emit 0x89
-        __emit 0x39
-        __emit 0x8b
-        __emit 0x4e
-        __emit 0x04
-        __emit 0x89
-        __emit 0x30
-        __emit 0x89
-        __emit 0x48
-        __emit 0x04
-        __emit 0x89
-        __emit 0x01
-        __emit 0x89
-        __emit 0x46
-        __emit 0x04
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x5f
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x5e
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc3
+	ShellMenuSchemeImage *schemeImage = new ShellMenuSchemeImage;
+	ini->initFromINI(schemeImage, g_shellMenuSchemeImageFieldParse);
+
+	if (schemeImage != 0)
+	{
+		ShellMenuScheme *scheme = static_cast<ShellMenuScheme *>(instance);
+		scheme->m_imageList.push_back(schemeImage);
 	}
 }
