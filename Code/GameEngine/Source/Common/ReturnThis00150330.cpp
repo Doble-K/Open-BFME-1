e2418ffbe1285fc9f93b25042206494a3f306a54
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 12:05:54 -0400

    Open-BFME5 convert adjacent return-this member to C++

---
+++ Code/GameEngine/Source/Common/ReturnThis00150330.cpp
@@ -0,0 +1,12 @@
+// cl: /DNDEBUG /MD
+
+class AddressObject00150330
+{
+public:
+	AddressObject00150330 *returnThis(void *unused);
+};
+
+AddressObject00150330 *AddressObject00150330::returnThis(void *unused)
+{
+	return this;
+}
--- Code/gen_asm/d_00138250.asm
+++ Code/gen_asm/d_00138250.asm
@@ -1894,12 +1894,6 @@ public ?d_001502b0@@YAXXZ
     db 04h, 89h, 41h, 08h, 0C2h, 0Ch, 00h
 ?d_001502b0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x00150330 size 5
-public ?d_00150330@@YAXXZ
-?d_00150330@@YAXXZ PROC
-    db 8Bh, 0C1h, 0C2h, 04h, 00h
-?d_00150330@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x001503D0 size 23
 public ?d_001503d0@@YAXXZ
 ?d_001503d0@@YAXXZ PROC
