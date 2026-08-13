263fd0f4517391d9deafdd556d7fd84ab759e035
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 08:35:29 -0400

    Open-BFME5 convert RVA A2190 getter to C++

---
+++ Code/GameEngine/Source/Common/RvaA2190Offset276Getter.cpp
@@ -0,0 +1,12 @@
+struct RvaA2190Offset276Getter
+{
+    unsigned char unused[276];
+    unsigned int value;
+
+    unsigned int get_value();
+};
+
+unsigned int RvaA2190Offset276Getter::get_value()
+{
+    return value;
+}
