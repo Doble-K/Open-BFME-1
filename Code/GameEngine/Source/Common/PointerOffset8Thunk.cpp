390d09f9cd129e50a92c48f0d51726b61d72032f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:33:21 -0400

    Open-BFME5 convert pointer offset eight helper to C++

---
+++ Code/GameEngine/Source/Common/PointerOffset8Thunk.cpp
@@ -0,0 +1,4 @@
+void *pointerOffset8(void *value)
+{
+    return static_cast<char *>(value) + 8;
+}
--- Code/gen_asm/d_003519c0.asm
+++ Code/gen_asm/d_003519c0.asm
@@ -3109,12 +3109,6 @@ public ?d_0036c780@@YAXXZ
     db 8Bh, 0C1h, 0C2h, 04h, 00h
 ?d_0036c780@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0036C790 size 8
-public ?d_0036c790@@YAXXZ
-?d_0036c790@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 08h, 0C3h
-?d_0036c790@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0036C7A0 size 8
 public ?d_0036c7a0@@YAXXZ
 ?d_0036c7a0@@YAXXZ PROC
