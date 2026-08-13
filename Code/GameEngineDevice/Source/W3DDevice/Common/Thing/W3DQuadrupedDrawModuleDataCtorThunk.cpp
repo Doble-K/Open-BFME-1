3b223a003a963a4f94fc0c77f85d5e31ab360fb1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:02:15 -0400

    Open-BFME5 convert W3DQuadruped module data constructor to C++

--- Code/GameEngineDevice/Source/W3DDevice/Common/Thing/W3DQuadrupedDrawModuleDataCtorThunk.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/Common/Thing/W3DQuadrupedDrawModuleDataCtorThunk.cpp
@@ -1,105 +1,38 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class W3DQuadrupedDrawModuleData
+// Open-BFME5: W3DQuadrupedDrawModuleData constructor.
+
+class W3DQuadrupedElement
 {
 public:
-    W3DQuadrupedDrawModuleData();
+	W3DQuadrupedElement();
+	~W3DQuadrupedElement();
+
+private:
+	unsigned int m_value;
 };
 
-__declspec(naked) W3DQuadrupedDrawModuleData::W3DQuadrupedDrawModuleData()
+class W3DQuadrupedDrawModuleDataBase
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x38;
-        __emit 0x9b;
-        __emit 0x04;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x04;
-        __emit 0xe8;
-        __emit 0x7a;
-        __emit 0x2a;
-        __emit 0x94;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x28;
-        __emit 0xd8;
-        __emit 0x40;
-        __emit 0x00;
-        __emit 0x68;
-        __emit 0xd9;
-        __emit 0x7b;
-        __emit 0x41;
-        __emit 0x00;
-        __emit 0x6a;
-        __emit 0x04;
-        __emit 0x6a;
-        __emit 0x04;
-        __emit 0x8d;
-        __emit 0x86;
-        __emit 0x5c;
-        __emit 0x01;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x24;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x90;
-        __emit 0xd1;
-        __emit 0x11;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0x9a;
-        __emit 0x70;
-        __emit 0x33;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+public:
+	W3DQuadrupedDrawModuleDataBase();
+	virtual ~W3DQuadrupedDrawModuleDataBase();
+
+private:
+	unsigned char m_pad[0x158];
+};
+
+class W3DQuadrupedDrawModuleData : public W3DQuadrupedDrawModuleDataBase
+{
+public:
+	W3DQuadrupedDrawModuleData();
+	virtual ~W3DQuadrupedDrawModuleData();
+
+private:
+	W3DQuadrupedElement m_elements[4];
+};
+
+// ??0W3DQuadrupedDrawModuleData@@QAE@XZ
+W3DQuadrupedDrawModuleData::W3DQuadrupedDrawModuleData()
+{
 }
