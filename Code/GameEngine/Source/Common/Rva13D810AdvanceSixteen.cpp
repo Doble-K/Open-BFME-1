0497ceff60fdf31a0070108a7cab9a6c1c95809d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 11:04:49 -0400

    Open-BFME5: convert RVA 13D810 pointer advance to C++

---
+++ Code/GameEngine/Source/Common/Rva13D810AdvanceSixteen.cpp
@@ -0,0 +1,4 @@
+unsigned char *rva_13d810_advance_sixteen(unsigned char *value)
+{
+    return value + 16;
+}
--- Code/gen_asm/d_00138250.asm
+++ Code/gen_asm/d_00138250.asm
@@ -405,12 +405,6 @@ public ?d_0013d7e0@@YAXXZ
     db 23h, 02h, 0F7h, 0D8h, 1Bh, 0C0h, 0F7h, 0D8h, 0C2h, 04h, 00h
 ?d_0013d7e0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0013D810 size 8
-public ?d_0013d810@@YAXXZ
-?d_0013d810@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 10h, 0C3h
-?d_0013d810@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0013D820 size 8
 public ?d_0013d820@@YAXXZ
 ?d_0013d820@@YAXXZ PROC
