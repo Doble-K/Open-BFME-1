89ae68517f4267e9e85509a6887631f8a136d719
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:44:23 -0400

    Open-BFME5 convert alternate pointer identity to C++

--- Code/GameEngine/Source/Common/PointerIdentityThunk.cpp
+++ Code/GameEngine/Source/Common/PointerIdentityThunk.cpp
@@ -2,3 +2,8 @@ void *__stdcall pointerIdentity(void *value)
 {
     return value;
 }
+
+void *__stdcall pointerIdentityAlternate(void *value)
+{
+    return value;
+}
--- Code/gen_asm/d_003519c0.asm
+++ Code/gen_asm/d_003519c0.asm
@@ -3109,12 +3109,6 @@ public ?d_0036c780@@YAXXZ
     db 8Bh, 0C1h, 0C2h, 04h, 00h
 ?d_0036c780@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0036C8C0 size 7
-public ?d_0036c8c0@@YAXXZ
-?d_0036c8c0@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 0C2h, 04h, 00h
-?d_0036c8c0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0036C8D0 size 7
 public ?d_0036c8d0@@YAXXZ
 ?d_0036c8d0@@YAXXZ PROC
