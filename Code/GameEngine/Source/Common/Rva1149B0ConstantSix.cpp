23a7f52c49d3e43498ea35fe690cc6bd3f60ea0f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 09:45:47 -0400

    Open-BFME5 convert RVA 1149B0 constant to C++

---
+++ Code/GameEngine/Source/Common/Rva1149B0ConstantSix.cpp
@@ -0,0 +1,4 @@
+unsigned int rva_1149b0_constant_six()
+{
+    return 6;
+}
--- Code/gen_asm/d_000f93a0.asm
+++ Code/gen_asm/d_000f93a0.asm
@@ -1668,12 +1668,6 @@ public ?d_00114280@@YAXXZ
     db 0B8h, 85h, 00h, 00h, 00h, 0C3h
 ?d_00114280@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x001149B0 size 6
-public ?d_001149b0@@YAXXZ
-?d_001149b0@@YAXXZ PROC
-    db 0B8h, 06h, 00h, 00h, 00h, 0C3h
-?d_001149b0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x00115350 size 6
 public ?d_00115350@@YAXXZ
 ?d_00115350@@YAXXZ PROC
