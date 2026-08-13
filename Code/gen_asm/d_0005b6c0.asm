f68072f5ea35123e424c853fbac1612e376761c8
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 23:45:06 -0400

    Open-BFME5 convert thirteenth vtable slot return to C++

--- Code/gen_asm/d_0005b6c0.asm
+++ Code/gen_asm/d_0005b6c0.asm
@@ -2761,12 +2761,6 @@ public ?d_0006c4c0@@YAXXZ
     db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 0Ch, 0C3h
 ?d_0006c4c0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0006C4D0 size 14
-public ?d_0006c4d0@@YAXXZ
-?d_0006c4d0@@YAXXZ PROC
-    db 8Bh, 01h, 8Bh, 48h, 08h, 8Bh, 44h, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
-?d_0006c4d0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0006C4F0 size 8
 public ?d_0006c4f0@@YAXXZ
 ?d_0006c4f0@@YAXXZ PROC
