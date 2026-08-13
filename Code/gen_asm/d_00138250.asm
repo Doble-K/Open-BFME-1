16b6b1b5b5c6cd8591cde77b64f8a77b50ef6158
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 10:59:49 -0400

    Open-BFME5: convert RVA 13D6D0 value store to C++

--- Code/gen_asm/d_00138250.asm
+++ Code/gen_asm/d_00138250.asm
@@ -398,12 +398,6 @@ public ?d_0013d5a0@@YAXXZ
     db 89h, 50h, 04h, 0C2h, 08h, 00h
 ?d_0013d5a0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0013D6D0 size 11
-public ?d_0013d6d0@@YAXXZ
-?d_0013d6d0@@YAXXZ PROC
-    db 8Bh, 0C1h, 8Bh, 4Ch, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
-?d_0013d6d0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0013D6E0 size 11
 public ?d_0013d6e0@@YAXXZ
 ?d_0013d6e0@@YAXXZ PROC
