f1646a577486aec2dfab56ab8151947823677cea
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 08:33:49 -0400

    Open-BFME5: convert RVA 724F0 value copy to C++

---
+++ Code/GameEngine/Source/Common/Rva724F0ValueCopy.cpp
@@ -0,0 +1,13 @@
+class Rva724F0ValueCopy {
+public:
+    unsigned int *copy(unsigned int *output);
+
+private:
+    unsigned int m_value;
+};
+
+unsigned int *Rva724F0ValueCopy::copy(unsigned int *output)
+{
+    *output = m_value;
+    return output;
+}
