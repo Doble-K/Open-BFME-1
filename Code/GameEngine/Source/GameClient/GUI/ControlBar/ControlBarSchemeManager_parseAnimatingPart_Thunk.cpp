7a8fcdc17d89a3191bc6f12acc9b6bcd039ce32a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 23:03:33 -0400

    Open-BFME5 convert control bar animation parser to C++

--- Code/GameEngine/Source/GameClient/GUI/ControlBar/ControlBarSchemeManager_parseAnimatingPart_Thunk.cpp
+++ Code/GameEngine/Source/GameClient/GUI/ControlBar/ControlBarSchemeManager_parseAnimatingPart_Thunk.cpp
@@ -1,234 +1,70 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS
+// stlport
 
-class INI;
-class ControlBarSchemeManager {
+#include <list>
+
+class INI
+{
 public:
-	static void parseAnimatingPart(INI *, void *, void *, const void *);
+	void initFromINI(void *, const void *);
+};
+
+class ControlBarSchemeImage
+{
+private:
+	unsigned char m_pad00[0x18];
+	int m_layer;
+
+	friend class ControlBarSchemeManager;
+};
+
+class ControlBarSchemeAnimation
+{
+public:
+	ControlBarSchemeAnimation();
+
+private:
+	unsigned char m_pad00[8];
+	ControlBarSchemeImage *m_animImage;
+	unsigned char m_pad0c[0x18];
+
+	friend class ControlBarSchemeManager;
+};
+
+class ControlBarScheme
+{
+private:
+	unsigned char m_pad00[0x158];
+	_STL::list<ControlBarSchemeImage *> m_layers[6];
+	_STL::list<ControlBarSchemeAnimation *> m_animations;
+
+	friend class ControlBarSchemeManager;
+};
+
+extern const unsigned char g_controlBarSchemeAnimationFieldParse[];
+
+class ControlBarSchemeManager
+{
+public:
+	static void __cdecl parseAnimatingPart(INI *, void *, void *, const void *);
 };
 
 // ?parseAnimatingPart@ControlBarSchemeManager@@SAXPAVINI@@PAX1PBX@Z
-__declspec(naked) void ControlBarSchemeManager::parseAnimatingPart(INI *, void *, void *, const void *)
+void __cdecl ControlBarSchemeManager::parseAnimatingPart(INI *ini,
+	void *instance, void *, const void *)
 {
-	__asm {
-		__emit 0x6a
-		__emit 0xff
-		__emit 0x68
-		__emit 0x9b
-		__emit 0x88
-		__emit 0x02
-		__emit 0x01
-		__emit 0x64
-		__emit 0xa1
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x50
-		__emit 0x64
-		__emit 0x89
-		__emit 0x25
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x51
-		__emit 0x53
-		__emit 0x56
-		__emit 0x57
-		__emit 0x6a
-		__emit 0x24
-		__emit 0xe8
-		__emit 0x10
-		__emit 0x35
-		__emit 0x3d
-		__emit 0x00
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x04
-		__emit 0x89
-		__emit 0x44
-		__emit 0x24
-		__emit 0x0c
-		__emit 0x85
-		__emit 0xc0
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x18
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x74
-		__emit 0x0b
-		__emit 0x8b
-		__emit 0xc8
-		__emit 0xe8
-		__emit 0x68
-		__emit 0x71
-		__emit 0xb8
-		__emit 0xff
-		__emit 0x8b
-		__emit 0xf8
-		__emit 0xeb
-		__emit 0x02
-		__emit 0x33
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x20
-		__emit 0x68
-		__emit 0xa8
-		__emit 0xd0
-		__emit 0x0f
-		__emit 0x01
-		__emit 0x57
-		__emit 0xc7
-		__emit 0x44
-		__emit 0x24
-		__emit 0x20
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xff
-		__emit 0xe8
-		__emit 0x49
-		__emit 0x36
-		__emit 0x3a
-		__emit 0x00
-		__emit 0x85
-		__emit 0xff
-		__emit 0x8b
-		__emit 0x5c
-		__emit 0x24
-		__emit 0x24
-		__emit 0x74
-		__emit 0x26
-		__emit 0x8b
-		__emit 0xb3
-		__emit 0x70
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x6a
-		__emit 0x0c
-		__emit 0xe8
-		__emit 0xd4
-		__emit 0xfa
-		__emit 0x37
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x48
-		__emit 0x08
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x04
-		__emit 0x85
-		__emit 0xc9
-		__emit 0x74
-		__emit 0x02
-		__emit 0x89
-		__emit 0x39
-		__emit 0x8b
-		__emit 0x4e
-		__emit 0x04
-		__emit 0x89
-		__emit 0x30
-		__emit 0x89
-		__emit 0x48
-		__emit 0x04
-		__emit 0x89
-		__emit 0x01
-		__emit 0x89
-		__emit 0x46
-		__emit 0x04
-		__emit 0x8b
-		__emit 0x77
-		__emit 0x08
-		__emit 0x85
-		__emit 0xf6
-		__emit 0x74
-		__emit 0x3d
-		__emit 0x8b
-		__emit 0x46
-		__emit 0x18
-		__emit 0x85
-		__emit 0xc0
-		__emit 0x7c
-		__emit 0x05
-		__emit 0x83
-		__emit 0xf8
-		__emit 0x06
-		__emit 0x7c
-		__emit 0x07
-		__emit 0xc7
-		__emit 0x46
-		__emit 0x18
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x8b
-		__emit 0x46
-		__emit 0x18
-		__emit 0x8b
-		__emit 0xbc
-		__emit 0x83
-		__emit 0x58
-		__emit 0x01
-		__emit 0x00
-		__emit 0x00
-		__emit 0x6a
-		__emit 0x0c
-		__emit 0xe8
-		__emit 0x90
-		__emit 0xfa
-		__emit 0x37
-		__emit 0x00
-		__emit 0x8d
-		__emit 0x48
-		__emit 0x08
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x04
-		__emit 0x85
-		__emit 0xc9
-		__emit 0x74
-		__emit 0x02
-		__emit 0x89
-		__emit 0x31
-		__emit 0x8b
-		__emit 0x4f
-		__emit 0x04
-		__emit 0x89
-		__emit 0x38
-		__emit 0x89
-		__emit 0x48
-		__emit 0x04
-		__emit 0x89
-		__emit 0x01
-		__emit 0x89
-		__emit 0x47
-		__emit 0x04
-		__emit 0x8b
-		__emit 0x4c
-		__emit 0x24
-		__emit 0x10
-		__emit 0x5f
-		__emit 0x5e
-		__emit 0x64
-		__emit 0x89
-		__emit 0x0d
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x00
-		__emit 0x5b
-		__emit 0x83
-		__emit 0xc4
-		__emit 0x10
-		__emit 0xc3
+	ControlBarSchemeAnimation *animation = new ControlBarSchemeAnimation;
+	ini->initFromINI(animation, g_controlBarSchemeAnimationFieldParse);
+
+	ControlBarScheme *scheme = static_cast<ControlBarScheme *>(instance);
+	if (animation != 0)
+		scheme->m_animations.push_back(animation);
+
+	ControlBarSchemeImage *image = animation->m_animImage;
+	if (image != 0)
+	{
+		if (image->m_layer < 0 || image->m_layer >= 6)
+			image->m_layer = 0;
+		scheme->m_layers[image->m_layer].push_back(image);
 	}
 }
