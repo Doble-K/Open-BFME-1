b2c5ccccd810442c69bc613ed5ef654ffad89393
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:13:10 -0400

    Open-BFME5 convert SiegeDeploySpecialPower constructor to C++

--- Code/GameEngine/Source/GameLogic/Object/SpecialPower/SiegeDeploySpecialPowerCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/SpecialPower/SiegeDeploySpecialPowerCtorThunk.cpp
@@ -1,124 +1,93 @@
 // cl: /DNDEBUG /MD /EHsc
 
+// Open-BFME5: SiegeDeploySpecialPower constructor.
+
 class Thing;
 class ModuleData;
 
-class SiegeDeploySpecialPower
+extern "C" char SiegeDeploySpecialPower_vtbl0;
+extern "C" char SiegeDeploySpecialPower_vtbl0C;
+extern "C" char SiegeDeploySpecialPower_vtbl10;
+extern "C" char SiegeDeploySpecialPower_vtbl20;
+extern "C" char SiegeDeploySpecialPower_vtbl24;
+
+class BehaviorModule
 {
 public:
-    SiegeDeploySpecialPower(Thing *, const ModuleData *);
+	virtual void behaviorModuleAnchor();
+private:
+	unsigned char m_data[8];
+};
+
+class SiegeDeploySpecialPowerIface1 { public: virtual void slot(); };
+class SiegeDeploySpecialPowerIface2
+{
+public:
+	virtual void slot();
+private:
+	unsigned char m_data[0x0c];
+};
+class SiegeDeploySpecialPowerIface3 { public: virtual void slot(); };
+class SiegeDeploySpecialPowerIface4 { public: virtual void slot(); };
+
+class SiegeDeploySpecialPowerBase : public BehaviorModule,
+	public SiegeDeploySpecialPowerIface1,
+	public SiegeDeploySpecialPowerIface2,
+	public SiegeDeploySpecialPowerIface3,
+	public SiegeDeploySpecialPowerIface4
+{
+public:
+	SiegeDeploySpecialPowerBase(Thing *, const ModuleData *);
+	virtual ~SiegeDeploySpecialPowerBase();
+private:
+	unsigned char m_data[0x10];
+};
+
+class __declspec(novtable) SiegeDeploySpecialPower : public SiegeDeploySpecialPowerBase
+{
+public:
+	SiegeDeploySpecialPower(Thing *, const ModuleData *);
+	virtual ~SiegeDeploySpecialPower();
+private:
+	volatile unsigned int m_f38;
+	volatile unsigned int m_f3c;
+	volatile unsigned int m_f40;
+	volatile unsigned int m_f44;
+	volatile unsigned int m_f48;
+	volatile unsigned int m_f4c;
+	volatile unsigned int m_f50;
+	volatile unsigned int m_f54;
+	volatile unsigned int m_f58;
+	volatile unsigned int m_f5c;
+	volatile bool m_f60;
+	volatile unsigned int m_f64;
+	volatile unsigned int m_f68;
+	volatile unsigned int m_f6c;
+	volatile bool m_f70;
 };
 
 // ??0SiegeDeploySpecialPower@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) SiegeDeploySpecialPower::SiegeDeploySpecialPower(Thing *, const ModuleData *)
+SiegeDeploySpecialPower::SiegeDeploySpecialPower(Thing *thing, const ModuleData *moduleData)
+	: SiegeDeploySpecialPowerBase(thing, moduleData)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0xec;
-        __emit 0x7a;
-        __emit 0xdb;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x38;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x3c;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x40;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x44;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x64;
-        __emit 0x75;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xa0;
-        __emit 0x74;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x94;
-        __emit 0x74;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x68;
-        __emit 0x74;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0xf0;
-        __emit 0x73;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x48;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x4c;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x50;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x54;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x58;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x5c;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x60;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x68;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x6c;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x70;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+	m_f38 = 0;
+	m_f3c = 0;
+	m_f40 = 0;
+	m_f44 = 0;
+	*(void *volatile *)this = &SiegeDeploySpecialPower_vtbl0;
+	*(void *volatile *)((char *)this + 0x0c) = &SiegeDeploySpecialPower_vtbl0C;
+	*(void *volatile *)((char *)this + 0x10) = &SiegeDeploySpecialPower_vtbl10;
+	*(void *volatile *)((char *)this + 0x20) = &SiegeDeploySpecialPower_vtbl20;
+	*(void *volatile *)((char *)this + 0x24) = &SiegeDeploySpecialPower_vtbl24;
+	m_f48 = 0;
+	m_f4c = 0;
+	m_f50 = 0;
+	m_f54 = 0;
+	m_f58 = 0;
+	m_f5c = 0;
+	m_f60 = false;
+	m_f64 = 0;
+	m_f68 = 0;
+	m_f6c = 0;
+	m_f70 = false;
 }
