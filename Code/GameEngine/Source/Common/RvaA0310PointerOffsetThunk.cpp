bf20bea432da29f0321706d18f5f087dea0e4b3c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 05:58:06 -0400

    Open-BFME5 convert RVA A0310 pointer offset to C++

---
+++ Code/GameEngine/Source/Common/RvaA0310PointerOffsetThunk.cpp
@@ -0,0 +1,4 @@
+char *rva_a0310_pointer_offset(char *value)
+{
+    return value + 16;
+}
--- Code/gen_asm/d_0007f6b0.asm
+++ Code/gen_asm/d_0007f6b0.asm
@@ -2643,12 +2643,6 @@ public ?d_000a0270@@YAXXZ
     db 8Bh, 0C1h, 8Bh, 4Ch, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
 ?d_000a0270@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x000A0310 size 8
-public ?d_000a0310@@YAXXZ
-?d_000a0310@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 10h, 0C3h
-?d_000a0310@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x000A0320 size 13
 public ?d_000a0320@@YAXXZ
 ?d_000a0320@@YAXXZ PROC
