fda9450d3b31ed46036ccb31a88c2a32fda0199b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 10:14:31 -0400

    Open-BFME5: convert RVA 13EA00 pointer advance to C++

--- Code/gen_asm/d_00138250.asm
+++ Code/gen_asm/d_00138250.asm
@@ -1048,12 +1048,6 @@ public ?d_0013e9e0@@YAXXZ
     db 81h, 21h, 0FFh, 0FFh, 0FFh, 1Fh, 0C3h
 ?d_0013e9e0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0013EA00 size 8
-public ?d_0013ea00@@YAXXZ
-?d_0013ea00@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 10h, 0C3h
-?d_0013ea00@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0013EAF0 size 27
 public ?d_0013eaf0@@YAXXZ
 ?d_0013eaf0@@YAXXZ PROC
