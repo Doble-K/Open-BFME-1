64dbc221a5b4650f107527107734c5a8a63b996e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 07:14:04 -0400

    Open-BFME5 convert RVA 1A1DB0 word getter to C++

---
+++ Code/GameEngine/Source/Common/Rva1A1DB0Offset482WordGetter.cpp
@@ -0,0 +1,12 @@
+struct Rva1A1DB0Offset482WordGetter
+{
+    unsigned char unused[0x482];
+    unsigned short value;
+
+    unsigned int get_value();
+};
+
+unsigned int Rva1A1DB0Offset482WordGetter::get_value()
+{
+    return value;
+}
--- Code/gen_asm/d_00188e80.asm
+++ Code/gen_asm/d_00188e80.asm
@@ -2608,12 +2608,6 @@ public ?d_001a1b00@@YAXXZ
     db 8Bh, 44h, 24h, 04h, 89h, 41h, 04h, 0C2h, 04h, 00h
 ?d_001a1b00@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x001A1DB0 size 8
-public ?d_001a1db0@@YAXXZ
-?d_001a1db0@@YAXXZ PROC
-    db 0Fh, 0B7h, 81h, 82h, 04h, 00h, 00h, 0C3h
-?d_001a1db0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x001A1DD0 size 7
 public ?d_001a1dd0@@YAXXZ
 ?d_001a1dd0@@YAXXZ PROC
