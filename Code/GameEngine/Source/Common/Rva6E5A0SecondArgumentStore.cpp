01ea9a50c647ab8f0800447eaf79b063d40e604f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 07:56:07 -0400

    Open-BFME5: convert RVA 6E5A0 second argument store to C++

---
+++ Code/GameEngine/Source/Common/Rva6E5A0SecondArgumentStore.cpp
@@ -0,0 +1,13 @@
+class Rva6E5A0SecondArgumentStore {
+public:
+    Rva6E5A0SecondArgumentStore *store(unsigned int ignored, unsigned int value);
+
+private:
+    unsigned int m_value;
+};
+
+Rva6E5A0SecondArgumentStore *Rva6E5A0SecondArgumentStore::store(unsigned int, unsigned int value)
+{
+    m_value = value;
+    return this;
+}
