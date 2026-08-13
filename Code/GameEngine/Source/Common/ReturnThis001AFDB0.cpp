08f2c0fc823ef77d2c4a288c742a9f7941c70dfa
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 12:46:43 -0400

    Open-BFME5 convert return-this member at 001AFDB0 to C++

---
+++ Code/GameEngine/Source/Common/ReturnThis001AFDB0.cpp
@@ -0,0 +1,12 @@
+// cl: /DNDEBUG /MD
+
+class AddressObject001AFDB0
+{
+public:
+	AddressObject001AFDB0 *returnThis(void *unused);
+};
+
+AddressObject001AFDB0 *AddressObject001AFDB0::returnThis(void *unused)
+{
+	return this;
+}
