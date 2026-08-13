318271e82843872e0ea74a08b909164e53bcee77
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:33:34 -0400

    Open-BFME5 convert respawn body module data to C++

--- Code/GameEngine/Source/GameLogic/Object/Behavior/RespawnBodyModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/RespawnBodyModuleDataCtorThunk.cpp
@@ -1,204 +1,47 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class RespawnBodyModuleData
+class RespawnBodyModuleDataBase
+{
+public:
+    RespawnBodyModuleDataBase();
+    virtual ~RespawnBodyModuleDataBase();
+
+private:
+    unsigned char m_data[0x58];
+};
+
+struct RespawnPolicy
+{
+    unsigned int values[6];
+};
+
+class RespawnPolicyMember
+{
+public:
+    RespawnPolicyMember();
+    ~RespawnPolicyMember();
+    void setPolicies(RespawnPolicy first, RespawnPolicy second);
+
+private:
+    unsigned int m_value;
+};
+
+extern RespawnPolicy g_defaultRespawnPolicy;
+
+class RespawnBodyModuleData : public RespawnBodyModuleDataBase
 {
 public:
     RespawnBodyModuleData();
+
+private:
+    RespawnPolicyMember m_policy;
+    bool m_enabled;
 };
 
 // ??0RespawnBodyModuleData@@QAE@XZ
-__declspec(naked) RespawnBodyModuleData::RespawnBodyModuleData()
+RespawnBodyModuleData::RespawnBodyModuleData()
+    : RespawnBodyModuleDataBase(), m_policy()
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0xd3;
-        __emit 0xc8;
-        __emit 0x00;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x53;
-        __emit 0x55;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x57;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0xe8;
-        __emit 0x23;
-        __emit 0x1e;
-        __emit 0xe2;
-        __emit 0xff;
-        __emit 0x8d;
-        __emit 0x7e;
-        __emit 0x5c;
-        __emit 0x8b;
-        __emit 0xcf;
-        __emit 0xc7;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xc8;
-        __emit 0x92;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0x6d;
-        __emit 0x2a;
-        __emit 0xe2;
-        __emit 0xff;
-        __emit 0xa1;
-        __emit 0xb8;
-        __emit 0xd8;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0x0d;
-        __emit 0xbc;
-        __emit 0xd8;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0x15;
-        __emit 0xc0;
-        __emit 0xd8;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0x1d;
-        __emit 0xc4;
-        __emit 0xd8;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xec;
-        __emit 0x18;
-        __emit 0x8b;
-        __emit 0xec;
-        __emit 0x89;
-        __emit 0x45;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x4d;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x55;
-        __emit 0x08;
-        __emit 0x89;
-        __emit 0x5d;
-        __emit 0x0c;
-        __emit 0x8b;
-        __emit 0x1d;
-        __emit 0xc8;
-        __emit 0xd8;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x5d;
-        __emit 0x10;
-        __emit 0x8b;
-        __emit 0x1d;
-        __emit 0xcc;
-        __emit 0xd8;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x5d;
-        __emit 0x14;
-        __emit 0x83;
-        __emit 0xec;
-        __emit 0x18;
-        __emit 0x8b;
-        __emit 0xec;
-        __emit 0x89;
-        __emit 0x45;
-        __emit 0x00;
-        __emit 0xa1;
-        __emit 0xc4;
-        __emit 0xd8;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x4d;
-        __emit 0x04;
-        __emit 0x8b;
-        __emit 0x0d;
-        __emit 0xc8;
-        __emit 0xd8;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x55;
-        __emit 0x08;
-        __emit 0x89;
-        __emit 0x45;
-        __emit 0x0c;
-        __emit 0x89;
-        __emit 0x4d;
-        __emit 0x10;
-        __emit 0x8b;
-        __emit 0xcf;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x4c;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x5d;
-        __emit 0x14;
-        __emit 0xe8;
-        __emit 0x6c;
-        __emit 0x89;
-        __emit 0xdf;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x5f;
-        __emit 0xc6;
-        __emit 0x46;
-        __emit 0x60;
-        __emit 0x01;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x5d;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x5b;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+    m_policy.setPolicies(g_defaultRespawnPolicy, g_defaultRespawnPolicy);
+    m_enabled = true;
 }
