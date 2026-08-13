73d2fb41f4ac0a26ef8b1391dbcc3b6acd5aa347
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:22:45 -0400

    Open-BFME5 convert EntEnragedUpdate constructor to C++

--- Code/GameEngine/Source/GameLogic/AI/EntEnragedUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/EntEnragedUpdateCtorThunk.cpp
@@ -1,196 +1,79 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class Thing;
-class ModuleData;
+// Open-BFME5: EntEnragedUpdate constructor with the retail module layout.
 
-class EntEnragedUpdate
+class Thing;
+class Object;
+
+class ModuleData
+{
+private:
+	unsigned char m_gap00[8];
+public:
+	unsigned int m_initialValue;
+};
+
+extern "C" char EntEnragedInitialValueData;
+int __cdecl entEnragedInitialValue(int, unsigned int, const void *, int);
+
+class PB_DeepBase
 {
 public:
-    EntEnragedUpdate(Thing *, const ModuleData *);
+	PB_DeepBase(Thing *, const ModuleData *);
+	virtual ~PB_DeepBase();
+
+protected:
+	const ModuleData *m_moduleData;
+	Object *m_object;
+};
+
+class BehaviorModuleInterface { public: virtual void slot(); };
+class UpdateModuleInterface { public: virtual void slot(); };
+
+class UpdateModule : public PB_DeepBase, public BehaviorModuleInterface,
+	public UpdateModuleInterface
+{
+public:
+	UpdateModule(Thing *thing, const ModuleData *moduleData)
+		: PB_DeepBase(thing, moduleData), m_field14(0),
+		  m_field18(-1), m_field1C(-1) {}
+
+private:
+	unsigned int m_field14;
+	int m_field18;
+	int m_field1C;
+};
+
+class EntEnragedUpdate : public UpdateModule
+{
+public:
+	EntEnragedUpdate(Thing *, const ModuleData *);
+	virtual ~EntEnragedUpdate();
+
+private:
+	int m_value;
+	unsigned int m_sleepTime;
+	unsigned int m_field28;
+	bool m_field2C;
+	bool m_field2D;
+	bool m_field2E;
+	bool m_field2F;
+	bool m_field30;
 };
 
 // ??0EntEnragedUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) EntEnragedUpdate::EntEnragedUpdate(Thing *, const ModuleData *)
+EntEnragedUpdate::EntEnragedUpdate(Thing *thing, const ModuleData *moduleData)
+	: UpdateModule(thing, moduleData)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x18;
-        __emit 0x19;
-        __emit 0x01;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x53;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0xe8;
-        __emit 0xb5;
-        __emit 0x56;
-        __emit 0xd8;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc8;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xdb;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x1c;
-        __emit 0x8b;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x6a;
-        __emit 0x4c;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xac;
-        __emit 0xe8;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xe8;
-        __emit 0xe7;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xdc;
-        __emit 0xe7;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x20;
-        __emit 0x8b;
-        __emit 0x50;
-        __emit 0x08;
-        __emit 0x68;
-        __emit 0x88;
-        __emit 0xe7;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x52;
-        __emit 0x53;
-        __emit 0x89;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x24;
-        __emit 0xe8;
-        __emit 0x34;
-        __emit 0x01;
-        __emit 0xd7;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4e;
-        __emit 0x20;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0x03;
-        __emit 0xc8;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x20;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x2c;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x2e;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x2f;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x2d;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x28;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x30;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0x3f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x5b;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+	const ModuleData *data = m_moduleData;
+	m_value = 0;
+	m_value += entEnragedInitialValue(0, data->m_initialValue,
+		&EntEnragedInitialValueData, 0x4c);
+	m_field2C = false;
+	m_field2E = false;
+	m_field2F = false;
+	m_field2D = false;
+	m_field28 = 0;
+	m_field30 = false;
+	m_sleepTime = 0x3fffffff;
 }
