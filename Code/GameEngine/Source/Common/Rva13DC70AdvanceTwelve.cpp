f342101624da091ef3404bd516e3b6a1102fb53e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 11:27:58 -0400

    Open-BFME5: convert RVA 13DC70 pointer advance to C++

---
+++ Code/GameEngine/Source/Common/Rva13DC70AdvanceTwelve.cpp
@@ -0,0 +1,4 @@
+unsigned char *rva_13dc70_advance_twelve(unsigned char *value)
+{
+    return value + 12;
+}
--- Code/gen_asm/d_00138250.asm
+++ Code/gen_asm/d_00138250.asm
@@ -421,12 +421,6 @@ public ?d_0013db70@@YAXXZ
     db 35h, 01h, 83h, 0C4h, 0Ch, 03h, 0C6h, 5Eh, 0C3h
 ?d_0013db70@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0013DC70 size 8
-public ?d_0013dc70@@YAXXZ
-?d_0013dc70@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 0Ch, 0C3h
-?d_0013dc70@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0013DC80 size 8
 public ?d_0013dc80@@YAXXZ
 ?d_0013dc80@@YAXXZ PROC
