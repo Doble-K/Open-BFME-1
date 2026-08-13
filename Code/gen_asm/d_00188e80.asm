e80e33a1df5f66758ef0cae58d31990585ae445c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 07:08:49 -0400

    Open-BFME5: convert RVA 192480 pointer offset to C++

--- Code/gen_asm/d_00188e80.asm
+++ Code/gen_asm/d_00188e80.asm
@@ -1238,12 +1238,6 @@ public ?d_00192450@@YAXXZ
     db 8Bh, 44h, 24h, 04h, 0C2h, 04h, 00h
 ?d_00192450@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x00192480 size 8
-public ?d_00192480@@YAXXZ
-?d_00192480@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 10h, 0C3h
-?d_00192480@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x00192490 size 8
 public ?d_00192490@@YAXXZ
 ?d_00192490@@YAXXZ PROC
