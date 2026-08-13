e5496f82a7ea0a51662585dca1a2baa068376b5a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 07:12:52 -0400

    Open-BFME5: convert RVA 192560 pointer offset to C++

---
+++ Code/GameEngine/Source/Common/Rva192560PointerOffset.cpp
@@ -0,0 +1,4 @@
+unsigned char *rva_192560_pointer_offset(unsigned char *value)
+{
+    return value + 4;
+}
--- Code/gen_asm/d_00188e80.asm
+++ Code/gen_asm/d_00188e80.asm
@@ -1244,12 +1244,6 @@ public ?d_001924b0@@YAXXZ
     db 8Bh, 0C1h, 8Bh, 4Ch, 24h, 04h, 8Bh, 11h, 89h, 10h, 0C2h, 04h, 00h
 ?d_001924b0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x00192560 size 8
-public ?d_00192560@@YAXXZ
-?d_00192560@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 04h, 0C3h
-?d_00192560@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x00192580 size 11
 public ?d_00192580@@YAXXZ
 ?d_00192580@@YAXXZ PROC
