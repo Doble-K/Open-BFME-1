c43c7ad4f7d3c69e5344ea6155fdef2854955100
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 08:42:18 -0400

    Open-BFME5: convert RVA 6E3B0 value copy to C++

---
+++ Code/GameEngine/Source/Common/Rva6E3B0ValueCopy.cpp
@@ -0,0 +1,13 @@
+class Rva6E3B0ValueCopy {
+public:
+    unsigned int *copy(unsigned int *output);
+
+private:
+    unsigned int m_value;
+};
+
+unsigned int *Rva6E3B0ValueCopy::copy(unsigned int *output)
+{
+    *output = m_value;
+    return output;
+}
--- Code/gen_asm/d_0005b6c0.asm
+++ Code/gen_asm/d_0005b6c0.asm
@@ -2954,12 +2954,6 @@ public ?d_0006e200@@YAXXZ
     db 83h, 0C4h, 04h, 8Bh, 0C6h, 5Eh, 0C2h, 04h, 00h
 ?d_0006e200@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0006E3B0 size 11
-public ?d_0006e3b0@@YAXXZ
-?d_0006e3b0@@YAXXZ PROC
-    db 8Bh, 09h, 8Bh, 44h, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
-?d_0006e3b0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0006E3C0 size 11
 public ?d_0006e3c0@@YAXXZ
 ?d_0006e3c0@@YAXXZ PROC
