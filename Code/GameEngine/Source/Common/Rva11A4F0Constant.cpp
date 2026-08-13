a7132c2ddf021e3b1a66fd843e1c685d8d8147b1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 06:19:45 -0400

    Open-BFME5 convert RVA 11A4F0 constant return to C++

---
+++ Code/GameEngine/Source/Common/Rva11A4F0Constant.cpp
@@ -0,0 +1,4 @@
+int rva_11a4f0_constant()
+{
+    return 3;
+}
--- Code/gen_asm/d_000f93a0.asm
+++ Code/gen_asm/d_000f93a0.asm
@@ -1979,12 +1979,6 @@ public ?d_0011a270@@YAXXZ
     db 8Bh, 0C1h, 0C7h, 00h, 00h, 00h, 00h, 00h, 0C3h
 ?d_0011a270@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0011A4F0 size 6
-public ?d_0011a4f0@@YAXXZ
-?d_0011a4f0@@YAXXZ PROC
-    db 0B8h, 03h, 00h, 00h, 00h, 0C3h
-?d_0011a4f0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0011A9E0 size 6
 public ?d_0011a9e0@@YAXXZ
 ?d_0011a9e0@@YAXXZ PROC
