023bf3dead442c6ebe050be749d9c431d62fc134
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 10:39:39 -0400

    Open-BFME5 convert RVA 121A60 constant to C++

---
+++ Code/GameEngine/Source/Common/Rva121A60ConstantSeventeen.cpp
@@ -0,0 +1,4 @@
+unsigned int rva_121a60_constant_seventeen()
+{
+    return 17;
+}
--- Code/gen_asm/d_000f93a0.asm
+++ Code/gen_asm/d_000f93a0.asm
@@ -1814,12 +1814,6 @@ public ?d_0011f660@@YAXXZ
     db 68h, 54h, 0ACh, 08h, 01h, 8Bh, 0CEh, 0E8h, 0A4h, 12h, 73h, 00h, 5Eh, 0C3h
 ?d_0011f660@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x00121A60 size 6
-public ?d_00121a60@@YAXXZ
-?d_00121a60@@YAXXZ PROC
-    db 0B8h, 11h, 00h, 00h, 00h, 0C3h
-?d_00121a60@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x00121C80 size 6
 public ?d_00121c80@@YAXXZ
 ?d_00121c80@@YAXXZ PROC
