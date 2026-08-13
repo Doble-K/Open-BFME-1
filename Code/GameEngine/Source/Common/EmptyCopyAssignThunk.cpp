79c359b8591123332613e45e93fe60fd074bc314
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:52:14 -0400

    Open-BFME5 convert alternate empty copy assignment to C++

--- Code/GameEngine/Source/Common/EmptyCopyAssignThunk.cpp
+++ Code/GameEngine/Source/Common/EmptyCopyAssignThunk.cpp
@@ -1,9 +1,15 @@
 struct EmptyCopyAssignThunk
 {
     EmptyCopyAssignThunk &assign(const EmptyCopyAssignThunk &other);
+    EmptyCopyAssignThunk &assignAlternate(const EmptyCopyAssignThunk &other);
 };
 
 EmptyCopyAssignThunk &EmptyCopyAssignThunk::assign(const EmptyCopyAssignThunk &)
+{
+    return *this;
+}
+
+EmptyCopyAssignThunk &EmptyCopyAssignThunk::assignAlternate(const EmptyCopyAssignThunk &)
 {
     return *this;
 }
--- Code/gen_asm/d_003519c0.asm
+++ Code/gen_asm/d_003519c0.asm
@@ -3097,12 +3097,6 @@ public ?d_0036c760@@YAXXZ
     db 8Bh, 0C1h, 0C2h, 04h, 00h
 ?d_0036c760@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0036C780 size 5
-public ?d_0036c780@@YAXXZ
-?d_0036c780@@YAXXZ PROC
-    db 8Bh, 0C1h, 0C2h, 04h, 00h
-?d_0036c780@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0036C9D0 size 27
 public ?d_0036c9d0@@YAXXZ
 ?d_0036c9d0@@YAXXZ PROC
