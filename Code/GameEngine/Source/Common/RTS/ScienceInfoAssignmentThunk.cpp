17c93bf6d06479a3a7db4ad33223853ac93abf0f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:46:44 -0400

    Open-BFME5 convert ScienceInfo assignment to C++

--- Code/GameEngine/Source/Common/RTS/ScienceInfoAssignmentThunk.cpp
+++ Code/GameEngine/Source/Common/RTS/ScienceInfoAssignmentThunk.cpp
@@ -1,94 +1,57 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class ScienceInfo
+// Open-BFME5: ScienceInfo assignment in the retail 0x30-byte layout.
+
+class ScienceInfoBase
 {
 public:
-    ScienceInfo &operator=(const ScienceInfo &);
+	ScienceInfoBase &operator=(const ScienceInfoBase &);
+private:
+	unsigned char m_data[0x0c];
 };
 
-__declspec(naked) ScienceInfo &ScienceInfo::operator=(const ScienceInfo &)
+class UnicodeString
 {
-    __asm {
-        __emit 0x56;
-        __emit 0x57;
-        __emit 0x8b;
-        __emit 0x7c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x57;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0xe8;
-        __emit 0x57;
-        __emit 0x01;
-        __emit 0xf6;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x47;
-        __emit 0x0c;
-        __emit 0x8d;
-        __emit 0x4f;
-        __emit 0x10;
-        __emit 0x51;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x10;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xe8;
-        __emit 0x50;
-        __emit 0xff;
-        __emit 0x79;
-        __emit 0x00;
-        __emit 0x8d;
-        __emit 0x57;
-        __emit 0x14;
-        __emit 0x52;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x14;
-        __emit 0xe8;
-        __emit 0x44;
-        __emit 0xff;
-        __emit 0x79;
-        __emit 0x00;
-        __emit 0x8d;
-        __emit 0x47;
-        __emit 0x18;
-        __emit 0x50;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x18;
-        __emit 0xe8;
-        __emit 0x8d;
-        __emit 0x1b;
-        __emit 0xf4;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4f;
-        __emit 0x24;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x24;
-        __emit 0x8b;
-        __emit 0x57;
-        __emit 0x28;
-        __emit 0x89;
-        __emit 0x56;
-        __emit 0x28;
-        __emit 0x8a;
-        __emit 0x47;
-        __emit 0x2c;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x2c;
-        __emit 0x5f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x04;
-        __emit 0x00;
-    }
+public:
+	UnicodeString &operator=(const UnicodeString &);
+private:
+	void *m_data;
+};
+
+class ScienceVecStorage
+{
+public:
+	ScienceVecStorage &operator=(const ScienceVecStorage &);
+private:
+	void *m_first;
+	void *m_last;
+	void *m_end;
+};
+
+class ScienceInfo : public ScienceInfoBase
+{
+public:
+	ScienceInfo &operator=(const ScienceInfo &);
+private:
+	unsigned int m_science;
+	UnicodeString m_name;
+	UnicodeString m_description;
+	ScienceVecStorage m_prerequisiteSciences;
+	int m_sciencePurchasePointCost;
+	int m_sciencePurchasePointCostMP;
+	bool m_grantable;
+};
+
+// ??4ScienceInfo@@QAEAAV0@ABV0@@Z
+ScienceInfo &ScienceInfo::operator=(const ScienceInfo &other)
+{
+	ScienceInfoBase::operator=(other);
+	m_science = other.m_science;
+	m_name = other.m_name;
+	m_description = other.m_description;
+	m_prerequisiteSciences = other.m_prerequisiteSciences;
+	m_sciencePurchasePointCost = other.m_sciencePurchasePointCost;
+	m_sciencePurchasePointCostMP = other.m_sciencePurchasePointCostMP;
+	m_grantable = other.m_grantable;
+	return *this;
 }
