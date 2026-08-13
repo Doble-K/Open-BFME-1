4301e34c021503e3d47c83f5e71d3a212f56bc87
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 08:30:42 -0400

    Open-BFME5 convert RVA 18C290 getter to C++

---
+++ Code/GameEngine/Source/Common/Rva18C290Offset148Getter.cpp
@@ -0,0 +1,12 @@
+struct Rva18C290Offset148Getter
+{
+    unsigned char unused[148];
+    unsigned int value;
+
+    unsigned int get_value();
+};
+
+unsigned int Rva18C290Offset148Getter::get_value()
+{
+    return value;
+}
--- Code/gen_asm/d_00188e80.asm
+++ Code/gen_asm/d_00188e80.asm
@@ -236,12 +236,6 @@ public ?d_0018b440@@YAXXZ
     db 56h, 8Bh, 32h, 89h, 30h, 83h, 0C0h, 04h, 49h, 75h, 0F6h, 5Eh, 0C3h
 ?d_0018b440@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0018C290 size 7
-public ?d_0018c290@@YAXXZ
-?d_0018c290@@YAXXZ PROC
-    db 8Bh, 81h, 94h, 00h, 00h, 00h, 0C3h
-?d_0018c290@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0018C300 size 98
 public ?d_0018c300@@YAXXZ
 ?d_0018c300@@YAXXZ PROC
