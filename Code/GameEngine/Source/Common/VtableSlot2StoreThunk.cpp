a6a2fd0b2b7825c7ce54624efa9824685af54a9b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 23:45:09 -0400

    Open-BFME5 convert thirteenth vtable slot return to C++

--- Code/GameEngine/Source/Common/VtableSlot2StoreThunk.cpp
+++ Code/GameEngine/Source/Common/VtableSlot2StoreThunk.cpp
@@ -19,6 +19,7 @@ struct VtableSlot2StoreThunk
     VtableSlot2Result getTenth() const;
     VtableSlot2Result getEleventh() const;
     VtableSlot2Result getTwelfth() const;
+    VtableSlot2Result getThirteenth() const;
 };
 
 VtableSlot2Result VtableSlot2StoreThunk::get() const
@@ -88,6 +89,12 @@ VtableSlot2Result VtableSlot2StoreThunk::getEleventh() const
 }
 
 VtableSlot2Result VtableSlot2StoreThunk::getTwelfth() const
+{
+    VtableSlot2Result result = { vtable[2] };
+    return result;
+}
+
+VtableSlot2Result VtableSlot2StoreThunk::getThirteenth() const
 {
     VtableSlot2Result result = { vtable[2] };
     return result;
