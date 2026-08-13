db05d8748c94558cd78c9300418f186763781358
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 23:24:45 -0400

    Open-BFME5 convert third pointer offset sixteen helper to C++

--- Code/GameEngine/Source/Common/PointerOffset16Thunk.cpp
+++ Code/GameEngine/Source/Common/PointerOffset16Thunk.cpp
@@ -7,3 +7,8 @@ void *pointerOffset16Alternate(void *value)
 {
     return static_cast<char *>(value) + 16;
 }
+
+void *pointerOffset16Third(void *value)
+{
+    return static_cast<char *>(value) + 16;
+}
--- Code/gen_asm/d_003519c0.asm
+++ Code/gen_asm/d_003519c0.asm
@@ -3215,12 +3215,6 @@ public ?d_0036d660@@YAXXZ
     db 00h
 ?d_0036d660@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0036D690 size 8
-public ?d_0036d690@@YAXXZ
-?d_0036d690@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 10h, 0C3h
-?d_0036d690@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0036D6A0 size 8
 public ?d_0036d6a0@@YAXXZ
 ?d_0036d6a0@@YAXXZ PROC
