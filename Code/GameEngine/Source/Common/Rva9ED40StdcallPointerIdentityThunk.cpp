683be50fbcf4cecab2039aed61d6a072584a30fa
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 06:38:19 -0400

    Open-BFME5 convert RVA 9ED40 stdcall identity to C++

---
+++ Code/GameEngine/Source/Common/Rva9ED40StdcallPointerIdentityThunk.cpp
@@ -0,0 +1,4 @@
+void *__stdcall rva_9ed40_pointer_identity(void *value)
+{
+    return value;
+}
--- Code/gen_asm/d_0007f6b0.asm
+++ Code/gen_asm/d_0007f6b0.asm
@@ -2311,12 +2311,6 @@ public ?d_0009ec20@@YAXXZ
     db 8Bh, 01h, 83h, 0C0h, 08h, 0C3h
 ?d_0009ec20@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0009ED40 size 7
-public ?d_0009ed40@@YAXXZ
-?d_0009ed40@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 0C2h, 04h, 00h
-?d_0009ed40@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0009ED50 size 11
 public ?d_0009ed50@@YAXXZ
 ?d_0009ed50@@YAXXZ PROC
