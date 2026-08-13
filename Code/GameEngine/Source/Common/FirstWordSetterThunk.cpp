bef634b7f2c5761540dd215406698a2fb42122d5
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 03:39:26 -0400

    Open-BFME5 convert first-word assignment thunk to C++

--- Code/GameEngine/Source/Common/FirstWordSetterThunk.cpp
+++ Code/GameEngine/Source/Common/FirstWordSetterThunk.cpp
@@ -3,10 +3,16 @@ struct FirstWordSetterThunk
     unsigned long value;
 
     FirstWordSetterThunk *set(unsigned long newValue);
+    void assign(unsigned long newValue);
 };
 
 FirstWordSetterThunk *FirstWordSetterThunk::set(unsigned long newValue)
 {
     value = newValue;
     return this;
+}
+
+void FirstWordSetterThunk::assign(unsigned long newValue)
+{
+    value = newValue;
 }
