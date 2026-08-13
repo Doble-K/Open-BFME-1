ffe4ec81ca67b79a20807b955de753e66bc1afbb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 08:50:24 -0400

    Open-BFME5 convert RVA A3160 getter to C++

---
+++ Code/GameEngine/Source/Common/RvaA3160Offset1088Getter.cpp
@@ -0,0 +1,12 @@
+struct RvaA3160Offset1088Getter
+{
+    unsigned char unused[1088];
+    unsigned int value;
+
+    unsigned int get_value();
+};
+
+unsigned int RvaA3160Offset1088Getter::get_value()
+{
+    return value;
+}
--- Code/gen_asm/d_0007f6b0.asm
+++ Code/gen_asm/d_0007f6b0.asm
@@ -2736,12 +2736,6 @@ public ?d_000a1f60@@YAXXZ
     db 0Ch, 52h, 8Bh, 54h, 24h, 10h, 52h, 50h, 0E8h, 7Ah, 1Eh, 0F7h, 0FFh, 0C2h, 04h, 00h
 ?d_000a1f60@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x000A3160 size 7
-public ?d_000a3160@@YAXXZ
-?d_000a3160@@YAXXZ PROC
-    db 8Bh, 81h, 40h, 04h, 00h, 00h, 0C3h
-?d_000a3160@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x000A3170 size 61
 public ?d_000a3170@@YAXXZ
 ?d_000a3170@@YAXXZ PROC
