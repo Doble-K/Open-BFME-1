aaf931938a60b686f534a51f2ad0474b43fafce6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 11:57:43 -0400

    Open-BFME5: convert RVA 13E670 identity to C++

---
+++ Code/GameEngine/Source/Common/Rva13E670Identity.cpp
@@ -0,0 +1,10 @@
+class Rva13E670Identity
+{
+public:
+    unsigned int identity(unsigned int value);
+};
+
+unsigned int Rva13E670Identity::identity(unsigned int value)
+{
+    return value;
+}
--- Code/gen_asm/d_00138250.asm
+++ Code/gen_asm/d_00138250.asm
@@ -429,12 +429,6 @@ public ?d_0013e440@@YAXXZ
     db 11h, 74h, 00h, 0C2h, 14h, 00h
 ?d_0013e440@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0013E670 size 7
-public ?d_0013e670@@YAXXZ
-?d_0013e670@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 0C2h, 04h, 00h
-?d_0013e670@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0013E690 size 7
 public ?d_0013e690@@YAXXZ
 ?d_0013e690@@YAXXZ PROC
