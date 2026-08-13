cbfda50b7521c931ddc3df3d772b264f5ff5718a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:19:48 -0400

    Open-BFME5 convert delayed Lua event constructor to C++

--- Code/GameEngine/Source/GameLogic/ScriptEngine/DelayedLuaEventUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/ScriptEngine/DelayedLuaEventUpdateCtorThunk.cpp
@@ -2,177 +2,87 @@
 
 class Thing;
 class ModuleData;
+class Object;
 
-class DelayedLuaEventUpdate
+enum UpdateSleepTime
+{
+    UPDATE_SLEEP_DUMMY
+};
+
+class DelayedLuaEventUpdateBase
+{
+public:
+    DelayedLuaEventUpdateBase(Thing *, const ModuleData *);
+    virtual ~DelayedLuaEventUpdateBase();
+
+protected:
+    void *m_f04;
+    Object *m_object;
+};
+
+class DelayedLuaEventUpdateInterface1
+{
+public:
+    virtual void slot();
+};
+
+class DelayedLuaEventUpdateInterface2
+{
+public:
+    virtual void slot();
+};
+
+class UpdateModule : public DelayedLuaEventUpdateBase,
+                     public DelayedLuaEventUpdateInterface1,
+                     public DelayedLuaEventUpdateInterface2
+{
+public:
+    UpdateModule(Thing *thing, const ModuleData *moduleData)
+        : DelayedLuaEventUpdateBase(thing, moduleData),
+          m_nextCallFrameAndPhase(0), m_indexInLogic(-1), m_updateState(-1)
+    {
+    }
+
+protected:
+    void setWakeFrameViaThunk(Object *, UpdateSleepTime);
+    Object *getObject() const { return m_object; }
+
+private:
+    unsigned int m_nextCallFrameAndPhase;
+    int m_indexInLogic;
+    int m_updateState;
+};
+
+class DelayedLuaEventList
+{
+public:
+    DelayedLuaEventList();
+    ~DelayedLuaEventList();
+
+private:
+    unsigned char m_data[0x4c];
+};
+
+class DelayedLuaEventUpdate : public UpdateModule
 {
 public:
     DelayedLuaEventUpdate(Thing *, const ModuleData *);
+
+private:
+    unsigned int m_f20;
+    DelayedLuaEventList m_events;
+    unsigned int m_f70;
+    bool m_f74;
+    bool m_f75;
 };
 
 // ??0DelayedLuaEventUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) DelayedLuaEventUpdate::DelayedLuaEventUpdate(Thing *, const ModuleData *)
+DelayedLuaEventUpdate::DelayedLuaEventUpdate(Thing *thing, const ModuleData *moduleData)
+    : UpdateModule(thing, moduleData), m_events()
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x73;
-        __emit 0x14;
-        __emit 0x01;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x53;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0xe8;
-        __emit 0xc5;
-        __emit 0xb1;
-        __emit 0xd8;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc8;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xdb;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x1c;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x24;
-        __emit 0x89;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xb4;
-        __emit 0xd3;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xf0;
-        __emit 0xd2;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xe0;
-        __emit 0xd2;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0x9e;
-        __emit 0xdd;
-        __emit 0xd8;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x56;
-        __emit 0x08;
-        __emit 0x68;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0xff;
-        __emit 0x3f;
-        __emit 0x52;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0x6a;
-        __emit 0x98;
-        __emit 0xd8;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x20;
-        __emit 0x89;
-        __emit 0x5e;
-        __emit 0x70;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x74;
-        __emit 0x88;
-        __emit 0x5e;
-        __emit 0x75;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x5b;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+    setWakeFrameViaThunk(getObject(), (UpdateSleepTime)0x3fffffff);
+    m_f20 = 0;
+    m_f70 = 0;
+    m_f74 = false;
+    m_f75 = false;
 }
