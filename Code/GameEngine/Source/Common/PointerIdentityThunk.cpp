d5f3a2da344d4a923b44f7d0785f45e3ef57926d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:43:47 -0400

    Open-BFME5 convert pointer identity helper to C++

---
+++ Code/GameEngine/Source/Common/PointerIdentityThunk.cpp
@@ -0,0 +1,4 @@
+void *__stdcall pointerIdentity(void *value)
+{
+    return value;
+}
--- Code/gen_asm/d_003519c0.asm
+++ Code/gen_asm/d_003519c0.asm
@@ -3109,12 +3109,6 @@ public ?d_0036c780@@YAXXZ
     db 8Bh, 0C1h, 0C2h, 04h, 00h
 ?d_0036c780@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0036C8B0 size 7
-public ?d_0036c8b0@@YAXXZ
-?d_0036c8b0@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 0C2h, 04h, 00h
-?d_0036c8b0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0036C8C0 size 7
 public ?d_0036c8c0@@YAXXZ
 ?d_0036c8c0@@YAXXZ PROC
