a68ab84154dceff1a3912ceb57024f96d9f7b780
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 05:51:56 -0400

    Open-BFME5 convert RVA C4220 false leaf to C++

---
+++ Code/GameEngine/Source/Common/RvaC4220FalseWithThreeArgumentsThunk.cpp
@@ -0,0 +1,9 @@
+struct RvaC4220FalseWithThreeArgumentsThunk
+{
+    bool reject(int first, int second, int third);
+};
+
+bool RvaC4220FalseWithThreeArgumentsThunk::reject(int, int, int)
+{
+    return false;
+}
