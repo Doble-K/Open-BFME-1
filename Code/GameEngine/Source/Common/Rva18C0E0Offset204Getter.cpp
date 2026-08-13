01c0410822f01b7ac7972f4fe707afa11cec547b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 07:59:17 -0400

    Open-BFME5 convert RVA 18C0E0 offset getter to C++

---
+++ Code/GameEngine/Source/Common/Rva18C0E0Offset204Getter.cpp
@@ -0,0 +1,12 @@
+struct Rva18C0E0Offset204Getter
+{
+    unsigned char unused[0x204];
+    unsigned int value;
+
+    unsigned int get_value();
+};
+
+unsigned int Rva18C0E0Offset204Getter::get_value()
+{
+    return value;
+}
