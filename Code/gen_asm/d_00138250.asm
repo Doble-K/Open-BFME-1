49e24e44a1482ebd14ec8df004e5bf8e7aa46fe9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 11:38:30 -0400

    Open-BFME5: convert RVA 13DEB0 pointer advance to C++

--- Code/gen_asm/d_00138250.asm
+++ Code/gen_asm/d_00138250.asm
@@ -421,12 +421,6 @@ public ?d_0013db70@@YAXXZ
     db 35h, 01h, 83h, 0C4h, 0Ch, 03h, 0C6h, 5Eh, 0C3h
 ?d_0013db70@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0013DEB0 size 8
-public ?d_0013deb0@@YAXXZ
-?d_0013deb0@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 10h, 0C3h
-?d_0013deb0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0013DEC0 size 8
 public ?d_0013dec0@@YAXXZ
 ?d_0013dec0@@YAXXZ PROC
