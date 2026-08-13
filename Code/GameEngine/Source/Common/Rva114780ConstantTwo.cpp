3f3558271f36c1b6ce90f1c6fb3e37efbf584d6a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 09:42:31 -0400

    Open-BFME5 convert RVA 114780 constant to C++

---
+++ Code/GameEngine/Source/Common/Rva114780ConstantTwo.cpp
@@ -0,0 +1,4 @@
+unsigned int rva_114780_constant_two()
+{
+    return 2;
+}
--- Code/gen_asm/d_000f93a0.asm
+++ Code/gen_asm/d_000f93a0.asm
@@ -1668,12 +1668,6 @@ public ?d_00114280@@YAXXZ
     db 0B8h, 85h, 00h, 00h, 00h, 0C3h
 ?d_00114280@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x00114780 size 6
-public ?d_00114780@@YAXXZ
-?d_00114780@@YAXXZ PROC
-    db 0B8h, 02h, 00h, 00h, 00h, 0C3h
-?d_00114780@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x001148A0 size 6
 public ?d_001148a0@@YAXXZ
 ?d_001148a0@@YAXXZ PROC
