7d567fec7dec417d26f19ad092fe63cb16ee3459
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 23:39:20 -0400

    Open-BFME5 convert alternate ordered tree lookup to C++

--- Code/GameEngine/Source/Common/OrderedTreeFindThunk.cpp
+++ Code/GameEngine/Source/Common/OrderedTreeFindThunk.cpp
@@ -12,9 +12,31 @@ struct OrderedTreeFindThunk
     OrderedTreeNode *header;
 
     OrderedTreeNode *find(const long *key);
+    OrderedTreeNode *findAlternate(const long *key);
 };
 
 OrderedTreeNode *OrderedTreeFindThunk::find(const long *key)
+{
+    OrderedTreeNode *node = header->root;
+    OrderedTreeNode *candidate = header;
+
+    while (node != 0) {
+        if (!(node->key < *key)) {
+            candidate = node;
+            node = node->left;
+        } else {
+            node = node->right;
+        }
+    }
+
+    if (candidate == header || *key < candidate->key) {
+        return header;
+    }
+
+    return candidate;
+}
+
+OrderedTreeNode *OrderedTreeFindThunk::findAlternate(const long *key)
 {
     OrderedTreeNode *node = header->root;
     OrderedTreeNode *candidate = header;
