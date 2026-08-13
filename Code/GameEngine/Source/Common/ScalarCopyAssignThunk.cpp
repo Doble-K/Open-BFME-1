aea7b569f809e9f73777c5a2618db4584f0c39d6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:49:40 -0400

    Open-BFME5 convert alternate scalar copy assignment to C++

--- Code/GameEngine/Source/Common/ScalarCopyAssignThunk.cpp
+++ Code/GameEngine/Source/Common/ScalarCopyAssignThunk.cpp
@@ -3,9 +3,16 @@ struct ScalarCopyAssignThunk
     unsigned long value;
 
     ScalarCopyAssignThunk &assign(const ScalarCopyAssignThunk &other);
+    ScalarCopyAssignThunk &assignAlternate(const ScalarCopyAssignThunk &other);
 };
 
 ScalarCopyAssignThunk &ScalarCopyAssignThunk::assign(const ScalarCopyAssignThunk &other)
+{
+    value = other.value;
+    return *this;
+}
+
+ScalarCopyAssignThunk &ScalarCopyAssignThunk::assignAlternate(const ScalarCopyAssignThunk &other)
 {
     value = other.value;
     return *this;
