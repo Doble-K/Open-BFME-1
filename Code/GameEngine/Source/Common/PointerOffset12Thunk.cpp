163fb70d2b1802d0f75dbfca90f29ee049c35ddc
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:40:27 -0400

    Open-BFME5 convert third offset twelve helper to C++

--- Code/GameEngine/Source/Common/PointerOffset12Thunk.cpp
+++ Code/GameEngine/Source/Common/PointerOffset12Thunk.cpp
@@ -7,3 +7,8 @@ void *pointerOffset12Alternate(void *value)
 {
     return static_cast<char *>(value) + 12;
 }
+
+void *pointerOffset12Third(void *value)
+{
+    return static_cast<char *>(value) + 12;
+}
