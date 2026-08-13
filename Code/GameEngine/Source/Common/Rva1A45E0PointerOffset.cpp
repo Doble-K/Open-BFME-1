675da52d28d71ca7a3957df3f5c882a29f9940b2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 07:19:09 -0400

    Open-BFME5 convert RVA 1A45E0 pointer offset to C++

---
+++ Code/GameEngine/Source/Common/Rva1A45E0PointerOffset.cpp
@@ -0,0 +1,4 @@
+unsigned char *rva_1a45e0_pointer_offset(unsigned char *value)
+{
+    return value + 8;
+}
--- Code/gen_asm/d_00188e80.asm
+++ Code/gen_asm/d_00188e80.asm
@@ -2827,12 +2827,6 @@ public ?d_001a4590@@YAXXZ
     db 00h
 ?d_001a4590@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x001A45E0 size 8
-public ?d_001a45e0@@YAXXZ
-?d_001a45e0@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 83h, 0C0h, 08h, 0C3h
-?d_001a45e0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x001A45F0 size 8
 public ?d_001a45f0@@YAXXZ
 ?d_001a45f0@@YAXXZ PROC
