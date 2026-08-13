05c8a794c11570d381f8c18b45f44d11216487ac
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 10:03:42 -0400

    Open-BFME5: convert RVA 13E970 argument store to C++

--- Code/gen_asm/d_00138250.asm
+++ Code/gen_asm/d_00138250.asm
@@ -1083,12 +1083,6 @@ public ?d_0013e920@@YAXXZ
     db 0C7h, 01h, 00h, 00h, 00h, 00h, 0C3h
 ?d_0013e920@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0013E970 size 11
-public ?d_0013e970@@YAXXZ
-?d_0013e970@@YAXXZ PROC
-    db 8Bh, 0C1h, 8Bh, 4Ch, 24h, 08h, 89h, 08h, 0C2h, 08h, 00h
-?d_0013e970@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0013E990 size 27
 public ?d_0013e990@@YAXXZ
 ?d_0013e990@@YAXXZ PROC
