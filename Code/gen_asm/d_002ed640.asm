bbe546d1ae74702240b77d99734b5c540a6bb3b7
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 02:30:39 -0400

    Open-BFME5 convert ninety-first vtable slot return to C++

--- Code/gen_asm/d_002ed640.asm
+++ Code/gen_asm/d_002ed640.asm
@@ -2359,12 +2359,6 @@ public ?d_0033b3c0@@YAXXZ
     db 8Bh, 09h, 8Bh, 44h, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
 ?d_0033b3c0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0033B3D0 size 14
-public ?d_0033b3d0@@YAXXZ
-?d_0033b3d0@@YAXXZ PROC
-    db 8Bh, 01h, 8Bh, 48h, 08h, 8Bh, 44h, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
-?d_0033b3d0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0033B3F0 size 11
 public ?d_0033b3f0@@YAXXZ
 ?d_0033b3f0@@YAXXZ PROC
