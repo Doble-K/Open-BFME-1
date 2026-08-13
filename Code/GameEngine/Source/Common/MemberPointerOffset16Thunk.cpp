073d66f0e6daa9be7fcf3c4a509436dd5172b878
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 23:06:56 -0400

    Open-BFME5 convert alternate member pointer offset to C++

--- Code/GameEngine/Source/Common/MemberPointerOffset16Thunk.cpp
+++ Code/GameEngine/Source/Common/MemberPointerOffset16Thunk.cpp
@@ -3,9 +3,15 @@ struct MemberPointerOffset16Thunk
     char *value;
 
     void *get() const;
+    void *getAlternate() const;
 };
 
 void *MemberPointerOffset16Thunk::get() const
+{
+    return value + 16;
+}
+
+void *MemberPointerOffset16Thunk::getAlternate() const
 {
     return value + 16;
 }
--- Code/gen_asm/d_003519c0.asm
+++ Code/gen_asm/d_003519c0.asm
@@ -3153,12 +3153,6 @@ public ?d_0036cfd0@@YAXXZ
     db 0C3h
 ?d_0036cfd0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0036D110 size 6
-public ?d_0036d110@@YAXXZ
-?d_0036d110@@YAXXZ PROC
-    db 8Bh, 01h, 83h, 0C0h, 10h, 0C3h
-?d_0036d110@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0036D120 size 6
 public ?d_0036d120@@YAXXZ
 ?d_0036d120@@YAXXZ PROC
