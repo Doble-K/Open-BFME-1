376324abe254188ae055cf134f7fb321d9c55440
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-12 22:42:40 -0400

    Open-BFME5 convert alternate scalar assignment to C++

--- Code/GameEngine/Source/Common/ScalarAssignThunk.cpp
+++ Code/GameEngine/Source/Common/ScalarAssignThunk.cpp
@@ -3,9 +3,16 @@ struct ScalarAssignThunk
     unsigned long value;
 
     ScalarAssignThunk &assign(unsigned long newValue);
+    ScalarAssignThunk &assignAlternate(unsigned long newValue);
 };
 
 ScalarAssignThunk &ScalarAssignThunk::assign(unsigned long newValue)
+{
+    value = newValue;
+    return *this;
+}
+
+ScalarAssignThunk &ScalarAssignThunk::assignAlternate(unsigned long newValue)
 {
     value = newValue;
     return *this;
