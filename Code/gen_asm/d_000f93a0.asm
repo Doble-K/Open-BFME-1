1d0e718fb13f5f124f1cc4f61a47b2b269e65e94
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 09:32:48 -0400

    Open-BFME5: convert RVA 124CF0 value copy to C++

--- Code/gen_asm/d_000f93a0.asm
+++ Code/gen_asm/d_000f93a0.asm
@@ -2689,12 +2689,6 @@ public ?d_00124ce0@@YAXXZ
     db 8Bh, 44h, 24h, 04h, 0C2h, 04h, 00h
 ?d_00124ce0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x00124CF0 size 11
-public ?d_00124cf0@@YAXXZ
-?d_00124cf0@@YAXXZ PROC
-    db 8Bh, 09h, 8Bh, 44h, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
-?d_00124cf0@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x00124D00 size 45
 public ?d_00124d00@@YAXXZ
 ?d_00124d00@@YAXXZ PROC
