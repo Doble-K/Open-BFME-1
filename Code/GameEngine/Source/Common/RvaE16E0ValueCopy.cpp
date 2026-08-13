a76b657bcd54e8b8537d48e802e475165781df45
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 09:26:12 -0400

    Open-BFME5: convert RVA E16E0 value copy to C++

---
+++ Code/GameEngine/Source/Common/RvaE16E0ValueCopy.cpp
@@ -0,0 +1,13 @@
+class RvaE16E0ValueCopy {
+public:
+    unsigned int *copy(unsigned int *output);
+
+private:
+    unsigned int m_value;
+};
+
+unsigned int *RvaE16E0ValueCopy::copy(unsigned int *output)
+{
+    *output = m_value;
+    return output;
+}
