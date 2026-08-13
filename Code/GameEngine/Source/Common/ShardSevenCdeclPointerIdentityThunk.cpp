2beff7948ffca629b326466f821ecb041317434f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 05:02:09 -0400

    Open-BFME5 convert shard-seven cdecl pointer identity to C++

---
+++ Code/GameEngine/Source/Common/ShardSevenCdeclPointerIdentityThunk.cpp
@@ -0,0 +1,4 @@
+void *shard_seven_pointer_identity(void *value)
+{
+    return value;
+}
--- Code/gen_asm/d_0007f6b0.asm
+++ Code/gen_asm/d_0007f6b0.asm
@@ -20,12 +20,6 @@ public ?d_0007f6e0@@YAXXZ
     db 8Bh, 0C1h, 8Bh, 4Ch, 24h, 04h, 89h, 08h, 0C2h, 04h, 00h
 ?d_0007f6e0@@YAXXZ ENDP
 
-; ghidra: bounds-high  retail @ 0x0007F700 size 5
-public ?d_0007f700@@YAXXZ
-?d_0007f700@@YAXXZ PROC
-    db 8Bh, 44h, 24h, 04h, 0C3h
-?d_0007f700@@YAXXZ ENDP
-
 ; ghidra: bounds-high  retail @ 0x0007F710 size 5
 public ?d_0007f710@@YAXXZ
 ?d_0007f710@@YAXXZ PROC
