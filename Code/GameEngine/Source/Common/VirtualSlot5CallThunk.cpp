c17f6a44afe8d2791fa89d29fcbd9e3b9c18729b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-13 03:49:05 -0400

    Open-BFME5 convert sixth integer zero constructor to C++

--- Code/GameEngine/Source/Common/VirtualSlot5CallThunk.cpp
+++ Code/GameEngine/Source/Common/VirtualSlot5CallThunk.cpp
@@ -1941,6 +1941,18 @@ SingleIntegerZeroConstructorFifthThunk::SingleIntegerZeroConstructorFifthThunk()
 {
 }
 
+struct SingleIntegerZeroConstructorSixthThunk
+{
+    int value;
+
+    SingleIntegerZeroConstructorSixthThunk();
+};
+
+SingleIntegerZeroConstructorSixthThunk::SingleIntegerZeroConstructorSixthThunk() :
+    value(0)
+{
+}
+
 struct FluentNoOpThunk
 {
     FluentNoOpThunk *ignore(int unused);
