5729ccae8358761a97f3624ad091c13213f7332e
dginovker <danielginovker@gmail.com>
2026-08-19 01:18:20 +0900

    Convert AIMoveAwayFromRepulsors onEnter to C++

--- Code/GameEngine/Source/GameLogic/AI/AIMoveAwayFromRepulsorsState_onEnter_Thunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/AIMoveAwayFromRepulsorsState_onEnter_Thunk.cpp
@@ -1,213 +1,243 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
 
-enum StateReturnType {};
-class AIMoveAwayFromRepulsorsState
+typedef bool Bool;
+typedef unsigned int UnsignedInt;
+
+enum StateReturnType
+{
+	STATE_FAILURE = -2
+};
+
+enum ObjectID
+{
+	INVALID_OBJECT_ID
+};
+
+enum LocomotorSetType
+{
+	LOCOMOTORSET_PANIC = 4
+};
+
+enum ModelConditionFlagType
+{
+	MODELCONDITION_PANICKING = 12
+};
+
+class ModelConditionFlags
+{
+public:
+	Bool test(ModelConditionFlagType condition) const
+	{
+		return (m_bits & (1u << condition)) != 0;
+	}
+
+	void set(ModelConditionFlagType condition)
+	{
+		m_bits |= 1u << condition;
+	}
+
+private:
+	UnsignedInt m_bits;
+};
+
+struct Coord3D
+{
+	float x;
+	float y;
+	float z;
+};
+
+class Object;
+
+#define BFME_VTABLE_SLOT(offset) virtual void slot##offset();
+
+class BfmeAIUpdateVTable
+{
+public:
+	BFME_VTABLE_SLOT(000) BFME_VTABLE_SLOT(004) BFME_VTABLE_SLOT(008) BFME_VTABLE_SLOT(00C)
+	BFME_VTABLE_SLOT(010) BFME_VTABLE_SLOT(014) BFME_VTABLE_SLOT(018) BFME_VTABLE_SLOT(01C)
+	BFME_VTABLE_SLOT(020) BFME_VTABLE_SLOT(024) BFME_VTABLE_SLOT(028) BFME_VTABLE_SLOT(02C)
+	BFME_VTABLE_SLOT(030) BFME_VTABLE_SLOT(034) BFME_VTABLE_SLOT(038) BFME_VTABLE_SLOT(03C)
+	BFME_VTABLE_SLOT(040) BFME_VTABLE_SLOT(044) BFME_VTABLE_SLOT(048) BFME_VTABLE_SLOT(04C)
+	BFME_VTABLE_SLOT(050) BFME_VTABLE_SLOT(054) BFME_VTABLE_SLOT(058) BFME_VTABLE_SLOT(05C)
+	BFME_VTABLE_SLOT(060) BFME_VTABLE_SLOT(064) BFME_VTABLE_SLOT(068) BFME_VTABLE_SLOT(06C)
+	BFME_VTABLE_SLOT(070) BFME_VTABLE_SLOT(074) BFME_VTABLE_SLOT(078) BFME_VTABLE_SLOT(07C)
+	BFME_VTABLE_SLOT(080) BFME_VTABLE_SLOT(084) BFME_VTABLE_SLOT(088) BFME_VTABLE_SLOT(08C)
+	BFME_VTABLE_SLOT(090) BFME_VTABLE_SLOT(094) BFME_VTABLE_SLOT(098) BFME_VTABLE_SLOT(09C)
+	BFME_VTABLE_SLOT(0A0) BFME_VTABLE_SLOT(0A4) BFME_VTABLE_SLOT(0A8) BFME_VTABLE_SLOT(0AC)
+	BFME_VTABLE_SLOT(0B0) BFME_VTABLE_SLOT(0B4) BFME_VTABLE_SLOT(0B8) BFME_VTABLE_SLOT(0BC)
+	BFME_VTABLE_SLOT(0C0) BFME_VTABLE_SLOT(0C4) BFME_VTABLE_SLOT(0C8) BFME_VTABLE_SLOT(0CC)
+	BFME_VTABLE_SLOT(0D0) BFME_VTABLE_SLOT(0D4) BFME_VTABLE_SLOT(0D8) BFME_VTABLE_SLOT(0DC)
+	BFME_VTABLE_SLOT(0E0) BFME_VTABLE_SLOT(0E4) BFME_VTABLE_SLOT(0E8) BFME_VTABLE_SLOT(0EC)
+	BFME_VTABLE_SLOT(0F0) BFME_VTABLE_SLOT(0F4) BFME_VTABLE_SLOT(0F8) BFME_VTABLE_SLOT(0FC)
+	BFME_VTABLE_SLOT(100) BFME_VTABLE_SLOT(104) BFME_VTABLE_SLOT(108) BFME_VTABLE_SLOT(10C)
+	BFME_VTABLE_SLOT(110) BFME_VTABLE_SLOT(114) BFME_VTABLE_SLOT(118) BFME_VTABLE_SLOT(11C)
+	BFME_VTABLE_SLOT(120) BFME_VTABLE_SLOT(124) BFME_VTABLE_SLOT(128) BFME_VTABLE_SLOT(12C)
+	BFME_VTABLE_SLOT(130) BFME_VTABLE_SLOT(134) BFME_VTABLE_SLOT(138) BFME_VTABLE_SLOT(13C)
+	BFME_VTABLE_SLOT(140) BFME_VTABLE_SLOT(144) BFME_VTABLE_SLOT(148) BFME_VTABLE_SLOT(14C)
+	BFME_VTABLE_SLOT(150) BFME_VTABLE_SLOT(154) BFME_VTABLE_SLOT(158) BFME_VTABLE_SLOT(15C)
+	BFME_VTABLE_SLOT(160) BFME_VTABLE_SLOT(164) BFME_VTABLE_SLOT(168) BFME_VTABLE_SLOT(16C)
+	BFME_VTABLE_SLOT(170) BFME_VTABLE_SLOT(174) BFME_VTABLE_SLOT(178) BFME_VTABLE_SLOT(17C)
+	BFME_VTABLE_SLOT(180) BFME_VTABLE_SLOT(184) BFME_VTABLE_SLOT(188) BFME_VTABLE_SLOT(18C)
+	BFME_VTABLE_SLOT(190) BFME_VTABLE_SLOT(194) BFME_VTABLE_SLOT(198) BFME_VTABLE_SLOT(19C)
+	BFME_VTABLE_SLOT(1A0) BFME_VTABLE_SLOT(1A4) BFME_VTABLE_SLOT(1A8) BFME_VTABLE_SLOT(1AC)
+	BFME_VTABLE_SLOT(1B0) BFME_VTABLE_SLOT(1B4) BFME_VTABLE_SLOT(1B8) BFME_VTABLE_SLOT(1BC)
+	BFME_VTABLE_SLOT(1C0) BFME_VTABLE_SLOT(1C4) BFME_VTABLE_SLOT(1C8) BFME_VTABLE_SLOT(1CC)
+	BFME_VTABLE_SLOT(1D0) BFME_VTABLE_SLOT(1D4) BFME_VTABLE_SLOT(1D8) BFME_VTABLE_SLOT(1DC)
+	BFME_VTABLE_SLOT(1E0) BFME_VTABLE_SLOT(1E4) BFME_VTABLE_SLOT(1E8) BFME_VTABLE_SLOT(1EC)
+	BFME_VTABLE_SLOT(1F0) BFME_VTABLE_SLOT(1F4) BFME_VTABLE_SLOT(1F8)
+};
+
+#undef BFME_VTABLE_SLOT
+
+class AIUpdateInterface : public BfmeAIUpdateVTable
+{
+public:
+	virtual void chooseLocomotorSet(LocomotorSetType set);
+	void requestSafePath(ObjectID enemyID);
+};
+
+class Object
+{
+public:
+	float getVisionRange() const;
+
+	AIUpdateInterface *getAI() const
+	{
+		return m_ai;
+	}
+
+	ObjectID getID() const
+	{
+		return m_id;
+	}
+
+	void setModelConditionState(ModelConditionFlagType condition)
+	{
+		if (!m_modelConditionFlags.test(condition))
+		{
+			m_modelConditionFlags.set(condition);
+			notifyModelConditionChanged();
+		}
+	}
+
+	void notifyModelConditionChanged();
+
+private:
+	unsigned char m_unreconstructed_000[0x74];
+	ObjectID m_id;
+	unsigned char m_unreconstructed_078[0xA0];
+	ModelConditionFlags m_modelConditionFlags;
+	unsigned char m_unreconstructed_11C[0xE8];
+	AIUpdateInterface *m_ai;
+};
+
+class Pathfinder
+{
+public:
+	void removeGoal(Object *object);
+};
+
+class AI
+{
+public:
+	Object *findClosestRepulsor(const Object *object, float visionRange);
+
+	Pathfinder *pathfinder() const
+	{
+		return m_pathfinder;
+	}
+
+private:
+	unsigned char m_unreconstructed_00[0x0C];
+	Pathfinder *m_pathfinder;
+};
+
+class StateMachine
+{
+public:
+	Object *getOwner() const
+	{
+		return m_owner;
+	}
+
+private:
+	unsigned char m_unreconstructed_00[0x10];
+	Object *m_owner;
+};
+
+class State
 {
 public:
 	virtual StateReturnType onEnter();
+
+protected:
+	unsigned char m_unreconstructed_04[0x18];
+	StateMachine *m_machine;
 };
 
-// ?onEnter@AIMoveAwayFromRepulsorsState@@UAE?AW4StateReturnType@@XZ
-__declspec(naked) StateReturnType AIMoveAwayFromRepulsorsState::onEnter()
+class AIInternalMoveToState : public State
 {
-	__asm {
-        __emit 0xa0
-        __emit 0x39
-        __emit 0x02
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x84
-        __emit 0xc0
-        __emit 0x53
-        __emit 0x55
-        __emit 0x56
-        __emit 0x57
-        __emit 0x8b
-        __emit 0xf9
-        __emit 0x74
-        __emit 0x17
-        __emit 0xa1
-        __emit 0xfc
-        __emit 0xd4
-        __emit 0x2e
-        __emit 0x01
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x0e
-        __emit 0x68
-        __emit 0x24
-        __emit 0x91
-        __emit 0x09
-        __emit 0x01
-        __emit 0x50
-        __emit 0xe8
-        __emit 0x07
-        __emit 0x64
-        __emit 0xec
-        __emit 0xff
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x08
-        __emit 0x8b
-        __emit 0x47
-        __emit 0x1c
-        __emit 0xc6
-        __emit 0x47
-        __emit 0x4c
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x70
-        __emit 0x10
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xe8
-        __emit 0xc4
-        __emit 0x0d
-        __emit 0xea
-        __emit 0xff
-        __emit 0x51
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x14
-        __emit 0xf2
-        __emit 0x2e
-        __emit 0x01
-        __emit 0xd9
-        __emit 0x1c
-        __emit 0x24
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x1d
-        __emit 0x80
-        __emit 0xeb
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4f
-        __emit 0x1c
-        __emit 0x8b
-        __emit 0x51
-        __emit 0x10
-        __emit 0x8b
-        __emit 0x9a
-        __emit 0x04
-        __emit 0x02
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xe8
-        __emit 0x85
-        __emit 0xed
-        __emit 0x74
-        __emit 0x62
-        __emit 0x85
-        __emit 0xdb
-        __emit 0x74
-        __emit 0x5e
-        __emit 0x8b
-        __emit 0x03
-        __emit 0x6a
-        __emit 0x04
-        __emit 0x8b
-        __emit 0xcb
-        __emit 0xff
-        __emit 0x90
-        __emit 0xfc
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x74
-        __emit 0x1e
-        __emit 0x8b
-        __emit 0x8e
-        __emit 0x18
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0xb8
-        __emit 0x00
-        __emit 0x10
-        __emit 0x00
-        __emit 0x00
-        __emit 0x85
-        __emit 0xc8
-        __emit 0x75
-        __emit 0x0f
-        __emit 0x0b
-        __emit 0xc8
-        __emit 0x89
-        __emit 0x8e
-        __emit 0x18
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0xce
-        __emit 0xe8
-        __emit 0x42
-        __emit 0xdb
-        __emit 0xea
-        __emit 0xff
-        __emit 0xb8
-        __emit 0x01
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x89
-        __emit 0x47
-        __emit 0x50
-        __emit 0x88
-        __emit 0x47
-        __emit 0x54
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x14
-        __emit 0xf2
-        __emit 0x2e
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x49
-        __emit 0x0c
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x0d
-        __emit 0x1f
-        __emit 0xea
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x55
-        __emit 0x74
-        __emit 0x52
-        __emit 0x8b
-        __emit 0xcb
-        __emit 0xe8
-        __emit 0x10
-        __emit 0xd6
-        __emit 0xe8
-        __emit 0xff
-        __emit 0x8b
-        __emit 0xcf
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5d
-        __emit 0x5b
-        __emit 0xe9
-        __emit 0x1c
-        __emit 0xe0
-        __emit 0xea
-        __emit 0xff
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x5d
-        __emit 0xb8
-        __emit 0xfe
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0x5b
-        __emit 0xc3
+public:
+	virtual StateReturnType onEnter();
+
+	Object *getMachineOwner() const
+	{
+		return m_machine->getOwner();
 	}
+
+	void setAdjustsDestination(Bool adjusts)
+	{
+		m_adjustDestinations = adjusts;
+	}
+
+protected:
+	unsigned char m_unreconstructed_20[0x04];
+	Coord3D m_goalPosition;
+	unsigned char m_unreconstructed_30[0x1C];
+	Bool m_adjustDestinations;
+	unsigned char m_unreconstructed_4D[0x03];
+};
+
+class AIMoveAwayFromRepulsorsState : public AIInternalMoveToState
+{
+public:
+	virtual StateReturnType onEnter();
+
+private:
+	int m_okToRepathTimes;
+	Bool m_checkForPath;
+};
+
+extern unsigned char g_012F0239;
+extern void *g_012ED4FC;
+extern AI *TheAI;
+extern void j_0003a17a();
+
+typedef void (__cdecl *DebugLogFunction)(void *, const char *);
+
+StateReturnType AIMoveAwayFromRepulsorsState::onEnter()
+{
+	if (g_012F0239 && g_012ED4FC)
+	{
+		((DebugLogFunction)j_0003a17a)(g_012ED4FC,
+			"CritterDesync: setAdjustDestination(FALSE) 11");
+	}
+
+	setAdjustsDestination(false);
+	Object *object = getMachineOwner();
+	Object *enemy = TheAI->findClosestRepulsor(getMachineOwner(), object->getVisionRange());
+	AIUpdateInterface *ai = getMachineOwner()->getAI();
+	if (!enemy || !ai)
+		return STATE_FAILURE;
+
+	ai->chooseLocomotorSet(LOCOMOTORSET_PANIC);
+	if (object)
+		object->setModelConditionState(MODELCONDITION_PANICKING);
+
+	m_okToRepathTimes = 1;
+	m_checkForPath = true;
+	TheAI->pathfinder()->removeGoal(object);
+	ai->requestSafePath(enemy->getID());
+	return AIInternalMoveToState::onEnter();
 }
