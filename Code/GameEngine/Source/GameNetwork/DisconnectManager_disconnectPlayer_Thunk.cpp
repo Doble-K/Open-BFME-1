8306e447615160ccbce40b5b43b8923e1a3847c9
dginovker <danielginovker@gmail.com>
2026-08-19 00:54:40 +0900

    Convert DisconnectManager disconnectPlayer to C++

--- Code/GameEngine/Source/GameNetwork/DisconnectManager_disconnectPlayer_Thunk.cpp
+++ Code/GameEngine/Source/GameNetwork/DisconnectManager_disconnectPlayer_Thunk.cpp
@@ -1,247 +1,111 @@
-// cl: /DNDEBUG /MD /EHsc
-// Open-BFME5: lift MASM dump to standalone C++ thunk.
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
 
-class ConnectionManager;
+typedef int Int;
+typedef unsigned int UnsignedInt;
+
+class UnicodeString;
+
+template <class T>
+class StringBase
+{
+private:
+	friend class UnicodeString;
+
+	StringBase(const StringBase<T> &other);
+	~StringBase();
+
+	void *m_data;
+};
+
+class UnicodeString : public StringBase<unsigned short>
+{
+public:
+	UnicodeString(const UnicodeString &other) : StringBase<unsigned short>(other) {}
+	~UnicodeString() {}
+};
+
+enum PlayerLeaveCode
+{
+	PLAYERLEAVECODE_UNKNOWN
+};
+
+class GameSlot
+{
+public:
+	void markAsDisconnected()
+	{
+		m_disconnected = true;
+	}
+
+private:
+	unsigned char m_unreconstructed_00[0x40];
+	bool m_disconnected;
+};
+
+class GameInfo
+{
+public:
+	GameSlot *getSlot(Int slot);
+};
+
+class ConnectionManager
+{
+public:
+	UnsignedInt getLocalPlayerID();
+	UnicodeString getPlayerName(Int playerNum);
+	PlayerLeaveCode disconnectPlayer(Int slot);
+};
+
+class RecorderClass
+{
+public:
+	void logPlayerDisconnect(UnicodeString player, Int slot);
+};
+
+class DisconnectMenu
+{
+public:
+	void removePlayer(Int slot, UnicodeString playerName);
+};
+
+extern GameInfo *TheGameInfo;
+extern RecorderClass *TheRecorder;
+extern DisconnectMenu *TheDisconnectMenu;
+
 class DisconnectManager
 {
 protected:
-	void disconnectPlayer(int, ConnectionManager *);
+	void disconnectPlayer(Int slot, ConnectionManager *conMgr);
 };
 
-// ?disconnectPlayer@DisconnectManager@@IAEXHPAVConnectionManager@@@Z
-__declspec(naked) void DisconnectManager::disconnectPlayer(int, ConnectionManager *)
+// BFME's slot record places m_disconnected four bytes later than Zero Hour's.
+void DisconnectManager::disconnectPlayer(Int slot, ConnectionManager *conMgr)
 {
-	__asm {
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0x18
-        __emit 0x48
-        __emit 0x04
-        __emit 0x01
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x53
-        __emit 0x56
-        __emit 0x8b
-        __emit 0x74
-        __emit 0x24
-        __emit 0x18
-        __emit 0x85
-        __emit 0xf6
-        __emit 0x57
-        __emit 0x0f
-        __emit 0x8c
-        __emit 0xaf
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xfe
-        __emit 0x08
-        __emit 0x0f
-        __emit 0x8d
-        __emit 0xa6
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x8c
-        __emit 0x70
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x85
-        __emit 0xc9
-        __emit 0x74
-        __emit 0x0e
-        __emit 0x56
-        __emit 0xe8
-        __emit 0x9b
-        __emit 0x2b
-        __emit 0x9b
-        __emit 0xff
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x04
-        __emit 0xc6
-        __emit 0x40
-        __emit 0x40
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x5c
-        __emit 0x24
-        __emit 0x20
-        __emit 0x8b
-        __emit 0xcb
-        __emit 0xe8
-        __emit 0x01
-        __emit 0xe2
-        __emit 0x9d
-        __emit 0xff
-        __emit 0x3b
-        __emit 0xf0
-        __emit 0x7d
-        __emit 0x04
-        __emit 0x8b
-        __emit 0xfe
-        __emit 0xeb
-        __emit 0x05
-        __emit 0x74
-        __emit 0x79
-        __emit 0x8d
-        __emit 0x7e
-        __emit 0xff
-        __emit 0x83
-        __emit 0xff
-        __emit 0xff
-        __emit 0x74
-        __emit 0x71
-        __emit 0x56
-        __emit 0x8d
-        __emit 0x44
-        __emit 0x24
-        __emit 0x20
-        __emit 0x50
-        __emit 0x8b
-        __emit 0xcb
-        __emit 0xe8
-        __emit 0xb3
-        __emit 0x69
-        __emit 0x9a
-        __emit 0xff
-        __emit 0x56
-        __emit 0x51
-        __emit 0x8d
-        __emit 0x54
-        __emit 0x24
-        __emit 0x24
-        __emit 0x89
-        __emit 0x64
-        __emit 0x24
-        __emit 0x28
-        __emit 0x8b
-        __emit 0xcc
-        __emit 0x52
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x20
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0xe8
-        __emit 0x37
-        __emit 0xc3
-        __emit 0x21
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x2c
-        __emit 0xd6
-        __emit 0x2e
-        __emit 0x01
-        __emit 0xe8
-        __emit 0x5c
-        __emit 0xca
-        __emit 0x9d
-        __emit 0xff
-        __emit 0xa1
-        __emit 0x64
-        __emit 0x49
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x85
-        __emit 0xc0
-        __emit 0x74
-        __emit 0x1d
-        __emit 0x51
-        __emit 0x8d
-        __emit 0x44
-        __emit 0x24
-        __emit 0x20
-        __emit 0x89
-        __emit 0x64
-        __emit 0x24
-        __emit 0x24
-        __emit 0x8b
-        __emit 0xcc
-        __emit 0x50
-        __emit 0xe8
-        __emit 0x12
-        __emit 0xc3
-        __emit 0x21
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x0d
-        __emit 0x64
-        __emit 0x49
-        __emit 0x2f
-        __emit 0x01
-        __emit 0x57
-        __emit 0xe8
-        __emit 0x39
-        __emit 0x31
-        __emit 0x9d
-        __emit 0xff
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xcb
-        __emit 0xe8
-        __emit 0x5b
-        __emit 0xcc
-        __emit 0x9b
-        __emit 0xff
-        __emit 0x8d
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x1c
-        __emit 0xc7
-        __emit 0x44
-        __emit 0x24
-        __emit 0x14
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0xe8
-        __emit 0xbd
-        __emit 0xc0
-        __emit 0x21
-        __emit 0x00
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x0c
-        __emit 0x5f
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x5b
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x0c
-        __emit 0xc2
-        __emit 0x08
-        __emit 0x00
+	if (slot < 0 || slot >= 8)
+		return;
+
+	if (TheGameInfo)
+	{
+		GameSlot *gameSlot = TheGameInfo->getSlot(slot);
+		if (gameSlot)
+			gameSlot->markAsDisconnected();
+	}
+
+	Int localSlot = conMgr->getLocalPlayerID();
+	Int translatedSlot;
+	if (slot < localSlot)
+		translatedSlot = slot;
+	else if (slot == localSlot)
+		return;
+	else
+		translatedSlot = slot - 1;
+
+	if (translatedSlot != -1)
+	{
+		UnicodeString playerName = conMgr->getPlayerName(slot);
+		TheRecorder->logPlayerDisconnect(playerName, slot);
+		if (TheDisconnectMenu)
+			TheDisconnectMenu->removePlayer(translatedSlot, playerName);
+		conMgr->disconnectPlayer(slot);
 	}
 }
