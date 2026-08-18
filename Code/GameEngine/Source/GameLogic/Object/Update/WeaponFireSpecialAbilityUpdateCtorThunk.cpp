74789efb4481385973417f676fc36f365ef58ff0
dginovker <danielginovker@gmail.com>
2026-08-19 03:00:52 +0900

    Convert WeaponFire special ability constructor

--- Code/GameEngine/Source/GameLogic/Object/Update/WeaponFireSpecialAbilityUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/WeaponFireSpecialAbilityUpdateCtorThunk.cpp
@@ -2,228 +2,127 @@
 
 class Thing;
 class ModuleData;
+class WeaponTemplate;
 
-class WeaponFireSpecialAbilityUpdate
+class BehaviorModule
 {
 public:
-    WeaponFireSpecialAbilityUpdate(Thing *, const ModuleData *);
+	virtual void behaviorModuleAnchor();
+
+private:
+	unsigned char m_data[8];
+};
+
+class BehaviorModuleInterface
+{
+public:
+	virtual void behaviorModuleInterfaceAnchor();
+};
+
+class UpdateModuleInterface
+{
+public:
+	virtual void updateModuleInterfaceAnchor();
+
+private:
+	unsigned char m_data[12];
+};
+
+class ModuleInterface
+{
+public:
+	virtual void moduleInterfaceAnchor();
+
+private:
+	unsigned char m_data[196];
+};
+
+class SpecialAbilityUpdate : public BehaviorModule,
+	public BehaviorModuleInterface,
+	public UpdateModuleInterface,
+	public ModuleInterface
+{
+public:
+	SpecialAbilityUpdate(Thing *thing, const ModuleData *moduleData);
+	virtual ~SpecialAbilityUpdate();
+};
+
+template <class T>
+class BFMERetailStringBase
+{
+public:
+	BFMERetailStringBase(const BFMERetailStringBase &other);
+	~BFMERetailStringBase() { releaseBuffer(); }
+
+private:
+	void releaseBuffer();
+	T *m_data;
+};
+
+class BFMERetailAsciiString : public BFMERetailStringBase<char>
+{
+};
+
+class WeaponFireSpecialAbilityUpdateModuleData
+{
+private:
+	unsigned char m_pad[0x254];
+
+public:
+	BFMERetailAsciiString m_specialWeapon;
+};
+
+enum WeaponSlotType
+{
+	PRIMARY_WEAPON = 0
+};
+
+class Weapon
+{
+public:
+	virtual ~Weapon();
+	void loadAmmoNow(const Thing *source);
+
+	unsigned int m_template;
+	unsigned int m_attackType;
+};
+
+class WeaponStore
+{
+public:
+	const WeaponTemplate *findWeaponTemplate(BFMERetailAsciiString name) const;
+	Weapon *allocateNewWeapon(const WeaponTemplate *weaponTemplate, WeaponSlotType slot) const;
+};
+
+extern WeaponStore *TheWeaponStore;
+
+class WeaponFireSpecialAbilityUpdate : public SpecialAbilityUpdate
+{
+public:
+	WeaponFireSpecialAbilityUpdate(Thing *thing, const ModuleData *moduleData);
+
+private:
+	Weapon *m_weapon;
 };
 
 // ??0WeaponFireSpecialAbilityUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) WeaponFireSpecialAbilityUpdate::WeaponFireSpecialAbilityUpdate(Thing *, const ModuleData *)
+WeaponFireSpecialAbilityUpdate::WeaponFireSpecialAbilityUpdate(
+	Thing *thing, const ModuleData *moduleData)
+	: SpecialAbilityUpdate(thing, moduleData),
+	  m_weapon(0)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0x40;
-        __emit 0x02;
-        __emit 0x01;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x53;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x10;
-        __emit 0xe8;
-        __emit 0xc3;
-        __emit 0x5d;
-        __emit 0xda;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x46;
-        __emit 0x04;
-        __emit 0x05;
-        __emit 0x54;
-        __emit 0x02;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x33;
-        __emit 0xdb;
-        __emit 0x50;
-        __emit 0x8d;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x24;
-        __emit 0x89;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x18;
-        __emit 0x8d;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0x50;
-        __emit 0x8c;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x44;
-        __emit 0x8c;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x18;
-        __emit 0x8c;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x9e;
-        __emit 0xe8;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xe8;
-        __emit 0x88;
-        __emit 0xa4;
-        __emit 0x61;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8d;
-        __emit 0x54;
-        __emit 0x24;
-        __emit 0x24;
-        __emit 0x89;
-        __emit 0x64;
-        __emit 0x24;
-        __emit 0x20;
-        __emit 0x8b;
-        __emit 0xcc;
-        __emit 0x52;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x1c;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0x72;
-        __emit 0xa4;
-        __emit 0x61;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0x0d;
-        __emit 0x38;
-        __emit 0xf7;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0x01;
-        __emit 0xfd;
-        __emit 0xd9;
-        __emit 0xff;
-        __emit 0x3b;
-        __emit 0xc3;
-        __emit 0x74;
-        __emit 0x25;
-        __emit 0x8b;
-        __emit 0x0d;
-        __emit 0x38;
-        __emit 0xf7;
-        __emit 0x2e;
-        __emit 0x01;
-        __emit 0x53;
-        __emit 0x50;
-        __emit 0xe8;
-        __emit 0xb5;
-        __emit 0x4b;
-        __emit 0xda;
-        __emit 0xff;
-        __emit 0x89;
-        __emit 0x86;
-        __emit 0xe8;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x89;
-        __emit 0x58;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0x46;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0x8e;
-        __emit 0xe8;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0xe8;
-        __emit 0xb2;
-        __emit 0x27;
-        __emit 0xda;
-        __emit 0xff;
-        __emit 0x8d;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x20;
-        __emit 0x88;
-        __emit 0x5c;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0xe8;
-        __emit 0x11;
-        __emit 0xa2;
-        __emit 0x61;
-        __emit 0x00;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x5b;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+	const WeaponFireSpecialAbilityUpdateModuleData *data =
+		reinterpret_cast<const WeaponFireSpecialAbilityUpdateModuleData *>(
+			*reinterpret_cast<const ModuleData **>(
+				reinterpret_cast<unsigned char *>(this) + 4));
+	BFMERetailAsciiString specialWeapon(data->m_specialWeapon);
+	const WeaponTemplate *weaponTemplate =
+		TheWeaponStore->findWeaponTemplate(specialWeapon);
+	if (weaponTemplate != 0)
+	{
+		m_weapon = TheWeaponStore->allocateNewWeapon(weaponTemplate, PRIMARY_WEAPON);
+		m_weapon->m_attackType = 0;
+		m_weapon->loadAmmoNow(*reinterpret_cast<Thing **>(
+			reinterpret_cast<unsigned char *>(this) + 8));
+	}
 }
