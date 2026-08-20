0989598ed6a9b74a39b0adf4d705f2f917190380
dginovker <danielginovker@gmail.com>
2026-08-20 23:32:37 +0000

    Open-BFME5: convert PrereqUnitRec copy loop

---
+++ Code/GameEngine/Source/Common/RTS/PrereqUnitRecCopyBody.cpp
@@ -0,0 +1,31 @@
+class AsciiString
+{
+public:
+	void set(const AsciiString &other);
+};
+
+struct PrereqUnitRecCopyABI
+{
+	void *unit;
+	int flags;
+	AsciiString name;
+};
+
+struct PrereqRandomAccessIteratorTagABI {};
+
+extern "C" PrereqUnitRecCopyABI *__cdecl bfme_PrereqUnitRecCopy_E4AE0(
+	PrereqUnitRecCopyABI *first,
+	PrereqUnitRecCopyABI *last,
+	PrereqUnitRecCopyABI *result,
+	const PrereqRandomAccessIteratorTagABI &,
+	int *)
+{
+	for (int count = last - first; count > 0; --count) {
+		result->unit = first->unit;
+		result->flags = first->flags;
+		result->name.set(first->name);
+		++first;
+		++result;
+	}
+	return result;
+}
