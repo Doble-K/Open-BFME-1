16d2539cb05d9aac02213513ee24cb67318b81f1
dginovker <danielginovker@gmail.com>
2026-08-22 04:32:14 +0900

    ControlBar: recover command button allocation

--- Code/GameEngine/Source/GameClient/GUI/ControlBar/ControlBarNewCommandSet.cpp
+++ Code/GameEngine/Source/GameClient/GUI/ControlBar/ControlBarNewCommandSet.cpp
@@ -1,6 +1,30 @@
 // cl: /DNDEBUG /MD /EHsc
 
-class AsciiString;
+class AsciiString
+{
+public:
+	AsciiString &operator =(const AsciiString &that);
+};
+
+class CommandButton
+{
+public:
+	CommandButton();
+
+	void setName(const AsciiString &name)
+	{
+		*reinterpret_cast<AsciiString *>(reinterpret_cast<char *>(this) + 0x0C) = name;
+	}
+
+	void friend_addToList(CommandButton **list)
+	{
+		*reinterpret_cast<CommandButton **>(reinterpret_cast<char *>(this) + 0x14) = *list;
+		*list = this;
+	}
+
+private:
+	unsigned char m_retailLayout[472];
+};
 
 class CommandSet
 {
@@ -15,12 +39,22 @@ private:
 class ControlBar
 {
 protected:
+	CommandButton *newCommandButton(const AsciiString & name);
 	CommandSet *newCommandSet(const AsciiString & name);
 
 private:
-	unsigned char m_prefix[0x2C];
+	unsigned char m_prefix[0x28];
+	CommandButton *m_commandButtons;
 	CommandSet *m_commandSets;
 };
+
+CommandButton *ControlBar::newCommandButton(const AsciiString &name)
+{
+	CommandButton *button = new CommandButton;
+	button->setName(name);
+	button->friend_addToList(&m_commandButtons);
+	return button;
+}
 
 CommandSet *ControlBar::newCommandSet(const AsciiString & name)
 {
