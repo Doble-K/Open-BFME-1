ef99ee48e89f784b66ef47f9c25915ef7c2bbb7d
dginovker <danielginovker@gmail.com>
2026-08-22 01:56:14 +0900

    Open-BFME5: convert Living World object lookup

--- Code/GameEngine/Source/Common/INI/INILivingWorldAnimObject.cpp
+++ Code/GameEngine/Source/Common/INI/INILivingWorldAnimObject.cpp
@@ -1,4 +1,5 @@
-// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/campaignmanagerascii /ICode/Libraries/Source/WWVegas/WWLib
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /D_STLP_USE_STATIC_LIB /Ireference/shims/campaignmanagerascii /ICode/Libraries/Source/WWVegas/WWLib
+// stlport
 
 // FILE: INILivingWorldAnimObject.cpp /////////////////////////////////////////
 //
@@ -18,6 +19,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <stddef.h>
+#include <vector>
 
 #include "Common/AsciiString.h"
 
@@ -33,16 +35,32 @@ public:
 class BfmeLivingWorldAnimObject
 {
 public:
+	BfmeLivingWorldAnimObject( const AsciiString &name );
 	static const FieldParse m_fieldParseTable[];		// 0x01115B60
+
+private:
+	char m_storage[ 40 ];
 };
 
 class BfmeLivingWorldManagerIcons
 {
 public:
 	BfmeLivingWorldAnimObject *findAnimObject( const AsciiString &name );
+
+private:
+	char m_unmodelled[ 0x258 ];
+	std::vector<BfmeLivingWorldAnimObject *> m_animObjects;
 };
 
 extern BfmeLivingWorldManagerIcons *TheLivingWorldManager;		// 0x012F706C
+
+BfmeLivingWorldAnimObject *BfmeLivingWorldManagerIcons::findAnimObject(
+	const AsciiString &name )
+{
+	BfmeLivingWorldAnimObject *object = new BfmeLivingWorldAnimObject( name );
+	m_animObjects.push_back( object );
+	return object;
+}
 
 // ?parseLivingWorldAnimObject@@YAXPAVINI@@@Z
 void parseLivingWorldAnimObject( INI *ini )
