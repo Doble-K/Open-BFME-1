4da73b0cda0aa1df450c6d4d555bb42347e8aaae
dginovker <danielginovker@gmail.com>
2026-08-21 08:59:30 +0000

    Open-BFME5: convert 0x000C8CA0 dword getter

---
+++ Code/GameEngine/Source/Common/Rva000C8CA0DwordGetter.cpp
@@ -0,0 +1,15 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Rva000C8CA0Holder
+{
+    char padding[0x258];
+    unsigned int value;
+
+public:
+    unsigned int get() const;
+};
+
+unsigned int Rva000C8CA0Holder::get() const
+{
+    return value;
+}
