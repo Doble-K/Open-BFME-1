7b89b784e7c1c4e26b04dfade4b159a6f29c0d6d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-21 13:01:37 -0400

    Open-BFME5: convert 0x0056A830 dword setter

---
+++ Code/GameEngine/Source/Common/Rva0056A830DwordSetter.cpp
@@ -0,0 +1,14 @@
+// Retail at 0x0056A830 writes 1 to the dword at this+0x258 and callee-cleans
+// one four-byte argument. The owning type was not recovered.
+class Rva0056A830
+{
+public:
+	void apply(int);
+	char m_lead[0x258];
+	unsigned int m_val;
+};
+
+void Rva0056A830::apply(int)
+{
+	m_val = 1;
+}
