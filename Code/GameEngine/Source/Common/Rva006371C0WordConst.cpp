58adf293ce2cadcc7e28eac5070949f015c3a6e2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-08-21 12:22:27 -0400

    Open-BFME5: convert 0x006371C0 word constant

---
+++ Code/GameEngine/Source/Common/Rva006371C0WordConst.cpp
@@ -0,0 +1,6 @@
+// Retail at 0x006371C0 returns the unsigned word constant 0x1F98.
+// The owning type and field semantics were not recovered.
+unsigned short rva_006371c0_word_const()
+{
+    return 0x1F98;
+}
