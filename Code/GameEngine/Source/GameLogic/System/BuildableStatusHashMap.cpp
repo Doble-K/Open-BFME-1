60923569801312459afc4f30ad4465bd532e0396
dginovker <danielginovker@gmail.com>
2026-08-22 04:47:32 +0900

    Open-BFME5: convert buildable status hash lookup

---
+++ Code/GameEngine/Source/GameLogic/System/BuildableStatusHashMap.cpp
@@ -0,0 +1,39 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+// stlport
+
+class AsciiString
+{
+public:
+	AsciiString(const AsciiString &that);
+	~AsciiString();
+
+private:
+	void *m_data;
+};
+
+enum BuildableStatus
+{
+	BUILDABLE_STATUS_PLACEHOLDER
+};
+
+namespace rts
+{
+	template <class T> struct hash;
+	template <class T> struct equal_to;
+
+	template <> struct hash<AsciiString>
+	{
+		unsigned int operator()(AsciiString value) const;
+	};
+
+	template <> struct equal_to<AsciiString>
+	{
+		int operator()(const AsciiString &left, const AsciiString &right) const;
+	};
+}
+
+#include <hash_map>
+
+typedef std::hash_map<AsciiString, BuildableStatus, rts::hash<AsciiString>, rts::equal_to<AsciiString> > BuildableStatusMap;
+
+template BuildableStatus &BuildableStatusMap::operator[](const AsciiString &key);
