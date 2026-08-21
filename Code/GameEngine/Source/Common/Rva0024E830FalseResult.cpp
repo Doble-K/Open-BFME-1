306fec85511d976cc4b2323b977f92e6ca50f110
dginovker <danielginovker@gmail.com>
2026-08-21 15:11:07 +0000

    Open-BFME5: convert 0x0024E830 false result

---
+++ Code/GameEngine/Source/Common/Rva0024E830FalseResult.cpp
@@ -0,0 +1,12 @@
+// Retail at 0x0024E830 returns false and callee-cleans six four-byte arguments.
+// The semantic owner and argument meanings were not recovered.
+bool __stdcall rva_0024e830_false_result(
+    unsigned int,
+    unsigned int,
+    unsigned int,
+    unsigned int,
+    unsigned int,
+    unsigned int)
+{
+    return false;
+}
