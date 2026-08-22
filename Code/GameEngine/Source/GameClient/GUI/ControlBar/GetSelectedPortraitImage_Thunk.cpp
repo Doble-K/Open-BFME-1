f89a0e0f6db1caefc33113a6e7a7898376f3305f
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-22 12:36:08 -0400

    Open-BFME5 convert Gandalf portrait selector to C++

---
+++ Code/GameEngine/Source/GameClient/GUI/ControlBar/GetSelectedPortraitImage_Thunk.cpp
@@ -0,0 +1,63 @@
+// cl: /DNDEBUG /MD /EHsc
+// BFME chooses a different portrait for unmounted Gandalf in game modes that
+// use the live object portrait.
+
+#include "../../../../../Libraries/Source/WWVegas/WWLib/ascii_string.h"
+
+class Image;
+
+class GameLogicPortraitShim
+{
+public:
+	bool isInMultiplayerOrSkirmishGame();
+};
+
+class MappedImageCollectionPortraitShim
+{
+public:
+	const Image *findImageByName(const AsciiString &name);
+};
+
+class ThingTemplatePortraitShim
+{
+public:
+	unsigned char m_beforeName[0x20];
+	AsciiString m_name;
+	unsigned char m_beforeKindOf[0x128 - 0x24];
+	unsigned int m_kindOfWord;
+
+	const Image *getSelectedPortraitImage() const;
+};
+
+extern GameLogicPortraitShim *TheGameLogic;
+extern MappedImageCollectionPortraitShim *TheMappedImageCollection;
+
+// The relevant BFME template flags are in the word at +0x128.  Bit 17
+// distinguishes the alternate GondorGandalf form at this call site.
+static const unsigned int GANDALF_MOUNTED_MASK = 0x00020000;
+
+// ?_bfme_getSelectedPortraitImage@@YAPBVImage@@PBVThingTemplate@@0@Z
+const Image * __cdecl _bfme_getSelectedPortraitImage(
+	const ThingTemplatePortraitShim *portraitTemplate,
+	const ThingTemplatePortraitShim *objectTemplate)
+{
+	if (portraitTemplate == 0 || objectTemplate == 0)
+		return 0;
+
+	if (TheGameLogic->isInMultiplayerOrSkirmishGame())
+	{
+		static AsciiString gandalfTemplate("GondorGandalf");
+		if (portraitTemplate->m_name.compare(gandalfTemplate) == 0 &&
+			(objectTemplate->m_kindOfWord & GANDALF_MOUNTED_MASK) == 0)
+		{
+			static const Image *gandalfTheGrey = 0;
+			if (gandalfTheGrey == 0)
+				gandalfTheGrey = TheMappedImageCollection->findImageByName(
+					AsciiString("HIGandalTheGrey"));
+			if (gandalfTheGrey != 0)
+				return gandalfTheGrey;
+		}
+	}
+
+	return portraitTemplate->getSelectedPortraitImage();
+}
