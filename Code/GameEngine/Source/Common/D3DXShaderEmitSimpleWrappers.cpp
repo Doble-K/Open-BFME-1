05f60d0cdc411b352792c0e627401bdbaa4f680f
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-22 14:05:14 -0400

    Open-BFME5: drain D3DX shader emit wrappers

--- Code/GameEngine/Source/Common/D3DXShaderEmitSimpleWrappers.cpp
+++ Code/GameEngine/Source/Common/D3DXShaderEmitSimpleWrappers.cpp
@@ -5,6 +5,7 @@ namespace D3DXShader
 	protected:
 		long EmitDSX();
 		long EmitDSY();
+		long EmitFRC();
 		long EmitMIN();
 		long EmitMAX();
 		long EmitLT();
@@ -12,12 +13,17 @@ namespace D3DXShader
 		long EmitADD();
 		long EmitMUL();
 		long EmitDST();
+		long EmitLIT();
+		long EmitCMP();
+		long EmitCND();
+		long EmitSINCOS();
 
 		long EmitInstSimple(unsigned long opcode);
 	};
 
 	long CShaderProgram::EmitDSX() { return EmitInstSimple(0x5b); }
 	long CShaderProgram::EmitDSY() { return EmitInstSimple(0x5c); }
+	long CShaderProgram::EmitFRC() { return EmitInstSimple(0x13); }
 	long CShaderProgram::EmitMIN() { return EmitInstSimple(0x0a); }
 	long CShaderProgram::EmitMAX() { return EmitInstSimple(0x0b); }
 	long CShaderProgram::EmitLT() { return EmitInstSimple(0x0c); }
@@ -25,4 +31,8 @@ namespace D3DXShader
 	long CShaderProgram::EmitADD() { return EmitInstSimple(0x02); }
 	long CShaderProgram::EmitMUL() { return EmitInstSimple(0x05); }
 	long CShaderProgram::EmitDST() { return EmitInstSimple(0x11); }
+	long CShaderProgram::EmitLIT() { return EmitInstSimple(0x10); }
+	long CShaderProgram::EmitCMP() { return EmitInstSimple(0x58); }
+	long CShaderProgram::EmitCND() { return EmitInstSimple(0x50); }
+	long CShaderProgram::EmitSINCOS() { return EmitInstSimple(0x23); }
 }
