3fde2878acf990bb654796a393c0040ae9123cb2
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-22 02:20:47 -0400

    Open-BFME5: convert AICommandInterface::aiEvacuate (210B at 0x000D8AC0)

---
+++ Code/GameEngine/Source/GameLogic/AI/AICommandInterface_aiEvacuate.cpp
@@ -0,0 +1,51 @@
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+// Open-BFME5: AICommandInterface::aiEvacuate, retail 0x000D8AC0.
+
+#include <vector>
+
+struct Coord3D
+{
+	float x;
+	float y;
+	float z;
+};
+
+enum AICommandType
+{
+	AICMD_EVACUATE = 0x1b
+};
+
+enum CommandSourceType
+{
+	CMD_FROM_PLAYER = 0
+};
+
+class AICommandParms
+{
+public:
+	AICommandParms( AICommandType cmd, CommandSourceType cmdSource );
+
+	char m_beforeCoords[0x20];
+	std::vector<Coord3D> m_coords;
+	char m_beforeIntValue[0x08];
+	int m_intValue;
+	char m_remaining[0x9c - 0x38];
+};
+
+class AICommandInterface
+{
+public:
+	virtual void aiDoCommand( const AICommandParms *parms ) = 0;
+	void aiEvacuate( bool exposeStealthUnits, CommandSourceType cmdSource );
+};
+
+void AICommandInterface::aiEvacuate( bool exposeStealthUnits, CommandSourceType cmdSource )
+{
+	AICommandParms parms( AICMD_EVACUATE, cmdSource );
+	if ( exposeStealthUnits )
+		parms.m_intValue = 1;
+	else
+		parms.m_intValue = 0;
+	aiDoCommand( &parms );
+}
