4d7a6d77ae330df0210659add33cc617096f082a
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-22 08:18:00 -0400

    Open-BFME5: convert TwiddlerClassBase constructor (127B at 0x00483760)

--- Code/Libraries/Source/WWVegas/WW3D2/TwiddlerClass_ctor.cpp
+++ Code/Libraries/Source/WWVegas/WW3D2/TwiddlerClass_ctor.cpp
@@ -1,14 +1,35 @@
-// cl: /DNDEBUG /MD /EHsc
+// cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB
+// stlport
+
+#include <list>
 
 // TwiddlerClass's constructor, retail 0x0078F500. The base's constructor and
 // then this class's own vftable pointer; nothing else is touched.
 
-class TwiddlerClassBase
+class SubsystemInterface
+{
+public:
+	SubsystemInterface();
+	virtual ~SubsystemInterface();
+	virtual void subsystemInterfaceAnchor();
+
+private:
+	unsigned int m_name;
+};
+
+class TwiddlerClassBase : public SubsystemInterface
 {
 public:
 	TwiddlerClassBase();
 
 	virtual void twiddlerClassBaseAnchor();
+
+private:
+	unsigned int m_fields08[9];
+	std::list<void *> m_objects;
+	unsigned int m_field30;
+	unsigned int m_field34;
+	int m_index;
 };
 
 class TwiddlerClass : public TwiddlerClassBase
@@ -18,6 +39,23 @@ public:
 
 	virtual void twiddlerClassAnchor();
 };
+
+// ??0TwiddlerClassBase@@QAE@XZ
+TwiddlerClassBase::TwiddlerClassBase()
+{
+	m_fields08[0] = 0;
+	m_fields08[1] = 0;
+	m_fields08[2] = 0;
+	m_fields08[3] = 0;
+	m_fields08[4] = 0;
+	m_fields08[5] = 0;
+	m_fields08[6] = 0;
+	m_fields08[7] = 0;
+	m_fields08[8] = 0;
+	m_field30 = 0;
+	m_field34 = 0;
+	m_index = -1;
+}
 
 // ??0TwiddlerClass@@QAE@XZ
 TwiddlerClass::TwiddlerClass()
