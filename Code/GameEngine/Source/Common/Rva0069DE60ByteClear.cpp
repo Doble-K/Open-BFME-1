678018f96d477b2a328b363298d4932ef42542d4
Jonathan <jonathan@users.noreply.github.com>
2026-08-22 01:41:45 -0400

    Open-BFME5: convert 0x0069DEB0 byte clear

--- Code/GameEngine/Source/Common/Rva0069DE60ByteClear.cpp
+++ Code/GameEngine/Source/Common/Rva0069DE60ByteClear.cpp
@@ -15,3 +15,20 @@ Rva0069DE60 *Rva0069DE60::clear()
 	m_flag = false;
 	return this;
 }
+
+// The sibling at 0x0069DEB0 has the same observed layout and code shape.
+class Rva0069DEB0
+{
+public:
+	Rva0069DEB0 *clear();
+
+private:
+	char m_pad00[ 8 ];
+	bool m_flag;
+};
+
+Rva0069DEB0 *Rva0069DEB0::clear()
+{
+	m_flag = false;
+	return this;
+}
