cc46fc7baca9c001879eaa9ee8f02bc969a88b38
dginovker <danielginovker@gmail.com>
2026-08-22 00:12:59 +0000

    Open-BFME5: convert 0x00202EB0 virtual dispatch

---
+++ Code/GameEngine/Source/Common/Rva00202EB0VirtualSlot11DispatchThunk.cpp
@@ -0,0 +1,26 @@
+// Retail at 0x00202EB0 is a five-byte thiscall tail dispatch through vtable
+// offset 0x2C.  The owning class is unknown, so only the observed slot shape
+// and calling convention are represented here.
+
+struct Rva00202EB0VirtualSlot11DispatchThunk
+{
+    virtual void slot00() = 0;
+    virtual void slot01() = 0;
+    virtual void slot02() = 0;
+    virtual void slot03() = 0;
+    virtual void slot04() = 0;
+    virtual void slot05() = 0;
+    virtual void slot06() = 0;
+    virtual void slot07() = 0;
+    virtual void slot08() = 0;
+    virtual void slot09() = 0;
+    virtual void slot10() = 0;
+    virtual void invoke() = 0;
+
+    void dispatch();
+};
+
+void Rva00202EB0VirtualSlot11DispatchThunk::dispatch()
+{
+    invoke();
+}
