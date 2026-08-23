497333d3c246f0a6ac96f88194d872642c1d6e8c
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-23 15:10:42 -0400

    Open-BFME5 convert eight 0x0082B0xx helpers to C++

--- Code/GameEngine/Source/Common/Rva0082B0A0Identity.cpp
+++ Code/GameEngine/Source/Common/Rva0082B0A0Identity.cpp
@@ -4,9 +4,68 @@ class Rva0082B0A0Identity
 {
 public:
     Rva0082B0A0Identity *self();
+    void touch();
 };
 
 Rva0082B0A0Identity *Rva0082B0A0Identity::self()
 {
     return this;
+}
+
+void Rva0082B0A0Identity::touch()
+{
+}
+
+class Rva0082B0E0Identity
+{
+public:
+    Rva0082B0E0Identity *self();
+    void touch();
+};
+
+Rva0082B0E0Identity *Rva0082B0E0Identity::self()
+{
+    return this;
+}
+
+void Rva0082B0E0Identity::touch()
+{
+}
+
+class Rva0082B140Helpers
+{
+public:
+    int firstValue(int value);
+    int secondValue(int value);
+    int maximumIndex();
+    void ignoreValue(int value);
+    int upperWord();
+
+private:
+    int m_lowerWord;
+    int m_upperWord;
+};
+
+int Rva0082B140Helpers::firstValue(int value)
+{
+    return value;
+}
+
+int Rva0082B140Helpers::secondValue(int value)
+{
+    return value;
+}
+
+int Rva0082B140Helpers::maximumIndex()
+{
+    return 0x3fffffff;
+}
+
+void Rva0082B140Helpers::ignoreValue(int)
+{
+}
+
+int Rva0082B140Helpers::upperWord()
+{
+    return m_upperWord;
 }
