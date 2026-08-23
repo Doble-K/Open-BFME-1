b5bcf2eceb842855f0fe1afb82a2ba3fb13365e2
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-23 12:02:49 -0400

    Open-BFME5 convert three small member bodies to C++

--- Code/GameEngine/Source/Common/Rva007AE420Table.cpp
+++ Code/GameEngine/Source/Common/Rva007AE420Table.cpp
@@ -2,9 +2,18 @@
 
 typedef unsigned int UnsignedInt;
 
+struct Rva007AE440Value
+{
+	UnsignedInt m_first;
+	UnsignedInt m_second;
+	UnsignedInt m_third;
+};
+
 struct Rva007AE420Entry
 {
-	unsigned char m_pad0[0x20];
+	unsigned char m_pad0[0x08];
+	Rva007AE440Value m_data;
+	unsigned char m_pad14[0x0c];
 	float m_value;
 };
 
@@ -12,6 +21,7 @@ class Rva007AE420Table
 {
 public:
 	float getValue( UnsignedInt index ) const;
+	void getEntry( UnsignedInt index, Rva007AE440Value *value ) const;
 
 private:
 	unsigned char m_pad0[0x58];
@@ -21,4 +31,11 @@ private:
 float Rva007AE420Table::getValue( UnsignedInt index ) const
 {
 	return m_entries[index]->m_value;
+}
+
+void Rva007AE420Table::getEntry( UnsignedInt index,
+	Rva007AE440Value *value ) const
+{
+	Rva007AE420Entry *entry = m_entries[index];
+	*value = entry->m_data;
 }
