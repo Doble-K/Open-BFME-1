3f04a5996a4ec8a0bc5c47ff19f5e0b07273c902
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-23 12:36:05 -0400

    Open-BFME5 convert 0x007AE9E0 conditional forwarder to C++

--- Code/GameEngine/Source/Common/Rva007AE9C0Dispatch.cpp
+++ Code/GameEngine/Source/Common/Rva007AE9C0Dispatch.cpp
@@ -11,14 +11,25 @@ public:
 	virtual void v3();
 	virtual void v4();
 	virtual void v5();
-	virtual void dispatch( UnsignedInt first, UnsignedInt mode,
+	virtual UnsignedInt dispatch( UnsignedInt first, void *mode,
 		UnsignedInt second, UnsignedInt third );
 
 	void forward( UnsignedInt first, UnsignedInt second, UnsignedInt third );
+	UnsignedInt forwardIf( UnsignedInt first, void *second,
+		UnsignedInt third, UnsignedInt fourth );
 };
 
 void Rva007AE9C0Dispatcher::forward( UnsignedInt first, UnsignedInt second,
 	UnsignedInt third )
 {
 	dispatch( first, 0, second, third );
+}
+
+UnsignedInt Rva007AE9C0Dispatcher::forwardIf( UnsignedInt first,
+	void *second, UnsignedInt third, UnsignedInt fourth )
+{
+	if ( second == 0 ) {
+		return 0;
+	}
+	return dispatch( first, second, third, fourth );
 }
