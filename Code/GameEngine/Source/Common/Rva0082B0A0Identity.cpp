bfeedc4196ea520e83c999a618ea9d33be2f22be
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-23 15:07:47 -0400

    Open-BFME5 convert 0x0082B0A0 identity method to C++

---
+++ Code/GameEngine/Source/Common/Rva0082B0A0Identity.cpp
@@ -0,0 +1,12 @@
+// cl: /Od /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class Rva0082B0A0Identity
+{
+public:
+    Rva0082B0A0Identity *self();
+};
+
+Rva0082B0A0Identity *Rva0082B0A0Identity::self()
+{
+    return this;
+}
