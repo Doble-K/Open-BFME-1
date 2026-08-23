19cb5ba1306199eac7903cf899416e448e5d1165
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-23 02:40:27 -0400

    Open-BFME5: convert profile-created-date setter

---
+++ Code/GameEngine/Source/Common/SkirmishBattleHonors_setProfileCreatedDate.cpp
@@ -0,0 +1,34 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/ini /Ireference/shims/iniexception /Ireference/shims/ini_noinline /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/debug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main
+// stlport
+
+#include "PreRTS.h"
+#include "Common/AsciiString.h"
+#include "Common/UnicodeString.h"
+
+extern UnicodeString getUnicodeDateBuffer( SYSTEMTIME timeValue );
+
+class SkirmishBattleHonors
+{
+public:
+	virtual void _bfme_slot0( void ) = 0;
+	virtual void _bfme_slot1( void ) = 0;
+	virtual void _bfme_slot2( void ) = 0;
+	virtual void _bfme_slot3( void ) = 0;
+	virtual void _bfme_slot4( void ) = 0;
+	virtual void _bfme_slot5( void ) = 0;
+	virtual void _bfme_slot6( void ) = 0;
+	virtual void setAsciiString( AsciiString key, AsciiString value ) = 0;
+
+	void setProfileCreatedDate( void );
+};
+
+void SkirmishBattleHonors::setProfileCreatedDate( void )
+{
+	SYSTEMTIME timeValue;
+	GetLocalTime( &timeValue );
+
+	UnicodeString displayDate = getUnicodeDateBuffer( timeValue );
+	AsciiString profileDate;
+	profileDate.translate( displayDate );
+	setAsciiString( "ProfileCreatedDate", profileDate );
+}
