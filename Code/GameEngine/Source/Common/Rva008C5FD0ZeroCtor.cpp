7f199f7d11069784acaf149d89903aa0cb3d1eb8
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 13:40:44 +0000

    Open-BFME5: convert Rva008C5FD0Zero's constructor (70B at 0x008C5FD0)

---
+++ Code/GameEngine/Source/Common/Rva008C5FD0ZeroCtor.cpp
@@ -0,0 +1,56 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class Rva008C5FD0Zero
+{
+public:
+	Rva008C5FD0Zero();
+
+	unsigned int m_00;
+	unsigned int m_01;
+	unsigned int m_02;
+	unsigned int m_03;
+	unsigned int m_04;
+	unsigned int m_05;
+	unsigned int m_06;
+	unsigned int m_07;
+	unsigned int m_08;
+	unsigned int m_09;
+	unsigned int m_10;
+	unsigned int m_11;
+	unsigned int m_12;
+	unsigned int m_13;
+	unsigned int m_14;
+	unsigned int m_15;
+	unsigned int m_16;
+	unsigned int m_17;
+	unsigned int m_18;
+	unsigned int m_19;
+	unsigned int m_20;
+	unsigned int m_21;
+};
+
+Rva008C5FD0Zero::Rva008C5FD0Zero()
+{
+	m_00 = 0;
+	m_01 = 0;
+	m_02 = 0;
+	m_03 = 0;
+	m_04 = 0;
+	m_05 = 0;
+	m_06 = 0;
+	m_07 = 0;
+	m_08 = 0;
+	m_09 = 0;
+	m_10 = 0;
+	m_11 = 0;
+	m_12 = 0;
+	m_13 = 0;
+	m_14 = 0;
+	m_15 = 0;
+	m_16 = 0;
+	m_17 = 0;
+	m_18 = 0;
+	m_19 = 0;
+	m_20 = 0;
+	m_21 = 0;
+}
