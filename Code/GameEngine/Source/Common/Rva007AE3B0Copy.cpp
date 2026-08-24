b08975797e657290e9fb60ee12ca513ac56867da
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 13:54:47 +0000

    Open-BFME5: convert Rva007AF2E0Copy::copyTo (26B at 0x007AF2E0)

--- Code/GameEngine/Source/Common/Rva007AE3B0Copy.cpp
+++ Code/GameEngine/Source/Common/Rva007AE3B0Copy.cpp
@@ -20,3 +20,17 @@ void Rva007AE3B0Copy::copyTo(Rva007AE3B0Vec *out)
 {
 	*out = m_vec;
 }
+
+class Rva007AF2E0Copy
+{
+public:
+	void copyTo(Rva007AE3B0Vec *out);
+
+	char m_pad[8];
+	Rva007AE3B0Vec m_vec;
+};
+
+void Rva007AF2E0Copy::copyTo(Rva007AE3B0Vec *out)
+{
+	*out = m_vec;
+}
