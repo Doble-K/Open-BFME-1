5f41aa418c26403d4e7a307113692497813d1277
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 16:13:29 +0000

    Open-BFME5: convert Rva00537DE0Swap::replace (13B at 0x00537DE0)

--- Code/GameEngine/Source/Common/Rva00537600Swap.cpp
+++ Code/GameEngine/Source/Common/Rva00537600Swap.cpp
@@ -16,3 +16,20 @@ int Rva00537600Swap::replace(int value)
 	m_value = value;
 	return old;
 }
+
+class Rva00537DE0Swap
+{
+public:
+	int replace(int value);
+
+private:
+	char m_pad[0x5C];
+	int m_value;
+};
+
+int Rva00537DE0Swap::replace(int value)
+{
+	int old = m_value;
+	m_value = value;
+	return old;
+}
