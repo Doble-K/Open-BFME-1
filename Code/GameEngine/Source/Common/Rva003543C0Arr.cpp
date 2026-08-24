55e664e9c0f2f79e7f62e8b687d864a3697be673
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 14:09:49 +0000

    Open-BFME5: convert Rva003543E0Arr::get (17B at 0x003543E0)

--- Code/GameEngine/Source/Common/Rva003543C0Arr.cpp
+++ Code/GameEngine/Source/Common/Rva003543C0Arr.cpp
@@ -22,3 +22,24 @@ unsigned int Rva003543C0Arr::at(int index)
 {
 	return m_items[index].a;
 }
+
+struct Rva003543E0Elem
+{
+	char m_pad[0x0C];
+	unsigned char m_0c;
+	char m_tail[7];
+};
+
+class Rva003543E0Arr
+{
+public:
+	unsigned char get(int index);
+
+	char m_pad[0x0C];
+	Rva003543E0Elem *m_items;
+};
+
+unsigned char Rva003543E0Arr::get(int index)
+{
+	return m_items[index].m_0c;
+}
