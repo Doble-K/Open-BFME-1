f63aebe19d95b766100002e26af62eb6c79daa95
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 22:57:06 +0000

    Open-BFME5: convert Rva000C9C50::dec (7B at 0x000C9C50)

---
+++ Code/GameEngine/Source/Common/Rva000C9C50Dec.cpp
@@ -0,0 +1,15 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva000C9C50
+{
+	char m_lead[0x648];
+	int m_value;
+
+public:
+	void dec();
+};
+
+void Rva000C9C50::dec()
+{
+	--m_value;
+}
