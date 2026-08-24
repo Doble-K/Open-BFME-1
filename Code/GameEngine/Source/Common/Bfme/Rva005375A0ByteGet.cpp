8e748d316e03d02b94476b0c018ebe16ed1a8894
dginovker <danielginovker@gmail.com>
2026-08-24 14:25:08 +0900

    Convert RVA 005375A0 byte getter

---
+++ Code/GameEngine/Source/Common/Bfme/Rva005375A0ByteGet.cpp
@@ -0,0 +1,12 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Rva005375A0
+{
+public:
+	static unsigned char get(const unsigned char *value);
+};
+
+unsigned char Rva005375A0::get(const unsigned char *value)
+{
+	return *value;
+}
