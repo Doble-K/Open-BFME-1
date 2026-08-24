bf1d613f1f0453e58626e0b913cf1ad1125f4672
dginovker <danielginovker@gmail.com>
2026-08-24 12:46:29 +0900

    Convert RVA 007FBBA0 field getter

---
+++ Code/GameEngine/Source/Common/Bfme/Rva007FBBA0FieldGet.cpp
@@ -0,0 +1,12 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Rva007FBBA0
+{
+public:
+	static int __stdcall get( const int *value );
+};
+
+int __stdcall Rva007FBBA0::get( const int *value )
+{
+	return value[8];
+}
