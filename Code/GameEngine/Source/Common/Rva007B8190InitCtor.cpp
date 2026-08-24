728db61de45d3dc7300b03f597a25d8aa792d1fd
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 13:47:19 +0000

    Open-BFME5: convert Rva007B8190Init's constructor (47B at 0x007B8190)

---
+++ Code/GameEngine/Source/Common/Rva007B8190InitCtor.cpp
@@ -0,0 +1,38 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class Rva007B8190Init
+{
+public:
+	Rva007B8190Init();
+
+	unsigned int m_00;
+	unsigned int m_04;
+	unsigned int m_08;
+	unsigned int m_0c;
+	unsigned int m_10;
+	unsigned int m_14;
+	unsigned int m_18;
+	unsigned int m_1c;
+	unsigned int m_20;
+	unsigned int m_24;
+	unsigned int m_28;
+	unsigned int m_2c;
+	unsigned char m_30;
+};
+
+Rva007B8190Init::Rva007B8190Init()
+{
+	m_00 = 0;
+	m_04 = 0;
+	m_08 = 0;
+	m_0c = 0xffffffff;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = 0;
+	m_1c = 0;
+	m_20 = 0;
+	m_24 = 0;
+	m_28 = 0;
+	m_2c = 0;
+	m_30 = 0;
+}
