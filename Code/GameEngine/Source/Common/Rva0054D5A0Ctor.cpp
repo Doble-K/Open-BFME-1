772d0b6f1d3b9b06faac94c135200bb32e31c881
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 21:40:41 +0000

    Open-BFME5: convert Rva0054D5A0 ctor (28B at 0x0054D5A0)

---
+++ Code/GameEngine/Source/Common/Rva0054D5A0Ctor.cpp
@@ -0,0 +1,25 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class UnicodeString
+{
+public:
+	UnicodeString(const UnicodeString &);
+
+private:
+	void *m_data;
+};
+
+class Rva0054D5A0
+{
+	UnicodeString m_str;
+	unsigned m_4;
+
+public:
+	Rva0054D5A0(const UnicodeString &s, const unsigned *p);
+};
+
+Rva0054D5A0::Rva0054D5A0(const UnicodeString &s, const unsigned *p)
+	: m_str(s)
+{
+	m_4 = *p;
+}
