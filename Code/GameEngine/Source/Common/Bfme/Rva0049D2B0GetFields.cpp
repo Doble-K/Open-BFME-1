3ccaba5a59a59ae2f719b6af2636e5a0c48c43e0
dginovker <danielginovker@gmail.com>
2026-08-24 13:28:16 +0900

    Convert 0x0049D2B0 field getter

---
+++ Code/GameEngine/Source/Common/Bfme/Rva0049D2B0GetFields.cpp
@@ -0,0 +1,19 @@
+// cl: /DNDEBUG /MD /EHsc
+// Structural BFME recovery, retail 0x0049D2B0 (27 bytes).
+
+class Rva0049D2B0
+{
+public:
+	void get(void **first, void **second);
+
+private:
+	unsigned char m_padding[0x2D0];
+	void *m_first;
+	void *m_second;
+};
+
+void Rva0049D2B0::get(void **first, void **second)
+{
+	*first = m_first;
+	*second = m_second;
+}
