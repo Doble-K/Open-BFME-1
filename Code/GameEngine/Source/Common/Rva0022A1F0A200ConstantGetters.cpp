f64a7237c31a786a9e26eb3ce9e20c11702967b6
dginovker <danielginovker@gmail.com>
2026-08-24 16:04:05 +0900

    Convert 0x0022A1F0 and 0x0022A200 constant getters

---
+++ Code/GameEngine/Source/Common/Rva0022A1F0A200ConstantGetters.cpp
@@ -0,0 +1,19 @@
+struct Rva0022A1F0ConstantGetter
+{
+	void *get();
+};
+
+void *Rva0022A1F0ConstantGetter::get()
+{
+	return (void *)0x012A6918;
+}
+
+struct Rva0022A200ConstantGetter
+{
+	void *get();
+};
+
+void *Rva0022A200ConstantGetter::get()
+{
+	return (void *)0x012A6670;
+}
