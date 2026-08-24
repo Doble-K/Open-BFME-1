65ed95751587547b9b1d8fa5544faf9d2f5baf1b
dginovker <danielginovker@gmail.com>
2026-08-24 15:10:30 +0900

    reverse: convert 00894d90 decrement pointer

---
+++ Code/GameEngine/Source/Common/Rva00894D90DecrementPointer.cpp
@@ -0,0 +1,10 @@
+class Rva00894D90Accessor
+{
+public:
+	static unsigned int decrement(unsigned int *value);
+};
+
+unsigned int Rva00894D90Accessor::decrement(unsigned int *value)
+{
+	return --*value;
+}
