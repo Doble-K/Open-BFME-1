b81f1a2e89f70702613d90e6fc6e7e3f7dc5bb29
dginovker <danielginovker@gmail.com>
2026-08-24 13:24:16 +0900

    Convert RVA 001C2B80 zero clear

---
+++ Code/GameEngine/Source/Common/Bfme/Rva001C2B80Clear.cpp
@@ -0,0 +1,33 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Rva001C2B80
+{
+public:
+	void clear();
+
+private:
+	unsigned m_word0;
+	unsigned m_word1;
+	unsigned m_word2;
+	unsigned m_word3;
+	unsigned m_word4;
+	unsigned m_word5;
+	unsigned m_word6;
+	unsigned m_word7;
+	unsigned m_word8;
+	unsigned m_word9;
+};
+
+void Rva001C2B80::clear()
+{
+	m_word0 = 0;
+	m_word1 = 0;
+	m_word2 = 0;
+	m_word3 = 0;
+	m_word4 = 0;
+	m_word5 = 0;
+	m_word6 = 0;
+	m_word7 = 0;
+	m_word8 = 0;
+	m_word9 = 0;
+}
