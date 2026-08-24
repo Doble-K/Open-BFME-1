b48d150a334803f2302b77507185f0f0b7aa247f
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 22:57:30 +0000

    Open-BFME5: convert Rva0010CF10::dec (7B at 0x0010CF10)

---
+++ Code/GameEngine/Source/Common/Rva0010CF10Dec.cpp
@@ -0,0 +1,15 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva0010CF10
+{
+	char m_lead[0x1A0];
+	int m_value;
+
+public:
+	void dec();
+};
+
+void Rva0010CF10::dec()
+{
+	--m_value;
+}
