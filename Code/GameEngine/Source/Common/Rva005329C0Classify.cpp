8c537e0924e1996d5b108bf7f2dbd8af86a98bd8
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 19:36:21 +0000

    Open-BFME5: convert Rva005329C0::wrap (15B at 0x005329C0)

---
+++ Code/GameEngine/Source/Common/Rva005329C0Classify.cpp
@@ -0,0 +1,20 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva005329C0Obj;
+
+class Rva005329Classify
+{
+public:
+	void classify(Rva005329C0Obj *obj, int slot);
+};
+
+class Rva005329C0
+{
+public:
+	void wrap(Rva005329C0Obj *obj);
+};
+
+void Rva005329C0::wrap(Rva005329C0Obj *obj)
+{
+	reinterpret_cast<Rva005329Classify *>(this)->classify(obj, 2);
+}
