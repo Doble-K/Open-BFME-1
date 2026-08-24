9975cf56b830938dcbecd97f5e9cda535f30acd4
dginovker <danielginovker@gmail.com>
2026-08-24 14:55:51 +0900

    reverse: convert 0033ae60 add eight

---
+++ Code/GameEngine/Source/Common/Rva0033AE60AddEight.cpp
@@ -0,0 +1,10 @@
+class Rva0033AE60
+{
+public:
+	unsigned int addEight(unsigned int value, unsigned int unused);
+};
+
+unsigned int Rva0033AE60::addEight(unsigned int value, unsigned int)
+{
+	return value + 8;
+}
