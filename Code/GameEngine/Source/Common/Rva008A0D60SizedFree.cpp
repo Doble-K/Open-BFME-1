ade88f7ef8789dc3b7be6a57fa9a532fced4fb1c
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 13:38:25 +0000

    Open-BFME5: convert Rva008C5B70Buf::clear (49B at 0x008C5B70)

---
+++ Code/GameEngine/Source/Common/Rva008A0D60SizedFree.cpp
@@ -0,0 +1,23 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+__declspec(dllimport) void __cdecl operator delete(void *, unsigned int);
+
+class Rva008C5B70Buf
+{
+public:
+	void clear();
+
+private:
+	int m_pad;
+	unsigned int m_count;
+	void *m_ptr;
+};
+
+void Rva008C5B70Buf::clear()
+{
+	if (m_ptr)
+		operator delete(m_ptr, m_count * sizeof(unsigned int));
+	m_count = 0;
+	m_pad = 0;
+	m_ptr = 0;
+}
