e8aba8cdbbdba571dc27f90684a621ee60e5f5d7
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 22:58:36 +0000

    Open-BFME5: convert Rva00695DB0::inc (7B at 0x00695DB0)

---
+++ Code/GameEngine/Source/Common/Rva00695DB0Inc.cpp
@@ -0,0 +1,15 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva00695DB0
+{
+	char m_lead[0x614];
+	int m_value;
+
+public:
+	void inc();
+};
+
+void Rva00695DB0::inc()
+{
+	++m_value;
+}
