7f116ed31b79db10015a0e3a03cec29a75040b5e
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 19:37:52 +0000

    Open-BFME5: convert Rva00299DC0 constructor (15B at 0x00299DC0)

---
+++ Code/GameEngine/Source/Common/Rva00299DC0Zero.cpp
@@ -0,0 +1,18 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva00299DC0
+{
+	unsigned m_0;
+	unsigned m_4;
+	unsigned m_8;
+
+public:
+	Rva00299DC0(int a, int b);
+};
+
+Rva00299DC0::Rva00299DC0(int, int)
+{
+	m_0 = 0;
+	m_4 = 0;
+	m_8 = 0;
+}
