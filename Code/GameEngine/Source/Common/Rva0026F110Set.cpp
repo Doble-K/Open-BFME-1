76bfa4797d626870e1cacfd6184aa9649a1ee330
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 19:44:17 +0000

    Open-BFME5: convert Rva0026F110::set (15B at 0x0026F110)

---
+++ Code/GameEngine/Source/Common/Rva0026F110Set.cpp
@@ -0,0 +1,17 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva0026F110
+{
+	unsigned char m_pad[0x323];
+	unsigned char m_323;
+	unsigned char m_324;
+
+public:
+	void set();
+};
+
+void Rva0026F110::set()
+{
+	m_323 = 1;
+	m_324 = 0;
+}
