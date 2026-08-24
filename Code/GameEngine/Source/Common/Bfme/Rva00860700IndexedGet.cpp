899a264002bdab95b27628a59db45a7ad991b9a3
dginovker <danielginovker@gmail.com>
2026-08-24 15:14:01 +0900

    Convert RVA 00860700 indexed getter

---
+++ Code/GameEngine/Source/Common/Bfme/Rva00860700IndexedGet.cpp
@@ -0,0 +1,12 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Rva00860700
+{
+public:
+	static unsigned get(const unsigned *value);
+};
+
+unsigned Rva00860700::get(const unsigned *value)
+{
+	return value[556];
+}
