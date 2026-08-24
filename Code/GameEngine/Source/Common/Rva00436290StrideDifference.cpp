5ad84e796615cac9cbe1c0c717d612a3af56562d
dginovker <danielginovker@gmail.com>
2026-08-24 16:02:03 +0900

    Convert RVA 00436290 stride difference

---
+++ Code/GameEngine/Source/Common/Rva00436290StrideDifference.cpp
@@ -0,0 +1,9 @@
+class Rva00436290StrideDifference {
+public:
+    static int difference(const int *left, const int *right);
+};
+
+int Rva00436290StrideDifference::difference(const int *left, const int *right)
+{
+    return (right[0] - left[0]) >> 3;
+}
