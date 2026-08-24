0571072a3dd5a9379f33e6276e4564c5f3e40b56
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 17:26:27 +0000

    Open-BFME5: convert Rva0082BEC0Copy::copy (59B at 0x0082BEC0)

---
+++ Code/GameEngine/Source/Common/Rva0082BEC0Copy.cpp
@@ -0,0 +1,27 @@
+// cl: /Od /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+namespace _STL
+{
+template <class T>
+struct char_traits
+{
+};
+
+template <>
+struct char_traits<char>
+{
+  static void assign(char &, const char &);
+};
+}
+
+class Rva0082BEC0Copy
+{
+public:
+  void copy(char *first, char *last, char *out);
+};
+
+void Rva0082BEC0Copy::copy(char *first, char *last, char *out)
+{
+  for (; first != last; ++first, ++out)
+    _STL::char_traits<char>::assign(*out, *first);
+}
