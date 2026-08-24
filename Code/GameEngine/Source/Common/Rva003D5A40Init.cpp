71d304cbd0b94952fd4c051af4c9b1aa2d8832dd
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 18:02:17 +0000

    Open-BFME5: convert Rva003D5A40::init (15B at 0x003D5A40)

---
+++ Code/GameEngine/Source/Common/Rva003D5A40Init.cpp
@@ -0,0 +1,17 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva003D5A40
+{
+	char m_pad[0x20];
+	unsigned m_20;
+	unsigned m_24;
+
+public:
+	void init();
+};
+
+void Rva003D5A40::init()
+{
+	m_20 = 0;
+	m_24 = 1;
+}
