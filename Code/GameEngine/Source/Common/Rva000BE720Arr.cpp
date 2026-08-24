dd8b9cdaf321ee2df8bcfd63d7cead04e1321fc9
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 13:53:08 +0000

    Open-BFME5: convert Rva000BE720Arr::at (13B at 0x000BE720)

---
+++ Code/GameEngine/Source/Common/Rva000BE720Arr.cpp
@@ -0,0 +1,17 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+struct Rva000BE720Elem
+{
+	char m_bytes[20];
+};
+
+class Rva000BE720Arr
+{
+public:
+	Rva000BE720Elem *at(int index);
+};
+
+Rva000BE720Elem *Rva000BE720Arr::at(int index)
+{
+	return ((Rva000BE720Elem *)this) + index;
+}
