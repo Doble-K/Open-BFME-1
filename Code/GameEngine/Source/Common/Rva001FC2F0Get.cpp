19bba98ab75d33b09cdc329dd9513f6330c3cd13
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 19:39:26 +0000

    Open-BFME5: convert Rva001FC310::get (15B at 0x001FC310)

--- Code/GameEngine/Source/Common/Rva001FC2F0Get.cpp
+++ Code/GameEngine/Source/Common/Rva001FC2F0Get.cpp
@@ -20,3 +20,22 @@ unsigned Rva001FC2F0::get() const
 		return m_0C;
 	return v;
 }
+
+class Rva001FC310
+{
+	unsigned char m_pad[0x0C];
+	unsigned m_0C;
+	unsigned char m_pad2[0x44 - 0x10];
+	unsigned m_44;
+
+public:
+	unsigned get() const;
+};
+
+unsigned Rva001FC310::get() const
+{
+	unsigned v = m_44;
+	if (v == g_sentinel012ADC38)
+		return m_0C;
+	return v;
+}
