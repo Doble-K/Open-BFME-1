1c7b1720b794c2440671d411d6cc6a82c3a38866
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 18:30:32 +0000

    Open-BFME5: convert Rva003D5930 constructor (34B at 0x003D5930)

---
+++ Code/GameEngine/Source/Common/Rva003D5930Ctor.cpp
@@ -0,0 +1,22 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva003D5930
+{
+	int m_0;
+	int m_4;
+	int m_8;
+	int m_C;
+	int m_10;
+
+public:
+	Rva003D5930(int a, int b);
+};
+
+Rva003D5930::Rva003D5930(int a, int b)
+{
+	m_0 = a;
+	m_4 = b;
+	m_8 = 0;
+	m_C = -1;
+	m_10 = -1;
+}
