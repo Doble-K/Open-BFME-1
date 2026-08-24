1699072a751f4af16be6410ef4172176bde11d62
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 22:58:57 +0000

    Open-BFME5: convert Rva0073A640::inc (7B at 0x0073A640)

---
+++ Code/GameEngine/Source/Common/Rva0073A640Inc.cpp
@@ -0,0 +1,15 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva0073A640
+{
+	char m_lead[0xC0];
+	int m_value;
+
+public:
+	void inc();
+};
+
+void Rva0073A640::inc()
+{
+	++m_value;
+}
