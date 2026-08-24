35133c24e0086b26f76bb8a74bf1b4ff3f1ccca4
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 15:33:14 +0000

    Open-BFME5: convert Rva0022A620Obj::set (78B at 0x0022A620)

---
+++ Code/GameEngine/Source/Common/Rva0022A620Set.cpp
@@ -0,0 +1,31 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class AsciiString
+{
+public:
+	AsciiString &operator=(const AsciiString &other)
+	{
+		set(other);
+		return *this;
+	}
+	~AsciiString();
+
+private:
+	void set(const AsciiString &other);
+	void *m_data;
+};
+
+class Rva0022A620Obj
+{
+public:
+	void set(AsciiString value);
+
+private:
+	char m_pad[0x328];
+	AsciiString m_name;
+};
+
+void Rva0022A620Obj::set(AsciiString value)
+{
+	m_name = value;
+}
