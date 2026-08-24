639d8e05c96f921e327b8b98217033271a0c3032
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 14:52:29 +0000

    Open-BFME5: convert Rva003FB9F0List::push (16B at 0x003FB9F0)

--- Code/GameEngine/Source/Common/Rva003FB920Short.cpp
+++ Code/GameEngine/Source/Common/Rva003FB920Short.cpp
@@ -14,3 +14,25 @@ void Rva003FB920Short::set(short value)
 {
 	m_value = value;
 }
+
+struct Rva003FB9F0Node
+{
+	int m_pad;
+	Rva003FB9F0Node *m_next;
+};
+
+class Rva003FB9F0List
+{
+public:
+	void push(Rva003FB9F0Node *node);
+
+private:
+	char m_pad[0x3C];
+	Rva003FB9F0Node *m_head;
+};
+
+void Rva003FB9F0List::push(Rva003FB9F0Node *node)
+{
+	node->m_next = m_head;
+	m_head = node;
+}
