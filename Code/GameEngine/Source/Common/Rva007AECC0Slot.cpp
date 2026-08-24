64c5a24c589371c39308a706803db210c203ca5b
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 14:05:31 +0000

    Open-BFME5: convert Rva007AECC0Slot::getPair (33B at 0x007AED70)

--- Code/GameEngine/Source/Common/Rva007AECC0Slot.cpp
+++ Code/GameEngine/Source/Common/Rva007AECC0Slot.cpp
@@ -4,6 +4,8 @@ struct Rva007AECC0Item
 {
 	char m_pad[0x68];
 	int m_value;
+	int m_6c;
+	int m_70;
 };
 
 class Rva007AECC0Slot
@@ -11,6 +13,7 @@ class Rva007AECC0Slot
 public:
 	void set(unsigned int index, int value);
 	int get(unsigned int index);
+	void getPair(unsigned int index, int *a, int *b);
 
 	char m_pad[0x58];
 	Rva007AECC0Item *m_items[1];
@@ -29,4 +32,14 @@ int Rva007AECC0Slot::get(unsigned int index)
 	if (item)
 		return item->m_value;
 	return 0;
+}
+
+void Rva007AECC0Slot::getPair(unsigned int index, int *a, int *b)
+{
+	Rva007AECC0Item *item = m_items[index];
+	if (item)
+	{
+		*a = item->m_6c;
+		*b = item->m_70;
+	}
 }
