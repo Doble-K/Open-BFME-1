5e9ca9651083981af0af17c7d2662bfd3eec33a6
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 17:01:34 +0000

    Open-BFME5: convert Rva0082AFD0Add::add (21B at 0x0082AFD0)

--- Code/GameEngine/Source/Common/Rva0082AFB0Add.cpp
+++ Code/GameEngine/Source/Common/Rva0082AFB0Add.cpp
@@ -12,3 +12,16 @@ char *Rva0082AFB0Add::add(int n)
 {
   return m_ptr + n;
 }
+
+class Rva0082AFD0Add
+{
+  char *m_ptr;
+
+public:
+  char *add(int n);
+};
+
+char *Rva0082AFD0Add::add(int n)
+{
+  return m_ptr + n;
+}
