524197a121701d7e1326e496c69e1e4caef5c632
dginovker <danielginovker@gmail.com>
2026-08-24 14:33:36 +0900

    Convert RVA 004A3B50 zero leaf

---
+++ Code/GameEngine/Source/Common/Bfme/Rva004A3B50Zero.cpp
@@ -0,0 +1,13 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class Rva004A3B50
+{
+public:
+	static int zero(volatile unsigned value);
+};
+
+int Rva004A3B50::zero(volatile unsigned value)
+{
+	(void)value;
+	return 0;
+}
