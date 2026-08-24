8bd28275bf52bb2eec66d32b2cd11c2d79f36ff7
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-24 22:57:53 +0000

    Open-BFME5: convert Rva0043A780::dec (7B at 0x0043A780)

---
+++ Code/GameEngine/Source/Common/Rva0043A780Dec.cpp
@@ -0,0 +1,15 @@
+// cl: /O2 /Ob0 /DNDEBUG /DWIN32 /D_WINDOWS /MD
+
+class Rva0043A780
+{
+	char m_lead[0x554];
+	int m_value;
+
+public:
+	void dec();
+};
+
+void Rva0043A780::dec()
+{
+	--m_value;
+}
