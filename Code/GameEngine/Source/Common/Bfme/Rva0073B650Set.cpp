d834392cc57b04a3667a2abf0f214bcc32e488f2
GL <gleontra@gmail.com>
2026-08-25 00:07:00 -0300

    Match Rva0073B650::setMode at 0x0073B650 (66B)

---
+++ Code/GameEngine/Source/Common/Bfme/Rva0073B650Set.cpp
@@ -0,0 +1,35 @@
+// 66-byte filterMode setter with W3DShaderManager::filterSetup call
+
+enum FilterTypes {};
+enum FilterModes {};
+
+class W3DShaderManager
+{
+public:
+	static bool filterSetup( FilterModes mode, FilterTypes filter );
+};
+
+class Rva0073B650
+{
+public:
+	bool setMode( FilterModes mode );
+
+	char        m_pad00[ 0x10C ];
+	FilterTypes m_filterType;
+	FilterModes m_filterMode;
+};
+
+bool Rva0073B650::setMode( FilterModes mode )
+{
+	FilterModes oldMode = m_filterMode;
+	m_filterMode = mode;
+	if ( m_filterType && mode )
+	{
+		if ( !W3DShaderManager::filterSetup( mode, m_filterType ) )
+		{
+			m_filterMode = oldMode;
+			return false;
+		}
+	}
+	return true;
+}
