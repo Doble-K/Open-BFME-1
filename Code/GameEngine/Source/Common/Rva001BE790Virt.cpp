ec0e97530215142a6dd905bb8de364958c8a198d
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-25 07:54:36 +0000

    Open-BFME5: convert Rva001BE790::run (13B at 0x001BE790)

---
+++ Code/GameEngine/Source/Common/Rva001BE790Virt.cpp
@@ -0,0 +1,29 @@
+// cl: /O2 /Ob0
+
+class Rva001BE790
+{
+public:
+	virtual void pad0();
+	virtual void pad1();
+	virtual void pad2();
+	virtual void pad3();
+	virtual void pad4();
+	virtual void pad5();
+	virtual void pad6();
+	virtual void pad7();
+	virtual void pad8();
+	virtual void pad9();
+	virtual void pad10();
+	virtual void pad11();
+	virtual void pad12();
+	virtual void pad13();
+	virtual void pad14();
+	virtual void pad15();
+	virtual void slot16(void *, int);
+	void run();
+};
+
+void Rva001BE790::run()
+{
+	slot16((void *)0x497423F0, 0);
+}
