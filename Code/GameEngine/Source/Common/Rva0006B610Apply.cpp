c26d647658e6dabc332422c285370083b892c5e8
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-25 01:09:04 +0000

    Open-BFME5: convert Rva0006B610::apply (18B at 0x0006B610)

---
+++ Code/GameEngine/Source/Common/Rva0006B610Apply.cpp
@@ -0,0 +1,34 @@
+// cl: /O2 /Ob0
+
+class Rva0006B610
+{
+public:
+	virtual void slot00(int value);
+	virtual void slot04();
+	virtual void slot08();
+	virtual void slot0c();
+	virtual void slot10();
+	virtual void slot14();
+	virtual void slot18();
+	virtual void slot1c();
+	virtual void slot20();
+	virtual void slot24();
+	virtual void slot28();
+	virtual void slot2c();
+	virtual void slot30();
+	virtual void slot34();
+	virtual void slot38();
+	virtual void slot3c();
+	virtual void slot40();
+	virtual void slot44();
+	virtual void slot48();
+	virtual void slot4c();
+
+	void apply();
+};
+
+void Rva0006B610::apply()
+{
+	slot4c();
+	slot00(1);
+}
