615d6a76014ae45e30d700af376692019fc8639b
GL <gleontra@gmail.com>
2026-08-24 22:26:44 -0300

    Match Rva002ED220::set at 0x002ED220 (23B)

--- Code/GameEngine/Source/Common/Bfme/Rva002ED240FieldSet.cpp
+++ Code/GameEngine/Source/Common/Bfme/Rva002ED240FieldSet.cpp
@@ -1,6 +1,25 @@
 // cl: /DNDEBUG /MD /EHsc
 // Retail stores an activation marker and the one-byte state beside it.
 
+class Rva002ED220
+{
+public:
+	void set( int count );
+
+private:
+	unsigned char m_padding[0x110];
+	int m_count;
+};
+
+void Rva002ED220::set( int count )
+{
+	if ( count < 1 )
+	{
+		count = 1;
+	}
+	m_count = count;
+}
+
 class Rva002ED240
 {
 public:
@@ -17,3 +36,4 @@ void Rva002ED240::set( unsigned char state )
 	m_active = 1;
 	m_state = state;
 }
+
