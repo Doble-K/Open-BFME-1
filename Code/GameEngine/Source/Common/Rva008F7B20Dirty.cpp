27ab325ded8e1d4dde3cdea55ba7edf1df16904e
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-25 07:06:58 +0000

    Open-BFME5: convert PartitionData::maybePrepend (16B at 0x008F7B20)

---
+++ Code/GameEngine/Source/Common/Rva008F7B20Dirty.cpp
@@ -0,0 +1,25 @@
+// cl: /O2 /Ob0
+
+class PartitionData;
+
+class PartitionDirtyListOwner
+{
+public:
+	void prependDirtyModule(PartitionData *node);
+};
+
+class PartitionData
+{
+	PartitionDirtyListOwner *m_owner;
+	char m_pad[0x10];
+	void *m_field14;
+
+public:
+	void maybePrepend();
+};
+
+void PartitionData::maybePrepend()
+{
+	if (!m_field14)
+		m_owner->prependDirtyModule(this);
+}
