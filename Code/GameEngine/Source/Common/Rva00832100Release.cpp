8e671faa28077d24c1e7699038b83aae4fa9b620
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-25 01:06:21 +0000

    Open-BFME5: convert rva00832100Release (27B at 0x00832100)

---
+++ Code/GameEngine/Source/Common/Rva00832100Release.cpp
@@ -0,0 +1,26 @@
+// cl: /O2 /Ob0 /DNDEBUG /MD
+
+namespace _STL
+{
+
+class _Locale_impl
+{
+public:
+	virtual void _bfme_slot0(void) = 0;
+	virtual void _bfme_incr(void) = 0;
+	virtual void _bfme_decr(void) = 0;
+};
+
+extern _Locale_impl *_Bfme_classic_locale;
+extern int _Bfme_classic_locale_refs;
+
+void rva00832100Release(void)
+{
+	if (_Bfme_classic_locale_refs)
+	{
+		_Bfme_classic_locale->_bfme_decr();
+		--_Bfme_classic_locale_refs;
+	}
+}
+
+}
