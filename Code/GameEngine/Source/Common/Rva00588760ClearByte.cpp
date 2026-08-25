00bd537281dec285b9950120cabf5118a5697bca
GL <gleontra@gmail.com>
2026-08-24 22:25:28 -0300

    Match Rva002ED510Object::clearByte at 0x002ED510 (6B)

--- Code/GameEngine/Source/Common/Rva00588760ClearByte.cpp
+++ Code/GameEngine/Source/Common/Rva00588760ClearByte.cpp
@@ -12,3 +12,19 @@ Rva00588760Object *Rva00588760Object::clearByte()
 	m_flag = 0;
 	return this;
 }
+
+class Rva002ED510Object
+{
+public:
+	Rva002ED510Object *clearByte();
+
+private:
+	unsigned char m_flag;
+};
+
+Rva002ED510Object *Rva002ED510Object::clearByte()
+{
+	m_flag = 0;
+	return this;
+}
+
