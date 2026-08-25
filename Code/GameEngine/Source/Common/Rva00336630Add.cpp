cc7d836a999f4b7e94dd42cc8880cbf8cf32cd1c
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-25 04:58:03 +0000

    Open-BFME5: convert Rva00336630::add (13B at 0x00336630)

---
+++ Code/GameEngine/Source/Common/Rva00336630Add.cpp
@@ -0,0 +1,15 @@
+// cl: /O2 /Ob0
+
+class Rva00336630
+{
+	char m_lead[0x34];
+	float m_value;
+
+public:
+	void add(float value);
+};
+
+void Rva00336630::add(float value)
+{
+	m_value += value;
+}
