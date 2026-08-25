cb231c32ecd8424f0dc4164a26e21a3d1d8905b6
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-25 05:48:21 +0000

    Open-BFME5: convert Rva004B5A10::get (11B at 0x004B5A10)

---
+++ Code/GameEngine/Source/Common/Rva004B5A10Get.cpp
@@ -0,0 +1,26 @@
+// cl: /O2 /Ob0
+
+class GameWindow
+{
+public:
+	void *winGetUserData(void);
+};
+
+struct Rva004B5A10User
+{
+	char m_lead[8];
+	void *m_field;
+};
+
+class Rva004B5A10
+{
+	GameWindow *m_window;
+
+public:
+	void *get();
+};
+
+void *Rva004B5A10::get()
+{
+	return ((Rva004B5A10User *)m_window->winGetUserData())->m_field;
+}
