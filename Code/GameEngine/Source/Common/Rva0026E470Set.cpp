67b354a6b29e66da3ee1a726fa1a50213f03633a
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-25 05:16:14 +0000

    Open-BFME5: convert Rva0026E470::set (13B at 0x0026E470)

---
+++ Code/GameEngine/Source/Common/Rva0026E470Set.cpp
@@ -0,0 +1,15 @@
+// cl: /O2 /Ob0
+
+class Rva0026E470
+{
+	char m_lead[0x18C];
+	float m_value;
+
+public:
+	void set(int value);
+};
+
+void Rva0026E470::set(int value)
+{
+	m_value = (float)value;
+}
