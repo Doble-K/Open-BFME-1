7cad1cdea507f16c4da94cd17e71dbde729b3cee
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 10:43:26 +0000

    Open-BFME5: convert Rva00336870 set (23B at 0x00336870)

---
+++ Code/GameEngine/Source/Common/Rva00336870Set.cpp
@@ -0,0 +1,22 @@
+// cl: /O2 /Ob0
+
+class GlobalData
+{
+public:
+	char m_pad[0xB8C];
+	int m_B8C;
+};
+
+extern GlobalData *TheWritableGlobalData;
+
+class Rva00336870
+{
+public:
+	void set(int value);
+};
+
+void Rva00336870::set(int value)
+{
+	if (value >= 0)
+		TheWritableGlobalData->m_B8C = value;
+}
