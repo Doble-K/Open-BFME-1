fc13ed67431e4aa7c7fd7426f571ba9096eb8f6b
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 12:40:24 +0000

    Open-BFME5: convert Rva003A5450Ctor ctor (92B at 0x003A5450)

---
+++ Code/GameEngine/Source/Common/Rva003A5450Ctor.cpp
@@ -0,0 +1,60 @@
+// cl: /O2 /Ob0
+
+class Rva003A5450
+{
+	void *m_00;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	int m_18;
+	char m_1C;
+	char m_1D;
+	char m_1E;
+	char m_1F;
+	int m_20;
+	int m_24;
+	int m_28;
+	int m_2C;
+	int m_30;
+	int m_34;
+	char m_38;
+	int m_3C;
+	int m_40;
+	char m_44;
+	int m_48;
+	int m_4C;
+	int m_50;
+
+public:
+	Rva003A5450();
+};
+
+Rva003A5450::Rva003A5450()
+{
+	m_00 = (void *)0x010EC738;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = 0;
+	m_1C = 0;
+	m_1D = 0;
+	m_1E = 0;
+	m_1F = 0;
+	m_24 = 1;
+	m_28 = 0;
+	m_2C = 0;
+	m_30 = 0;
+	m_34 = 0;
+	m_38 = 0;
+	m_3C = 2;
+	m_40 = 0;
+	m_44 = 0;
+	m_48 = 0;
+	m_4C = 0;
+	m_50 = 0;
+	m_20 = 0x3F2AAAAB;
+}
