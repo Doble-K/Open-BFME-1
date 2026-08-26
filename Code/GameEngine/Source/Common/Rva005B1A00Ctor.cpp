f21ee29fd134f7db8cdc807231197c86c2f42418
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:22:41 +0000

    Open-BFME5: convert Rva005B1A00 ctor (53B at 0x005B1A00)

---
+++ Code/GameEngine/Source/Common/Rva005B1A00Ctor.cpp
@@ -0,0 +1,42 @@
+// cl: /O2 /Ob0
+
+class Rva005B1A00
+{
+	void *m_vptr;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	char m_14;
+	int m_18;
+	int m_1C;
+	int m_20;
+	int m_24;
+	int m_28;
+	int m_2C;
+	char m_30;
+	int m_34;
+	int m_38;
+
+public:
+	Rva005B1A00();
+};
+
+Rva005B1A00::Rva005B1A00()
+{
+	m_vptr = (void *)0x0110DDFC;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = 0;
+	m_1C = 0;
+	m_20 = 0;
+	m_24 = 0;
+	m_28 = 0;
+	m_2C = 0;
+	m_30 = 0;
+	m_34 = 0;
+	m_38 = 0;
+}
