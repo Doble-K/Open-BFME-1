b1d2ca3074fd6cce6bfd2e5901310b0d329bfcc5
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:22:13 +0000

    Open-BFME5: convert Rva007DB820 ctor (71B at 0x007DB820)

---
+++ Code/GameEngine/Source/Common/Rva007DB820Ctor.cpp
@@ -0,0 +1,54 @@
+// cl: /O2 /Ob0
+
+class Rva007DB820
+{
+	void *m_vptr;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	int m_18;
+	int m_1C;
+	int m_20;
+	int m_24;
+	int m_28;
+	int m_2C;
+	int m_30;
+	int m_34;
+	int m_38;
+	int m_3C;
+	int m_40;
+	int m_44;
+	int m_48;
+	int m_4C;
+	int m_50;
+
+public:
+	Rva007DB820();
+};
+
+Rva007DB820::Rva007DB820()
+{
+	m_vptr = (void *)0x01128C0C;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = 0;
+	m_1C = 0;
+	m_20 = 0;
+	m_24 = 0;
+	m_28 = 0;
+	m_2C = 0;
+	m_30 = 0;
+	m_34 = 0;
+	m_38 = 0;
+	m_3C = 0;
+	m_40 = 0;
+	m_44 = 0;
+	m_48 = 0;
+	m_4C = 0;
+	m_50 = 0;
+}
