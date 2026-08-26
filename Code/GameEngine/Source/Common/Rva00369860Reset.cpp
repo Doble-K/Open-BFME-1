cc2ff6cf943de4f5ad6a26f642c358f76e095c8a
dginovker <danielginovker@gmail.com>
2026-08-26 11:08:35 -0700

    Convert primary state value lookup

--- Code/GameEngine/Source/Common/Rva00369860Reset.cpp
+++ Code/GameEngine/Source/Common/Rva00369860Reset.cpp
@@ -3,6 +3,35 @@
 extern "C" void _ReadWriteBarrier(void);
 #pragma intrinsic(_ReadWriteBarrier)
 
+template <typename T>
+const T &Rva003690A0Min(const T &left, const T &right)
+{
+	return right < left ? right : left;
+}
+
+class Rva00368C10Mask
+{
+public:
+	unsigned int m_bits;
+};
+
+class Rva00368C10Values
+{
+public:
+	int valueFor(const Rva00368C10Mask *mask) const;
+};
+
+class BfmeVisionBonusSource;
+class Rva00369860State;
+
+class Object
+{
+	friend class Rva00369860State;
+
+private:
+	BfmeVisionBonusSource *bfmeGetBonusSource(void) const;
+};
+
 class Rva00369860Pair
 {
 public:
@@ -40,21 +69,19 @@ class Rva00369860State
 {
 public:
 	Rva00369860Range m_pairs;
-	char m_unreconstructed0C[ 8 ];
+	Rva00368C10Mask m_mask;
+	char m_unreconstructed10[ 4 ];
 	int m_mode;
 	int m_field18;
 	int m_firstValues[ 10 ];
 	int m_secondValues[ 10 ];
-	int m_field6C;
-	int m_field70;
-	int m_field74;
-	int m_field78;
-	int m_field7C;
-	int m_field80;
+	int m_primaryValues[ 3 ];
+	int m_secondaryValues[ 3 ];
 	char m_unreconstructed84[ 8 ];
 	bool m_flag8C;
 
 	void reset(int mode);
+	int primaryValueFor(const Object *object) const;
 };
 
 void Rva00369860State::reset(int mode)
@@ -67,12 +94,30 @@ void Rva00369860State::reset(int mode)
 	m_flag8C = false;
 	// Keep the flag store ahead of the saved-register restore, as in retail.
 	_ReadWriteBarrier();
-	m_field6C = 0;
-	m_field78 = 0;
-	m_field70 = 0;
-	m_field7C = 0;
-	m_field74 = 0;
-	m_field80 = 0;
+	m_primaryValues[0] = 0;
+	m_secondaryValues[0] = 0;
+	m_primaryValues[1] = 0;
+	m_secondaryValues[1] = 0;
+	m_primaryValues[2] = 0;
+	m_secondaryValues[2] = 0;
 }
 
 // @?reset@Rva00369860State@@QAEXH@Z 0x00369860
+
+int Rva00369860State::primaryValueFor(const Object *object) const
+{
+	if( !m_flag8C || object == 0 )
+		return m_primaryValues[0];
+
+	// Only this lookup interface is proven; the concrete bonus-source type is opaque.
+	Rva00368C10Values *values =
+		(Rva00368C10Values *)object->bfmeGetBonusSource();
+	if( values == 0 )
+		return m_primaryValues[0];
+
+	int index = values->valueFor(&m_mask) - 1;
+	index = Rva003690A0Min(index, 2);
+	return index >= 0 ? m_primaryValues[index] : m_primaryValues[0];
+}
+
+// @?primaryValueFor@Rva00369860State@@QBEHPBVObject@@@Z 0x003690A0
