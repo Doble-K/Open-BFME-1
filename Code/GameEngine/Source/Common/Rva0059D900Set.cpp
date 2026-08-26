6f9d960fcc10bf3995fab2a5c5779ee603e6d699
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 18:19:00 +0000

    Open-BFME5: convert Rva0059D900Set set (45B at 0x0059D900)

---
+++ Code/GameEngine/Source/Common/Rva0059D900Set.cpp
@@ -0,0 +1,25 @@
+// cl: /O2 /Ob0
+
+class Rva0059D900
+{
+	char pad[8];
+	char m_08;
+	char pad2[7];
+	int m_10, m_14;
+	char pad3[0x14];
+	int m_2C;
+
+public:
+	void set(int v);
+};
+
+void Rva0059D900::set(int v)
+{
+	m_2C = -1;
+	if (v >= m_10 && v <= m_14)
+	{
+		if (v == m_10 || v == m_14)
+			m_08 = 1;
+		m_2C = v;
+	}
+}
