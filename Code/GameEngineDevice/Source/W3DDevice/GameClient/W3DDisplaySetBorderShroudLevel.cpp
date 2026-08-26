8958be9288af8014fa2ccdd128100bbd2b9a5f07
dginovker <danielginovker@gmail.com>
2026-08-26 09:17:50 -0700

    Convert paired W3DDisplay border setter to C++

--- Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DDisplaySetBorderShroudLevel.cpp
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DDisplaySetBorderShroudLevel.cpp
@@ -4,6 +4,7 @@ class BaseHeightMapResetShroud
 {
 public:
 	void setBorderShroudLevel30B8(unsigned char level);
+	void setBorderShroudLevel30BC(unsigned char level);
 };
 
 class BaseHeightMapRenderObjClass
@@ -11,11 +12,17 @@ class BaseHeightMapRenderObjClass
 private:
 	unsigned char m_unmodelled_00[0x30b8];
 	BaseHeightMapResetShroud *m_shroud;
+	BaseHeightMapResetShroud *m_shroud30BC;
 
 public:
 	BaseHeightMapResetShroud *getShroud() const
 	{
 		return m_shroud;
+	}
+
+	BaseHeightMapResetShroud *getShroud30BC() const
+	{
+		return m_shroud30BC;
 	}
 };
 
@@ -25,6 +32,7 @@ class W3DDisplay
 {
 public:
 	virtual void setBorderShroudLevel(unsigned char level);
+	virtual void setBorderShroudLevel30BC(unsigned char level);
 };
 
 // ?setBorderShroudLevel@W3DDisplay@@UAEXE@Z
@@ -32,5 +40,13 @@ void W3DDisplay::setBorderShroudLevel(unsigned char level)
 {
 	if (TheTerrainRenderObject && TheTerrainRenderObject->getShroud()) {
 		TheTerrainRenderObject->getShroud()->setBorderShroudLevel30B8(level);
+	}
+}
+
+// ?setBorderShroudLevel30BC@W3DDisplay@@UAEXE@Z
+void W3DDisplay::setBorderShroudLevel30BC(unsigned char level)
+{
+	if (TheTerrainRenderObject && TheTerrainRenderObject->getShroud30BC()) {
+		TheTerrainRenderObject->getShroud30BC()->setBorderShroudLevel30BC(level);
 	}
 }
