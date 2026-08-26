75a4ae3b9d481cbe4932ba29697f28bfab05864b
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:19:21 +0000

    Open-BFME5: convert Rva00464B20 ctor (47B at 0x00464B20)

---
+++ Code/GameEngine/Source/Common/Rva00464B20Ctor.cpp
@@ -0,0 +1,38 @@
+// cl: /O2 /Ob0
+
+class Rva00464B20
+{
+	void *m_vptr;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	int m_18;
+	int m_1C;
+	int m_20;
+	int m_24;
+	int m_28;
+	int m_2C;
+	int m_30;
+
+public:
+	Rva00464B20();
+};
+
+Rva00464B20::Rva00464B20()
+{
+	m_vptr = (void *)0x010F7114;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = 0;
+	m_1C = 0;
+	m_20 = 0;
+	m_24 = 0;
+	m_28 = 0;
+	m_2C = 0;
+	m_30 = 0;
+}
