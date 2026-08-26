713125c303990489881944b3c44f56d66c75ef9a
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 07:15:24 +0000

    Open-BFME5: convert Rva007B1380 derived ctor (26B at 0x007B1380)

---
+++ Code/GameEngine/Source/Common/Rva007B1380Ctor.cpp
@@ -0,0 +1,26 @@
+// cl: /O2 /Ob0
+
+class Rva007B12F0Base
+{
+public:
+	Rva007B12F0Base();
+	virtual void handle();
+
+private:
+	char m_pad[0x54];
+};
+
+class Rva007B1380 : public Rva007B12F0Base
+{
+	int m_58;
+	int m_5C;
+
+public:
+	Rva007B1380();
+};
+
+Rva007B1380::Rva007B1380()
+{
+	m_58 = 0;
+	m_5C = 0;
+}
