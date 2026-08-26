c53bd6cd242130adce3439dcfc44cb4c963c5cbe
dginovker <danielginovker@gmail.com>
2026-08-26 14:33:31 -0700

    Convert D3DX instruction allocator setter

---
+++ Code/GameEngine/Source/Common/D3DXShaderSetAlloc.cpp
@@ -0,0 +1,25 @@
+// cl: /O1
+// Both absolute stores are present in the archive body: changing the allocator
+// invalidates the associated cached state before publishing the new pointer.
+
+namespace D3DXCore
+{
+class CAlloc;
+}
+
+namespace D3DXShader {
+extern int D3DXData01351400;
+extern D3DXCore::CAlloc * D3DXData01351404;
+
+class CInstruction
+{
+public:
+	static void __stdcall SetAlloc( D3DXCore::CAlloc * allocator );
+};
+
+void __stdcall CInstruction::SetAlloc( D3DXCore::CAlloc * allocator )
+{
+	D3DXData01351400 = 0;
+	D3DXData01351404 = allocator;
+}
+}
