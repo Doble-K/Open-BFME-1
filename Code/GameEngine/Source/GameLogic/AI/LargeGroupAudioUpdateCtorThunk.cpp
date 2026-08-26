1b958c766aa7f0bd102d74d899f5b1c1b98692fb
dginovker <danielginovker@gmail.com>
2026-08-26 01:58:26 -0700

    Convert LargeGroupAudioUpdate constructor

--- Code/GameEngine/Source/GameLogic/AI/LargeGroupAudioUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/AI/LargeGroupAudioUpdateCtorThunk.cpp
@@ -1,165 +1,109 @@
 // cl: /DNDEBUG /MD /EHsc
+// ModuleFactory's 0x68-byte allocation fixes the BFME-only layout. The two
+// small state blocks explain the retail constructor's repeated zero stores.
+
+#include <string.h>
 
 class Thing;
 class ModuleData;
+class Object;
 
-class LargeGroupAudioUpdate
+class LGAU_DeepBase
+{
+public:
+    LGAU_DeepBase(Thing *, const ModuleData *);
+    virtual ~LGAU_DeepBase();
+
+protected:
+    const ModuleData *m_moduleData;
+    Object *m_object;
+};
+
+class LGAU_Iface1 { public: virtual void slot(); };
+class LGAU_Iface2 { public: virtual void slot(); };
+
+class LGAU_UpdateModule : public LGAU_DeepBase,
+                          public LGAU_Iface1,
+                          public LGAU_Iface2
+{
+public:
+    LGAU_UpdateModule(Thing *thing, const ModuleData *moduleData)
+        : LGAU_DeepBase(thing, moduleData),
+          m_indexInLogic(-1), m_updateState(-1),
+          m_nextCallFrameAndPhase(0)
+    {
+    }
+
+private:
+    unsigned int m_nextCallFrameAndPhase;
+    int m_indexInLogic;
+    int m_updateState;
+};
+
+class LGAU_Interface { public: virtual void slot(); };
+
+class LGAU_State3A
+{
+public:
+    LGAU_State3A() : m_f00(0), m_f04(0), m_f08(0) {}
+
+private:
+    unsigned int m_f00;
+    unsigned int m_f04;
+    unsigned int m_f08;
+};
+
+class LGAU_State10
+{
+public:
+    __forceinline LGAU_State10()
+    {
+        memset(this, 0, sizeof(*this));
+    }
+
+private:
+    unsigned int m_f00;
+    unsigned int m_f04;
+    unsigned int m_f08;
+    unsigned int m_f0c;
+    unsigned int m_f10;
+    unsigned int m_f14;
+    unsigned int m_f18;
+    unsigned int m_f1c;
+    unsigned int m_f20;
+    unsigned int m_f24;
+};
+
+class LGAU_State3B
+{
+public:
+    __forceinline LGAU_State3B() { memset(this, 0, sizeof(*this)); }
+
+private:
+    unsigned int m_f00;
+    unsigned int m_f04;
+    unsigned int m_f08;
+};
+
+class LargeGroupAudioUpdate : public LGAU_UpdateModule,
+                              public LGAU_Interface
 {
 public:
     LargeGroupAudioUpdate(Thing *, const ModuleData *);
+
+private:
+    LGAU_State3A m_state24;
+    LGAU_State10 m_state30;
+    LGAU_State3B m_state58;
+    bool m_f64;
+    bool m_f65;
 };
 
 // ??0LargeGroupAudioUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) LargeGroupAudioUpdate::LargeGroupAudioUpdate(Thing *, const ModuleData *)
+LargeGroupAudioUpdate::LargeGroupAudioUpdate(
+    Thing *thing, const ModuleData *moduleData)
+    : LGAU_UpdateModule(thing, moduleData)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0x50;
-        __emit 0x04;
-        __emit 0xd8;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc9;
-        __emit 0xff;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x1c;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x84;
-        __emit 0xf8;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x1c;
-        __emit 0xfa;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0x58;
-        __emit 0xf9;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x4c;
-        __emit 0xf9;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0x40;
-        __emit 0xf9;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x28;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x2c;
-        __emit 0x33;
-        __emit 0xd2;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x30;
-        __emit 0x89;
-        __emit 0x11;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x08;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x0c;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x10;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x1c;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x20;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x24;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x58;
-        __emit 0x89;
-        __emit 0x11;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x04;
-        __emit 0x89;
-        __emit 0x51;
-        __emit 0x08;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x64;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x65;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+    m_f64 = false;
+    m_f65 = false;
 }
