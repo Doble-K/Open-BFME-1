71df4831356d4dae138db361f982915fdc8ab7c5
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:48:29 +0000

    Open-BFME5: convert Rva0045BE40 set (18B at 0x0045BE40)

---
+++ Code/GameEngine/Source/Common/Rva0045BE40Set.cpp
@@ -0,0 +1,20 @@
+// cl: /O2 /Ob0
+
+class Rva0045BE40
+{
+	char m_pad[0x58];
+	int m_58;
+	int m_5C;
+	int m_60;
+	int m_64;
+
+public:
+	void set(int a);
+};
+
+void Rva0045BE40::set(int a)
+{
+	m_58 = a;
+	m_64 = 0;
+	m_60 = 0;
+}
