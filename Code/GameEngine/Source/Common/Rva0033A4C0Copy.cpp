ab4c91e6caefae7e320fa584196c020d46a82570
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 07:44:25 +0000

    Open-BFME5: convert Rva0033A4C0 copy ctor (33B at 0x0033A4C0)

---
+++ Code/GameEngine/Source/Common/Rva0033A4C0Copy.cpp
@@ -0,0 +1,26 @@
+// cl: /O2 /Ob0
+
+class Rva0033CopyBase
+{
+public:
+	Rva0033CopyBase(const Rva0033CopyBase &other);
+
+private:
+	char m_pad[8];
+};
+
+class Rva0033A4C0 : public Rva0033CopyBase
+{
+	int m_08;
+	int m_0C;
+
+public:
+	Rva0033A4C0(const Rva0033A4C0 &other);
+};
+
+Rva0033A4C0::Rva0033A4C0(const Rva0033A4C0 &other)
+	: Rva0033CopyBase(other)
+	, m_08(other.m_08)
+	, m_0C(other.m_0C)
+{
+}
