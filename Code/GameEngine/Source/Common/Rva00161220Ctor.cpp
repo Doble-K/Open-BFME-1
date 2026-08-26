da67c12c970cc93d548f28f86e7171f37d34207c
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:23:08 +0000

    Open-BFME5: convert Rva00161220 ctor (50B at 0x00161220)

---
+++ Code/GameEngine/Source/Common/Rva00161220Ctor.cpp
@@ -0,0 +1,40 @@
+// cl: /O2 /Ob0
+
+class Rva00161220
+{
+	void *m_vptr;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	char m_18;
+	int m_1C;
+	int m_20;
+	int m_24;
+	char m_28;
+	char m_29;
+	char m_2A;
+	int m_2C;
+
+public:
+	Rva00161220();
+};
+
+Rva00161220::Rva00161220()
+{
+	m_vptr = (void *)0x01096940;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = 0;
+	m_1C = 0;
+	m_20 = 0;
+	m_24 = 0;
+	m_28 = 0;
+	m_29 = 0;
+	m_2A = 0;
+	m_2C = 0;
+}
