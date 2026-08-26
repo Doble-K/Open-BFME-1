dc58f7cdbe3f8ac92b30b68ae379c6001628b05c
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:46:22 +0000

    Open-BFME5: convert Rva00494380 set (31B at 0x00494380)

---
+++ Code/GameEngine/Source/Common/Rva00494380Set.cpp
@@ -0,0 +1,21 @@
+// cl: /O2 /Ob0
+
+class Rva00494380
+{
+	char m_pad[0x0C];
+	int m_0C;
+	int m_10;
+	int m_14;
+	int m_18;
+
+public:
+	void set(int a, int b, int c, int d);
+};
+
+void Rva00494380::set(int a, int b, int c, int d)
+{
+	m_0C = a;
+	m_10 = b;
+	m_14 = c;
+	m_18 = d;
+}
