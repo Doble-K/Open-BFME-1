05dcf5a67acdb9d9c9cb5b5b72e7c5248a254927
dginovker <danielginovker@gmail.com>
2026-08-25 19:57:09 -0700

    Open-BFME5: convert PartitionManager cell range helper

---
+++ Code/GameEngine/Source/GameLogic/Object/PartitionManager_getCellRange.cpp
@@ -0,0 +1,37 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc
+
+class PartitionCell
+{
+private:
+	unsigned char m_unreconstructed[0x68];
+};
+
+class PartitionManager
+{
+public:
+	void getCellRange(PartitionCell **first, PartitionCell **last, int x1, int x2, int y);
+
+private:
+	unsigned char m_unreconstructed[0x24];
+	int m_cellCountX;
+	int m_cellCountY;
+	PartitionCell *m_cells;
+};
+
+void PartitionManager::getCellRange(PartitionCell **first, PartitionCell **last, int x1, int x2, int y)
+{
+	if (x2 < 0 || x1 >= m_cellCountX || y < 0 || y >= m_cellCountY)
+	{
+		*last = 0;
+		*first = 0;
+		return;
+	}
+
+	PartitionCell *row = m_cells + y * m_cellCountX;
+	*last = row;
+	*first = row;
+	if (x1 > 0)
+		*first = row + x1;
+
+	*last += x2 < m_cellCountX ? x2 + 1 : m_cellCountX;
+}
