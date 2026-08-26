9625f336df39513c113b9eb418b951066a720729
dginovker <danielginovker@gmail.com>
2026-08-26 01:39:57 -0700

    Convert FlammableUpdate constructor

--- Code/GameEngine/Source/GameLogic/Object/Update/FlammableUpdateConstructorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/FlammableUpdateConstructorThunk.cpp
@@ -1,204 +1,90 @@
 // cl: /DNDEBUG /MD /EHsc
+// ModuleFactory's 0x50-byte allocation proves the compact BFME layout. Zero
+// Hour names the fields through +0x3C; the remaining tail is BFME-only state
+// whose initialization order is fixed by the retail constructor.
 
 class Thing;
 class ModuleData;
+class Object;
+enum UpdateSleepTime { UPDATE_SLEEP_FOREVER = 0x3fffffff };
 
-class FlammableUpdate
+class FU_DeepBase
+{
+public:
+    FU_DeepBase(Thing *, const ModuleData *);
+    virtual ~FU_DeepBase();
+
+protected:
+    const ModuleData *m_moduleData;
+    Object *m_object;
+};
+
+class FU_Iface1 { public: virtual void slot(); };
+class FU_Iface2 { public: virtual void slot(); };
+
+class UpdateModule : public FU_DeepBase, public FU_Iface1, public FU_Iface2
+{
+public:
+    UpdateModule(Thing *thing, const ModuleData *moduleData)
+        : FU_DeepBase(thing, moduleData), m_f14(0), m_f18(-1), m_f1c(-1) {}
+
+protected:
+    void setWakeFrame(Object *, UpdateSleepTime);
+    Object *getObject() const { return m_object; }
+    const ModuleData *getModuleData() const { return m_moduleData; }
+
+private:
+    unsigned int m_f14;
+    int m_f18;
+    int m_f1c;
+};
+
+class DamageModuleInterface { public: virtual void slot(); };
+
+class FlammableUpdateModuleData
+{
+private:
+    unsigned char m_pad[0x1c];
+
+public:
+    float m_flameDamageLimit;
+};
+
+class FlammableUpdate : public UpdateModule, public DamageModuleInterface
 {
 public:
     FlammableUpdate(Thing *, const ModuleData *);
+
+private:
+    int m_status;
+    unsigned int m_aflameEndFrame;
+    unsigned int m_burnedEndFrame;
+    unsigned int m_damageEndFrame;
+    void *m_audioHandle;
+    float m_flameDamageLimit;
+    unsigned int m_lastFlameDamageDealt;
+    bool m_flag40;
+    unsigned int m_field44;
+    unsigned int m_field48;
+    bool m_flag4c;
 };
 
 // ??0FlammableUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) FlammableUpdate::FlammableUpdate(Thing *, const ModuleData *)
+FlammableUpdate::FlammableUpdate(
+    Thing *thing, const ModuleData *moduleData)
+    : UpdateModule(thing, moduleData)
 {
-    __asm {
-        __emit 0x6a
-        __emit 0xff
-        __emit 0x68
-        __emit 0xf8
-        __emit 0x1a
-        __emit 0x01
-        __emit 0x01
-        __emit 0x64
-        __emit 0xa1
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x50
-        __emit 0x64
-        __emit 0x89
-        __emit 0x25
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x51
-        __emit 0x8b
-        __emit 0x44
-        __emit 0x24
-        __emit 0x18
-        __emit 0x56
-        __emit 0x8b
-        __emit 0xf1
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x18
-        __emit 0x50
-        __emit 0x51
-        __emit 0x8b
-        __emit 0xce
-        __emit 0x89
-        __emit 0x74
-        __emit 0x24
-        __emit 0x0c
-        __emit 0xe8
-        __emit 0x46
-        __emit 0x3e
-        __emit 0xd8
-        __emit 0xff
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0xd0
-        __emit 0xc9
-        __emit 0x09
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0xa0
-        __emit 0xcb
-        __emit 0x09
-        __emit 0x01
-        __emit 0x83
-        __emit 0xc9
-        __emit 0xff
-        __emit 0x33
-        __emit 0xc0
-        __emit 0x89
-        __emit 0x46
-        __emit 0x14
-        __emit 0x89
-        __emit 0x4e
-        __emit 0x18
-        __emit 0x89
-        __emit 0x4e
-        __emit 0x1c
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x20
-        __emit 0xfc
-        __emit 0x1b
-        __emit 0x0a
-        __emit 0x01
-        __emit 0x8b
-        __emit 0x56
-        __emit 0x04
-        __emit 0xc7
-        __emit 0x06
-        __emit 0x3c
-        __emit 0xef
-        __emit 0x0b
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x0c
-        __emit 0x78
-        __emit 0xee
-        __emit 0x0b
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x10
-        __emit 0x6c
-        __emit 0xee
-        __emit 0x0b
-        __emit 0x01
-        __emit 0xc7
-        __emit 0x46
-        __emit 0x20
-        __emit 0x5c
-        __emit 0xee
-        __emit 0x0b
-        __emit 0x01
-        __emit 0x89
-        __emit 0x46
-        __emit 0x24
-        __emit 0x89
-        __emit 0x46
-        __emit 0x28
-        __emit 0x89
-        __emit 0x46
-        __emit 0x2c
-        __emit 0x89
-        __emit 0x46
-        __emit 0x30
-        __emit 0x89
-        __emit 0x46
-        __emit 0x34
-        __emit 0x8b
-        __emit 0x4a
-        __emit 0x1c
-        __emit 0x8b
-        __emit 0x56
-        __emit 0x08
-        __emit 0x68
-        __emit 0xff
-        __emit 0xff
-        __emit 0xff
-        __emit 0x3f
-        __emit 0x89
-        __emit 0x4e
-        __emit 0x38
-        __emit 0x52
-        __emit 0x8b
-        __emit 0xce
-        __emit 0x89
-        __emit 0x44
-        __emit 0x24
-        __emit 0x18
-        __emit 0x89
-        __emit 0x46
-        __emit 0x3c
-        __emit 0x88
-        __emit 0x46
-        __emit 0x40
-        __emit 0x89
-        __emit 0x46
-        __emit 0x44
-        __emit 0x89
-        __emit 0x46
-        __emit 0x48
-        __emit 0x88
-        __emit 0x46
-        __emit 0x4c
-        __emit 0xe8
-        __emit 0xc3
-        __emit 0x24
-        __emit 0xd8
-        __emit 0xff
-        __emit 0x8b
-        __emit 0x4c
-        __emit 0x24
-        __emit 0x08
-        __emit 0x8b
-        __emit 0xc6
-        __emit 0x5e
-        __emit 0x64
-        __emit 0x89
-        __emit 0x0d
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x00
-        __emit 0x83
-        __emit 0xc4
-        __emit 0x10
-        __emit 0xc2
-        __emit 0x08
-        __emit 0x00
-    }
+    m_status = 0;
+    m_aflameEndFrame = 0;
+    m_burnedEndFrame = 0;
+    m_damageEndFrame = 0;
+    m_audioHandle = 0;
+    m_flameDamageLimit =
+        ((const FlammableUpdateModuleData *)getModuleData())->m_flameDamageLimit;
+    m_lastFlameDamageDealt = 0;
+    m_flag40 = false;
+    m_field44 = 0;
+    m_field48 = 0;
+    m_flag4c = false;
+    setWakeFrame(getObject(), UPDATE_SLEEP_FOREVER);
 }
