531928e27fe70cb5e12b6323cafeb82921ec0ed3
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 12:27:16 +0000

    Open-BFME5: convert Rva000ED2D0 ctor (79B at 0x000ED2D0)

---
+++ Code/GameEngine/Source/Common/Rva000ED2D0Ctor.cpp
@@ -0,0 +1,50 @@
+// cl: /O2 /Ob0
+
+class Rva000ED2D0
+{
+	void *m_00;
+	int m_04;
+	unsigned short m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	int m_18;
+	char m_1C;
+	char m_1D;
+	int m_20;
+	int m_24;
+	int m_28;
+	int m_2C;
+	int m_30;
+	int m_34;
+	int m_38;
+	int m_3C;
+	int m_40;
+	int m_44;
+
+public:
+	Rva000ED2D0();
+};
+
+Rva000ED2D0::Rva000ED2D0()
+{
+	m_00 = (void *)0x01085DE0;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0x16;
+	m_10 = 0x0F;
+	m_14 = 0;
+	m_18 = 0;
+	m_1C = 0;
+	m_1D = 1;
+	m_20 = 0;
+	m_24 = 0;
+	m_28 = 0;
+	m_2C = 0;
+	m_30 = 0;
+	m_34 = 0;
+	m_38 = 0;
+	m_3C = 0;
+	m_40 = 0;
+	m_44 = 0x3F800000;
+}
