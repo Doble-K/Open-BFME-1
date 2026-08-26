df5eaf4d2c01df4ad3c354dcf8b62e290842003e
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:19:48 +0000

    Open-BFME5: convert Rva007D6B70 ctor (62B at 0x007D6B70)

---
+++ Code/GameEngine/Source/Common/Rva007D6B70Ctor.cpp
@@ -0,0 +1,48 @@
+// cl: /O2 /Ob0
+
+class Rva007D6B70
+{
+	void *m_vptr;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	int m_18;
+	int m_1C;
+	int m_20;
+	int m_24;
+	int m_28;
+	int m_2C;
+	int m_30;
+	int m_34;
+	int m_38;
+	int m_3C;
+	int m_40;
+	int m_44;
+
+public:
+	Rva007D6B70();
+};
+
+Rva007D6B70::Rva007D6B70()
+{
+	m_vptr = (void *)0x01128B24;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = 0;
+	m_1C = 0;
+	m_20 = 0;
+	m_24 = 0;
+	m_28 = 0;
+	m_2C = 0;
+	m_30 = 0;
+	m_34 = 0;
+	m_38 = 0;
+	m_3C = 0;
+	m_40 = 0;
+	m_44 = 0;
+}
