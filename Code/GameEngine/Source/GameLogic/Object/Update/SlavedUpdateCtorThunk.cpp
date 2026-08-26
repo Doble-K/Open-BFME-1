2f35602e6ed1b4683799117391d17d5556b23127
dginovker <danielginovker@gmail.com>
2026-08-26 01:32:17 -0700

    Convert SlavedUpdate constructor

--- Code/GameEngine/Source/GameLogic/Object/Update/SlavedUpdateCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/SlavedUpdateCtorThunk.cpp
@@ -3,127 +3,80 @@
 class Thing;
 class ModuleData;
 
-class SlavedUpdate
+class PB_DeepBase
+{
+public:
+    PB_DeepBase(Thing *, const ModuleData *);
+    virtual ~PB_DeepBase();
+
+private:
+    void *m_unmodelled04;
+    void *m_unmodelled08;
+};
+
+class PB_Iface1
+{
+public:
+    virtual void slot();
+};
+
+class PB_Iface2
+{
+public:
+    virtual void slot();
+};
+
+class UpdateModule : public PB_DeepBase, public PB_Iface1, public PB_Iface2
+{
+public:
+    UpdateModule(Thing *thing, const ModuleData *moduleData)
+        : PB_DeepBase(thing, moduleData), m_nextCallFrame(0), m_moduleIndex(-1), m_wakeFrame(-1)
+    {
+    }
+
+private:
+    unsigned int m_nextCallFrame;
+    int m_moduleIndex;
+    int m_wakeFrame;
+};
+
+class SlavedUpdateInterface
+{
+public:
+    virtual void slot();
+};
+
+struct SlavedGuardPointOffset
+{
+    SlavedGuardPointOffset() : x(0), y(0), z(0) {}
+
+    unsigned int x;
+    unsigned int y;
+    unsigned int z;
+};
+
+class SlavedUpdate : public UpdateModule, public SlavedUpdateInterface
 {
 public:
     SlavedUpdate(Thing *, const ModuleData *);
+
+private:
+    unsigned int m_slaver;
+    SlavedGuardPointOffset m_guardPointOffset;
+    int m_framesToWait;
+    int m_repairState;
+    bool m_repairing;
+    unsigned int m_unmodelled40;
 };
 
 // ??0SlavedUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
-__declspec(naked) SlavedUpdate::SlavedUpdate(Thing *, const ModuleData *)
+SlavedUpdate::SlavedUpdate(Thing *thing, const ModuleData *moduleData)
+    : UpdateModule(thing, moduleData),
+      m_slaver(0),
+      m_guardPointOffset(),
+      m_framesToWait(0),
+      m_repairState(0),
+      m_repairing(false),
+      m_unmodelled40(0)
 {
-    __asm {
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0xe8;
-        __emit 0x70;
-        __emit 0x2e;
-        __emit 0xd7;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x83;
-        __emit 0xc9;
-        __emit 0xff;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x1c;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0xa4;
-        __emit 0x2b;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x24;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xa4;
-        __emit 0x2c;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xe0;
-        __emit 0x2b;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xd4;
-        __emit 0x2b;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0xbc;
-        __emit 0x2b;
-        __emit 0x0c;
-        __emit 0x01;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x28;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x2c;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x30;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x34;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x38;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x3c;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x40;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
 }
