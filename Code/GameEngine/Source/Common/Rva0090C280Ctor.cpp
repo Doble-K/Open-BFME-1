654aa2e2a776f180b07f00cae4b8c5388f0b448b
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:48:16 +0000

    Open-BFME5: convert Rva0090C280 ctor (71B at 0x0090C280)

---
+++ Code/GameEngine/Source/Common/Rva0090C280Ctor.cpp
@@ -0,0 +1,48 @@
+// cl: /O2 /Ob0
+
+class Rva0090C280
+{
+	void *m_vptr;
+	char m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	int m_18;
+	int m_1C;
+	int m_20;
+	int m_24;
+	int m_28;
+	int m_2C;
+	int m_30;
+	int m_34;
+	int m_38;
+	int m_3C;
+	int m_40;
+	int m_44;
+
+public:
+	Rva0090C280();
+};
+
+Rva0090C280::Rva0090C280()
+{
+	m_vptr = (void *)0x0113A56C;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = 0;
+	m_1C = 0;
+	m_20 = 0;
+	m_24 = 1;
+	m_28 = 1;
+	m_2C = 1;
+	m_30 = 1;
+	m_34 = 0;
+	m_38 = 0;
+	m_3C = 0;
+	m_40 = 2;
+	m_44 = 0;
+}
