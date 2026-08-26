65b5e1a8bf2363703b32e6af6db7aaef88022b1c
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 12:40:52 +0000

    Open-BFME5: convert Rva004D8EE0Ctor ctor (72B at 0x004D8EE0)

---
+++ Code/GameEngine/Source/Common/Rva004D8EE0Ctor.cpp
@@ -0,0 +1,32 @@
+// cl: /O2 /Ob0
+
+class Rva004D8EE0
+{
+	int m_00;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	int m_18;
+	int m_1C;
+	int m_20;
+	int m_24;
+
+public:
+	Rva004D8EE0();
+};
+
+Rva004D8EE0::Rva004D8EE0()
+{
+	m_00 = 0;
+	m_04 = 5;
+	m_08 = 10;
+	m_0C = 30;
+	m_10 = 50;
+	m_14 = 150;
+	m_18 = 300;
+	m_1C = 500;
+	m_20 = 800;
+	m_24 = 1500;
+}
