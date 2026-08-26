ed36e6aa66e189cef8ba7eac3a9b8dced5db7fde
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:20:15 +0000

    Open-BFME5: convert Rva003B7E90 ctor (47B at 0x003B7E90)

---
+++ Code/GameEngine/Source/Common/Rva003B7E90Ctor.cpp
@@ -0,0 +1,38 @@
+// cl: /O2 /Ob0
+
+class Rva003B7E90
+{
+	void *m_vptr;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	char m_18;
+	char m_19;
+	int m_1C;
+	int m_20;
+	int m_24;
+	int m_28;
+	char m_2C;
+
+public:
+	Rva003B7E90();
+};
+
+Rva003B7E90::Rva003B7E90()
+{
+	m_vptr = (void *)0x010EC774;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = 0;
+	m_19 = 0;
+	m_1C = 0;
+	m_20 = 0;
+	m_24 = 0;
+	m_28 = 0;
+	m_2C = 0;
+}
