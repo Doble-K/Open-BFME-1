edfda6b4d6a5d99b305e29cd8c8e3cf2f1630826
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:06:24 +0000

    Open-BFME5: convert Rva001B74A0 ctor (28B at 0x001B74A0)

---
+++ Code/GameEngine/Source/Common/Rva001B74A0Ctor.cpp
@@ -0,0 +1,26 @@
+// cl: /O2 /Ob0
+
+class Rva001B74A0
+{
+	void *m_vptr;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	int m_18;
+	int m_1C;
+
+public:
+	Rva001B74A0(int dummy);
+};
+
+Rva001B74A0::Rva001B74A0(int)
+{
+	m_vptr = (void *)0x0109DF3C;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0;
+	m_18 = 0;
+	m_1C = 0;
+}
