aa9412d4b890643921c5e339c78f60ba7a7a2e60
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 09:35:24 +0000

    Open-BFME5: convert Rva0013A0C0 assign (175B at 0x0013A0C0)

---
+++ Code/GameEngine/Source/Common/Rva0013A0C0Assign.cpp
@@ -0,0 +1,57 @@
+// cl: /O2 /Ob0
+
+class Rva0036CA00Str
+{
+public:
+	Rva0036CA00Str &operator=(const Rva0036CA00Str &other);
+
+private:
+	void *m_item;
+};
+
+class Rva0013A0C0
+{
+	Rva0036CA00Str m_00;
+	Rva0036CA00Str m_04;
+	Rva0036CA00Str m_08;
+	Rva0036CA00Str m_0C;
+	Rva0036CA00Str m_10;
+	int m_14;
+	int m_18;
+	Rva0036CA00Str m_1C;
+	Rva0036CA00Str m_20;
+	int m_24;
+	Rva0036CA00Str m_28;
+	int m_2C;
+	int m_30;
+	int m_34;
+	int m_38;
+	Rva0036CA00Str m_3C;
+	int m_40;
+	Rva0036CA00Str m_44;
+
+public:
+	void operator=(const Rva0013A0C0 &other);
+};
+
+void Rva0013A0C0::operator=(const Rva0013A0C0 &other)
+{
+	m_00 = other.m_00;
+	m_04 = other.m_04;
+	m_08 = other.m_08;
+	m_0C = other.m_0C;
+	m_10 = other.m_10;
+	m_14 = other.m_14;
+	m_18 = other.m_18;
+	m_1C = other.m_1C;
+	m_20 = other.m_20;
+	m_24 = other.m_24;
+	m_28 = other.m_28;
+	m_2C = other.m_2C;
+	m_30 = other.m_30;
+	m_34 = other.m_34;
+	m_38 = other.m_38;
+	m_3C = other.m_3C;
+	m_40 = other.m_40;
+	m_44 = other.m_44;
+}
