4043ef765aa0640033d86aa5537ee1055367e317
dginovker <danielginovker@gmail.com>
2026-08-26 02:47:41 -0700

    Convert BuildingBehavior constructor

--- Code/GameEngine/Source/GameLogic/Object/Behavior/BuildingBehaviorCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/BuildingBehaviorCtorThunk.cpp
@@ -1,154 +1,61 @@
 // cl: /DNDEBUG /MD /EHsc
+// The named ModuleFactory entry reaches this constructor through its unique
+// 0x24-byte allocation, fixing the two bytes of derived state.
 
 class Thing;
 class ModuleData;
+class Object;
+enum UpdateSleepTime { UPDATE_SLEEP_NONE = 1 };
 
-class BuildingBehavior
+class BB_DeepBase
+{
+public:
+    BB_DeepBase(Thing *, const ModuleData *);
+    virtual ~BB_DeepBase();
+
+protected:
+    const ModuleData *m_moduleData;
+    Object *m_object;
+};
+
+class BB_Iface1 { public: virtual void slot(); };
+class BB_Iface2 { public: virtual void slot(); };
+
+class UpdateModule : public BB_DeepBase, public BB_Iface1, public BB_Iface2
+{
+public:
+    UpdateModule(Thing *thing, const ModuleData *moduleData)
+        : BB_DeepBase(thing, moduleData),
+          m_indexInLogic(-1), m_updateState(-1),
+          m_nextCallFrameAndPhase(0)
+    {
+    }
+
+protected:
+    void setWakeFrame(Object *, UpdateSleepTime);
+    Object *getObject() const { return m_object; }
+
+private:
+    unsigned int m_nextCallFrameAndPhase;
+    int m_indexInLogic;
+    int m_updateState;
+};
+
+class BuildingBehavior : public UpdateModule
 {
 public:
     BuildingBehavior(Thing *, const ModuleData *);
+
+private:
+    bool m_field20;
+    bool m_field21;
 };
 
-__declspec(naked) BuildingBehavior::BuildingBehavior(Thing *, const ModuleData *)
+// ??0BuildingBehavior@@QAE@PAVThing@@PBVModuleData@@@Z
+BuildingBehavior::BuildingBehavior(Thing *thing, const ModuleData *moduleData)
+    : UpdateModule(thing, moduleData)
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0xa8;
-        __emit 0xad;
-        __emit 0x00;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0x50;
-        __emit 0x51;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0xe8;
-        __emit 0x36;
-        __emit 0x09;
-        __emit 0xe2;
-        __emit 0xff;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0xd0;
-        __emit 0xc9;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0xa0;
-        __emit 0xcb;
-        __emit 0x09;
-        __emit 0x01;
-        __emit 0x83;
-        __emit 0xc9;
-        __emit 0xff;
-        __emit 0x33;
-        __emit 0xc0;
-        __emit 0x89;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x4e;
-        __emit 0x1c;
-        __emit 0x8b;
-        __emit 0x56;
-        __emit 0x08;
-        __emit 0x6a;
-        __emit 0x01;
-        __emit 0x52;
-        __emit 0x8b;
-        __emit 0xce;
-        __emit 0x89;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x18;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0x54;
-        __emit 0x2c;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x0c;
-        __emit 0x90;
-        __emit 0x2b;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x10;
-        __emit 0x84;
-        __emit 0x2b;
-        __emit 0x0a;
-        __emit 0x01;
-        __emit 0x88;
-        __emit 0x46;
-        __emit 0x20;
-        __emit 0xc6;
-        __emit 0x46;
-        __emit 0x21;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0xe4;
-        __emit 0xef;
-        __emit 0xe1;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc2;
-        __emit 0x08;
-        __emit 0x00;
-    }
+    m_field20 = false;
+    m_field21 = true;
+    setWakeFrame(getObject(), UPDATE_SLEEP_NONE);
 }
