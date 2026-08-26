f85d132d636b8f5d2ddf227c31389caeaf3fbe24
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:06:01 +0000

    Open-BFME5: convert Rva000948E0 ctor (19B at 0x000948E0)

---
+++ Code/GameEngine/Source/Common/Rva000948E0Ctor.cpp
@@ -0,0 +1,18 @@
+// cl: /O2 /Ob0
+
+class Rva000948E0
+{
+	void *m_vptr;
+	int m_04;
+	char m_08;
+
+public:
+	Rva000948E0(int dummy);
+};
+
+Rva000948E0::Rva000948E0(int)
+{
+	m_vptr = (void *)0x0107FCB0;
+	m_04 = 0;
+	m_08 = 0;
+}
