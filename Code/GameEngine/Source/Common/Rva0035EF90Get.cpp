f20878e3ffd81300e1eb79973f1b33dd868a5e0e
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 12:02:56 +0000

    Open-BFME5: convert Rva0035EF90 get (28B at 0x0035EF90)

---
+++ Code/GameEngine/Source/Common/Rva0035EF90Get.cpp
@@ -0,0 +1,22 @@
+// cl: /O2 /Ob0
+
+class Rva0035EF90
+{
+	char m_pad[0x28];
+	int m_count;
+	char m_mid[0x4D - 0x2C];
+	struct Slot
+	{
+		char rest[0x61];
+	} m_slots[1];
+
+public:
+	void *get(int index);
+};
+
+void *Rva0035EF90::get(int index)
+{
+	if (index < 0 || index >= m_count)
+		return 0;
+	return &m_slots[index];
+}
