6f834090cdb8e4ba9c40c40ab6e053c774213de8
dginovker <danielginovker@gmail.com>
2026-08-26 03:48:22 -0700

    Convert CommandSetUpgrade ModuleData constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/Upgrade/CommandSetUpgradeModuleDataCtor.cpp
@@ -0,0 +1,60 @@
+// cl: /DNDEBUG /MD /EHsc
+// ModuleFactory's verified allocator fixes the BFME object at 0x74 bytes.
+// EA's class declaration and the clean destructor independently place the
+// UpgradeModuleData subobject at +0x08 and its AsciiString at +0x70.
+
+class AsciiString
+{
+public:
+	AsciiString() : m_data( 0 ) {}
+	~AsciiString();
+	AsciiString &operator=( const AsciiString &other );
+
+private:
+	char *m_data;
+};
+
+extern AsciiString TheBfmeCrateNameDefault;
+
+class CommandSetUpgradeModuleDataBase
+{
+public:
+	CommandSetUpgradeModuleDataBase();
+	virtual ~CommandSetUpgradeModuleDataBase();
+
+private:
+	unsigned char m_data[0x64];
+};
+
+class __declspec(novtable) CommandSetUpgradeModuleDataPrimaryBase
+{
+public:
+	virtual void moduleDataAnchor();
+
+private:
+	unsigned int m_field04;
+};
+
+class __declspec(novtable) CommandSetUpgradeModuleDataIntermediateBase
+	: public CommandSetUpgradeModuleDataPrimaryBase
+{
+protected:
+	CommandSetUpgradeModuleDataBase m_upgradeModuleData;
+};
+
+class CommandSetUpgradeModuleData : public CommandSetUpgradeModuleDataIntermediateBase
+{
+public:
+	CommandSetUpgradeModuleData();
+	virtual ~CommandSetUpgradeModuleData();
+	virtual void moduleDataAnchor();
+
+private:
+	AsciiString m_triggerAlt;
+};
+
+// ??0CommandSetUpgradeModuleData@@QAE@XZ
+CommandSetUpgradeModuleData::CommandSetUpgradeModuleData()
+{
+	m_triggerAlt = TheBfmeCrateNameDefault;
+}
