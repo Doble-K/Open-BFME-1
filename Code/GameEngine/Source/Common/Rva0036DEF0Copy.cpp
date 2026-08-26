35bc5d1aeaf9ab83793e739b5705ee4c13d82b49
dginovker <danielginovker@gmail.com>
2026-08-26 11:27:35 -0700

    Convert aggregate string payload copy

---
+++ Code/GameEngine/Source/Common/Rva0036DEF0Copy.cpp
@@ -0,0 +1,35 @@
+class AsciiString
+{
+public:
+	AsciiString(const AsciiString &other);
+
+private:
+	void *m_data;
+};
+
+struct Rva0036DEF0Payload
+{
+	AsciiString m_text;
+	int m_first;
+	int m_second;
+};
+
+struct Rva0036DEF0Result
+{
+	int m_key;
+	Rva0036DEF0Payload m_payload;
+
+	Rva0036DEF0Result(int key, const Rva0036DEF0Payload &payload)
+		: m_key(key), m_payload(payload) {}
+};
+
+extern "C" Rva0036DEF0Result __cdecl Rva0036DEF0Copy(
+	const int &key,
+	const Rva0036DEF0Payload &payload)
+{
+	// VC7 retains this state word around the potentially throwing string copy.
+	volatile int constructionState = 0;
+	return Rva0036DEF0Result(key, payload);
+}
+
+// @_Rva0036DEF0Copy 0x0036DEF0
