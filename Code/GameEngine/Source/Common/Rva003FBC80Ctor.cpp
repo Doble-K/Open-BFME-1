60cb292066affaaca4fec39056ff128572a00d57
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:48:02 +0000

    Open-BFME5: convert Rva003FBC80 ctor (58B at 0x003FBC80)

---
+++ Code/GameEngine/Source/Common/Rva003FBC80Ctor.cpp
@@ -0,0 +1,46 @@
+// cl: /O2 /Ob0
+
+class Rva003FBC80
+{
+	int m_00;
+	int m_04;
+	int m_08;
+	int m_0C;
+	int m_10;
+	int m_14;
+	int m_18;
+	int m_1C;
+	int m_20;
+	int m_24;
+	int m_28;
+	int m_2C;
+	int m_30;
+	char m_34;
+	int m_38;
+	int m_3C;
+	int m_40;
+
+public:
+	Rva003FBC80();
+};
+
+Rva003FBC80::Rva003FBC80()
+{
+	m_00 = 0;
+	m_04 = 0;
+	m_08 = 0;
+	m_0C = 0;
+	m_10 = 0;
+	m_14 = 0;
+	m_18 = -1;
+	m_1C = -1;
+	m_20 = -1;
+	m_24 = -1;
+	m_28 = 0;
+	m_2C = 0;
+	m_30 = 0;
+	m_34 = 0;
+	m_38 = 0;
+	m_3C = 0;
+	m_40 = -1;
+}
