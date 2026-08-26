cc38af6ae50a17597917d332e749154329036487
dginovker <danielginovker@gmail.com>
2026-08-26 16:37:41 -0700

    Open-BFME7: convert a map metadata refresh

---
+++ Code/GameEngine/Source/GameClient/GUI/Gen00525EE0MapRefresh.cpp
@@ -0,0 +1,82 @@
+// cl: /DNDEBUG /MD /EHsc
+
+class AsciiString
+{
+public:
+	AsciiString(const AsciiString &other);
+	~AsciiString();
+
+private:
+	void *m_data;
+};
+
+class GameInfo
+{
+public:
+	AsciiString getMap(void) const;
+};
+
+class MapMetaData
+{
+public:
+	unsigned char m_unmodelled[0x25];
+	bool m_isMultiplayer;
+};
+
+class MapCache
+{
+public:
+	const MapMetaData *findMap(AsciiString mapName);
+};
+
+extern MapCache *TheMapCache;
+
+class Gen00525EE0Owner
+{
+public:
+	virtual void bfmeSlot0(void) = 0;
+	virtual void bfmeSlot1(void) = 0;
+	virtual void bfmeSlot2(void) = 0;
+	virtual void bfmeSlot3(void) = 0;
+	virtual void bfmeSlot4(void) = 0;
+	virtual void bfmeSlot5(void) = 0;
+	virtual void bfmeSlot6(void) = 0;
+	virtual void bfmeSlot7(void) = 0;
+	virtual void bfmeSlot8(void) = 0;
+	virtual bool bfmeContains(GameInfo *game) = 0;
+};
+
+class Gen_00525EE0
+{
+public:
+	void bfmeRefresh(void);
+
+private:
+	unsigned char m_unmodelled[4];
+	Gen00525EE0Owner *m_owner;
+	GameInfo *m_first;
+	GameInfo *m_second;
+	unsigned char m_unmodelled10[0x114];
+	bool m_isMultiplayer;
+};
+
+// The enclosing type has no exposed retail spelling; the real GameInfo,
+// MapCache, and MapMetaData identities are established independently.
+// ?bfmeRefresh@Gen_00525EE0@@QAEXXZ
+void Gen_00525EE0::bfmeRefresh(void)
+{
+	m_isMultiplayer = false;
+
+	if (m_first && !m_owner->bfmeContains(m_first))
+		m_first = 0;
+
+	if (m_second && !m_owner->bfmeContains(m_second))
+		m_second = 0;
+
+	if (m_first)
+	{
+		const MapMetaData *map = TheMapCache->findMap(m_first->getMap());
+		if (map && map->m_isMultiplayer)
+			m_isMultiplayer = true;
+	}
+}
