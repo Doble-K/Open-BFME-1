33ca4152f6f12c0c8f7219bdeb1ed5028885f119
dginovker <danielginovker@gmail.com>
2026-08-26 10:15:36 -0700

    Convert sentinel value selector to clean C++

---
+++ Code/GameEngine/Source/Common/Rva0036CF70SentinelSelection.cpp
@@ -0,0 +1,38 @@
+class Rva0036CF70Values
+{
+public:
+	char m_unreconstructed[ 8 ];
+	int m_fallbackFirst;
+	int m_fallbackSecond;
+	int m_selectedFirst;
+	int m_selectedSecond;
+};
+
+class Rva0036CF70Selection
+{
+public:
+	char m_unreconstructed04[ 4 ];
+	Rva0036CF70Values *m_values;
+	char m_unreconstructed08[ 0x1C ];
+	bool m_useSelected;
+
+	int firstValue(void) const;
+};
+
+int Rva0036CF70Selection::firstValue(void) const
+{
+	Rva0036CF70Values *values = m_values;
+	if( m_useSelected )
+	{
+		int value = values->m_selectedFirst;
+		if( value != -2 )
+			return value;
+	}
+
+	int value = values->m_fallbackFirst;
+	if( value == -2 )
+		value = 1;
+	return value;
+}
+
+// @?firstValue@Rva0036CF70Selection@@QBEHXZ 0x0036CF70
