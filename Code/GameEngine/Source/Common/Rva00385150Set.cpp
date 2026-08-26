456e7cacca56b327aa2c9d561b22ee6d620f7af9
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 08:50:18 +0000

    Open-BFME5: convert Rva00385150 set (35B at 0x00385150)

---
+++ Code/GameEngine/Source/Common/Rva00385150Set.cpp
@@ -0,0 +1,34 @@
+// cl: /O2 /Ob0
+
+class Rva0036CA00Str
+{
+public:
+	Rva0036CA00Str &operator=(const Rva0036CA00Str &other);
+
+private:
+	void *m_item;
+};
+
+struct Rva00385150Extra
+{
+	int a;
+	int b;
+};
+
+class Rva00385150
+{
+	virtual void handle();
+	Rva0036CA00Str m_04;
+	int m_08;
+	int m_0C;
+
+public:
+	void set(const Rva0036CA00Str &s, const Rva00385150Extra *extra);
+};
+
+void Rva00385150::set(const Rva0036CA00Str &s, const Rva00385150Extra *extra)
+{
+	m_04 = s;
+	m_08 = extra->a;
+	m_0C = extra->b;
+}
