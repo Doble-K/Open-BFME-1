beae293a0917124a309053e3b9c312cd63f5c7b5
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 07:14:14 +0000

    Open-BFME5: convert Rva007F4DA0 derived ctor (33B at 0x007F4DA0)

---
+++ Code/GameEngine/Source/Common/Rva007F4DA0Ctor.cpp
@@ -0,0 +1,26 @@
+// cl: /O2 /Ob0
+
+class Snapshot
+{
+public:
+	Snapshot();
+	virtual void handle();
+};
+
+class Rva007F4DA0 : public Snapshot
+{
+	int m_04;
+	char m_08;
+	char m_gap[0x1B];
+	int m_24;
+
+public:
+	Rva007F4DA0();
+};
+
+Rva007F4DA0::Rva007F4DA0()
+{
+	m_08 = 0;
+	m_24 = 0;
+	m_04 = 1;
+}
