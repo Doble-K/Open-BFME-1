bd53f47f61070ec7d454ce9dc7aaccf67279a05b
dginovker <danielginovker@gmail.com>
2026-08-26 03:30:56 -0700

    Convert HitReactionBehavior ModuleData constructor

--- Code/GameEngine/Source/GameLogic/Object/Behavior/HitReactionBehaviorModuleDataCtorThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Behavior/HitReactionBehaviorModuleDataCtorThunk.cpp
@@ -1,127 +1,54 @@
 // cl: /DNDEBUG /MD /EHsc
+// The byte-verified ModuleFactory friend allocator fixes this class at 0x30
+// bytes. Its clean destructor proves the two member subobjects at +0x18/+0x1C.
 
-class HitReactionBehaviorModuleData
+class HRBMD_Buffer
+{
+public:
+    HRBMD_Buffer();
+    ~HRBMD_Buffer();
+
+private:
+    unsigned int m_value;
+};
+
+class HRBMD_Base
+{
+public:
+    virtual ~HRBMD_Base() {}
+
+protected:
+    unsigned int m_field04;
+    unsigned int m_field08;
+    unsigned int m_field0c;
+    unsigned int m_field10;
+    float m_field14;
+};
+
+class HitReactionBehaviorModuleData : public HRBMD_Base
 {
 public:
     HitReactionBehaviorModuleData();
+    virtual ~HitReactionBehaviorModuleData();
+
+private:
+    HRBMD_Buffer m_bufferA;
+    HRBMD_Buffer m_bufferB;
+    unsigned int m_field20;
+    unsigned int m_field24;
+    unsigned int m_field28;
+    unsigned int m_field2c;
 };
 
-__declspec(naked) HitReactionBehaviorModuleData::HitReactionBehaviorModuleData()
+// ??0HitReactionBehaviorModuleData@@QAE@XZ
+HitReactionBehaviorModuleData::HitReactionBehaviorModuleData()
 {
-    __asm {
-        __emit 0x6a;
-        __emit 0xff;
-        __emit 0x68;
-        __emit 0xf3;
-        __emit 0x18;
-        __emit 0x01;
-        __emit 0x01;
-        __emit 0x64;
-        __emit 0xa1;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x50;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x25;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x51;
-        __emit 0x56;
-        __emit 0x8b;
-        __emit 0xf1;
-        __emit 0x57;
-        __emit 0x89;
-        __emit 0x74;
-        __emit 0x24;
-        __emit 0x08;
-        __emit 0x33;
-        __emit 0xff;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x18;
-        __emit 0x89;
-        __emit 0x7c;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0xc7;
-        __emit 0x06;
-        __emit 0xa0;
-        __emit 0xe5;
-        __emit 0x0b;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0xf8;
-        __emit 0x5a;
-        __emit 0xda;
-        __emit 0xff;
-        __emit 0x8d;
-        __emit 0x4e;
-        __emit 0x1c;
-        __emit 0xc6;
-        __emit 0x44;
-        __emit 0x24;
-        __emit 0x14;
-        __emit 0x01;
-        __emit 0xe8;
-        __emit 0xeb;
-        __emit 0x5a;
-        __emit 0xda;
-        __emit 0xff;
-        __emit 0x8b;
-        __emit 0x4c;
-        __emit 0x24;
-        __emit 0x0c;
-        __emit 0x89;
-        __emit 0x7e;
-        __emit 0x0c;
-        __emit 0x89;
-        __emit 0x7e;
-        __emit 0x10;
-        __emit 0x89;
-        __emit 0x7e;
-        __emit 0x20;
-        __emit 0x89;
-        __emit 0x7e;
-        __emit 0x24;
-        __emit 0x89;
-        __emit 0x7e;
-        __emit 0x28;
-        __emit 0x89;
-        __emit 0x7e;
-        __emit 0x2c;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x08;
-        __emit 0x05;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0xc7;
-        __emit 0x46;
-        __emit 0x14;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x96;
-        __emit 0x43;
-        __emit 0x5f;
-        __emit 0x8b;
-        __emit 0xc6;
-        __emit 0x5e;
-        __emit 0x64;
-        __emit 0x89;
-        __emit 0x0d;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x00;
-        __emit 0x83;
-        __emit 0xc4;
-        __emit 0x10;
-        __emit 0xc3;
-    }
+    m_field0c = 0;
+    m_field10 = 0;
+    m_field20 = 0;
+    m_field24 = 0;
+    m_field28 = 0;
+    m_field2c = 0;
+    m_field08 = 5;
+    m_field14 = 300.0f;
 }
