2ae366b3a6a2dd6d0444500da89e930208767351
302596555 <302596555@student.rocmondriaan.nl>
2026-08-26 11:03:40 +0200

    Open-BFME5: replace the TerrainLogic::xfer naked lift with real C++

--- Code/GameEngine/Source/GameLogic/Map/TerrainLogicXferThunk.cpp
+++ Code/GameEngine/Source/GameLogic/Map/TerrainLogicXferThunk.cpp
@@ -1,62 +1,52 @@
 // cl: /DNDEBUG /MD /EHsc
+// Open-BFME5: TerrainLogic::xfer, retail 0x006BE430, 45 bytes.
+//
+// The Zero Hour prologue: a version pair is built on the stack -- both bytes
+// written from one materialised 1, which is what `const XferVersion
+// currentVersion = 1; XferVersion version = currentVersion;` compiles to --
+// handed to Xfer's vftable slot 0x28, and then the rest of the transfer is a
+// single call into the body at 0x001AD370 (ILT 0x00043ED7).
+
+class Xfer;
+
+struct TerrainLogicXferVersion
+{
+	unsigned char m_version;
+	unsigned char m_currentVersion;
+};
 
 class Xfer
 {
+public:
+	virtual void xferSlot00();
+	virtual void xferSlot04();
+	virtual void xferSlot08();
+	virtual void xferSlot0c();
+	virtual void xferSlot10();
+	virtual void xferSlot14();
+	virtual void xferSlot18();
+	virtual void xferSlot1c();
+	virtual void xferSlot20();
+	virtual void xferSlot24();
+	virtual void xferVersion(TerrainLogicXferVersion *versionData);
 };
 
 class TerrainLogic
 {
 protected:
-    virtual void xfer(Xfer *);
+	virtual void xfer(Xfer *xfer);
+	void xferTerrainState(Xfer *xfer);
 };
 
-__declspec(naked) void TerrainLogic::xfer(Xfer *)
+// ?xfer@TerrainLogic@@MAEXPAVXfer@@@Z
+void TerrainLogic::xfer(Xfer *xfer)
 {
-    __asm {
-        _emit 051h
-        _emit 056h
-        _emit 08Bh
-        _emit 074h
-        _emit 024h
-        _emit 00Ch
-        _emit 057h
-        _emit 08Bh
-        _emit 0F9h
-        _emit 0B0h
-        _emit 001h
-        _emit 08Dh
-        _emit 04Ch
-        _emit 024h
-        _emit 008h
-        _emit 088h
-        _emit 044h
-        _emit 024h
-        _emit 008h
-        _emit 088h
-        _emit 044h
-        _emit 024h
-        _emit 009h
-        _emit 08Bh
-        _emit 006h
-        _emit 051h
-        _emit 08Bh
-        _emit 0CEh
-        _emit 0FFh
-        _emit 050h
-        _emit 028h
-        _emit 056h
-        _emit 08Bh
-        _emit 0CFh
-        _emit 0E8h
-        _emit 080h
-        _emit 05Ah
-        _emit 098h
-        _emit 0FFh
-        _emit 05Fh
-        _emit 05Eh
-        _emit 059h
-        _emit 0C2h
-        _emit 004h
-        _emit 000h
-    }
+	const unsigned char currentVersion = 1;
+	TerrainLogicXferVersion version;
+
+	version.m_version = currentVersion;
+	version.m_currentVersion = currentVersion;
+	xfer->xferVersion(&version);
+
+	xferTerrainState(xfer);
 }
