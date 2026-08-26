5de46bce7e21d64e143268e44561f737e07e0e13
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 07:29:47 +0000

    Open-BFME5: convert Rva006982B0 copy ctor (33B at 0x006982B0)

---
+++ Code/GameEngine/Source/Common/Rva006982B0Copy.cpp
@@ -0,0 +1,26 @@
+// cl: /O2 /Ob0
+
+class AudioEventRTS
+{
+public:
+	AudioEventRTS(const AudioEventRTS &other);
+
+private:
+	char m_pad[0x70];
+};
+
+class Rva006982B0 : public AudioEventRTS
+{
+	int m_70;
+	char m_74;
+
+public:
+	Rva006982B0(const Rva006982B0 &other);
+};
+
+Rva006982B0::Rva006982B0(const Rva006982B0 &other)
+	: AudioEventRTS(other)
+	, m_70(other.m_70)
+	, m_74(other.m_74)
+{
+}
