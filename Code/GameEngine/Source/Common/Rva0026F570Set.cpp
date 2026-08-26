f64e759c158236a2766a1b7cdb8c985cc5c9897a
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 11:51:13 +0000

    Open-BFME5: convert Rva0026F570 set (23B at 0x0026F570)

---
+++ Code/GameEngine/Source/Common/Rva0026F570Set.cpp
@@ -0,0 +1,17 @@
+// cl: /O2 /Ob0
+
+class Rva0026F570
+{
+	char m_pad[0x1D8];
+	int m_1D8;
+	int m_1DC;
+
+public:
+	void set(int a);
+};
+
+void Rva0026F570::set(int a)
+{
+	m_1D8 = 3;
+	m_1DC = a;
+}
