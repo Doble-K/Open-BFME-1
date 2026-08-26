f76d240566d4ce1d0eacc2326b605e74cd46f208
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 04:33:58 +0000

    Open-BFME5: convert Rva003826C0::set (17B at 0x003826C0)
    Replace the gen-dump with a thiscall store of two ints at ecx+0x1C and ecx+0x18.

---
+++ Code/GameEngine/Source/Common/Rva003826C0Set.cpp
@@ -0,0 +1,17 @@
+// cl: /O2 /Ob0
+
+class Rva003826C0
+{
+	char m_lead[0x18];
+	int m_18;
+	int m_1c;
+
+public:
+	void set(int a, int b);
+};
+
+void Rva003826C0::set(int a, int b)
+{
+	m_1c = a;
+	m_18 = b;
+}
