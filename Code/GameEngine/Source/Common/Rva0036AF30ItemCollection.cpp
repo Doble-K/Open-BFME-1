1353be28ede3dfe9c18c053152e253a7b7033c4f
dginovker <danielginovker@gmail.com>
2026-08-26 11:03:19 -0700

    Convert bounded item pair lookup

--- Code/GameEngine/Source/Common/Rva0036AF30ItemCollection.cpp
+++ Code/GameEngine/Source/Common/Rva0036AF30ItemCollection.cpp
@@ -1,7 +1,27 @@
+extern "C" void _ReadWriteBarrier(void);
+#pragma intrinsic(_ReadWriteBarrier)
+
+struct Rva00367DC0Pair
+{
+	unsigned int m_key;
+	unsigned int m_value;
+};
+
+class Rva00367DC0PairRange
+{
+public:
+	bool find(unsigned int key, unsigned int *value) const;
+
+private:
+	Rva00367DC0Pair *m_first;
+	Rva00367DC0Pair *m_last;
+};
+
 class Rva0036AF30Item
 {
 public:
-	char m_unreconstructed[ 0x14 ];
+	Rva00367DC0PairRange m_pairs;
+	char m_unreconstructed08[ 0x0C ];
 	int m_key;
 	int m_value;
 };
@@ -24,6 +44,7 @@ public:
 
 	int indexOf(int key) const;
 	int valueAt(int index) const;
+	bool findPairAt(int index, unsigned int key, unsigned int *value) const;
 };
 
 int Rva0036AF30ItemCollection::indexOf(int key) const
@@ -53,3 +74,22 @@ int Rva0036AF30ItemCollection::valueAt(int index) const
 }
 
 // @?valueAt@Rva0036AF30ItemCollection@@QBEHH@Z 0x0036AF70
+
+bool Rva0036AF30ItemCollection::findPairAt(
+	int index,
+	unsigned int key,
+	unsigned int *value) const
+{
+	if( index < 0 || (unsigned int)index > m_items.size() - 1 )
+		return false;
+
+	Rva0036AF30Item **items = m_items.m_begin;
+	if( items[index] == 0 )
+		return false;
+	// Retail independently evaluates the slot for the check and the dispatch.
+	_ReadWriteBarrier();
+
+	return items[index]->m_pairs.find(key, value);
+}
+
+// @?findPairAt@Rva0036AF30ItemCollection@@QBE_NHIPAI@Z 0x0036AFB0
