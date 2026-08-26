3b99868f351f5c208a0c8ea64c00b829349ad0df
Jonathan <1760627+Ancalgonn@users.noreply.github.com>
2026-08-26 10:45:18 +0000

    Open-BFME5: convert Rva0073B880 set (45B at 0x0073B880)

---
+++ Code/GameEngine/Source/Common/Rva0073B880Set.cpp
@@ -0,0 +1,35 @@
+// cl: /O2 /Ob0
+
+class GlobalData
+{
+public:
+	char m_pad[0xA74];
+	unsigned char m_A74;
+};
+
+extern GlobalData *TheWritableGlobalData;
+
+class Rva0073B880
+{
+	char m_pad0[0x58];
+	int m_58;
+	char m_pad1[4];
+	int m_60;
+	int m_64;
+	char m_pad2[0x27D - 0x68];
+	unsigned char m_27D;
+
+public:
+	void set(int value);
+};
+
+void Rva0073B880::set(int value)
+{
+	if (!TheWritableGlobalData->m_A74 || value == 0)
+	{
+		m_58 = value;
+		m_64 = 0;
+		m_60 = 0;
+		m_27D = 0;
+	}
+}
