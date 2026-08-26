91cbb455a9a37f888ce5b586832a86566628f11a
dginovker <danielginovker@gmail.com>
2026-08-26 02:38:43 -0700

    Convert HeightDieUpdate constructor

--- Code/GameEngine/Source/GameLogic/Object/Update/HeightDieUpdateConstructor.cpp
+++ Code/GameEngine/Source/GameLogic/Object/Update/HeightDieUpdateConstructor.cpp
@@ -1,119 +1,67 @@
 // cl: /DNDEBUG /MD /EHsc /D_STLP_USE_STATIC_LIB /D_STLP_NO_EXCEPTIONS /ICode/GameEngine/Source/Common/System /ICode/GameEngine/Include /ICode/GameEngine/Include/Precompiled /ICode/Libraries/Source/WWVegas/WWLib
+// EA's Generals source supplies the member semantics; BFME's ModuleFactory
+// allocation proves that the same layout ends at 0x34 here.
 
-class Thing
-{
-};
+class Thing;
+class ModuleData;
 
-class ModuleData
-{
-};
-
-class HeightDieUpdate
+class HDU_DeepBase
 {
 public:
-	HeightDieUpdate( Thing *, const ModuleData * );
+    HDU_DeepBase(Thing *, const ModuleData *);
+    virtual ~HDU_DeepBase();
+
+protected:
+    const ModuleData *m_moduleData;
+    void *m_object;
 };
 
-__declspec(naked) HeightDieUpdate::HeightDieUpdate( Thing *, const ModuleData * )
-{
-	__asm {
-		_emit 08Bh
-		_emit 044h
-		_emit 024h
-		_emit 008h
-		_emit 056h
-		_emit 08Bh
-		_emit 0F1h
-		_emit 08Bh
-		_emit 04Ch
-		_emit 024h
-		_emit 008h
-		_emit 050h
-		_emit 051h
-		_emit 08Bh
-		_emit 0CEh
-		_emit 0E8h
-		_emit 080h
-		_emit 01Bh
-		_emit 0D8h
-		_emit 0FFh
-		_emit 0C7h
-		_emit 046h
-		_emit 00Ch
-		_emit 0D0h
-		_emit 0C9h
-		_emit 009h
-		_emit 001h
-		_emit 0C7h
-		_emit 046h
-		_emit 010h
-		_emit 0A0h
-		_emit 0CBh
-		_emit 009h
-		_emit 001h
-		_emit 033h
-		_emit 0C0h
-		_emit 089h
-		_emit 046h
-		_emit 014h
-		_emit 088h
-		_emit 046h
-		_emit 020h
-		_emit 088h
-		_emit 046h
-		_emit 021h
-		_emit 0B8h
-		_emit 000h
-		_emit 000h
-		_emit 080h
-		_emit 0BFh
-		_emit 083h
-		_emit 0C9h
-		_emit 0FFh
-		_emit 089h
-		_emit 046h
-		_emit 024h
-		_emit 089h
-		_emit 046h
-		_emit 028h
-		_emit 089h
-		_emit 046h
-		_emit 02Ch
-		_emit 089h
-		_emit 04Eh
-		_emit 018h
-		_emit 089h
-		_emit 04Eh
-		_emit 01Ch
-		_emit 0C7h
-		_emit 006h
-		_emit 0DCh
-		_emit 0F6h
-		_emit 00Bh
-		_emit 001h
-		_emit 0C7h
-		_emit 046h
-		_emit 00Ch
-		_emit 018h
-		_emit 0F6h
-		_emit 00Bh
-		_emit 001h
-		_emit 0C7h
-		_emit 046h
-		_emit 010h
-		_emit 008h
-		_emit 0F6h
-		_emit 00Bh
-		_emit 001h
-		_emit 089h
-		_emit 04Eh
-		_emit 030h
-		_emit 08Bh
-		_emit 0C6h
-		_emit 05Eh
-		_emit 0C2h
-		_emit 008h
-		_emit 000h
-	}
-}
+class HDU_Iface1 { public: virtual void slot(); };
+class HDU_Iface2 { public: virtual void slot(); };
 
+class UpdateModule : public HDU_DeepBase, public HDU_Iface1, public HDU_Iface2
+{
+public:
+    UpdateModule(Thing *thing, const ModuleData *moduleData)
+        : HDU_DeepBase(thing, moduleData),
+          m_indexInLogic(-1), m_updateState(-1),
+          m_nextCallFrameAndPhase(0)
+    {
+    }
+
+private:
+    unsigned int m_nextCallFrameAndPhase;
+    int m_indexInLogic;
+    int m_updateState;
+};
+
+struct Coord3D
+{
+    float x;
+    float y;
+    float z;
+};
+
+class HeightDieUpdate : public UpdateModule
+{
+public:
+    HeightDieUpdate(Thing *, const ModuleData *);
+
+private:
+    bool m_hasDied;
+    bool m_particlesDestroyed;
+    Coord3D m_lastPosition;
+    unsigned int m_earliestDeathFrame;
+};
+
+// ??0HeightDieUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
+HeightDieUpdate::HeightDieUpdate(Thing *thing, const ModuleData *moduleData)
+    : UpdateModule(thing, moduleData)
+{
+    m_hasDied = false;
+    m_particlesDestroyed = false;
+    m_lastPosition.x = -1.0f;
+    m_lastPosition.y = -1.0f;
+    m_lastPosition.z = -1.0f;
+    m_earliestDeathFrame = (unsigned int)-1;
+}
